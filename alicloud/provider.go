1f267ff82b0cde3073e3225908fd754bcd0cf351
Maxim Alferov <malferov44@gmail.com>
2018-05-08 02:29:12 +0200

    add to provider new data source rds instances

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -65,6 +65,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_ram_policies":         dataSourceAlicloudRamPolicies(),
 			"alicloud_security_groups":      dataSourceAlicloudSecurityGroups(),
 			"alicloud_security_group_rules": dataSourceAlicloudSecurityGroupRules(),
+			"alicloud_rds_instances":        dataSourceAlicloudRdsInstances(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"alicloud_instance":                  resourceAliyunInstance(),
