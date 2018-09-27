be69052530b07f9aca983b0fb38bb22084b0ef9a
siyong.dxh <siyong.dxh@taobao.com>
2018-09-27 14:47:02 +0800

    add mns queue

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -196,6 +196,8 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_cen_bandwidth_package":       resourceAlicloudCenBandwidthPackage(),
 			"alicloud_kvstore_instance":            resourceAlicloudKVStoreInstance(),
 			"alicloud_kvstore_backup_policy":       resourceAlicloudKVStoreBackupPolicy(),
+			//mns
+			"alicloud_mns_queue": resourceAlicloudMNSQueue(),
 		},
 
 		ConfigureFunc: providerConfigure,
