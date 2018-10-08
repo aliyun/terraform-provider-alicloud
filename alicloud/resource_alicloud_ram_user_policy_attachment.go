dc403646dbcedb982323dcd5f695309eb815e065
He Guimin <guimin.hgm@alibaba-inc.com>
2018-10-08 15:45:58 +0800

    Fix alicloud_ram_user_policy_attachment EntityNotExist.User error

--- alicloud/resource_alicloud_ram_user_policy_attachment.go
+++ alicloud/resource_alicloud_ram_user_policy_attachment.go
@@ -62,6 +62,13 @@ func resourceAlicloudRamUserPolicyAttachmentCreate(d *schema.ResourceData, meta 
 func resourceAlicloudRamUserPolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AliyunClient).ramconn
 
+	// In order to be compatible with previous Id (before 1.9.6) which format to user<policuy_name><policy_type><user_name>
+	id := fmt.Sprintf("%s%s%s%s%s", d.Get("user_name").(string), COLON_SEPARATED, d.Get("policy_name").(string), COLON_SEPARATED, d.Get("policy_type").(string))
+
+	if d.Id() != id {
+		d.SetId(id)
+	}
+
 	split := strings.Split(d.Id(), COLON_SEPARATED)
 	args := ram.UserQueryRequest{
 		UserName: split[0],
@@ -89,6 +96,14 @@ func resourceAlicloudRamUserPolicyAttachmentRead(d *schema.ResourceData, meta in
 
 func resourceAlicloudRamUserPolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AliyunClient).ramconn
+
+	// In order to be compatible with previous Id (before 1.9.6) which format to user<policuy_name><policy_type><user_name>
+	id := fmt.Sprintf("%s%s%s%s%s", d.Get("user_name").(string), COLON_SEPARATED, d.Get("policy_name").(string), COLON_SEPARATED, d.Get("policy_type").(string))
+
+	if d.Id() != id {
+		d.SetId(id)
+	}
+
 	split := strings.Split(d.Id(), COLON_SEPARATED)
 
 	args := ram.AttachPolicyRequest{
