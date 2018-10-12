1594436099641f5fa830e7f2e9aa4bcb976fc4e6
kevin <subo.ysb@alibaba-inc.com>
2018-10-12 16:37:57 +0800

    add fc example

---
+++ examples/fc/outputs.tf
@@ -0,0 +1,11 @@
+output "fc-service" {
+  value = "${alicloud_fc_service.foo.id}"
+}
+
+output "fc-function" {
+  value = "${alicloud_fc_function.foo.id}"
+}
+
+output "fc-trigger" {
+  value = "${alicloud_fc_trigger.foo.id}"
+}
