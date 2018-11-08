01f28d5e229331b2290630edb09045ec30895ba4
He Guimin <guimin.hgm@alibaba-inc.com>
2018-11-08 10:10:29 +0800

    Fix cs_application cluster not found

--- alicloud/resource_alicloud_network_interface_test.go
+++ alicloud/resource_alicloud_network_interface_test.go
@@ -2,11 +2,12 @@ package alicloud
 
 import (
 	"fmt"
-	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"log"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/hashicorp/terraform/helper/resource"
