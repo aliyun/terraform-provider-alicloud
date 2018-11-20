76d35d58d0a944fd24e03e1fe9c837e35ed8bcd9
郭鹏 <xingsui.gp@alibaba-inc.com>
2018-11-20 19:43:24 +0800

    Add support drds import vpc

--- alicloud/import_alicloud_drds_instance_test.go
+++ alicloud/import_alicloud_drds_instance_test.go
@@ -11,10 +11,29 @@ func TestAccAlicloudDRDSInstance_importBasic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckInstanceDestroy,
+		CheckDestroy: testAccCheckDRDSInstanceDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccDrdsInstance,
+			},
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: false,
+			},
+		},
+	})
+}
+
+func TestAccAlicloudDRDSInstance_importVpc(t *testing.T) {
+	resourceName := "alicloud_drds_instance.vpc"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDRDSInstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDrdsInstance_Vpc,
 			},
 			resource.TestStep{
 				ResourceName:      resourceName,
