269fa07a9e2345d6d85607eab9480d29e5d82971
He Guimin <guimin.hgm@alibaba-inc.com>
2018-12-19 06:49:30 +0800

    Improve kvstore backup policy testcase

--- alicloud/import_alicloud_kvstore_backup_policy_test.go
+++ alicloud/import_alicloud_kvstore_backup_policy_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAlicloudKVStoreBackupPolicy_import(t *testing.T) {
+func TestAccAlicloudKVStoreRedisBackupPolicy_import(t *testing.T) {
 	resourceName := "alicloud_kvstore_backup_policy.policy"
 
 	resource.Test(t, resource.TestCase{
@@ -15,7 +15,28 @@ func TestAccAlicloudKVStoreBackupPolicy_import(t *testing.T) {
 		CheckDestroy: testAccCheckKVStoreBackupPolicyDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccKVStoreBackupPolicy_basic,
+				Config: testAccKVStoreBackupPolicy_classic(string(KVStoreRedis), redisInstanceClassForTest, string(KVStore4Dot0)),
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAlicloudKVStoreMemcacheBackupPolicy_import(t *testing.T) {
+	resourceName := "alicloud_kvstore_backup_policy.policy"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckKVStoreBackupPolicyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccKVStoreBackupPolicy_vpc(DatabaseCommonTestCase, string(KVStoreMemcache), memcacheInstanceClassForTest, string(KVStore2Dot8)),
 			},
 
 			resource.TestStep{
