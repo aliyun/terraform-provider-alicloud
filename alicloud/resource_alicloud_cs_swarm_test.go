6103ea8483f11ccac5e1d43cb8cb7057fa4dbc7b
arafato <oliver.arafat@outlook.com>
2019-01-08 13:30:38 +0100

    fixes #589

--- alicloud/resource_alicloud_cs_swarm_test.go
+++ alicloud/resource_alicloud_cs_swarm_test.go
@@ -101,6 +101,73 @@ func TestAccAlicloudCSSwarm_vpc(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudCSSwarm_vpc_zero_node(t *testing.T) {
+	var container cs.ClusterType
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		IDRefreshName: "alicloud_cs_swarm.cs_vpc",
+
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSwarmClusterDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCSSwarm_basic_zero_node,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerClusterExists("alicloud_cs_swarm.cs_vpc", &container),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "node_number", "0"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "nodes.#", "0"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_category", ""),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_size", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAlicloudCSSwarm_vpc_zero_node_update(t *testing.T) {
+	var container cs.ClusterType
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		IDRefreshName: "alicloud_cs_swarm.cs_vpc",
+
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSwarmClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCSSwarm_basic_zero_node,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerClusterExists("alicloud_cs_swarm.cs_vpc", &container),
+					testAccCheckContainerClusterExists("alicloud_cs_swarm.cs_vpc", &container),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "node_number", "0"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "nodes.#", "0"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_category", ""),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_size", "0"),
+				),
+			},
+
+			{
+				Config: testAccCSSwarm_basic_zero_node_update,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContainerClusterExists("alicloud_cs_swarm.cs_vpc", &container),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "node_number", "2"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "name", "testAccCSSwarm-basic-zero-node-update"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "nodes.#", "2"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_category", "cloud_efficiency"),
+					resource.TestCheckResourceAttr("alicloud_cs_swarm.cs_vpc", "disk_size", "20"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccAlicloudCSSwarm_vpc_noslb(t *testing.T) {
 	var container cs.ClusterType
 
@@ -260,6 +327,90 @@ resource "alicloud_cs_swarm" "cs_vpc" {
   image_id = "${data.alicloud_images.main.images.0.id}"
   vswitch_id = "${alicloud_vswitch.foo.id}"
   release_eip = "true"
+}
+`
+const testAccCSSwarm_basic_zero_node = `
+variable "name" {
+	default = "testAccCSSwarm-basic-zero-node"
+}
+data "alicloud_images" main {
+	most_recent = true
+	name_regex = "^centos_6\\w{1,5}[64].*"
+}
+
+data "alicloud_zones" main {
+  	available_resource_creation = "VSwitch"
+}
+data "alicloud_instance_types" "default" {
+ 	availability_zone = "${data.alicloud_zones.main.zones.0.id}"
+	cpu_core_count = 1
+	memory_size = 2
+}
+
+resource "alicloud_vpc" "foo" {
+  name = "${var.name}"
+  cidr_block = "10.1.0.0/21"
+}
+
+resource "alicloud_vswitch" "foo" {
+  name = "${var.name}"
+  vpc_id = "${alicloud_vpc.foo.id}"
+  cidr_block = "10.1.1.0/24"
+  availability_zone = "${data.alicloud_zones.main.zones.0.id}"
+}
+
+resource "alicloud_cs_swarm" "cs_vpc" {
+  password = "Just$test"
+  instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
+  name_prefix = "${var.name}"
+  node_number = 0
+  cidr_block = "172.20.0.0/24"
+  image_id = "${data.alicloud_images.main.images.0.id}"
+  vswitch_id = "${alicloud_vswitch.foo.id}"
+  release_eip = "true"
+}
+`
+
+const testAccCSSwarm_basic_zero_node_update = `
+variable "name" {
+	default = "testAccCSSwarm-basic-zero-node-update"
+}
+data "alicloud_images" main {
+	most_recent = true
+	name_regex = "^centos_6\\w{1,5}[64].*"
+}
+
+data "alicloud_zones" main {
+  	available_resource_creation = "VSwitch"
+}
+data "alicloud_instance_types" "default" {
+ 	availability_zone = "${data.alicloud_zones.main.zones.0.id}"
+	cpu_core_count = 1
+	memory_size = 2
+}
+
+resource "alicloud_vpc" "foo" {
+  name = "${var.name}"
+  cidr_block = "10.1.0.0/21"
+}
+
+resource "alicloud_vswitch" "foo" {
+  name = "${var.name}"
+  vpc_id = "${alicloud_vpc.foo.id}"
+  cidr_block = "10.1.1.0/24"
+  availability_zone = "${data.alicloud_zones.main.zones.0.id}"
+}
+
+resource "alicloud_cs_swarm" "cs_vpc" {
+  password = "Just$test"
+  instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
+  name = "${var.name}"
+  node_number = 2
+  disk_category = "cloud_efficiency"
+  disk_size = 20
+  cidr_block = "172.20.0.0/24"
+  image_id = "${data.alicloud_images.main.images.0.id}"
+  vswitch_id = "${alicloud_vswitch.foo.id}"
 }
 `
 
