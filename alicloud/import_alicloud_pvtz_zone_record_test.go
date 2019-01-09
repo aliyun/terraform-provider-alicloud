7f88e8151247156c9d6bf4c8825e822d2f3d5adc
He Guimin <guimin.hgm@alibaba-inc.com>
2019-01-09 15:13:20 +0800

    Improve pvtz_zone testcase by using rand

--- alicloud/import_alicloud_pvtz_zone_record_test.go
+++ alicloud/import_alicloud_pvtz_zone_record_test.go
@@ -3,6 +3,7 @@ package alicloud
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
@@ -15,7 +16,7 @@ func TestAccAlicloudPvtzZoneRecord_importBasic(t *testing.T) {
 		CheckDestroy: testAccAlicloudPvtzZoneRecordDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPvtzZoneRecordConfig,
+				Config: testAccPvtzZoneRecordConfig(acctest.RandIntRange(10000, 999999)),
 			},
 
 			{
--- alicloud/import_alicloud_pvtz_zone_test.go
+++ alicloud/import_alicloud_pvtz_zone_test.go
@@ -3,6 +3,7 @@ package alicloud
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
@@ -15,7 +16,7 @@ func TestAccAlicloudPvtzZone_importBasic(t *testing.T) {
 		CheckDestroy: testAccAlicloudPvtzZoneDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPvtzZoneConfig,
+				Config: testAccPvtzZoneConfig(acctest.RandIntRange(10000, 999999)),
 			},
 
 			{
