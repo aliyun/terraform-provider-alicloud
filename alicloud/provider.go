901e0c3092a646c57cb6490875d2bf6114e9a316
xuxiaohang <evan@xuxiaohangdeMacBook-Air.local>
2019-01-18 14:03:19 +0800

    Add logtail config resouce

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -217,6 +217,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_log_store":                           resourceAlicloudLogStore(),
 			"alicloud_log_store_index":                     resourceAlicloudLogStoreIndex(),
 			"alicloud_log_machine_group":                   resourceAlicloudLogMachineGroup(),
+			"alicloud_logtail_config":                      resourceAlicoudLogtailConfig(),
 			"alicloud_fc_service":                          resourceAlicloudFCService(),
 			"alicloud_fc_function":                         resourceAlicloudFCFunction(),
 			"alicloud_fc_trigger":                          resourceAlicloudFCTrigger(),
