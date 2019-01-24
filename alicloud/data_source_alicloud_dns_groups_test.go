13eb9a965c3c25d69015d212f0dc980a62e1f49d
He Guimin <guimin.hgm@alibaba-inc.com>
2019-01-24 16:17:48 +0800

    Improve dns group testcase

--- alicloud/data_source_alicloud_dns_groups_test.go
+++ alicloud/data_source_alicloud_dns_groups_test.go
@@ -3,10 +3,13 @@ package alicloud
 import (
 	"testing"
 
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAlicloudDnsGroupsDataSource_name_regex(t *testing.T) {
+func TestAccAlicloudDnsGroupsDataSource_nameregexAll(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -14,12 +17,33 @@ func TestAccAlicloudDnsGroupsDataSource_name_regex(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckAlicloudDnsGroupsDataSourceNameRegexConfig,
+				Config: testAccCheckAlicloudDnsGroupsDataSourceNameRegexAll,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudDataSourceID("data.alicloud_dns_groups.group"),
 					resource.TestCheckResourceAttr("data.alicloud_dns_groups.group", "groups.#", "1"),
 					resource.TestCheckResourceAttr("data.alicloud_dns_groups.group", "groups.0.group_id", ""),
 					resource.TestCheckResourceAttr("data.alicloud_dns_groups.group", "groups.0.group_name", "ALL"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAlicloudDnsGroupsDataSource_nameregex(t *testing.T) {
+	rand := acctest.RandIntRange(1000, 9999)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudDnsGroupsDataSourceNameRegex(rand),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_dns_groups.group"),
+					resource.TestCheckResourceAttr("data.alicloud_dns_groups.group", "groups.#", "1"),
+					resource.TestCheckResourceAttrSet("data.alicloud_dns_groups.group", "groups.0.group_id"),
+					resource.TestCheckResourceAttr("data.alicloud_dns_groups.group", "groups.0.group_name", fmt.Sprintf("tf-testacc-%d", rand)),
 				),
 			},
 		},
@@ -46,10 +70,23 @@ func TestAccAlicloudDnsGroupsDataSource_empty(t *testing.T) {
 	})
 }
 
-const testAccCheckAlicloudDnsGroupsDataSourceNameRegexConfig = `
+const testAccCheckAlicloudDnsGroupsDataSourceNameRegexAll = `
 data "alicloud_dns_groups" "group" {
   name_regex = "^ALL"
 }`
+
+func testAccCheckAlicloudDnsGroupsDataSourceNameRegex(rand int) string {
+	return fmt.Sprintf(`
+	resource "alicloud_dns_group" "foo" {
+	  name = "tf-testacc%d-"
+	}
+	resource "alicloud_dns_group" "group" {
+	  name = "tf-testacc-%d"
+	}
+	data "alicloud_dns_groups" "group" {
+	  name_regex = "${alicloud_dns_group.group.name}"
+	}`, rand, rand)
+}
 
 const testAccCheckAlicloudDnsGroupsDataSourceNameRegexEmpty = `
 data "alicloud_dns_groups" "group" {
