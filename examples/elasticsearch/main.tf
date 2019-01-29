a00b4087719e40b76d5b60f5ff69bda8da558a76
曲广凡 <guangfan.qu@alibaba-inc.com>
2019-01-29 18:01:52 +0800

    Resolved tf files

--- examples/elasticsearch/main.tf
+++ examples/elasticsearch/main.tf
@@ -1,2 +1,11 @@
-data "alicloud_elasticsearch_instances" "instances" {
+resource "alicloud_elasticsearch_instance" "instance" {
+  instance_charge_type = "${var.instance_charge_type}"
+  data_node_amount     = "${var.data_node_amount}"
+  data_node_spec       = "${var.data_node_spec}"
+  data_node_disk_size  = "${var.data_node_disk_size}"
+  data_node_disk_type  = "${var.data_node_disk_type}"
+  vswitch_id           = "${var.vswitch_id}"
+  password             = "${var.password}"
+  version              = "${var.version}"
+  description          = "${var.description}"
 }
