9457f384d601bc63db9d34ce6f9c8f4e22b032f3
曲广凡 <guangfan.qu@alibaba-inc.com>
2019-01-29 17:24:46 +0800

    test(elasticsearch): Add ids test case.

--- examples/elasticsearch/main.tf
+++ examples/elasticsearch/main.tf
@@ -1,11 +1,2 @@
-resource "alicloud_elasticsearch_instance" "instance" {
-  instance_charge_type = "${var.instance_charge_type}"
-  data_node_amount     = "${var.data_node_amount}"
-  data_node_spec       = "${var.data_node_spec}"
-  data_node_disk_size  = "${var.data_node_disk_size}"
-  data_node_disk_type  = "${var.data_node_disk_type}"
-  vswitch_id           = "${var.vswitch_id}"
-  password             = "${var.password}"
-  version              = "${var.version}"
-  description          = "${var.description}"
+data "alicloud_elasticsearch_instances" "instances" {
 }
--- examples/elasticsearch/output.tf
+++
@@ -1,3 +0,0 @@
-output "instance_id" {
-  value = "${alicloud_elasticsearch_instance.instance.id}"
-}
