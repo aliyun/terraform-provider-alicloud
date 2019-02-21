265505cc14e1c3c30a876baf267a7472b33a59aa
huangyi <mangqin.hy@alibaba-inc.com>
2019-02-21 19:40:12 +0800

    kvstore support parameter configuration

--- alicloud/service_alicloud_kvstore.go
+++ alicloud/service_alicloud_kvstore.go
@@ -1,11 +1,11 @@
 package alicloud
 
 import (
-	"time"
-
+	"fmt"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/r-kvstore"
 	"github.com/denverdino/aliyungo/common"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+	"time"
 )
 
 type KvstoreService struct {
@@ -98,5 +98,43 @@ func (s *KvstoreService) WaitForRKVInstanceVpcAuthMode(instanceId string, status
 		timeout = timeout - DefaultIntervalMedium
 		time.Sleep(DefaultIntervalMedium * time.Second)
 	}
+	return nil
+}
+
+func (s *KvstoreService) DescribeParameters(instanceId string) (ds *r_kvstore.DescribeParametersResponse, err error) {
+	request := r_kvstore.CreateDescribeParametersRequest()
+	request.DBInstanceId = instanceId
+
+	raw, err := s.client.WithRkvClient(func(rkvClient *r_kvstore.Client) (interface{}, error) {
+		return rkvClient.DescribeParameters(request)
+	})
+	if err != nil {
+		if IsExceptedErrors(err, []string{InvalidDBInstanceIdNotFound, InvalidDBInstanceNameNotFound}) {
+			return nil, GetNotFoundErrorFromString(GetNotFoundMessage("DB Instance", instanceId))
+		}
+		return nil, err
+	}
+	resp, _ := raw.(*r_kvstore.DescribeParametersResponse)
+	if resp == nil {
+		err = GetNotFoundErrorFromString(GetNotFoundMessage("KVStore Instance Parameter", instanceId))
+	}
+	return resp, err
+}
+
+func (s *KvstoreService) ModifyInstanceConfig(instanceId string, config string) error {
+	request := r_kvstore.CreateModifyInstanceConfigRequest()
+	request.InstanceId = instanceId
+	request.Config = config
+
+	if err := s.WaitForRKVInstance(instanceId, Normal, DefaultLongTimeout); err != nil {
+		return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
+	}
+	_, err := s.client.WithRkvClient(func(rkvClient *r_kvstore.Client) (interface{}, error) {
+		return rkvClient.ModifyInstanceConfig(request)
+	})
+	if err != nil {
+		return fmt.Errorf("ModifyInstanceConfig got an error: %#v", err)
+	}
+
 	return nil
 }
