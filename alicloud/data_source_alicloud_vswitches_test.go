356500acd9a4e828f098e72762f012bdf7306af3
yuanye <yuanye@admindeMacBook-Pro.local>
2019-03-28 18:56:42 +0800

    security_groups vpcs vswitches add ids names

--- alicloud/data_source_alicloud_vswitches_test.go
+++ alicloud/data_source_alicloud_vswitches_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccAlicloudVSwitchesDataSource(t *testing.T) {
+func TestAccAlicloudVSwitchesDataSource_all(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -21,6 +21,9 @@ func TestAccAlicloudVSwitchesDataSource(t *testing.T) {
 				Config: testAccCheckAlicloudVSwitchesDataSourceConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudDataSourceID("data.alicloud_vswitches.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "ids.#", "1"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "names.#", "1"),
+					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "names.0"),
 					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.#", "1"),
 					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "vswitches.0.id"),
 					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "vswitches.0.vpc_id"),
@@ -61,6 +64,15 @@ func TestAccAlicloudVSwitchesDataSource_Name_Regex(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "vswitches.0.creation_time"),
 				),
 			},
+			{
+				Config: testAccCheckAlicloudVSwitchesDataSourceConfigNameRegexEmpty(rand),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_vswitches.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "ids.#", "0"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "names.#", "0"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.#", "0"),
+				),
+			},
 		},
 	})
 }
@@ -86,6 +98,15 @@ func TestAccAlicloudVSwitchesDataSource_vpcid(t *testing.T) {
 					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.description", ""),
 					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.is_default", "false"),
 					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "vswitches.0.creation_time"),
+				),
+			},
+			{
+				Config: testAccCheckAlicloudVSwitchesDataSourceConfigVPCIDEmpty,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_vswitches.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "ids.#", "0"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "names.#", "0"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.#", "0"),
 				),
 			},
 		},
@@ -114,31 +135,13 @@ func TestAccAlicloudVSwitchesDataSource_Zone_ID(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.alicloud_vswitches.foo", "vswitches.0.creation_time"),
 				),
 			},
-		},
-	})
-}
-
-func TestAccAlicloudVSwitchesDataSourceEmpty(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckAlicloudVSwitchesDataSourceEmpty,
+				Config: testAccCheckAlicloudVSwitchesDataSourceConfigAvailabilityZoneIDEmpty,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudDataSourceID("data.alicloud_vswitches.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "ids.#", "0"),
+					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "names.#", "0"),
 					resource.TestCheckResourceAttr("data.alicloud_vswitches.foo", "vswitches.#", "0"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.id"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.vpc_id"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.zone_id"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.name"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.instance_ids.#"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.cidr_block"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.description"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.is_default"),
-					resource.TestCheckNoResourceAttr("data.alicloud_vswitches.foo", "vswitches.0.creation_time"),
 				),
 			},
 		},
@@ -199,6 +202,36 @@ data "alicloud_vswitches" "foo" {
 `, rand)
 }
 
+func testAccCheckAlicloudVSwitchesDataSourceConfigNameRegexEmpty(rand int) string {
+	return fmt.Sprintf(
+		`
+variable "name" {
+  default = "tf-testAcc-for-vswitch-datasource-name-regex-%d"
+}
+data "alicloud_zones" "default" {}
+
+resource "alicloud_vpc" "vpc" {
+  cidr_block = "172.16.0.0/16"
+  name = "${var.name}"
+}
+resource "alicloud_vswitch" "vswitch" {
+  name = "${var.name}"
+  cidr_block = "172.16.0.0/24"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+}
+resource "alicloud_vswitch" "vswitch1" {
+  name = "${var.name}-A"
+  cidr_block = "172.16.1.0/24"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.1.id}"
+}
+data "alicloud_vswitches" "foo" {
+  name_regex = "${alicloud_vswitch.vswitch1.name}-fake"
+}
+`, rand)
+}
+
 const testAccCheckAlicloudVSwitchesDataSourceConfigAvailabilityZoneID = `
 variable "name" {
   default = "tf-testAcc-for-vswitch-datasource-availability_zone_ID"
@@ -225,6 +258,35 @@ resource "alicloud_vswitch" "vswitch1" {
 data "alicloud_vswitches" "foo" {
 	vpc_id = "${alicloud_vpc.vpc.id}"
   zone_id = "${alicloud_vswitch.vswitch.availability_zone}"
+}
+`
+
+const testAccCheckAlicloudVSwitchesDataSourceConfigAvailabilityZoneIDEmpty = `
+variable "name" {
+  default = "tf-testAcc-for-vswitch-datasource-availability_zone_ID"
+}
+data "alicloud_zones" "default" {}
+
+resource "alicloud_vpc" "vpc" {
+  cidr_block = "172.16.0.0/16"
+  name = "${var.name}"
+}
+
+resource "alicloud_vswitch" "vswitch" {
+  name = "${var.name}"
+  cidr_block = "172.16.0.0/24"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+}
+resource "alicloud_vswitch" "vswitch1" {
+  name = "${var.name}-A"
+  cidr_block = "172.16.1.0/24"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.1.id}"
+}
+data "alicloud_vswitches" "foo" {
+	vpc_id = "${alicloud_vpc.vpc.id}"
+  zone_id = "${alicloud_vswitch.vswitch.availability_zone}-fake"
 }
 `
 const testAccCheckAlicloudVSwitchesDataSourceConfigVPCID = `
@@ -258,8 +320,35 @@ data "alicloud_vswitches" "foo" {
 	vpc_id = "${alicloud_vswitch.vswitch.vpc_id}"
 }
 `
-const testAccCheckAlicloudVSwitchesDataSourceEmpty = `
+
+const testAccCheckAlicloudVSwitchesDataSourceConfigVPCIDEmpty = `
+variable "name" {
+  default = "tf-testAcc-for-vswitch-datasource-VPC-ID"
+}
+data "alicloud_zones" "default" {}
+
+resource "alicloud_vpc" "vpc" {
+  cidr_block = "172.16.0.0/16"
+  name = "${var.name}"
+}
+resource "alicloud_vpc" "vpc1" {
+  cidr_block = "192.168.0.0/16"
+  name = "${var.name}-A"
+}
+resource "alicloud_vswitch" "vswitch" {
+  name = "${var.name}"
+  cidr_block = "172.16.0.0/24"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+}
+
+resource "alicloud_vswitch" "vswitch1" {
+  name = "${var.name}"
+  cidr_block = "192.168.0.0/16"
+  vpc_id = "${alicloud_vpc.vpc1.id}"
+  availability_zone = "${data.alicloud_zones.default.zones.1.id}"
+}
 data "alicloud_vswitches" "foo" {
-  name_regex = "^tf-fake-name"
+	vpc_id = "${alicloud_vswitch.vswitch.vpc_id}-fake"
 }
 `
