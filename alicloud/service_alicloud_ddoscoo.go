f0522dbb452461e0ecdd904c7d3dd655326e68d2
文波 <yiduo.qyd@alibaba-inc.com>
2019-03-29 15:51:40 +0800

    fix code bugs in ddoscoo service and test

--- alicloud/service_alicloud_ddoscoo.go
+++ alicloud/service_alicloud_ddoscoo.go
@@ -36,7 +36,7 @@ func (s *DdoscooService) DescribeDdoscooInstance(id string) (v ddoscoo.Instance,
 			return WrapErrorf(Error(GetNotFoundMessage("Ddoscoo Instance", id)), NotFoundMsg, ProviderERROR)
 		}
 
-		v = *resp.Instances[0]
+		v = resp.Instances[0]
 		return nil
 	})
 
@@ -66,7 +66,7 @@ func (s *DdoscooService) DescribeDdoscooInstanceSpec(id string) (v ddoscoo.Insta
 			return WrapErrorf(Error(GetNotFoundMessage("Ddoscoo Instance", id)), NotFoundMsg, ProviderERROR)
 		}
 
-		v = *resp.InstanceSpecs[0]
+		v = resp.InstanceSpecs[0]
 		return nil
 	})
 
