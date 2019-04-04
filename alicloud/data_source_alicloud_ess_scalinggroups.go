fb20542f136fb90c877d81177cb2556f9f5cd3f8
声烦 <kangning.tkn@alibaba-inc.com>
2019-04-04 16:25:29 +0800

    support data source alicloud_ess_scaling_groups

---
+++ alicloud/data_source_alicloud_ess_scalinggroups.go
@@ -0,0 +1,242 @@
+package alicloud
+
+import (
+	"regexp"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ess"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func dataSourceAlicloudEssScalingGroups() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAlicloudEssScalingGroupsRead,
+		Schema: map[string]*schema.Schema{
+			"name_regex": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validateNameRegex,
+				ForceNew:     true,
+			},
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				ForceNew: true,
+			},
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"groups": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"active_scaling_configuration": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"launch_template_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"launch_template_version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"region_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"min_size": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"max_size": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"cooldown_time": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"removal_policies": {
+							Type:     schema.TypeList,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Computed: true,
+						},
+						"load_balancer_ids": {
+							Type:     schema.TypeList,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Computed: true,
+						},
+						"db_instance_ids": {
+							Type:     schema.TypeList,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Computed: true,
+						},
+						"vswitch_ids": {
+							Type:     schema.TypeList,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Computed: true,
+						},
+						"lifecycle_state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"total_capacity": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"active_capacity": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"pending_capacity": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"removing_capacity": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"creation_time": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAlicloudEssScalingGroupsRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	args := ess.CreateDescribeScalingGroupsRequest()
+	args.PageSize = requests.NewInteger(PageSizeLarge)
+	args.PageNumber = requests.NewInteger(1)
+
+	var allScalingGroups []ess.ScalingGroup
+
+	for {
+		raw, err := client.WithEssClient(func(essClient *ess.Client) (interface{}, error) {
+			return essClient.DescribeScalingGroups(args)
+		})
+		if err != nil {
+			return err
+		}
+		resp, _ := raw.(*ess.DescribeScalingGroupsResponse)
+		if resp == nil || len(resp.ScalingGroups.ScalingGroup) < 1 {
+			break
+		}
+
+		allScalingGroups = append(allScalingGroups, resp.ScalingGroups.ScalingGroup...)
+
+		if len(resp.ScalingGroups.ScalingGroup) < PageSizeLarge {
+			break
+		}
+
+		if page, err := getNextpageNumber(args.PageNumber); err != nil {
+			return err
+		} else {
+			args.PageNumber = page
+		}
+	}
+
+	var filteredScalingGroupsTemp = make([]ess.ScalingGroup, 0)
+
+	nameRegex, okNameRegex := d.GetOk("name_regex")
+	idsMap := make(map[string]string)
+	ids, okIds := d.GetOk("ids")
+	if okIds {
+		for _, i := range ids.([]interface{}) {
+			idsMap[i.(string)] = i.(string)
+		}
+	}
+	if okNameRegex || okIds {
+		for _, group := range allScalingGroups {
+			if okNameRegex && nameRegex != "" {
+				var r = regexp.MustCompile(nameRegex.(string))
+				if r != nil && !r.MatchString(group.ScalingGroupName) {
+					continue
+				}
+			}
+			if okIds && len(idsMap) > 0 {
+				if _, ok := idsMap[group.ScalingGroupId]; !ok {
+					continue
+				}
+			}
+			filteredScalingGroupsTemp = append(filteredScalingGroupsTemp, group)
+		}
+	} else {
+		filteredScalingGroupsTemp = allScalingGroups
+	}
+	return scalingGroupsDescriptionAttribute(d, filteredScalingGroupsTemp, meta)
+}
+
+func scalingGroupsDescriptionAttribute(d *schema.ResourceData, scalingGroups []ess.ScalingGroup, meta interface{}) error {
+	var ids []string
+	var names []string
+	var s = make([]map[string]interface{}, 0)
+	for _, scalingGroup := range scalingGroups {
+		mapping := map[string]interface{}{
+			"id":                           scalingGroup.ScalingGroupId,
+			"name":                         scalingGroup.ScalingGroupName,
+			"active_scaling_configuration": scalingGroup.ActiveScalingConfigurationId,
+			"launch_template_id":           scalingGroup.LaunchTemplateId,
+			"launch_template_version":      scalingGroup.LaunchTemplateVersion,
+			"region_id":                    scalingGroup.RegionId,
+			"min_size":                     scalingGroup.MinSize,
+			"max_size":                     scalingGroup.MaxSize,
+			"cooldown_time":                scalingGroup.DefaultCooldown,
+			"removal_policies":             scalingGroup.RemovalPolicies.RemovalPolicy,
+			"load_balancer_ids":            scalingGroup.LoadBalancerIds.LoadBalancerId,
+			"db_instance_ids":              scalingGroup.DBInstanceIds.DBInstanceId,
+			"vswitch_ids":                  scalingGroup.VSwitchIds.VSwitchId,
+			"lifecycle_state":              scalingGroup.LifecycleState,
+			"total_capacity":               scalingGroup.TotalCapacity,
+			"active_capacity":              scalingGroup.ActiveCapacity,
+			"pending_capacity":             scalingGroup.PendingCapacity,
+			"removing_capacity":            scalingGroup.RemovingCapacity,
+			"creation_time":                scalingGroup.CreationTime,
+		}
+		ids = append(ids, scalingGroup.ScalingGroupId)
+		names = append(names, scalingGroup.ScalingGroupName)
+		s = append(s, mapping)
+	}
+
+	d.SetId(dataResourceIdHash(ids))
+	if err := d.Set("groups", s); err != nil {
+		return err
+	}
+
+	if err := d.Set("ids", ids); err != nil {
+		return err
+	}
+
+	if err := d.Set("names", names); err != nil {
+		return err
+	}
+
+	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
+		writeToFile(output.(string), s)
+	}
+	return nil
+}
---
+++ website/docs/d/ess_scaling_groups.html.markdown
@@ -0,0 +1,61 @@
+---
+layout: "alicloud"
+page_title: "Alicloud: alicloud_ess_scaling_groups"
+sidebar_current: "docs-alicloud_ess_scaling_groups"
+description: |-
+    Provides a list of scaling groups available to the user.
+---
+
+# alicloud_ess_scaling_groups
+
+This data source provides available scaling group resources. 
+
+## Example Usage
+
+```
+data "alicloud_ess_scaling_groups" "scalinggroups_ds" {
+	ids = ["scaling_group_id1","scaling_group_id2"]
+	name_regex = "scaling_group_name"
+}
+
+output "first_scaling_group" {
+  value = "${data.alicloud_ess_scaling_groups.scalinggroups_ds.scaling_groups.0.id}"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name_regex` - (Optional) A regex string to filter resulting scaling groups by name.
+* `ids` - (Optional) A list of scaling group IDs.
+* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
+
+## Attributes Reference
+
+The following attributes are exported in addition to the arguments listed above:
+
+* `ids` - A list of scaling group ids.
+* `names` - A list of scaling group names.
+* `scaling_groups` - A list of scaling groups. Each element contains the following attributes:
+  * `id` - ID of the scaling group.
+  * `name` - Name of the scaling group.
+  * `active_scaling_configuration` -Active scaling configuration for scaling group.
+  * `launch_template_id` - Active launch template ID for scaling group.
+  * `launch_template_version` - Version of active launch template.
+  * `region_id` - Region ID the scaling group belongs to.
+  * `min_size` - The minimum number of ECS instances.
+  * `max_size` - The maximum number of ECS instances.
+  * `cooldown_time` - Default cooldown time of scaling group.
+  * `removal_policies` - Removal policy used to select the ECS instance to remove from the scaling group.
+  * `load_balancer_ids` - Slb instances id which the ECS instance attached to.
+  * `db_instance_ids` - Db instances id which the ECS instance attached to.
+  * `vswitch_ids` - Vswitches id in which the ECS instance launched.
+  * `lifecycle_state` - Lifecycle state of scaling group.
+  * `total_capacity` - Number of instances in scaling group.
+  * `active_capacity` - Number of active instances in scaling group.
+  * `pending_capacity` - Number of pending instances in scaling group.
+  * `removing_capacity` - Number of removing instances in scaling group.
+  * `creation_time` - Creation time of scaling group.
+  
+  
