eaae00ee4b76d87253c210728256410e9795d38c
jijun.jj <jijun.jj@alibaba-inc.com>
2019-04-04 14:54:04 +0800

    add data_source of ssl_vpn_server and ssl_vpn_client_cert

---
+++ alicloud/data_source_alicloud_ssl_vpn_client_certs_test.go
@@ -0,0 +1,109 @@
+package alicloud
+
+import (
+	"testing"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAlicloudSslVpnClientCertsDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithAccountSiteType(t, IntlSite)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudSslVpnClientCertsDataCfg,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_ssl_vpn_client_certs.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.#", "1"),
+					resource.TestCheckResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.name", "tf-testAccSslVpnClientCertsDataResource"),
+					resource.TestCheckResourceAttrSet("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.ssl_vpn_server_id"),
+					resource.TestCheckResourceAttrSet("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.create_time"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAlicloudSslVpnClientCertsDataSource_empty(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudSslVpnClientCertsDataEmpty,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_ssl_vpn_client_certs.foo"),
+					resource.TestCheckResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.#", "0"),
+					resource.TestCheckNoResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.name"),
+					resource.TestCheckNoResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.ssl_vpn_server_id"),
+					resource.TestCheckNoResourceAttr("data.alicloud_ssl_vpn_client_certs.foo", "ssl_vpn_client_certs.0.create_time"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAlicloudSslVpnClientCertsDataCfg = `
+variable "name" {
+	default = "tf-testAccSslVpnClientCertsDataResource"
+}
+
+data "alicloud_zones" "default" {
+	"available_resource_creation"= "VSwitch"
+}
+
+resource "alicloud_vswitch" "foo" {
+	vpc_id = "${alicloud_vpc.foo.id}"
+	cidr_block = "172.16.0.0/21"
+	availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+	name = "${var.name}"
+}
+
+resource "alicloud_vpc" "foo" {
+	cidr_block = "172.16.0.0/12"
+	name = "${var.name}"
+}
+
+resource "alicloud_vpn_gateway" "foo" {
+	name = "${var.name}"
+	vpc_id = "${alicloud_vpc.foo.id}"
+	bandwidth = "10"
+	enable_ssl = true
+	instance_charge_type = "PostPaid"
+	description = "test_create_description"
+}
+
+resource "alicloud_ssl_vpn_server" "foo" {
+	name="${var.name}"
+	vpn_gateway_id="${alicloud_vpn_gateway.foo.id}"
+	client_ip_pool="192.168.1.0/24"
+	local_subnet="172.16.1.0/24"
+	protocol="UDP"
+	port="1194"
+	cipher="AES-128-CBC"
+	compress="false"
+}
+
+resource "alicloud_ssl_vpn_client_cert" "foo" {
+	name="${var.name}"
+	ssl_vpn_server_id="${alicloud_ssl_vpn_server.foo.id}"
+}
+
+data "alicloud_ssl_vpn_client_certs" "foo" {
+	name_regex = "tf-testAcc*"
+	ids = ["${alicloud_ssl_vpn_client_cert.foo.id}"]
+}
+`
+
+const testAccCheckAlicloudSslVpnClientCertsDataEmpty = `
+data "alicloud_ssl_vpn_client_certs" "foo" {
+	name_regex = "^tf-testacc-fake-name"
+}
+`
+
+
---
+++ alicloud/data_source_alicloud_ssl_vpn_servers.go
@@ -0,0 +1,207 @@
+package alicloud
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/vpc"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func dataSourceAlicloudSslVpnServers() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAlicloudSslVpnServersRead,
+
+		Schema: map[string]*schema.Schema{
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				ForceNew: true,
+				MinItems: 1,
+			},
+
+			"name_regex": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validateNameRegex,
+				ForceNew:     true,
+			},
+
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			// Computed values
+			"ssl_vpn_servers": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"region_id": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"ssl_vpn_server_id": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"vpn_gateway_id": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"local_subnet": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"client_ip_pool": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"create_time": {
+							Type:   schema.TypeInt,
+							Computed: true,
+						},
+						"cipher": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"proto": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+						"port": {
+							Type:	schema.TypeInt,
+							Computed: true,
+						},
+						"compress": {
+							Type:	schema.TypeBool,
+							Computed: true,
+						},
+						"connections": {
+							Type:	schema.TypeInt,
+							Computed: true,
+						},
+						"max_connections": {
+							Type:	schema.TypeInt,
+							Computed: true,
+						},
+						"internet_ip": {
+							Type:	schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAlicloudSslVpnServersRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	args := vpc.CreateDescribeSslVpnServersRequest()
+	args.RegionId = client.RegionId
+	args.PageSize = requests.NewInteger(PageSizeLarge)
+	args.PageNumber = requests.NewInteger(1)
+	var allSslVpnServers []vpc.SslVpnServer
+
+	for {
+		raw, err := client.WithVpcClient(func(vpcClient *vpc.Client) (interface{}, error) {
+			return vpcClient.DescribeSslVpnServers(args)
+		})
+		if err != nil {
+			return err
+		}
+		resp, _ := raw.(*vpc.DescribeSslVpnServersResponse)
+		if resp == nil || len(resp.SslVpnServers.SslVpnServer) < 1 {
+			break
+		}
+		allSslVpnServers = append(allSslVpnServers, resp.SslVpnServers.SslVpnServer...)
+		if len(resp.SslVpnServers.SslVpnServer) < PageSizeLarge {
+			break
+		}
+		if page, err := getNextpageNumber(args.PageNumber); err != nil {
+			return err
+		} else {
+			args.PageNumber = page
+		}
+	}
+
+	var filteredSslVpnServers []vpc.SslVpnServer
+	var reg *regexp.Regexp
+	var ids []string
+	if v, ok := d.GetOk("ids"); ok && len(v.([]interface{})) > 0 {
+		for _, item := range v.([]interface{}) {
+			ids = append(ids, strings.Trim(item.(string), " "))
+		}
+	}
+
+	if nameRegex, ok := d.GetOk("name_regex"); ok && nameRegex.(string) != "" {
+		if r, err := regexp.Compile(nameRegex.(string)); err == nil {
+			reg = r
+		}
+	}
+
+	for _, sslVpnServer := range allSslVpnServers {
+		if reg != nil {
+			if !reg.MatchString(sslVpnServer.Name) {
+				continue
+			}
+		}
+
+		if ids != nil && len(ids) != 0 {
+			for _, id := range ids {
+				if sslVpnServer.SslVpnServerId == id {
+					filteredSslVpnServers = append(filteredSslVpnServers, sslVpnServer)
+				}
+			}
+		} else {
+			filteredSslVpnServers = append(filteredSslVpnServers, sslVpnServer)
+		}
+	}
+
+	return sslVpnServersDecriptionAttributes(d, filteredSslVpnServers, meta)
+}
+
+func sslVpnServersDecriptionAttributes(d *schema.ResourceData, vpnSetTypes []vpc.SslVpnServer, meta interface{}) error {
+	var ids []string
+	var s []map[string]interface{}
+	for _, vpn := range vpnSetTypes {
+		mapping := map[string]interface{}{
+			"ssl_vpn_server_id":          vpn.SslVpnServerId,
+			"vpn_gateway_id":		 vpn.VpnGatewayId,
+			"region_id":	vpn.RegionId,
+			"local_subnet":	vpn.LocalSubnet,
+			"name":        vpn.Name,
+			"client_ip_pool":	vpn.ClientIpPool,
+			"create_time": vpn.CreateTime,
+			"cipher":	vpn.Cipher,
+			"proto": vpn.Proto,
+			"port": vpn.Port,
+			"compress":  vpn.Compress,
+			"connections": vpn.Connections,
+			"max_connections":  vpn.MaxConnections,
+			"internet_ip": vpn.InternetIp,
+		}
+		ids = append(ids, vpn.SslVpnServerId)
+		s = append(s, mapping)
+	}
+
+	d.SetId(dataResourceIdHash(ids))
+	if err := d.Set("ssl_vpn_servers", s); err != nil {
+		return err
+	}
+
+	// create a json file in current directory and write data source to it.
+	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
+		writeToFile(output.(string), s)
+	}
+	return nil
+}
