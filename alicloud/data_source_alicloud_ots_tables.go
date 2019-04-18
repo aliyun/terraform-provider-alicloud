22ed1313457e0d1c47dc193c43f33f68bf5c8ccd
声烦 <kangning.tkn@alibaba-inc.com>
2019-04-18 17:42:50 +0800

    bugfix: ess go sdk compatibility

--- alicloud/data_source_alicloud_ots_tables.go
+++ alicloud/data_source_alicloud_ots_tables.go
@@ -2,10 +2,11 @@ package alicloud
 
 import (
 	"fmt"
+	"regexp"
+
 	"github.com/aliyun/aliyun-tablestore-go-sdk/tablestore"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
-	"regexp"
 )
 
 func dataSourceAlicloudOtsTables() *schema.Resource {
