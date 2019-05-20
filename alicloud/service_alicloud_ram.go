f4585c1237712575a4a2571c9e1df919a3580e15
lexwong <lexwong@lexwongdeMacBook-Air.local>
2019-05-20 14:24:04 +0800

    fix ram user diff bug

--- alicloud/service_alicloud_ram.go
+++ alicloud/service_alicloud_ram.go
@@ -187,24 +187,42 @@ func (s *RamService) GetIntersection(dataMap []map[string]interface{}, allDataMa
 
 func (s *RamService) DescribeRamUser(id string) (*ram.User, error) {
 
-	request := ram.CreateListUsersRequest()
+	listUsersRequest := ram.CreateListUsersRequest()
+	var userName string
 	raw, err := s.client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-		return ramClient.ListUsers(request)
+		return ramClient.ListUsers(listUsersRequest)
 	})
 	if err != nil {
-		if IsExceptedError(err, NotFound) {
+		if RamEntityNotExist(err) {
 			return nil, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
 		}
-		return nil, WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return nil, WrapErrorf(err, DefaultErrorMsg, id, listUsersRequest.GetActionName(), AlibabaCloudSdkGoERROR)
 	}
-	addDebug(request.GetActionName(), raw)
+	addDebug(listUsersRequest.GetActionName(), raw)
 	users, _ := raw.(*ram.ListUsersResponse)
 	for _, user := range users.Users.User {
 		if user.UserId == id {
-			return &user, nil
+			userName = user.UserName
 		}
 	}
-	return nil, WrapErrorf(Error(GetNotFoundMessage("RamUser", id)), NotFoundMsg, ProviderERROR)
+	if userName == "" {
+		return nil, WrapErrorf(Error(GetNotFoundMessage("RamUser", id)), NotFoundMsg, ProviderERROR)
+	}
+	getUserRequest := ram.CreateGetUserRequest()
+	getUserRequest.UserName = userName
+	raw, err = s.client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+		return ramClient.GetUser(getUserRequest)
+	})
+	if err != nil {
+		if RamEntityNotExist(err) {
+			return nil, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
+		}
+		return nil, WrapErrorf(err, DefaultErrorMsg, id, getUserRequest.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+	addDebug(listUsersRequest.GetActionName(), raw)
+	user, _ := raw.(*ram.GetUserResponse)
+
+	return &user.User, nil
 }
 
 func (s *RamService) WaitForRamUser(id string, status Status, timeout int) error {
@@ -227,6 +245,24 @@ func (s *RamService) WaitForRamUser(id string, status Status, timeout int) error
 		}
 	}
 	return nil
+}
+
+func (s *RamService) DescribeRamAccountAlias(id string) (*ram.GetAccountAliasResponse, error) {
+	request := ram.CreateGetAccountAliasRequest()
+
+	raw, err := s.client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+		return ramClient.GetAccountAlias(request)
+	})
+	if err != nil {
+		if RamEntityNotExist(err) {
+			return nil, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
+		}
+		return nil, WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+	addDebug(request.GetActionName(), raw)
+	response := raw.(*ram.GetAccountAliasResponse)
+
+	return response, nil
 }
 
 func (s *RamService) DescribeRamAccessKey(id, userName string) (*ram.AccessKey, error) {
@@ -274,22 +310,4 @@ func (s *RamService) WaitForRamAccessKey(id, useName string, status Status, time
 			return WrapErrorf(err, WaitTimeoutMsg, id, GetFunc(1), timeout, object.Status, status, ProviderERROR)
 		}
 	}
-}
-
-func (s *RamService) DescribeRamAccountAlias(id string) (*ram.GetAccountAliasResponse, error) {
-	request := ram.CreateGetAccountAliasRequest()
-
-	raw, err := s.client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-		return ramClient.GetAccountAlias(request)
-	})
-	if err != nil {
-		if RamEntityNotExist(err) {
-			return nil, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
-		}
-		return nil, WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
-	}
-	addDebug(request.GetActionName(), raw)
-	response := raw.(*ram.GetAccountAliasResponse)
-
-	return response, nil
 }
