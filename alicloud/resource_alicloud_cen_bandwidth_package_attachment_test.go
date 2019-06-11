6e285a62a6c81937215d5dbe604aa5a566c583e4
lexwong <lexwong@lexwongdeMacBook-Air.local>
2019-06-11 14:49:29 +0800

    Improve cen_bandwidth_package_attachment resource use common method.

--- alicloud/resource_alicloud_cen_bandwidth_package_attachment_test.go
+++ alicloud/resource_alicloud_cen_bandwidth_package_attachment_test.go
@@ -4,98 +4,86 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
+
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/cbn"
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
 )
 
 func TestAccAlicloudCenBandwidthPackageAttachment_basic(t *testing.T) {
 	var cenBwp cbn.CenBandwidthPackage
 
+	resourceId := "alicloud_cen_bandwidth_package_attachment.default"
+	ra := resourceAttrInit(resourceId, cenBandwidthPackageAttachmentMap)
+
+	serviceFunc := func() interface{} {
+		return &CenService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}
+	rc := resourceCheckInit(resourceId, &cenBwp, serviceFunc)
+
+	rac := resourceAttrCheckInit(rc, ra)
+
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000000, 9999999)
+	name := fmt.Sprintf(`"tf-testAcc%sCenBandwidthPackageAttachment-%d"`, defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceCenBandwidthPackageAttachmentConfigDependence)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
 			testAccPreCheckWithAccountSiteType(t, DomesticSite)
 		},
-
 		// module name
-		IDRefreshName: "alicloud_cen_bandwidth_package_attachment.foo",
+		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
-		CheckDestroy:  testAccCheckCenBandwidthPackageAttachmentDestroy,
+		CheckDestroy:  rac.checkResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCenBandwidthPackageAttachmentConfig,
+				Config: testAccConfig(map[string]interface{}{
+					"instance_id":          "${alicloud_cen_instance.default.id}",
+					"bandwidth_package_id": "${alicloud_cen_bandwidth_package.default.id}",
+				}),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckCenBandwidthPackageAttachmentExists("alicloud_cen_bandwidth_package_attachment.foo", &cenBwp),
+					testAccCheck(cenBandwidthPackageAttachmentMap),
+					testAccCheckCenBandwidthPackageRegionId(&cenBwp, "China", "Asia-Pacific"),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"instance_id":          "${alicloud_cen_instance.default.id}",
+					"bandwidth_package_id": "${alicloud_cen_bandwidth_package.default.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(cenBandwidthPackageAttachmentMap),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckCenBandwidthPackageAttachmentExists(n string, cenBwp *cbn.CenBandwidthPackage) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No CenBandwidthPackage ID is set")
-		}
-
-		client := testAccProvider.Meta().(*connectivity.AliyunClient)
-		cenService := CenService{client}
-
-		instance, err := cenService.DescribeCenBandwidthPackageById(rs.Primary.ID)
-		if err != nil {
-			return err
-		}
-
-		*cenBwp = instance
-		return nil
-	}
-}
-
-func testAccCheckCenBandwidthPackageAttachmentDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*connectivity.AliyunClient)
-	cenService := CenService{client}
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "alicloud_cen_bandwidth_package_attachment" {
-			continue
-		}
-
-		instance, err := cenService.DescribeCenBandwidthPackageById(rs.Primary.ID)
-		if err != nil {
-			if NotFoundError(err) {
-				continue
-			}
-			return err
-		} else {
-			return fmt.Errorf("CEN %s bandwidth package %s still attach", instance.CenIds.CenId[0], instance.CenBandwidthPackageId)
-		}
-	}
-
-	return nil
-}
-
-const testAccCenBandwidthPackageAttachmentConfig = `
-resource "alicloud_cen_instance" "cen" {
-     name = "tf-testAccCenBandwidthPackageAttachmentConfig"
+func resourceCenBandwidthPackageAttachmentConfigDependence(name string) string {
+	return fmt.Sprintf(`
+resource "alicloud_cen_instance" "default" {
+     name = %s
      description = "tf-testAccCenBandwidthPackageAttachmentDescription"
 }
 
-resource "alicloud_cen_bandwidth_package" "bwp" {
-    bandwidth = 20
+resource "alicloud_cen_bandwidth_package" "default" {
+    bandwidth = 5
     geographic_region_ids = [
 		"China",
 		"Asia-Pacific"]
 }
-
-resource "alicloud_cen_bandwidth_package_attachment" "foo" {
-    instance_id = "${alicloud_cen_instance.cen.id}"
-    bandwidth_package_id = "${alicloud_cen_bandwidth_package.bwp.id}"
+`, name)
 }
-`
+
+var cenBandwidthPackageAttachmentMap = map[string]string{
+	"instance_id":          CHECKSET,
+	"bandwidth_package_id": CHECKSET,
+}
