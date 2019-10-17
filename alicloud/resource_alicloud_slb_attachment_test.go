6c5970bb41c4f91420339f43e1531878a5aef701
wb-cw577008 <wb-cw577008@alibaba-inc.com>
2019-10-17 17:46:17 +0800

    add argument server_type and update testcase

--- alicloud/resource_alicloud_slb_attachment_test.go
+++ alicloud/resource_alicloud_slb_attachment_test.go
@@ -157,7 +157,7 @@ func TestAccAlicloudSlbAttachment_classic_basic(t *testing.T) {
 	rac := resourceAttrCheckInit(rc, ra)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	name := fmt.Sprintf("tf-testAccSlbAttachment")
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceSlbAttachmentBasicdependence)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceSlbAttachmentClassBasicdependence)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheckWithRegions(t, false, connectivity.SlbClassicNoSupportedRegions)
@@ -202,9 +202,23 @@ func TestAccAlicloudSlbAttachment_classic_basic(t *testing.T) {
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
+					"depends_on":   []string{"alicloud_network_interface_attachment.default"},
+					"instance_ids": []string{"${alicloud_network_interface.default.0.id}"},
+					"server_type":  "eni",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"instance_ids.#": "1",
+						"server_type":    "eni",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
 					"load_balancer_id": "${alicloud_slb.default.id}",
 					"instance_ids":     []string{"${alicloud_instance.default.0.id}"},
 					"weight":           "90",
+					"server_type":  "ecs",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
@@ -212,6 +226,7 @@ func TestAccAlicloudSlbAttachment_classic_basic(t *testing.T) {
 						"weight":           "90",
 						"instance_ids.#":   "1",
 						"backend_servers":  CHECKSET,
+						"server_type":  "ecs",
 					}),
 				),
 			},
@@ -345,6 +360,84 @@ resource "alicloud_instance" "default" {
 resource "alicloud_slb" "default" {
   name = "${var.name}"
   vswitch_id = "${alicloud_vswitch.default.id}"
+}
+`, name)
+}
+
+func resourceSlbAttachmentClassBasicdependence(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+  default = "%s"
+}
+data "alicloud_instance_types" "default" {
+  cpu_core_count    = 1
+  memory_size       = 2
+}
+data "alicloud_instance_types" "new" {
+	eni_amount = 2
+}
+data "alicloud_images" "default" {
+    name_regex = "^ubuntu_18.*_64"
+  most_recent = true
+  owners = "system"
+}
+resource "alicloud_vpc" "default" {
+  name = "${var.name}"
+  cidr_block = "172.16.0.0/16"
+}
+resource "alicloud_vswitch" "default" {
+  vpc_id = "${alicloud_vpc.default.id}"
+  cidr_block = "172.16.0.0/16"
+  availability_zone = "${data.alicloud_instance_types.default.instance_types.0.availability_zones.0}"
+  name = "${var.name}"
+
+}
+resource "alicloud_security_group" "default" {
+  name = "${var.name}"
+  vpc_id = "${alicloud_vpc.default.id}"
+}
+resource "alicloud_network_interface" "default" {
+    count = 1
+    name = "${var.name}"
+    vswitch_id = "${alicloud_vswitch.default.id}"
+    security_groups = [ "${alicloud_security_group.default.id}" ]
+}
+resource "alicloud_instance" "default" {
+  # cn-beijing
+  image_id = "${data.alicloud_images.default.images.0.id}"
+
+  # series III
+  instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
+  internet_charge_type = "PayByTraffic"
+  internet_max_bandwidth_out = "5"
+  system_disk_category = "cloud_efficiency"
+  count = 2
+  security_groups = ["${alicloud_security_group.default.id}"]
+  instance_name = "${var.name}"
+  vswitch_id = "${alicloud_vswitch.default.id}"
+  availability_zone = "${data.alicloud_instance_types.default.instance_types.0.availability_zones.0}"
+}
+resource "alicloud_instance" "new" {
+  image_id = "${data.alicloud_images.default.images.0.id}"
+  instance_type = "${data.alicloud_instance_types.new.instance_types.0.id}"
+  instance_name = "${var.name}"
+  count = "1"
+  security_groups = "${alicloud_security_group.default.*.id}"
+  internet_charge_type = "PayByTraffic"
+  internet_max_bandwidth_out = "10"
+  availability_zone = "${data.alicloud_instance_types.new.instance_types.0.availability_zones.0}"
+  instance_charge_type = "PostPaid"
+  system_disk_category = "cloud_efficiency"
+  vswitch_id = "${alicloud_vswitch.default.id}"
+}
+resource "alicloud_network_interface_attachment" "default" {
+	count = 1
+    instance_id = "${alicloud_instance.new.0.id}"
+    network_interface_id = "${element(alicloud_network_interface.default.*.id, count.index)}"
+}
+resource "alicloud_slb" "default" {
+  name = "${var.name}"
+  specification  = "slb.s2.small"
 }
 `, name)
 }
