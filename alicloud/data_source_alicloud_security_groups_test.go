4ec775ca45d0db4747e959a0e64e71e9d4e63788
wb-jyj529329 <wb-jyj529329@alibaba-inc.com>
2019-10-18 10:41:19 +0800

    improve(security_group):make security_group support resource_group_id

--- alicloud/data_source_alicloud_security_groups_test.go
+++ alicloud/data_source_alicloud_security_groups_test.go
@@ -2,6 +2,7 @@ package alicloud
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -59,11 +60,23 @@ func TestAccAlicloudSecurityGroupsDataSourceBasic(t *testing.T) {
 		}),
 	}
 
+	resourceGroupIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudSecurityGroupsDataSourceConfig(rand, map[string]string{
+			"name_regex":        `"${alicloud_security_group.default.name}"`,
+			"resource_group_id": fmt.Sprintf(`"%s"`, os.Getenv("ALICLOUD_RESOURCE_GROUP_ID")),
+		}),
+		fakeConfig: testAccCheckAlicloudSecurityGroupsDataSourceConfig(rand, map[string]string{
+			"name_regex":        `"${alicloud_security_group.default.name}"`,
+			"resource_group_id": fmt.Sprintf(`"%s_fake"`, os.Getenv("ALICLOUD_RESOURCE_GROUP_ID")),
+		}),
+	}
+
 	allConf := dataSourceTestAccConfig{
 		existConfig: testAccCheckAlicloudSecurityGroupsDataSourceConfig(rand, map[string]string{
-			"name_regex": `"${alicloud_security_group.default.name}"`,
-			"ids":        `[ "${alicloud_security_group.default.id}" ]`,
-			"vpc_id":     `"${alicloud_security_group.default.vpc_id}"`,
+			"name_regex":        `"${alicloud_security_group.default.name}"`,
+			"ids":               `[ "${alicloud_security_group.default.id}" ]`,
+			"vpc_id":            `"${alicloud_security_group.default.vpc_id}"`,
+			"resource_group_id": fmt.Sprintf(`"%s"`, os.Getenv("ALICLOUD_RESOURCE_GROUP_ID")),
 			"tags": `{
                          from = "datasource"
                          usage1 = "test"
@@ -71,9 +84,10 @@ func TestAccAlicloudSecurityGroupsDataSourceBasic(t *testing.T) {
                         }`,
 		}),
 		fakeConfig: testAccCheckAlicloudSecurityGroupsDataSourceConfig(rand, map[string]string{
-			"name_regex": `"${alicloud_security_group.default.name}_fake"`,
-			"ids":        `[ "${alicloud_security_group.default.id}" ]`,
-			"vpc_id":     `"${alicloud_security_group.default.vpc_id}"`,
+			"name_regex":        `"${alicloud_security_group.default.name}_fake"`,
+			"ids":               `[ "${alicloud_security_group.default.id}" ]`,
+			"vpc_id":            `"${alicloud_security_group.default.vpc_id}"`,
+			"resource_group_id": fmt.Sprintf(`"%s"`, os.Getenv("ALICLOUD_RESOURCE_GROUP_ID")),
 			"tags": `{
                          from = "datasource"
                          usage1 = "test"
@@ -82,7 +96,7 @@ func TestAccAlicloudSecurityGroupsDataSourceBasic(t *testing.T) {
 		}),
 	}
 
-	securityGroupsCheckInfo.dataSourceTestCheck(t, rand, nameRegexConf, idsConf, vpcIdConf, tagsConf, allConf)
+	securityGroupsCheckInfo.dataSourceTestCheck(t, rand, nameRegexConf, idsConf, vpcIdConf, tagsConf, resourceGroupIdConf, allConf)
 }
 
 func testAccCheckAlicloudSecurityGroupsDataSourceConfig(rand int, attrMap map[string]string) string {
@@ -104,6 +118,7 @@ resource "alicloud_security_group" "default" {
   name        = "${var.name}"
   description = "test security group"
   vpc_id      = "${alicloud_vpc.default.id}"
+  resource_group_id = "%s"
   tags = {
 		from = "datasource"
 		usage1 = "test"
@@ -113,7 +128,7 @@ resource "alicloud_security_group" "default" {
 
 data "alicloud_security_groups" "default" {
   %s
-}`, rand, strings.Join(pairs, "\n  "))
+}`, rand, os.Getenv("ALICLOUD_RESOURCE_GROUP_ID"), strings.Join(pairs, "\n  "))
 	return config
 }
 
@@ -123,6 +138,7 @@ var existSecurityGroupsMapFunc = func(rand int) map[string]string {
 		"names.#":                      "1",
 		"groups.#":                     "1",
 		"groups.0.vpc_id":              CHECKSET,
+		"groups.0.resource_group_id":   os.Getenv("ALICLOUD_RESOURCE_GROUP_ID"),
 		"groups.0.security_group_type": "normal",
 		"groups.0.name":                fmt.Sprintf("tf-testAccCheckAlicloudSecurityGroupsDataSourceConfig%d", rand),
 		"groups.0.tags.from":           "datasource",
