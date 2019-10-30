436213d7dee3a271070ea7f866e838f88868120a
He Guimin <guimin.hgm@alibaba-inc.com>
2019-10-30 21:57:23 +0800

    fix(slb_listener): fix server_certificate_id diff bug and add sag ci

--- examples/security-group/main.tf
+++ examples/security-group/main.tf
@@ -1,12 +1,16 @@
+resource "alicloud_vpc" "this" {
+  count      = var.vpc_id == "" ? 1 : 0
+  cidr_block = "172.16.0.0/16"
+}
 resource "alicloud_security_group" "default" {
   name   = var.security_group_name
-  vpc_id = var.vpc_id
+  vpc_id = var.vpc_id == "" ? alicloud_vpc.this.0.id : var.vpc_id
 }
 
 resource "alicloud_security_group_rule" "http-in" {
   type              = "ingress"
   ip_protocol       = "tcp"
-  nic_type          = "internet"
+  nic_type          = "intranet"
   policy            = "accept"
   port_range        = "80/80"
   priority          = 1
@@ -17,11 +21,10 @@ resource "alicloud_security_group_rule" "http-in" {
 resource "alicloud_security_group_rule" "ssh-in" {
   type              = "ingress"
   ip_protocol       = "tcp"
-  nic_type          = "internet"
+  nic_type          = "intranet"
   policy            = "accept"
   port_range        = "22/22"
   priority          = 1
   security_group_id = alicloud_security_group.default.id
   cidr_ip           = "0.0.0.0/0"
 }
-
