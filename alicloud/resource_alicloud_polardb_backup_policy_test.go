c509933c22f3cef4e1e9d75bd7adbafcb23293f8
wb-zyg545395 <wb-zyg545395@alibaba-inc.com>
2019-11-15 15:32:54 +0800

    improve(polardb): feature polardb support backup policy

---
+++ alicloud/resource_alicloud_polardb_backup_policy_test.go
@@ -0,0 +1,105 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/polardb"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func TestAccAlicloudPolarDBBackupPolicy(t *testing.T) {
+	var v *polardb.DescribeBackupPolicyResponse
+	resourceId := "alicloud_polardb_backup_policy.default"
+	serverFunc := func() interface{} {
+		return &PolarDBService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, serverFunc, "DescribeBackupPolicy")
+	ra := resourceAttrInit(resourceId, nil)
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	name := "tf-testAccPolarDBbackuppolicy"
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourcePolarDBBackupPolicyConfigDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"db_cluster_id":           "${alicloud_polardb_cluster.default.id}",
+					"preferred_backup_period": []string{"Tuesday", "Wednesday"},
+					"preferred_backup_time":   "10:00Z-11:00Z",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"preferred_backup_period.#": "2",
+						"preferred_backup_time":     "10:00Z-11:00Z",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"preferred_backup_period": []string{"Wednesday", "Monday", "Saturday"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"preferred_backup_period.#": "3",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"preferred_backup_time": "15:00Z-16:00Z",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"preferred_backup_time": "15:00Z-16:00Z",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"preferred_backup_period": []string{"Tuesday", "Thursday", "Friday", "Sunday"},
+					"preferred_backup_time":   "17:00Z-18:00Z",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"preferred_backup_period.#": "4",
+						"preferred_backup_time":     "17:00Z-18:00Z",
+					}),
+				),
+			}},
+	})
+}
+
+func resourcePolarDBBackupPolicyConfigDependence(name string) string {
+	return fmt.Sprintf(`
+	%s
+	variable "creation" {
+		default = "PolarDB"
+	}
+
+	variable "name" {
+		default = "%s"
+	}
+
+	resource "alicloud_polardb_cluster" "default" {
+		db_type = "MySQL"
+		db_version = "8.0"
+		pay_type = "PostPaid"
+		db_node_class = "polar.mysql.x4.large"
+		vswitch_id = "${alicloud_vswitch.default.id}"
+		description = "${var.name}"
+	}
+`, PolarDBCommonTestCase, name)
+}
