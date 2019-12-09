4a3a4d08d1b73a92061b8efe0084211c13afc339
Tareq Sharafy <tareq.sharafy@sap.com>
2019-12-09 23:56:10 +0200

    unused import

--- alicloud/data_source_alicloud_sts_caller_identity_test.go
+++ alicloud/data_source_alicloud_sts_caller_identity_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccAliclouStsCallerIdentityDataSource_basic(t *testing.T) {
