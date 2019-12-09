7a1250a2bff827cb275f47a3342953bef45aad4b
Tareq Sharafy <tareq.sharafy@sap.com>
2019-12-09 23:51:51 +0200

    add acceptance test

---
+++ alicloud/data_source_alicloud_sts_caller_identity_test.go
@@ -0,0 +1,34 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccAliclouStsCallerIdentityDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: TestAccAliclouStsCallerIdentityDataSourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_sts_caller_identity.cur"),
+					resource.TestCheckResourceAttrSet("data.alicloud_sts_caller_identity.cur", "arn"),
+					resource.TestCheckResourceAttrSet("data.alicloud_sts_caller_identity.cur", "account_id"),
+					resource.TestCheckResourceAttrSet("data.alicloud_sts_caller_identity.cur", "identity_type"),
+					resource.TestCheckResourceAttrSet("data.alicloud_sts_caller_identity.cur", "principal_id"),
+				),
+			},
+		},
+	})
+}
+
+const TestAccAliclouStsCallerIdentityDataSourceBasic = `
+data "alicloud_sts_caller_identity" "cur" {
+}
+`
