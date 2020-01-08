434e26d91e6fa883c4ba15217aecd5d1d034b73f
LexWong <914088735@qq.com>
2020-01-08 20:57:42 +0800

    modify(market): modify the attributes of market products datasource.

--- alicloud/data_source_alicloud_market_products_test.go
+++ alicloud/data_source_alicloud_market_products_test.go
@@ -14,40 +14,41 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 		"",
 		dataSourceMarketProductsConfigDependence)
 
+	basicConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"product_type": "MIRROR",
+			"category_id":  "53366009",
+		}),
+	}
 	nameRegexConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"name_regex":   "BatchCompute Ubuntu14.04",
 			"product_type": "MIRROR",
+			"category_id":  "53366009",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"name_regex":   "BatchCompute_fake",
 			"product_type": "MIRROR",
+			"category_id":  "53366009",
 		}),
 	}
 	idsConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"ids":          []string{"cmjj022644"},
 			"product_type": "MIRROR",
+			"category_id":  "53366009",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"ids":          []string{"cmjj022644_fake"},
 			"product_type": "MIRROR",
+			"category_id":  "53366009",
 		}),
 	}
 	sortConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"sort":         "user_count-desc",
 			"product_type": "MIRROR",
-		}),
-	}
-	categoryIdConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"category_id": "53366009",
-		}),
-	}
-	productTypeConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"product_type": "MIRROR",
+			"category_id":  "53366009",
 		}),
 	}
 
@@ -70,19 +71,12 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 
 	var existMarketProductsMapFunc = func(rand int) map[string]string {
 		return map[string]string{
-			"ids.#":                        CHECKSET,
-			"ids.0":                        CHECKSET,
-			"products.#":                   CHECKSET,
-			"products.0.category_id":       CHECKSET,
-			"products.0.code":              CHECKSET,
-			"products.0.delivery_way":      CHECKSET,
-			"products.0.image_url":         CHECKSET,
-			"products.0.name":              CHECKSET,
-			"products.0.score":             CHECKSET,
-			"products.0.short_description": CHECKSET,
-			"products.0.supplier_id":       CHECKSET,
-			"products.0.supplier_name":     CHECKSET,
-			"products.0.target_url":        CHECKSET,
+			"ids.#":                 CHECKSET,
+			"ids.0":                 CHECKSET,
+			"products.#":            CHECKSET,
+			"products.0.code":       CHECKSET,
+			"products.0.name":       CHECKSET,
+			"products.0.target_url": CHECKSET,
 		}
 	}
 
@@ -99,7 +93,7 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 		fakeMapFunc:  fakeMarketProductsMapFunc,
 	}
 
-	pvtzZoneRecordsCheckInfo.dataSourceTestCheck(t, rand, nameRegexConf, idsConf, sortConf, categoryIdConf, productTypeConf, allConf)
+	pvtzZoneRecordsCheckInfo.dataSourceTestCheck(t, rand, basicConf, nameRegexConf, idsConf, sortConf, allConf)
 }
 
 func dataSourceMarketProductsConfigDependence(name string) string {
