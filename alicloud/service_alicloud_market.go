a57c3a152e1506ee7cb30588fe0baac98725fbf7
LexWong <914088735@qq.com>
2020-01-10 17:54:04 +0800

    feat(market): added new resource alicloud_market_order.

--- alicloud/service_alicloud_market.go
+++ alicloud/service_alicloud_market.go
@@ -1,7 +1,30 @@
 package alicloud
 
-import "github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+import (
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/market"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
 
 type MarketService struct {
 	client *connectivity.AliyunClient
+}
+
+func (s *MarketService) DescribeMarketOrder(id string) (order *market.DescribeOrderResponse, err error) {
+	request := market.CreateDescribeOrderRequest()
+	request.OrderId = id
+	raw, err := s.client.WithMarketClient(func(client *market.Client) (interface{}, error) {
+		return client.DescribeOrder(request)
+	})
+	response, _ := raw.(*market.DescribeOrderResponse)
+	if err != nil {
+		if IsExceptedErrors(err, []string{"null"}) {
+			return order, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
+		}
+		return order, WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+	if response.OrderId == 0 {
+		return order, WrapErrorf(Error(GetNotFoundMessage("Market Order", id)), NotFoundMsg, AlibabaCloudSdkGoERROR)
+	}
+	return response, nil
 }
