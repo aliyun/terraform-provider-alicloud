b76f33792fc71a520105a000ea317f5de282ce4b
LexWong <914088735@qq.com>
2020-01-13 18:50:25 +0800

    improve(market): supported new field 'search_term'.

--- alicloud/data_source_alicloud_market_products_test.go
+++ alicloud/data_source_alicloud_market_products_test.go
@@ -14,41 +14,45 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 		"",
 		dataSourceMarketProductsConfigDependence)
 
-	basicConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"product_type": "MIRROR",
-			"category_id":  "53366009",
-		}),
-	}
 	nameRegexConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"name_regex":   "BatchCompute Ubuntu14.04",
 			"product_type": "MIRROR",
-			"category_id":  "53366009",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"name_regex":   "BatchCompute_fake",
 			"product_type": "MIRROR",
-			"category_id":  "53366009",
 		}),
 	}
 	idsConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"ids":          []string{"cmjj022644"},
 			"product_type": "MIRROR",
-			"category_id":  "53366009",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"ids":          []string{"cmjj022644_fake"},
 			"product_type": "MIRROR",
-			"category_id":  "53366009",
 		}),
 	}
 	sortConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"sort":         "user_count-desc",
 			"product_type": "MIRROR",
-			"category_id":  "53366009",
+		}),
+	}
+	categoryIdConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"category_id": "53366009",
+		}),
+	}
+	productTypeConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"product_type": "MIRROR",
+		}),
+	}
+	searchTermConf := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"search_term": "镜像",
 		}),
 	}
 
@@ -59,6 +63,7 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 			"product_type": "MIRROR",
 			"name_regex":   "SQLServer2016_Ent_FullFeature_winupdate",
 			"ids":          []string{"cmjj031537"},
+			"search_term":  "SQLServer2016_Ent_FullFeature_winupdate",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"sort":         "created_on-desc",
@@ -66,17 +71,25 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 			"product_type": "MIRROR",
 			"name_regex":   "SQLServer2016_Ent_FullFeature_winupdate_fake",
 			"ids":          []string{"cmjj031537"},
+			"search_term":  "镜像",
 		}),
 	}
 
 	var existMarketProductsMapFunc = func(rand int) map[string]string {
 		return map[string]string{
-			"ids.#":                 CHECKSET,
-			"ids.0":                 CHECKSET,
-			"products.#":            CHECKSET,
-			"products.0.code":       CHECKSET,
-			"products.0.name":       CHECKSET,
-			"products.0.target_url": CHECKSET,
+			"ids.#":                        CHECKSET,
+			"ids.0":                        CHECKSET,
+			"products.#":                   CHECKSET,
+			"products.0.category_id":       CHECKSET,
+			"products.0.code":              CHECKSET,
+			"products.0.delivery_way":      CHECKSET,
+			"products.0.image_url":         CHECKSET,
+			"products.0.name":              CHECKSET,
+			"products.0.score":             CHECKSET,
+			"products.0.short_description": CHECKSET,
+			"products.0.supplier_id":       CHECKSET,
+			"products.0.supplier_name":     CHECKSET,
+			"products.0.target_url":        CHECKSET,
 		}
 	}
 
@@ -93,7 +106,7 @@ func TestAccAlicloudMarketProductsDataSource(t *testing.T) {
 		fakeMapFunc:  fakeMarketProductsMapFunc,
 	}
 
-	pvtzZoneRecordsCheckInfo.dataSourceTestCheck(t, rand, basicConf, nameRegexConf, idsConf, sortConf, allConf)
+	pvtzZoneRecordsCheckInfo.dataSourceTestCheck(t, rand, nameRegexConf, idsConf, sortConf, categoryIdConf, productTypeConf, searchTermConf, allConf)
 }
 
 func dataSourceMarketProductsConfigDependence(name string) string {
