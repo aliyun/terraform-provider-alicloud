91c51e8fc0bb186a836613c9d05334809c954d49
He Guimin <guimin.hgm@alibaba-inc.com>
2020-01-14 20:53:01 +0800

    improve(pvtz_zone): correct the docs error

--- alicloud/data_source_alicloud_alikafka_sasl_users_test.go
+++ alicloud/data_source_alicloud_alikafka_sasl_users_test.go
@@ -78,7 +78,7 @@ func dataSourceAlikafkaSaslUsersConfigDependence(name string) string {
 		}
 
 		resource "alicloud_alikafka_instance" "default" {
-          name = "tf-testacc-alikafkainstance"
+          name = "${var.name}"
 		  topic_quota = "50"
 		  disk_type = "1"
 		  disk_size = "500"
