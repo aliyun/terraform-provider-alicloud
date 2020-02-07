1fa714bc29f19f8c1519fd269d2b3641023fa6bd
He Guimin <guimin.hgm@alibaba-inc.com>
2020-02-07 20:50:42 +0800

    fix(db_account_privilege): resolve privilege timeout bug on PosrgreSql

--- alicloud/resource_alicloud_db_account_privilege_test.go
+++ alicloud/resource_alicloud_db_account_privilege_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
 )
 
-func TestAccAlicloudDBAccountPrivilege_update(t *testing.T) {
+func TestAccAlicloudDBAccountPrivilege_mysql(t *testing.T) {
 
 	var v *rds.DBInstanceAccount
 	rand := acctest.RandInt()
@@ -31,7 +31,7 @@ func TestAccAlicloudDBAccountPrivilege_update(t *testing.T) {
 	rac := resourceAttrCheckInit(rc, ra)
 
 	testAccCheck := rac.resourceAttrMapUpdateSet()
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceDBAccountPrivilegeConfigDependence)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceDBAccountPrivilegeConfigDependenceForMySql)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -91,7 +91,74 @@ func TestAccAlicloudDBAccountPrivilege_update(t *testing.T) {
 
 }
 
-func resourceDBAccountPrivilegeConfigDependence(name string) string {
+func TestAccAlicloudDBAccountPrivilege_PostgreSql(t *testing.T) {
+
+	var v *rds.DBInstanceAccount
+	rand := acctest.RandInt()
+	name := fmt.Sprintf("tf-testacc%sdnsrecordbasic%v.abc", defaultRegionToTest, rand)
+	resourceId := "alicloud_db_account_privilege.default"
+	var basicMap = map[string]string{
+		"instance_id":  CHECKSET,
+		"account_name": "tftestprivilege",
+		"privilege":    "DBOwner",
+		"db_names.#":   "1",
+	}
+	ra := resourceAttrInit(resourceId, basicMap)
+	serviceFunc := func() interface{} {
+		return &RdsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, serviceFunc, "DescribeDBAccountPrivilege")
+	rac := resourceAttrCheckInit(rc, ra)
+
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceDBAccountPrivilegeConfigDependenceForPostgreSql)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		// module name
+		IDRefreshName: resourceId,
+
+		Providers:    testAccProviders,
+		CheckDestroy: rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"instance_id":  "${alicloud_db_instance.default.id}",
+					"account_name": "${alicloud_db_account.default.name}",
+					"privilege":    "DBOwner",
+					"db_names":     []string{"${alicloud_db_database.default.0.name}"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(nil),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"instance_id":  "${alicloud_db_instance.default.id}",
+					"account_name": "${alicloud_db_account.default.name}",
+					"privilege":    "DBOwner",
+					"db_names":     "${alicloud_db_database.default.*.name}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"db_names.#": "2",
+					}),
+				),
+			},
+		},
+	})
+
+}
+
+func resourceDBAccountPrivilegeConfigDependenceForMySql(name string) string {
 	return fmt.Sprintf(`
 %s
 	variable "creation" {
@@ -127,6 +194,50 @@ func resourceDBAccountPrivilegeConfigDependence(name string) string {
 	  instance_id = "${alicloud_db_instance.default.id}"
 	  name = "tfaccountpri_${count.index}"
 	  description = "from terraform"
+	}
+
+	resource "alicloud_db_account" "default" {
+	  instance_id = "${alicloud_db_instance.default.id}"
+	  name = "tftestprivilege"
+	  password = "Test12345"
+	  description = "from terraform"
+	}
+`, RdsCommonTestCase, name)
+}
+
+func resourceDBAccountPrivilegeConfigDependenceForPostgreSql(name string) string {
+	return fmt.Sprintf(`
+%s
+	variable "creation" {
+		default = "Rds"
+	}
+
+	variable "name" {
+		default = "%s"
+	}
+	
+	data "alicloud_db_instance_classes" "default" {
+		instance_charge_type = "PostPaid"
+		engine               = "PostgreSQL"
+		engine_version       = "10.0"
+		storage_type         = "cloud_ssd"
+	}
+
+	resource "alicloud_db_instance" "default" {
+		engine = "PostgreSQL"
+		engine_version = "10.0"
+		instance_type = "${data.alicloud_db_instance_classes.default.instance_classes.0.instance_class}"
+		instance_storage = "${data.alicloud_db_instance_classes.default.instance_classes.0.storage_range.min}"
+		vswitch_id = "${alicloud_vswitch.default.id}"
+		instance_name = "${var.name}"
+	}
+
+	resource "alicloud_db_database" "default" {
+	  count = 2
+	  instance_id = "${alicloud_db_instance.default.id}"
+	  name = "tfaccountpri_${count.index}"
+	  description = "from terraform"
+      character_set = "UTF8"
 	}
 
 	resource "alicloud_db_account" "default" {
