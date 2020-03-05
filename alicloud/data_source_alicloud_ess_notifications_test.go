af14a2ecf46239c4cf0072c2c34f4444251c6b98
琚晓朝 <wb-jxc665366@alibaba-inc.com>
2020-03-05 21:51:27 +0800

    first update

---
+++ alicloud/data_source_alicloud_ess_notifications_test.go
@@ -0,0 +1,99 @@
+package alicloud
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+)
+
+func TestAccAlicloudEssNotificationsDataSource(t *testing.T) {
+	rand := acctest.RandInt()
+	scalingGroupIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEssNotificationsDataSourceConfig(rand, map[string]string{
+			"scaling_group_id": `"${alicloud_ess_notification.default.scaling_group_id}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEssNotificationsDataSourceConfig(rand, map[string]string{
+			"scaling_group_id": `"${alicloud_ess_notification.default.scaling_group_id}_fake"`,
+		}),
+	}
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEssNotificationsDataSourceConfig(rand, map[string]string{
+			"scaling_group_id": `"${alicloud_ess_notification.default.scaling_group_id}"`,
+			"ids":              `["${alicloud_ess_notification.default.id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudEssNotificationsDataSourceConfig(rand, map[string]string{
+			"scaling_group_id": `"${alicloud_ess_notification.default.scaling_group_id}"`,
+			"ids":              `["${alicloud_ess_notification.default.id}_fake"]`,
+		}),
+	}
+
+	var existEssnotificationsMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                            "1",
+			"notifications.#":                  "1",
+			"notifications.0.notification_arn": CHECKSET,
+			"notifications.0.scaling_group_id": CHECKSET,
+		}
+	}
+
+	var fakeEssnotificationsMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"notifications.#": "0",
+			"ids.#":           "0",
+		}
+	}
+
+	var essNotificationsCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_ess_notifications.default",
+		existMapFunc: existEssnotificationsMapFunc,
+		fakeMapFunc:  fakeEssnotificationsMapFunc,
+	}
+
+	essNotificationsCheckInfo.dataSourceTestCheck(t, rand, scalingGroupIdConf, allConf)
+}
+
+func testAccCheckAlicloudEssNotificationsDataSourceConfig(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+
+	config := fmt.Sprintf(`
+%s
+
+variable "name" {
+	default = "tf-testAccDataSourceEssNotifications-%d"
+}
+data "alicloud_regions" "default" {
+    current = true
+}
+
+data "alicloud_account" "default" {
+}
+
+resource "alicloud_ess_scaling_group" "default" {
+    min_size = 1
+    max_size = 1
+    scaling_group_name = "${var.name}"
+    removal_policies = ["OldestInstance", "NewestInstance"]
+    vswitch_ids = ["${alicloud_vswitch.default.id}"]
+}
+
+resource "alicloud_mns_queue" "default"{
+    name="${var.name}"
+}
+
+resource "alicloud_ess_notification" "default" {
+    scaling_group_id = "${alicloud_ess_scaling_group.default.id}"
+    notification_types = ["AUTOSCALING:SCALE_OUT_SUCCESS"]
+    notification_arn = "acs:ess:${data.alicloud_regions.default.regions.0.id}:${data.alicloud_account.default.id}:queue/${alicloud_mns_queue.default.name}"
+}
+
+data "alicloud_ess_notifications" "default"{
+  %s
+}
+`, EcsInstanceCommonTestCase, rand, strings.Join(pairs, "\n  "))
+	return config
+}
