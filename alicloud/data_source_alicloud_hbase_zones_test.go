123869c5996a29e4c8e93ab00b423c239ecc12c6
zhouqilin <1373413131@qq.com>
2020-03-12 20:13:05 +0800

    A new datasource for HBase zones.

---
+++ alicloud/data_source_alicloud_hbase_zones_test.go
@@ -0,0 +1,46 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+)
+
+func TestAccAlicloudHBaseZonesDataSource_basic(t *testing.T) {
+	rand := acctest.RandInt()
+	resourceId := "data.alicloud_hbase_zones.default"
+
+	testAccConfig := dataSourceTestAccConfigFunc(resourceId, "", dataSourceHBaseZonesConfigDependence)
+
+	multiConfig := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"multi": "true",
+		}),
+	}
+
+	var existHBaseZonesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":   CHECKSET,
+			"zones.#": CHECKSET,
+		}
+	}
+
+	var fakeHBaseZonesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":   "0",
+			"zones.#": "0",
+		}
+	}
+
+	var HBaseZonesCheckInfo = dataSourceAttr{
+		resourceId:   resourceId,
+		existMapFunc: existHBaseZonesMapFunc,
+		fakeMapFunc:  fakeHBaseZonesMapFunc,
+	}
+
+	HBaseZonesCheckInfo.dataSourceTestCheck(t, rand, multiConfig)
+}
+
+func dataSourceHBaseZonesConfigDependence(name string) string {
+	return ""
+}
