f743244b4334eccb270c14b6c1d4b4802fa4123f
zhouqilin <1373413131@qq.com>
2020-03-12 16:44:13 +0800

    A new datasource for KVStore zones.

---
+++ alicloud/data_source_alicloud_kvstore_zones_test.go
@@ -0,0 +1,62 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+)
+
+func TestAccAlicloudKVStoreZonesDataSource_basic(t *testing.T) {
+	rand := acctest.RandInt()
+	resourceId := "data.alicloud_kvstore_zones.default"
+
+	testAccConfig := dataSourceTestAccConfigFunc(resourceId, "", dataSourceKVStoreZonesDependence)
+
+	multiConfig := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"multi": "true",
+		}),
+	}
+
+	chargeTypeConfig := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"instance_charge_type": "PrePaid",
+		}),
+	}
+
+	allconfig := dataSourceTestAccConfig{
+		existConfig: testAccConfig(map[string]interface{}{
+			"multi":                "true",
+			"instance_charge_type": "PrePaid",
+		}),
+	}
+
+	var existKVStoreDBZonesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                    CHECKSET,
+			"ids.0":                    CHECKSET,
+			"zones.#":                  CHECKSET,
+			"zones.0.id":               CHECKSET,
+			"zones.0.multi_zone_ids.#": CHECKSET,
+		}
+	}
+
+	var fakeKVStoreDBZonesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":   "0",
+			"zones.#": "0",
+		}
+	}
+
+	var kvStoreDBZonesCheckInfo = dataSourceAttr{
+		resourceId:   resourceId,
+		existMapFunc: existKVStoreDBZonesMapFunc,
+		fakeMapFunc:  fakeKVStoreDBZonesMapFunc,
+	}
+
+	kvStoreDBZonesCheckInfo.dataSourceTestCheck(t, rand, multiConfig, chargeTypeConfig, allconfig)
+}
+
+func dataSourceKVStoreZonesDependence(name string) string {
+	return ""
+}
--- alicloud/provider.go
+++ alicloud/provider.go
@@ -198,6 +198,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_gpdb_instances":                    dataSourceAlicloudGpdbInstances(),
 			"alicloud_gpdb_zones":                        dataSourceAlicloudGpdbZones(),
 			"alicloud_kvstore_instances":                 dataSourceAlicloudKVStoreInstances(),
+			"alicloud_kvstore_zones":                     dataSourceAlicloudKVStoreZones(),
 			"alicloud_kvstore_instance_classes":          dataSourceAlicloudKVStoreInstanceClasses(),
 			"alicloud_kvstore_instance_engines":          dataSourceAlicloudKVStoreInstanceEngines(),
 			"alicloud_cen_instances":                     dataSourceAlicloudCenInstances(),
