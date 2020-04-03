b1972ce016788abf7d5bf7a07f7ea35c692bca2e
Rich-yi <785301535@qq.com>
2020-04-03 20:20:17 +0800

    Update CHANGELOG.md

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -153,7 +153,7 @@ const Module = "Terraform-Module"
 
 var goSdkMutex = sync.RWMutex{} // The Go SDK is not thread-safe
 // The main version number that is being run at the moment.
-var providerVersion = "1.77.0"
+var providerVersion = "1.78.0"
 var terraformVersion = strings.TrimSuffix(schema.Provider{}.TerraformVersion, "-dev")
 
 // Client for AliyunClient
