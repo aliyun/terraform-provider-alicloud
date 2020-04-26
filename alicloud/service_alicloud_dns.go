57fc70885590200623d6180675d9aba7b7be1c5d
zhouqilin <1373413131@qq.com>
2020-04-26 12:49:08 +0800

    improve: upgraded the sdk and add new resource for DNS domain.

--- alicloud/service_alicloud_dns.go
+++ alicloud/service_alicloud_dns.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
 )
 
@@ -174,4 +175,89 @@ func (s *DnsService) WaitForAlidnsDomainAttachment(id string, expected map[strin
 		}
 		time.Sleep(DefaultIntervalShort * time.Second)
 	}
+}
+
+func (s *DnsService) ListTagResources(id string) (object alidns.ListTagResourcesResponse, err error) {
+	request := alidns.CreateListTagResourcesRequest()
+	request.RegionId = s.client.RegionId
+
+	request.ResourceType = "DOMAIN"
+	request.ResourceId = &[]string{id}
+
+	raw, err := s.client.WithDnsClient(func(alidnsClient *alidns.Client) (interface{}, error) {
+		return alidnsClient.ListTagResources(request)
+	})
+	if err != nil {
+		err = WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return
+	}
+	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+	response, _ := raw.(*alidns.ListTagResourcesResponse)
+	return *response, nil
+}
+
+func (s *DnsService) SetResourceTags(d *schema.ResourceData, resourceType string) error {
+	oldItems, newItems := d.GetChange("tags")
+	added := make([]alidns.TagResourcesTag, 0)
+	for key, value := range newItems.(map[string]interface{}) {
+		added = append(added, alidns.TagResourcesTag{
+			Key:   key,
+			Value: value.(string),
+		})
+	}
+	removed := make([]string, 0)
+	for key, _ := range oldItems.(map[string]interface{}) {
+		removed = append(removed, key)
+	}
+	if len(removed) > 0 {
+		request := alidns.CreateUntagResourcesRequest()
+		request.RegionId = s.client.RegionId
+		request.ResourceId = &[]string{d.Id()}
+		request.ResourceType = resourceType
+		request.TagKey = &removed
+		raw, err := s.client.WithDnsClient(func(alidnsClient *alidns.Client) (interface{}, error) {
+			return alidnsClient.UntagResources(request)
+		})
+		addDebug(request.GetActionName(), raw)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		}
+	}
+	if len(added) > 0 {
+		request := alidns.CreateTagResourcesRequest()
+		request.RegionId = s.client.RegionId
+		request.ResourceId = &[]string{d.Id()}
+		request.ResourceType = resourceType
+		request.Tag = &added
+		raw, err := s.client.WithDnsClient(func(alidnsClient *alidns.Client) (interface{}, error) {
+			return alidnsClient.TagResources(request)
+		})
+		addDebug(request.GetActionName(), raw)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		}
+	}
+	return nil
+}
+
+func (s *DnsService) DescribeDnsDomain(id string) (object alidns.DescribeDomainInfoResponse, err error) {
+	request := alidns.CreateDescribeDomainInfoRequest()
+	request.RegionId = s.client.RegionId
+
+	request.DomainName = id
+
+	raw, err := s.client.WithDnsClient(func(alidnsClient *alidns.Client) (interface{}, error) {
+		return alidnsClient.DescribeDomainInfo(request)
+	})
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InvalidDomainName.NoExist"}) {
+			err = WrapErrorf(Error(GetNotFoundMessage("DnsDomain", id)), NotFoundMsg, ProviderERROR)
+			return
+		}
+		err = WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return
+	}
+	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+	response, _ := raw.(*alidns.DescribeDomainInfoResponse)
+	return *response, nil
 }
