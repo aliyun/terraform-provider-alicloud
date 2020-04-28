f39ef6b61270e5755ac4b746cf4daa5a2945dc6c
吕戈 <steve.cmm@alibaba-inc.com>
2020-04-28 11:29:32 +0800

    add adb connection resource

---
+++ alicloud/resource_alicloud_adb_connection_test.go
@@ -0,0 +1,83 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/adb"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func TestAccAlicloudAdbConnectionConfig(t *testing.T) {
+	var v *adb.Address
+	rand := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	name := fmt.Sprintf("tf-testAccAdbConnection%s", rand)
+	var basicMap = map[string]string{
+		"db_cluster_id":     CHECKSET,
+		"connection_string": CHECKSET,
+		"ip_address":        CHECKSET,
+		"port":              CHECKSET,
+	}
+	resourceId := "alicloud_adb_connection.default"
+	ra := resourceAttrInit(resourceId, basicMap)
+	serviceFunc := func() interface{} {
+		return &AdbService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, serviceFunc, "DescribeAdbConnection")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceAdbConnectionConfigDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		// module name
+		IDRefreshName: resourceId,
+
+		Providers:    testAccProviders,
+		CheckDestroy: rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"db_cluster_id":     "${alicloud_adb_cluster.cluster.id}",
+					"connection_prefix": fmt.Sprintf("tf-testacc%s", rand),
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(nil),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func resourceAdbConnectionConfigDependence(name string) string {
+	return fmt.Sprintf(`
+	%s
+	variable "creation" {
+		default = "ADB"
+	}
+
+	variable "name" {
+		default = "%s"
+	}
+
+	resource "alicloud_adb_cluster" "cluster" {
+        db_cluster_version      = "3.0"
+        db_cluster_category     = "Cluster"
+        db_node_class           = "C8"
+        db_node_count           = 2
+        db_node_storage         = 200
+		pay_type                = "PostPaid"
+		vswitch_id              = "${data.alicloud_vswitches.default.ids.0}"
+		description             = "${var.name}"
+	}`, AdbCommonTestCase, name)
+}
--- website/docs/r/adb_backup_policy.html.markdown
+++ website/docs/r/adb_backup_policy.html.markdown
@@ -1,5 +1,5 @@
 ---
-subcategory: "ADB"
+subcategory: "AnalyticDB for MySQL (ADB)"
 layout: "alicloud"
 page_title: "Alicloud: alicloud_adb_backup_policy"
 sidebar_current: "docs-alicloud-resource-adb-backup-policy"
