919ad42ec4743c9f5e4cd0bbe72977ef4d290ad0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-04-30 12:24:48 +0200

    Return error if file deletion fails

--- alicloud/common.go
+++ alicloud/common.go
@@ -454,7 +454,9 @@ func writeToFile(filePath string, data interface{}) error {
 		}
 	}
 
-	os.Remove(filePath)
+	if err := os.Remove(filePath); err != nil {
+		return err
+	}
 
 	var out string
 	switch data.(type) {
