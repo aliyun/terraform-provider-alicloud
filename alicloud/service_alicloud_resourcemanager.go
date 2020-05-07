f4b8aba654ae4166719ed94ffb9cb116ddadc277
刘日 <wb-lr721942@alibaba-inc.com>
2020-05-07 18:37:47 +0800

    A new resource for ResourceManager ResourceGroup.

--- alicloud/service_alicloud_resourcemanager.go
+++ alicloud/service_alicloud_resourcemanager.go
@@ -1,7 +1,10 @@
 package alicloud
 
 import (
+	"log"
+
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/resourcemanager"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
 )
 
@@ -29,4 +32,50 @@ func (s *ResourcemanagerService) DescribeResourceManagerRole(id string) (object 
 	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 	response, _ := raw.(*resourcemanager.GetRoleResponse)
 	return response.Role, nil
+}
+
+func (s *ResourcemanagerService) DescribeResourceManagerResourceGroup(id string) (object resourcemanager.ResourceGroup, err error) {
+	request := resourcemanager.CreateGetResourceGroupRequest()
+	request.RegionId = s.client.RegionId
+
+	request.ResourceGroupId = id
+
+	raw, err := s.client.WithResourcemanagerClient(func(resourcemanagerClient *resourcemanager.Client) (interface{}, error) {
+		return resourcemanagerClient.GetResourceGroup(request)
+	})
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EntityNotExists.ResourceGroup"}) {
+			err = WrapErrorf(Error(GetNotFoundMessage("ResourceManagerResourceGroup", id)), NotFoundMsg, ProviderERROR)
+			return
+		}
+		err = WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return
+	}
+	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+	response, _ := raw.(*resourcemanager.GetResourceGroupResponse)
+	if response.ResourceGroup.Status == "PendingDelete" {
+		log.Printf("[WARN] Removing ResourceManagerResourceGroup  %s because it's already gone", id)
+		return response.ResourceGroup, WrapErrorf(Error(GetNotFoundMessage("ResourceManagerResourceGroup", id)), NotFoundMsg, ProviderERROR)
+	}
+	return response.ResourceGroup, nil
+}
+
+func (s *ResourcemanagerService) ResourceManagerResourceGroupStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeResourceManagerResourceGroup(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if object.Status == failState {
+				return object, object.Status, WrapError(Error(FailedToReachTargetStatus, object.Status))
+			}
+		}
+		return object, object.Status, nil
+	}
 }
