f8b4df0c569f6149dcc9985ddd17394381aadace
刘日 <wb-lr721942@alibaba-inc.com>
2020-05-25 09:28:53 +0800

    A new datasource for Resource Manager Policies

---
+++ alicloud/data_source_alicloud_resource_manager_policies.go
@@ -0,0 +1,183 @@
+package alicloud
+
+import (
+	"regexp"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/resourcemanager"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func dataSourceAlicloudResourceManagerPolicies() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAlicloudResourceManagerPoliciesRead,
+		Schema: map[string]*schema.Schema{
+			"name_regex": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.ValidateRegexp,
+				ForceNew:     true,
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"policy_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{"Custom", "System"}, false),
+			},
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"policies": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"attachment_count": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"create_date": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"default_version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"policy_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"policy_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"update_date": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAlicloudResourceManagerPoliciesRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+
+	request := resourcemanager.CreateListPoliciesRequest()
+	if v, ok := d.GetOk("policy_type"); ok {
+		request.PolicyType = v.(string)
+	}
+	request.PageSize = requests.NewInteger(PageSizeLarge)
+	request.PageNumber = requests.NewInteger(1)
+	var objects []resourcemanager.Policy
+	var nameRegex *regexp.Regexp
+	if v, ok := d.GetOk("name_regex"); ok {
+		r, err := regexp.Compile(v.(string))
+		if err != nil {
+			return WrapError(err)
+		}
+		nameRegex = r
+	}
+	idsMap := make(map[string]string)
+	if v, ok := d.GetOk("ids"); ok {
+		for _, vv := range v.([]interface{}) {
+			idsMap[vv.(string)] = vv.(string)
+		}
+	}
+	for {
+		raw, err := client.WithResourcemanagerClient(func(resourcemanagerClient *resourcemanager.Client) (interface{}, error) {
+			return resourcemanagerClient.ListPolicies(request)
+		})
+		if err != nil {
+			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_resource_manager_policies", request.GetActionName(), AlibabaCloudSdkGoERROR)
+		}
+		addDebug(request.GetActionName(), raw)
+		response, _ := raw.(*resourcemanager.ListPoliciesResponse)
+
+		for _, item := range response.Policies.Policy {
+			if nameRegex != nil {
+				if !nameRegex.MatchString(item.PolicyName) {
+					continue
+				}
+			}
+			if len(idsMap) > 0 {
+				if _, ok := idsMap[item.PolicyName]; !ok {
+					continue
+				}
+			}
+			objects = append(objects, item)
+		}
+		if len(response.Policies.Policy) < PageSizeLarge {
+			break
+		}
+
+		page, err := getNextpageNumber(request.PageNumber)
+		if err != nil {
+			return WrapError(err)
+		}
+		request.PageNumber = page
+	}
+	ids := make([]string, len(objects))
+	names := make([]string, len(objects))
+	s := make([]map[string]interface{}, len(objects))
+
+	for i, object := range objects {
+		mapping := map[string]interface{}{
+			"attachment_count": object.AttachmentCount,
+			"create_date":      object.CreateDate,
+			"default_version":  object.DefaultVersion,
+			"description":      object.Description,
+			"id":               object.PolicyName,
+			"policy_name":      object.PolicyName,
+			"policy_type":      object.PolicyType,
+			"update_date":      object.UpdateDate,
+		}
+		ids[i] = object.PolicyName
+		names[i] = object.PolicyName
+		s[i] = mapping
+	}
+
+	d.SetId(dataResourceIdHash(ids))
+	if err := d.Set("ids", ids); err != nil {
+		return WrapError(err)
+	}
+
+	if err := d.Set("names", names); err != nil {
+		return WrapError(err)
+	}
+
+	if err := d.Set("policies", s); err != nil {
+		return WrapError(err)
+	}
+	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
+		writeToFile(output.(string), s)
+	}
+
+	return nil
+}
--- alicloud/provider.go
+++ alicloud/provider.go
@@ -275,6 +275,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_edas_clusters":                     dataSourceAlicloudEdasClusters(),
 			"alicloud_resource_manager_folders":          dataSourceAlicloudResourceManagerFolders(),
 			"alicloud_dns_instances":                     dataSourceAlicloudDnsInstances(),
+			"alicloud_resource_manager_policies":         dataSourceAlicloudResourceManagerPolicies(),
 			"alicloud_resource_manager_resource_groups":  dataSourceAlicloudResourceManagerResourceGroups(),
 			"alicloud_resource_manager_policy_versions":  dataSourceAlicloudResourceManagerPolicyVersions(),
 			"alicloud_alidns_domain_groups":              dataSourceAlicloudAlidnsDomainGroups(),
