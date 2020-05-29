f887332839db1097410577030def6f22dca7c85f
wb-xxh498912 <wb-xxh498912@alibaba-inc.com>
2020-05-29 19:30:23 +0800

    add log dashboard resource

---
+++ alicloud/resource_alicloud_log_dashboard.go
@@ -0,0 +1,174 @@
+package alicloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	sls "github.com/aliyun/aliyun-log-go-sdk"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAlicloudLogDashboard() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudLogDashboardCreate,
+		Read:   resourceAlicloudLogDashboardRead,
+		Update: resourceAlicloudLogDashboardUpdate,
+		Delete: resourceAlicloudLogDashboardDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"project_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"dashboard_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"char_list": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceAlicloudLogDashboardCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var requestInfo *sls.Client
+
+	dashBoard := sls.Dashboard{
+		DashboardName: d.Get("dashboard_name").(string),
+		DisplayName:   d.Get("display_name").(string),
+	}
+	jsonErr := json.Unmarshal([]byte(d.Get("char_list").(string)), &dashBoard.ChartList)
+	if jsonErr != nil {
+		return WrapError(jsonErr)
+	}
+
+	if err := resource.Retry(2*time.Minute, func() *resource.RetryError {
+		_, err := client.WithLogClient(func(slsClient *sls.Client) (interface{}, error) {
+			requestInfo = slsClient
+			return nil, slsClient.CreateDashboard(d.Get("project_name").(string), dashBoard)
+		})
+		if err != nil {
+			if IsExpectedErrors(err, []string{LogClientTimeout}) {
+				time.Sleep(5 * time.Second)
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug("CreateDashboard", dashBoard, requestInfo, map[string]interface{}{
+			"dashBoard": dashBoard,
+		})
+		d.SetId(fmt.Sprintf("%s%s%s", d.Get("project_name").(string), COLON_SEPARATED, d.Get("dashboard_name").(string)))
+		return nil
+	}); err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_log_dashboard", "CreateDashboard", AliyunLogGoSdkERROR)
+	}
+	return resourceAlicloudLogDashboardRead(d, meta)
+}
+
+func resourceAlicloudLogDashboardRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	logService := LogService{client}
+	parts, err := ParseResourceId(d.Id(), 2)
+	object, err := logService.DescribeLogDashboard(d.Id())
+	if err != nil {
+		if NotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("project_name", parts[0])
+	d.Set("dashboard_name", object.DashboardName)
+	d.Set("display_name", object.DisplayName)
+	charlist, err := json.Marshal(object.ChartList)
+	if err != nil {
+		return WrapError(err)
+	}
+	d.Set("char_list", string(charlist))
+	return nil
+}
+
+func resourceAlicloudLogDashboardUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	update := false
+	dashboard := sls.Dashboard{}
+
+	if d.HasChange("display_name") {
+		dashboard.DisplayName = d.Get("display_name").(string)
+		update = true
+	}
+	if d.HasChange("char_list") {
+		data := d.Get("char_list").(string)
+		err := json.Unmarshal([]byte(data), &dashboard.ChartList)
+		if err != nil {
+			return WrapError(err)
+		}
+		update = true
+	}
+
+	if update {
+		parts, err := ParseResourceId(d.Id(), 2)
+		if err != nil {
+			return WrapError(err)
+		}
+		dashboard.DashboardName = parts[1]
+		_, err = client.WithLogClient(func(slsClient *sls.Client) (interface{}, error) {
+			return nil, slsClient.UpdateDashboard(parts[0], dashboard)
+		})
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), "UpdateDashboard", AliyunLogGoSdkERROR)
+		}
+	}
+	return resourceAlicloudLogDashboardRead(d, meta)
+}
+
+func resourceAlicloudLogDashboardDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	logService := LogService{client}
+	parts, err := ParseResourceId(d.Id(), 2)
+	if err != nil {
+		return WrapError(err)
+	}
+	var requestInfo *sls.Client
+	err = resource.Retry(3*time.Minute, func() *resource.RetryError {
+		raw, err := client.WithLogClient(func(slsClient *sls.Client) (interface{}, error) {
+			requestInfo = slsClient
+			return nil, slsClient.DeleteDashboard(parts[0], parts[1])
+		})
+		if err != nil {
+			if IsExpectedErrors(err, []string{LogClientTimeout, "RequestTimeout"}) {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug("DeleteDashboard", raw, requestInfo, map[string]interface{}{
+			"project_name": parts[0],
+			"dashboard":    parts[1],
+		})
+		return nil
+	})
+	if err != nil {
+		if IsExpectedErrors(err, []string{"DashboardNotExist", "ProjectNotExist"}) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), "DeleteDashboard", AliyunLogGoSdkERROR)
+	}
+	return WrapError(logService.WaitForLogDashboard(d.Id(), Deleted, DefaultTimeout))
+}
---
+++ website/docs/r/log_dashboard.html.markdown
@@ -0,0 +1,88 @@
+---
+subcategory: "Log Service (SLS)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_log_dashboard"
+sidebar_current: "docs-alicloud-resource-log-dashboard"
+description: |-
+  Provides a Alicloud Log Dashboard resource.
+---
+
+# alicloud\_log\_dashboard
+The dashboard is a real-time data analysis platform provided by the log service. You can display frequently used query and analysis statements in the form of charts and save statistical charts to the dashboard.
+[Refer to details](https://www.alibabacloud.com/help/doc-detail/102530.htm).
+
+-> **NOTE:** Available in 1.86.0
+
+## Example Usage
+
+Basic Usage
+
+```
+
+resource "alicloud_log_project" "default" {
+  name = "tf-project"
+  description = "tf unit test"
+}
+resource "alicloud_log_store" "default" {
+  project = ""tf-project"
+  name = "tf-logstore"
+  retention_period = "3000"
+  shard_count = 1
+}
+resource "alicloud_log_dashboard" "example" {
+  project_name        = "tf-project"
+  dashboard_name = "tf-dashboard"
+  char_list = <<EOF
+  [
+    {
+      "title":"new_title",
+      "type":"map",
+      "search":{
+        "logstore":"tf-logstore",
+        "topic":"new_topic",
+        "query":"* | SELECT COUNT(name) as ct_name, COUNT(product) as ct_product, name,product GROUP BY name,product",
+        "start":"-86400s",
+        "end":"now"
+      },
+      "display":{
+        "xAxis":[
+          "ct_name"
+        ],
+        "yAxis":[
+          "ct_product"
+        ],
+        "xPos":0,
+        "yPos":0,
+        "width":10,
+        "height":12,
+        "displayName":"xixihaha911"
+      }
+    }
+  ]
+EOF
+}
+```
+
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `project_name` - (Required, ForceNew) The name of the log project. It is the only in one Alicloud account.
+* `dashboard_name` - (Required, ForceNew) The name of the Log Dashboard.
+* `char_list` - (Required) Configuration of charts in the dashboard.
+* `display_name` - (Optional) Dashboard alias.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The ID of the Log Dashboard. It sames as its name.
+
+## Import
+
+Log Dashboard can be imported using the id or name, e.g.
+
+```
+$ terraform import alicloud_log_dashboard.example tf-project:tf-logstore:tf-dashboard
+```
