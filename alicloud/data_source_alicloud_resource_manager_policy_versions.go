85038042c64e5acf4d7c20aafc7223e223208f1b
刘日 <wb-lr721942@alibaba-inc.com>
2020-05-29 15:55:24 +0800

    A new datasource for Resource Manager Policy Versions

---
+++ alicloud/data_source_alicloud_resource_manager_policy_versions.go
@@ -0,0 +1,123 @@
+package alicloud
+
+import (
+	"fmt"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/resourcemanager"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-alicloud/alicloud/connectivity"
+)
+
+func dataSourceAlicloudResourceManagerPolicyVersions() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAlicloudResourceManagerPolicyVersionsRead,
+		Schema: map[string]*schema.Schema{
+			"policy_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"policy_type": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{"Custom", "System"}, false),
+			},
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"versions": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"create_date": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"is_default_version": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"version_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAlicloudResourceManagerPolicyVersionsRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+
+	request := resourcemanager.CreateListPolicyVersionsRequest()
+	request.PolicyName = d.Get("policy_name").(string)
+	request.PolicyType = d.Get("policy_type").(string)
+	var objects []resourcemanager.PolicyVersion
+	idsMap := make(map[string]string)
+	if v, ok := d.GetOk("ids"); ok {
+		for _, vv := range v.([]interface{}) {
+			idsMap[vv.(string)] = vv.(string)
+		}
+	}
+	raw, err := client.WithResourcemanagerClient(func(resourcemanagerClient *resourcemanager.Client) (interface{}, error) {
+		return resourcemanagerClient.ListPolicyVersions(request)
+	})
+	if err != nil {
+		return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_resource_manager_policy_versions", request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+	addDebug(request.GetActionName(), raw)
+	response, _ := raw.(*resourcemanager.ListPolicyVersionsResponse)
+
+	for _, item := range response.PolicyVersions.PolicyVersion {
+		if len(idsMap) > 0 {
+			if _, ok := idsMap[request.PolicyName+":"+item.VersionId]; !ok {
+				continue
+			}
+		}
+		objects = append(objects, item)
+	}
+	ids := make([]string, len(objects))
+	s := make([]map[string]interface{}, len(objects))
+
+	for i, object := range objects {
+		mapping := map[string]interface{}{
+			"create_date":        object.CreateDate,
+			"is_default_version": object.IsDefaultVersion,
+			"id":                 fmt.Sprintf("%v:%v", request.PolicyName, object.VersionId),
+			"version_id":         object.VersionId,
+		}
+		ids[i] = fmt.Sprintf("%v:%v", request.PolicyName, object.VersionId)
+		s[i] = mapping
+	}
+
+	d.SetId(dataResourceIdHash(ids))
+	if err := d.Set("ids", ids); err != nil {
+		return WrapError(err)
+	}
+
+	if err := d.Set("versions", s); err != nil {
+		return WrapError(err)
+	}
+	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
+		writeToFile(output.(string), s)
+	}
+
+	return nil
+}
---
+++ alicloud/data_source_alicloud_resource_manager_policy_versions_test.go
@@ -0,0 +1,80 @@
+package alicloud
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+)
+
+func TestAccAlicloudResourceManagerPolicyVersionsDataSource(t *testing.T) {
+	rand := acctest.RandIntRange(1000000, 9999999)
+
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudResourceManagerPolicyVersionsSourceConfig(rand, map[string]string{
+			"ids":         fmt.Sprintf(`["tf-%d:v1"]`, rand),
+			"policy_name": `"${alicloud_resource_manager_policy.default.policy_name}"`,
+			"policy_type": `"${alicloud_resource_manager_policy.default.policy_type}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudResourceManagerPolicyVersionsSourceConfig(rand, map[string]string{
+			"ids":         `["fake"]`,
+			"policy_name": `"${alicloud_resource_manager_policy.default.policy_name}"`,
+			"policy_type": `"${alicloud_resource_manager_policy.default.policy_type}"`,
+		}),
+	}
+
+	var existResourceManagerPolicyVersionsRecordsMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                         "1",
+			"versions.#":                    "1",
+			"versions.0.create_date":        CHECKSET,
+			"versions.0.is_default_version": "true",
+			"versions.0.version_id":         "v1",
+		}
+	}
+
+	var fakeResourceManagerPolicyVersionsRecordsMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":      "0",
+			"versions.#": "0",
+		}
+	}
+
+	var policyVersionsRecordsCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_resource_manager_policy_versions.default",
+		existMapFunc: existResourceManagerPolicyVersionsRecordsMapFunc,
+		fakeMapFunc:  fakeResourceManagerPolicyVersionsRecordsMapFunc,
+	}
+
+	policyVersionsRecordsCheckInfo.dataSourceTestCheck(t, rand, allConf)
+
+}
+
+func testAccCheckAlicloudResourceManagerPolicyVersionsSourceConfig(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+	config := fmt.Sprintf(`
+resource "alicloud_resource_manager_policy" "default"{
+	policy_name = "tf-%d"
+	policy_document = <<EOF
+		{
+			"Statement": [{
+				"Action": ["oss:*"],
+				"Effect": "Allow",
+				"Resource": ["acs:oss:*:*:*"]
+			}],
+			"Version": "1"
+		}
+	EOF
+}
+
+data "alicloud_resource_manager_policy_versions" "default"{
+%s
+}
+
+`, rand, strings.Join(pairs, "\n   "))
+	return config
+}
