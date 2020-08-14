1ecb3c6e76423ab14ee8a4aaf789b360704342cd
Heleninsa <979579683@qq.com>
2020-08-14 21:41:41 +0800

    recover. find unknown import error

--- alicloud/resource_alicloud_edas_k8s_cluster.go
+++ alicloud/resource_alicloud_edas_k8s_cluster.go
@@ -134,7 +134,6 @@ func resourceAlicloudEdasK8sClusterRead(d *schema.ResourceData, meta interface{}
 		return WrapError(err)
 	}
 
-	d.Set("id", d.Id())
 	d.Set("cluster_name", object.ClusterName)
 	d.Set("cluster_type", object.ClusterType)
 	d.Set("network_mode", object.NetworkMode)
