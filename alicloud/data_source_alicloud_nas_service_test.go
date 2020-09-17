ca063b4c911f04b04d12077c2dc4645616fedccd
He Guimin <guimin.hgm@alibaba-inc.com>
2020-09-17 15:54:56 +0800

    add missing pull request

---
+++ alicloud/data_source_alicloud_nas_service_test.go
@@ -0,0 +1,32 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccAlicloudNasServiceDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudNasServiceDataSource,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_nas_service.current"),
+					resource.TestCheckResourceAttrSet("data.alicloud_nas_service.current", "id"),
+					resource.TestCheckResourceAttr("data.alicloud_nas_service.current", "status", "Opened"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAlicloudNasServiceDataSource = `
+data "alicloud_nas_service" "current" {
+	enable = "On"
+}
+`
