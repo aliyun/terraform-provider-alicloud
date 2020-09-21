3b6770e92bd017da91e1376b5ed5d1a21abb8b80
He Guimin <guimin.hgm@alibaba-inc.com>
2020-09-21 15:13:41 +0800

    add a new datasource to enable cdn service automactically

---
+++ alicloud/data_source_alicloud_cdn_service_test.go
@@ -0,0 +1,37 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccAlicloudCdnServiceDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudCdnServiceDataSource,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_cdn_service.current"),
+					resource.TestCheckResourceAttrSet("data.alicloud_cdn_service.current", "id"),
+					resource.TestCheckResourceAttr("data.alicloud_cdn_service.current", "status", "Opened"),
+					resource.TestCheckResourceAttrSet("data.alicloud_cdn_service.current", "internet_charge_type"),
+					resource.TestCheckResourceAttrSet("data.alicloud_cdn_service.current", "opening_time"),
+					resource.TestCheckResourceAttrSet("data.alicloud_cdn_service.current", "changing_charge_type"),
+					resource.TestCheckResourceAttrSet("data.alicloud_cdn_service.current", "changing_affect_time"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAlicloudCdnServiceDataSource = `
+data "alicloud_cdn_service" "current" {
+	enable = "On"
+	internet_charge_type = "PayByBandwidth"
+}
+`
