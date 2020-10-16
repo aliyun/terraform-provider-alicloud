ab6f89e8d4703f52220b58c677f51257a3260b36
lixue <1120722567@qq.com>
2020-10-16 16:25:57 +0800

    fix testcase: replace alicloud_zones with alicloud_kvstore_zones.

--- alicloud/resource_alicloud_kvstore_account_test.go
+++ alicloud/resource_alicloud_kvstore_account_test.go
@@ -201,14 +201,14 @@ func TestAccAlicloudKVStoreAccountUpdateV5(t *testing.T) {
 
 func resourceKVstoreAccountConfigDependenceV4(name string) string {
 	return fmt.Sprintf(`
-	data "alicloud_zones" "default" {
-		available_resource_creation = "KVStore"
+	data "alicloud_kvstore_zones" "default"{
+		instance_charge_type = "PostPaid"
 	}
 	variable "name" {
 		default = "%v"
 	}
 	resource "alicloud_kvstore_instance" "instance" {
-		availability_zone = "${lookup(data.alicloud_zones.default.zones[(length(data.alicloud_zones.default.zones)-1)%%length(data.alicloud_zones.default.zones)], "id")}"
+		availability_zone = data.alicloud_kvstore_zones.default.zones[length(data.alicloud_kvstore_zones.default.ids) - 1].id
 		instance_class = "redis.master.small.default"
 		instance_name  = "${var.name}"
 		instance_charge_type = "PostPaid"
@@ -219,14 +219,14 @@ func resourceKVstoreAccountConfigDependenceV4(name string) string {
 
 func resourceKVstoreAccountConfigDependenceV5(name string) string {
 	return fmt.Sprintf(`
-	data "alicloud_zones" "default" {
-		available_resource_creation = "KVStore"
+	data "alicloud_kvstore_zones" "default"{
+		instance_charge_type = "PostPaid"
 	}
 	variable "name" {
 		default = "%v"
 	}
 	resource "alicloud_kvstore_instance" "instance" {
-		availability_zone = "${lookup(data.alicloud_zones.default.zones[(length(data.alicloud_zones.default.zones)-1)%%length(data.alicloud_zones.default.zones)], "id")}"
+		availability_zone = data.alicloud_kvstore_zones.default.zones[length(data.alicloud_kvstore_zones.default.ids) - 1].id
 		instance_class = "redis.master.small.default"
 		instance_name  = "${var.name}"
 		instance_charge_type = "PostPaid"
