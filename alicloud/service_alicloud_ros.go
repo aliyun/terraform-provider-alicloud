a0813bf44dab473d70cc546fe8366d0fe2e28c7c
lixue <1120722567@qq.com>
2020-12-08 10:52:19 +0800

    Add new resource and datasource for ros stack group

--- alicloud/service_alicloud_ros.go
+++ alicloud/service_alicloud_ros.go
@@ -275,3 +275,54 @@ func (s *RosService) SetResourceTags(d *schema.ResourceData, resourceType string
 	}
 	return nil
 }
+
+func (s *RosService) DescribeRosStackGroup(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewRosClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "GetStackGroup"
+	request := map[string]interface{}{
+		"RegionId":       s.client.RegionId,
+		"StackGroupName": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-09-10"), StringPointer("AK"), nil, request, &runtime)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"StackGroupNotFound"}) {
+			err = WrapErrorf(Error(GetNotFoundMessage("RosStackGroup", id)), NotFoundMsg, ProviderERROR)
+			return object, err
+		}
+		err = WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+		return object, err
+	}
+	addDebug(action, response, request)
+	v, err := jsonpath.Get("$.StackGroup", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.StackGroup", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
+
+func (s *RosService) RosStackGroupStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeRosStackGroup(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if object["Status"].(string) == failState {
+				return object, object["Status"].(string), WrapError(Error(FailedToReachTargetStatus, object["Status"].(string)))
+			}
+		}
+		return object, object["Status"].(string), nil
+	}
+}
