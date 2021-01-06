e7b8285c1160496ac095117e71734acb58bb61ad
lixue <1120722567@qq.com>
2021-01-06 18:39:41 +0800

    change actiontrail to common

--- alicloud/resource_alicloud_actiontrail_trail_test.go
+++ alicloud/resource_alicloud_actiontrail_trail_test.go
@@ -7,7 +7,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/actiontrail"
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -29,62 +31,64 @@ func testSweepActiontrailTrail(region string) error {
 	}
 	rawClient, err := sharedClientForRegion(region)
 	if err != nil {
-		return fmt.Errorf("error getting Alicloud client: %s", err)
+		return WrapErrorf(err, "Error getting Alicloud client.")
 	}
 	client := rawClient.(*connectivity.AliyunClient)
 
 	prefixes := []string{
+		"tf-testAcc",
 		"tf-testacc",
-		"tf_testacc",
 	}
-
-	request := actiontrail.CreateDescribeTrailsRequest()
-	raw, err := client.WithActiontrailClient(func(actiontrailClient *actiontrail.Client) (interface{}, error) {
-		return actiontrailClient.DescribeTrails(request)
-	})
+	request := make(map[string]interface{})
+	var response map[string]interface{}
+	action := "DescribeTrails"
+	conn, err := client.NewActiontrailClient()
 	if err != nil {
-		return fmt.Errorf("Error Describe Trails: %s", err)
+		return WrapError(err)
 	}
-	response := raw.(*actiontrail.DescribeTrailsResponse)
-
-	swept := false
-
-	for _, v := range response.TrailList {
-		name := v.Name
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-12-04"), StringPointer("AK"), nil, request, &runtime)
+	if err != nil {
+		return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_actiontrail_trails", action, AlibabaCloudSdkGoERROR)
+	}
+	resp, err := jsonpath.Get("$.TrailList", response)
+	if err != nil {
+		return WrapErrorf(err, FailedGetAttributeMsg, action, "$.TrailList", response)
+	}
+	sweeped := false
+	for _, v := range resp.([]interface{}) {
+		item := v.(map[string]interface{})
 		skip := true
 		for _, prefix := range prefixes {
-			if strings.HasPrefix(strings.ToLower(name), strings.ToLower(prefix)) {
+			if strings.HasPrefix(strings.ToLower(item["Name"].(string)), strings.ToLower(prefix)) {
 				skip = false
-				break
 			}
 		}
 		if skip {
-			log.Printf("[INFO] Skipping Trail: %s", name)
+			log.Printf("[INFO] Skipping ActionTrail Trails: %s", item["Name"].(string))
 			continue
 		}
-		swept = true
-
-		log.Printf("[INFO] Deleting Trail: %s", name)
-
-		request := actiontrail.CreateDeleteTrailRequest()
-		request.Name = name
-
-		_, err := client.WithActiontrailClient(func(actiontrailClient *actiontrail.Client) (interface{}, error) {
-			return actiontrailClient.DeleteTrail(request)
-		})
-
-		if err != nil {
-			log.Printf("[ERROR] Failed to delete trail (%s): %s", name, err)
+		sweeped = true
+		action = "DeleteTrail"
+		request := map[string]interface{}{
+			"Name": item["Name"],
 		}
-	}
-	if swept {
-		time.Sleep(5 * time.Second)
+		_, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-12-04"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			log.Printf("[ERROR] Failed to delete ActionTrail Trail (%s): %s", item["Name"].(string), err)
+		}
+		if sweeped {
+			// Waiting 5 seconds to ensure these ActionTrail Trails have been deleted.
+			time.Sleep(5 * time.Second)
+		}
+		log.Printf("[INFO] Delete ActionTrail Trail success: %s ", item["Name"].(string))
 	}
 	return nil
 }
 
 func TestAccAlicloudActiontrailTrail_basic(t *testing.T) {
-	var v actiontrail.TrailListItem
+	var v map[string]interface{}
 	resourceId := "alicloud_actiontrail_trail.default"
 	ra := resourceAttrInit(resourceId, ActiontrailTrailMap)
 	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
