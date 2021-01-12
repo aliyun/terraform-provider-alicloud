94179b52aba75d96bb9acbeb96e0076c5bd9f0d2
张军栋 <wb-zjd739376@alibaba-inc.com>
2021-01-12 10:04:10 +0800

    A new resource ga_bandwidth_package_attachment

--- alicloud/service_alicloud_ga.go
+++ alicloud/service_alicloud_ga.go
@@ -199,9 +199,63 @@ func (s *GaService) DescribeGaEndpointGroup(id string) (object map[string]interf
 	return object, nil
 }
 
+func (s *GaService) DescribeGaBandwidthPackageAttachment(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewGaplusClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeBandwidthPackage"
+	request := map[string]interface{}{
+		"RegionId":           s.client.RegionId,
+		"BandwidthPackageId": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"NotExist.BandwidthPackage"}) {
+			err = WrapErrorf(Error(GetNotFoundMessage("GaBandwidthPackageAttachment", id)), NotFoundMsg, ProviderERROR)
+			return object, err
+		}
+		err = WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+		return object, err
+	}
+	addDebug(action, response, request)
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	if len(object["Accelerators"].([]interface{})) == 0 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("GaBandwidthPackageAttachment", id)), NotFoundMsg, ProviderERROR)
+	}
+	return object, nil
+}
+
 func (s *GaService) GaEndpointGroupStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
 		object, err := s.DescribeGaEndpointGroup(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if object["State"].(string) == failState {
+				return object, object["State"].(string), WrapError(Error(FailedToReachTargetStatus, object["State"].(string)))
+			}
+		}
+		return object, object["State"].(string), nil
+	}
+}
+
+func (s *GaService) GaBandwidthPackageAttachmentStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeGaBandwidthPackageAttachment(id)
 		if err != nil {
 			if NotFoundError(err) {
 				// Set this to nil as if we didn't find anything.
---
+++ website/docs/r/ga_bandwidth_package_attachment.html.markdown
@@ -0,0 +1,71 @@
+---
+subcategory: "Global Accelerator (GA)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_ga_bandwidth_package_attachment"
+sidebar_current: "docs-alicloud-resource-ga-bandwidth-package-attachment"
+description: |-
+  Provides a Alicloud Global Accelerator (GA) Bandwidth Package Attachment resource.
+---
+
+# alicloud\_ga\_bandwidth\_package\_attachment
+
+Provides a Global Accelerator (GA) Bandwidth Package Attachment resource.
+
+For information about Global Accelerator (GA) Bandwidth Package Attachment and how to use it, see [What is Bandwidth Package Attachment](https://www.alibabacloud.com/help/en/doc-detail/153241.htm).
+
+-> **NOTE:** Available in v1.113.0+.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+resource "alicloud_ga_accelerator" "example" {
+  duration        = 1
+  auto_use_coupon = true
+  spec            = "1"
+}
+resource "alicloud_ga_bandwidth_package" "example" {
+  bandwidth      = 20
+  type           = "Basic"
+  bandwidth_type = "Basic"
+  duration       = 1
+  auto_pay       = true
+  ratio          = 30
+}
+resource "alicloud_ga_bandwidth_package_attachment" "example" {
+  accelerator_id       = "alicloud_ga_accelerator.example.id"
+  bandwidth_package_id = "alicloud_ga_bandwidth_package.example.id"
+}
+
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `accelerator_id` - (Required) The ID of the Global Accelerator instance from which you want to disassociate the bandwidth plan.
+* `bandwidth_package_id` - (Required, ForceNew) The ID of the bandwidth plan to disassociate.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The resource ID in terraform of Bandwidth Package Attachment. Value as `bandwidth_package_id`.
+* `accelerators` - Accelerators bound with current Bandwidth Package.
+* `status` - State of Bandwidth Package.
+
+### Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+
+* `create` - (Defaults to 1 mins) Used when create the Bandwidth Package Attachment.
+* `delete` - (Defaults to 1 mins) Used when delete the Bandwidth Package Attachment.
+
+## Import
+
+Ga Bandwidth Package Attachment can be imported using the id, e.g.
+
+```
+$ terraform import alicloud_ga_bandwidth_package_attachment.example <bandwidth_package_id>
+```
