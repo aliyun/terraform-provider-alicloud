f0709318f125f4d894e0e89ccb52d1d23df2c77e
张军栋 <wb-zjd739376@alibaba-inc.com>
2021-01-12 16:21:00 +0800

    A new datasource and resource ga_ip_set

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -367,6 +367,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_ga_bandwidth_packages":                      dataSourceAlicloudGaBandwidthPackages(),
 			"alicloud_ga_endpoint_groups":                         dataSourceAlicloudGaEndpointGroups(),
 			"alicloud_brain_industrial_pid_organizations":         dataSourceAlicloudBrainIndustrialPidOrganizations(),
+			"alicloud_ga_ip_sets":                                 dataSourceAlicloudGaIpSets(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"alicloud_instance":                           resourceAliyunInstance(),
@@ -657,6 +658,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_ga_endpoint_group":                         resourceAlicloudGaEndpointGroup(),
 			"alicloud_brain_industrial_pid_organization":         resourceAlicloudBrainIndustrialPidOrganization(),
 			"alicloud_ga_bandwidth_package_attachment":           resourceAlicloudGaBandwidthPackageAttachment(),
+			"alicloud_ga_ip_set":                                 resourceAlicloudGaIpSet(),
 		},
 
 		ConfigureFunc: providerConfigure,
