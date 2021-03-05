58f9d53c67541334e5f8ba7094046022b01763fc
lixue <1120722567@qq.com>
2021-03-05 09:59:37 +0800

    resource/alicloud_ecs_key_pair: Deprecates the key_name and use key_pair_name instead; Upgrades the its dependence sdk

---
+++ alicloud/resource_alicloud_ecs_key_pair_attachment_test.go
@@ -0,0 +1,130 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func testAccCheckKeyPairAttachmentDestroy(s *terraform.State) error {
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "alicloud_ecs_key_pair_attachment" {
+			continue
+		}
+		// Try to find the Disk
+		client := testAccProvider.Meta().(*connectivity.AliyunClient)
+		ecsService := EcsService{client}
+
+		instanceIds := rs.Primary.Attributes["instance_ids"]
+
+		for _, inst := range instanceIds {
+			response, err := ecsService.DescribeInstance(string(inst))
+			if err != nil {
+				return err
+			}
+
+			if response.KeyPairName != "" {
+				return fmt.Errorf("Error Key Pair Attachment still exist")
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func TestAccAlicloudEcsKeyPairAttachmentBasic(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_ecs_key_pair_attachment.default"
+	ra := resourceAttrInit(resourceId, AlicloudEcsKeyPairAttachmentMap)
+	rc := resourceCheckInit(resourceId, &v, func() interface{} {
+		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	})
+	rac := resourceAttrCheckInit(rc, ra)
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAccAlicloudEcsKeyPairAttachment%d", rand)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckKeyPairAttachmentDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: AlicloudEcsKeyPairAttachmentBasicDependence(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(nil),
+				),
+			},
+		},
+	})
+}
+
+var AlicloudEcsKeyPairAttachmentMap = map[string]string{
+	"key_pair_name":  CHECKSET,
+	"instance_ids.#": "1",
+}
+
+func AlicloudEcsKeyPairAttachmentBasicDependence(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+			default = "%s"
+		}
+
+data "alicloud_zones" default {
+  available_resource_creation = "Instance"
+}
+
+data "alicloud_instance_types" "default" {
+	availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  	cpu_core_count    = 1
+	memory_size       = 2
+}
+
+data "alicloud_vpcs" "default" {
+	is_default = true
+}
+
+data "alicloud_vswitches" "default" {
+ vpc_id = data.alicloud_vpcs.default.ids.0
+ zone_id = data.alicloud_zones.default.zones.0.id
+}
+
+resource "alicloud_security_group" "default" {
+  name = "tf-test"
+  description = "New security group"
+  vpc_id = data.alicloud_vpcs.default.ids.0
+}
+
+
+data "alicloud_images" "default" {
+  owners = "system"
+}
+
+resource "alicloud_instance" "default" {
+  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  instance_name   = "${var.name}"
+  host_name       = "tf-testAcc"
+  image_id        = data.alicloud_images.default.images.0.id
+  instance_type   = data.alicloud_instance_types.default.instance_types.0.id
+  security_groups = [alicloud_security_group.default.id]
+  vswitch_id      = data.alicloud_vswitches.default.ids.0
+}
+
+resource "alicloud_ecs_key_pair" "default" {
+  key_pair_name = "${var.name}"
+}
+resource "alicloud_ecs_key_pair_attachment" "default" {
+  key_pair_name = "${alicloud_ecs_key_pair.default.id}"
+  instance_ids = [alicloud_instance.default.id]
+}
+`, name)
+}
---
+++ website/docs/d/ecs_key_pairs.html.markdown
@@ -0,0 +1,60 @@
+---
+subcategory: "ECS"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_ecs_key_pairs"
+sidebar_current: "docs-alicloud-datasource-ecs-key-pairs"
+description: |-
+  Provides a list of Ecs Key Pairs to the user.
+---
+
+# alicloud\_ecs\_key\_pairs
+
+This data source provides the Ecs Key Pairs of the current Alibaba Cloud user.
+
+-> **NOTE:** Available in v1.121.0+.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+data "alicloud_ecs_key_pairs" "example" {
+  ids        = ["key_pair_name"]
+  name_regex = "key_pair_name"
+}
+
+output "first_ecs_key_pair_id" {
+  value = data.alicloud_ecs_key_pairs.example.pairs.0.id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `finger_print` - (Optional, ForceNew) The finger print of the key pair.
+* `ids` - (Optional, ForceNew, Computed)  A list of Key Pair IDs.
+* `name_regex` - (Optional, ForceNew) A regex string to filter results by Key Pair name.
+* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
+* `resource_group_id` - (Optional, ForceNew) The resource group Id.
+
+## Argument Reference
+
+The following attributes are exported in addition to the arguments listed above:
+
+* `names` - A list of Key Pair names.
+* `pairs` - A list of Ecs Key Pairs. Each element contains the following attributes:
+	* `finger_print` - The finger print of the key pair.
+	* `id` - The ID of the Key Pair.
+	* `key_name` - The Key Pair Name.
+	* `resource_group_id` - The Resource Group Id.
+	* `tags` - The tags.
+		* `tag_key` - The tag key.
+		* `tag_value` - The tag value.
+  * `instances` - A list of ECS instances that has been bound this key pair.
+    * `availability_zone` - The ID of the availability zone where the ECS instance is located.
+    * `instance_id` - The ID of the ECS instance.
+    * `instance_name` - The name of the ECS instance.
+    * `vswitch_id` - The ID of the VSwitch attached to the ECS instance.
+    * `public_ip` - The public IP address or EIP of the ECS instance.
+    * `private_ip` - The private IP address of the ECS instance.
