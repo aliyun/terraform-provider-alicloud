0f3eaf2f40316e1d322be194a1b83d9b39a0d882
刘日 <wb-lr721942@alibaba-inc.com>
2021-03-29 09:53:40 +0800

    Resource/alicloud_snat_entry:Add retry code for create.

--- alicloud/resource_alicloud_snat_entry.go
+++ alicloud/resource_alicloud_snat_entry.go
@@ -94,7 +94,7 @@ func resourceAlicloudSnatEntryCreate(d *schema.ResourceData, meta interface{}) e
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-04-28"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
 		if err != nil {
-			if IsExpectedErrors(err, []string{"EIP_NOT_IN_GATEWAY", "OperationUnsupported.EipInBinding", "OperationUnsupported.EipNatBWPCheck"}) || NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"EIP_NOT_IN_GATEWAY", "OperationUnsupported.EipInBinding", "OperationUnsupported.EipNatBWPCheck", "InternalError"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
