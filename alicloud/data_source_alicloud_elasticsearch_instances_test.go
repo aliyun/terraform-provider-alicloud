5119ce9a12bf06458d9cc50942df9a1e37af8129
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-05-22 17:51:09 +0800

    testcase: Improves the elasitcsearch instance testcase

--- alicloud/data_source_alicloud_elasticsearch_instances_test.go
+++ alicloud/data_source_alicloud_elasticsearch_instances_test.go
@@ -111,24 +111,34 @@ var fakeElasticsearchMapFunc = func(rand int) map[string]string {
 
 func dataSourceElasticsearchConfigDependence(name string) string {
 	return fmt.Sprintf(`
-
-data "alicloud_zones" "default" {
-  available_resource_creation = "Elasticsearch"
-}
-
 variable "name" {
   default = "%s"
 }
 
+data "alicloud_elasticsearch_zones" "default" {}
+data "alicloud_vpcs" "default" {
+  is_default = true
+}
 data "alicloud_vswitches" "default" {
-  zone_id = data.alicloud_zones.default.ids[0]
-  name_regex = "default-tf--testAcc-00"
+  vpc_id = data.alicloud_vpcs.default.ids.0
+  zone_id = data.alicloud_elasticsearch_zones.default.ids.0
+}
+resource "alicloud_vswitch" "vswitch" {
+  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
+  vpc_id            = data.alicloud_vpcs.default.ids.0
+  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 8)
+  zone_id = data.alicloud_elasticsearch_zones.default.ids.0
+  vswitch_name              = var.name
+}
+
+locals {
+  vswitch_id = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids[0] : concat(alicloud_vswitch.vswitch.*.id, [""])[0]
 }
 
 resource "alicloud_elasticsearch_instance" "default" {
-  description          = "${var.name}"
+  description          = var.name
   password             = "Yourpassword1234"
-  vswitch_id           = "${data.alicloud_vswitches.default.ids.0}"
+  vswitch_id           = local.vswitch_id
   data_node_amount     = "2"
   data_node_spec       = "elasticsearch.sn2ne.large"
   data_node_disk_size  = "20"
