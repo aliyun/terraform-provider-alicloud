f9ab8cf1e6188164ad72c6150eab7218a8a364e1
lixue <1120722567@qq.com>
2021-05-24 16:39:35 +0800

    change secret to commom

--- alicloud/service_alicloud_kms.go
+++ alicloud/service_alicloud_kms.go
@@ -81,49 +81,82 @@ func (s *KmsService) Decrypt(ciphertextBlob string, encryptionContext map[string
 	return response, err
 }
 
-func (s *KmsService) DescribeKmsSecret(id string) (object kms.DescribeSecretResponse, err error) {
-	request := kms.CreateDescribeSecretRequest()
-	request.RegionId = s.client.RegionId
-
-	request.SecretName = id
-	request.FetchTags = "true"
-
-	raw, err := s.client.WithKmsClient(func(kmsClient *kms.Client) (interface{}, error) {
-		return kmsClient.DescribeSecret(request)
+func (s *KmsService) DescribeKmsSecret(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewKmsClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeSecret"
+	request := map[string]interface{}{
+		"RegionId":   s.client.RegionId,
+		"SecretName": id,
+		"FetchTags":  "true",
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
 	})
+	addDebug(action, response, request)
 	if err != nil {
 		if IsExpectedErrors(err, []string{"Forbidden.ResourceNotFound"}) {
-			err = WrapErrorf(Error(GetNotFoundMessage("KmsSecret", id)), NotFoundMsg, ProviderERROR)
-			return object, err
+			return object, WrapErrorf(Error(GetNotFoundMessage("KMS:Secret", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
 		}
-		err = WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
-		return object, err
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
 	}
-	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
-	response, _ := raw.(*kms.DescribeSecretResponse)
-	return *response, nil
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
 }
 
-func (s *KmsService) GetSecretValue(id string) (object kms.GetSecretValueResponse, err error) {
-	request := kms.CreateGetSecretValueRequest()
-	request.RegionId = s.client.RegionId
-
-	request.SecretName = id
-
-	raw, err := s.client.WithKmsClient(func(kmsClient *kms.Client) (interface{}, error) {
-		return kmsClient.GetSecretValue(request)
-	})
+func (s *KmsService) GetSecretValue(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewKmsClient()
 	if err != nil {
-		if IsExpectedErrors(err, []string{"Forbidden.ResourceNotFound"}) {
-			err = WrapErrorf(Error(GetNotFoundMessage("KmsSecret", id)), NotFoundMsg, ProviderERROR)
-			return object, err
-		}
-		err = WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
-		return object, err
+		return nil, WrapError(err)
 	}
-	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
-	response, _ := raw.(*kms.GetSecretValueResponse)
-	return *response, nil
+	action := "GetSecretValue"
+	request := map[string]interface{}{
+		"RegionId":   s.client.RegionId,
+		"SecretName": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
 }
 
 func (s *KmsService) DescribeKmsAlias(id string) (object kms.KeyMetadata, err error) {
--- website/docs/d/kms_secrets.html.markdown
+++ website/docs/d/kms_secrets.html.markdown
@@ -40,6 +40,8 @@ The following arguments are supported:
 * `name_regex` - (Optional) A regex string to filter the results by the KMS secret_name.
 * `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
 * `tags` - (Optional) A mapping of tags to assign to the resource.
+* `filters` - (Optional, Available in 1.124.0+) Credential filter. It is composed of Key-Values ​​key-value pairs, the length is 0~1. When using a tag key to filter resources, the number of resources queried cannot exceed 4000.
+* `enable_details` - (Optional, Available in 1.124.0+) Default to `false`. Set it to true can output more details.
 
 ## Attributes Reference
 
@@ -51,5 +53,13 @@ The following attributes are exported in addition to the arguments listed above:
   * `id` - ID of the Kms Secret. The value is same as KMS secret_name.
   * `secret_name` - Name of the KMS Secret.
   * `planned_delete_time` - Schedule deletion time.
-  * `tags` - A mapping of tags to assign to the resource.
+  * `arn` - (Available in 1.124.0+) A mapping of tags to assign to the resource.
+  * `description` - (Available in 1.124.0+)  The description of the secret.
+  * `encryption_key_id` - (Available in 1.124.0+)  The ID of the KMS CMK that is used to encrypt the secret value.
+  * `secret_data` - (Available in 1.124.0+)  The value of the secret that you want to create.
+  * `secret_data_type` - (Available in 1.124.0+)  The type of the secret data value.
+  * `secret_type` - (Available in 1.124.0+)  The type of the secret.
+  * `version_id` - (Available in 1.124.0+)  The version number of the initial version.
+  * `version_stages` - (Available in 1.124.0+)  The stage labels that mark the new secret version.
+  * `tags` - (Optional) A mapping of tags to assign to the resource.
 
