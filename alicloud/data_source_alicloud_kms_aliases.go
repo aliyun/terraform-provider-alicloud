7e7704a9406c078a3b4d19be61ab1048d7d4834a
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-06-19 15:09:47 +0800

    resource/alicloud_kms_alias: Upgrades its dependence SDK

--- alicloud/data_source_alicloud_kms_aliases.go
+++ alicloud/data_source_alicloud_kms_aliases.go
@@ -1,11 +1,14 @@
 package alicloud
 
 import (
+	"fmt"
 	"regexp"
+	"time"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/kms"
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
@@ -28,6 +31,7 @@ func dataSourceAlicloudKmsAliases() *schema.Resource {
 			"ids": {
 				Type:     schema.TypeList,
 				Optional: true,
+				ForceNew: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
 			},
@@ -62,18 +66,20 @@ func dataSourceAlicloudKmsAliases() *schema.Resource {
 func dataSourceAlicloudKmsAliasesRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 
-	request := kms.CreateListAliasesRequest()
-	request.PageSize = requests.NewInteger(PageSizeLarge)
-	request.PageNumber = requests.NewInteger(1)
-	var objects []kms.Alias
-	var nameRegex *regexp.Regexp
+	action := "ListAliases"
+	request := make(map[string]interface{})
+	request["PageSize"] = PageSizeLarge
+	request["PageNumber"] = 1
+	var objects []map[string]interface{}
+	var aliasNameRegex *regexp.Regexp
 	if v, ok := d.GetOk("name_regex"); ok {
 		r, err := regexp.Compile(v.(string))
 		if err != nil {
 			return WrapError(err)
 		}
-		nameRegex = r
+		aliasNameRegex = r
 	}
+
 	idsMap := make(map[string]string)
 	if v, ok := d.GetOk("ids"); ok {
 		for _, vv := range v.([]interface{}) {
@@ -83,61 +89,77 @@ func dataSourceAlicloudKmsAliasesRead(d *schema.ResourceData, meta interface{}) 
 			idsMap[vv.(string)] = vv.(string)
 		}
 	}
+	var response map[string]interface{}
+	conn, err := client.NewKmsClient()
+	if err != nil {
+		return WrapError(err)
+	}
 	for {
-		raw, err := client.WithKmsClient(func(kmsClient *kms.Client) (interface{}, error) {
-			return kmsClient.ListAliases(request)
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
 		})
+		addDebug(action, response, request)
 		if err != nil {
-			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_kms_aliases", request.GetActionName(), AlibabaCloudSdkGoERROR)
+			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_kms_aliases", action, AlibabaCloudSdkGoERROR)
 		}
-		addDebug(request.GetActionName(), raw)
-		response, _ := raw.(*kms.ListAliasesResponse)
-
-		for _, item := range response.Aliases.Alias {
-			if nameRegex != nil {
-				if !nameRegex.MatchString(item.AliasName) {
+		resp, err := jsonpath.Get("$.Aliases.Alias", response)
+		if err != nil {
+			return WrapErrorf(err, FailedGetAttributeMsg, action, "$.Aliases.Alias", response)
+		}
+		result, _ := resp.([]interface{})
+		for _, v := range result {
+			item := v.(map[string]interface{})
+			if aliasNameRegex != nil {
+				if !aliasNameRegex.MatchString(fmt.Sprint(item["AliasName"])) {
 					continue
 				}
 			}
 			if len(idsMap) > 0 {
-				if _, ok := idsMap[item.AliasName]; !ok {
+				if _, ok := idsMap[fmt.Sprint(item["AliasName"])]; !ok {
 					continue
 				}
 			}
 			objects = append(objects, item)
 		}
-		if len(response.Aliases.Alias) < PageSizeLarge {
+		if len(result) < PageSizeLarge {
 			break
 		}
-
-		page, err := getNextpageNumber(request.PageNumber)
-		if err != nil {
-			return WrapError(err)
-		}
-		request.PageNumber = page
+		request["PageNumber"] = request["PageNumber"].(int) + 1
 	}
-	ids := make([]string, len(objects))
-	names := make([]string, len(objects))
-	s := make([]map[string]interface{}, len(objects))
-
-	for i, object := range objects {
+	ids := make([]string, 0)
+	names := make([]interface{}, 0)
+	s := make([]map[string]interface{}, 0)
+	for _, object := range objects {
 		mapping := map[string]interface{}{
-			"id":         object.AliasName,
-			"alias_name": object.AliasName,
-			"key_id":     object.KeyId,
+			"id":         fmt.Sprint(object["AliasName"]),
+			"alias_name": fmt.Sprint(object["AliasName"]),
+			"key_id":     object["KeyId"],
 		}
-		ids[i] = object.AliasName
-		names[i] = object.AliasName
-		s[i] = mapping
+		ids = append(ids, fmt.Sprint(mapping["id"]))
+		names = append(names, object["AliasName"])
+		s = append(s, mapping)
 	}
 
 	d.SetId(dataResourceIdHash(ids))
 	if err := d.Set("ids", ids); err != nil {
 		return WrapError(err)
 	}
+
 	if err := d.Set("names", names); err != nil {
 		return WrapError(err)
 	}
+
 	if err := d.Set("aliases", s); err != nil {
 		return WrapError(err)
 	}
