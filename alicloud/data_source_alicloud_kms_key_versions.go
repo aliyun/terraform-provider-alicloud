4681c2b9ece719dd0000962ec411df91aebe2cee
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-06-21 08:59:43 +0800

    datasource/alicloud_kms_plaintext: Upgrades its dependence sdk

--- alicloud/data_source_alicloud_kms_key_versions.go
+++ alicloud/data_source_alicloud_kms_key_versions.go
@@ -52,6 +52,11 @@ func dataSourceAlicloudKmsKeyVersions() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"creation_date": {
+							Type:     schema.TypeString,
+							Computed: true,
+							Removed:  "Attribute 'creation_date' has been removed and using 'create_time' instead.",
+						},
 					},
 				},
 			},
@@ -129,6 +134,7 @@ func dataSourceAlicloudKmsKeyVersionsRead(d *schema.ResourceData, meta interface
 			"key_id":         object["KeyId"],
 			"id":             fmt.Sprint(object["KeyVersionId"]),
 			"key_version_id": fmt.Sprint(object["KeyVersionId"]),
+			"creation_date":  object["CreateTime"],
 		}
 		ids = append(ids, fmt.Sprint(mapping["id"]))
 		s = append(s, mapping)
