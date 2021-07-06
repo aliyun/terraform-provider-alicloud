157db5c0df41331b5300b88f11ab25e6840b0b97
wb-dyj960556 <wb-dyj960556@alibaba-inc.com>
2021-07-06 17:59:23 +0800

    NewRsource
        `alicloud_cen_transit_router`
        `alicloud_cen_transit_router_route_table`
        `alicloud_cen_transit_router_route_table_association`
        `alicloud_cen_transit_router_route_table_propagation`
        `alicloud_cen_transit_router_route_entry`
        `alicloud_cen_transit_router_vbr_attachment`
    NewDataSource
        `alicloud_cen_transit_routers`
        `alicloud_cen_transit_router_route_tables`
        `alicloud_cen_transit_router_route_table_associations`
        `alicloud_cen_transit_router_route_table_propagations`
        `alicloud_cen_transit_router_route_entries`
        `alicloud_cen_transit_router_vbr_attachment`

---
+++ alicloud/resource_alicloud_cen_transit_router_test.go
@@ -0,0 +1,107 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccAlicloudCenTransitRouter_basic(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cen_transit_router.default"
+	ra := resourceAttrInit(resourceId, AlicloudCenTransitRouterMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CbnService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCenTransitRouter")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%scentransitrouter%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudCenTransitRouterBasicDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"cen_id":                     "${alicloud_cen_instance.default.id}",
+					"transit_router_name":        "${var.name}",
+					"transit_router_description": "tf",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"cen_id":                     CHECKSET,
+						"transit_router_name":        name,
+						"transit_router_description": "tf",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"dry_run"},
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_description": "deds",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_description": "deds",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_name": name + "1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_name": name + "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_description": "desd",
+					"transit_router_name":        "${var.name}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_description": "desd",
+						"transit_router_name":        name,
+					}),
+				),
+			},
+		},
+	})
+}
+
+var AlicloudCenTransitRouterMap = map[string]string{
+	"cen_id":                     CHECKSET,
+	"dry_run":                    NOSET,
+	"status":                     CHECKSET,
+	"transit_router_description": CHECKSET,
+	"transit_router_name":        CHECKSET,
+}
+
+func AlicloudCenTransitRouterBasicDependence(name string) string {
+	return fmt.Sprintf(`
+	variable "name" {
+		default = "%s"
+	}
+
+	resource "alicloud_cen_instance" "default" {
+		cen_instance_name = var.name
+	}
+	`, name)
+}
---
+++ website/docs/d/cen_transit_router_vbr_attachments.html.markdown
@@ -0,0 +1,52 @@
+---
+subcategory: "Cloud Enterprise Network (CEN)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_cen_transit_router_vbr_attachments"
+sidebar_current: "docs-alicloud-datasource-cen-transit-router-vbr-attachments"
+description: |-
+Provides a list of CEN Transit Router VBR Attachments owned by an Alibaba Cloud account.
+---
+
+# alicloud\_cen\_transit\_router\_vbr\_attachments
+
+This data source provides CEN Transit Router VBR Attachments available to the user.
+
+-> **NOTE:** Available in 1.125.0+
+
+## Example Usage
+
+```
+data "alicloud_cen_transit_router_vbr_attachments" "default" {
+  cen_id    = "cen-id1"
+  region_id = "cn-****"
+}
+
+output "first_transit_router_vbr_attachments_vbr_id" {
+  value = "${data.alicloud_cen_transit_router_vbr_attachments.default.transit_router_attachments.0.vbr_id}"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `cen_id` - (Required) ID of the CEN instance.
+* `region_id` - (Optional) Region ID of the VBR.
+* `transit_router_id` - (Optional) ID of the transit router.
+* `resource_type` - (Optional) Type of the resource.
+* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
+
+## Attributes Reference
+
+The following attributes are exported in addition to the arguments listed above:
+
+* `transit_router_attachments` - A list of CEN Transit Router VBR Attachments. Each element contains the following attributes:
+    * `transit_router_attachment_id` - ID of the transit router attachment.
+    * `transit_router_attachment_name` - Name of the transit router attachment.
+    * `resource_type` - Type of the resource.
+    * `status` - The status of the transit router attachment.
+    * `creation_time` - The time when it's created.
+    * `vbr_id` - ID of the VBR.
+    * `vbr_owner_id` - The Owner ID of the VBR.
+    * `vbr_region_id` - ID of the region where the conflicted VBR is located.
+    * `auto_publish_route_enabled` - ID of the region where the conflicted VBR is located.
