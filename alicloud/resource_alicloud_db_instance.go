d3737f8076cd6fe0081a8489a37db0ab08071b7a
wb-zgl885911 <wb-zgl885911@alibaba-inc.com>
2021-07-22 19:18:10 +0800

    ModifySQLCollectorRetention buy fixed

--- alicloud/resource_alicloud_db_instance.go
+++ alicloud/resource_alicloud_db_instance.go
@@ -568,7 +568,7 @@ func resourceAlicloudDBInstanceUpdate(d *schema.ResourceData, meta interface{}) 
 		d.SetPartial("sql_collector_status")
 	}
 
-	if d.Get("sql_collector_status").(string) == "Enabled" && d.HasChange("sql_collector_config_value") {
+	if d.Get("sql_collector_status").(string) == "Enabled" && d.HasChange("sql_collector_config_value") && d.Get("engine").(string) == string(MySQL) {
 		action := "ModifySQLCollectorRetention"
 		request := map[string]interface{}{
 			"RegionId":     client.RegionId,
