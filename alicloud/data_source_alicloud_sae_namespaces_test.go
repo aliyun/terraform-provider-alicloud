f438d8fab2e6b5d44fda91eaf05f3e8bba5ae748
wanghx <wanghx0991@outlook.com>
2021-07-30 17:58:47 +0800

    New Resource:alicloud_sae_namespace

---
+++ alicloud/data_source_alicloud_sae_namespaces_test.go
@@ -0,0 +1,91 @@
+package alicloud
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+)
+
+func TestAccAlicloudSaeNamespaceDataSource(t *testing.T) {
+	rand := acctest.RandIntRange(1, 100)
+	idsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_sae_namespace.default.namespace_id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_sae_namespace.default.namespace_id}_fake"]`,
+		}),
+	}
+	nameRegexConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_sae_namespace.default.namespace_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_sae_namespace.default.namespace_name}_fake"`,
+		}),
+	}
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"ids":        `["${alicloud_sae_namespace.default.namespace_id}"]`,
+			"name_regex": `"${alicloud_sae_namespace.default.namespace_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudSaeNamespaceDataSourceName(rand, map[string]string{
+			"ids":        `["${alicloud_sae_namespace.default.namespace_id}_fake"]`,
+			"name_regex": `"${alicloud_sae_namespace.default.namespace_name}_fake"`,
+		}),
+	}
+	var existAlicloudSaeNamespaceDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                              "1",
+			"names.#":                            "1",
+			"namespaces.#":                       "1",
+			"namespaces.0.namespace_description": fmt.Sprintf("tf-testAccsaenamespacedesc-%d", rand),
+			"namespaces.0.namespace_name":        fmt.Sprintf("tf-testAccsaenamespace-%d", rand),
+		}
+	}
+	var fakeAlicloudSaeNamespaceDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":   "0",
+			"names.#": "0",
+		}
+	}
+	var alicloudSaeNamespaceCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_sae_namespaces.default",
+		existMapFunc: existAlicloudSaeNamespaceDataSourceNameMapFunc,
+		fakeMapFunc:  fakeAlicloudSaeNamespaceDataSourceNameMapFunc,
+	}
+	alicloudSaeNamespaceCheckInfo.dataSourceTestCheck(t, rand, idsConf, nameRegexConf, allConf)
+}
+func testAccCheckAlicloudSaeNamespaceDataSourceName(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+	region := os.Getenv("ALICLOUD_REGION")
+	config := fmt.Sprintf(`
+
+variable "name" {	
+	default = "tf-testAccsaenamespace-%d"
+}
+variable "desc" {	
+	default = "tf-testAccsaenamespacedesc-%d"
+}
+variable "namespace_id" {	
+	default = "%s:test%d"
+}
+
+resource "alicloud_sae_namespace" "default" {
+	namespace_id = var.namespace_id
+	namespace_name = var.name
+	namespace_description = var.desc
+}
+
+data "alicloud_sae_namespaces" "default" {	
+	%s
+}
+`, rand, rand,region, rand, strings.Join(pairs, " \n "))
+	return config
+}
---
+++ alicloud/resource_alicloud_sae_namespace_test.go
@@ -0,0 +1,104 @@
+package alicloud
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+var region = os.Getenv("ALICLOUD_REGION")
+
+func TestAccAlicloudSAENamespace_basic0(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_sae_namespace.default"
+	ra := resourceAttrInit(resourceId, AlicloudSAENamespaceMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &SaeService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeSaeNamespace")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1, 100)
+	name := fmt.Sprintf("tf-testacc%ssaenamespace%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudSAENamespaceBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name":        name,
+					"namespace_id":          fmt.Sprintf("%s:tftest%d", region, rand),
+					"namespace_description": "tftestaccdescription",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name,
+						"namespace_id":          fmt.Sprintf("%s:tftest%d", region, rand),
+						"namespace_description": "tftestaccdescription",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name": name + "update",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "update",
+						"namespace_description": "tftestaccdescription",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_description": "tftestaccdescriptionupdate",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "update",
+						"namespace_description": "tftestaccdescriptionupdate",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name":        name + "updateall",
+					"namespace_description": "tftestaccdescriptionupdateall",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "updateall",
+						"namespace_description": "tftestaccdescriptionupdateall",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+var AlicloudSAENamespaceMap0 = map[string]string{
+	"namespace_name":        "namespace_name",
+	"namespace_id":          fmt.Sprintf("%s:tftest", region),
+	"namespace_description": "tftestaccdescription",
+}
+
+func AlicloudSAENamespaceBasicDependence0(name string) string {
+	return fmt.Sprintf(` 
+variable "name" {
+  default = "%s"
+}
+`, name)
+}
