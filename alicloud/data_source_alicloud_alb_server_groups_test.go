bb9bd80121c7d14ecb515a64631c74802007fc4a
ll221756 <ll221756@alibaba-inc.com>
2021-08-10 15:44:51 +0800

    New Resource `alb_server_group` New DataSource `alb_server_groups`

---
+++ alicloud/data_source_alicloud_alb_server_groups_test.go
@@ -0,0 +1,167 @@
+package alicloud
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+)
+
+func TestAccAlicloudAlbServerGroupsDataSource(t *testing.T) {
+	rand := acctest.RandInt()
+
+	serverGroupIdsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"server_group_ids": `["${alicloud_alb_server_group.default.id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"server_group_ids": `["${alicloud_alb_server_group.default.id}_fake"]`,
+		}),
+	}
+	resourceGroupIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":               `["${alicloud_alb_server_group.default.id}"]`,
+			"resource_group_id": `"${alicloud_alb_server_group.default.resource_group_id}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":               `["${alicloud_alb_server_group.default.id}_fake"]`,
+			"resource_group_id": `"${alicloud_alb_server_group.default.resource_group_id}_fake"`,
+		}),
+	}
+	tagsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":  `["${alicloud_alb_server_group.default.id}"]`,
+			"tags": `{Created = "TF"}`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":  `["${alicloud_alb_server_group.default.id}_fake"]`,
+			"tags": `{Created = "TF_fake"}`,
+		}),
+	}
+	idsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_alb_server_group.default.id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_alb_server_group.default.id}_fake"]`,
+		}),
+	}
+	nameRegexConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_alb_server_group.default.server_group_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_alb_server_group.default.server_group_name}_fake"`,
+		}),
+	}
+	vpcIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":    `["${alicloud_alb_server_group.default.id}"]`,
+			"vpc_id": `"${alicloud_alb_server_group.default.vpc_id}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":    `["${alicloud_alb_server_group.default.id}_fake"]`,
+			"vpc_id": `"${alicloud_alb_server_group.default.vpc_id}"`,
+		}),
+	}
+	serverGroupNameConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"server_group_name": `"${alicloud_alb_server_group.default.server_group_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"server_group_name": `"${alicloud_alb_server_group.default.server_group_name}_fake"`,
+		}),
+	}
+	statusConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":    `["${alicloud_alb_server_group.default.id}"]`,
+			"status": `"Available"`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":    `["${alicloud_alb_server_group.default.id}"]`,
+			"status": `"Configuring"`,
+		}),
+	}
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":               `["${alicloud_alb_server_group.default.id}"]`,
+			"name_regex":        `"${alicloud_alb_server_group.default.server_group_name}"`,
+			"vpc_id":            `"${alicloud_alb_server_group.default.vpc_id}"`,
+			"server_group_name": `"${alicloud_alb_server_group.default.server_group_name}"`,
+			"status":            `"Available"`,
+			"tags":              `{Created = "TF"}`,
+			"resource_group_id": `"${alicloud_alb_server_group.default.resource_group_id}"`,
+			"server_group_ids":  `["${alicloud_alb_server_group.default.id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudAlbServerGroupsDataSourceName(rand, map[string]string{
+			"ids":               `["${alicloud_alb_server_group.default.id}_fake"]`,
+			"name_regex":        `"${alicloud_alb_server_group.default.server_group_name}_fake"`,
+			"vpc_id":            `"${alicloud_alb_server_group.default.vpc_id}"`,
+			"server_group_name": `"${alicloud_alb_server_group.default.server_group_name}_fake"`,
+			"status":            `"Configuring"`,
+			"tags":              `{Created = "TF_fake"}`,
+			"resource_group_id": `"${alicloud_alb_server_group.default.resource_group_id}_fake"`,
+			"server_group_ids":  `["${alicloud_alb_server_group.default.id}_fake"]`,
+		}),
+	}
+	var existDataAlicloudAlbServerGroupsDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":                      "1",
+			"groups.#":                   "1",
+			"groups.0.server_group_name": fmt.Sprintf("tf-testAccAlbServerGroup-%d", rand),
+		}
+	}
+	var fakeDataAlicloudAlbServerGroupsDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"ids.#":    "0",
+			"groups.#": "0",
+		}
+	}
+	var alicloudAlbServerGroupCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_alb_server_groups.default",
+		existMapFunc: existDataAlicloudAlbServerGroupsDataSourceNameMapFunc,
+		fakeMapFunc:  fakeDataAlicloudAlbServerGroupsDataSourceNameMapFunc,
+	}
+	alicloudAlbServerGroupCheckInfo.dataSourceTestCheck(t, rand, idsConf, nameRegexConf, vpcIdConf, statusConf, serverGroupNameConf, tagsConf, serverGroupIdsConf, resourceGroupIdConf, allConf)
+}
+func testAccCheckAlicloudAlbServerGroupsDataSourceName(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+
+	config := fmt.Sprintf(`
+
+variable "name" {	
+	default = "tf-testAccAlbServerGroup-%d"
+}
+
+data "alicloud_vpcs" "default" {
+  name_regex = "default-NODELETING"
+}
+
+data "alicloud_resource_manager_resource_groups" "default" {}
+
+resource "alicloud_alb_server_group" "default" {
+	protocol = "HTTP"
+	vpc_id = data.alicloud_vpcs.default.vpcs.0.id
+	server_group_name = var.name
+    resource_group_id = data.alicloud_resource_manager_resource_groups.default.groups.0.id
+	health_check_config {
+       health_check_enabled = "false"
+	}
+	sticky_session_config {
+       sticky_session_enabled = "false"
+	}
+	tags = {
+		Created = "TF"
+	}
+}
+
+data "alicloud_alb_server_groups" "default" {	
+	%s
+}
+`, rand, strings.Join(pairs, " \n "))
+	return config
+}
--- alicloud/service_alicloud_alb.go
+++ alicloud/service_alicloud_alb.go
@@ -363,3 +363,118 @@ func (s *AlbService) AlbSecurityPolicyStateRefreshFunc(id string, failStates []s
 		return object, fmt.Sprint(object["SecurityPolicyStatus"]), nil
 	}
 }
+
+func (s *AlbService) ListServerGroupServers(id string) (object []interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewAlbClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "ListServerGroupServers"
+	request := map[string]interface{}{
+		"ServerGroupId": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-06-16"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, _ := jsonpath.Get("$.Servers", response)
+	if v == nil {
+		return object, nil
+	}
+
+	if fmt.Sprint(v.([]interface{})[0].(map[string]interface{})["ServerGroupId"]) != id {
+		return object, WrapErrorf(Error(GetNotFoundMessage("ALB", id)), NotFoundWithResponse, response)
+	}
+
+	return v.([]interface{}), nil
+}
+
+func (s *AlbService) DescribeAlbServerGroup(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewAlbClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "ListServerGroups"
+	request := map[string]interface{}{
+		"MaxResults": 100,
+	}
+	idExist := false
+	for {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-06-16"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+		}
+		v, err := jsonpath.Get("$.ServerGroups", response)
+		if formatInt(response["TotalCount"]) != 0 && err != nil {
+			return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.ServerGroups", response)
+		}
+		if v != nil {
+			for _, v := range v.([]interface{}) {
+				if fmt.Sprint(v.(map[string]interface{})["ServerGroupId"]) == id {
+					idExist = true
+					return v.(map[string]interface{}), nil
+				}
+			}
+		}
+
+		if nextToken, ok := response["NextToken"].(string); ok && nextToken != "" {
+			request["NextToken"] = nextToken
+		} else {
+			break
+		}
+	}
+	if !idExist {
+		return object, WrapErrorf(Error(GetNotFoundMessage("ALB", id)), NotFoundWithResponse, response)
+	}
+	return
+}
+
+func (s *AlbService) AlbServerGroupStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeAlbServerGroup(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["ServerGroupStatus"]) == failState {
+				return object, fmt.Sprint(object["ServerGroupStatus"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["ServerGroupStatus"])))
+			}
+		}
+		return object, fmt.Sprint(object["ServerGroupStatus"]), nil
+	}
+}
