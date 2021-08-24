6f7ed1240dcb82c615fdcb4e0490ec7bdf59eff4
guozhiyuan <gzy01137900@alibaba-inc.com>
2021-08-24 18:14:06 +0800

    NewResource `alicloud_alb_listener` NewDataSource `alicloud_alb_listeners`

--- alicloud/service_alicloud_alb.go
+++ alicloud/service_alicloud_alb.go
@@ -604,3 +604,62 @@ func (s *AlbService) AlbAclStateRefreshFunc(id string, failStates []string) reso
 		return object, fmt.Sprint(object["AclStatus"]), nil
 	}
 }
+
+func (s *AlbService) AlbListenerStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeAlbListener(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+
+			if fmt.Sprint(object["ListenerStatus"]) == failState {
+				return object, fmt.Sprint(object["ListenerStatus"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["ListenerStatus"])))
+			}
+		}
+		return object, fmt.Sprint(object["ListenerStatus"]), nil
+	}
+}
+
+func (s *AlbService) DescribeAlbListener(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewAlbClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "GetListenerAttribute"
+	request := map[string]interface{}{
+		"ListenerId": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-06-16"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"ResourceNotFound.Listener"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("ALB:Listener", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
