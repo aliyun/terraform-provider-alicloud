95615d8445eb0a4287d6c198f9042fd553cdd4af
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2021-08-27 18:17:53 +0800

    add new resource: ens keypair

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -3429,3 +3429,28 @@ func (client *AliyunClient) NewEhpcClient() (*rpc.Client, error) {
 	}
 	return conn, nil
 }
+
+func (client *AliyunClient) NewEnsClient() (*rpc.Client, error) {
+	productCode := "ens"
+	endpoint := ""
+	if v, ok := client.config.Endpoints[productCode]; !ok || v.(string) == "" {
+		if err := client.loadEndpoint(productCode); err != nil {
+			endpoint = "ens.aliyuncs.com"
+			client.config.Endpoints[productCode] = endpoint
+			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the endpoint %s instead.", productCode, err, endpoint)
+		}
+	}
+	if v, ok := client.config.Endpoints[productCode]; ok && v.(string) != "" {
+		endpoint = v.(string)
+	}
+	if endpoint == "" {
+		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
+	}
+	sdkConfig := client.teaSdkConfig
+	sdkConfig.SetEndpoint(endpoint)
+	conn, err := rpc.NewClient(&sdkConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
+	}
+	return conn, nil
+}
