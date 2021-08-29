963eda51f9e921937361580deb517a1c18f5a954
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-08-30 00:00:15 +0800

    add New Resource alicloud_alb_rule and its datasource

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -498,6 +498,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_alb_listeners":                               dataSourceAlicloudAlbListeners(),
 			"alicloud_ens_key_pairs":                               dataSourceAlicloudEnsKeyPairs(),
 			"alicloud_sae_applications":                            dataSourceAlicloudSaeApplications(),
+			"alicloud_alb_rules":                                   dataSourceAlicloudAlbRules(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"alicloud_instance":                           resourceAliyunInstance(),
@@ -891,6 +892,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_alb_listener":                               resourceAlicloudAlbListener(),
 			"alicloud_ens_key_pair":                               resourceAlicloudEnsKeyPair(),
 			"alicloud_sae_application":                            resourceAlicloudSaeApplication(),
+			"alicloud_alb_rule":                                   resourceAlicloudAlbRule(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
