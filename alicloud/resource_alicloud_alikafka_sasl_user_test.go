6ed91c017ebe1e0211c53400774abfa20d523512
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-08-31 14:07:23 +0800

    resource/alicloud_alikafka_instance: Fixes the diff error caused by eip_max and security_group

--- alicloud/resource_alicloud_alikafka_sasl_user_test.go
+++ alicloud/resource_alicloud_alikafka_sasl_user_test.go
@@ -237,24 +237,33 @@ func TestAccAlicloudAlikafkaSaslUser_multi(t *testing.T) {
 
 func resourceAlikafkaSaslUserConfigDependence(name string) string {
 	return fmt.Sprintf(`
-		variable "name" {
- 			default = "%v"
-		}
+variable "name" {
+	default = "%v"
+}
 
-        data "alicloud_vswitches" "default" {
-		  name_regex = "^default"
-		}
+data "alicloud_vpcs" "default" {
+ name_regex = "^default-NODELETING"
+}
+data "alicloud_vswitches" "default" {
+  vpc_id = data.alicloud_vpcs.default.ids.0
+}
 
-		resource "alicloud_alikafka_instance" "default" {
-          name = "${var.name}"
-		  topic_quota = "50"
-		  disk_type = "1"
-		  disk_size = "500"
-		  deploy_type = "5"
-		  io_max = "20"
-          vswitch_id = "${data.alicloud_vswitches.default.ids.0}"
-		}
-		`, name)
+resource "alicloud_security_group" "default" {
+  name   = var.name
+  vpc_id = data.alicloud_vpcs.default.ids.0
+}
+
+resource "alicloud_alikafka_instance" "default" {
+  name = "${var.name}"
+  topic_quota = "50"
+  disk_type = "1"
+  disk_size = "500"
+  deploy_type = "5"
+  io_max = "20"
+  vswitch_id = "${data.alicloud_vswitches.default.ids.0}"
+  security_group = alicloud_security_group.default.id
+}
+`, name)
 }
 
 var alikafkaSaslUserBasicMap = map[string]string{
