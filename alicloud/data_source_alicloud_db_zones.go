c3dcbf5f8f61fc91426cc618b916c34d3a12aacb
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-09-05 11:30:15 +0800

    datasource/alicloud_db_zones: Upgrades its dependence OpenAPI to DescribeAvailableZones

--- alicloud/data_source_alicloud_db_zones.go
+++ alicloud/data_source_alicloud_db_zones.go
@@ -89,13 +89,16 @@ func dataSourceAlicloudDBZones() *schema.Resource {
 
 func dataSourceAlicloudDBZonesRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	action := "DescribeAvailableResource"
+	action := "DescribeAvailableZones"
+	engines := make([]string, 0)
+	if v, ok := d.GetOk("engine"); ok && v.(string) != "" {
+		engines = append(engines, v.(string))
+	} else {
+		engines = []string{"MySQL", "SQLServer", "PostgreSQL", "PPAS", "MariaDB"}
+	}
 	request := map[string]interface{}{
 		"RegionId": client.RegionId,
 		"SourceIp": client.SourceIp,
-	}
-	if v, ok := d.GetOk("engine"); ok && v.(string) != "" {
-		request["Engine"] = v.(string)
 	}
 	if v, ok := d.GetOk("engine_version"); ok && v.(string) != "" {
 		request["EngineVersion"] = v.(string)
@@ -125,34 +128,37 @@ func dataSourceAlicloudDBZonesRead(d *schema.ResourceData, meta interface{}) err
 	}
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2014-08-15"), StringPointer("AK"), nil, request, &runtime)
-		if err != nil {
-			if NeedRetry(err) {
-				wait()
-				return resource.RetryableError(err)
+	for _, engine := range engines {
+		request["Engine"] = engine
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2014-08-15"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
 			}
-			return resource.NonRetryableError(err)
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_db_zones", action, AlibabaCloudSdkGoERROR)
 		}
-		return nil
-	})
-	addDebug(action, response, request)
-	if err != nil {
-		return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_db_zones", action, AlibabaCloudSdkGoERROR)
-	}
-	resp, err := jsonpath.Get("$.AvailableZones", response)
-	if err != nil {
-		return WrapErrorf(err, FailedGetAttributeMsg, action, "$.Accounts.DBInstanceAccount", response)
-	}
-	result, _ := resp.([]interface{})
-	for _, v := range result {
-		zoneId := fmt.Sprint(v.(map[string]interface{})["ZoneId"])
+		resp, err := jsonpath.Get("$.AvailableZones", response)
+		if err != nil {
+			return WrapErrorf(err, FailedGetAttributeMsg, action, "$.AvailableZones", response)
+		}
+		result, _ := resp.([]interface{})
+		for _, v := range result {
+			zoneId := fmt.Sprint(v.(map[string]interface{})["ZoneId"])
 
-		if (multi && !strings.Contains(zoneId, MULTI_IZ_SYMBOL)) || (!multi && strings.Contains(zoneId, MULTI_IZ_SYMBOL)) {
-			continue
+			if (multi && !strings.Contains(zoneId, MULTI_IZ_SYMBOL)) || (!multi && strings.Contains(zoneId, MULTI_IZ_SYMBOL)) {
+				continue
+			}
+			ids = append(ids, zoneId)
 		}
-		ids = append(ids, zoneId)
 	}
 	if len(ids) > 0 {
 		sort.Strings(ids)
