86457ef01bcb48f6a806e8fcfc4473d85bd092c6
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-09-26 21:46:59 +0800

    Adds a new resource alicloud_imp_app_template and datasource alicloud_imp_app_templates

---
+++ alicloud/resource_alicloud_imp_app_template_test.go
@@ -0,0 +1,151 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccAlicloudIMPAppTemplate_basic0(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_imp_app_template.default"
+	ra := resourceAttrInit(resourceId, AlicloudIMPAppTemplateMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &ImpService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeImpAppTemplate")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%simpapptemplate%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudIMPAppTemplateBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"scene":             "business",
+					"integration_mode":  "paasSDK",
+					"component_list":    []string{"component.live"},
+					"app_template_name": "tf_testAcc_GWcpq51dSi5td18Qd",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"scene":             "business",
+						"integration_mode":  "paasSDK",
+						"component_list.#":  "1",
+						"app_template_name": "tf_testAcc_GWcpq51dSi5td18Qd",
+					}),
+				),
+			},
+			//{
+			//	Config: testAccConfig(map[string]interface{}{
+			//		"app_template_name": "tf_testAcc_IN1u0gHPAo",
+			//	}),
+			//	Check: resource.ComposeTestCheckFunc(
+			//		testAccCheck(map[string]string{
+			//			"app_template_name": "tf_testAcc_IN1u0gHPAo",
+			//		}),
+			//	),
+			//},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"config_list": []map[string]interface{}{
+						{
+							"key":   "config.appCallbackAuthKey",
+							"value": "tf-testAcc-jdD4qhGOujVlYcCUqTDUumAV",
+						},
+						{
+							"key":   "config.appCallbackUrl",
+							"value": "http://aliyun.com/tf-testAcc-jdD4qhGOujVlYcCUqTDUumAV",
+						},
+						{
+							"key":   "config.livePullDomain",
+							"value": "tf-testAcc-jdD4qhGOujVlYcCUqTDUumAV.com",
+						},
+						{
+							"key":   "config.livePushDomain",
+							"value": "tf-testAcc-jdD4qhGOujVlYcCUqTD.com",
+						},
+						{
+							"key":   "config.regionId",
+							"value": "cn-hangzhou",
+						},
+						{
+							"key":   "config.streamChangeCallbackUrl",
+							"value": "https://aliyun.com/tf-testAcc-jdD4qhGOujVlYcCU",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"config_list.#": "6",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					//"app_template_name": "tf_testAcc_tqPHQU5xU",
+					"config_list": []map[string]interface{}{
+						{
+							"key":   "config.appCallbackAuthKey",
+							"value": "tf-testAcc-jdD4qhGOxxxxxxxx",
+						},
+						{
+							"key":   "config.appCallbackUrl",
+							"value": "http://aliyun.com/tf-testAcc-jdD4qhGOxxxxxxxx",
+						},
+						{
+							"key":   "config.livePullDomain",
+							"value": "tf-testAcc-jdD4qhGOxxxxxxxx.com",
+						},
+						{
+							"key":   "config.livePushDomain",
+							"value": "tf-testAcc-jdD4qhGOxxxxxxxx.com",
+						},
+						{
+							"key":   "config.regionId",
+							"value": "cn-shanghai",
+						},
+						{
+							"key":   "config.streamChangeCallbackUrl",
+							"value": "https://aliyun.com/tf-testAcc-jdD4qhGOxxxxxxxx",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						//"app_template_name": "tf_testAcc_tqPHQU5xU",
+						"config_list.#": "6",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+var AlicloudIMPAppTemplateMap0 = map[string]string{
+	"component_list.#": CHECKSET,
+	"config_list.#":    CHECKSET,
+	"status":           CHECKSET,
+}
+
+func AlicloudIMPAppTemplateBasicDependence0(name string) string {
+	return fmt.Sprintf(` 
+variable "name" {
+  default = "%s"
+}
+`, name)
+}
---
+++ website/docs/r/imp_app_template.html.markdown
@@ -0,0 +1,65 @@
+---
+subcategory: "Apsara Agile Live (IMP)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_imp_app_template"
+sidebar_current: "docs-alicloud-resource-imp-app-template"
+description: |-
+  Provides a Alicloud Apsara Agile Live (IMP) App Template resource.
+---
+
+# alicloud\_imp\_app\_template
+
+Provides a Apsara Agile Live (IMP) App Template resource.
+
+For information about Apsara Agile Live (IMP) App Template and how to use it, see [What is App Template](https://help.aliyun.com/document_detail/273168.html).
+
+-> **NOTE:** Available in v1.137.0+.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+resource "alicloud_imp_app_template" "example" {
+  app_template_name = "example_value"
+  component_list    = ["component.live", "component.liveRecord"]
+  integration_mode  = "paasSDK"
+  scene             = "business"
+}
+
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `app_template_name` - (Required) The name of the resource.
+* `component_list` - (Required, ForceNew) List of components. Its element valid values: ["component.live","component.liveRecord","component.liveBeauty","component.rtc","component.rtcRecord","component.im","component.whiteboard","component.liveSecurity","component.chatSecurity"].
+* `config_list` - (Optional, Computed) Configuration list. It have several default configs after the resource is created. See the following `Block config_list`.
+* `integration_mode` - (Optional, ForceNew) Integration mode. Valid values:
+  * paasSDK: Integrated SDK.
+  * standardRoom: Model Room.
+  
+* `scene` - (Optional, ForceNew) Application Template scenario. Valid values: ["business", "classroom"].
+
+#### Block config_list
+
+The config_list supports the following: 
+
+* `key` - (Optional) Configuration item key. Valid values: 
+* `value` - (Optional) Configuration item content.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The resource ID in terraform of App Template.
+* `status` - Application template usage status.
+
+## Import
+
+Apsara Agile Live (IMP) App Template can be imported using the id, e.g.
+
+```
+$ terraform import alicloud_imp_app_template.example <id>
+```
