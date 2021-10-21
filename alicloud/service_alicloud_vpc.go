bb257b7f5c8a83cbf41931a87c0bea06bf721a1a
super-eggs <gzy01137900@alibaba-inc.com>
2021-10-21 14:59:54 +0800

    New Resource `alicloud_vpc_traffic_mirror_filter_ingress_rule` New Datasource `alicloud_vpc_traffic_mirror_filter_ingress_rules`

--- alicloud/service_alicloud_vpc.go
+++ alicloud/service_alicloud_vpc.go
@@ -2436,3 +2436,86 @@ func (s *VpcService) VpcTrafficMirrorFilterEgressRuleStateRefreshFunc(id string,
 		return object, fmt.Sprint(object["TrafficMirrorFilterRuleStatus"]), nil
 	}
 }
+
+func (s *VpcService) DescribeVpcTrafficMirrorFilterIngressRule(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewVpcClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "ListTrafficMirrorFilters"
+	parts, err := ParseResourceId(id, 2)
+	if err != nil {
+		err = WrapError(err)
+		return
+	}
+
+	request := map[string]interface{}{
+		"RegionId":               s.client.RegionId,
+		"MaxResults":             10,
+		"TrafficMirrorFilterIds": []string{parts[0]},
+	}
+	idExist := false
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-04-28"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.TrafficMirrorFilters", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.TrafficMirrorFilters", response)
+	}
+	if len(v.([]interface{})) < 1 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("VPC", id)), NotFoundWithResponse, response)
+	}
+	for _, v := range v.([]interface{}) {
+		EgressRules := v.(map[string]interface{})["IngressRules"]
+		if EgressRulesMap, ok := EgressRules.([]interface{}); ok {
+			for _, v := range EgressRulesMap {
+				if fmt.Sprint(v.(map[string]interface{})["TrafficMirrorFilterRuleId"]) == parts[1] {
+					idExist = true
+					return v.(map[string]interface{}), nil
+				}
+			}
+		}
+	}
+
+	if !idExist {
+		return object, WrapErrorf(Error(GetNotFoundMessage("VPC", id)), NotFoundWithResponse, response)
+	}
+	return
+}
+
+func (s *VpcService) VpcTrafficMirrorFilterIngressRuleStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeVpcTrafficMirrorFilterIngressRule(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["TrafficMirrorFilterRuleStatus"]) == failState {
+				return object, fmt.Sprint(object["TrafficMirrorFilterRuleStatus"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["TrafficMirrorFilterRuleStatus"])))
+			}
+		}
+		return object, fmt.Sprint(object["TrafficMirrorFilterRuleStatus"]), nil
+	}
+}
