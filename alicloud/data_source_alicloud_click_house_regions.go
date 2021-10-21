9618698983ebf526eea2c776cee0c73a6807c566
wanghx <wanghx0991@outlook.com>
2021-10-21 17:04:30 +0800

    doc/r/sae_application add comma.

--- alicloud/data_source_alicloud_click_house_regions.go
+++ alicloud/data_source_alicloud_click_house_regions.go
@@ -75,6 +75,7 @@ func dataSourceAlicloudClickHouseRegionsRead(d *schema.ResourceData, meta interf
 	}
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
+
 	wait := incrementalWait(3*time.Second, 3*time.Second)
 	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-11"), StringPointer("AK"), nil, request, &runtime)
