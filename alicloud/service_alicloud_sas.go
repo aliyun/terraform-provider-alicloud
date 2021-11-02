b8e1828f955c3e6647c45e19b6400bb53499d647
ll221756 <ll221756@alibaba-inc.com>
2021-11-02 16:28:29 +0800

    New Resource `alicloud_security_center_slr` resource/alicloud_event_bridge_service_sle: After the version 1.142.0, the resource is renamed as `alicloud_event_bridge_service_linked_role`.

--- alicloud/service_alicloud_sas.go
+++ alicloud/service_alicloud_sas.go
@@ -59,3 +59,57 @@ func (s *SasService) DescribeAllGroups(id string) (object map[string]interface{}
 	}
 	return object, nil
 }
+
+func (s *SasService) DescribeSecurityCenterServiceLinkedRole(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewSasClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeServiceLinkedRoleStatus"
+	request := map[string]interface{}{}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("GET"), StringPointer("2018-12-03"), StringPointer("AK"), request, nil, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.RoleStatus", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.RoleStatus", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
+
+func (s *SasService) SecurityCenterServiceLinkedRoleStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeSecurityCenterServiceLinkedRole(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["Status"]) == failState {
+				return object, fmt.Sprint(object["Status"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["Status"])))
+			}
+		}
+		return object, fmt.Sprint(object["Status"]), nil
+	}
+}
