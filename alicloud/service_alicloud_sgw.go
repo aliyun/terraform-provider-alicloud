3be990254bc57089ffb14c8a3698417cf50ede03
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-11-07 21:33:09 +0800

    resource/alicloud_cloud_storage_gateway_gateway_smb_user: Adds waiting codes to wait the task is completed

--- alicloud/service_alicloud_sgw.go
+++ alicloud/service_alicloud_sgw.go
@@ -194,3 +194,88 @@ func (s *SgwService) DescribeCloudStorageGatewayGatewaySmbUser(id string) (objec
 	}
 	return
 }
+
+func (s *SgwService) DescribeTasks(id, taskId string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewHcsSgwClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeTasks"
+	request := map[string]interface{}{
+		"TargetId":   id,
+		"PageSize":   PageSizeLarge,
+		"PageNumber": 1,
+	}
+	if taskId != "" {
+		request["TaskId"] = taskId
+	}
+	idExist := false
+	for {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2018-05-11"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"GatewayNotExist"}) {
+				return object, WrapErrorf(Error(GetNotFoundMessage("CloudStorageGateway:Gateway:Task", id+":"+taskId)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+			}
+			return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+		}
+		if fmt.Sprint(response["Success"]) == "false" {
+			return object, WrapError(fmt.Errorf("%s failed, response: %v", action, response))
+		}
+		v, err := jsonpath.Get("$.Tasks.SimpleTask", response)
+		if err != nil {
+			return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Tasks.SimpleTask", response)
+		}
+		if len(v.([]interface{})) < 1 {
+			return object, WrapErrorf(Error(GetNotFoundMessage("CloudStorageGateway Task", id+":"+taskId)), NotFoundWithResponse, response)
+		}
+		for _, v := range v.([]interface{}) {
+			if taskId != "" && fmt.Sprint(v.(map[string]interface{})["TaskId"]) == taskId {
+				idExist = true
+				return v.(map[string]interface{}), nil
+			}
+		}
+		if len(v.([]interface{})) < request["PageSize"].(int) {
+			break
+		}
+		request["PageNumber"] = request["PageNumber"].(int) + 1
+	}
+	if !idExist {
+		return object, WrapErrorf(Error(GetNotFoundMessage("CloudStorageGateway Task", id+":"+taskId)), NotFoundWithResponse, response)
+	}
+	return
+}
+
+func (s *SgwService) CloudStorageGatewayTaskStateRefreshFunc(id, taskId string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeTasks(id, taskId)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["StateCode"]) == failState {
+				return object, fmt.Sprint(object["StateCode"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["StateCode"])))
+			}
+		}
+		return object, fmt.Sprint(object["StateCode"]), nil
+	}
+}
