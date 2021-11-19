7ddf683d7cf183e93a7079edec52cd61f40d4c53
mengliux <vivianmliu@sina.com>
2021-11-19 11:17:08 +0800

    bugfix

--- alicloud/resource_alicloud_polardb_cluster.go
+++ alicloud/resource_alicloud_polardb_cluster.go
@@ -563,14 +563,15 @@ func resourceAlicloudPolarDBClusterRead(d *schema.ResourceData, meta interface{}
 		return WrapError(err)
 	}
 
-	if err = polarDBService.DBClusterIPArrays(d, "db_cluster_ip_array"); err != nil {
-		return WrapError(err)
-	}
-
 	dbClusterIPArrayName := "default"
 	if v, ok := d.GetOk("db_cluster_ip_array_name"); ok {
 		dbClusterIPArrayName = v.(string)
 	}
+
+	if err = polarDBService.DBClusterIPArrays(d, "db_cluster_ip_array", dbClusterIPArrayName); err != nil {
+		return WrapError(err)
+	}
+
 	ips, err := polarDBService.DescribeDBSecurityIps(d.Id(), dbClusterIPArrayName)
 	if err != nil {
 		return WrapError(err)
