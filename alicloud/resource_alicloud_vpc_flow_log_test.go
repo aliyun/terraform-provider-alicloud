4609e5d6275f92754983bb0e8fa13f43dc5eb4ad
nihonglei <nhl01089624@alibaba-inc.com>
2021-11-23 10:54:55 +0800

    resource/alicloud_vpc_flow_log: Add optional parameter verification

--- alicloud/resource_alicloud_vpc_flow_log_test.go
+++ alicloud/resource_alicloud_vpc_flow_log_test.go
@@ -111,6 +111,58 @@ func TestAccAlicloudVpcFlowLog_basic(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudVpcFlowLog_basic1(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_vpc_flow_log.default"
+	ra := resourceAttrInit(resourceId, AlicloudVpcFlowLogMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &VpcService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeVpcFlowLog")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%svpcflowlog%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudVpcFlowLogBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"flow_log_name":  "${var.name}",
+					"log_store_name": "${alicloud_log_store.default.name}",
+					"project_name":   "${alicloud_log_project.default.name}",
+					"resource_id":    "${alicloud_vpc.default.id}",
+					"resource_type":  "VPC",
+					"traffic_type":   "All",
+					"description":    name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"flow_log_name":  name,
+						"log_store_name": CHECKSET,
+						"project_name":   CHECKSET,
+						"resource_id":    CHECKSET,
+						"resource_type":  "VPC",
+						"traffic_type":   "All",
+						"description":    name,
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 var AlicloudVpcFlowLogMap0 = map[string]string{
 	"status":         "Active",
 	"log_store_name": CHECKSET,
