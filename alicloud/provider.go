be80a3634553e5e167f3a63684e3acaab5cc6839
zhaoguolong-sy2 <wb-zgl885911@alibaba-inc.com>
2021-11-24 11:05:54 +0800

    New Resource `alicloud_rds_clone_db_instance`

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -642,6 +642,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_db_connection":                      resourceAlicloudDBConnection(),
 			"alicloud_db_read_write_splitting_connection": resourceAlicloudDBReadWriteSplittingConnection(),
 			"alicloud_db_instance":                        resourceAlicloudDBInstance(),
+			"alicloud_rds_clone_db_instance":              resourceAlicloudRdsCloneDbInstance(),
 			"alicloud_mongodb_instance":                   resourceAlicloudMongoDBInstance(),
 			"alicloud_mongodb_sharding_instance":          resourceAlicloudMongoDBShardingInstance(),
 			"alicloud_gpdb_instance":                      resourceAlicloudGpdbInstance(),
