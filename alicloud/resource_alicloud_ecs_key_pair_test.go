91bdad2ce2982be7addc18d86e5c9e350aae65b3
nihonglei <nhl01089624@alibaba-inc.com>
2021-12-02 16:48:37 +0800

    resource/alicloud_ecs_key_pair: Add optional parameter verification

--- alicloud/resource_alicloud_ecs_key_pair_test.go
+++ alicloud/resource_alicloud_ecs_key_pair_test.go
@@ -169,6 +169,55 @@ func TestAccAlicloudEcsKeyPair_public_key(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudEcsKeyPair_basic1(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_ecs_key_pair.default"
+	ra := resourceAttrInit(resourceId, AlicloudEcsKeyPairMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeEcsKeyPair")
+	rac := resourceAttrCheckInit(rc, ra)
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAccAlicloudEcsKeyPair%d", rand)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudEcsKeyPairBasicDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"key_name":          name,
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.0}",
+					"tags": map[string]string{
+						"Created": "Terraform",
+						"For":     "Test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"key_name":          name,
+						"resource_group_id": CHECKSET,
+						"tags.%":            "2",
+						"tags.Created":      "Terraform",
+						"tags.For":          "Test",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 var AlicloudEcsKeyPairMap = map[string]string{}
 
 func AlicloudEcsKeyPairBasicDependence(name string) string {
