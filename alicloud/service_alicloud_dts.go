10f09e5f10b2a3f24af3a21597a0fa8f9ecd266d
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2021-12-02 11:21:07 +0800

    resource/alicloud_dts_synchronization_instance: replace api DescribeDtsJobDetail

--- alicloud/service_alicloud_dts.go
+++ alicloud/service_alicloud_dts.go
@@ -127,57 +127,41 @@ func (s *DtsService) DescribeDtsSynchronizationInstance(id string) (object map[s
 	if err != nil {
 		return nil, WrapError(err)
 	}
-	action := "DescribeSynchronizationJobs"
+	action := "DescribeDtsJobDetail"
 	request := map[string]interface{}{
-		"RegionId": s.client.RegionId,
-		"PageNum":  1,
-		"PageSize": 30,
+		"RegionId":      s.client.RegionId,
+		"DtsInstanceID": id,
 	}
-	idExist := false
-	for {
-		runtime := util.RuntimeOptions{}
-		runtime.SetAutoretry(true)
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-01-01"), StringPointer("AK"), nil, request, &runtime)
-			if err != nil {
-				if NeedRetry(err) {
-					wait()
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-			return nil
-		})
-		addDebug(action, response, request)
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-01-01"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
-			if IsExpectedErrors(err, []string{"Forbidden.InstanceNotFound"}) {
-				return object, WrapErrorf(Error(GetNotFoundMessage("DTS:SynchronizationInstance", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
 			}
-			return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+			return resource.NonRetryableError(err)
 		}
-		v, err := jsonpath.Get("$.SynchronizationInstances", response)
-		if err != nil {
-			return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.SynchronizationInstances", response)
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"Forbidden.InstanceNotFound"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("DTS:SynchronizationInstance", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
 		}
-		if len(v.([]interface{})) < 1 {
-			return object, WrapErrorf(Error(GetNotFoundMessage("DTS", id)), NotFoundWithResponse, response)
-		}
-		for _, v := range v.([]interface{}) {
-			if fmt.Sprint(v.(map[string]interface{})["SynchronizationJobId"]) == id {
-				idExist = true
-				return v.(map[string]interface{}), nil
-			}
-		}
-		if len(v.([]interface{})) < request["PageSize"].(int) {
-			break
-		}
-		request["PageNum"] = request["PageNum"].(int) + 1
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
 	}
-	if !idExist {
-		return object, WrapErrorf(Error(GetNotFoundMessage("DTS", id)), NotFoundWithResponse, response)
+	if fmt.Sprint(response["Success"]) == "false" {
+		return object, WrapError(fmt.Errorf("%s failed, response: %v", action, response))
 	}
-	return
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
 }
 
 func (s *DtsService) ListTagResources(id string, resourceType string) (object interface{}, err error) {
