0b3f66c3010abf4c9177fe604cfb78e9e23b230d
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-12-05 16:37:43 +0800

    testcase: Improves the log project sweeper test case

--- alicloud/resource_alicloud_log_project_test.go
+++ alicloud/resource_alicloud_log_project_test.go
@@ -2,6 +2,7 @@ package alicloud
 
 import (
 	"fmt"
+	"github.com/denverdino/aliyungo/cs"
 	"log"
 	"strings"
 	"testing"
@@ -58,6 +59,25 @@ func testSweepLogProjectsWithPrefixAndSuffix(region string, prefixes, suffixes [
 				if strings.HasSuffix(strings.ToLower(name), strings.ToLower(suffix)) {
 					skip = false
 					break
+				}
+			}
+		}
+		// Sweep the project which from the k8s cluster
+		if skip && strings.HasPrefix(name, "k8s-log-") {
+			k8sId := strings.TrimPrefix(name, "k8s-log-")
+			raw, err := client.WithCsClient(func(csClient *cs.Client) (interface{}, error) {
+				return csClient.DescribeCluster(k8sId)
+			})
+			if err != nil {
+				if IsExpectedErrors(err, []string{"ErrorClusterNotFound"}) {
+					skip = false
+				} else {
+					log.Printf("[ERROR] DescribeCluster got an error: %#v", err)
+				}
+			} else {
+				cluster, _ := raw.(cs.ClusterType)
+				if strings.HasPrefix(strings.ToLower(cluster.Name), "tf-testacc") || strings.HasPrefix(strings.ToLower(cluster.Name), "tf_testacc") {
+					skip = false
 				}
 			}
 		}
