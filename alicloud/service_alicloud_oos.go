4eb14b87258936b12fe14a5472bc7fe899360420
新木 <ll221756@alibaba-inc.com>
2021-12-06 15:17:39 +0800

    Mew Resource `alicloud_oos_service_setting`

--- alicloud/service_alicloud_oos.go
+++ alicloud/service_alicloud_oos.go
@@ -223,3 +223,39 @@ func (s *OosService) DescribeOosPatchBaseline(id string) (object map[string]inte
 	object = v.(map[string]interface{})
 	return object, nil
 }
+
+func (s *OosService) DescribeOosServiceSetting(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewOosClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "GetServiceSettings"
+	request := map[string]interface{}{
+		"RegionId": s.client.RegionId,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-06-01"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.ServiceSettings", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.ServiceSettings", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
