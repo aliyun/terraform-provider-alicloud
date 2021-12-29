6ed4e83e75bd52025dfa461b6aed296f41f3110a
nihonglei <nhl01089624@alibaba-inc.com>
2021-12-29 17:30:15 +0800

    testcase: Adds a new test case for resource alicloud_cen_transit_router_vbr_attachment

--- alicloud/resource_alicloud_cen_transit_router_vbr_attachment_test.go
+++ alicloud/resource_alicloud_cen_transit_router_vbr_attachment_test.go
@@ -113,6 +113,68 @@ func TestAccAlicloudCenTransitRouterVbrAttachment_basic(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudCenTransitRouterVbrAttachment_basic1(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cen_transit_router_vbr_attachment.default"
+	ra := resourceAttrInit(resourceId, AlicloudCenTransitRouterVbrAttachmentMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CbnService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCenTransitRouterVbrAttachment")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000000, 9999999)
+	name := fmt.Sprintf("tf-testAccCenTransitRouterVbrAttachment%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudCenTransitRouterVbrAttachmentBasicDependence1)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, true, connectivity.VbrSupportRegions)
+		},
+
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"cen_id":                                "${alicloud_cen_instance.default.id}",
+					"transit_router_id":                     "${alicloud_cen_transit_router.default.transit_router_id}",
+					"transit_router_attachment_name":        "test",
+					"transit_router_attachment_description": "test",
+					"vbr_id":                                "${alicloud_express_connect_virtual_border_router.default.id}",
+					"dry_run":                               "false",
+					"auto_publish_route_enabled":            `false`,
+					"resource_type":                         "VBR",
+					"route_table_association_enabled":       "false",
+					"route_table_propagation_enabled":       "false",
+					"vbr_owner_id":                          "${data.alicloud_account.default.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"cen_id":                                CHECKSET,
+						"transit_router_id":                     CHECKSET,
+						"transit_router_attachment_name":        "test",
+						"transit_router_attachment_description": "test",
+						"vbr_id":                                CHECKSET,
+						"dry_run":                               "false",
+						"auto_publish_route_enabled":            `false`,
+						"resource_type":                         "VBR",
+						"route_table_association_enabled":       "false",
+						"route_table_propagation_enabled":       "false",
+						"vbr_owner_id":                          CHECKSET,
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"dry_run", "resource_type", "route_table_association_enabled", "route_table_propagation_enabled"},
+			},
+		},
+	})
+}
+
 var AlicloudCenTransitRouterVbrAttachmentMap = map[string]string{
 	"auto_publish_route_enabled":            CHECKSET,
 	"cen_id":                                CHECKSET,
@@ -155,5 +217,37 @@ resource "alicloud_express_connect_virtual_border_router" "default" {
   min_tx_interval            = 1000
   detect_multiplier          = 10
 }
+`, name)
+}
+
+func AlicloudCenTransitRouterVbrAttachmentBasicDependence1(name string) string {
+	return fmt.Sprintf(`
+variable "name" {	
+	default = "%s"
+}
+resource "alicloud_cen_instance" "default" {
+  cen_instance_name = var.name
+  protection_level = "REDUCED"
+}
+resource "alicloud_cen_transit_router" "default" {
+cen_id= alicloud_cen_instance.default.id
+}
+data "alicloud_express_connect_physical_connections" "nameRegex" {
+  name_regex = "^preserved-NODELETING"
+}
+
+resource "alicloud_express_connect_virtual_border_router" "default" {
+  local_gateway_ip           = "10.0.0.1"
+  peer_gateway_ip            = "10.0.0.2"
+  peering_subnet_mask        = "255.255.255.252"
+  physical_connection_id     = data.alicloud_express_connect_physical_connections.nameRegex.connections.0.id
+  virtual_border_router_name = var.name
+  vlan_id                    = 19
+  min_rx_interval            = 1000
+  min_tx_interval            = 1000
+  detect_multiplier          = 10
+}
+
+data "alicloud_account" "default" {}
 `, name)
 }
