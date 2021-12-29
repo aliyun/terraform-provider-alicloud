1b2e096217f9170bd451fb08491d0047a1ded2ef
nihonglei <nhl01089624@alibaba-inc.com>
2021-12-29 17:09:25 +0800

    tcase: Adds a new test case for resource alicloud_cen_transit_router_route_table_propagation

--- alicloud/resource_alicloud_cen_transit_router_route_table_propagation_test.go
+++ alicloud/resource_alicloud_cen_transit_router_route_table_propagation_test.go
@@ -54,6 +54,52 @@ func TestAccAlicloudCenTransitRouterRouteTablePropagation_basic(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudCenTransitRouterRouteTablePropagation_basic1(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cen_transit_router_route_table_propagation.default"
+	ra := resourceAttrInit(resourceId, AlicloudCenTransitRouterRouteTablePropagationMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CbnService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCenTransitRouterRouteTablePropagation")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000000, 9999999)
+	name := fmt.Sprintf("tf-testAccCenTransitRouterRouteTablePropagation%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudCenTransitRouterRouteTablePropagationBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, true, connectivity.VbrSupportRegions)
+		},
+
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_attachment_id":  "${alicloud_cen_transit_router_vbr_attachment.default.transit_router_attachment_id}",
+					"transit_router_route_table_id": "${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}",
+					"dry_run":                       "false",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_attachment_id":  CHECKSET,
+						"transit_router_route_table_id": CHECKSET,
+						"dry_run":                       "false",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"dry_run"},
+			},
+		},
+	})
+}
+
 var AlicloudCenTransitRouterRouteTablePropagationMap0 = map[string]string{
 	"dry_run":                       NOSET,
 	"status":                        CHECKSET,
