d732b06b16d2d6e545f0f89ac26ee107633a2a8a
新木 <ll221756@alibaba-inc.com>
2022-01-05 12:02:57 +0800

    New Resource `alidns_gtm_instance` New Daatsource `alidns_gtm_instances`

--- alicloud/service_alicloud_alidns.go
+++ alicloud/service_alicloud_alidns.go
@@ -332,3 +332,42 @@ func (s *AlidnsService) DescribeCustomLine(id string) (object map[string]interfa
 	object = v.(map[string]interface{})
 	return object, nil
 }
+
+func (s *AlidnsService) DescribeAlidnsGtmInstance(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewAlidnsClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeDnsGtmInstance"
+	request := map[string]interface{}{
+		"InstanceId": id,
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2015-01-09"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"DnsGtmInstance.NotExists"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("Alidns:GtmInstance", id)), NotFoundWithResponse, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
