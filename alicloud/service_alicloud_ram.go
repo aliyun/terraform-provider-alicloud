ff646f22e86604d65b1fae28215e2b59a3baf83a
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2022-01-11 17:17:35 +0800

    New Resource alicloud_ram_security_preference

--- alicloud/service_alicloud_ram.go
+++ alicloud/service_alicloud_ram.go
@@ -828,3 +828,37 @@ func (s *RamService) DescribeRamAccountPasswordPolicy(id string) (*ram.GetPasswo
 
 	return response, nil
 }
+
+func (s *RamService) DescribeRamSecurityPreference(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewImsClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "GetSecurityPreference"
+	request := map[string]interface{}{}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-08-15"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.SecurityPreference", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.SecurityPreference", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
