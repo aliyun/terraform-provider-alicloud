1e913f728519cded016f6a6004f19914d5b5558e
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2022-03-08 15:53:53 +0800

    add some modules configuration

--- ci/tasks/resource-test.sh
+++ ci/tasks/resource-test.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#: ${ALICLOUD_ACCESS_KEY:?}
-#: ${ALICLOUD_SECRET_KEY:?}
-#: ${ALICLOUD_REGION:?}
-#: ${RESOURCE_NAME:?}
-#
-#
-#export ALICLOUD_ACCESS_KEY=${ALICLOUD_ACCESS_KEY}
-#export ALICLOUD_SECRET_KEY=${ALICLOUD_SECRET_KEY}
-#export ALICLOUD_REGION=${ALICLOUD_REGION}
+: ${ALICLOUD_ACCESS_KEY:?}
+: ${ALICLOUD_SECRET_KEY:?}
+: ${ALICLOUD_REGION:?}
+: ${RESOURCE_NAME:?}
+
+
+export ALICLOUD_ACCESS_KEY=${ALICLOUD_ACCESS_KEY}
+export ALICLOUD_SECRET_KEY=${ALICLOUD_SECRET_KEY}
+export ALICLOUD_REGION=${ALICLOUD_REGION}
 
 go version
 
@@ -17,31 +17,67 @@ CURRENT_PATH=$(pwd)
 PINK='\E[1;35m'        #粉红
 RES='\E[0m'
 
-RESOURCE_NAME=$1
-echo "Line 14: RESOURCE_NAME = $RESOURCE_NAME"
 
 echo -e  "Current Go Version: $(go version)"
 
 
 CURRENT_PATH=$(pwd)
+TERRAFORM_SOURCE_PATH=$CURRENT_PATH/terraform-provider-alicloud
 
 
+echo -e  "${PINK}RESOURCE_NAME = ${RESOURCE_NAME} ${RES}"
+cd $GOPATH
+mkdir -p src/github.com/aliyun
+cd src/github.com/aliyun
 
-#echo -e  "${PINK}RESOURCE_NAME = ${RESOURCE_NAME}${RES}"
-#cd $GOPATH
-#mkdir -p src/github.com/aliyun
-#cd src/github.com/aliyun
-#
-#cp -rf ${CURRENT_PATH}/terraform-provider-alicloud ./
-#echo -e  "${PINK} 31line${RES}"
-#cd ./terraform-provider-alicloud
+cp -rf ${CURRENT_PATH}/terraform-provider-alicloud ./
+echo -e  "${PINK} 31line${RES}"
+cd ./terraform-provider-alicloud
 
 resourceArray=(`echo $RESOURCE_NAME | tr ',' ' '`)
-for filename in ls ./alicloud
+
+FAILED_COUNT=0
+
+for resource in "${!resourceArray[@]}"
 do
-  for resource in "${!resourceArray[@]}"
-   do
-      res=${resourceArray[resource]}
-      echo "resource = $res"
+  res=${resourceArray[resource]}
+  fileName="alicloud/resource_${res}_test.go"
+  echo "filename = $fileName"
+  checkFuncs=$(grep "func TestAcc.*" ${fileName})
+  echo -e "found the test funcs:\n${checkFuncs}\n"
+  funcs=(${checkFuncs//"(t *testing.T) {"/ })
+  for func in ${funcs[@]};
+  do
+    checkUnit=$(echo $func | grep "_unit")
+    if [[ ${func} != "TestAcc"* || $checkUnit != "" ]]; then
+      continue
+    fi
+    echo -e  "${PINK} Running the test: ${func} ${RES}"
+    TF_ACC=1 go test ./alicloud -v -run=${func} -timeout=1200m | {
+      while read LINE
+      do
+          echo -e "$LINE"
+          if [[ $LINE == "--- FAIL: "* || ${LINE} == "FAIL"* ]]; then
+              FAILED_COUNT=$((${FAILED_COUNT}+1))
+          fi
+          if [[ $LINE == "panic: "* ]]; then
+              FAILED_COUNT=$((${FAILED_COUNT}+1))
+              break
+          fi
+      done
+      # send child var to an failed file
+      if [[ $FAILED_COUNT -gt 0 ]]; then
+        echo -e "\033[31mrecord the failed count $FAILED_COUNT into a temp file\033[0m"
+        echo $FAILED_COUNT > failed.txt
+      fi
+    }
   done
+  echo -e "\033[34mFinished\033[0m"
 done
+
+# read var from failed file and remove this file
+read FAILED_COUNT < failed.txt
+if [[ $FAILED_COUNT -gt 0 ]]; then
+  echo -e "\033[31mThere gets $FAILED_COUNT failed testcase.\033[0m"
+fi
+rm -rf failed.txt
