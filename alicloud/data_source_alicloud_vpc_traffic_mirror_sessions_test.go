934f4bdc03c0955c5e48c5c87219a6054542425a
super-eggs <gzy01137900@alibaba-inc.com>
2022-03-11 19:12:12 +0800

    resource/vpc_traffic_mirror_session: Fix test cases; data_source/vpc_traffic_mirror_sessions: Fix test cases.

--- alicloud/data_source_alicloud_vpc_traffic_mirror_sessions_test.go
+++ alicloud/data_source_alicloud_vpc_traffic_mirror_sessions_test.go
@@ -47,7 +47,7 @@ func TestAccAlicloudVpcTrafficMirrorSessionsDataSource(t *testing.T) {
 	priorityConf := dataSourceTestAccConfig{
 		existConfig: testAccConfig(map[string]interface{}{
 			"ids":      []string{"${alicloud_vpc_traffic_mirror_session.default.id}"},
-			"priority": "1",
+			"priority": "11",
 		}),
 		fakeConfig: testAccConfig(map[string]interface{}{
 			"ids":      []string{"${alicloud_vpc_traffic_mirror_session.default.id}"},
@@ -109,7 +109,7 @@ func TestAccAlicloudVpcTrafficMirrorSessionsDataSource(t *testing.T) {
 			"name_regex":                  "${alicloud_vpc_traffic_mirror_session.default.traffic_mirror_session_name}",
 			"ids":                         []string{"${alicloud_vpc_traffic_mirror_session.default.id}"},
 			"enabled":                     "false",
-			"priority":                    "1",
+			"priority":                    "11",
 			"traffic_mirror_filter_id":    "${alicloud_vpc_traffic_mirror_session.default.traffic_mirror_filter_id}",
 			"traffic_mirror_session_name": "${alicloud_vpc_traffic_mirror_session.default.traffic_mirror_session_name}",
 			"traffic_mirror_source_id":    "${alicloud_vpc_traffic_mirror_session.default.traffic_mirror_source_ids[0]}",
@@ -138,7 +138,7 @@ func TestAccAlicloudVpcTrafficMirrorSessionsDataSource(t *testing.T) {
 			"sessions.0.traffic_mirror_session_description":     fmt.Sprintf("tf-testacc-vpctrafficmirrorsession-%d", rand),
 			"sessions.0.status":                                 "Created",
 			"sessions.0.traffic_mirror_target_type":             "NetworkInterface",
-			"sessions.0.priority":                               "1",
+			"sessions.0.priority":                               "11",
 			"sessions.0.traffic_mirror_target_id":               CHECKSET,
 			"sessions.0.traffic_mirror_source_ids.#":            "1",
 			"sessions.0.traffic_mirror_filter_id":               CHECKSET,
@@ -177,24 +177,75 @@ variable "name" {
   default = "%s"
 }
 
+data "alicloud_instance_types" "default" {
+  instance_type_family = "ecs.g7"
+}
+
+data "alicloud_zones" "default" {
+  available_resource_creation = "Instance"
+  available_instance_type     = data.alicloud_instance_types.default.instance_types.0.id
+}
+
+data "alicloud_vpcs" "default" {
+  name_regex = "default-NODELETING"
+}
+data "alicloud_vswitches" "default" {
+  vpc_id  = data.alicloud_vpcs.default.ids.0
+  zone_id = data.alicloud_zones.default.zones.0.id
+}
+
+locals {
+  vswitch_id = data.alicloud_vswitches.default.ids[0]
+}
+
+resource "alicloud_security_group" "default" {
+  name        = var.name
+  description = var.name
+  vpc_id      = data.alicloud_vpcs.default.ids.0
+}
+
+data "alicloud_images" "default" {
+  name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
+  most_recent = true
+  owners      = "system"
+}
+
+resource "alicloud_instance" "default" {
+  availability_zone    = data.alicloud_zones.default.zones.0.id
+  instance_name        = var.name
+  host_name            = var.name
+  image_id             = data.alicloud_images.default.images.0.id
+  instance_type        = data.alicloud_instance_types.default.instance_types.0.id
+  security_groups      = [alicloud_security_group.default.id]
+  vswitch_id           = local.vswitch_id
+  system_disk_category = "cloud_essd"
+}
+
+resource "alicloud_ecs_network_interface" "default" {
+  count                  = 2
+  network_interface_name = var.name
+  vswitch_id             = local.vswitch_id
+  security_group_ids     = [alicloud_security_group.default.id]
+}
+
+resource "alicloud_ecs_network_interface_attachment" "default" {
+  count                = 2
+  instance_id          = alicloud_instance.default.id
+  network_interface_id = element(alicloud_ecs_network_interface.default.*.id, count.index)
+}
+
 resource "alicloud_vpc_traffic_mirror_filter" "default" {
   traffic_mirror_filter_name        = var.name
   traffic_mirror_filter_description = var.name
 }
 
-data "alicloud_ecs_network_interfaces" "default" {
-  tags = {
-    tf-testacc = "vpctrafficmirrorsession"
-  }
-}
-
 resource "alicloud_vpc_traffic_mirror_session" "default" {
-  priority                           = "1"
-  virtual_network_id                 = "10"
+  priority                           = 11
+  virtual_network_id                 = 10
   traffic_mirror_session_description = var.name
   traffic_mirror_session_name        = var.name
-  traffic_mirror_target_id           = data.alicloud_ecs_network_interfaces.default.ids.0
-  traffic_mirror_source_ids          = [data.alicloud_ecs_network_interfaces.default.ids.1]
+  traffic_mirror_target_id           = alicloud_ecs_network_interface_attachment.default[0].network_interface_id
+  traffic_mirror_source_ids          = [alicloud_ecs_network_interface_attachment.default[1].network_interface_id]
   traffic_mirror_filter_id           = alicloud_vpc_traffic_mirror_filter.default.id
   traffic_mirror_target_type         = "NetworkInterface"
 }`, name)
