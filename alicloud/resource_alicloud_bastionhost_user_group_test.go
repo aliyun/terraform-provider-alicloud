364445dd38eb19cec539e5fb1a40083958776a45
nihonglei <nhl01089624@alibaba-inc.com>
2022-03-15 17:15:21 +0800

    testcase: Adds new unit test case for resource alicloud_bastionhost_user_attachment alicloud_cms_group_metric_rule alicloud_bastionhost_user_group

--- alicloud/resource_alicloud_bastionhost_user_group_test.go
+++ alicloud/resource_alicloud_bastionhost_user_group_test.go
@@ -2,8 +2,18 @@ package alicloud
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"reflect"
 	"testing"
 
+	"github.com/agiledragon/gomonkey/v2"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/alibabacloud-go/tea-rpc/client"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -127,4 +137,267 @@ resource "alicloud_bastionhost_instance" "default" {
   security_group_ids = [alicloud_security_group.default.id]
 }
 `, name)
+}
+
+func TestAccAlicloudBastionhostUserGroup_unit(t *testing.T) {
+	p := Provider().(*schema.Provider).ResourcesMap
+	dInit, _ := schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(nil, nil)
+	dExisted, _ := schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(nil, nil)
+	dInit.MarkNewResource()
+	attributes := map[string]interface{}{
+		"comment":         "CreateUserGroupValue",
+		"instance_id":     "CreateUserGroupValue",
+		"user_group_name": "CreateUserGroupValue",
+	}
+	for key, value := range attributes {
+		err := dInit.Set(key, value)
+		assert.Nil(t, err)
+		err = dExisted.Set(key, value)
+		assert.Nil(t, err)
+		if err != nil {
+			log.Printf("[ERROR] the field %s setting error", key)
+		}
+	}
+	region := os.Getenv("ALICLOUD_REGION")
+	rawClient, err := sharedClientForRegion(region)
+	if err != nil {
+		t.Skipf("Skipping the test case with err: %s", err)
+		t.Skipped()
+	}
+	rawClient = rawClient.(*connectivity.AliyunClient)
+	ReadMockResponse := map[string]interface{}{
+		// GetUserGroup
+		"UserGroup": map[string]interface{}{
+			"InstanceId":    "CreateUserGroupValue",
+			"UserGroupId":   "CreateUserGroupValue",
+			"Comment":       "CreateUserGroupValue",
+			"UserGroupName": "CreateUserGroupValue",
+		},
+		"UserGroupId": "CreateUserGroupValue",
+	}
+	CreateMockResponse := map[string]interface{}{
+		// CreateUserGroup
+		"UserGroupId": "CreateUserGroupValue",
+	}
+	ReadMockResponseDiff := map[string]interface{}{}
+	failedResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, &tea.SDKError{
+			Code:       String(errorCode),
+			Data:       String(errorCode),
+			Message:    String(errorCode),
+			StatusCode: tea.Int(400),
+		}
+	}
+	notFoundResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, GetNotFoundErrorFromString(GetNotFoundMessage("alicloud_bastionhost_user_group", errorCode))
+	}
+	successResponseMock := func(operationMockResponse map[string]interface{}) (map[string]interface{}, error) {
+		if len(operationMockResponse) > 0 {
+			mapMerge(ReadMockResponse, operationMockResponse)
+		}
+		return ReadMockResponse, nil
+	}
+
+	// Create
+	t.Run("Create", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewBastionhostClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudBastionhostUserGroupCreate(dInit, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		ReadMockResponseDiff = map[string]interface{}{
+			// GetUserGroup Response
+			"UserGroup": map[string]interface{}{
+				"UserGroupId": "CreateUserGroupValue",
+			},
+		}
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1 // a counter used to cover retry scenario; the same below
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "CreateUserGroup" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if retryIndex >= len(errorCodes)-1 {
+							successResponseMock(ReadMockResponseDiff)
+							return CreateMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudBastionhostUserGroupCreate(dInit, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			default:
+				assert.Nil(t, err)
+				dCompare, _ := schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(dInit.State(), nil)
+				for key, value := range attributes {
+					_ = dCompare.Set(key, value)
+				}
+				assert.Equal(t, dCompare.State().Attributes, dInit.State().Attributes)
+			}
+			if retryIndex >= len(errorCodes)-1 {
+				break
+			}
+		}
+	})
+
+	// Update
+	t.Run("Update", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewBastionhostClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudBastionhostUserGroupUpdate(dExisted, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		// ModifyUserGroup
+		attributesDiff := map[string]interface{}{
+			"comment":         "ModifyUserGroupValue",
+			"user_group_name": "ModifyUserGroupValue",
+		}
+		diff, err := newInstanceDiff("alicloud_bastionhost_user_group", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(dInit.State(), diff)
+		ReadMockResponseDiff = map[string]interface{}{
+			// GetUserGroup Response
+			"UserGroup": map[string]interface{}{
+				"Comment":       "ModifyUserGroupValue",
+				"UserGroupName": "ModifyUserGroupValue",
+			},
+		}
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "ModifyUserGroup" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if retryIndex >= len(errorCodes)-1 {
+							return successResponseMock(ReadMockResponseDiff)
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudBastionhostUserGroupUpdate(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			default:
+				assert.Nil(t, err)
+				dCompare, _ := schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(dExisted.State(), nil)
+				for key, value := range attributes {
+					_ = dCompare.Set(key, value)
+				}
+				assert.Equal(t, dCompare.State().Attributes, dExisted.State().Attributes)
+			}
+			if retryIndex >= len(errorCodes)-1 {
+				break
+			}
+		}
+	})
+
+	// Read
+	t.Run("Read", func(t *testing.T) {
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil", "{}"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "GetUserGroup" {
+					switch errorCode {
+					case "{}":
+						return notFoundResponseMock(errorCode)
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudBastionhostUserGroupRead(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "{}":
+				assert.Nil(t, err)
+			}
+		}
+	})
+
+	// Delete
+	t.Run("Delete", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewBastionhostClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudBastionhostUserGroupDelete(dExisted, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		attributesDiff := map[string]interface{}{}
+		diff, err := newInstanceDiff("alicloud_bastionhost_user_group", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_bastionhost_user_group"].Schema).Data(dInit.State(), diff)
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil", "OBJECT_NOT_FOUND"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "DeleteUserGroup" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							ReadMockResponse = map[string]interface{}{}
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudBastionhostUserGroupDelete(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "OBJECT_NOT_FOUND":
+				assert.Nil(t, err)
+			}
+		}
+	})
 }
--- alicloud/resource_alicloud_cms_group_metric_rule_test.go
+++ alicloud/resource_alicloud_cms_group_metric_rule_test.go
@@ -3,12 +3,20 @@ package alicloud
 import (
 	"fmt"
 	"log"
+	"os"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/PaesslerAG/jsonpath"
 	util "github.com/alibabacloud-go/tea-utils/service"
 
+	"github.com/alibabacloud-go/tea-rpc/client"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -288,4 +296,435 @@ resource "alicloud_cms_monitor_group" "default" {
 }
 
 `, name)
+}
+
+func TestAccAlicloudCmsGroupMetricRule_unit(t *testing.T) {
+	p := Provider().(*schema.Provider).ResourcesMap
+	dInit, _ := schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(nil, nil)
+	dExisted, _ := schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(nil, nil)
+	dInit.MarkNewResource()
+	attributes := map[string]interface{}{
+		"category":           "CreateGroupMetricRuleValue",
+		"contact_groups":     "CreateGroupMetricRuleValue",
+		"dimensions":         "CreateGroupMetricRuleValue",
+		"effective_interval": "CreateGroupMetricRuleValue",
+		"email_subject":      "CreateGroupMetricRuleValue",
+		"escalations": []map[string]interface{}{
+			{
+				"critical": []map[string]interface{}{
+					{
+						"comparison_operator": "CreateGroupMetricRuleValue",
+						"statistics":          "CreateGroupMetricRuleValue",
+						"threshold":           "CreateGroupMetricRuleValue",
+						"times":               1,
+					},
+				},
+				"info": []map[string]interface{}{
+					{
+						"comparison_operator": "CreateGroupMetricRuleValue",
+						"statistics":          "CreateGroupMetricRuleValue",
+						"threshold":           "CreateGroupMetricRuleValue",
+						"times":               1,
+					},
+				},
+				"warn": []map[string]interface{}{
+					{
+						"comparison_operator": "CreateGroupMetricRuleValue",
+						"statistics":          "CreateGroupMetricRuleValue",
+						"threshold":           "CreateGroupMetricRuleValue",
+						"times":               1,
+					},
+				},
+			},
+		},
+		"group_id":               "CreateGroupMetricRuleValue",
+		"group_metric_rule_name": "CreateGroupMetricRuleValue",
+		"interval":               "CreateGroupMetricRuleValue",
+		"metric_name":            "CreateGroupMetricRuleValue",
+		"namespace":              "CreateGroupMetricRuleValue",
+		"no_effective_interval":  "CreateGroupMetricRuleValue",
+		"period":                 1,
+		"rule_id":                "CreateGroupMetricRuleValue",
+		"silence_time":           10,
+		"webhook":                "CreateGroupMetricRuleValue",
+	}
+	for key, value := range attributes {
+		err := dInit.Set(key, value)
+		assert.Nil(t, err)
+		err = dExisted.Set(key, value)
+		assert.Nil(t, err)
+		if err != nil {
+			log.Printf("[ERROR] the field %s setting error", key)
+		}
+	}
+	region := os.Getenv("ALICLOUD_REGION")
+	rawClient, err := sharedClientForRegion(region)
+	if err != nil {
+		t.Skipf("Skipping the test case with err: %s", err)
+		t.Skipped()
+	}
+	rawClient = rawClient.(*connectivity.AliyunClient)
+	ReadMockResponse := map[string]interface{}{
+		// DescribeMetricRuleList
+		"Alarms": map[string]interface{}{
+			"Alarm": []interface{}{
+				map[string]interface{}{
+					"ContactGroups":     "CreateGroupMetricRuleValue",
+					"Dimensions":        "CreateGroupMetricRuleValue",
+					"EffectiveInterval": "CreateGroupMetricRuleValue",
+					"MailSubject":       "CreateGroupMetricRuleValue",
+					"Escalations": map[string]interface{}{
+						"Critical": map[string]interface{}{
+							"ComparisonOperator": "CreateGroupMetricRuleValue",
+							"Statistics":         "CreateGroupMetricRuleValue",
+							"Threshold":          "CreateGroupMetricRuleValue",
+							"Times":              1,
+						},
+						"Info": map[string]interface{}{
+							"ComparisonOperator": "CreateGroupMetricRuleValue",
+							"Statistics":         "CreateGroupMetricRuleValue",
+							"Threshold":          "CreateGroupMetricRuleValue",
+							"Times":              1,
+						},
+						"Warn": map[string]interface{}{
+							"ComparisonOperator": "CreateGroupMetricRuleValue",
+							"Statistics":         "CreateGroupMetricRuleValue",
+							"Threshold":          "CreateGroupMetricRuleValue",
+							"Times":              1,
+						},
+					},
+					"GroupId":             "CreateGroupMetricRuleValue",
+					"RuleName":            "CreateGroupMetricRuleValue",
+					"MetricName":          "CreateGroupMetricRuleValue",
+					"Namespace":           "CreateGroupMetricRuleValue",
+					"NoEffectiveInterval": "CreateGroupMetricRuleValue",
+					"Period":              1,
+					"SilenceTime":         10,
+					"AlertState":          "CreateGroupMetricRuleValue",
+					"Webhook":             "CreateGroupMetricRuleValue",
+					"RuleId":              "CreateGroupMetricRuleValue",
+				},
+			},
+		},
+		"Code":    200,
+		"Message": "Message",
+	}
+	CreateMockResponse := map[string]interface{}{
+		// PutGroupMetricRule
+		"Alarms": map[string]interface{}{
+			"Alarm": []interface{}{
+				map[string]interface{}{
+					"RuleId": "CreateGroupMetricRuleValue",
+				},
+			},
+		},
+		"Code":    200,
+		"Message": "Message",
+	}
+	ReadMockResponseDiff := map[string]interface{}{}
+	failedResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, &tea.SDKError{
+			Code:       String(errorCode),
+			Data:       String(errorCode),
+			Message:    String(errorCode),
+			StatusCode: tea.Int(400),
+		}
+	}
+	notFoundResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, GetNotFoundErrorFromString(GetNotFoundMessage("alicloud_cms_group_metric_rule", errorCode))
+	}
+	successResponseMock := func(operationMockResponse map[string]interface{}) (map[string]interface{}, error) {
+		if len(operationMockResponse) > 0 {
+			mapMerge(ReadMockResponse, operationMockResponse)
+		}
+		return ReadMockResponse, nil
+	}
+
+	// Create
+	t.Run("Create", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewCmsClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudCmsGroupMetricRuleCreate(dInit, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		ReadMockResponseDiff = map[string]interface{}{
+			// DescribeMetricRuleList Response
+			"Alarms": map[string]interface{}{
+				"Alarm": []interface{}{
+					map[string]interface{}{
+						"RuleId": "CreateGroupMetricRuleValue",
+					},
+				},
+			},
+			"Code":    200,
+			"Message": "Message",
+		}
+		errorCodes := []string{"NonRetryableError", "Throttling", "ExceedingQuota", "Throttling.User", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1 // a counter used to cover retry scenario; the same below
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "PutGroupMetricRule" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if retryIndex >= len(errorCodes)-1 {
+							successResponseMock(ReadMockResponseDiff)
+							return CreateMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCmsGroupMetricRuleCreate(dInit, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			default:
+				assert.Nil(t, err)
+				dCompare, _ := schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(dInit.State(), nil)
+				for key, value := range attributes {
+					_ = dCompare.Set(key, value)
+				}
+				assert.Equal(t, dCompare.State().Attributes, dInit.State().Attributes)
+			}
+			if retryIndex >= len(errorCodes)-1 {
+				break
+			}
+		}
+	})
+
+	// Update
+	t.Run("Update", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewCmsClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudCmsGroupMetricRuleUpdate(dExisted, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		// PutGroupMetricRule
+		attributesDiff := map[string]interface{}{
+			"group_id":               "PutGroupMetricRuleValue",
+			"group_metric_rule_name": "PutGroupMetricRuleValue",
+			"metric_name":            "PutGroupMetricRuleValue",
+			"namespace":              "PutGroupMetricRuleValue",
+			"contact_groups":         "PutGroupMetricRuleValue",
+			"dimensions":             "PutGroupMetricRuleValue",
+			"effective_interval":     "PutGroupMetricRuleValue",
+			"email_subject":          "PutGroupMetricRuleValue",
+			"escalations": []map[string]interface{}{
+				{
+					"critical": []map[string]interface{}{
+						{
+							"comparison_operator": "PutGroupMetricRuleValue",
+							"statistics":          "PutGroupMetricRuleValue",
+							"threshold":           "PutGroupMetricRuleValue",
+							"times":               2,
+						},
+					},
+					"info": []map[string]interface{}{
+						{
+							"comparison_operator": "PutGroupMetricRuleValue",
+							"statistics":          "PutGroupMetricRuleValue",
+							"threshold":           "PutGroupMetricRuleValue",
+							"times":               2,
+						},
+					},
+					"warn": []map[string]interface{}{
+						{
+							"comparison_operator": "PutGroupMetricRuleValue",
+							"statistics":          "PutGroupMetricRuleValue",
+							"threshold":           "PutGroupMetricRuleValue",
+							"times":               2,
+						},
+					},
+				},
+			},
+			"no_effective_interval": "PutGroupMetricRuleValue",
+			"period":                2,
+			"silence_time":          20,
+			"webhook":               "PutGroupMetricRuleValue",
+			"category":              "PutGroupMetricRuleValue",
+			"interval":              "PutGroupMetricRuleValue",
+		}
+		diff, err := newInstanceDiff("alicloud_cms_group_metric_rule", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(dInit.State(), diff)
+		ReadMockResponseDiff = map[string]interface{}{
+			// DescribeMetricRuleList Response
+			"Alarms": map[string]interface{}{
+				"Alarm": []interface{}{
+					map[string]interface{}{
+						"GroupId":           "PutGroupMetricRuleValue",
+						"RuleName":          "PutGroupMetricRuleValue",
+						"MetricName":        "PutGroupMetricRuleValue",
+						"Namespace":         "PutGroupMetricRuleValue",
+						"ContactGroups":     "PutGroupMetricRuleValue",
+						"Dimensions":        "PutGroupMetricRuleValue",
+						"EffectiveInterval": "PutGroupMetricRuleValue",
+						"MailSubject":       "PutGroupMetricRuleValue",
+						"Escalations": map[string]interface{}{
+							"Critical": map[string]interface{}{
+								"ComparisonOperator": "PutGroupMetricRuleValue",
+								"Statistics":         "PutGroupMetricRuleValue",
+								"Threshold":          "PutGroupMetricRuleValue",
+								"Times":              2,
+							},
+							"Info": map[string]interface{}{
+								"ComparisonOperator": "PutGroupMetricRuleValue",
+								"Statistics":         "PutGroupMetricRuleValue",
+								"Threshold":          "PutGroupMetricRuleValue",
+								"Times":              2,
+							},
+							"Warn": map[string]interface{}{
+								"ComparisonOperator": "PutGroupMetricRuleValue",
+								"Statistics":         "PutGroupMetricRuleValue",
+								"Threshold":          "PutGroupMetricRuleValue",
+								"Times":              2,
+							},
+						},
+						"NoEffectiveInterval": "PutGroupMetricRuleValue",
+						"Period":              2,
+						"SilenceTime":         20,
+						"Webhook":             "PutGroupMetricRuleValue",
+						"Category":            "PutGroupMetricRuleValue",
+						"Interval":            "PutGroupMetricRuleValue",
+					},
+				},
+			},
+			"Code":    200,
+			"Message": "Message",
+		}
+		errorCodes := []string{"NonRetryableError", "Throttling", "Throttling.User", "ExceedingQuota", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "PutGroupMetricRule" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if retryIndex >= len(errorCodes)-1 {
+							return successResponseMock(ReadMockResponseDiff)
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCmsGroupMetricRuleUpdate(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			default:
+				assert.Nil(t, err)
+				dCompare, _ := schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(dExisted.State(), nil)
+				for key, value := range attributes {
+					_ = dCompare.Set(key, value)
+				}
+				assert.Equal(t, dCompare.State().Attributes, dExisted.State().Attributes)
+			}
+			if retryIndex >= len(errorCodes)-1 {
+				break
+			}
+		}
+	})
+
+	// Read
+	t.Run("Read", func(t *testing.T) {
+		errorCodes := []string{"nil", "{}"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "DescribeMetricRuleList" {
+					switch errorCode {
+					case "{}":
+						return notFoundResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCmsGroupMetricRuleRead(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "{}":
+				assert.Nil(t, err)
+			}
+		}
+	})
+
+	// Delete
+	t.Run("Delete", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewCmsClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudCmsGroupMetricRuleDelete(dExisted, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		attributesDiff := map[string]interface{}{}
+		diff, err := newInstanceDiff("alicloud_cms_group_metric_rule", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_cms_group_metric_rule"].Schema).Data(dInit.State(), diff)
+		errorCodes := []string{"NonRetryableError", "Throttling", "ExceedingQuota", "Throttling.User", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "DeleteMetricRules" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							ReadMockResponse = map[string]interface{}{
+								"Code":    200,
+								"Message": "Message",
+							}
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCmsGroupMetricRuleDelete(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "nil":
+				assert.Nil(t, err)
+			}
+		}
+	})
 }
