b90049ef7c883983a2bb1283a84115fcbf3e78fe
super-eggs <gzy01137900@alibaba-inc.com>
2022-03-16 10:44:16 +0800

    resource/alicloud_mongodb_audit_policy:Fix test cases; supports timeouts setting.

--- alicloud/resource_alicloud_mongodb_audit_policy_test.go
+++ alicloud/resource_alicloud_mongodb_audit_policy_test.go
@@ -25,6 +25,7 @@ func TestAccAlicloudMongoDBAuditPolicy_basic0(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
+			testAccPreCheckEnterpriseAccountEnabled(t)
 		},
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -93,20 +94,12 @@ data "alicloud_vswitches" "default" {
   zone_id = data.alicloud_mongodb_zones.default.zones.0.id
 }
 
-resource "alicloud_vswitch" "vswitch" {
-  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 8)
-  zone_id           = data.alicloud_mongodb_zones.default.zones.0.id
-  vswitch_name      = "subnet-for-local-test"
-}
-
 resource "alicloud_mongodb_instance" "default" {
   engine_version      = "3.4"
   db_instance_class   = "dds.mongo.mid"
   db_instance_storage = 10
   name                = var.name
-  vswitch_id          = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids[0] : concat(alicloud_vswitch.vswitch.*.id, [""])[0]
+  vswitch_id          = data.alicloud_vswitches.default.ids[0]
   tags = {
     Created = "TF"
     For     = "acceptance test"
