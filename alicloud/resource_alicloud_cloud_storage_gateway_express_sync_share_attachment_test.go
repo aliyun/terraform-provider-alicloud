c72bf0e326a851d2fd71f332cfe9f79a06fddd95
nihonglei <nhl01089624@alibaba-inc.com>
2022-03-16 18:18:55 +0800

    testcase: Adds new unit test case for resource alicloud_cloud_storage_gateway_gateway_block_volume alicloud_cloud_storage_gateway_express_sync_share_attachment alicloud_cloud_storage_gateway_express_sync

--- alicloud/resource_alicloud_cloud_storage_gateway_express_sync_share_attachment_test.go
+++ alicloud/resource_alicloud_cloud_storage_gateway_express_sync_share_attachment_test.go
@@ -2,8 +2,18 @@ package alicloud
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"reflect"
 	"testing"
 
+	"github.com/agiledragon/gomonkey/v2"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/alibabacloud-go/tea-rpc/client"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -15,7 +25,7 @@ func TestAccAlicloudCloudStorageGatewayExpressSyncShareAttachment_basic0(t *test
 	ra := resourceAttrInit(resourceId, AlicloudCloudStorageGatewayExpressSyncShareAttachmentMap0)
 	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &SgwService{testAccProvider.Meta().(*connectivity.AliyunClient)}
-	}, "DescribeExpressSyncShares")
+	}, "DescribeExpressSyncShareshares")
 	rac := resourceAttrCheckInit(rc, ra)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := acctest.RandIntRange(10000, 99999)
@@ -139,4 +149,254 @@ resource "alicloud_cloud_storage_gateway_express_sync" "default" {
   express_sync_name = var.name
 }
 `, name, defaultRegionToTest)
+}
+
+func TestAccAlicloudCloudStorageGatewayExpressSyncShareAttachment_unit(t *testing.T) {
+	p := Provider().(*schema.Provider).ResourcesMap
+	dInit, _ := schema.InternalMap(p["alicloud_cloud_storage_gateway_express_sync_share_attachment"].Schema).Data(nil, nil)
+	dExisted, _ := schema.InternalMap(p["alicloud_cloud_storage_gateway_express_sync_share_attachment"].Schema).Data(nil, nil)
+	dInit.MarkNewResource()
+	attributes := map[string]interface{}{
+		"express_sync_id": "AddSharesToExpressSyncValue",
+		"gateway_id":      "AddSharesToExpressSyncValue",
+		"share_name":      "AddSharesToExpressSyncValue",
+	}
+	for key, value := range attributes {
+		err := dInit.Set(key, value)
+		assert.Nil(t, err)
+		err = dExisted.Set(key, value)
+		assert.Nil(t, err)
+		if err != nil {
+			log.Printf("[ERROR] the field %s setting error", key)
+		}
+	}
+	region := os.Getenv("ALICLOUD_REGION")
+	rawClient, err := sharedClientForRegion(region)
+	if err != nil {
+		t.Skipf("Skipping the test case with err: %s", err)
+		t.Skipped()
+	}
+	rawClient = rawClient.(*connectivity.AliyunClient)
+	ReadMockResponse := map[string]interface{}{
+		// DescribeExpressSyncShares
+		"Shares": map[string]interface{}{
+			"Share": []interface{}{
+				map[string]interface{}{
+					"GatewayId":     "AddSharesToExpressSyncValue",
+					"ShareName":     "AddSharesToExpressSyncValue",
+					"ExpressSyncId": "AddSharesToExpressSyncValue",
+				},
+			},
+		},
+		"Tasks": map[string]interface{}{
+			"SimpleTask": []interface{}{
+				map[string]interface{}{
+					"TaskId":    "AddSharesToExpressSyncValue",
+					"StateCode": "task.state.completed",
+				},
+			},
+		},
+		"TaskId":        "AddSharesToExpressSyncValue",
+		"ExpressSyncId": "AddSharesToExpressSyncValue",
+		"Success":       true,
+	}
+	CreateMockResponse := map[string]interface{}{
+		// AddSharesToExpressSync
+		"ExpressSyncs": map[string]interface{}{
+			"ExpressSync": []interface{}{
+				map[string]interface{}{
+					"ExpressSyncId": "AddSharesToExpressSyncValue",
+				},
+			},
+		},
+		"Tasks": map[string]interface{}{
+			"SimpleTask": []interface{}{
+				map[string]interface{}{
+					"TaskId":    "AddSharesToExpressSyncValue",
+					"StateCode": "task.state.completed",
+				},
+			},
+		},
+		"TaskId":        "AddSharesToExpressSyncValue",
+		"ExpressSyncId": "AddSharesToExpressSyncValue",
+		"Success":       true,
+	}
+	ReadMockResponseDiff := map[string]interface{}{}
+	failedResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, &tea.SDKError{
+			Code:       String(errorCode),
+			Data:       String(errorCode),
+			Message:    String(errorCode),
+			StatusCode: tea.Int(400),
+		}
+	}
+	notFoundResponseMock := func(errorCode string) (map[string]interface{}, error) {
+		return nil, GetNotFoundErrorFromString(GetNotFoundMessage("alicloud_cloud_storage_gateway_express_sync_share_attachment", errorCode))
+	}
+	successResponseMock := func(operationMockResponse map[string]interface{}) (map[string]interface{}, error) {
+		if len(operationMockResponse) > 0 {
+			mapMerge(ReadMockResponse, operationMockResponse)
+		}
+		return ReadMockResponse, nil
+	}
+
+	// Create
+	t.Run("Create", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewHcsSgwClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudCloudStorageGatewayExpressSyncShareAttachmentCreate(dInit, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		ReadMockResponseDiff = map[string]interface{}{
+			// DescribeExpressSyncShares Response
+			"ExpressSyncs": map[string]interface{}{
+				"ExpressSync": []interface{}{
+					map[string]interface{}{
+						"ExpressSyncId": "AddSharesToExpressSyncValue",
+					},
+				},
+			},
+			"ExpressSyncId": "AddSharesToExpressSyncValue",
+			"Success":       true,
+		}
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1 // a counter used to cover retry scenario; the same below
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "AddSharesToExpressSync" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if retryIndex >= len(errorCodes)-1 {
+							successResponseMock(ReadMockResponseDiff)
+							return CreateMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCloudStorageGatewayExpressSyncShareAttachmentCreate(dInit, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			default:
+				assert.Nil(t, err)
+				dCompare, _ := schema.InternalMap(p["alicloud_cloud_storage_gateway_express_sync_share_attachment"].Schema).Data(dInit.State(), nil)
+				for key, value := range attributes {
+					_ = dCompare.Set(key, value)
+				}
+				assert.Equal(t, dCompare.State().Attributes, dInit.State().Attributes)
+			}
+			if retryIndex >= len(errorCodes)-1 {
+				break
+			}
+		}
+	})
+
+	// Read
+	t.Run("Read", func(t *testing.T) {
+		attributesDiff := map[string]interface{}{}
+		diff, err := newInstanceDiff("alicloud_cloud_storage_gateway_express_sync_share_attachment", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_cloud_storage_gateway_express_sync_share_attachment"].Schema).Data(dInit.State(), diff)
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil", "{}"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "DescribeExpressSyncShares" {
+					switch errorCode {
+					case "{}":
+						return notFoundResponseMock(errorCode)
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCloudStorageGatewayExpressSyncShareAttachmentRead(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "{}":
+				assert.Nil(t, err)
+			}
+		}
+	})
+
+	// Delete
+	t.Run("Delete", func(t *testing.T) {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(&connectivity.AliyunClient{}), "NewHcsSgwClient", func(_ *connectivity.AliyunClient) (*client.Client, error) {
+			return nil, &tea.SDKError{
+				Code:    String("loadEndpoint error"),
+				Data:    String("loadEndpoint error"),
+				Message: String("loadEndpoint error"),
+			}
+		})
+		err := resourceAlicloudCloudStorageGatewayExpressSyncShareAttachmentDelete(dExisted, rawClient)
+		patches.Reset()
+		assert.NotNil(t, err)
+		attributesDiff := map[string]interface{}{}
+		diff, err := newInstanceDiff("alicloud_cloud_storage_gateway_express_sync_share_attachment", attributes, attributesDiff, dInit.State())
+		if err != nil {
+			t.Error(err)
+		}
+		dExisted, _ = schema.InternalMap(p["alicloud_cloud_storage_gateway_express_sync_share_attachment"].Schema).Data(dInit.State(), diff)
+		errorCodes := []string{"NonRetryableError", "Throttling", "nil", "ExpressSyncNotExist", "GatewayNotExist"}
+		for index, errorCode := range errorCodes {
+			retryIndex := index - 1
+			patches := gomonkey.ApplyMethod(reflect.TypeOf(&client.Client{}), "DoRequest", func(_ *client.Client, action *string, _ *string, _ *string, _ *string, _ *string, _ map[string]interface{}, _ map[string]interface{}, _ *util.RuntimeOptions) (map[string]interface{}, error) {
+				if *action == "RemoveSharesFromExpressSync" {
+					switch errorCode {
+					case "NonRetryableError":
+						return failedResponseMock(errorCode)
+					default:
+						retryIndex++
+						if errorCodes[retryIndex] == "nil" {
+							ReadMockResponse = map[string]interface{}{
+								"Tasks": map[string]interface{}{
+									"SimpleTask": []interface{}{
+										map[string]interface{}{
+											"TaskId":    "AddSharesToExpressSyncValue",
+											"StateCode": "task.state.completed",
+										},
+									},
+								},
+								"TaskId":  "AddSharesToExpressSyncValue",
+								"Success": true,
+							}
+							return ReadMockResponse, nil
+						}
+						return failedResponseMock(errorCodes[retryIndex])
+					}
+				}
+				return ReadMockResponse, nil
+			})
+			err := resourceAlicloudCloudStorageGatewayExpressSyncShareAttachmentDelete(dExisted, rawClient)
+			patches.Reset()
+			switch errorCode {
+			case "NonRetryableError":
+				assert.NotNil(t, err)
+			case "ExpressSyncNotExist", "GatewayNotExist":
+				assert.Nil(t, err)
+			}
+		}
+	})
 }
