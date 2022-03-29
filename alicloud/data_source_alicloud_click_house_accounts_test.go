44b61cfde5529d8aaf0ea90ce5a1d7748421ac24
super-eggs <gzy01137900@alibaba-inc.com>
2022-03-29 18:09:46 +0800

    resource/click_house_account: Fix test cases; supports timeouts setting. resource/click_house_db_cluster: Update optional values for `db_cluster_version` attributes.

--- alicloud/data_source_alicloud_click_house_accounts_test.go
+++ alicloud/data_source_alicloud_click_house_accounts_test.go
@@ -97,12 +97,16 @@ variable "pwd" {
   default = "Tf-test%d"
 }
 
+data "alicloud_click_house_regions" "default" {
+  current = true
+}
 data "alicloud_vpcs" "default" {
   name_regex = "default-NODELETING"
 }
 
 data "alicloud_vswitches" "default" {
-  vpc_id = "${data.alicloud_vpcs.default.ids.0}"
+  vpc_id = data.alicloud_vpcs.default.ids.0
+  zone_id = data.alicloud_click_house_regions.default.regions.0.zone_ids.0.zone_id
 }
 
 resource "alicloud_click_house_db_cluster" "default" {
@@ -111,11 +115,11 @@ resource "alicloud_click_house_db_cluster" "default" {
   db_cluster_class        = "S8"
   db_cluster_network_type = "vpc"
   db_cluster_description  = var.name
-  db_node_group_count     = "1"
+  db_node_group_count     = 1
   payment_type            = "PayAsYouGo"
-  db_node_storage         = "500"
+  db_node_storage         = "100"
   storage_type            = "cloud_essd"
-  vswitch_id              = "${data.alicloud_vswitches.default.vswitches.0.id}"
+  vswitch_id              = data.alicloud_vswitches.default.vswitches.0.id
 }
 
 resource "alicloud_click_house_account" "default" {
