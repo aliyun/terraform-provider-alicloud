5add16799df2c772d109b9f99f3cb5cc75fa01d7
super-eggs <gzy01137900@alibaba-inc.com>
2022-04-26 15:39:23 +0800

    New Resource `alicloud_ecs_network_interface_permission`; New Datasource `alicloud_ecs_network_interface_permissions`;

--- alicloud/service_alicloud_ecs.go
+++ alicloud/service_alicloud_ecs.go
@@ -2650,3 +2650,67 @@ func (s *EcsService) DescribeEcsImagePipeline(id string) (object map[string]inte
 	}
 	return v.([]interface{})[0].(map[string]interface{}), nil
 }
+
+func (s *EcsService) DescribeEcsNetworkInterfacePermission(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewEcsClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeNetworkInterfacePermissions"
+	request := map[string]interface{}{
+		"RegionId":                     s.client.RegionId,
+		"NetworkInterfacePermissionId": []string{id},
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2014-05-26"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.NetworkInterfacePermissions.NetworkInterfacePermission", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.NetworkInterfacePermissions.NetworkInterfacePermission", response)
+	}
+	if len(v.([]interface{})) < 1 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("ECS", id)), NotFoundWithResponse, response)
+	} else {
+		if fmt.Sprint(v.([]interface{})[0].(map[string]interface{})["NetworkInterfacePermissionId"]) != id {
+			return object, WrapErrorf(Error(GetNotFoundMessage("ECS", id)), NotFoundWithResponse, response)
+		}
+	}
+	object = v.([]interface{})[0].(map[string]interface{})
+	return object, nil
+}
+
+func (s *EcsService) EcsNetworkInterfacePermissionStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeEcsNetworkInterfacePermission(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["PermissionState"]) == failState {
+				return object, fmt.Sprint(object["PermissionState"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["PermissionState"])))
+			}
+		}
+		return object, fmt.Sprint(object["PermissionState"]), nil
+	}
+}
