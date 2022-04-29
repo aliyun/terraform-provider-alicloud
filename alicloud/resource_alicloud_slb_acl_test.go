eaf13b378657aea75f49f973d05f212840309c48
super-eggs <gzy01137900@alibaba-inc.com>
2022-04-29 15:49:53 +0800

    resource/alicloud_slb_acl: Fix bugs that cannot create ipv6.

--- alicloud/resource_alicloud_slb_acl_test.go
+++ alicloud/resource_alicloud_slb_acl_test.go
@@ -283,6 +283,67 @@ func TestAccAlicloudSLBAcl_basic1(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudSLBAcl_basic_ipv6(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_slb_acl.default"
+	ra := resourceAttrInit(resourceId, nil)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &SlbService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeSlbAcl")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAccSlbAcl%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudSLBAclBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":       "${var.name}",
+					"ip_version": "ipv6",
+					"entry_list": []map[string]interface{}{
+						{
+							"entry":   "1082:0:0:7::a/128",
+							"comment": "first",
+						},
+						{
+							"entry":   "1082:0:0:8::a/128",
+							"comment": "second",
+						},
+					},
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "acceptance test123",
+					},
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.0}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name":              name,
+						"ip_version":        "ipv6",
+						"entry_list.#":      "2",
+						"tags.%":            "2",
+						"tags.Created":      "TF",
+						"tags.For":          "acceptance test123",
+						"resource_group_id": CHECKSET,
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func AlicloudSLBAclBasicDependence0(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
