49a328bacfaa45b1f85fbd8ef50cfae640a1cd11
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2022-05-20 21:11:07 +0800

    New Resource:alicloud_ddos_basic_defense_threshold

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -4105,3 +4105,29 @@ func (client *AliyunClient) NewEssClient() (*rpc.Client, error) {
 
 	return conn, nil
 }
+
+func (client *AliyunClient) NewDdosbasicClient() (*rpc.Client, error) {
+	productCode := "ddosbasic"
+	endpoint := ""
+	if v, ok := client.config.Endpoints[productCode]; !ok || v.(string) == "" {
+		if err := client.loadEndpoint(productCode); err != nil {
+			endpoint = fmt.Sprintf("ddosbasic.%s.aliyuncs.com", client.config.RegionId)
+			client.config.Endpoints[productCode] = endpoint
+			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the endpoint %s instead.", productCode, err, endpoint)
+		}
+	}
+	if v, ok := client.config.Endpoints[productCode]; ok && v.(string) != "" {
+		endpoint = v.(string)
+	}
+	if endpoint == "" {
+		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
+	}
+
+	sdkConfig := client.teaSdkConfig
+	sdkConfig.SetEndpoint(endpoint)
+	conn, err := rpc.NewClient(&sdkConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
+	}
+	return conn, nil
+}
