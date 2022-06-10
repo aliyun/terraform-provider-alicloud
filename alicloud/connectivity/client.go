302d6518b2373ac8b887c47aad5ba7d3026ad83f
nihonglei <nhl01089624@alibaba-inc.com>
2022-06-10 17:59:41 +0800

    CHANGELOG

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -172,7 +172,7 @@ var loadSdkfromRemoteMutex = sync.Mutex{}
 var loadSdkEndpointMutex = sync.Mutex{}
 
 // The main version number that is being run at the moment.
-var providerVersion = "1.170.0"
+var providerVersion = "1.171.0"
 var terraformVersion = strings.TrimSuffix(schema.Provider{}.TerraformVersion, "-dev")
 
 // Temporarily maintain map for old ecs client methods and store special endpoint information
