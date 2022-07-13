8a545ce8f48e35ab6e6d1d26a987cc1b978ba117
super-eggs <gzy01137900@alibaba-inc.com>
2022-07-13 18:22:00 +0800

    New Resource `alicloud_hbr_hana_backup_plan`; New Datasource `alicloud_hbr_hana_backup_plans`;

---
+++ alicloud/data_source_alicloud_hbr_hana_backup_plans.go
@@ -0,0 +1,242 @@
+package alicloud
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAlicloudHbrHanaBackupPlans() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAlicloudHbrHanaBackupPlansRead,
+		Schema: map[string]*schema.Schema{
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"database_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name_regex": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.ValidateRegexp,
+				ForceNew:     true,
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"vault_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"page_number": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"page_size": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  50,
+			},
+			"plans": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"backup_prefix": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"backup_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cluster_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"database_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"page_total": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"plan_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"plan_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"schedule": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vault_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAlicloudHbrHanaBackupPlansRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "DescribeHanaBackupPlans"
+	request := make(map[string]interface{})
+	if v, ok := d.GetOk("database_name"); ok {
+		request["DatabaseName"] = v
+	}
+	if v, ok := d.GetOk("vault_id"); ok {
+		request["VaultId"] = v
+	}
+	request["ClusterId"] = d.Get("cluster_id")
+	setPagingRequest(d, request, PageSizeLarge)
+	var objects []map[string]interface{}
+
+	idsMap := make(map[string]string)
+	if v, ok := d.GetOk("ids"); ok {
+		for _, vv := range v.([]interface{}) {
+			if vv == nil {
+				continue
+			}
+			idsMap[vv.(string)] = vv.(string)
+		}
+	}
+	var response map[string]interface{}
+	var hbrHanaBackupPlanNameRegex *regexp.Regexp
+	if v, ok := d.GetOk("name_regex"); ok {
+		r, err := regexp.Compile(v.(string))
+		if err != nil {
+			return WrapError(err)
+		}
+		hbrHanaBackupPlanNameRegex = r
+	}
+	conn, err := client.NewHbrClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	for {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-08"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_hbr_hana_backup_plans", action, AlibabaCloudSdkGoERROR)
+		}
+		if fmt.Sprint(response["Success"]) == "false" {
+			return WrapError(fmt.Errorf("%s failed, response: %v", action, response))
+		}
+		resp, err := jsonpath.Get("$.HanaBackupPlans.HanaBackupPlan", response)
+		if err != nil {
+			return WrapErrorf(err, FailedGetAttributeMsg, action, "$.HanaBackupPlans.HanaBackupPlan", response)
+		}
+		result, _ := resp.([]interface{})
+		for _, v := range result {
+			item := v.(map[string]interface{})
+			if hbrHanaBackupPlanNameRegex != nil && !hbrHanaBackupPlanNameRegex.MatchString(fmt.Sprint(item["PlanName"])) {
+				continue
+			}
+			if len(idsMap) > 0 {
+				if _, ok := idsMap[fmt.Sprint(item["PlanId"], ":", item["VaultId"], ":", item["ClusterId"])]; !ok {
+					continue
+				}
+			}
+			objects = append(objects, item)
+		}
+		if len(result) < request["PageSize"].(int) {
+			break
+		}
+		request["PageNumber"] = request["PageNumber"].(int) + 1
+	}
+	ids := make([]string, 0)
+	names := make([]interface{}, 0)
+	s := make([]map[string]interface{}, 0)
+	for _, object := range objects {
+		mapping := map[string]interface{}{
+			"backup_prefix": object["BackupPrefix"],
+			"backup_type":   object["BackupType"],
+			"id":            fmt.Sprint(object["PlanId"], ":", object["VaultId"], ":", object["ClusterId"]),
+			"cluster_id":    fmt.Sprint(object["ClusterId"]),
+			"database_name": object["DatabaseName"],
+			"plan_id":       object["PlanId"],
+			"plan_name":     object["PlanName"],
+			"schedule":      object["Schedule"],
+			"vault_id":      object["VaultId"],
+		}
+		mapping["status"] = convertStatusByDisabled(object["Disabled"].(bool))
+		ids = append(ids, fmt.Sprint(mapping["id"]))
+		names = append(names, object["PlanName"])
+		s = append(s, mapping)
+	}
+
+	d.SetId(dataResourceIdHash(ids))
+	if err := d.Set("ids", ids); err != nil {
+		return WrapError(err)
+	}
+
+	if err := d.Set("names", names); err != nil {
+		return WrapError(err)
+	}
+
+	if err := d.Set("plans", s); err != nil {
+		return WrapError(err)
+	}
+	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
+		writeToFile(output.(string), s)
+	}
+
+	return nil
+}
--- alicloud/service_alicloud_hbr.go
+++ alicloud/service_alicloud_hbr.go
@@ -742,3 +742,67 @@ func (s *HbrService) DescribeHbrHanaInstance(id string) (object map[string]inter
 	object = v.([]interface{})[0].(map[string]interface{})
 	return object, nil
 }
+func (s *HbrService) DescribeHbrHanaBackupPlan(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewHbrClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeHanaBackupPlans"
+	parts, err := ParseResourceId(id, 3)
+	if err != nil {
+		err = WrapError(err)
+		return
+	}
+	request := map[string]interface{}{
+		"ClusterId":  parts[2],
+		"VaultId":    parts[1],
+		"PageNumber": 1,
+		"PageSize":   PageSizeLarge,
+	}
+	idExist := false
+	for {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-08"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+		}
+		if fmt.Sprint(response["Success"]) == "false" {
+			return object, WrapError(fmt.Errorf("%s failed, response: %v", action, response))
+		}
+		v, err := jsonpath.Get("$.HanaBackupPlans.HanaBackupPlan", response)
+		if err != nil {
+			return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.HanaBackupPlans.HanaBackupPlan", response)
+		}
+		if len(v.([]interface{})) < 1 {
+			return object, WrapErrorf(Error(GetNotFoundMessage("HBR", id)), NotFoundWithResponse, response)
+		}
+		for _, v := range v.([]interface{}) {
+			if fmt.Sprint(v.(map[string]interface{})["PlanId"]) == parts[0] {
+				idExist = true
+				return v.(map[string]interface{}), nil
+			}
+		}
+		if len(v.([]interface{})) < request["PageSize"].(int) {
+			break
+		}
+		request["PageNumber"] = request["PageNumber"].(int) + 1
+	}
+	if !idExist {
+		return object, WrapErrorf(Error(GetNotFoundMessage("HBR", id)), NotFoundWithResponse, response)
+	}
+	return
+}
