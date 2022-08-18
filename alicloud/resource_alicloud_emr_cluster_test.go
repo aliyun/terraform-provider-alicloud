bedc26ad8a0acde02e1557aa463734c718597be5
qiyinfei <qiyf_shadow@yeah.net>
2022-08-18 17:08:10 +0800

    resource/alicloud_emr_cluster: fix test case testAccAlicloudEmrCluster_basic

--- alicloud/resource_alicloud_emr_cluster_test.go
+++ alicloud/resource_alicloud_emr_cluster_test.go
@@ -183,7 +183,7 @@ func TestAccAlicloudEmrCluster_basic(t *testing.T) {
 				ResourceName:            resourceId,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"ssh_enable", "master_pwd", "is_open_public_ip", "host_group", "resource_group_id", "use_local_metadb"},
+				ImportStateVerifyIgnore: []string{"ssh_enable", "master_pwd", "is_open_public_ip", "host_group", "resource_group_id", "use_local_metadb", "meta_store_type"},
 			},
 		},
 	})
