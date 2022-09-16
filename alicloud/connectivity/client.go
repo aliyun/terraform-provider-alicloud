5774c8c9d50b0890493a4cb677c715d12ee55981
super-eggs <gzy01137900@alibaba-inc.com>
2022-09-16 16:21:46 +0800

    New Resource `alicloud_vpc_peer_connection`; New Datasource `alicloud_vpc_peer_connections`;

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -4422,6 +4422,31 @@ func (client *AliyunClient) NewApigatewayClient() (*rpc.Client, error) {
 	return conn, nil
 }
 
+func (client *AliyunClient) NewVpcpeerClient() (*rpc.Client, error) {
+	productCode := "vpcpeer"
+	endpoint := ""
+	if v, ok := client.config.Endpoints[productCode]; !ok || v.(string) == "" {
+		if err := client.loadEndpoint(productCode); err != nil {
+			endpoint = "vpcpeer-openapi-share.aliyuncs.com"
+			client.config.Endpoints[productCode] = endpoint
+			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the endpoint %s instead.", productCode, err, endpoint)
+		}
+	}
+	if v, ok := client.config.Endpoints[productCode]; ok && v.(string) != "" {
+		endpoint = v.(string)
+	}
+	if endpoint == "" {
+		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
+	}
+	sdkConfig := client.teaSdkConfig
+	sdkConfig.SetEndpoint(endpoint)
+	conn, err := rpc.NewClient(&sdkConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
+	}
+	return conn, nil
+}
+
 func (client *AliyunClient) NewCbsClient() (*rpc.Client, error) {
 	productCode := "cbs"
 	endpoint := ""
