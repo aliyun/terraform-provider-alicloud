8a7dce9308a7d6ab39235a2fa311a648cebb0a67
新木 <ll221756@alibaba-inc.com>
2022-09-26 16:08:24 +0800

    New Resource: `alicloud_cen_transit_router_grant_attachment`

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -1395,6 +1395,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_nlb_security_policy":                                  resourceAlicloudNlbSecurityPolicy(),
 			"alicloud_vod_editing_project":                                  resourceAlicloudVodEditingProject(),
 			"alicloud_api_gateway_model":                                    resourceAlicloudApiGatewayModel(),
+			"alicloud_cen_transit_router_grant_attachment":                  resourceAlicloudCenTransitRouterGrantAttachment(),
 		},
 
 		ConfigureFunc: providerConfigure,
