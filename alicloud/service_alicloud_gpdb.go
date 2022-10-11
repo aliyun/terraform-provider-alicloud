7fce3c39252ec1b160321e539f4fdb9bd99900f9
nihonglei <nhl01089624@alibaba-inc.com>
2022-10-11 15:38:35 +0800

    resource/alicloud_gpdb_instance: Adds new attribute ssl_enabled

--- alicloud/service_alicloud_gpdb.go
+++ alicloud/service_alicloud_gpdb.go
@@ -611,6 +611,46 @@ func (s *GpdbService) DescribeDBInstanceIPArrayList(id string) (object map[strin
 	return object, nil
 }
 
+func (s *GpdbService) DescribeDBInstanceSSL(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	action := "DescribeDBInstanceSSL"
+	request := map[string]interface{}{
+		"RegionId":     s.client.RegionId,
+		"DBInstanceId": id,
+	}
+	conn, err := s.client.NewGpdbClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-05-03"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InvalidDBInstanceId.NotFound"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("GPDB:DBInstance", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+}
+
 func (s *GpdbService) DescribeGpdbDbInstance(id string) (object map[string]interface{}, err error) {
 	var response map[string]interface{}
 	conn, err := s.client.NewGpdbClient()
