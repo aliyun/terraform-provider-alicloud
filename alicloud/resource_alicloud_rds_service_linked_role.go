9325778d0a825e0ab22fd56b854c201960db3ac6
zcc01029273 <zcc01029273@alibaba-inc.com>
2022-10-13 09:37:59 +0800

    New Resource `alicloud_rds_service_linked_role`
    New Resource `alicloud_rds_service_linked_role`
    
    New Resource `alicloud_rds_service_linked_role`
    
    New Resource `alicloud_rds_service_linked_role`
    
    New Resource `alicloud_rds_service_linked_role`

---
+++ alicloud/resource_alicloud_rds_service_linked_role.go
@@ -0,0 +1,128 @@
+package alicloud
+
+import (
+	"fmt"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func resourceAlicloudRdsServiceLinkedRole() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudRdsServiceLinkedRoleCreate,
+		Read:   resourceAlicloudRdsServiceLinkedRoleRead,
+		Delete: resourceAlicloudRdsServiceLinkedRoleDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"service_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"AliyunServiceRoleForRdsPgsqlOnEcs",
+					"AliyunServiceRoleForRDSProxyOnEcs",
+				}, false),
+			},
+			"role_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"role_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"arn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAlicloudRdsServiceLinkedRoleCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var response map[string]interface{}
+	action := "CreateServiceLinkedRole"
+	request := make(map[string]interface{})
+	conn, err := client.NewRdsClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request["ServiceLinkedRole"] = d.Get("service_name")
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2014-08-15"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_rds_service_linked_role", action, AlibabaCloudSdkGoERROR)
+	}
+	d.SetId(fmt.Sprint(request["ServiceLinkedRole"]))
+	return resourceAlicloudRdsServiceLinkedRoleRead(d, meta)
+}
+func resourceAlicloudRdsServiceLinkedRoleRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	rdsService := RdsService{client}
+	object, err := rdsService.DescribeRdsServiceLinkedRole(d.Id())
+	if err != nil {
+		if NotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+	d.Set("role_id", object.Role.RoleId)
+	d.Set("role_name", object.Role.RoleName)
+	d.Set("arn", object.Role.Arn)
+	d.Set("service_name", d.Id())
+	return nil
+
+}
+
+func resourceAlicloudRdsServiceLinkedRoleDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var response map[string]interface{}
+	action := "DeleteServiceLinkedRole"
+	request := make(map[string]interface{})
+	conn, err := client.NewResourcemanagerClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request["RoleName"] = d.Id()
+
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-03-31"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_rds_service_linked_role", action, AlibabaCloudSdkGoERROR)
+	}
+
+	return nil
+}
--- alicloud/service_alicloud_rds.go
+++ alicloud/service_alicloud_rds.go
@@ -12,6 +12,7 @@ import (
 	"github.com/PaesslerAG/jsonpath"
 	util "github.com/alibabacloud-go/tea-utils/service"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ram"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/denverdino/aliyungo/common"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -2272,4 +2273,33 @@ func (s *RdsService) RdsUpgradeMajorVersionRefreshFunc(id string, taskId int, fa
 		}
 		return object, object["Result"].(string), nil
 	}
+}
+
+func (s *RdsService) DescribeRdsServiceLinkedRole(id string) (*ram.GetRoleResponse, error) {
+	response := &ram.GetRoleResponse{}
+	request := ram.CreateGetRoleRequest()
+	request.RegionId = s.client.RegionId
+	request.RoleName = id
+	err := resource.Retry(5*time.Minute, func() *resource.RetryError {
+		raw, err := s.client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+			return ramClient.GetRole(request)
+		})
+		if err != nil {
+			if IsExpectedErrors(err, []string{ThrottlingUser}) {
+				time.Sleep(2 * time.Second)
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+		response, _ = raw.(*ram.GetRoleResponse)
+		return nil
+	})
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EntityNotExist.Role"}) {
+			return response, WrapErrorf(err, NotFoundMsg, AlibabaCloudSdkGoERROR)
+		}
+		return response, WrapErrorf(err, DefaultErrorMsg, id, request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+	return response, nil
 }
