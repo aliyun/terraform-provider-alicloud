5c8acf51da263370186410a9e84858ac57fe75db
MrWolong <ly01269865@alibaba-inc.com>
2022-11-07 18:20:40 +0800

    resource/alicloud_ga_bandwidth_package_attachment: Removed the ForceNew for field `bandwidth_package_id`, supports modifying them online; Supported for new action `ReplaceBandwidthPackage`

--- alicloud/resource_alicloud_ga_bandwidth_package_attachment.go
+++ alicloud/resource_alicloud_ga_bandwidth_package_attachment.go
@@ -16,18 +16,24 @@ func resourceAlicloudGaBandwidthPackageAttachment() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAlicloudGaBandwidthPackageAttachmentCreate,
 		Read:   resourceAlicloudGaBandwidthPackageAttachmentRead,
+		Update: resourceAlicloudGaBandwidthPackageAttachmentUpdate,
 		Delete: resourceAlicloudGaBandwidthPackageAttachmentDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
 			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"accelerator_id": {
 				Type:     schema.TypeString,
+				Required: true,
 				ForceNew: true,
+			},
+			"bandwidth_package_id": {
+				Type:     schema.TypeString,
 				Required: true,
 			},
 			"accelerators": {
@@ -36,11 +42,6 @@ func resourceAlicloudGaBandwidthPackageAttachment() *schema.Resource {
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
-			},
-			"bandwidth_package_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
 			},
 			"status": {
 				Type:     schema.TypeString,
@@ -60,6 +61,7 @@ func resourceAlicloudGaBandwidthPackageAttachmentCreate(d *schema.ResourceData, 
 	if err != nil {
 		return WrapError(err)
 	}
+
 	request["AcceleratorId"] = d.Get("accelerator_id")
 	request["BandwidthPackageId"] = d.Get("bandwidth_package_id")
 	request["RegionId"] = client.RegionId
@@ -73,9 +75,10 @@ func resourceAlicloudGaBandwidthPackageAttachmentCreate(d *schema.ResourceData, 
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ga_bandwidth_package_attachment", action, AlibabaCloudSdkGoERROR)
 	}
@@ -117,6 +120,66 @@ func resourceAlicloudGaBandwidthPackageAttachmentRead(d *schema.ResourceData, me
 	return nil
 }
 
+func resourceAlicloudGaBandwidthPackageAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	var response map[string]interface{}
+	update := false
+	parts, err := ParseResourceId(d.Id(), 2)
+	if err != nil {
+		return WrapError(err)
+	}
+	request := map[string]interface{}{
+		"RegionId":      client.RegionId,
+		"AcceleratorId": parts[0],
+	}
+
+	if d.HasChange("bandwidth_package_id") {
+		update = true
+	}
+	oldValue, newValue := d.GetChange("bandwidth_package_id")
+	oldBandwidthPackageId := oldValue.(string)
+	newBandwidthPackageId := newValue.(string)
+	request["BandwidthPackageId"] = newBandwidthPackageId
+	request["TargetBandwidthPackageId"] = oldBandwidthPackageId
+
+	if update {
+		action := "ReplaceBandwidthPackage"
+		conn, err := client.NewGaplusClient()
+		if err != nil {
+			return WrapError(err)
+		}
+
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+			if err != nil {
+				if IsExpectedErrors(err, []string{"StateError.BandwidthPackage", "StateError.Accelerator", "GreaterThanGa.IpSetBandwidth", "BandwidthIllegal.BandwidthPackage", "NotExist.BasicBandwidthPackage"}) || NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+
+		d.SetId(fmt.Sprintf("%v:%v", parts[0], request["BandwidthPackageId"]))
+
+		stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 5*time.Second, gaService.GaBandwidthPackageAttachmentStateRefreshFunc(d.Id(), []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+
+	}
+
+	return resourceAlicloudGaBandwidthPackageAttachmentRead(d, meta)
+}
+
 func resourceAlicloudGaBandwidthPackageAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	gaService := GaService{client}
@@ -149,18 +212,21 @@ func resourceAlicloudGaBandwidthPackageAttachmentDelete(d *schema.ResourceData, 
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		if IsExpectedErrors(err, []string{"NotExist.BandwidthPackage", "Exist.EndpointGroup"}) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, gaService.GaBandwidthPackageAttachmentStateRefreshFunc(d.Id(), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	return nil
 }
