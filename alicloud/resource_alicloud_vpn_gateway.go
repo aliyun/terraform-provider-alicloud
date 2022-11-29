e81ba6b31a75f18d1e2d2b89e6d56e0b26cb3ad5
nihonglei <nhl01089624@alibaba-inc.com>
2022-11-29 15:23:52 +0800

    resource/alicloud_vpn_gateway: Added retry stragety for error code;

--- alicloud/resource_alicloud_vpn_gateway.go
+++ alicloud/resource_alicloud_vpn_gateway.go
@@ -381,7 +381,7 @@ func resourceAliyunVpnGatewayDelete(d *schema.ResourceData, meta interface{}) er
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-04-28"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"VpnGateway.Configuring"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
