1d892bee2078f1482ee665d4122e324421af637e
MrWolong <ly01269865@alibaba-inc.com>
2022-12-21 22:01:25 +0800

    New Datasource `alicloud_threat_detection_log_shipper`

--- alicloud/service_alicloud_threat_detection.go
+++ alicloud/service_alicloud_threat_detection.go
@@ -191,3 +191,46 @@ func (s *ThreatDetectionService) ThreatDetectionHoneypotNodeStateRefreshFunc(d *
 		return object, fmt.Sprint(object["TotalStatus"]), nil
 	}
 }
+
+func (s *ThreatDetectionService) DescribeThreatDetectionLogShipper(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	action := "DescribeLogShipperStatus"
+
+	conn, err := s.client.NewThreatdetectionClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"From": "sas",
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2018-12-03"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.LogShipperStatus", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.LogShipperStatus", response)
+	}
+
+	object = v.(map[string]interface{})
+
+	return object, nil
+}
