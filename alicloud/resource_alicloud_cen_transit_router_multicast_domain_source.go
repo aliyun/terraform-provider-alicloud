5b983ead4a562784d922954cade17c1c3a63c005
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2022-12-29 22:02:13 +0800

    New resource alicloud_cen_transit_router_multicast_domain_source and its datasource.

---
+++ alicloud/resource_alicloud_cen_transit_router_multicast_domain_source.go
@@ -0,0 +1,178 @@
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAlicloudCenTransitRouterMulticastDomainSource() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudCenTransitRouterMulticastDomainSourceCreate,
+		Read:   resourceAlicloudCenTransitRouterMulticastDomainSourceRead,
+		Delete: resourceAlicloudCenTransitRouterMulticastDomainSourceDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(1 * time.Minute),
+			Delete: schema.DefaultTimeout(1 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"transit_router_multicast_domain_id": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"group_ip_address": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"network_interface_id": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"vpc_id": {
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"status": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+		},
+	}
+}
+
+func resourceAlicloudCenTransitRouterMulticastDomainSourceCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	cbnService := CbnService{client}
+	request := make(map[string]interface{})
+	conn, err := client.NewCbnClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	if v, ok := d.GetOk("group_ip_address"); ok {
+		request["GroupIpAddress"] = v
+	}
+	if v, ok := d.GetOk("network_interface_id"); ok {
+		request["NetworkInterfaceIds.1"] = v
+	}
+	if v, ok := d.GetOk("transit_router_multicast_domain_id"); ok {
+		request["TransitRouterMulticastDomainId"] = v
+	}
+
+	request["ClientToken"] = buildClientToken("RegisterTransitRouterMulticastGroupSources")
+	var response map[string]interface{}
+	action := "RegisterTransitRouterMulticastGroupSources"
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
+		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-12"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		response = resp
+		addDebug(action, response, request)
+		return nil
+	})
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_cen_transit_router_multicast_domain_source", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprint(request["TransitRouterMulticastDomainId"], ":", request["GroupIpAddress"], ":", request["NetworkInterfaceIds.1"]))
+	stateConf := BuildStateConf([]string{}, []string{"Registered"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, cbnService.CenTransitRouterMulticastDomainSourceStateRefreshFunc(d, []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return resourceAlicloudCenTransitRouterMulticastDomainSourceRead(d, meta)
+}
+
+func resourceAlicloudCenTransitRouterMulticastDomainSourceRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	cbnService := CbnService{client}
+
+	object, err := cbnService.DescribeCenTransitRouterMulticastDomainSource(d.Id())
+	if err != nil {
+		if NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_cen_transit_router_multicast_domain_source cbnService.DescribeCenTransitRouterMulticastDomainSource Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+	d.Set("transit_router_multicast_domain_id", parts[0])
+	d.Set("group_ip_address", parts[1])
+	d.Set("network_interface_id", parts[2])
+	d.Set("status", object["Status"])
+	d.Set("vpc_id", object["ResourceId"])
+
+	return nil
+}
+
+func resourceAlicloudCenTransitRouterMulticastDomainSourceDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	cbnService := CbnService{client}
+	conn, err := client.NewCbnClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"TransitRouterMulticastDomainId": parts[0],
+		"GroupIpAddress":                 parts[1],
+		"NetworkInterfaceIds.1":          parts[2],
+	}
+
+	request["ClientToken"] = buildClientToken("DeregisterTransitRouterMulticastGroupSources")
+	action := "DeregisterTransitRouterMulticastGroupSources"
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
+		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-12"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, resp, request)
+		return nil
+	})
+	if err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, cbnService.CenTransitRouterMulticastDomainSourceStateRefreshFunc(d, []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return nil
+}
---
+++ website/docs/d/cen_transit_router_multicast_domain_sources.html.markdown
@@ -0,0 +1,43 @@
+---
+subcategory: "Cen"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_cen_transit_router_multicast_domain_sources"
+sidebar_current: "docs-alicloud-datasource-cen-transit-router-multicast-domain-sources"
+description: |-
+  Provides a list of Cen Transit Router Multicast Domain Source owned by an Alibaba Cloud account.
+---
+
+# alicloud_cen_transit_router_multicast_domain_sources
+
+This data source provides Cen Transit Router Multicast Domain Source available to the user.[What is Transit Router Multicast Domain Source](https://www.alibabacloud.com/help/en/)
+
+-> **NOTE:** Available in 1.195.0+
+
+## Example Usage
+
+```
+data "alicloud_cen_transit_router_multicast_domain_sources" "default" {
+  transit_router_multicast_domain_id = "tr-mcast-domain-xxxxxx"
+}
+
+output "alicloud_cen_transit_router_multicast_domain_source_example_id" {
+  value = data.alicloud_cen_transit_router_multicast_domain_sources.default.sources.0.id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `transit_router_multicast_domain_id` - (Required,ForceNew) The ID of the multicast domain to which the multicast source belongs.
+* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
+
+
+## Attributes Reference
+
+The following attributes are exported in addition to the arguments listed above:
+* `sources` - A list of Transit Router Multicast Domain Source Entries. Each element contains the following attributes:
+  * `transit_router_multicast_domain_id` - The ID of the multicast domain to which the multicast source belongs.
+  * `group_ip_address` - The IP address of the multicast group to which the multicast source belongs. Value range: **224.0.0.1** to **239.255.255.254**. If the multicast group you specified does not exist in the current multicast domain, the system will automatically create a new multicast group for you.
+  * `network_interface_id` - ENI ID of the multicast source.
+  * `vpc_id` - The VPC to which the ENI of the multicast source belongs. This field is mandatory for VPCs that is owned by another accounts.
+  * `status` - The status of the resource.
