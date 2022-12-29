b373f86df476eaa76773aca62c662d92a5222560
super-eggs <gzy01137900@alibaba-inc.com>
2022-12-29 14:39:43 +0800

    resource/alicloud_threat_detection_backup_policy: fix test case TestAccAlicloudThreatDetectionBackupPolicy_basic0 resource/alicloud_threat_detection_web_lock_config: fix test case TestAccAlicloudThreatDetectionWebLockConfig_basic1875

--- alicloud/data_source_alicloud_threat_detection_backup_policies_test.go
+++ alicloud/data_source_alicloud_threat_detection_backup_policies_test.go
@@ -111,11 +111,15 @@ func testAccCheckAlicloudThreatDetectionBackupPoliciesDataSourceName(rand int, a
   		default = "tf-testAccThreatDetectionBackupPolicy-%d"
 	}
 
+	data "alicloud_threat_detection_assets" "default" {
+	  machine_types = "ecs"
+	}
+
 	resource "alicloud_threat_detection_backup_policy" "default" {
   		backup_policy_name = var.name
   		policy             = "{\"Exclude\":[\"/bin/\",\"/usr/bin/\",\"/sbin/\",\"/boot/\",\"/proc/\",\"/sys/\",\"/srv/\",\"/lib/\",\"/selinux/\",\"/usr/sbin/\",\"/run/\",\"/lib32/\",\"/lib64/\",\"/lost+found/\",\"/var/lib/kubelet/\",\"/var/lib/ntp/proc\",\"/var/lib/container\"],\"ExcludeSystemPath\":true,\"Include\":[],\"IsDefault\":1,\"Retention\":7,\"Schedule\":\"I|1668703620|PT24H\",\"Source\":[],\"SpeedLimiter\":\"\",\"UseVss\":true}"
   		policy_version     = "2.0.0"
-  		uuid_list          = ["0fcb3feb-e7fd-4a41-b9ae-69c56c6f402d"]
+  		uuid_list          = [data.alicloud_threat_detection_assets.default.ids.0]
 	}
 
 	data "alicloud_threat_detection_backup_policies" "default" {
