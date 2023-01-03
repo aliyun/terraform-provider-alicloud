803c366357493ad0a46e77bc29d39cd14e696445
super-eggs <gzy01137900@alibaba-inc.com>
2023-01-03 18:12:26 +0800

    New Resource `alicloud_express_connect_vbr_pconn_association`; New Datasource `alicloud_express_connect_vbr_pconn_associations`;

--- alicloud/service_alicloud_express_connect.go
+++ alicloud/service_alicloud_express_connect.go
@@ -7,10 +7,86 @@ import (
 	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+	"fmt"
 )
 
 type ExpressConnectService struct {
 	client *connectivity.AliyunClient
+}
+
+func (s *ExpressConnectService) DescribeExpressConnectVbrPconnAssociation(id string) (object map[string]interface{}, err error) {
+	conn, err := s.client.NewVpcClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	parts, err := ParseResourceId(id, 2)
+	if err != nil {
+		return object, WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"Filter.1.Key":     "VbrId",
+		"Filter.1.Value.1": parts[0],
+		"RegionId":         s.client.RegionId,
+	}
+
+	var response map[string]interface{}
+	action := "DescribeVirtualBorderRouters"
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-04-28"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		response = resp
+		addDebug(action, response, request)
+		return nil
+	})
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$.VirtualBorderRouterSet.VirtualBorderRouterType[*].AssociatedPhysicalConnections.AssociatedPhysicalConnection[*]", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.VirtualBorderRouterSet.VirtualBorderRouterType[*].AssociatedPhysicalConnections.AssociatedPhysicalConnection[*]", response)
+	}
+	if len(v.([]interface{})) < 1 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("VbrPconnAssociation", id)), NotFoundWithResponse, response)
+	} else {
+		for _, vv := range v.([]interface{}) {
+			item := vv.(map[string]interface{})
+
+			if fmt.Sprint(item["PhysicalConnectionId"]) == parts[1] {
+				return item, nil
+			}
+		}
+	}
+	return object, WrapErrorf(Error(GetNotFoundMessage("VbrPconnAssociation", id)), NotFoundWithResponse, response)
+}
+
+func (s *ExpressConnectService) ExpressConnectVbrPconnAssociationStateRefreshFunc(d *schema.ResourceData, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeExpressConnectVbrPconnAssociation(d.Id())
+		if err != nil {
+			if NotFoundError(err) {
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		for _, failState := range failStates {
+			if fmt.Sprint(object["Status"]) == failState {
+				return object, fmt.Sprint(object["Status"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["Status"])))
+			}
+		}
+		return object, fmt.Sprint(object["Status"]), nil
+	}
 }
 
 func (s *ExpressConnectService) DescribeExpressConnectVirtualPhysicalConnection(id string) (object map[string]interface{}, err error) {
