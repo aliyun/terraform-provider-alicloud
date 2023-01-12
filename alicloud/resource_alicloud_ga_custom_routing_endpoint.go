6086d9e3adf6a85c31f7bb9b874fa39ad994963d
MrWolong <ly01269865@alibaba-inc.com>
2023-01-12 18:56:11 +0800

    New Resource `alicloud_ga_custom_routing_endpoint`; New Datasource `alicloud_ga_custom_routing_endpoints`

---
+++ alicloud/resource_alicloud_ga_custom_routing_endpoint.go
@@ -0,0 +1,278 @@
+package alicloud
+
+import (
+	"fmt"
+	"github.com/PaesslerAG/jsonpath"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAlicloudGaCustomRoutingEndpoint() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudGaCustomRoutingEndpointCreate,
+		Read:   resourceAlicloudGaCustomRoutingEndpointRead,
+		Update: resourceAlicloudGaCustomRoutingEndpointUpdate,
+		Delete: resourceAlicloudGaCustomRoutingEndpointDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"endpoint_group_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"endpoint": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"type": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringInSlice([]string{"PrivateSubNet"}, false),
+			},
+			"traffic_to_endpoint_policy": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.StringInSlice([]string{"DenyAll", "AllowAll", "AllowCustom"}, false),
+			},
+			"accelerator_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"listener_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"custom_routing_endpoint_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAlicloudGaCustomRoutingEndpointCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	var response map[string]interface{}
+	action := "CreateCustomRoutingEndpoints"
+	request := make(map[string]interface{})
+	conn, err := client.NewGaplusClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request["RegionId"] = client.RegionId
+	request["ClientToken"] = buildClientToken("CreateCustomRoutingEndpoints")
+	request["EndpointGroupId"] = d.Get("endpoint_group_id")
+
+	endpointConfigurationsMaps := make([]map[string]interface{}, 0)
+	endpointConfigurationsMap := map[string]interface{}{}
+	endpointConfigurationsMap["Endpoint"] = d.Get("endpoint")
+	endpointConfigurationsMap["Type"] = d.Get("type")
+
+	if v, ok := d.GetOk("traffic_to_endpoint_policy"); ok {
+		endpointConfigurationsMap["TrafficToEndpointPolicy"] = v
+	}
+
+	endpointConfigurationsMaps = append(endpointConfigurationsMaps, endpointConfigurationsMap)
+	request["EndpointConfigurations"] = endpointConfigurationsMaps
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"NotActive.Listener", "StateError.EndPointGroup"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ga_custom_routing_endpoint", action, AlibabaCloudSdkGoERROR)
+	}
+
+	if resp, err := jsonpath.Get("$.EndpointIds", response); err != nil || resp == nil {
+		return WrapErrorf(err, IdMsg, "alicloud_ga_custom_routing_endpoint")
+	} else {
+		endpointId := resp.([]interface{})[0]
+		d.SetId(fmt.Sprintf("%v:%v", request["EndpointGroupId"], endpointId))
+	}
+
+	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, gaService.GaCustomRoutingEndpointStateRefreshFunc(d.Id(), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAlicloudGaCustomRoutingEndpointRead(d, meta)
+}
+
+func resourceAlicloudGaCustomRoutingEndpointRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+
+	object, err := gaService.DescribeGaCustomRoutingEndpoint(d.Id())
+	if err != nil {
+		if NotFoundError(err) {
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("endpoint_group_id", object["EndpointGroupId"])
+	d.Set("endpoint", object["Endpoint"])
+	d.Set("type", object["Type"])
+	d.Set("traffic_to_endpoint_policy", object["TrafficToEndpointPolicy"])
+	d.Set("accelerator_id", object["AcceleratorId"])
+	d.Set("listener_id", object["ListenerId"])
+	d.Set("custom_routing_endpoint_id", object["EndpointId"])
+	d.Set("status", object["State"])
+
+	return nil
+}
+
+func resourceAlicloudGaCustomRoutingEndpointUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	var response map[string]interface{}
+	update := false
+
+	parts, err := ParseResourceId(d.Id(), 2)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"RegionId":        client.RegionId,
+		"ClientToken":     buildClientToken("UpdateCustomRoutingEndpoints"),
+		"EndpointGroupId": parts[0],
+	}
+
+	endpointConfigurationsMaps := make([]map[string]interface{}, 0)
+	endpointConfigurationsMap := map[string]interface{}{}
+	endpointConfigurationsMap["EndpointId"] = parts[1]
+
+	if d.HasChange("traffic_to_endpoint_policy") {
+		update = true
+	}
+	if v, ok := d.GetOk("traffic_to_endpoint_policy"); ok {
+		endpointConfigurationsMap["TrafficToEndpointPolicy"] = v
+	}
+
+	endpointConfigurationsMaps = append(endpointConfigurationsMaps, endpointConfigurationsMap)
+	request["EndpointConfigurations"] = endpointConfigurationsMaps
+
+	if update {
+		action := "UpdateCustomRoutingEndpoints"
+		conn, err := client.NewGaplusClient()
+		if err != nil {
+			return WrapError(err)
+		}
+
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if IsExpectedErrors(err, []string{"NotActive.Listener", "StateError.EndPointGroup"}) || NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+
+		stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 5*time.Second, gaService.GaCustomRoutingEndpointStateRefreshFunc(d.Id(), []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+	}
+
+	return resourceAlicloudGaCustomRoutingEndpointRead(d, meta)
+}
+
+func resourceAlicloudGaCustomRoutingEndpointDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	action := "DeleteCustomRoutingEndpoints"
+	var response map[string]interface{}
+
+	conn, err := client.NewGaplusClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	parts, err := ParseResourceId(d.Id(), 2)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"RegionId":        client.RegionId,
+		"ClientToken":     buildClientToken("DeleteCustomRoutingEndpoints"),
+		"EndpointGroupId": parts[0],
+		"EndpointIds":     []string{parts[1]},
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"NotActive.Listener", "StateError.EndPointGroup"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{"NotExist.EndPointGroup", "NotExist.Listener"}) || NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, gaService.GaCustomRoutingEndpointStateRefreshFunc(d.Id(), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return nil
+}
---
+++ alicloud/resource_alicloud_ga_custom_routing_endpoint_test.go
@@ -0,0 +1,134 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccAlicloudGaCustomRoutingEndpoint_basic0(t *testing.T) {
+	var v map[string]interface{}
+	checkoutSupportedRegions(t, true, connectivity.GaSupportRegions)
+	resourceId := "alicloud_ga_custom_routing_endpoint.default"
+	ra := resourceAttrInit(resourceId, AlicloudGaCustomRoutingEndpointMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &GaService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeGaCustomRoutingEndpoint")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%sGaCustomRoutingEndpoint%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudGaCustomRoutingEndpointBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"endpoint_group_id":          "${alicloud_ga_custom_routing_endpoint_group.default.id}",
+					"endpoint":                   "${data.alicloud_vswitches.default.ids.0}",
+					"type":                       "PrivateSubNet",
+					"traffic_to_endpoint_policy": "DenyAll",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"endpoint_group_id":          CHECKSET,
+						"endpoint":                   CHECKSET,
+						"type":                       "PrivateSubNet",
+						"traffic_to_endpoint_policy": "DenyAll",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"traffic_to_endpoint_policy": "AllowAll",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"traffic_to_endpoint_policy": "AllowAll",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"traffic_to_endpoint_policy": "AllowCustom",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"traffic_to_endpoint_policy": "AllowCustom",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+var AlicloudGaCustomRoutingEndpointMap = map[string]string{
+	"accelerator_id":             CHECKSET,
+	"listener_id":                CHECKSET,
+	"custom_routing_endpoint_id": CHECKSET,
+	"status":                     CHECKSET,
+}
+
+func AlicloudGaCustomRoutingEndpointBasicDependence0(name string) string {
+	return fmt.Sprintf(`
+	variable "name" {
+  		default = "%s"
+	}
+
+	data "alicloud_vpcs" "default" {
+  		name_regex = "default-NODELETING"
+	}
+
+	data "alicloud_vswitches" "default" {
+  		vpc_id = data.alicloud_vpcs.default.ids.0
+	}
+
+	data "alicloud_ga_accelerators" "default" {
+  		status = "active"
+	}
+	
+	resource "alicloud_ga_bandwidth_package" "default" {
+  		bandwidth      = 100
+  		type           = "Basic"
+  		bandwidth_type = "Basic"
+  		payment_type   = "PayAsYouGo"
+  		billing_type   = "PayBy95"
+  		ratio          = 30
+	}
+	
+	resource "alicloud_ga_bandwidth_package_attachment" "default" {
+  		accelerator_id       = data.alicloud_ga_accelerators.default.accelerators.0.id
+  		bandwidth_package_id = alicloud_ga_bandwidth_package.default.id
+	}
+	
+	resource "alicloud_ga_listener" "default" {
+  		accelerator_id = alicloud_ga_bandwidth_package_attachment.default.accelerator_id
+		listener_type  = "CustomRouting"
+  		port_ranges {
+    		from_port = 10000
+    		to_port   = 16000
+  		}
+	}
+
+	resource "alicloud_ga_custom_routing_endpoint_group" "default" {
+  		accelerator_id                     = alicloud_ga_listener.default.accelerator_id
+  		listener_id                        = alicloud_ga_listener.default.id
+  		endpoint_group_region              = "%s"
+  		custom_routing_endpoint_group_name = var.name
+  		description                        = var.name
+	}
+`, name, defaultRegionToTest)
+}
