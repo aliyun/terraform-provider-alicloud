eddede6a0f269ef94374d8894db1d687d7a3fa50
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2023-01-12 15:55:39 +0800

    datasource/alicloud_eventbridge_service: Adds retry policy when checking status is UP

--- alicloud/data_source_alicloud_event_bridge_service.go
+++ alicloud/data_source_alicloud_event_bridge_service.go
@@ -84,14 +84,11 @@ func dataSourceAlicloudEventBridgeServiceRead(d *schema.ResourceData, meta inter
 		runtime := util.RuntimeOptions{}
 		runtime.SetAutoretry(true)
 		wait = incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		err = resource.Retry(10*time.Minute, func() *resource.RetryError {
 			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("GET"), StringPointer("2020-04-01"), StringPointer("AK"), nil, nil, &runtime)
 			if err != nil {
-				if NeedRetry(err) {
-					wait()
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
+				wait()
+				return resource.RetryableError(err)
 			}
 			return nil
 		})
