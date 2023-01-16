ac68da6a3104471e5280b5492e8ca9b2c24ac673
zcc01029273 <zcc01029273@alibaba-inc.com>
2023-01-16 11:41:47 +0800

    New Datasource alicloud_rds_character_set_names.

---
+++ alicloud/data_source_alicloud_rds_character_set_names_test.go
@@ -0,0 +1,53 @@
+package alicloud
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"strings"
+	"testing"
+)
+
+func TestAccAlicloudRdsCharacterSetNamesDataSource(t *testing.T) {
+	rand := acctest.RandInt()
+	resourceId := "data.alicloud_rds_character_set_names.default"
+	rdsCharacterSetNamesConfig := dataSourceTestAccConfig{
+		existConfig: dataSourceRdsCharacterSetNamesConfigDependence(rand, map[string]string{
+			"engine": `"MySQL"`,
+		}),
+		fakeConfig: dataSourceRdsCharacterSetNamesConfigDependence(rand, map[string]string{
+			"engine": `"MySQL"`,
+		}),
+	}
+
+	var existAlicloudRdsCharacterSetNamesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"names.#": CHECKSET,
+			"names.0": CHECKSET,
+		}
+	}
+
+	var fakeAlicloudRdsCharacterSetNamesMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"names.#": CHECKSET,
+		}
+	}
+
+	var alicloudRdsCharacterSetNamesCheckInfo = dataSourceAttr{
+		resourceId:   resourceId,
+		existMapFunc: existAlicloudRdsCharacterSetNamesMapFunc,
+		fakeMapFunc:  fakeAlicloudRdsCharacterSetNamesMapFunc,
+	}
+
+	alicloudRdsCharacterSetNamesCheckInfo.dataSourceTestCheck(t, rand, rdsCharacterSetNamesConfig)
+}
+
+func dataSourceRdsCharacterSetNamesConfigDependence(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+	return fmt.Sprintf(`
+data "alicloud_rds_character_set_names" "default" {	
+  %s
+}`, strings.Join(pairs, "\n"))
+}
