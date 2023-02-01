505b9281fd305c20220968a2c14cb32c5340f3cc
super-eggs <gzy01137900@alibaba-inc.com>
2023-02-01 11:16:45 +0800

    New Datasource `alicloud_dcdn_kv_account`;

--- alicloud/service_alicloud_dcdn.go
+++ alicloud/service_alicloud_dcdn.go
@@ -423,3 +423,39 @@ func (s *DcdnService) DescribeDcdnWafPolicyDomainAttachment(id string) (object m
 	}
 	return object, nil
 }
+
+func (s *DcdnService) DescribeDcdnKvAccountStatus() (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	conn, err := s.client.NewDcdnClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+	action := "DescribeDcdnKvAccountStatus"
+	request := map[string]interface{}{}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("GET"), StringPointer("2018-01-15"), StringPointer("AK"), request, nil, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, "", action, AlibabaCloudSdkGoERROR)
+	}
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, "", "$", response)
+	}
+	object = v.(map[string]interface{})
+	return object, nil
+
+}
