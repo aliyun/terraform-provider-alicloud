ea8f7872c355e3155807ade7ae4a8e5db35b61c3
super-eggs <gzy01137900@alibaba-inc.com>
2023-02-23 14:24:17 +0800

    New Datasource `alicloud_eflo_vpds`; New Resource `alicloud_eflo_vpd`;

---
+++ alicloud/data_source_alicloud_eflo_vpds_test.go
@@ -0,0 +1,150 @@
+package alicloud
+
+import (
+	"fmt"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+)
+
+func TestAccAlicloudEfloVpdDataSource(t *testing.T) {
+	rand := acctest.RandIntRange(1000000, 9999999)
+	checkoutSupportedRegions(t, true, connectivity.EfloSupportRegions)
+
+	idsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids": `["${alicloud_eflo_vpd.default.id}"]`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids": `["${alicloud_eflo_vpd.default.id}_fake"]`,
+		}),
+	}
+	VpdNameConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":      `["${alicloud_eflo_vpd.default.id}"]`,
+			"vpd_name": `"${alicloud_eflo_vpd.default.vpd_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":      `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"vpd_name": `"${alicloud_eflo_vpd.default.vpd_name}_fake"`,
+		}),
+	}
+	VpdIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":    `["${alicloud_eflo_vpd.default.id}"]`,
+			"vpd_id": `"${alicloud_eflo_vpd.default.id}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":    `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"vpd_id": `"${alicloud_eflo_vpd.default.id}_fake"`,
+		}),
+	}
+	nameRegexConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":        `["${alicloud_eflo_vpd.default.id}"]`,
+			"name_regex": `"${alicloud_eflo_vpd.default.vpd_name}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":        `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"name_regex": `"${alicloud_eflo_vpd.default.vpd_name}_fake"`,
+		}),
+	}
+	statusConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":    `["${alicloud_eflo_vpd.default.id}"]`,
+			"status": `"${alicloud_eflo_vpd.default.status}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":    `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"status": `"Not Available"`,
+		}),
+	}
+	resourceGroupIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":               `["${alicloud_eflo_vpd.default.id}"]`,
+			"resource_group_id": `"${alicloud_eflo_vpd.default.resource_group_id}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":               `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"resource_group_id": `"${alicloud_eflo_vpd.default.resource_group_id}_fake"`,
+		}),
+	}
+
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":               `["${alicloud_eflo_vpd.default.id}"]`,
+			"vpd_name":          `"${alicloud_eflo_vpd.default.vpd_name}"`,
+			"name_regex":        `"${alicloud_eflo_vpd.default.vpd_name}"`,
+			"resource_group_id": `"${alicloud_eflo_vpd.default.resource_group_id}"`,
+			"status":            `"${alicloud_eflo_vpd.default.status}"`,
+		}),
+		fakeConfig: testAccCheckAlicloudEfloVpdSourceConfig(rand, map[string]string{
+			"ids":               `["${alicloud_eflo_vpd.default.id}_fake"]`,
+			"vpd_name":          `"${alicloud_eflo_vpd.default.vpd_name}_fake"`,
+			"name_regex":        `"${alicloud_eflo_vpd.default.vpd_name}_fake"`,
+			"resource_group_id": `"${alicloud_eflo_vpd.default.resource_group_id}_fake"`,
+			"status":            `"Not Available"`,
+		}),
+	}
+
+	EfloVpdCheckInfo.dataSourceTestCheck(t, rand, idsConf, VpdNameConf, VpdIdConf, nameRegexConf, statusConf, resourceGroupIdConf, allConf)
+}
+
+var existEfloVpdMapFunc = func(rand int) map[string]string {
+	return map[string]string{
+		"ids.#":                    "1",
+		"names.#":                  "1",
+		"vpds.#":                   "1",
+		"vpds.0.id":                CHECKSET,
+		"vpds.0.cidr":              "10.0.0.0/8",
+		"vpds.0.create_time":       CHECKSET,
+		"vpds.0.gmt_modified":      CHECKSET,
+		"vpds.0.resource_group_id": CHECKSET,
+		"vpds.0.status":            CHECKSET,
+		"vpds.0.vpd_id":            CHECKSET,
+		"vpds.0.vpd_name":          CHECKSET,
+	}
+}
+
+var fakeEfloVpdMapFunc = func(rand int) map[string]string {
+	return map[string]string{
+		"ids.#":   "0",
+		"names.#": "0",
+		"vpds.#":  "0",
+	}
+}
+
+var EfloVpdCheckInfo = dataSourceAttr{
+	resourceId:   "data.alicloud_eflo_vpds.default",
+	existMapFunc: existEfloVpdMapFunc,
+	fakeMapFunc:  fakeEfloVpdMapFunc,
+}
+
+func testAccCheckAlicloudEfloVpdSourceConfig(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+	config := fmt.Sprintf(`
+variable "name" {
+	default = "tf-testAccEfloVpd%d"
+}
+
+data "alicloud_resource_manager_resource_groups" "default" {
+}
+
+resource "alicloud_eflo_vpd" "default" {
+  cidr      = "10.0.0.0/8"
+  vpd_name  = var.name
+  resource_group_id = data.alicloud_resource_manager_resource_groups.default.groups.0.id
+}
+
+data "alicloud_eflo_vpds" "default" {
+  enable_details = true
+%s
+}
+`, rand, strings.Join(pairs, "\n   "))
+	return config
+}
---
+++ alicloud/resource_alicloud_eflo_vpd.go
@@ -0,0 +1,216 @@
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAlicloudEfloVpd() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudEfloVpdCreate,
+		Read:   resourceAlicloudEfloVpdRead,
+		Update: resourceAlicloudEfloVpdUpdate,
+		Delete: resourceAlicloudEfloVpdDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"cidr": {
+				Required: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"create_time": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+			"gmt_modified": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+			"resource_group_id": {
+				Optional: true,
+				ForceNew: true,
+				Type:     schema.TypeString,
+			},
+			"status": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+			"vpd_name": {
+				Required: true,
+				Type:     schema.TypeString,
+			},
+		},
+	}
+}
+
+func resourceAlicloudEfloVpdCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	efloService := EfloService{client}
+	request := make(map[string]interface{})
+	conn, err := client.NewEfloClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request["ClientToken"] = buildClientToken("CreateVpd")
+	request["VpdName"] = d.Get("vpd_name")
+	request["Cidr"] = d.Get("cidr")
+	if v, ok := d.GetOk("resource_group_id"); ok {
+		request["ResourceGroupId"] = v
+	}
+	var response map[string]interface{}
+	action := "CreateVpd"
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
+		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-05-30"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		response = resp
+		addDebug(action, response, request)
+		return nil
+	})
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_eflo_vpd", action, AlibabaCloudSdkGoERROR)
+	}
+
+	if v, err := jsonpath.Get("$.Content.VpdId", response); err != nil || v == nil {
+		return WrapErrorf(err, IdMsg, "alicloud_eflo_vpd")
+	} else {
+		d.SetId(fmt.Sprint(v))
+	}
+	stateConf := BuildStateConf([]string{}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, efloService.EfloVpdStateRefreshFunc(d.Id(), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return resourceAlicloudEfloVpdRead(d, meta)
+}
+
+func resourceAlicloudEfloVpdRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	efloService := EfloService{client}
+
+	object, err := efloService.DescribeEfloVpd(d.Id())
+	if err != nil {
+		if NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_eflo_vpd efloService.DescribeEfloVpd Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("cidr", object["Cidr"])
+	d.Set("create_time", object["CreateTime"])
+	d.Set("gmt_modified", object["GmtModified"])
+	d.Set("resource_group_id", object["ResourceGroupId"])
+	d.Set("status", object["Status"])
+	d.Set("vpd_name", object["VpdName"])
+
+	return nil
+}
+
+func resourceAlicloudEfloVpdUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+
+	conn, err := client.NewEfloClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	update := false
+	request := map[string]interface{}{
+		"VpdId":    d.Id(),
+		"RegionId": client.RegionId,
+	}
+
+	if d.HasChange("vpd_name") {
+		update = true
+		if v, ok := d.GetOk("vpd_name"); ok {
+			request["VpdName"] = v
+		}
+	}
+
+	if update {
+		action := "UpdateVpd"
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
+			resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-05-30"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			addDebug(action, resp, request)
+			return nil
+		})
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+	}
+
+	return resourceAlicloudEfloVpdRead(d, meta)
+}
+
+func resourceAlicloudEfloVpdDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	efloService := EfloService{client}
+	conn, err := client.NewEfloClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+
+		"VpdId": d.Id(),
+	}
+
+	action := "DeleteVpd"
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
+		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-05-30"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, resp, request)
+		return nil
+	})
+	if err != nil {
+		if IsExpectedErrors(err, []string{"1003"}) || NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, efloService.EfloVpdStateRefreshFunc(d.Id(), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return nil
+}
