f58b082e781d30afd53f9dd7e1a485cdb39b4915
MrWolong <ly01269865@alibaba-inc.com>
2023-02-28 14:52:57 +0800

    docs: Improves the docs example and Improves the resource testcases

--- alicloud/data_source_alicloud_mongodb_sharding_network_private_addresses_test.go
+++ alicloud/data_source_alicloud_mongodb_sharding_network_private_addresses_test.go
@@ -58,52 +58,55 @@ func testAccCheckAlicloudMongodbShardingNetworkPrivateAddressesDataSourceName(ra
 	}
 
 	config := fmt.Sprintf(`
+	variable "name" {
+  		default = "tf-testAccShardingNetworkPrivateAddress-%d"
+	}
 
-variable "name" {	
-	default = "tf-testAccShardingNetworkPrivateAddress-%d"
-}
-data "alicloud_mongodb_zones" "default" {}
-data "alicloud_vpcs" "default" {
-    name_regex = "^default-NODELETING$"
-}
-data "alicloud_vswitches" "default" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_mongodb_zones.default.zones.0.id
-}
+	data "alicloud_mongodb_zones" "default" {
+	}
 
-resource "alicloud_mongodb_sharding_instance" "default" {
-  zone_id        = data.alicloud_mongodb_zones.default.zones.0.id
-  vswitch_id     = data.alicloud_vswitches.default.ids[0]
-  engine_version = "3.4"
-  name           = var.name
-  mongo_list {
-    node_class = "dds.mongos.mid"
-  }
-  mongo_list {
-    node_class = "dds.mongos.mid"
-  }
-  shard_list {
-    node_class   = "dds.shard.mid"
-    node_storage = 10
-  }
-  shard_list {
-    node_class   = "dds.shard.mid"
-    node_storage = 10
-  }
-}
+	data "alicloud_vpcs" "default" {
+  		name_regex = "^default-NODELETING$"
+	}
 
-resource "alicloud_mongodb_sharding_network_private_address" "default" {
-	account_name = "tf_test"
-	account_password = "YourPassword+12345"
-	db_instance_id = alicloud_mongodb_sharding_instance.default.id
-	node_id = alicloud_mongodb_sharding_instance.default.shard_list.0.node_id
-	zone_id = alicloud_mongodb_sharding_instance.default.zone_id
-}
+	data "alicloud_vswitches" "default" {
+  		vpc_id  = data.alicloud_vpcs.default.ids.0
+  		zone_id = data.alicloud_mongodb_zones.default.zones.0.id
+	}
 
-data "alicloud_mongodb_sharding_network_private_addresses" "default" {	
-	db_instance_id = alicloud_mongodb_sharding_instance.default.id
-	%s
-}
+	resource "alicloud_mongodb_sharding_instance" "default" {
+  		zone_id        = data.alicloud_mongodb_zones.default.zones.0.id
+  		vswitch_id     = data.alicloud_vswitches.default.ids[0]
+  		engine_version = "4.2"
+  		name           = var.name
+  		mongo_list {
+    		node_class = "dds.mongos.mid"
+  		}
+  		mongo_list {
+    		node_class = "dds.mongos.mid"
+  		}
+  		shard_list {
+    		node_class   = "dds.shard.mid"
+    		node_storage = 10
+  		}
+  		shard_list {
+    		node_class   = "dds.shard.mid"
+    		node_storage = 10
+  		}
+	}
+
+	resource "alicloud_mongodb_sharding_network_private_address" "default" {
+  		account_name     = "tf_test"
+  		account_password = "YourPassword+12345"
+  		db_instance_id   = alicloud_mongodb_sharding_instance.default.id
+  		node_id          = alicloud_mongodb_sharding_instance.default.shard_list.0.node_id
+  		zone_id          = alicloud_mongodb_sharding_instance.default.zone_id
+	}
+
+	data "alicloud_mongodb_sharding_network_private_addresses" "default" {
+  		db_instance_id = alicloud_mongodb_sharding_instance.default.id
+		%s
+	}
 `, rand, strings.Join(pairs, " \n "))
 	return config
 }
