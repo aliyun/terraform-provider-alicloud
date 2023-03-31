ddf5b744c0c0bac182729de5c59459cbb6d4ba38
MrWolong <ly01269865@alibaba-inc.com>
2023-03-31 15:48:24 +0800

    resource/alicloud_route_entry: Fixed the parse error caused by `destination_cidrblock` value

--- alicloud/resource_alicloud_route_entry_test.go
+++ alicloud/resource_alicloud_route_entry_test.go
@@ -158,6 +158,47 @@ func TestAccAlicloudVPCRouteEntryMulti(t *testing.T) {
 	})
 }
 
+func TestAccAlicloudVPCRouteEntryInstance_Ipv6(t *testing.T) {
+	var v *vpc.RouteEntry
+	rand := acctest.RandIntRange(1000, 9999)
+	resourceId := "alicloud_route_entry.default"
+	ra := resourceAttrInit(resourceId, testAccRouteEntryCheckMap)
+	serviceFunc := func() interface{} {
+		return &VpcService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}
+	rc := resourceCheckInit(resourceId, &v, serviceFunc)
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+
+		// module name
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckRouteEntryDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRouteEntryConfig_instance_ipv6(rand),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"nexthop_type":          "Instance",
+						"destination_cidrblock": "2001:ffff:ffff:ffff::/64",
+						"name":                  fmt.Sprintf("tf-testAccRouteEntryConfigName%d", rand),
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccCheckRouteEntryDestroy(s *terraform.State) error {
 	client := testAccProvider.Meta().(*connectivity.AliyunClient)
 	vpcService := VpcService{client}
@@ -187,217 +228,292 @@ func testAccCheckRouteEntryDestroy(s *terraform.State) error {
 func testAccRouteEntryConfig_instance(rand int) string {
 	return fmt.Sprintf(
 		`
-data "alicloud_zones" "default" {
-	available_resource_creation= "VSwitch"
-}
-data "alicloud_instance_types" "default" {
- 	availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-	cpu_core_count    = 1
-    memory_size       = 2
-}
-data "alicloud_images" "default" {
-	name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
-	most_recent = true
-	owners = "system"
-}
+	variable "name" {
+  		default = "tf-testAccRouteEntryConfigName%d"
+	}
 
-variable "name" {
-	default = "tf-testAccRouteEntryConfigName%d"
-}
-resource "alicloud_vpc" "default" {
-	vpc_name = "${var.name}"
-	cidr_block = "10.1.0.0/21"
-}
+	data "alicloud_zones" "default" {
+  		available_resource_creation = "VSwitch"
+	}
 
-resource "alicloud_vswitch" "default" {
-	vpc_id = "${alicloud_vpc.default.id}"
-	cidr_block = "10.1.1.0/24"
-	availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-	vswitch_name = "${var.name}"
-}
+	data "alicloud_instance_types" "default" {
+  		availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  		cpu_core_count    = 1
+  		memory_size       = 2
+	}
 
-resource "alicloud_security_group" "default" {
-	name = "${var.name}"
-	description = "default"
-	vpc_id = "${alicloud_vpc.default.id}"
-}
+	data "alicloud_images" "default" {
+  		name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
+  		most_recent = true
+  		owners      = "system"
+	}
 
-resource "alicloud_security_group_rule" "default" {
-	type = "ingress"
-	ip_protocol = "tcp"
-	nic_type = "intranet"
-	policy = "accept"
-	port_range = "22/22"
-	priority = 1
-	security_group_id = "${alicloud_security_group.default.id}"
-	cidr_ip = "0.0.0.0/0"
-}
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = "${var.name}"
+  		cidr_block = "10.1.0.0/21"
+	}
 
-resource "alicloud_instance" "default" {
-	security_groups = ["${alicloud_security_group.default.id}"]
+	resource "alicloud_vswitch" "default" {
+  		vpc_id            = "${alicloud_vpc.default.id}"
+  		cidr_block        = "10.1.1.0/24"
+  		availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  		vswitch_name      = "${var.name}"
+	}
 
-	vswitch_id = "${alicloud_vswitch.default.id}"
-	allocate_public_ip = true
+	resource "alicloud_security_group" "default" {
+  		name        = "${var.name}"
+  		description = "default"
+  		vpc_id      = "${alicloud_vpc.default.id}"
+	}
 
-	instance_charge_type = "PostPaid"
-	instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
-	internet_charge_type = "PayByTraffic"
-	internet_max_bandwidth_out = 5
+	resource "alicloud_security_group_rule" "default" {
+  		type              = "ingress"
+  		ip_protocol       = "tcp"
+  		nic_type          = "intranet"
+  		policy            = "accept"
+  		port_range        = "22/22"
+  		priority          = 1
+  		security_group_id = "${alicloud_security_group.default.id}"
+  		cidr_ip           = "0.0.0.0/0"
+	}
 
-	system_disk_category = "cloud_efficiency"
-	image_id = "${data.alicloud_images.default.images.0.id}"
-	instance_name = "${var.name}"
-}
+	resource "alicloud_instance" "default" {
+  		security_groups            = ["${alicloud_security_group.default.id}"]
+  		vswitch_id                 = "${alicloud_vswitch.default.id}"
+  		allocate_public_ip         = true
+  		instance_charge_type       = "PostPaid"
+  		instance_type              = "${data.alicloud_instance_types.default.instance_types.0.id}"
+	  	internet_charge_type       = "PayByTraffic"
+  		internet_max_bandwidth_out = 5
+  		system_disk_category       = "cloud_efficiency"
+  		image_id                   = "${data.alicloud_images.default.images.0.id}"
+  		instance_name              = "${var.name}"
+	}
 
-resource "alicloud_route_entry" "default" {
-	route_table_id = "${alicloud_vpc.default.route_table_id}"
-	destination_cidrblock = "172.11.1.1/32"
-	nexthop_type = "Instance"
-	nexthop_id = "${alicloud_instance.default.id}"
-	name = "${var.name}"
-}
+	resource "alicloud_route_entry" "default" {
+  		route_table_id        = "${alicloud_vpc.default.route_table_id}"
+  		destination_cidrblock = "172.11.1.1/32"
+  		nexthop_type          = "Instance"
+  		nexthop_id            = "${alicloud_instance.default.id}"
+  		name                  = "${var.name}"
+	}
 `, rand)
 }
 
 func testAccRouteEntryConfig_interface(rand int) string {
 	return fmt.Sprintf(
 		`
-data "alicloud_zones" "default" {
-  available_resource_creation= "VSwitch"
-}
-variable "name" {
-	default = "tf-testAccRouteEntryInterfaceConfig%d"
-}
-resource "alicloud_vpc" "default" {
-  vpc_name = "${var.name}"
-  cidr_block = "10.1.0.0/21"
-}
+	variable "name" {
+  		default = "tf-testAccRouteEntryInterfaceConfig%d"
+	}
 
-resource "alicloud_vswitch" "default" {
-  vpc_id = "${alicloud_vpc.default.id}"
-  cidr_block = "10.1.1.0/24"
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-  vswitch_name = "${var.name}"
-}
+	data "alicloud_zones" "default" {
+  		available_resource_creation = "VSwitch"
+	}
 
-resource "alicloud_security_group" "default" {
-  name = "${var.name}"
-  description = "${var.name}"
-  vpc_id = "${alicloud_vpc.default.id}"
-}
+	data "alicloud_regions" "default" {
+  		current = true
+	}
 
-resource "alicloud_security_group_rule" "default" {
-  type = "ingress"
-  ip_protocol = "tcp"
-  nic_type = "intranet"
-  policy = "accept"
-  port_range = "22/22"
-  priority = 1
-  security_group_id = "${alicloud_security_group.default.id}"
-  cidr_ip = "0.0.0.0/0"
-}
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = "${var.name}"
+  		cidr_block = "10.1.0.0/21"
+	}
 
-data "alicloud_regions" "default" {
-  current = true
-}
+	resource "alicloud_vswitch" "default" {
+  		vpc_id            = "${alicloud_vpc.default.id}"
+  		cidr_block        = "10.1.1.0/24"
+  		availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  		vswitch_name      = "${var.name}"
+	}
 
-resource "alicloud_router_interface" "default" {
-  opposite_region = "${data.alicloud_regions.default.regions.0.id}"
-  router_type = "VRouter"
-  router_id = "${alicloud_vpc.default.router_id}"
-  role = "InitiatingSide"
-  specification = "Large.2"
-  name = "${var.name}"
-  description = "${var.name}"
-}
+	resource "alicloud_security_group" "default" {
+  		name        = "${var.name}"
+  		description = "${var.name}"
+  		vpc_id      = "${alicloud_vpc.default.id}"
+	}
 
-resource "alicloud_route_entry" "default" {
-  route_table_id = "${alicloud_vpc.default.route_table_id}"
-  destination_cidrblock = "172.11.1.1/32"
-  nexthop_type = "RouterInterface"
-  nexthop_id = "${alicloud_router_interface.default.id}"
-  name = "${var.name}"
-}
+	resource "alicloud_security_group_rule" "default" {
+  		type              = "ingress"
+  		ip_protocol       = "tcp"
+  		nic_type          = "intranet"
+  		policy            = "accept"
+  		port_range        = "22/22"
+  		priority          = 1
+  		security_group_id = "${alicloud_security_group.default.id}"
+  		cidr_ip           = "0.0.0.0/0"
+	}
+
+	resource "alicloud_router_interface" "default" {
+  		opposite_region = "${data.alicloud_regions.default.regions.0.id}"
+  		router_type     = "VRouter"
+  		router_id       = "${alicloud_vpc.default.router_id}"
+  		role            = "InitiatingSide"
+  		specification   = "Large.2"
+  		name            = "${var.name}"
+  		description     = "${var.name}"
+	}
+
+	resource "alicloud_route_entry" "default" {
+  		route_table_id        = "${alicloud_vpc.default.route_table_id}"
+  		destination_cidrblock = "172.11.1.1/32"
+  		nexthop_type          = "RouterInterface"
+  		nexthop_id            = "${alicloud_router_interface.default.id}"
+  		name                  = "${var.name}"
+	}
 `, rand)
 }
 
 func testAccRouteEntryConfig_natGateway(rand int) string {
 	return fmt.Sprintf(
 		`
-data "alicloud_zones" "default" {
-  available_resource_creation= "VSwitch"
-}
-variable "name" {
-	default = "tf-testAccRouteEntryNatGatewayConfig%d"
-}
-resource "alicloud_vpc" "default" {
-  vpc_name = "${var.name}"
-  cidr_block = "10.1.0.0/21"
-}
+	variable "name" {
+  		default = "tf-testAccRouteEntryNatGatewayConfig%d"
+	}
 
-resource "alicloud_vswitch" "default" {
-  vpc_id = "${alicloud_vpc.default.id}"
-  cidr_block = "10.1.1.0/24"
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-  vswitch_name = "${var.name}"
-}
+	data "alicloud_zones" "default" {
+  		available_resource_creation = "VSwitch"
+	}
 
-resource "alicloud_nat_gateway" "default" {
-	vpc_id = "${alicloud_vpc.default.id}"
-	nat_type = "Enhanced"
-	vswitch_id = alicloud_vswitch.default.id
-	nat_gateway_name = "${var.name}"
-}
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = "${var.name}"
+  		cidr_block = "10.1.0.0/21"
+	}
 
-resource "alicloud_route_entry" "default" {
-  route_table_id = "${alicloud_vpc.default.route_table_id}"
-  destination_cidrblock = "172.11.1.1/32"
-  nexthop_type = "NatGateway"
-  nexthop_id = "${alicloud_nat_gateway.default.id}"
-  name = "${var.name}"
-}`, rand)
+	resource "alicloud_vswitch" "default" {
+  		vpc_id            = "${alicloud_vpc.default.id}"
+  		cidr_block        = "10.1.1.0/24"
+  		availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  		vswitch_name      = "${var.name}"
+	}
+
+	resource "alicloud_nat_gateway" "default" {
+  		vpc_id           = "${alicloud_vpc.default.id}"
+  		nat_type         = "Enhanced"
+  		vswitch_id       = alicloud_vswitch.default.id
+  		nat_gateway_name = "${var.name}"
+	}
+
+	resource "alicloud_route_entry" "default" {
+  		route_table_id        = "${alicloud_vpc.default.route_table_id}"
+  		destination_cidrblock = "172.11.1.1/32"
+  		nexthop_type          = "NatGateway"
+  		nexthop_id            = "${alicloud_nat_gateway.default.id}"
+  		name                  = "${var.name}"
+	}
+`, rand)
 }
 
 func testAccRouteEntryConfigMulti(rand int) string {
 	return fmt.Sprintf(`
-data "alicloud_zones" "default" {
-	available_resource_creation= "VSwitch"
+	variable "name" {
+  		default = "tf-testAccRouteEntryConcurrence%d"
+	}
+
+	data "alicloud_zones" "default" {
+  		available_resource_creation = "VSwitch"
+	}
+
+
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = "${var.name}"
+  		cidr_block = "10.1.0.0/21"
+	}
+
+	resource "alicloud_vswitch" "default" {
+  		vswitch_name      = "${var.name}"
+  		cidr_block        = "10.1.1.0/24"
+  		availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  		vpc_id            = "${alicloud_vpc.default.id}"
+	}
+
+	resource "alicloud_security_group" "default" {
+  		name   = "${var.name}"
+  		vpc_id = "${alicloud_vpc.default.id}"
+	}
+
+	resource "alicloud_ecs_network_interface" "default" {
+  		name            = "${var.name}"
+  		vswitch_id      = "${alicloud_vswitch.default.id}"
+  		security_groups = ["${alicloud_security_group.default.id}"]
+	}
+
+	resource "alicloud_route_entry" "default" {
+  		count                 = 5
+  		route_table_id        = "${alicloud_vpc.default.route_table_id}"
+  		destination_cidrblock = "172.16.${count.index}.0/24"
+  		nexthop_type          = "NetworkInterface"
+  		nexthop_id            = "${alicloud_ecs_network_interface.default.id}"
+  		name                  = "${var.name}"
+	}
+`, rand)
 }
 
-variable "name" {
-	default = "tf-testAccRouteEntryConcurrence%d"
-}
-resource "alicloud_vpc" "default" {
-	vpc_name = "${var.name}"
-	cidr_block = "10.1.0.0/21"
-}
+func testAccRouteEntryConfig_instance_ipv6(rand int) string {
+	return fmt.Sprintf(
+		`
+	variable "name" {
+  		default = "tf-testAccRouteEntryConfigName%d"
+	}
 
-resource "alicloud_vswitch" "default" {
-    vswitch_name = "${var.name}"
-    cidr_block = "10.1.1.0/24"
-    availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-    vpc_id = "${alicloud_vpc.default.id}"
-}
+	data "alicloud_zones" "default" {
+  		available_resource_creation = "VSwitch"
+	}
 
-resource "alicloud_security_group" "default" {
-    name = "${var.name}"
-    vpc_id = "${alicloud_vpc.default.id}"
-}
+	data "alicloud_instance_types" "default" {
+  		availability_zone                 = "${data.alicloud_zones.default.zones.0.id}"
+  		cpu_core_count                    = 2
+  		minimum_eni_ipv6_address_quantity = 2
+  		system_disk_category              = "cloud_essd"
+	}
 
-resource "alicloud_ecs_network_interface" "default" {
-    name = "${var.name}"
-    vswitch_id = "${alicloud_vswitch.default.id}"
-    security_groups = [ "${alicloud_security_group.default.id}" ]
-}
+	data "alicloud_images" "default" {
+  		name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
+  		most_recent = true
+  		owners      = "system"
+	}
 
-resource "alicloud_route_entry" "default" {
-	count = 5
-	route_table_id = "${alicloud_vpc.default.route_table_id}"
-	destination_cidrblock = "172.16.${count.index}.0/24"
-	nexthop_type = "NetworkInterface"
-	nexthop_id = "${alicloud_ecs_network_interface.default.id}"
-	name = "${var.name}"
-}
+	resource "alicloud_vpc" "default" {
+  		vpc_name    = "${var.name}"
+  		cidr_block  = "10.1.0.0/21"
+  		enable_ipv6 = "true"
+	}
+
+	resource "alicloud_vswitch" "default" {
+  		vpc_id               = "${alicloud_vpc.default.id}"
+  		cidr_block           = "10.1.1.0/24"
+  		availability_zone    = "${data.alicloud_zones.default.zones.0.id}"
+  		vswitch_name         = "${var.name}"
+  		ipv6_cidr_block_mask = 64
+	}
+
+	resource "alicloud_security_group" "default" {
+  		name        = "${var.name}"
+  		description = "default"
+  		vpc_id      = "${alicloud_vpc.default.id}"
+	}
+
+	resource "alicloud_instance" "default" {
+  		security_groups            = ["${alicloud_security_group.default.id}"]
+  		vswitch_id                 = "${alicloud_vswitch.default.id}"
+  		allocate_public_ip         = true
+  		instance_charge_type       = "PostPaid"
+  		instance_type              = "${data.alicloud_instance_types.default.instance_types.0.id}"
+  		internet_charge_type       = "PayByTraffic"
+  		internet_max_bandwidth_out = 5
+  		system_disk_category       = "cloud_essd"
+  		image_id                   = "${data.alicloud_images.default.images.0.id}"
+  		instance_name              = "${var.name}"
+  		ipv6_address_count         = 1
+	}
+
+	resource "alicloud_route_entry" "default" {
+  		route_table_id        = "${alicloud_vpc.default.route_table_id}"
+  		destination_cidrblock = "2001:ffff:ffff:ffff::/64"
+  		nexthop_type          = "Instance"
+  		nexthop_id            = "${alicloud_instance.default.id}"
+  		name                  = "${var.name}"
+	}
 `, rand)
 }
 
