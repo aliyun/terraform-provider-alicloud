430a26fa146cc77a8a2c898f5c96b0fe262d9a88
MrWolong <ly01269865@alibaba-inc.com>
2023-04-04 18:33:08 +0800

    resource/alicloud_nlb_load_balancer_security_group_attachment: Fixed the panic error caused by index out of range

--- alicloud/resource_alicloud_nlb_load_balancer_security_group_attachment.go
+++ alicloud/resource_alicloud_nlb_load_balancer_security_group_attachment.go
@@ -26,19 +26,19 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachment() *schema.Resource {
 			Delete: schema.DefaultTimeout(1 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
-			"dry_run": {
-				Optional: true,
-				Type:     schema.TypeBool,
-			},
 			"load_balancer_id": {
+				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-				Type:     schema.TypeString,
 			},
 			"security_group_id": {
+				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-				Type:     schema.TypeString,
+			},
+			"dry_run": {
+				Type:     schema.TypeBool,
+				Optional: true,
 			},
 		},
 	}
@@ -52,25 +52,24 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentCreate(d *schema.Reso
 		return WrapError(err)
 	}
 
-	if v, ok := d.GetOk("load_balancer_id"); ok {
-		request["LoadBalancerId"] = v
-	}
-	if v, ok := d.GetOk("security_group_id"); ok {
-		request["SecurityGroupIds.1"] = v
-	}
+	request["RegionId"] = client.RegionId
+	request["ClientToken"] = buildClientToken("LoadBalancerJoinSecurityGroup")
+	request["LoadBalancerId"] = d.Get("load_balancer_id")
+
+	securityGroupId := d.Get("security_group_id").(string)
+	request["SecurityGroupIds"] = []string{securityGroupId}
 
 	if v, ok := d.GetOkExists("dry_run"); ok {
 		request["DryRun"] = v
 	}
 
-	request["ClientToken"] = buildClientToken("LoadBalancerJoinSecurityGroup")
 	var response map[string]interface{}
 	action := "LoadBalancerJoinSecurityGroup"
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
 	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
-		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-04-30"), StringPointer("AK"), nil, request, &runtime)
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-04-30"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
 			if NeedRetry(err) || IsExpectedErrors(err, []string{"Conflict.Lock"}) {
 				wait()
@@ -78,13 +77,14 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentCreate(d *schema.Reso
 			}
 			return resource.NonRetryableError(err)
 		}
-		response = resp
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_nlb_load_balancer_security_group_attachment", action, AlibabaCloudSdkGoERROR)
 	}
+
 	nlbService := NlbService{client}
 
 	jobId := fmt.Sprint(response["JobId"])
@@ -93,7 +93,7 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentCreate(d *schema.Reso
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
 
-	d.SetId(fmt.Sprint(request["LoadBalancerId"], ":", request["SecurityGroupIds.1"]))
+	d.SetId(fmt.Sprint(request["LoadBalancerId"], ":", securityGroupId))
 
 	return resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentRead(d, meta)
 }
@@ -111,10 +111,12 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentRead(d *schema.Resour
 		}
 		return WrapError(err)
 	}
+
 	parts, err := ParseResourceId(d.Id(), 2)
 	if err != nil {
 		return WrapError(err)
 	}
+
 	d.Set("load_balancer_id", parts[0])
 	d.Set("security_group_id", parts[1])
 
@@ -138,11 +140,12 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentDelete(d *schema.Reso
 	}
 
 	request := map[string]interface{}{
+		"RegionId":           client.RegionId,
+		"ClientToken":        buildClientToken("LoadBalancerLeaveSecurityGroup"),
 		"LoadBalancerId":     parts[0],
 		"SecurityGroupIds.1": parts[1],
 	}
 
-	request["ClientToken"] = buildClientToken("LoadBalancerLeaveSecurityGroup")
 	action := "LoadBalancerLeaveSecurityGroup"
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
@@ -159,11 +162,13 @@ func resourceAlicloudNlbLoadBalancerSecurityGroupAttachmentDelete(d *schema.Reso
 		addDebug(action, resp, request)
 		return nil
 	})
+
 	if err != nil {
 		if NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	return nil
 }
--- alicloud/resource_alicloud_nlb_load_balancer_security_group_attachment_test.go
+++ alicloud/resource_alicloud_nlb_load_balancer_security_group_attachment_test.go
@@ -55,59 +55,61 @@ var AlicloudNlbLoadBalancerSecurityGroupAttachmentMap = map[string]string{}
 
 func AlicloudNlbLoadBalancerSecurityGroupAttachmentBasicDependence(name string) string {
 	return fmt.Sprintf(`
-variable "name" {
-    default = "%s"
-}
+	variable "name" {
+  		default = "%s"
+	}
 
-data "alicloud_nlb_zones" "default" {}
+	data "alicloud_nlb_zones" "default" {
+	}
 
-data "alicloud_vpcs" "default" {
-    name_regex = "^default-NODELETING$"
-}
+	data "alicloud_vpcs" "default" {
+  		name_regex = "^default-NODELETING$"
+	}
 
-data "alicloud_resource_manager_resource_groups" "default" {}
+	data "alicloud_resource_manager_resource_groups" "default" {
+	}
 
-data "alicloud_vswitches" "default_1" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_nlb_zones.default.zones.0.id
-}
+	data "alicloud_vswitches" "default_1" {
+  		vpc_id  = data.alicloud_vpcs.default.ids.0
+  		zone_id = data.alicloud_nlb_zones.default.zones.0.id
+	}
 
-data "alicloud_vswitches" "default_2" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_nlb_zones.default.zones.1.id
-}
+	data "alicloud_vswitches" "default_2" {
+  		vpc_id  = data.alicloud_vpcs.default.ids.0
+  		zone_id = data.alicloud_nlb_zones.default.zones.1.id
+	}
 
-resource "alicloud_security_group" "default" {
-  name   = var.name
-  vpc_id = data.alicloud_vpcs.default.ids.0
-}
+	resource "alicloud_security_group" "default" {
+  		name   = var.name
+  		vpc_id = data.alicloud_vpcs.default.ids.0
+	}
 
-locals {
-  zone_id_1    = data.alicloud_nlb_zones.default.zones.0.id
-  vswitch_id_1 = data.alicloud_vswitches.default_1.ids[0]
-  zone_id_2    = data.alicloud_nlb_zones.default.zones.1.id
-  vswitch_id_2 = data.alicloud_vswitches.default_2.ids[0]
-}
-resource "alicloud_nlb_load_balancer" "default" {
-  load_balancer_name = var.name
-  resource_group_id  = data.alicloud_resource_manager_resource_groups.default.ids.0
-  load_balancer_type = "Network"
-  address_type       = "Internet"
-  address_ip_version = "Ipv4"
-  tags               = {
-    Created = "tfTestAcc0"
-    For     = "Tftestacc 0"
-  }
-  vpc_id = data.alicloud_vpcs.default.ids.0
-  zone_mappings {
-    vswitch_id = local.vswitch_id_1
-    zone_id    = local.zone_id_1
-  }
-  zone_mappings {
-    vswitch_id = local.vswitch_id_2
-    zone_id    = local.zone_id_2
-  }
-}
+	locals {
+  		zone_id_1    = data.alicloud_nlb_zones.default.zones.0.id
+  		vswitch_id_1 = data.alicloud_vswitches.default_1.ids[0]
+  		zone_id_2    = data.alicloud_nlb_zones.default.zones.1.id
+  		vswitch_id_2 = data.alicloud_vswitches.default_2.ids[0]
+	}
 
+	resource "alicloud_nlb_load_balancer" "default" {
+  		load_balancer_name = var.name
+  		resource_group_id  = data.alicloud_resource_manager_resource_groups.default.ids.0
+  		load_balancer_type = "Network"
+  		address_type       = "Internet"
+  		address_ip_version = "Ipv4"
+  		tags = {
+    		Created = "tfTestAcc0"
+    		For     = "Tftestacc 0"
+  		}
+  		vpc_id = data.alicloud_vpcs.default.ids.0
+  		zone_mappings {
+    		vswitch_id = local.vswitch_id_1
+    		zone_id    = local.zone_id_1
+  		}
+  		zone_mappings {
+    		vswitch_id = local.vswitch_id_2
+    		zone_id    = local.zone_id_2
+  		}
+	}
 `, name)
 }
