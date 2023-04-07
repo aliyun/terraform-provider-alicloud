8a44782bb9b28297ae75c2608f7e61528234c7ef
张利男 <wb-zln530580@alibaba-inc.com>
2023-04-07 17:29:34 +0800

    tag_policy

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -883,6 +883,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_gpdb_instance":                      resourceAlicloudGpdbInstance(),
 			"alicloud_gpdb_elastic_instance":              resourceAlicloudGpdbElasticInstance(),
 			"alicloud_gpdb_connection":                    resourceAlicloudGpdbConnection(),
+			"alicloud_tag_policy":                         resourceAlicloudTagPolicy(),
 			"alicloud_db_readonly_instance":               resourceAlicloudDBReadonlyInstance(),
 			"alicloud_auto_provisioning_group":            resourceAlicloudAutoProvisioningGroup(),
 			"alicloud_ess_scaling_group":                  resourceAlicloudEssScalingGroup(),
