3ae7f73de8aac183965217a3af63ccf9e7f9521b
MrWolong <ly01269865@alibaba-inc.com>
2023-05-15 15:10:39 +0800

    resource/alicloud_ga_accelerator: Added the field `bandwidth_billing_type`

--- alicloud/resource_alicloud_ga_accelerator_test.go
+++ alicloud/resource_alicloud_ga_accelerator_test.go
@@ -264,7 +264,6 @@ func TestAccAlicloudGaAccelerator_basic(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
-
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
 		CheckDestroy:  nil,
@@ -282,12 +281,6 @@ func TestAccAlicloudGaAccelerator_basic(t *testing.T) {
 						"duration":        "1",
 					}),
 				),
-			},
-			{
-				ResourceName:            resourceId,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"auto_use_coupon", "duration"},
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
@@ -332,6 +325,107 @@ func TestAccAlicloudGaAccelerator_basic(t *testing.T) {
 						"spec":             "1",
 					}),
 				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"auto_use_coupon", "duration"},
+			},
+		},
+	})
+}
+
+func TestAccAlicloudGaAccelerator_basic01(t *testing.T) {
+	var v map[string]interface{}
+	checkoutSupportedRegions(t, true, connectivity.GaSupportRegions)
+	resourceId := "alicloud_ga_accelerator.default"
+	ra := resourceAttrInit(resourceId, AlicloudGaAcceleratorMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &GaService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeGaAccelerator")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAcc%sAlicloudGaAccelerator%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudGaAcceleratorBasicDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"accelerator_name":       name,
+					"description":            "accelerator",
+					"spec":                   "1",
+					"auto_use_coupon":        "true",
+					"duration":               "1",
+					"bandwidth_billing_type": "CDT",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"accelerator_name":       name,
+						"description":            "accelerator",
+						"spec":                   "1",
+						"auto_use_coupon":        "true",
+						"duration":               "1",
+						"bandwidth_billing_type": "CDT",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"accelerator_name": name + "update",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"accelerator_name": name + "update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": "accelerator_update",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": "accelerator_update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"spec": `2`,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"spec": "2",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"accelerator_name": name,
+					"description":      "accelerator",
+					"spec":             `1`,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"accelerator_name": name,
+						"description":      "accelerator",
+						"spec":             "1",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"auto_use_coupon", "duration"},
 			},
 		},
 	})
