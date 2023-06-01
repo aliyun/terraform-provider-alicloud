2d50918acadee2240ad72ae16812852140faf429
MrWolong <ly01269865@alibaba-inc.com>
2023-06-01 13:12:07 +0800

    resource/alicloud_sae_namespace: Added the field `namespace_short_id` and `enable_micro_registration`

--- alicloud/resource_alicloud_sae_namespace.go
+++ alicloud/resource_alicloud_sae_namespace.go
@@ -3,6 +3,7 @@ package alicloud
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	util "github.com/alibabacloud-go/tea-utils/service"
@@ -24,18 +25,31 @@ func resourceAlicloudSaeNamespace() *schema.Resource {
 			Delete: schema.DefaultTimeout(1 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
+			"namespace_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"namespace_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Computed:     true,
+				ExactlyOneOf: []string{"namespace_id", "namespace_short_id"},
+			},
+			"namespace_short_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
 			"namespace_description": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"namespace_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"namespace_name": {
-				Type:     schema.TypeString,
-				Required: true,
+			"enable_micro_registration": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
 			},
 		},
 	}
@@ -50,12 +64,27 @@ func resourceAlicloudSaeNamespaceCreate(d *schema.ResourceData, meta interface{}
 	if err != nil {
 		return WrapError(err)
 	}
+
+	request["NamespaceName"] = StringPointer(d.Get("namespace_name").(string))
+
+	if v, ok := d.GetOk("namespace_id"); ok {
+		request["NamespaceId"] = StringPointer(v.(string))
+	}
+
+	if v, ok := d.GetOk("namespace_short_id"); ok {
+		request["NameSpaceShortId"] = StringPointer(v.(string))
+	}
+
 	if v, ok := d.GetOk("namespace_description"); ok {
 		request["NamespaceDescription"] = StringPointer(v.(string))
 	}
-	request["NamespaceId"], request["NamespaceName"] = StringPointer(d.Get("namespace_id").(string)), StringPointer(d.Get("namespace_name").(string))
+
+	if v, ok := d.GetOkExists("enable_micro_registration"); ok {
+		request["EnableMicroRegistration"] = StringPointer(strconv.FormatBool(v.(bool)))
+	}
+
 	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer("2019-05-06"), nil, StringPointer("POST"), StringPointer("AK"), StringPointer(action), request, nil, nil, &util.RuntimeOptions{})
 		if err != nil {
 			if NeedRetry(err) {
@@ -66,28 +95,37 @@ func resourceAlicloudSaeNamespaceCreate(d *schema.ResourceData, meta interface{}
 		}
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_sae_namespace", "POST "+action, AlibabaCloudSdkGoERROR)
 	}
+
+	if fmt.Sprint(response["Success"]) == "false" {
+		return WrapError(fmt.Errorf("%s failed, response: %v", "POST "+action, response))
+	}
+
 	if respBody, isExist := response["body"]; isExist {
 		response = respBody.(map[string]interface{})
 	} else {
 		return WrapError(fmt.Errorf("%s failed, response: %v", "POST "+action, response))
 	}
-	addDebug(action, response, request)
-	if fmt.Sprint(response["Success"]) == "false" {
+
+	if responseData, ok := response["Data"].(map[string]interface{}); ok {
+		d.SetId(fmt.Sprint(responseData["NamespaceId"]))
+	} else {
 		return WrapError(fmt.Errorf("%s failed, response: %v", "POST "+action, response))
 	}
-	d.SetId(*request["NamespaceId"])
 
 	return resourceAlicloudSaeNamespaceRead(d, meta)
 }
+
 func resourceAlicloudSaeNamespaceRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	saeService := SaeService{client}
 	object, err := saeService.DescribeSaeNamespace(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
+		if !d.IsNewResource() && NotFoundError(err) {
 			log.Printf("[DEBUG] Resource alicloud_sae_namespace saeService.DescribeSaeNamespace Failed!!! %s", err)
 			d.SetId("")
 			return nil
@@ -95,11 +133,15 @@ func resourceAlicloudSaeNamespaceRead(d *schema.ResourceData, meta interface{}) 
 		return WrapError(err)
 	}
 
-	d.Set("namespace_id", d.Id())
-	d.Set("namespace_description", object["NamespaceDescription"])
 	d.Set("namespace_name", object["NamespaceName"])
+	d.Set("namespace_id", object["NamespaceId"])
+	d.Set("namespace_short_id", object["NameSpaceShortId"])
+	d.Set("namespace_description", object["NamespaceDescription"])
+	d.Set("enable_micro_registration", object["EnableMicroRegistration"])
+
 	return nil
 }
+
 func resourceAlicloudSaeNamespaceUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	var response map[string]interface{}
@@ -107,14 +149,26 @@ func resourceAlicloudSaeNamespaceUpdate(d *schema.ResourceData, meta interface{}
 	request := map[string]*string{
 		"NamespaceId": StringPointer(d.Id()),
 	}
+
 	if d.HasChange("namespace_name") {
 		update = true
 	}
 	request["NamespaceName"] = StringPointer(d.Get("namespace_name").(string))
+
 	if d.HasChange("namespace_description") {
 		update = true
 	}
-	request["NamespaceDescription"] = StringPointer(d.Get("namespace_description").(string))
+	if v, ok := d.GetOk("namespace_description"); ok {
+		request["NamespaceDescription"] = StringPointer(v.(string))
+	}
+
+	if d.HasChange("enable_micro_registration") {
+		update = true
+	}
+	if v, ok := d.GetOkExists("enable_micro_registration"); ok {
+		request["EnableMicroRegistration"] = StringPointer(strconv.FormatBool(v.(bool)))
+	}
+
 	if update {
 		action := "/pop/v1/paas/namespace"
 		conn, err := client.NewServerlessClient()
@@ -122,7 +176,7 @@ func resourceAlicloudSaeNamespaceUpdate(d *schema.ResourceData, meta interface{}
 			return WrapError(err)
 		}
 		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
 			response, err = conn.DoRequest(StringPointer("2019-05-06"), nil, StringPointer("PUT"), StringPointer("AK"), StringPointer(action), request, nil, nil, &util.RuntimeOptions{})
 			if err != nil {
 				if NeedRetry(err) {
@@ -133,24 +187,30 @@ func resourceAlicloudSaeNamespaceUpdate(d *schema.ResourceData, meta interface{}
 			}
 			return nil
 		})
+		addDebug(action, response, request)
+
 		if err != nil {
 			if IsExpectedErrors(err, []string{"InvalidNamespaceId.NotFound"}) {
 				return WrapErrorf(Error(GetNotFoundMessage("SAE:Namespace", d.Id())), NotFoundMsg, ProviderERROR)
 			}
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), "PUT "+action, AlibabaCloudSdkGoERROR)
 		}
+
+		if fmt.Sprint(response["Success"]) == "false" {
+			return WrapError(fmt.Errorf("%s failed, response: %v", "Put "+action, response))
+		}
+
 		if respBody, isExist := response["body"]; isExist {
 			response = respBody.(map[string]interface{})
 		} else {
 			return WrapError(fmt.Errorf("%s failed, response: %v", "Put "+action, response))
 		}
-		addDebug(action, response, request)
-		if fmt.Sprint(response["Success"]) == "false" {
-			return WrapError(fmt.Errorf("%s failed, response: %v", "Put "+action, response))
-		}
+
 	}
+
 	return resourceAlicloudSaeNamespaceRead(d, meta)
 }
+
 func resourceAlicloudSaeNamespaceDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	action := "/pop/v1/paas/namespace"
@@ -164,7 +224,7 @@ func resourceAlicloudSaeNamespaceDelete(d *schema.ResourceData, meta interface{}
 	}
 
 	wait := incrementalWait(3*time.Second, 1*time.Second)
-	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer("2019-05-06"), nil, StringPointer("DELETE"), StringPointer("AK"), StringPointer(action), request, nil, nil, &util.RuntimeOptions{})
 		if err != nil {
 			if IsExpectedErrors(err, []string{"InvalidNamespaceId.NotFound"}) || NeedRetry(err) {
@@ -175,20 +235,25 @@ func resourceAlicloudSaeNamespaceDelete(d *schema.ResourceData, meta interface{}
 		}
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), "DELETE "+action, AlibabaCloudSdkGoERROR)
 	}
+
+	if fmt.Sprint(response["Success"]) == "false" {
+		return WrapError(fmt.Errorf("%s failed, response: %v", "AlicloudSaeNamespaceDelete", response))
+	}
+
 	if respBody, isExist := response["body"]; isExist {
 		response = respBody.(map[string]interface{})
 	} else {
 		return WrapError(fmt.Errorf("%s failed, response: %v", "DELETE "+action, response))
 	}
-	addDebug(action, response, request)
+
 	if IsExpectedErrorCodes(fmt.Sprint(response["Code"]), []string{"InvalidNamespaceId.NotFound"}) {
 		return nil
 	}
-	if fmt.Sprint(response["Success"]) == "false" {
-		return WrapError(fmt.Errorf("%s failed, response: %v", "AlicloudSaeNamespaceDelete", response))
-	}
+
 	return nil
 }
--- alicloud/resource_alicloud_sae_namespace_test.go
+++ alicloud/resource_alicloud_sae_namespace_test.go
@@ -29,15 +29,17 @@ func TestAccAlicloudSAENamespace_basic0(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"namespace_name":        name,
-					"namespace_id":          fmt.Sprintf("%s:tftest%d", defaultRegionToTest, rand),
-					"namespace_description": "tftestaccdescription",
+					"namespace_name":            name,
+					"namespace_id":              fmt.Sprintf("%s:tftest%d", defaultRegionToTest, rand),
+					"namespace_description":     "tftestaccdescription",
+					"enable_micro_registration": "false",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"namespace_name":        name,
-						"namespace_id":          fmt.Sprintf("%s:tftest%d", defaultRegionToTest, rand),
-						"namespace_description": "tftestaccdescription",
+						"namespace_name":            name,
+						"namespace_id":              fmt.Sprintf("%s:tftest%d", defaultRegionToTest, rand),
+						"namespace_description":     "tftestaccdescription",
+						"enable_micro_registration": "false",
 					}),
 				),
 			},
@@ -76,6 +78,103 @@ func TestAccAlicloudSAENamespace_basic0(t *testing.T) {
 				),
 			},
 			{
+				Config: testAccConfig(map[string]interface{}{
+					"enable_micro_registration": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"enable_micro_registration": "true",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccAlicloudSAENamespace_basic1(t *testing.T) {
+	var v map[string]interface{}
+	checkoutSupportedRegions(t, true, connectivity.SaeSupportRegions)
+	resourceId := "alicloud_sae_namespace.default"
+	ra := resourceAttrInit(resourceId, AlicloudSAENamespaceMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &SaeService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeSaeNamespace")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1, 100)
+	name := fmt.Sprintf("tf-testacc%ssaenamespace%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudSAENamespaceBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name":            name,
+					"namespace_short_id":        fmt.Sprintf("tftest%d", rand),
+					"namespace_description":     "tftestaccdescription",
+					"enable_micro_registration": "false",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":            name,
+						"namespace_short_id":        CHECKSET,
+						"namespace_description":     "tftestaccdescription",
+						"enable_micro_registration": "false",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name": name + "update",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "update",
+						"namespace_description": "tftestaccdescription",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_description": "tftestaccdescriptionupdate",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "update",
+						"namespace_description": "tftestaccdescriptionupdate",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"namespace_name":        name + "updateall",
+					"namespace_description": "tftestaccdescriptionupdateall",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"namespace_name":        name + "updateall",
+						"namespace_description": "tftestaccdescriptionupdateall",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"enable_micro_registration": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"enable_micro_registration": "true",
+					}),
+				),
+			},
+			{
 				ResourceName:      resourceId,
 				ImportState:       true,
 				ImportStateVerify: true,
@@ -85,15 +184,15 @@ func TestAccAlicloudSAENamespace_basic0(t *testing.T) {
 }
 
 var AlicloudSAENamespaceMap0 = map[string]string{
-	"namespace_name":        CHECKSET,
-	"namespace_id":          CHECKSET,
-	"namespace_description": CHECKSET,
+	"namespace_id":              CHECKSET,
+	"namespace_short_id":        CHECKSET,
+	"enable_micro_registration": CHECKSET,
 }
 
 func AlicloudSAENamespaceBasicDependence0(name string) string {
 	return fmt.Sprintf(` 
-variable "name" {
-  default = "%s"
-}
+	variable "name" {
+  		default = "%s"
+	}
 `, name)
 }
