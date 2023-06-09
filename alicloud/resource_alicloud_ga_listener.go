b528897c651a78f4aee1d6b917bfcd5c09f6a80f
MrWolong <ly01269865@alibaba-inc.com>
2023-06-09 11:01:30 +0800

    resource/alicloud_ga_listener: Added the field forwarded_for_config

--- alicloud/resource_alicloud_ga_listener.go
+++ alicloud/resource_alicloud_ga_listener.go
@@ -29,54 +29,7 @@ func resourceAlicloudGaListener() *schema.Resource {
 			"accelerator_id": {
 				Type:     schema.TypeString,
 				Required: true,
-			},
-			"certificates": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": {
-							Type:     schema.TypeString,
-							Optional: true,
-						},
-					},
-				},
-			},
-			"client_affinity": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: StringInSlice([]string{"NONE", "SOURCE_IP"}, false),
-				Default:      "NONE",
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					if d.Get("protocol") == "UDP" && new == "SOURCE_IP" {
-						return true
-					}
-					return false
-				},
-			},
-			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"port_ranges": {
-				Type:     schema.TypeList,
-				Required: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"from_port": {
-							Type:     schema.TypeInt,
-							Required: true,
-						},
-						"to_port": {
-							Type:     schema.TypeInt,
-							Required: true,
-						},
-					},
-				},
+				ForceNew: true,
 			},
 			"protocol": {
 				Type:         schema.TypeString,
@@ -101,6 +54,83 @@ func resourceAlicloudGaListener() *schema.Resource {
 				Computed:     true,
 				ValidateFunc: StringInSlice([]string{"Standard", "CustomRouting"}, false),
 			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"client_affinity": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: StringInSlice([]string{"NONE", "SOURCE_IP"}, false),
+				Default:      "NONE",
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					if d.Get("protocol") == "UDP" && new == "SOURCE_IP" {
+						return true
+					}
+					return false
+				},
+			},
+			"certificates": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"port_ranges": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"from_port": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+						"to_port": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+					},
+				},
+			},
+			"forwarded_for_config": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"forwarded_for_ga_id_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"forwarded_for_ga_ap_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"forwarded_for_proto_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"forwarded_for_port_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"real_ip_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+					},
+				},
+			},
 			"status": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -119,38 +149,9 @@ func resourceAlicloudGaListenerCreate(d *schema.ResourceData, meta interface{}) 
 	if err != nil {
 		return WrapError(err)
 	}
+
+	request["RegionId"] = client.RegionId
 	request["AcceleratorId"] = d.Get("accelerator_id")
-	if v, ok := d.GetOk("certificates"); ok {
-		Certificates := make([]map[string]interface{}, len(v.([]interface{})))
-		for i, CertificatesValue := range v.([]interface{}) {
-			CertificatesMap := CertificatesValue.(map[string]interface{})
-			Certificates[i] = make(map[string]interface{})
-			Certificates[i]["Id"] = CertificatesMap["id"]
-		}
-		request["Certificates"] = Certificates
-
-	}
-
-	if v, ok := d.GetOk("client_affinity"); ok {
-		request["ClientAffinity"] = v
-	}
-
-	if v, ok := d.GetOk("description"); ok {
-		request["Description"] = v
-	}
-
-	if v, ok := d.GetOk("name"); ok {
-		request["Name"] = v
-	}
-
-	PortRanges := make([]map[string]interface{}, len(d.Get("port_ranges").([]interface{})))
-	for i, PortRangesValue := range d.Get("port_ranges").([]interface{}) {
-		PortRangesMap := PortRangesValue.(map[string]interface{})
-		PortRanges[i] = make(map[string]interface{})
-		PortRanges[i]["FromPort"] = PortRangesMap["from_port"]
-		PortRanges[i]["ToPort"] = PortRangesMap["to_port"]
-	}
-	request["PortRanges"] = PortRanges
 
 	if v, ok := d.GetOk("protocol"); ok {
 		request["Protocol"] = v
@@ -168,11 +169,57 @@ func resourceAlicloudGaListenerCreate(d *schema.ResourceData, meta interface{}) 
 		request["Type"] = v
 	}
 
-	request["RegionId"] = client.RegionId
+	if v, ok := d.GetOk("name"); ok {
+		request["Name"] = v
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		request["Description"] = v
+	}
+
+	if v, ok := d.GetOk("client_affinity"); ok {
+		request["ClientAffinity"] = v
+	}
+
+	if v, ok := d.GetOk("certificates"); ok {
+		Certificates := make([]map[string]interface{}, len(v.([]interface{})))
+		for i, CertificatesValue := range v.([]interface{}) {
+			CertificatesMap := CertificatesValue.(map[string]interface{})
+			Certificates[i] = make(map[string]interface{})
+			Certificates[i]["Id"] = CertificatesMap["id"]
+		}
+		request["Certificates"] = Certificates
+
+	}
+
+	PortRanges := make([]map[string]interface{}, len(d.Get("port_ranges").([]interface{})))
+	for i, PortRangesValue := range d.Get("port_ranges").([]interface{}) {
+		PortRangesMap := PortRangesValue.(map[string]interface{})
+		PortRanges[i] = make(map[string]interface{})
+		PortRanges[i]["FromPort"] = PortRangesMap["from_port"]
+		PortRanges[i]["ToPort"] = PortRangesMap["to_port"]
+	}
+	request["PortRanges"] = PortRanges
+
+	if v, ok := d.GetOk("forwarded_for_config"); ok {
+		forwardedForConfigMap := map[string]interface{}{}
+		for _, forwardedForConfigList := range v.([]interface{}) {
+			forwardedForConfigArg := forwardedForConfigList.(map[string]interface{})
+
+			forwardedForConfigMap["XForwardedForGaIdEnabled"] = forwardedForConfigArg["forwarded_for_ga_id_enabled"]
+			forwardedForConfigMap["XForwardedForGaApEnabled"] = forwardedForConfigArg["forwarded_for_ga_ap_enabled"]
+			forwardedForConfigMap["XForwardedForProtoEnabled"] = forwardedForConfigArg["forwarded_for_proto_enabled"]
+			forwardedForConfigMap["XForwardedForPortEnabled"] = forwardedForConfigArg["forwarded_for_port_enabled"]
+			forwardedForConfigMap["XRealIpEnabled"] = forwardedForConfigArg["real_ip_enabled"]
+		}
+
+		request["XForwardedForConfig"] = forwardedForConfigMap
+	}
+
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate))), func() *resource.RetryError {
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
 		request["ClientToken"] = buildClientToken("CreateListener")
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
@@ -190,6 +237,7 @@ func resourceAlicloudGaListenerCreate(d *schema.ResourceData, meta interface{}) 
 	}
 
 	d.SetId(fmt.Sprint(response["ListenerId"]))
+
 	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 30*time.Second, gaService.GaListenerStateRefreshFunc(d.Id(), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
@@ -202,14 +250,24 @@ func resourceAlicloudGaListenerRead(d *schema.ResourceData, meta interface{}) er
 	client := meta.(*connectivity.AliyunClient)
 	gaService := GaService{client}
 	object, err := gaService.DescribeGaListener(d.Id())
-	if !d.IsNewResource() && NotFoundError(err) {
-		if NotFoundError(err) {
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
 			log.Printf("[DEBUG] Resource alicloud_ga_listener gaService.DescribeGaListener Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
+
+	d.Set("accelerator_id", object["AcceleratorId"])
+	d.Set("protocol", object["Protocol"])
+	d.Set("proxy_protocol", object["ProxyProtocol"])
+	d.Set("security_policy_id", object["SecurityPolicyId"])
+	d.Set("listener_type", object["Type"])
+	d.Set("name", object["Name"])
+	d.Set("description", object["Description"])
+	d.Set("client_affinity", object["ClientAffinity"])
+	d.Set("status", object["State"])
 
 	certificates := make([]map[string]interface{}, 0)
 	if certificatesList, ok := object["Certificates"].([]interface{}); ok {
@@ -226,12 +284,6 @@ func resourceAlicloudGaListenerRead(d *schema.ResourceData, meta interface{}) er
 	if err := d.Set("certificates", certificates); err != nil {
 		return WrapError(err)
 	}
-
-	d.Set("accelerator_id", object["AcceleratorId"])
-	d.Set("client_affinity", object["ClientAffinity"])
-	d.Set("description", object["Description"])
-	d.Set("name", object["Name"])
-	d.Set("proxy_protocol", object["ProxyProtocol"])
 
 	portRanges := make([]map[string]interface{}, 0)
 	if portRangesList, ok := object["PortRanges"].([]interface{}); ok {
@@ -250,10 +302,35 @@ func resourceAlicloudGaListenerRead(d *schema.ResourceData, meta interface{}) er
 		return WrapError(err)
 	}
 
-	d.Set("protocol", object["Protocol"])
-	d.Set("security_policy_id", object["SecurityPolicyId"])
-	d.Set("listener_type", object["Type"])
-	d.Set("status", object["State"])
+	if forwardedForConfig, ok := object["XForwardedForConfig"]; ok {
+		forwardedForConfigMaps := make([]map[string]interface{}, 0)
+		forwardedForConfigArg := forwardedForConfig.(map[string]interface{})
+		forwardedForConfigMap := map[string]interface{}{}
+
+		if forwardedForGaIdEnabled, ok := forwardedForConfigArg["XForwardedForGaIdEnabled"]; ok {
+			forwardedForConfigMap["forwarded_for_ga_id_enabled"] = forwardedForGaIdEnabled
+		}
+
+		if forwardedForGaApEnabled, ok := forwardedForConfigArg["XForwardedForGaApEnabled"]; ok {
+			forwardedForConfigMap["forwarded_for_ga_ap_enabled"] = forwardedForGaApEnabled
+		}
+
+		if forwardedForProtoEnabled, ok := forwardedForConfigArg["XForwardedForProtoEnabled"]; ok {
+			forwardedForConfigMap["forwarded_for_proto_enabled"] = forwardedForProtoEnabled
+		}
+
+		if forwardedForPortEnabled, ok := forwardedForConfigArg["XForwardedForPortEnabled"]; ok {
+			forwardedForConfigMap["forwarded_for_port_enabled"] = forwardedForPortEnabled
+		}
+
+		if realIpEnabled, ok := forwardedForConfigArg["XRealIpEnabled"]; ok {
+			forwardedForConfigMap["real_ip_enabled"] = realIpEnabled
+		}
+
+		forwardedForConfigMaps = append(forwardedForConfigMaps, forwardedForConfigMap)
+
+		d.Set("forwarded_for_config", forwardedForConfigMaps)
+	}
 
 	return nil
 }
@@ -268,6 +345,7 @@ func resourceAlicloudGaListenerUpdate(d *schema.ResourceData, meta interface{}) 
 	var response map[string]interface{}
 	update := false
 	request := map[string]interface{}{
+		"RegionId":   client.RegionId,
 		"ListenerId": d.Id(),
 	}
 
@@ -284,18 +362,22 @@ func resourceAlicloudGaListenerUpdate(d *schema.ResourceData, meta interface{}) 
 		}
 
 	}
+
 	if d.HasChange("client_affinity") {
 		update = true
 		request["ClientAffinity"] = d.Get("client_affinity")
 	}
+
 	if d.HasChange("description") {
 		update = true
 		request["Description"] = d.Get("description")
 	}
+
 	if d.HasChange("name") {
 		update = true
 		request["Name"] = d.Get("name")
 	}
+
 	if d.HasChange("port_ranges") {
 		update = true
 		PortRanges := make([]map[string]interface{}, len(d.Get("port_ranges").([]interface{})))
@@ -308,10 +390,12 @@ func resourceAlicloudGaListenerUpdate(d *schema.ResourceData, meta interface{}) 
 		request["PortRanges"] = PortRanges
 
 	}
+
 	if d.HasChange("protocol") {
 		update = true
 		request["Protocol"] = d.Get("protocol")
 	}
+
 	if d.HasChange("security_policy_id") {
 		update = true
 		request["SecurityPolicyId"] = d.Get("security_policy_id")
@@ -324,13 +408,30 @@ func resourceAlicloudGaListenerUpdate(d *schema.ResourceData, meta interface{}) 
 		request["ProxyProtocol"] = v
 	}
 
-	request["RegionId"] = client.RegionId
+	if d.HasChange("forwarded_for_config") {
+		update = true
+	}
+	if v, ok := d.GetOk("forwarded_for_config"); ok {
+		forwardedForConfigMap := map[string]interface{}{}
+		for _, forwardedForConfigList := range v.([]interface{}) {
+			forwardedForConfigArg := forwardedForConfigList.(map[string]interface{})
+
+			forwardedForConfigMap["XForwardedForGaIdEnabled"] = forwardedForConfigArg["forwarded_for_ga_id_enabled"]
+			forwardedForConfigMap["XForwardedForGaApEnabled"] = forwardedForConfigArg["forwarded_for_ga_ap_enabled"]
+			forwardedForConfigMap["XForwardedForProtoEnabled"] = forwardedForConfigArg["forwarded_for_proto_enabled"]
+			forwardedForConfigMap["XForwardedForPortEnabled"] = forwardedForConfigArg["forwarded_for_port_enabled"]
+			forwardedForConfigMap["XRealIpEnabled"] = forwardedForConfigArg["real_ip_enabled"]
+		}
+
+		request["XForwardedForConfig"] = forwardedForConfigMap
+	}
+
 	if update {
 		action := "UpdateListener"
 		runtime := util.RuntimeOptions{}
 		runtime.SetAutoretry(true)
 		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(client.GetRetryTimeout(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate))), func() *resource.RetryError {
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
 			request["ClientToken"] = buildClientToken("UpdateListener")
 			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 			if err != nil {
@@ -340,17 +441,20 @@ func resourceAlicloudGaListenerUpdate(d *schema.ResourceData, meta interface{}) 
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, response, request)
 			return nil
 		})
+		addDebug(action, response, request)
+
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
+
 		stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 30*time.Second, gaService.GaListenerStateRefreshFunc(d.Id(), []string{}))
 		if _, err := stateConf.WaitForState(); err != nil {
 			return WrapErrorf(err, IdMsg, d.Id())
 		}
 	}
+
 	return resourceAlicloudGaListenerRead(d, meta)
 }
 
@@ -371,7 +475,7 @@ func resourceAlicloudGaListenerDelete(d *schema.ResourceData, meta interface{}) 
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete))), func() *resource.RetryError {
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
 		request["ClientToken"] = buildClientToken("DeleteListener")
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
@@ -381,12 +485,14 @@ func resourceAlicloudGaListenerDelete(d *schema.ResourceData, meta interface{}) 
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, gaService.GaListenerStateRefreshFunc(d.Id(), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
--- website/docs/r/ga_listener.html.markdown
+++ website/docs/r/ga_listener.html.markdown
@@ -53,57 +53,72 @@ resource "alicloud_ga_listener" "default" {
 
 The following arguments are supported:
 
-* `accelerator_id` - (Required) The accelerator id.
-* `certificates` - (Optional) The certificates of the listener. See [`certificates`](#certificates) below.
-
--> **NOTE:** This parameter needs to be configured only for monitoring of the HTTPS protocol.
-             
-* `client_affinity` - (Optional) The clientAffinity of the listener. Default value is `NONE`. Valid values:
-    `NONE`: client affinity is not maintained, that is, connection requests from the same client cannot always be directed to the same terminal node.
-    `SOURCE_IP`: maintain client affinity. When a client accesses a stateful application, all requests from the same client can be directed to the same terminal node, regardless of the source port and protocol.
-* `description` - (Optional) The description of the listener.
+* `accelerator_id` - (Required, ForceNew) The accelerator id.
+* `protocol` - (Optional) Type of network transport protocol monitored. Default value: `TCP`. Valid values: `TCP`, `UDP`, `HTTP`, `HTTPS`.
+* `proxy_protocol` - (Optional, Bool) The proxy protocol of the listener. Default value: `false`. Valid values:
+  - `true`: Turn on the keep client source IP function. After it is turned on, the back-end service is supported to view the original IP address of the client.
+  - `false`: keep client source IP function is not turned on.
+* `security_policy_id` - (Optional, Computed, Available since v1.183.0) The ID of the security policy. **NOTE:** Only `HTTPS` listeners support this parameter. Valid values:
+  - `tls_cipher_policy_1_0`:
+    - Supported TLS versions: TLS 1.0, TLS 1.1, and TLS 1.2.
+    - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
+  - `tls_cipher_policy_1_1`:
+    - Supported TLS versions: TLS 1.1 and TLS 1.2.
+    - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
+  - `tls_cipher_policy_1_2`:
+    - Supported TLS version: TLS 1.2.
+    - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
+  - `tls_cipher_policy_1_2_strict`:
+    - Supported TLS version: TLS 1.2.
+    - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, ECDHE-RSA-AES128-SHA, and ECDHE-RSA-AES256-SHA.
+  - `tls_cipher_policy_1_2_strict_with_1_3`:
+    - Supported TLS versions: TLS 1.2 and TLS 1.3.
+    - Supported cipher suites: TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256, TLS_AES_128_CCM_SHA256, TLS_AES_128_CCM_8_SHA256, ECDHE-ECDSA-AES128-GCM-SHA256, ECDHE-ECDSA-AES256-GCM-SHA384, ECDHE-ECDSA-AES128-SHA256, ECDHE-ECDSA-AES256-SHA384, ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, ECDHE-ECDSA-AES128-SHA, ECDHE-ECDSA-AES256-SHA, ECDHE-RSA-AES128-SHA, and ECDHE-RSA-AES256-SHA.
+* `listener_type` - (Optional, ForceNew, Computed, Available since v1.196.0) The routing type of the listener. Default Value: `Standard`. Valid values:
+  - `Standard`: intelligent routing.
+  - `CustomRouting`: custom routing.
 * `name` - (Optional) The name of the listener. The length of the name is 2-128 characters. It starts with uppercase and lowercase letters or Chinese characters. It can contain numbers and underscores and dashes.
-* `port_ranges` - (Required) The portRanges of the listener. See [`port_ranges`](#port_ranges) below.
+* `description` - (Optional) The description of the listener.
+* `client_affinity` - (Optional) The clientAffinity of the listener. Default value: `NONE`. Valid values:
+  - `NONE`: client affinity is not maintained, that is, connection requests from the same client cannot always be directed to the same terminal node.
+  - `SOURCE_IP`: maintain client affinity. When a client accesses a stateful application, all requests from the same client can be directed to the same terminal node, regardless of the source port and protocol.
+* `certificates` - (Optional, Set) The certificates of the listener. See [`certificates`](#certificates) below.
+-> **NOTE:** This parameter needs to be configured only for monitoring of the `HTTPS` protocol.
+* `port_ranges` - (Required, Set) The portRanges of the listener. See [`port_ranges`](#port_ranges) below.
+-> **NOTE:** For `HTTP` or `HTTPS` protocol monitoring, only one monitoring port can be configured, that is, the start monitoring port and end monitoring port should be the same.
+* `forwarded_for_config` - (Optional, Set, Available since v1.207.0) The XForward headers. See [`forwarded_for_config`](#forwarded_for_config) below.
 
--> **NOTE:** For HTTP or HTTPS protocol monitoring, only one monitoring port can be configured, that is, the start monitoring port and end monitoring port should be the same. 
+### `forwarded_for_config`
 
-* `protocol` - (Optional) Type of network transport protocol monitored. Default value is `TCP`. Valid values: `TCP`, `UDP`, `HTTP`, `HTTPS`.
+The forwarded_for_config supports the following:
 
--> **NOTE:** At present, the white list of HTTP and HTTPS monitoring protocols is open. If you need to use it, please submit a work order.
-             
-* `proxy_protocol` - (Optional) The proxy protocol of the listener. Default value is `false`. Valid values:
-    `true`: Turn on the keep client source IP function. After it is turned on, the back-end service is supported to view the original IP address of the client. 
-    `false`: keep client source IP function is not turned on.
-* `security_policy_id` - (Optional, Computed, Available in v1.183.0+) The ID of the security policy. **NOTE:** Only HTTPS listeners support this parameter. Valid values:
-    - `tls_cipher_policy_1_0`:
-      - Supported TLS versions: TLS 1.0, TLS 1.1, and TLS 1.2.
-      - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
-    - `tls_cipher_policy_1_1`:
-      - Supported TLS versions: TLS 1.1 and TLS 1.2.
-      - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
-    - `tls_cipher_policy_1_2`:
-      - Supported TLS version: TLS 1.2.
-      - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, AES128-GCM-SHA256, AES256-GCM-SHA384, AES128-SHA256, AES256-SHA256, ECDHE-RSA-AES128-SHA, ECDHE-RSA-AES256-SHA, AES128-SHA, AES256-SHA, and DES-CBC3-SHA.
-    - `tls_cipher_policy_1_2_strict`:
-      - Supported TLS version: TLS 1.2.
-      - Supported cipher suites: ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, ECDHE-RSA-AES128-SHA, and ECDHE-RSA-AES256-SHA.
-    - `tls_cipher_policy_1_2_strict_with_1_3`:
-      - Supported TLS versions: TLS 1.2 and TLS 1.3.
-      - Supported cipher suites: TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256, TLS_AES_128_CCM_SHA256, TLS_AES_128_CCM_8_SHA256, ECDHE-ECDSA-AES128-GCM-SHA256, ECDHE-ECDSA-AES256-GCM-SHA384, ECDHE-ECDSA-AES128-SHA256, ECDHE-ECDSA-AES256-SHA384, ECDHE-RSA-AES128-GCM-SHA256, ECDHE-RSA-AES256-GCM-SHA384, ECDHE-RSA-AES128-SHA256, ECDHE-RSA-AES256-SHA384, ECDHE-ECDSA-AES128-SHA, ECDHE-ECDSA-AES256-SHA, ECDHE-RSA-AES128-SHA, and ECDHE-RSA-AES256-SHA.
-* `listener_type` - (Optional, ForceNew, Computed, Available in v1.196.0+) The routing type of the listener. Default Value: `Standard`. Valid values:
-    - `Standard`: intelligent routing.
-    - `CustomRouting`: custom routing.
+* `forwarded_for_ga_id_enabled` - (Optional, Bool) Specifies whether to use the GA-ID header to retrieve the ID of the GA instance. Default value: `false`. Valid values:
+  - `true`: yes.
+  - `false `: no.
+* `forwarded_for_ga_ap_enabled` - (Optional, Bool) Specifies whether to use the GA-AP header to retrieve the information about acceleration regions. Default value: `false`. Valid values:
+  - `true`: yes.
+  - `false `: no.
+* `forwarded_for_proto_enabled` - (Optional, Bool) Specifies whether to use the GA-X-Forward-Proto header to retrieve the listener protocol of the GA instance. Default value: `false`. Valid values:
+  - `true`: yes.
+  - `false `: no.
+* `forwarded_for_port_enabled` - (Optional, Bool) Specifies whether to use the GA-X-Forward-Port header to retrieve the listener ports of the GA instance. Default value: `false`. Valid values:
+  - `true`: yes.
+  - `false `: no.
+* `real_ip_enabled` - (Optional, Bool) Specifies whether to use the X-Real-IP header to retrieve client IP addresses. Default value: `false`. Valid values:
+  - `true`: yes.
+  - `false `: no.
+-> **NOTE:** These `forwarded_for_ga_id_enabled`, `forwarded_for_ga_ap_enabled`, `forwarded_for_proto_enabled`, `forwarded_for_port_enabled`, `real_ip_enabled` are available only when you create an `HTTPS` or `HTTP` listener.
 
 ### `port_ranges`
 
-The port_ranges supports the following: 
+The port_ranges supports the following:
 
-* `from_port` - (Required) The initial listening port used to receive requests and forward them to terminal nodes.
-* `to_port` - (Required) The end listening port used to receive requests and forward them to terminal nodes.
+* `from_port` - (Required, Int) The initial listening port used to receive requests and forward them to terminal nodes.
+* `to_port` - (Required, Int) The end listening port used to receive requests and forward them to terminal nodes.
 
 ### `certificates`
 
-The certificates supports the following: 
+The certificates supports the following:
 
 * `id` - (Optional) The id of the certificate.
 
