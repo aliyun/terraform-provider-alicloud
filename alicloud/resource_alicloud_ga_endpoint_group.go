7446e81d125359c185231af1425236fc34746d69
MrWolong <ly01269865@alibaba-inc.com>
2023-06-12 13:36:57 +0800

    resource/alicloud_ga_endpoint_group: Added the field enable_proxy_protocol and tags

--- alicloud/resource_alicloud_ga_endpoint_group.go
+++ alicloud/resource_alicloud_ga_endpoint_group.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 func resourceAlicloudGaEndpointGroup() *schema.Resource {
@@ -32,36 +31,10 @@ func resourceAlicloudGaEndpointGroup() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
-			"description": {
+			"listener_id": {
 				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"endpoint_configurations": {
-				Type:     schema.TypeList,
 				Required: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"enable_clientip_preservation": {
-							Type:     schema.TypeBool,
-							Optional: true,
-							Computed: true,
-						},
-						"endpoint": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"type": {
-							Type:         schema.TypeString,
-							Required:     true,
-							ValidateFunc: validation.StringInSlice([]string{"Domain", "Ip", "PublicIp", "ECS", "SLB"}, false),
-						},
-						"weight": {
-							Type:         schema.TypeInt,
-							Required:     true,
-							ValidateFunc: validation.IntBetween(0, 255),
-						},
-					},
-				},
+				ForceNew: true,
 			},
 			"endpoint_group_region": {
 				Type:     schema.TypeString,
@@ -73,12 +46,12 @@ func resourceAlicloudGaEndpointGroup() *schema.Resource {
 				Optional:     true,
 				ForceNew:     true,
 				Computed:     true,
-				ValidateFunc: validation.StringInSlice([]string{"default", "virtual"}, false),
+				ValidateFunc: StringInSlice([]string{"default", "virtual"}, false),
 			},
 			"endpoint_request_protocol": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.StringInSlice([]string{"HTTP", "HTTPS"}, false),
+				ValidateFunc: StringInSlice([]string{"HTTP", "HTTPS"}, false),
 			},
 			"health_check_interval_seconds": {
 				Type:     schema.TypeInt,
@@ -95,16 +68,56 @@ func resourceAlicloudGaEndpointGroup() *schema.Resource {
 			"health_check_protocol": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.StringInSlice([]string{"http", "https", "tcp"}, false),
+				ValidateFunc: StringInSlice([]string{"http", "https", "tcp"}, false),
 			},
-			"listener_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+			"threshold_count": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"traffic_percentage": {
+				Type:     schema.TypeInt,
+				Optional: true,
 			},
 			"name": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": tagsSchema(),
+			"endpoint_configurations": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"endpoint": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"type": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: StringInSlice([]string{"Domain", "Ip", "PublicIp", "ECS", "SLB"}, false),
+						},
+						"weight": {
+							Type:         schema.TypeInt,
+							Required:     true,
+							ValidateFunc: IntBetween(0, 255),
+						},
+						"enable_proxy_protocol": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"enable_clientip_preservation": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
 			},
 			"port_overrides": {
 				Type:     schema.TypeList,
@@ -127,15 +140,6 @@ func resourceAlicloudGaEndpointGroup() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"threshold_count": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default:  3,
-			},
-			"traffic_percentage": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
 		},
 	}
 }
@@ -150,22 +154,10 @@ func resourceAlicloudGaEndpointGroupCreate(d *schema.ResourceData, meta interfac
 	if err != nil {
 		return WrapError(err)
 	}
+
+	request["RegionId"] = client.RegionId
 	request["AcceleratorId"] = d.Get("accelerator_id")
-	if v, ok := d.GetOk("description"); ok {
-		request["Description"] = v
-	}
-
-	EndpointConfigurations := make([]map[string]interface{}, len(d.Get("endpoint_configurations").([]interface{})))
-	for i, EndpointConfigurationsValue := range d.Get("endpoint_configurations").([]interface{}) {
-		EndpointConfigurationsMap := EndpointConfigurationsValue.(map[string]interface{})
-		EndpointConfigurations[i] = make(map[string]interface{})
-		EndpointConfigurations[i]["EnableClientIPPreservation"] = EndpointConfigurationsMap["enable_clientip_preservation"]
-		EndpointConfigurations[i]["Endpoint"] = EndpointConfigurationsMap["endpoint"]
-		EndpointConfigurations[i]["Type"] = EndpointConfigurationsMap["type"]
-		EndpointConfigurations[i]["Weight"] = EndpointConfigurationsMap["weight"]
-	}
-
-	request["EndpointConfigurations"] = EndpointConfigurations
+	request["ListenerId"] = d.Get("listener_id")
 	request["EndpointGroupRegion"] = d.Get("endpoint_group_region")
 
 	if v, ok := d.GetOk("endpoint_group_type"); ok {
@@ -192,10 +184,33 @@ func resourceAlicloudGaEndpointGroupCreate(d *schema.ResourceData, meta interfac
 		request["HealthCheckProtocol"] = v
 	}
 
-	request["ListenerId"] = d.Get("listener_id")
+	if v, ok := d.GetOk("threshold_count"); ok {
+		request["ThresholdCount"] = v
+	}
+
+	if v, ok := d.GetOk("traffic_percentage"); ok {
+		request["TrafficPercentage"] = v
+	}
+
 	if v, ok := d.GetOk("name"); ok {
 		request["Name"] = v
 	}
+
+	if v, ok := d.GetOk("description"); ok {
+		request["Description"] = v
+	}
+
+	EndpointConfigurations := make([]map[string]interface{}, len(d.Get("endpoint_configurations").([]interface{})))
+	for i, EndpointConfigurationsValue := range d.Get("endpoint_configurations").([]interface{}) {
+		EndpointConfigurationsMap := EndpointConfigurationsValue.(map[string]interface{})
+		EndpointConfigurations[i] = make(map[string]interface{})
+		EndpointConfigurations[i]["Endpoint"] = EndpointConfigurationsMap["endpoint"]
+		EndpointConfigurations[i]["Type"] = EndpointConfigurationsMap["type"]
+		EndpointConfigurations[i]["Weight"] = EndpointConfigurationsMap["weight"]
+		EndpointConfigurations[i]["EnableProxyProtocol"] = EndpointConfigurationsMap["enable_proxy_protocol"]
+		EndpointConfigurations[i]["EnableClientIPPreservation"] = EndpointConfigurationsMap["enable_clientip_preservation"]
+	}
+	request["EndpointConfigurations"] = EndpointConfigurations
 
 	if v, ok := d.GetOk("port_overrides"); ok {
 		PortOverrides := make([]map[string]interface{}, len(v.([]interface{})))
@@ -206,16 +221,6 @@ func resourceAlicloudGaEndpointGroupCreate(d *schema.ResourceData, meta interfac
 			PortOverrides[i]["ListenerPort"] = PortOverridesMap["listener_port"]
 		}
 		request["PortOverrides"] = PortOverrides
-
-	}
-
-	request["RegionId"] = client.RegionId
-	if v, ok := d.GetOk("threshold_count"); ok {
-		request["ThresholdCount"] = v
-	}
-
-	if v, ok := d.GetOk("traffic_percentage"); ok {
-		request["TrafficPercentage"] = v
 	}
 
 	runtime := util.RuntimeOptions{}
@@ -225,26 +230,28 @@ func resourceAlicloudGaEndpointGroupCreate(d *schema.ResourceData, meta interfac
 		request["ClientToken"] = buildClientToken("CreateEndpointGroup")
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
-			if IsExpectedErrors(err, []string{"GA_NOT_STEADY", "StateError.Accelerator", "StateError.EndPointGroup", "NotActive.Listener"}) {
+			if IsExpectedErrors(err, []string{"GA_NOT_STEADY", "StateError.Accelerator", "StateError.EndPointGroup", "NotActive.Listener"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ga_endpoint_group", action, AlibabaCloudSdkGoERROR)
 	}
 
 	d.SetId(fmt.Sprint(response["EndpointGroupId"]))
+
 	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 30*time.Second, gaService.GaEndpointGroupStateRefreshFunc(d.Id(), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
 
-	return resourceAlicloudGaEndpointGroupRead(d, meta)
+	return resourceAlicloudGaEndpointGroupUpdate(d, meta)
 }
 
 func resourceAlicloudGaEndpointGroupRead(d *schema.ResourceData, meta interface{}) error {
@@ -252,7 +259,7 @@ func resourceAlicloudGaEndpointGroupRead(d *schema.ResourceData, meta interface{
 	gaService := GaService{client}
 	object, err := gaService.DescribeGaEndpointGroup(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
+		if !d.IsNewResource() && NotFoundError(err) {
 			log.Printf("[DEBUG] Resource alicloud_ga_endpoint_group gaService.DescribeGaEndpointGroup Failed!!! %s", err)
 			d.SetId("")
 			return nil
@@ -266,16 +273,18 @@ func resourceAlicloudGaEndpointGroupRead(d *schema.ResourceData, meta interface{
 		for _, v := range endpointConfigurationsList {
 			if m1, ok := v.(map[string]interface{}); ok {
 				temp1 := map[string]interface{}{
-					"enable_clientip_preservation": m1["EnableClientIPPreservation"],
 					"endpoint":                     m1["Endpoint"],
 					"type":                         m1["Type"],
 					"weight":                       m1["Weight"],
+					"enable_proxy_protocol":        m1["EnableProxyProtocol"],
+					"enable_clientip_preservation": m1["EnableClientIPPreservation"],
 				}
 				endpointConfigurations = append(endpointConfigurations, temp1)
 
 			}
 		}
 	}
+
 	if err := d.Set("endpoint_configurations", endpointConfigurations); err != nil {
 		return WrapError(err)
 	}
@@ -305,9 +314,18 @@ func resourceAlicloudGaEndpointGroupRead(d *schema.ResourceData, meta interface{
 	if err := d.Set("port_overrides", portOverrides); err != nil {
 		return WrapError(err)
 	}
+
 	d.Set("status", object["State"])
 	d.Set("threshold_count", formatInt(object["ThresholdCount"]))
 	d.Set("traffic_percentage", formatInt(object["TrafficPercentage"]))
+
+	listTagResourcesObject, err := gaService.ListTagResources(d.Id(), "endpointgroup")
+	if err != nil {
+		return WrapError(err)
+	}
+
+	d.Set("tags", tagsToMap(listTagResourcesObject))
+
 	return nil
 }
 
@@ -323,48 +341,77 @@ func resourceAlicloudGaEndpointGroupUpdate(d *schema.ResourceData, meta interfac
 	request := map[string]interface{}{
 		"EndpointGroupId": d.Id(),
 	}
-	if d.HasChange("endpoint_configurations") {
+
+	if d.HasChange("tags") {
+		if err := gaService.SetResourceTags(d, "endpointgroup"); err != nil {
+			return WrapError(err)
+		}
+		d.SetPartial("tags")
+	}
+
+	request["RegionId"] = client.RegionId
+	request["EndpointGroupRegion"] = d.Get("endpoint_group_region")
+
+	if !d.IsNewResource() && d.HasChange("health_check_interval_seconds") {
+		update = true
+		request["HealthCheckIntervalSeconds"] = d.Get("health_check_interval_seconds")
+	}
+
+	if !d.IsNewResource() && d.HasChange("health_check_path") {
+		update = true
+		request["HealthCheckPath"] = d.Get("health_check_path")
+	}
+
+	if !d.IsNewResource() && d.HasChange("health_check_port") {
+		update = true
+		request["HealthCheckPort"] = d.Get("health_check_port")
+	}
+
+	if !d.IsNewResource() && d.HasChange("health_check_protocol") {
+		update = true
+		request["HealthCheckProtocol"] = d.Get("health_check_protocol")
+	}
+
+	if !d.IsNewResource() && d.HasChange("name") {
+		update = true
+		request["Name"] = d.Get("name")
+	}
+
+	if !d.IsNewResource() && d.HasChange("description") {
+		update = true
+		request["Description"] = d.Get("description")
+	}
+
+	if !d.IsNewResource() && d.HasChange("threshold_count") {
+		update = true
+	}
+	if v, ok := d.GetOk("threshold_count"); ok {
+		request["ThresholdCount"] = v
+	}
+
+	if !d.IsNewResource() && d.HasChange("traffic_percentage") {
+		update = true
+	}
+	if v, ok := d.GetOkExists("traffic_percentage"); ok {
+		request["TrafficPercentage"] = v
+	}
+
+	if !d.IsNewResource() && d.HasChange("endpoint_configurations") {
 		update = true
 	}
 	EndpointConfigurations := make([]map[string]interface{}, len(d.Get("endpoint_configurations").([]interface{})))
 	for i, EndpointConfigurationsValue := range d.Get("endpoint_configurations").([]interface{}) {
 		EndpointConfigurationsMap := EndpointConfigurationsValue.(map[string]interface{})
 		EndpointConfigurations[i] = make(map[string]interface{})
-		EndpointConfigurations[i]["EnableClientIPPreservation"] = EndpointConfigurationsMap["enable_clientip_preservation"]
 		EndpointConfigurations[i]["Endpoint"] = EndpointConfigurationsMap["endpoint"]
 		EndpointConfigurations[i]["Type"] = EndpointConfigurationsMap["type"]
 		EndpointConfigurations[i]["Weight"] = EndpointConfigurationsMap["weight"]
+		EndpointConfigurations[i]["EnableProxyProtocol"] = EndpointConfigurationsMap["enable_proxy_protocol"]
+		EndpointConfigurations[i]["EnableClientIPPreservation"] = EndpointConfigurationsMap["enable_clientip_preservation"]
 	}
-
 	request["EndpointConfigurations"] = EndpointConfigurations
-	request["EndpointGroupRegion"] = d.Get("endpoint_group_region")
-	request["RegionId"] = client.RegionId
 
-	if d.HasChange("description") {
-		update = true
-		request["Description"] = d.Get("description")
-	}
-	if d.HasChange("health_check_interval_seconds") {
-		update = true
-		request["HealthCheckIntervalSeconds"] = d.Get("health_check_interval_seconds")
-	}
-	if d.HasChange("health_check_path") {
-		update = true
-		request["HealthCheckPath"] = d.Get("health_check_path")
-	}
-	if d.HasChange("health_check_port") {
-		update = true
-		request["HealthCheckPort"] = d.Get("health_check_port")
-	}
-	if d.HasChange("health_check_protocol") {
-		update = true
-		request["HealthCheckProtocol"] = d.Get("health_check_protocol")
-	}
-	if d.HasChange("name") {
-		update = true
-		request["Name"] = d.Get("name")
-	}
-	if d.HasChange("port_overrides") {
+	if !d.IsNewResource() && d.HasChange("port_overrides") {
 		update = true
 	}
 	PortOverrides := make([]map[string]interface{}, len(d.Get("port_overrides").([]interface{})))
@@ -375,20 +422,6 @@ func resourceAlicloudGaEndpointGroupUpdate(d *schema.ResourceData, meta interfac
 		PortOverrides[i]["ListenerPort"] = PortOverridesMap["listener_port"]
 	}
 	request["PortOverrides"] = PortOverrides
-
-	if d.HasChange("threshold_count") {
-		update = true
-	}
-	if v, ok := d.GetOk("threshold_count"); ok {
-		request["ThresholdCount"] = v
-	}
-
-	if d.HasChange("traffic_percentage") {
-		update = true
-	}
-	if v, ok := d.GetOkExists("traffic_percentage"); ok {
-		request["TrafficPercentage"] = v
-	}
 
 	if update {
 		if _, ok := d.GetOk("endpoint_request_protocol"); ok {
@@ -402,23 +435,37 @@ func resourceAlicloudGaEndpointGroupUpdate(d *schema.ResourceData, meta interfac
 			request["ClientToken"] = buildClientToken("UpdateEndpointGroup")
 			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 			if err != nil {
-				if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.EndPointGroup"}) {
+				if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.EndPointGroup"}) || NeedRetry(err) {
 					wait()
 					return resource.RetryableError(err)
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, response, request)
 			return nil
 		})
+		addDebug(action, response, request)
+
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
+
 		stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 30*time.Second, gaService.GaEndpointGroupStateRefreshFunc(d.Id(), []string{}))
 		if _, err := stateConf.WaitForState(); err != nil {
 			return WrapErrorf(err, IdMsg, d.Id())
 		}
+
+		d.SetPartial("health_check_interval_seconds")
+		d.SetPartial("health_check_path")
+		d.SetPartial("health_check_port")
+		d.SetPartial("health_check_protocol")
+		d.SetPartial("name")
+		d.SetPartial("description")
+		d.SetPartial("threshold_count")
+		d.SetPartial("traffic_percentage")
+		d.SetPartial("endpoint_configurations")
+		d.SetPartial("port_overrides")
 	}
+
 	return resourceAlicloudGaEndpointGroupRead(d, meta)
 }
 
@@ -443,21 +490,24 @@ func resourceAlicloudGaEndpointGroupDelete(d *schema.ResourceData, meta interfac
 		request["ClientToken"] = buildClientToken("DeleteEndpointGroup")
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
-			if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.EndPointGroup"}) {
+			if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.EndPointGroup"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, gaService.GaEndpointGroupStateRefreshFunc(d.Id(), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	return nil
 }
