91e5a060710809b2b2d0b0c7caf4be7757305765
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2023-07-21 14:53:20 +0800

    resource/alicloud_oss_bucket: 1.change transitions.storage_class from optional required. 2.fix oss_bucket.html.markdown  warning.``

--- alicloud/resource_alicloud_oss_bucket.go
+++ alicloud/resource_alicloud_oss_bucket.go
@@ -204,7 +204,7 @@ func resourceAlicloudOssBucket() *schema.Resource {
 									"storage_class": {
 										Type:     schema.TypeString,
 										Default:  oss.StorageStandard,
-										Optional: true,
+										Required: true,
 										ValidateFunc: validation.StringInSlice([]string{
 											string(oss.StorageStandard),
 											string(oss.StorageIA),
