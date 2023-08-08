6acf4a5e813643440e93aaef41171874595f73d3
MrWolong <ly01269865@alibaba-inc.com>
2023-08-08 17:56:30 +0800

    resource/alicloud_ga_additional_certificate: Removed the ForceNew for field certificate_id; Supported for new action UpdateAdditionalCertificateWithListener

--- alicloud/resource_alicloud_ga_additional_certificate.go
+++ alicloud/resource_alicloud_ga_additional_certificate.go
@@ -11,30 +11,22 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func resourceAlicloudGaAdditionalCertificate() *schema.Resource {
+func resourceAliCloudGaAdditionalCertificate() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudGaAdditionalCertificateCreate,
-		Read:   resourceAlicloudGaAdditionalCertificateRead,
-		Delete: resourceAlicloudGaAdditionalCertificateDelete,
+		Create: resourceAliCloudGaAdditionalCertificateCreate,
+		Read:   resourceAliCloudGaAdditionalCertificateRead,
+		Update: resourceAliCloudGaAdditionalCertificateUpdate,
+		Delete: resourceAliCloudGaAdditionalCertificateDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(3 * time.Minute),
+			Update: schema.DefaultTimeout(3 * time.Minute),
 			Delete: schema.DefaultTimeout(3 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"accelerator_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"certificate_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"domain": {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -44,12 +36,22 @@ func resourceAlicloudGaAdditionalCertificate() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
+			"domain": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"certificate_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
 		},
 	}
 }
 
-func resourceAlicloudGaAdditionalCertificateCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudGaAdditionalCertificateCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
 	var response map[string]interface{}
 	action := "AssociateAdditionalCertificatesWithListener"
 	request := make(map[string]interface{})
@@ -57,89 +59,18 @@ func resourceAlicloudGaAdditionalCertificateCreate(d *schema.ResourceData, meta 
 	if err != nil {
 		return WrapError(err)
 	}
-	request["AcceleratorId"] = d.Get("accelerator_id")
-	request["Certificates.1.Domain"] = d.Get("domain")
-	request["Certificates.1.Id"] = d.Get("certificate_id")
-	request["ListenerId"] = d.Get("listener_id")
+
 	request["RegionId"] = client.RegionId
 	request["ClientToken"] = buildClientToken("AssociateAdditionalCertificatesWithListener")
+	request["AcceleratorId"] = d.Get("accelerator_id")
+	request["ListenerId"] = d.Get("listener_id")
+	request["Certificates.1.Domain"] = d.Get("domain")
+	request["Certificates.1.Id"] = d.Get("certificate_id")
+
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
-		if err != nil {
-			if IsExpectedErrors(err, []string{"StateError.Listener", "StateError.Accelerator", "NotActive.Listener"}) || NeedRetry(err) {
-				wait()
-				return resource.RetryableError(err)
-			}
-			return resource.NonRetryableError(err)
-		}
-		return nil
-	})
-	addDebug(action, response, request)
-	if err != nil {
-		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ga_additional_certificate", action, AlibabaCloudSdkGoERROR)
-	}
-
-	d.SetId(fmt.Sprint(request["AcceleratorId"], ":", request["ListenerId"], ":", request["Certificates.1.Domain"]))
-	gaService := GaService{client}
-	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 1*time.Second, gaService.GaListenerStateRefreshFunc(fmt.Sprint(request["ListenerId"]), []string{}))
-	if _, err := stateConf.WaitForState(); err != nil {
-		return WrapErrorf(err, IdMsg, d.Id())
-	}
-	stateConf = BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 1*time.Second, gaService.GaAcceleratorStateRefreshFunc(fmt.Sprint(request["AcceleratorId"]), []string{}))
-	if _, err := stateConf.WaitForState(); err != nil {
-		return WrapErrorf(err, IdMsg, d.Id())
-	}
-	return resourceAlicloudGaAdditionalCertificateRead(d, meta)
-}
-func resourceAlicloudGaAdditionalCertificateRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	gaService := GaService{client}
-	object, err := gaService.DescribeGaAdditionalCertificate(d.Id())
-	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_ga_additional_certificate gaService.DescribeGaAdditionalCertificate Failed!!! %s", err)
-			d.SetId("")
-			return nil
-		}
-		return WrapError(err)
-	}
-	parts, err := ParseResourceId(d.Id(), 3)
-	if err != nil {
-		return WrapError(err)
-	}
-	d.Set("accelerator_id", parts[0])
-	d.Set("domain", parts[2])
-	d.Set("listener_id", parts[1])
-	d.Set("certificate_id", object["CertificateId"])
-	return nil
-}
-func resourceAlicloudGaAdditionalCertificateDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	parts, err := ParseResourceId(d.Id(), 3)
-	if err != nil {
-		return WrapError(err)
-	}
-	action := "DissociateAdditionalCertificatesFromListener"
-	var response map[string]interface{}
-	conn, err := client.NewGaplusClient()
-	if err != nil {
-		return WrapError(err)
-	}
-	request := map[string]interface{}{
-		"AcceleratorId": parts[0],
-		"ListenerId":    parts[1],
-	}
-
-	request["Domains.1"] = parts[2]
-	request["RegionId"] = client.RegionId
-	request["ClientToken"] = buildClientToken("DissociateAdditionalCertificatesFromListener")
-	runtime := util.RuntimeOptions{}
-	runtime.SetAutoretry(true)
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
 		if err != nil {
 			if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.Listener", "NotActive.Listener"}) || NeedRetry(err) {
@@ -151,17 +82,170 @@ func resourceAlicloudGaAdditionalCertificateDelete(d *schema.ResourceData, meta 
 		return nil
 	})
 	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ga_additional_certificate", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprint(request["AcceleratorId"], ":", request["ListenerId"], ":", request["Certificates.1.Domain"]))
+
+	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 1*time.Second, gaService.GaListenerStateRefreshFunc(fmt.Sprint(request["ListenerId"]), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	stateConf = BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutCreate), 1*time.Second, gaService.GaAcceleratorStateRefreshFunc(fmt.Sprint(request["AcceleratorId"]), []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudGaAdditionalCertificateRead(d, meta)
+}
+
+func resourceAliCloudGaAdditionalCertificateRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+
+	object, err := gaService.DescribeGaAdditionalCertificate(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_ga_additional_certificate gaService.DescribeGaAdditionalCertificate Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	d.Set("accelerator_id", parts[0])
+	d.Set("listener_id", parts[1])
+	d.Set("domain", parts[2])
+	d.Set("certificate_id", object["CertificateId"])
+
+	return nil
+}
+
+func resourceAliCloudGaAdditionalCertificateUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	var response map[string]interface{}
+	update := false
+
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"RegionId":      client.RegionId,
+		"AcceleratorId": parts[0],
+		"ListenerId":    parts[1],
+		"Domain":        parts[2],
+	}
+
+	if d.HasChange("certificate_id") {
+		update = true
+	}
+	request["CertificateId"] = d.Get("certificate_id")
+
+	if update {
+		action := "UpdateAdditionalCertificateWithListener"
+		conn, err := client.NewGaplusClient()
+		if err != nil {
+			return WrapError(err)
+		}
+
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.Listener", "NotActive.Listener"}) || NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+
+		stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 1*time.Second, gaService.GaListenerStateRefreshFunc(fmt.Sprint(parts[1]), []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+
+		stateConf = BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutUpdate), 1*time.Second, gaService.GaAcceleratorStateRefreshFunc(fmt.Sprint(parts[0]), []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+	}
+
+	return resourceAliCloudGaAdditionalCertificateRead(d, meta)
+}
+
+func resourceAliCloudGaAdditionalCertificateDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	gaService := GaService{client}
+	action := "DissociateAdditionalCertificatesFromListener"
+	var response map[string]interface{}
+
+	conn, err := client.NewGaplusClient()
+	if err != nil {
+		return WrapError(err)
+	}
+
+	parts, err := ParseResourceId(d.Id(), 3)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"RegionId":      client.RegionId,
+		"ClientToken":   buildClientToken("DissociateAdditionalCertificatesFromListener"),
+		"AcceleratorId": parts[0],
+		"ListenerId":    parts[1],
+		"Domains.1":     parts[2],
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"StateError.Accelerator", "StateError.Listener", "NotActive.Listener"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
-	gaService := GaService{client}
+
 	stateConf := BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutDelete), 1*time.Second, gaService.GaListenerStateRefreshFunc(fmt.Sprint(parts[1]), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	stateConf = BuildStateConf([]string{}, []string{"active"}, d.Timeout(schema.TimeoutDelete), 1*time.Second, gaService.GaAcceleratorStateRefreshFunc(fmt.Sprint(parts[0]), []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	return nil
 }
