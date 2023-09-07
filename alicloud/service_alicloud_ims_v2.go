174d3b3ab7933a57aa682bc16419984d0d85e970
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2023-09-07 17:45:40 +0800

    New Resource: alicloud_ims_oidc_provider.

---
+++ alicloud/service_alicloud_ims_v2.go
@@ -0,0 +1,84 @@
+package alicloud
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+type ImsServiceV2 struct {
+	client *connectivity.AliyunClient
+}
+
+// DescribeImsOidcProvider <<< Encapsulated get interface for Ims OidcProvider.
+
+func (s *ImsServiceV2) DescribeImsOidcProvider(id string) (object map[string]interface{}, err error) {
+
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	action := "GetOIDCProvider"
+	conn, err := client.NewImsClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["OIDCProviderName"] = id
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-08-15"), StringPointer("AK"), query, request, &util.RuntimeOptions{})
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EntityNotExist.OIDCProvider"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("OidcProvider", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.OIDCProvider", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.OIDCProvider", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *ImsServiceV2) ImsOidcProviderStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeImsOidcProvider(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		currentStatus := fmt.Sprint(object[field])
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeImsOidcProvider >>> Encapsulated.
---
+++ website/docs/r/ims_oidc_provider.html.markdown
@@ -0,0 +1,70 @@
+---
+subcategory: "IMS"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_ims_oidc_provider"
+description: |-
+  Provides a Alicloud IMS Oidc Provider resource.
+---
+
+# alicloud_ims_oidc_provider
+
+Provides a IMS Oidc Provider resource. OpenID Connect Provider.
+
+For information about IMS Oidc Provider and how to use it, see [What is Oidc Provider](https://www.alibabacloud.com/help/en/ram/developer-reference/api-ims-2019-08-15-createoidcprovider).
+
+-> **NOTE:** Available since v1.210.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+variable "oidc_provider_name" {
+  default = "amp-resource-example-oidc-provider"
+}
+
+
+resource "alicloud_ims_oidc_provider" "default" {
+  description         = var.oidc_provider_name
+  issuer_url          = "https://oauth.aliyun.com"
+  fingerprints        = ["902ef2deeb3c5b13ea4c3d5193629309e231ae55"]
+  issuance_limit_time = "12"
+  oidc_provider_name  = var.name
+  client_ids          = ["123", "456"]
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `client_ids` - (Optional) Client ID.
+* `description` - (Optional) Description of OIDC identity provider.
+* `fingerprints` - (Optional) The authentication fingerprint of the HTTPS CA certificate.
+* `issuance_limit_time` - (Optional, Computed) The earliest time when an external IdP is allowed to issue an ID Token. If the iat field in the ID Token is greater than the current time, the request is rejected.Unit: hours. Value range: 1~168.
+* `issuer_url` - (Required, ForceNew) The issuer URL of the OIDC identity provider.
+* `oidc_provider_name` - (Required, ForceNew) The name of the OIDC identity provider.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.
+* `create_time` - Creation Time (UTC time).
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Oidc Provider.
+* `delete` - (Defaults to 5 mins) Used when delete the Oidc Provider.
+* `update` - (Defaults to 5 mins) Used when update the Oidc Provider.
+
+## Import
+
+IMS Oidc Provider can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_ims_oidc_provider.example <id>
+```
