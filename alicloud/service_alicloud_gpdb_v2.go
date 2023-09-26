10aea4c520326d9fc6aa690353845a1c5a5fbc7a
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2023-09-26 14:41:32 +0800

    New Resource: alicloud_gpdb_backup_policy.

---
+++ alicloud/service_alicloud_gpdb_v2.go
@@ -0,0 +1,173 @@
+package alicloud
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+type GpdbServiceV2 struct {
+	client *connectivity.AliyunClient
+}
+
+// DescribeGpdbLogBackup <<< Encapsulated get interface for Gpdb LogBackup.
+
+func (s *GpdbServiceV2) DescribeGpdbLogBackup(id string) (object map[string]interface{}, err error) {
+
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	action := "DescribeLogBackups"
+	conn, err := client.NewGpdbClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["DBInstanceId"] = parts[0]
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-05-03"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("LogBackup", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Items[*]", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Items[*]", response)
+	}
+
+	if len(v.([]interface{})) == 0 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("LogBackup", id)), NotFoundMsg, response)
+	}
+
+	result, _ := v.([]interface{})
+	for _, v := range result {
+		item := v.(map[string]interface{})
+		if item["DBInstanceId"] != parts[0] {
+			continue
+		}
+		if item["BackupId"] != parts[1] {
+			continue
+		}
+		return item, nil
+	}
+	return object, WrapErrorf(Error(GetNotFoundMessage("LogBackup", id)), NotFoundMsg, response)
+}
+
+func (s *GpdbServiceV2) GpdbLogBackupStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeGpdbLogBackup(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeGpdbLogBackup >>> Encapsulated.
+// DescribeGpdbBackupPolicy <<< Encapsulated get interface for Gpdb BackupPolicy.
+
+func (s *GpdbServiceV2) DescribeGpdbBackupPolicy(id string) (object map[string]interface{}, err error) {
+
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	action := "DescribeBackupPolicy"
+	conn, err := client.NewGpdbClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["DBInstanceId"] = id
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-05-03"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("BackupPolicy", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	return response, nil
+}
+
+func (s *GpdbServiceV2) GpdbBackupPolicyStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeGpdbBackupPolicy(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeGpdbBackupPolicy >>> Encapsulated.
---
+++ website/docs/r/gpdb_backup_policy.html.markdown
@@ -0,0 +1,132 @@
+---
+subcategory: "GPDB"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_gpdb_backup_policy"
+description: |-
+  Provides a Alicloud GPDB Backup Policy resource.
+---
+
+# alicloud_gpdb_backup_policy
+
+Provides a GPDB Backup Policy resource. Describe the instance backup strategy.
+
+For information about GPDB Backup Policy and how to use it, see [What is Backup Policy](https://www.alibabacloud.com/help/en/analyticdb-for-postgresql/latest/api-gpdb-2016-05-03-modifybackuppolicy).
+
+-> **NOTE:** Available since v1.211.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-hangzhou"
+}
+
+data "alicloud_resource_manager_resource_groups" "default" {}
+data "alicloud_gpdb_zones" "default" {}
+data "alicloud_vpcs" "default" {
+  name_regex = "^default-NODELETING$"
+}
+data "alicloud_vswitches" "default" {
+  vpc_id  = data.alicloud_vpcs.default.ids.0
+  zone_id = data.alicloud_gpdb_zones.default.ids.0
+}
+resource "alicloud_vswitch" "vswitch" {
+  count        = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
+  vpc_id       = data.alicloud_vpcs.default.ids.0
+  cidr_block   = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 8)
+  zone_id      = data.alicloud_gpdb_zones.default.ids.0
+  vswitch_name = var.name
+}
+locals {
+  vswitch_id = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids[0] : concat(alicloud_vswitch.vswitch.*.id, [""])[0]
+}
+
+resource "alicloud_gpdb_instance" "default" {
+  db_instance_category  = "HighAvailability"
+  db_instance_class     = "gpdb.group.segsdx1"
+  db_instance_mode      = "StorageElastic"
+  description           = var.name
+  engine                = "gpdb"
+  engine_version        = "6.0"
+  zone_id               = data.alicloud_gpdb_zones.default.ids.0
+  instance_network_type = "VPC"
+  instance_spec         = "2C16G"
+  master_node_num       = 1
+  payment_type          = "PayAsYouGo"
+  private_ip_address    = "1.1.1.1"
+  seg_storage_type      = "cloud_essd"
+  seg_node_num          = 4
+  storage_size          = 50
+  vpc_id                = data.alicloud_vpcs.default.ids.0
+  vswitch_id            = local.vswitch_id
+  ip_whitelist {
+    security_ip_list = "127.0.0.1"
+  }
+  tags = {
+    Created = "TF"
+    For     = "acceptance test"
+  }
+}
+
+resource "alicloud_gpdb_backup_policy" "default" {
+  db_instance_id          = alicloud_gpdb_instance.default.id
+  recovery_point_period   = "1"
+  enable_recovery_point   = "true"
+  preferred_backup_period = "Wednesday"
+  preferred_backup_time   = "15:00Z-16:00Z"
+  backup_retention_period = "7"
+}
+```
+
+### Deleting `alicloud_gpdb_backup_policy` or removing it from your configuration
+
+Terraform cannot destroy resource `alicloud_gpdb_backup_policy`. Terraform will remove this resource from the state file, however resources may remain.
+
+## Argument Reference
+
+The following arguments are supported:
+* `backup_retention_period` - (Optional) Data backup retention days.
+* `db_instance_id` - (Required, ForceNew) The instance ID.
+-> **NOTE:**  You can call the [DescribeDBInstances](~~ 86911 ~~) operation to view the details of all AnalyticDB PostgreSQL instances in the target region, including the instance ID.
+* `enable_recovery_point` - (Optional) Whether to enable automatic recovery points. Value Description:
+  - **true**: enabled.
+  - **false**: Closed.
+* `preferred_backup_period` - (Required) Data backup cycle. Separate multiple values with commas (,). Value Description:
+  - **Monday**: Monday.
+  - **Tuesday**: Tuesday.
+  - **Wednesday**: Wednesday.
+  - **Thursday**: Thursday.
+  - **Friday**: Friday.
+  - **Saturday**: Saturday.
+  - **Sunday**: Sunday.
+* `preferred_backup_time` - (Required) Data backup time. Format: HH:mmZ-HH:mmZ(UTC time).
+* `recovery_point_period` - (Optional) Recovery point frequency. Value Description:
+  - **1**: Hourly.
+  - **2**: Every two hours.
+  - **4**: Every four hours.
+  - **8**: Every eight hours.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Backup Policy.
+* `update` - (Defaults to 5 mins) Used when update the Backup Policy.
+
+## Import
+
+GPDB Backup Policy can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_gpdb_backup_policy.example <id>
+```
