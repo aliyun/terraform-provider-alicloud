f6945c81e9807c6162cfb2173b2ebd68c9a89006
guimin.hgm <guimin.hgm@alibaba-inc.com>
2023-10-11 10:29:53 +0800

    ci: Skipping docs-example checking when there is only test file has changed

--- ci/tasks/docs-example.sh
+++ ci/tasks/docs-example.sh
@@ -64,7 +64,7 @@ declare -A allResources
 allResources["init"]=1
 #check if need run
 for fileName in ${changeFiles[@]}; do
-  if [[ ${fileName} != *?_test.go ]]; then
+  if [[ ${fileName} == *?_test.go ]]; then
       echo -e "\033[33m[SKIPPED]\033[0m skipping the file $fileName, continue..."
       continue
   fi
