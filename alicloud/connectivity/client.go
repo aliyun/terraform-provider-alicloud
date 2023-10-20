26c3a2adbc6bc6fc4dd095655fa44dc0651cdd64
MrWolong <ly01269865@alibaba-inc.com>
2023-10-20 18:05:59 +0800

    CHANGELOG

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -176,7 +176,7 @@ var loadSdkfromRemoteMutex = sync.Mutex{}
 var loadSdkEndpointMutex = sync.Mutex{}
 
 // The main version number that is being run at the moment.
-var providerVersion = "1.211.1"
+var providerVersion = "1.211.2"
 
 // Temporarily maintain map for old ecs client methods and store special endpoint information
 var EndpointMap = map[string]string{
