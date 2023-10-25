f5e113248b9c507db46bd70271ac9f9e54c629ff
wb-zln530580 <wb-zln530580@alibaba-inc.com>
2023-10-25 18:15:16 +0800

    New Datasource:alicloud_ess_eci_scaling_configurations

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -309,6 +309,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_ess_scaling_groups":                               dataSourceAlicloudEssScalingGroups(),
 			"alicloud_ess_scaling_rules":                                dataSourceAlicloudEssScalingRules(),
 			"alicloud_ess_scaling_configurations":                       dataSourceAlicloudEssScalingConfigurations(),
+			"alicloud_ess_eci_scaling_configurations":                   dataSourceAlicloudEssEciScalingConfigurations(),
 			"alicloud_ess_lifecycle_hooks":                              dataSourceAlicloudEssLifecycleHooks(),
 			"alicloud_ess_scheduled_tasks":                              dataSourceAlicloudEssScheduledTasks(),
 			"alicloud_ots_service":                                      dataSourceAlicloudOtsService(),
