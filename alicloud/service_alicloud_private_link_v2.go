5f83d1918563f5c24cec771d12f0478b676e4c51
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2023-10-29 12:27:22 +0800

    resource/alicloud_privatelink_vpc_endpoint_service: add new attribute resource_group_id, service_resource_type, service_support_ipv6, zone_affinity_enabled.

---
+++ alicloud/service_alicloud_private_link_v2.go
@@ -0,0 +1,231 @@
+package alicloud
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	rpc "github.com/alibabacloud-go/tea-rpc/client"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+type PrivateLinkServiceV2 struct {
+	client *connectivity.AliyunClient
+}
+
+// DescribePrivateLinkVpcEndpointService <<< Encapsulated get interface for PrivateLink VpcEndpointService.
+
+func (s *PrivateLinkServiceV2) DescribePrivateLinkVpcEndpointService(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	action := "GetVpcEndpointServiceAttribute"
+	conn, err := client.NewPrivatelinkClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["ServiceId"] = id
+	request["RegionId"] = client.RegionId
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-04-15"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EndpointServiceNotFound"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("VpcEndpointService", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	return response, nil
+}
+func (s *PrivateLinkServiceV2) DescribeListTagResources(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	action := "ListTagResources"
+	conn, err := client.NewPrivatelinkClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["ResourceId.1"] = id
+	request["RegionId"] = client.RegionId
+	request["ClientToken"] = buildClientToken(action)
+
+	request["ResourceType"] = "vpcendpointservice"
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-04-15"), StringPointer("AK"), query, request, &runtime)
+		request["ClientToken"] = buildClientToken(action)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	return response, nil
+}
+
+func (s *PrivateLinkServiceV2) PrivateLinkVpcEndpointServiceStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribePrivateLinkVpcEndpointService(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribePrivateLinkVpcEndpointService >>> Encapsulated.
+
+// SetResourceTags <<< Encapsulated tag function for PrivateLink.
+func (s *PrivateLinkServiceV2) SetResourceTags(d *schema.ResourceData, resourceType string) error {
+	if d.HasChange("tags") {
+		var err error
+		var action string
+		var conn *rpc.Client
+		client := s.client
+		var request map[string]interface{}
+		var response map[string]interface{}
+		var query map[string]interface{}
+
+		added, removed := parsingTags(d)
+		removedTagKeys := make([]string, 0)
+		for _, v := range removed {
+			if !ignoredTags(v, "") {
+				removedTagKeys = append(removedTagKeys, v)
+			}
+		}
+		if len(removedTagKeys) > 0 {
+			action = "UntagResources"
+			conn, err = client.NewPrivatelinkClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["ResourceId.1"] = d.Id()
+			request["RegionId"] = client.RegionId
+			request["ClientToken"] = buildClientToken(action)
+			for i, key := range removedTagKeys {
+				request[fmt.Sprintf("TagKey.%d", i+1)] = key
+			}
+
+			request["ResourceType"] = resourceType
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-04-15"), StringPointer("AK"), query, request, &runtime)
+				request["ClientToken"] = buildClientToken(action)
+
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				addDebug(action, response, request)
+				return nil
+			})
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+		if len(added) > 0 {
+			action = "TagResources"
+			conn, err = client.NewPrivatelinkClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["ResourceId.1"] = d.Id()
+			request["RegionId"] = client.RegionId
+			request["ClientToken"] = buildClientToken(action)
+			count := 1
+			for key, value := range added {
+				request[fmt.Sprintf("Tag.%d.Key", count)] = key
+				request[fmt.Sprintf("Tag.%d.Value", count)] = value
+				count++
+			}
+
+			request["ResourceType"] = resourceType
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-04-15"), StringPointer("AK"), query, request, &runtime)
+				request["ClientToken"] = buildClientToken(action)
+
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				addDebug(action, response, request)
+				return nil
+			})
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+		d.SetPartial("tags")
+	}
+
+	return nil
+}
+
+// SetResourceTags >>> tag function encapsulated.
--- website/docs/r/privatelink_vpc_endpoint_service.html.markdown
+++ website/docs/r/privatelink_vpc_endpoint_service.html.markdown
@@ -2,14 +2,13 @@
 subcategory: "Private Link"
 layout: "alicloud"
 page_title: "Alicloud: alicloud_privatelink_vpc_endpoint_service"
-sidebar_current: "docs-alicloud-resource-privatelink-vpc-endpoint-service"
 description: |-
   Provides a Alicloud Private Link Vpc Endpoint Service resource.
 ---
 
 # alicloud_privatelink_vpc_endpoint_service
 
-Provides a Private Link Vpc Endpoint Service resource.
+Provides a Private Link Vpc Endpoint Service resource. 
 
 For information about Private Link Vpc Endpoint Service and how to use it, see [What is Vpc Endpoint Service](https://www.alibabacloud.com/help/en/privatelink/latest/api-privatelink-2020-04-15-createvpcendpointservice).
 
@@ -34,35 +33,45 @@ resource "alicloud_privatelink_vpc_endpoint_service" "example" {
 ## Argument Reference
 
 The following arguments are supported:
-
 * `auto_accept_connection` - (Optional) Whether to automatically accept terminal node connections.
 * `connect_bandwidth` - (Optional) The connection bandwidth.
 * `dry_run` - (Optional) Whether to pre-check this request only. Default to: `false`
 * `payer` - (Optional, ForceNew) The payer type. Valid Value: `EndpointService`, `Endpoint`. Default to: `Endpoint`.
+* `resource_group_id` - (Optional, Computed, Available since v1.212.0) The ID of the resource group.
 * `service_description` - (Optional) The description of the terminal node service.
-
--> **NOTE:** The `resources` only support load balancing instance with private network type and PrivateLink function.
+* `service_resource_type` - (Optional, ForceNew, Available since v1.212.0) The service type of resource. Valid values:
+  - **slb**: indicates that the service resource type is classic load balancer (clb).
+  - **alb**: indicates that the service resource type is application load balancer (alb).
+  - **nlb**: indicates that the service resource type is network load balancer (nlb).
+* `service_support_ipv6` - (Optional, Available since v1.212.0) Whether the endpoint supports IPv6. Value:
+  - **true**: Yes.
+  - **false** (default): No.
+* `tags` - (Optional, Map, Available since v1.212.0) The tag of the resource.
+* `zone_affinity_enabled` - (Optional, Available since v1.212.0) Whether to support the nearby resolution of the available area. Valid values:
+  - **true**: Yes.
+  - **false**: No.
 
 ## Attributes Reference
 
 The following attributes are exported:
-
-* `id` - The resource ID in terraform of Vpc Endpoint Service. Value as `service_id`.
+* `id` - The ID of the resource supplied above.
+* `create_time` - The creation time of the resource.
 * `service_business_status` - The business status of Vpc Endpoint Service.
 * `service_domain` - Service Domain.
-* `status` - The status of Vpc Endpoint Service.
+* `status` - The status of the resource.
+* `vpc_endpoint_service_name` - VpcEndpointServiceName.
 
 ## Timeouts
 
 The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
-
-* `create` - (Defaults to 6 mins) Used when create the Vpc Endpoint Service.
-* `update` - (Defaults to 4 mins) Used when update the Vpc Endpoint Service.
+* `create` - (Defaults to 5 mins) Used when create the Vpc Endpoint Service.
+* `delete` - (Defaults to 5 mins) Used when delete the Vpc Endpoint Service.
+* `update` - (Defaults to 5 mins) Used when update the Vpc Endpoint Service.
 
 ## Import
 
 Private Link Vpc Endpoint Service can be imported using the id, e.g.
 
 ```shell
-$ terraform import alicloud_privatelink_vpc_endpoint_service.example <service_id>
+$ terraform import alicloud_privatelink_vpc_endpoint_service.example <id>
 ```
