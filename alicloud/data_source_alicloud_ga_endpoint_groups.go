d8505a068a638b075022dd2c996d7d5e7f47fa38
MrWolong <ly01269865@alibaba-inc.com>
2023-12-07 11:46:46 +0800

    data-source/alicloud_ga_endpoint_groups: Added the field endpoint_group_ip_list

--- alicloud/data_source_alicloud_ga_endpoint_groups.go
+++ alicloud/data_source_alicloud_ga_endpoint_groups.go
@@ -3,104 +3,71 @@ package alicloud
 import (
 	"fmt"
 	"regexp"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 
 	"github.com/PaesslerAG/jsonpath"
 	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func dataSourceAlicloudGaEndpointGroups() *schema.Resource {
+func dataSourceAliCloudGaEndpointGroups() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceAlicloudGaEndpointGroupsRead,
+		Read: dataSourceAliCloudGaEndpointGroupsRead,
 		Schema: map[string]*schema.Schema{
-			"accelerator_id": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
 			"ids": {
 				Type:     schema.TypeList,
 				Optional: true,
 				ForceNew: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
-			"endpoint_group_type": {
+			"name_regex": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ForceNew:     true,
-				Default:      "default",
-				ValidateFunc: validation.StringInSlice([]string{"default", "virtual"}, false),
+				ValidateFunc: validation.ValidateRegexp,
+			},
+			"accelerator_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 			"listener_id": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
-			"name_regex": {
+			"endpoint_group_type": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.ValidateRegexp,
 				ForceNew:     true,
+				Default:      "default",
+				ValidateFunc: StringInSlice([]string{"default", "virtual"}, false),
+			},
+			"status": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: StringInSlice([]string{"active", "configuring", "creating", "init"}, false),
+			},
+			"output_file": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 			"names": {
 				Type:     schema.TypeList,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 				Computed: true,
 			},
-			"status": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"active", "configuring", "creating", "init"}, false),
-			},
-			"output_file": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
 			"groups": {
 				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"description": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"endpoint_configurations": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"enable_clientip_preservation": {
-										Type:     schema.TypeBool,
-										Computed: true,
-									},
-									"endpoint": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-									"probe_port": {
-										Type:     schema.TypeInt,
-										Computed: true,
-									},
-									"probe_protocol": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-									"type": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-									"weight": {
-										Type:     schema.TypeInt,
-										Computed: true,
-									},
-								},
-							},
-						},
 						"id": {
 							Type:     schema.TypeString,
 							Computed: true,
@@ -109,7 +76,19 @@ func dataSourceAlicloudGaEndpointGroups() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"listener_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"endpoint_group_region": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"description": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -129,11 +108,20 @@ func dataSourceAlicloudGaEndpointGroups() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"listener_id": {
-							Type:     schema.TypeString,
+						"threshold_count": {
+							Type:     schema.TypeInt,
 							Computed: true,
 						},
-						"name": {
+						"traffic_percentage": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"endpoint_group_ip_list": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"status": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -153,17 +141,37 @@ func dataSourceAlicloudGaEndpointGroups() *schema.Resource {
 								},
 							},
 						},
-						"status": {
-							Type:     schema.TypeString,
+						"endpoint_configurations": {
+							Type:     schema.TypeList,
 							Computed: true,
-						},
-						"threshold_count": {
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-						"traffic_percentage": {
-							Type:     schema.TypeInt,
-							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"endpoint": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"probe_protocol": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"probe_port": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"weight": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+									"enable_clientip_preservation": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+								},
+							},
 						},
 					},
 				},
@@ -172,29 +180,34 @@ func dataSourceAlicloudGaEndpointGroups() *schema.Resource {
 	}
 }
 
-func dataSourceAlicloudGaEndpointGroupsRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceAliCloudGaEndpointGroupsRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 
 	action := "ListEndpointGroups"
 	request := make(map[string]interface{})
-	request["AcceleratorId"] = d.Get("accelerator_id")
-	if v, ok := d.GetOk("endpoint_group_type"); ok {
-		request["EndpointGroupType"] = v
-	}
-	if v, ok := d.GetOk("listener_id"); ok {
-		request["ListenerId"] = v
-	}
 	request["RegionId"] = client.RegionId
 	request["PageSize"] = PageSizeLarge
 	request["PageNumber"] = 1
+	request["AcceleratorId"] = d.Get("accelerator_id")
+
+	if v, ok := d.GetOk("listener_id"); ok {
+		request["ListenerId"] = v
+	}
+
+	if v, ok := d.GetOk("endpoint_group_type"); ok {
+		request["EndpointGroupType"] = v
+	}
+
+	status, statusOk := d.GetOk("status")
+
 	var objects []map[string]interface{}
-	var nameRegex *regexp.Regexp
+	var endpointGroupNameRegex *regexp.Regexp
 	if v, ok := d.GetOk("name_regex"); ok {
 		r, err := regexp.Compile(v.(string))
 		if err != nil {
 			return WrapError(err)
 		}
-		nameRegex = r
+		endpointGroupNameRegex = r
 	}
 
 	idsMap := make(map[string]string)
@@ -206,7 +219,7 @@ func dataSourceAlicloudGaEndpointGroupsRead(d *schema.ResourceData, meta interfa
 			idsMap[vv.(string)] = vv.(string)
 		}
 	}
-	status, statusOk := d.GetOk("status")
+
 	var response map[string]interface{}
 	conn, err := client.NewGaplusClient()
 	if err != nil {
@@ -215,58 +228,92 @@ func dataSourceAlicloudGaEndpointGroupsRead(d *schema.ResourceData, meta interfa
 	for {
 		runtime := util.RuntimeOptions{}
 		runtime.SetAutoretry(true)
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-11-20"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+
 		if err != nil {
 			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_ga_endpoint_groups", action, AlibabaCloudSdkGoERROR)
 		}
-		addDebug(action, response, request)
 
 		resp, err := jsonpath.Get("$.EndpointGroups", response)
 		if err != nil {
 			return WrapErrorf(err, FailedGetAttributeMsg, action, "$.EndpointGroups", response)
 		}
+
 		result, _ := resp.([]interface{})
 		for _, v := range result {
 			item := v.(map[string]interface{})
-			if nameRegex != nil {
-				if !nameRegex.MatchString(fmt.Sprint(item["Name"])) {
+			if endpointGroupNameRegex != nil {
+				if !endpointGroupNameRegex.MatchString(fmt.Sprint(item["Name"])) {
 					continue
 				}
 			}
+
 			if len(idsMap) > 0 {
 				if _, ok := idsMap[fmt.Sprint(item["EndpointGroupId"])]; !ok {
 					continue
 				}
 			}
+
 			if statusOk && status.(string) != "" && status.(string) != item["State"].(string) {
 				continue
 			}
+
 			objects = append(objects, item)
 		}
+
 		if len(result) < PageSizeLarge {
 			break
 		}
+
 		request["PageNumber"] = request["PageNumber"].(int) + 1
 	}
+
 	ids := make([]string, 0)
 	names := make([]interface{}, 0)
 	s := make([]map[string]interface{}, 0)
 	for _, object := range objects {
 		mapping := map[string]interface{}{
-			"description":                   object["Description"],
 			"id":                            fmt.Sprint(object["EndpointGroupId"]),
 			"endpoint_group_id":             fmt.Sprint(object["EndpointGroupId"]),
+			"listener_id":                   object["ListenerId"],
 			"endpoint_group_region":         object["EndpointGroupRegion"],
+			"name":                          object["Name"],
+			"description":                   object["Description"],
 			"health_check_interval_seconds": formatInt(object["HealthCheckIntervalSeconds"]),
 			"health_check_path":             object["HealthCheckPath"],
 			"health_check_port":             formatInt(object["HealthCheckPort"]),
 			"health_check_protocol":         object["HealthCheckProtocol"],
-			"listener_id":                   object["ListenerId"],
-			"name":                          object["Name"],
-			"status":                        object["State"],
 			"threshold_count":               formatInt(object["ThresholdCount"]),
 			"traffic_percentage":            formatInt(object["TrafficPercentage"]),
+			"endpoint_group_ip_list":        object["EndpointGroupIpList"],
+			"status":                        object["State"],
 		}
+
+		portOverrides := make([]map[string]interface{}, 0)
+		if portOverridesList, ok := object["PortOverrides"].([]interface{}); ok {
+			for _, v := range portOverridesList {
+				if m1, ok := v.(map[string]interface{}); ok {
+					temp1 := map[string]interface{}{
+						"endpoint_port": m1["EndpointPort"],
+						"listener_port": m1["ListenerPort"],
+					}
+					portOverrides = append(portOverrides, temp1)
+				}
+			}
+		}
+		mapping["port_overrides"] = portOverrides
 
 		endpointConfigurations := make([]map[string]interface{}, 0)
 		if endpointConfigurationsList, ok := object["EndpointConfigurations"].([]interface{}); ok {
@@ -286,25 +333,13 @@ func dataSourceAlicloudGaEndpointGroupsRead(d *schema.ResourceData, meta interfa
 		}
 		mapping["endpoint_configurations"] = endpointConfigurations
 
-		portOverrides := make([]map[string]interface{}, 0)
-		if portOverridesList, ok := object["PortOverrides"].([]interface{}); ok {
-			for _, v := range portOverridesList {
-				if m1, ok := v.(map[string]interface{}); ok {
-					temp1 := map[string]interface{}{
-						"endpoint_port": m1["EndpointPort"],
-						"listener_port": m1["ListenerPort"],
-					}
-					portOverrides = append(portOverrides, temp1)
-				}
-			}
-		}
-		mapping["port_overrides"] = portOverrides
-		ids = append(ids, fmt.Sprint(object["EndpointGroupId"]))
+		ids = append(ids, fmt.Sprint(mapping["id"]))
 		names = append(names, object["Name"])
 		s = append(s, mapping)
 	}
 
 	d.SetId(dataResourceIdHash(ids))
+
 	if err := d.Set("ids", ids); err != nil {
 		return WrapError(err)
 	}
@@ -316,6 +351,7 @@ func dataSourceAlicloudGaEndpointGroupsRead(d *schema.ResourceData, meta interfa
 	if err := d.Set("groups", s); err != nil {
 		return WrapError(err)
 	}
+
 	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
 		writeToFile(output.(string), s)
 	}
--- alicloud/data_source_alicloud_ga_endpoint_groups_test.go
+++ alicloud/data_source_alicloud_ga_endpoint_groups_test.go
@@ -2,178 +2,189 @@ package alicloud
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 )
 
-func TestAccAlicloudGaEndpointGroupsDataSource(t *testing.T) {
+func TestAccAliCloudGaEndpointGroupsDataSource(t *testing.T) {
 	rand := acctest.RandInt()
-	resourceId := "data.alicloud_ga_endpoint_groups.default"
-	name := fmt.Sprintf("tf-testEndpointGroups_datasource-%d", rand)
-	testAccConfig := dataSourceTestAccConfigFunc(resourceId, name, dataSourceGaEndpointGroupsConfigDependence)
 	checkoutSupportedRegions(t, true, connectivity.GaSupportRegions)
 	idsConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}"},
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_ga_endpoint_group.default.id}"]`,
 		}),
-		fakeConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}_fake"},
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"ids": `["${alicloud_ga_endpoint_group.default.id}_fake"]`,
 		}),
 	}
-
-	statusConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}"},
-			"status":         "active",
-		}),
-		fakeConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}_fake"},
-			"status":         "creating",
-		}),
-	}
-
 	nameRegexConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"name_regex":     "${alicloud_ga_endpoint_group.default.name}",
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_ga_endpoint_group.default.name}"`,
 		}),
-		fakeConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"name_regex":     "${alicloud_ga_endpoint_group.default.name}_fake",
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"name_regex": `"${alicloud_ga_endpoint_group.default.name}_fake"`,
 		}),
 	}
-
+	listenerIdConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"listener_id": `"${alicloud_ga_endpoint_group.default.listener_id}"`,
+		}),
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"listener_id": `"${alicloud_ga_endpoint_group.default.listener_id}_fake"`,
+		}),
+	}
+	endpointGroupTypeConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"endpoint_group_type": `"default"`,
+		}),
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"endpoint_group_type": `"virtual"`,
+		}),
+	}
+	statusConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"status": `"active"`,
+		}),
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"status": `"configuring"`,
+		}),
+	}
 	allConf := dataSourceTestAccConfig{
-		existConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}"},
-			"name_regex":     "${alicloud_ga_endpoint_group.default.name}",
-			"status":         "active",
+		existConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"ids":                 `["${alicloud_ga_endpoint_group.default.id}"]`,
+			"name_regex":          `"${alicloud_ga_endpoint_group.default.name}"`,
+			"listener_id":         `"${alicloud_ga_endpoint_group.default.listener_id}"`,
+			"endpoint_group_type": `"default"`,
+			"status":              `"active"`,
 		}),
-		fakeConfig: testAccConfig(map[string]interface{}{
-			"accelerator_id": "${alicloud_ga_endpoint_group.default.accelerator_id}",
-			"ids":            []string{"${alicloud_ga_endpoint_group.default.id}_fake"},
-			"name_regex":     "${alicloud_ga_endpoint_group.default.name}_fake",
-			"status":         "creating",
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand, map[string]string{
+			"ids":                 `["${alicloud_ga_endpoint_group.default.id}_fake"]`,
+			"name_regex":          `"${alicloud_ga_endpoint_group.default.name}_fake"`,
+			"listener_id":         `"${alicloud_ga_endpoint_group.default.listener_id}_fake"`,
+			"endpoint_group_type": `"virtual"`,
+			"status":              `"configuring"`,
 		}),
 	}
-
-	var existMapFunc = func(rand int) map[string]string {
+	var existAliCloudGaEndpointGroupsDataSourceNameMapFunc = func(rand int) map[string]string {
 		return map[string]string{
 			"ids.#":                                  "1",
 			"names.#":                                "1",
-			"groups.#":                               CHECKSET,
-			"groups.0.description":                   name,
-			"groups.0.endpoint_configurations.#":     "1",
+			"groups.#":                               "1",
 			"groups.0.id":                            CHECKSET,
 			"groups.0.endpoint_group_id":             CHECKSET,
-			"groups.0.endpoint_group_region":         defaultRegionToTest,
-			"groups.0.health_check_interval_seconds": "3",
-			"groups.0.health_check_path":             "/healthcheck",
-			"groups.0.health_check_port":             "9999",
-			"groups.0.health_check_protocol":         "http",
 			"groups.0.listener_id":                   CHECKSET,
-			"groups.0.name":                          name,
-			"groups.0.port_overrides.#":              "1",
+			"groups.0.endpoint_group_region":         CHECKSET,
+			"groups.0.name":                          CHECKSET,
+			"groups.0.description":                   CHECKSET,
+			"groups.0.health_check_interval_seconds": CHECKSET,
+			"groups.0.health_check_path":             CHECKSET,
+			"groups.0.health_check_port":             CHECKSET,
+			"groups.0.health_check_protocol":         CHECKSET,
+			"groups.0.threshold_count":               CHECKSET,
+			"groups.0.traffic_percentage":            CHECKSET,
+			"groups.0.endpoint_group_ip_list.#":      CHECKSET,
 			"groups.0.status":                        "active",
-			"groups.0.threshold_count":               "4",
-			"groups.0.traffic_percentage":            "20",
+			"groups.0.port_overrides.#":              "1",
+			"groups.0.endpoint_configurations.#":     "1",
 		}
 	}
-
-	var fakeMapFunc = func(rand int) map[string]string {
+	var fakeAliCloudGaEndpointGroupsDataSourceNameMapFunc = func(rand int) map[string]string {
 		return map[string]string{
-			"groups.#": "0",
 			"ids.#":    "0",
+			"names.#":  "0",
+			"groups.#": "0",
 		}
 	}
-
-	var CheckInfo = dataSourceAttr{
-		resourceId:   resourceId,
-		existMapFunc: existMapFunc,
-		fakeMapFunc:  fakeMapFunc,
+	var alicloudGaEndpointGroupsCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_ga_endpoint_groups.default",
+		existMapFunc: existAliCloudGaEndpointGroupsDataSourceNameMapFunc,
+		fakeMapFunc:  fakeAliCloudGaEndpointGroupsDataSourceNameMapFunc,
 	}
 	preCheck := func() {
 		testAccPreCheck(t)
+	}
+	alicloudGaEndpointGroupsCheckInfo.dataSourceTestCheckWithPreCheck(t, rand, preCheck, idsConf, nameRegexConf, listenerIdConf, endpointGroupTypeConf, statusConf, allConf)
+}
 
+func testAccCheckAliCloudGaEndpointGroupsDataSourceName(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
 	}
 
-	CheckInfo.dataSourceTestCheckWithPreCheck(t, rand, preCheck, idsConf, statusConf, nameRegexConf, allConf)
-}
+	config := fmt.Sprintf(`
+	variable "name" {
+  		default = "tf-testAccGaEndpointGroup-%d"
+	}
 
-func dataSourceGaEndpointGroupsConfigDependence(name string) string {
-	return fmt.Sprintf(`
-variable "name" {
-  default  = "%s"
-}
+	data "alicloud_ga_accelerators" "default" {
+  		status = "active"
+	}
 
-data "alicloud_ga_accelerators" "default" {
-  status = "active"
-}
+	resource "alicloud_ga_bandwidth_package" "default" {
+  		bandwidth              = 100
+  		type                   = "Basic"
+  		bandwidth_type         = "Basic"
+  		payment_type           = "PayAsYouGo"
+  		billing_type           = "PayBy95"
+  		ratio                  = 30
+  		bandwidth_package_name = var.name
+  		auto_pay               = true
+  		auto_use_coupon        = true
+	}
 
-resource "alicloud_ga_bandwidth_package" "default" {
-   	bandwidth              =  100
-  	type                   = "Basic"
-  	bandwidth_type         = "Basic"
-	payment_type           = "PayAsYouGo"
-  	billing_type           = "PayBy95"
-	ratio       = 30
-	bandwidth_package_name = var.name
-    auto_pay               = true
-    auto_use_coupon        = true
-}
+	resource "alicloud_ga_bandwidth_package_attachment" "default" {
+		accelerator_id       = data.alicloud_ga_accelerators.default.ids.0
+		bandwidth_package_id = alicloud_ga_bandwidth_package.default.id
+	}
 
-resource "alicloud_ga_bandwidth_package_attachment" "default" {
-	// Please run resource ga_accelerator test case to ensure this account has at least one accelerator before run this case.
-	accelerator_id = data.alicloud_ga_accelerators.default.ids.0
-	bandwidth_package_id = alicloud_ga_bandwidth_package.default.id
-}
+	resource "alicloud_ga_listener" "default" {
+		accelerator_id  = alicloud_ga_bandwidth_package_attachment.default.accelerator_id
+		client_affinity = "SOURCE_IP"
+		protocol        = "UDP"
+		name            = var.name
+  		port_ranges {
+    		from_port = "60"
+    		to_port   = "70"
+  		}
+	}
 
-resource "alicloud_ga_listener" "default" {
-  port_ranges{
-    from_port="60"
-    to_port="70"
-  }
-  accelerator_id=alicloud_ga_bandwidth_package_attachment.default.accelerator_id
-  client_affinity="SOURCE_IP"
-  protocol="UDP"
-  name=var.name
-}
+	resource "alicloud_eip_address" "default" {
+  		bandwidth            = "10"
+  		internet_charge_type = "PayByBandwidth"
+  		address_name         = var.name
+	}
 
-resource "alicloud_eip_address" "default" {
-  bandwidth            = "10"
-  internet_charge_type = "PayByBandwidth"
-  address_name = var.name
-}
+	resource "alicloud_ga_endpoint_group" "default" {
+  		accelerator_id = alicloud_ga_listener.default.accelerator_id
+		listener_id        = alicloud_ga_listener.default.id
+  		description                   = var.name
+  		name                          = var.name
+  		threshold_count               = 4
+  		traffic_percentage = 20
+  		endpoint_group_region         = "%s"
+  		health_check_interval_seconds = "3"
+  		health_check_path             = "/healthcheck"
+  		health_check_port             = "9999"
+  		health_check_protocol         = "http"
+  		port_overrides {
+    		endpoint_port = "10"
+    		listener_port = "60"
+  		}
+  		endpoint_configurations {
+    		endpoint = alicloud_eip_address.default.ip_address
+    		type     = "PublicIp"
+    		weight   = "20"
+  		}
+	}
 
-resource "alicloud_ga_endpoint_group" "default" {
-  accelerator_id=alicloud_ga_listener.default.accelerator_id
-  endpoint_configurations{
-    endpoint=alicloud_eip_address.default.ip_address
-    type="PublicIp"
-    weight="20"
-  }
-  description=var.name
-  name=var.name
-  threshold_count=4
-  endpoint_group_region="%s"
-  health_check_interval_seconds="3"
-  health_check_path="/healthcheck"
-  health_check_port="9999"
-  health_check_protocol="http"
-  port_overrides{
-    endpoint_port="10"
-    listener_port="60"
-  }
-  traffic_percentage=20
-  listener_id=alicloud_ga_listener.default.id
-}
-`, name, defaultRegionToTest)
+	data "alicloud_ga_endpoint_groups" "default" {
+  		accelerator_id = alicloud_ga_endpoint_group.default.accelerator_id
+		%s
+	}
+`, rand, defaultRegionToTest, strings.Join(pairs, " \n "))
+	return config
 }
