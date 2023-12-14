da96c5165629d245b95865d341c4bc83531db0b8
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2023-12-14 16:12:22 +0800

    New Resource: alicloud_quotas_template_applications.

---
+++ alicloud/resource_alicloud_quotas_template_applications.go
@@ -0,0 +1,342 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/blues/jsonata-go"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudQuotasTemplateApplications() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudQuotasTemplateApplicationsCreate,
+		Read:   resourceAliCloudQuotasTemplateApplicationsRead,
+		Update: resourceAliCloudQuotasTemplateApplicationsUpdate,
+		Delete: resourceAliCloudQuotasTemplateApplicationsDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"aliyun_uids": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"desire_value": {
+				Type:     schema.TypeFloat,
+				Required: true,
+				ForceNew: true,
+			},
+			"dimensions": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"value": {
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+						"key": {
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+						},
+					},
+				},
+			},
+			"effective_time": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"env_language": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"expire_time": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"notice_type": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"product_code": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"quota_action_code": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"quota_application_details": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"quota_description": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"quota_unit": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"audit_reason": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"period": {
+							Type:     schema.TypeList,
+							Computed: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"period_value": {
+										Type:     schema.TypeInt,
+										Computed: true,
+									},
+									"period_unit": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"dimensions": {
+							Type:     schema.TypeMap,
+							Computed: true,
+						},
+						"approve_value": {
+							Type:     schema.TypeFloat,
+							Computed: true,
+						},
+						"reason": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"env_language": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"quota_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"quota_arn": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"aliyun_uid": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"notice_type": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"application_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"quota_category": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: StringInSlice([]string{"CommonQuota", "FlowControl", "WhiteListLabel"}, false),
+			},
+			"reason": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudQuotasTemplateApplicationsCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateQuotaApplicationsForTemplate"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	conn, err := client.NewQuotasClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+
+	request["QuotaCategory"] = d.Get("quota_category")
+	request["ProductCode"] = d.Get("product_code")
+	request["QuotaActionCode"] = d.Get("quota_action_code")
+	if v, ok := d.GetOk("effective_time"); ok {
+		request["EffectiveTime"] = v
+	}
+	if v, ok := d.GetOk("expire_time"); ok {
+		request["ExpireTime"] = v
+	}
+	if v, ok := d.GetOk("aliyun_uids"); ok {
+		aliyunUidsMaps := v.([]interface{})
+		request["AliyunUids"] = aliyunUidsMaps
+	}
+
+	request["DesireValue"] = d.Get("desire_value")
+	if v, ok := d.GetOk("notice_type"); ok {
+		request["NoticeType"] = v
+	}
+	if v, ok := d.GetOk("env_language"); ok {
+		request["EnvLanguage"] = v
+	}
+	request["Reason"] = d.Get("reason")
+	if v, ok := d.GetOk("dimensions"); ok {
+		dimensionsMaps := make([]map[string]interface{}, 0)
+		for _, dataLoop1 := range v.(*schema.Set).List() {
+			dataLoop1Tmp := dataLoop1.(map[string]interface{})
+			dataLoop1Map := make(map[string]interface{})
+			dataLoop1Map["Key"] = dataLoop1Tmp["key"]
+			dataLoop1Map["Value"] = dataLoop1Tmp["value"]
+			dimensionsMaps = append(dimensionsMaps, dataLoop1Map)
+		}
+		request["Dimensions"] = dimensionsMaps
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-05-10"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_quotas_template_applications", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprint(response["BatchQuotaApplicationId"]))
+
+	quotasServiceV2 := QuotasServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{fmt.Sprint(response["BatchQuotaApplicationId"])}, d.Timeout(schema.TimeoutCreate), 5*time.Second, quotasServiceV2.QuotasTemplateApplicationsStateRefreshFunc(d.Id(), "BatchQuotaApplicationId", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudQuotasTemplateApplicationsRead(d, meta)
+}
+
+func resourceAliCloudQuotasTemplateApplicationsRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	quotasServiceV2 := QuotasServiceV2{client}
+
+	objectRaw, err := quotasServiceV2.DescribeQuotasTemplateApplications(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_quotas_template_applications DescribeQuotasTemplateApplications Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("desire_value", objectRaw["DesireValue"])
+	d.Set("effective_time", objectRaw["EffectiveTime"])
+	d.Set("expire_time", objectRaw["ExpireTime"])
+	d.Set("product_code", objectRaw["ProductCode"])
+	d.Set("quota_action_code", objectRaw["QuotaActionCode"])
+	d.Set("quota_category", objectRaw["QuotaCategory"])
+	d.Set("reason", objectRaw["Reason"])
+
+	aliyunUids1Raw := make([]interface{}, 0)
+	if objectRaw["AliyunUids"] != nil {
+		aliyunUids1Raw = objectRaw["AliyunUids"].([]interface{})
+	}
+
+	d.Set("aliyun_uids", aliyunUids1Raw)
+
+	e := jsonata.MustCompile("$each($.Dimensions, function($v, $k) {{\"value\":$v, \"key\": $k}})[]")
+	evaluation, _ := e.Eval(objectRaw)
+	d.Set("dimensions", evaluation)
+	objectRaw, err = quotasServiceV2.DescribeListQuotaApplicationsDetailForTemplate(d.Id())
+	if err != nil {
+		return WrapError(err)
+	}
+
+	quotaApplications1Raw := objectRaw["QuotaApplications"]
+	quotaApplicationDetailsMaps := make([]map[string]interface{}, 0)
+	if quotaApplications1Raw != nil {
+		for _, quotaApplicationsChild1Raw := range quotaApplications1Raw.([]interface{}) {
+			quotaApplicationDetailsMap := make(map[string]interface{})
+			quotaApplicationsChild1Raw := quotaApplicationsChild1Raw.(map[string]interface{})
+			quotaApplicationDetailsMap["aliyun_uid"] = quotaApplicationsChild1Raw["AliyunUid"]
+			quotaApplicationDetailsMap["application_id"] = quotaApplicationsChild1Raw["ApplicationId"]
+			quotaApplicationDetailsMap["approve_value"] = quotaApplicationsChild1Raw["ApproveValue"]
+			quotaApplicationDetailsMap["audit_reason"] = quotaApplicationsChild1Raw["AuditReason"]
+			quotaApplicationDetailsMap["dimensions"] = quotaApplicationsChild1Raw["QuotaDimension"]
+			quotaApplicationDetailsMap["env_language"] = quotaApplicationsChild1Raw["EnvLanguage"]
+			quotaApplicationDetailsMap["notice_type"] = quotaApplicationsChild1Raw["NoticeType"]
+			quotaApplicationDetailsMap["quota_arn"] = quotaApplicationsChild1Raw["QuotaArn"]
+			quotaApplicationDetailsMap["quota_description"] = quotaApplicationsChild1Raw["QuotaDescription"]
+			quotaApplicationDetailsMap["quota_name"] = quotaApplicationsChild1Raw["QuotaName"]
+			quotaApplicationDetailsMap["quota_unit"] = quotaApplicationsChild1Raw["QuotaUnit"]
+			quotaApplicationDetailsMap["reason"] = quotaApplicationsChild1Raw["Reason"]
+			quotaApplicationDetailsMap["status"] = quotaApplicationsChild1Raw["Status"]
+
+			periodMaps := make([]map[string]interface{}, 0)
+			periodMap := make(map[string]interface{})
+			period1Raw := quotaApplicationsChild1Raw["Period"].(map[string]interface{})
+			periodMap["period_unit"] = period1Raw["PeriodUnit"]
+			periodMap["period_value"] = formatInt(period1Raw["PeriodValue"])
+
+			periodMaps = append(periodMaps, periodMap)
+			quotaApplicationDetailsMap["period"] = periodMaps
+			quotaApplicationDetailsMaps = append(quotaApplicationDetailsMaps, quotaApplicationDetailsMap)
+		}
+	}
+	d.Set("quota_application_details", quotaApplicationDetailsMaps)
+
+	return nil
+}
+
+func resourceAliCloudQuotasTemplateApplicationsUpdate(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[INFO] Cannot update resource Alicloud Resource Template Applications.")
+	return nil
+}
+
+func resourceAliCloudQuotasTemplateApplicationsDelete(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[WARN] Cannot destroy resource AliCloud Resource Template Applications. Terraform will remove this resource from the state file, however resources may remain.")
+	return nil
+}
---
+++ website/docs/r/quotas_template_applications.html.markdown
@@ -0,0 +1,130 @@
+---
+subcategory: "Quotas"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_quotas_template_applications"
+description: |-
+  Provides a Alicloud Quotas Template Applications resource.
+---
+
+# alicloud_quotas_template_applications
+
+Provides a Quotas Template Applications resource. Template Batch Application.
+
+For information about Quotas Template Applications and how to use it, see [What is Template Applications](https://www.alibabacloud.com/help/en/quota-center/developer-reference/api-quotas-2020-05-10-createquotaapplicationsfortemplate).
+
+-> **NOTE:** Available since v1.214.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-hangzhou"
+}
+
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
+resource "alicloud_resource_manager_account" "account" {
+  display_name = "${var.name}-${random_integer.default.result}"
+}
+
+resource "alicloud_quotas_template_applications" "default" {
+  env_language      = "zh"
+  notice_type       = "0"
+  quota_category    = "WhiteListLabel"
+  desire_value      = "1"
+  reason            = "example"
+  quota_action_code = "quotas.label_multi/A"
+  effective_time    = "2023-12-03T16:00:00Z"
+  aliyun_uids = [
+    "${alicloud_resource_manager_account.account.id}"
+  ]
+  product_code = "quotas"
+  expire_time  = "2023-12-26T16:00:00Z"
+}
+```
+
+### Deleting `alicloud_quotas_template_applications` or removing it from your configuration
+
+Terraform cannot destroy resource `alicloud_quotas_template_applications`. Terraform will remove this resource from the state file, however resources may remain.
+
+## Argument Reference
+
+The following arguments are supported:
+* `aliyun_uids` - (Required, ForceNew) The list of Alibaba Cloud accounts (primary accounts) of the resource directory members to which the quota is applied.
+-> **NOTE:**  Only 50 members can apply for quota increase in batch at a time. For more information about the members of the resource directory, see [Query the list of all members in the resource directory](~~ 604207 ~~).
+* `desire_value` - (Required, ForceNew) The value of the quota request.
+-> **NOTE:**  The quota request is approved by the technical support of each cloud service. If you want to increase the chance of passing, please fill in a reasonable application value and detailed application reasons when applying for quota.
+* `dimensions` - (Optional, ForceNew) Quota dimension. See [`dimensions`](#dimensions) below.
+* `effective_time` - (Optional, ForceNew) The UTC time when the quota takes effect. This parameter applies only to the equity quota (WhiteListLabel).
+-> **NOTE:**  If the current account does not select the effective time, the default is the submission time.
+* `env_language` - (Optional) The language of the quota application result notification. Value:
+  - zh (default): Chinese.
+  - en: English.
+* `expire_time` - (Optional, ForceNew) The UTC time when the quota expires. This parameter applies only to the equity quota (WhiteListLabel).
+-> **NOTE:**  If No Expiration Time is selected for the current account, the expiration time is 99 years from the effective time of the current quota.
+* `notice_type` - (Optional) Whether to send notification of quota application result. Value:
+  - 0 (default): No.
+  - 3: Yes.
+* `product_code` - (Required, ForceNew) Cloud service name abbreviation.
+-> **NOTE:**  For more information about cloud services that support quota centers, see [Cloud services that support quota centers](~~ 182368 ~~).
+* `quota_action_code` - (Required, ForceNew) The quota ID.
+* `quota_category` - (Required, ForceNew) The quota type. Value:
+  - CommonQuota (default): Generic quota.
+  - FlowControl:API rate quota.
+  - WhiteListLabel: Equity quota.
+* `reason` - (Required, ForceNew) Reason for quota application.
+-> **NOTE:**  The quota request is approved by the technical support of each cloud service. If you want to increase the chance of passing, please fill in a reasonable application value and detailed application reasons when applying for quota.
+
+### `dimensions`
+
+The dimensions supports the following:
+* `key` - (Optional, ForceNew) Quota dimension Key.
+* `value` - (Optional, ForceNew) Quota dimension Value.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.
+* `quota_application_details` - List of quota application details.
+  * `aliyun_uid` - Alibaba Cloud account (primary account).
+  * `application_id` - The ID of the quota promotion request.
+  * `approve_value` - The approved quota value of the quota increase request.
+  * `audit_reason` - Approval comments on quota increase applications.
+  * `env_language` - The language of the quota alert notification. Value: zh: Chinese, en: English.
+  * `notice_type` - Whether to notify the quota increase application result. Value: 0: No, 3: Yes.
+  * `period` - Quota calculation period.
+    * `period_unit` - Quota calculation cycle unit.
+    * `period_value` - The quota calculation period value.
+  * `dimensions` - Quota dimension.
+  * `quota_arn` - Quota ARN.
+  * `quota_description` - The quota description.
+  * `quota_name` - The quota name.
+  * `quota_unit` - Quota unit.
+  * `reason` - The reason for the quota increase application.
+  * `status` - The approval status of the quota promotion application. Value:
+  - Disagree: reject.
+  - Approve: approved.
+  - Process: under review.
+  - Cancel: Closed.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Template Applications.
+
+## Import
+
+Quotas Template Applications can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_quotas_template_applications.example <id>
+```
