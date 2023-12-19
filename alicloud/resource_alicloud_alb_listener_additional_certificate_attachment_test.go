270476684264b0cb1b37f026eaa36a4b1817057e
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2023-12-19 14:31:44 +0800

    testcase: fix alb, hbr test case; resource/alicloud_alb_load_balancer: add computed tag for bandwidth_package_id.

--- alicloud/resource_alicloud_alb_listener_additional_certificate_attachment_test.go
+++ alicloud/resource_alicloud_alb_listener_additional_certificate_attachment_test.go
@@ -68,28 +68,32 @@ variable "name" {
   default = "%s"
 }
 
-
 data "alicloud_alb_zones" "default" {}
 
-data "alicloud_vpcs" "default" {
-    name_regex = "^default-NODELETING$"
-}
-data "alicloud_vswitches" "default_1" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_alb_zones.default.zones.0.id
+resource "alicloud_vpc" "default" {
+  vpc_name   = var.name
+  cidr_block = "172.16.0.0/16"
 }
 
+resource "alicloud_vswitch" "default" {
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "172.16.1.0/24"
+  zone_id      = data.alicloud_alb_zones.default.zones.0.id
+  vswitch_name = var.name
+}
 
-data "alicloud_vswitches" "default_2" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_alb_zones.default.zones.1.id
+resource "alicloud_vswitch" "default2" {
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "172.16.0.0/24"
+  zone_id      = data.alicloud_alb_zones.default.zones.1.id
+  vswitch_name = var.name
 }
 
 data "alicloud_resource_manager_resource_groups" "default" {}
 
 
 resource "alicloud_alb_load_balancer" "default_3" {
-  vpc_id                 = data.alicloud_vpcs.default.ids.0
+  vpc_id                 = alicloud_vpc.default.id
   address_type           = "Internet"
   address_allocated_mode = "Fixed"
   load_balancer_name     = var.name
@@ -102,11 +106,11 @@ resource "alicloud_alb_load_balancer" "default_3" {
     Created = "TF"
   }
   zone_mappings {
-    vswitch_id = data.alicloud_vswitches.default_1.ids[0]
+    vswitch_id = alicloud_vswitch.default.id
     zone_id    = data.alicloud_alb_zones.default.zones.0.id
   }
   zone_mappings {
-    vswitch_id = data.alicloud_vswitches.default_2.ids[0]
+    vswitch_id = alicloud_vswitch.default2.id
     zone_id    = data.alicloud_alb_zones.default.zones.1.id
   }
   modification_protection_config {
@@ -116,7 +120,7 @@ resource "alicloud_alb_load_balancer" "default_3" {
 
 resource "alicloud_alb_server_group" "default_4" {
   protocol          = "HTTP"
-  vpc_id            = data.alicloud_vpcs.default.vpcs.0.id
+  vpc_id            = alicloud_vpc.default.id
   server_group_name = var.name
   resource_group_id = data.alicloud_resource_manager_resource_groups.default.groups.0.id
   health_check_config {
--- alicloud/resource_alicloud_alb_load_balancer_common_bandwidth_package_attachment_test.go
+++ alicloud/resource_alicloud_alb_load_balancer_common_bandwidth_package_attachment_test.go
@@ -61,35 +61,32 @@ variable "name" {
 
 data "alicloud_alb_zones" "default"{}
 
-data "alicloud_vpcs" "default" {
-    name_regex = "^default-NODELETING$"
-}
-data "alicloud_vswitches" "default_1" {
-  vpc_id = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_alb_zones.default.zones.0.id
-}
-resource "alicloud_vswitch" "vswitch_1" {
-  count             = length(data.alicloud_vswitches.default_1.ids) > 0 ? 0 : 1
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 2)
-  zone_id =  data.alicloud_alb_zones.default.zones.0.id
-  vswitch_name              = var.name
+data "alicloud_zones" "default" {
+  available_disk_category     = "cloud_efficiency"
+  available_resource_creation = "VSwitch"
 }
 
-data "alicloud_vswitches" "default_2" {
-  vpc_id = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_alb_zones.default.zones.1.id
+resource "alicloud_vpc" "default" {
+  name       = var.name
+  cidr_block = "172.16.0.0/16"
 }
+
+resource "alicloud_vswitch" "vswitch_1" {
+  vpc_id            = alicloud_vpc.default.id
+  cidr_block        = cidrsubnet(alicloud_vpc.default.cidr_block, 8, 2)
+  zone_id           = data.alicloud_zones.default.zones.0.id
+  vswitch_name      = "${var.name}_1"
+}
+
 resource "alicloud_vswitch" "vswitch_2" {
-  count             = length(data.alicloud_vswitches.default_2.ids) > 0 ? 0 : 1
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 4)
-  zone_id = data.alicloud_alb_zones.default.zones.1.id
-  vswitch_name              = var.name
+  vpc_id            = alicloud_vpc.default.id
+  cidr_block        = cidrsubnet(alicloud_vpc.default.cidr_block, 8, 4)
+  zone_id           = data.alicloud_zones.default.zones.1.id
+  vswitch_name      = "${var.name}_2"
 }
 
 resource "alicloud_alb_load_balancer" "default" {
-  vpc_id =              data.alicloud_vpcs.default.ids.0
+  vpc_id =              alicloud_vpc.default.id
   address_type =        "Internet"
   address_allocated_mode = "Fixed"
   load_balancer_name =    var.name
@@ -98,11 +95,11 @@ resource "alicloud_alb_load_balancer" "default" {
     pay_type = 	"PayAsYouGo"
   }
   zone_mappings{
-		vswitch_id =  length(data.alicloud_vswitches.default_1.ids) > 0 ? data.alicloud_vswitches.default_1.ids[0] : concat(alicloud_vswitch.vswitch_1.*.id, [""])[0]
+		vswitch_id = alicloud_vswitch.vswitch_1.id
 		zone_id =  data.alicloud_alb_zones.default.zones.0.id
 	}
   zone_mappings{
-		vswitch_id = length(data.alicloud_vswitches.default_2.ids) > 0 ? data.alicloud_vswitches.default_2.ids[0] : concat(alicloud_vswitch.vswitch_2.*.id, [""])[0]
+		vswitch_id = alicloud_vswitch.vswitch_2.id
 		zone_id =   data.alicloud_alb_zones.default.zones.1.id
 	}
 }
