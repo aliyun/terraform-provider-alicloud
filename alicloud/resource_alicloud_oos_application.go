2f940cbca8b920184baa7fbd630af9f16406c40d
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-01-01 17:22:13 +0800

    resource/alicloud_oos_application: add delete retry error code.

--- alicloud/resource_alicloud_oos_application.go
+++ alicloud/resource_alicloud_oos_application.go
@@ -171,7 +171,7 @@ func resourceAlicloudOosApplicationDelete(d *schema.ResourceData, meta interface
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
 		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-06-01"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
 		if err != nil {
-			if NeedRetry(err) {
+			if NeedRetry(err) || IsExpectedErrors(err, []string{"DeleteConflict.Application"}) {
 				wait()
 				return resource.RetryableError(err)
 			}
