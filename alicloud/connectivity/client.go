cb5722d08c3d8834dc2f9e3c0933a93125d3d85a
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-01-19 11:39:30 +0800

    New Resource: alicloud_cloud_monitor_service_enterprise_public; New Resource: alicloud_cloud_monitor_service_basic_public.

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -5249,3 +5249,25 @@ func (client *AliyunClient) NewExpressconnectClient() (*rpc.Client, error) {
 	}
 	return conn, nil
 }
+func (client *AliyunClient) NewCloudmonitorserviceClient() (*rpc.Client, error) {
+	productCode := "cms"
+	endpoint := ""
+	if v, ok := client.config.Endpoints.Load(productCode); !ok || v.(string) == "" {
+		if err := client.loadEndpoint(productCode); err != nil {
+			return nil, err
+		}
+	}
+	if v, ok := client.config.Endpoints.Load(productCode); ok && v.(string) != "" {
+		endpoint = v.(string)
+	}
+	if endpoint == "" {
+		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
+	}
+	sdkConfig := client.teaSdkConfig
+	sdkConfig.SetEndpoint(endpoint)
+	conn, err := rpc.NewClient(&sdkConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
+	}
+	return conn, nil
+}
