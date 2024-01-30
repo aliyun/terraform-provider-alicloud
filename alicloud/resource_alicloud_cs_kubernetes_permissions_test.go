c719e6bbae24bf236c8298eb939a45050e77c1cc
hengyuwu <wuhengyu.why@alibaba-inc.com>
2024-01-30 21:01:35 +0800

    resource/alicloud_cs_kubernetes_permissions: fix deleted and update bug

--- alicloud/resource_alicloud_cs_kubernetes_permissions_test.go
+++ alicloud/resource_alicloud_cs_kubernetes_permissions_test.go
@@ -11,7 +11,7 @@ import (
 	cs "github.com/alibabacloud-go/cs-20151215/v3/client"
 )
 
-func TestAccAlicloudCSKubernetesPermissions_basic(t *testing.T) {
+func TestAccAliCloudCSKubernetesPermissions_basic(t *testing.T) {
 	var v []*cs.GrantPermissionsRequestBody
 	resourceId := "alicloud_cs_kubernetes_permissions.default"
 	serviceFunc := func() interface{} {
@@ -27,8 +27,8 @@ func TestAccAlicloudCSKubernetesPermissions_basic(t *testing.T) {
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := acctest.RandIntRange(1000000, 9999999)
 	name := fmt.Sprintf("tf-testAccCSKubernetesPermission-%d", rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceCSPermissionsConfigDependence)
-
+	testAccCreateConfig := resourceTestAccConfigFunc(resourceId, name, resourceCSCreatePermissionsConfigDependence)
+	testAccUpdateConfig := resourceTestAccConfigFunc(resourceId, name, resourceCSUpdatePermissionsConfigDependence)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -40,7 +40,7 @@ func TestAccAlicloudCSKubernetesPermissions_basic(t *testing.T) {
 		//CheckDestroy:  rac.checkResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccConfig(map[string]interface{}{
+				Config: testAccCreateConfig(map[string]interface{}{
 					"uid": "${alicloud_ram_user.user.id}",
 					"permissions": []map[string]string{
 						{
@@ -48,6 +48,92 @@ func TestAccAlicloudCSKubernetesPermissions_basic(t *testing.T) {
 							"role_type":   "cluster",
 							"role_name":   "dev",
 							"namespace":   "",
+							"is_custom":   "false",
+							"is_ram_role": "false",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"uid": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccUpdateConfig(map[string]interface{}{
+					"uid": "${alicloud_ram_user.user.id}",
+					"permissions": []map[string]string{
+						{
+							"cluster":     "${alicloud_cs_managed_kubernetes.default.0.id}",
+							"role_type":   "cluster",
+							"role_name":   "admin",
+							"namespace":   "",
+							"is_custom":   "false",
+							"is_ram_role": "false",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"uid": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccUpdateConfig(map[string]interface{}{
+					"uid": "${alicloud_ram_user.user.id}",
+					"permissions": []map[string]string{
+						{
+							"cluster":     "${alicloud_cs_managed_kubernetes.default.0.id}",
+							"role_type":   "cluster",
+							"role_name":   "admin",
+							"namespace":   "",
+							"is_custom":   "false",
+							"is_ram_role": "false",
+						},
+						{
+							"cluster":     "${alicloud_cs_managed_kubernetes.default.0.id}",
+							"role_type":   "cluster",
+							"role_name":   "admin",
+							"namespace":   "",
+							"is_custom":   "true",
+							"is_ram_role": "false",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"uid": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccUpdateConfig(map[string]interface{}{
+					"uid": "${alicloud_ram_user.user.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"uid": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccUpdateConfig(map[string]interface{}{
+					"uid": "${alicloud_ram_user.user.id}",
+					"permissions": []map[string]string{
+						{
+							"cluster":     "${alicloud_cs_managed_kubernetes.default.1.id}",
+							"role_type":   "namespace",
+							"role_name":   "dev",
+							"namespace":   "default",
+							"is_custom":   "false",
+							"is_ram_role": "true",
+						},
+						{
+							"cluster":     "${alicloud_cs_managed_kubernetes.default.1.id}",
+							"role_type":   "namespace",
+							"role_name":   "dev",
+							"namespace":   "kube-system",
 							"is_custom":   "false",
 							"is_ram_role": "false",
 						},
@@ -67,65 +153,148 @@ var permissionMap = map[string]string{
 	"uid": CHECKSET,
 }
 
-func resourceCSPermissionsConfigDependence(name string) string {
+func resourceCSCreatePermissionsConfigDependence(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
 	default = "%s"
 }
 
-data "alicloud_zones" "default" {
-  available_resource_creation  = "VSwitch"
+# leave it to empty would create a new one
+variable "vpc_id" {
+  description = "Existing vpc id used to create several vswitches and other resources."
+  default     = ""
 }
 
-data "alicloud_instance_types" "default" {
-	availability_zone          = data.alicloud_zones.default.zones.0.id
-	cpu_core_count             = 4
-	memory_size                = 8
-	kubernetes_node_role       = "Worker"
-    instance_type_family       = "ecs.sn1ne"
+variable "vpc_cidr" {
+  description = "The cidr block used to launch a new vpc when 'vpc_id' is not specified."
+  default     = "10.0.0.0/8"
 }
 
-data "alicloud_vpcs" "default" {
-    name_regex = "^default-NODELETING$"
+# leave it to empty then terraform will create several vswitches
+variable "vswitch_ids" {
+  description = "List of existing vswitch id."
+  type        = list(string)
+  default     = []
 }
 
-data "alicloud_vswitches" "default" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_zones.default.zones.0.id
+
+variable "vswitch_cidrs" {
+  description = "List of cidr blocks used to create several new vswitches when 'vswitch_ids' is not specified."
+  type        = list(string)
+  default     = ["10.1.0.0/16", "10.2.0.0/16"]
 }
 
-resource "alicloud_vswitch" "vswitch" {
-  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 8)
-  zone_id           = data.alicloud_zones.default.zones.0.id
-  vswitch_name      = var.name
+# options: between 24-28
+variable "node_cidr_mask" {
+  description = "The node cidr block to specific how many pods can run on single node."
+  default     = 24
 }
 
-locals {
-  vswitch_id = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids[0] : concat(alicloud_vswitch.vswitch.*.id, [""])[0]
+# options: ipvs|iptables
+variable "proxy_mode" {
+  description = "Proxy mode is option of kube-proxy."
+  default     = "ipvs"
 }
 
-# Create a management cluster
+variable "service_cidr" {
+  description = "The kubernetes service cidr block. It cannot be equals to vpc's or vswitch's or pod's and cannot be in them."
+  default     = "192.168.0.0/16"
+}
+
+variable "terway_vswitch_ids" {
+  description = "List of existing vswitch ids for terway."
+  type        = list(string)
+  default     = []
+}
+
+variable "terway_vswitch_cidrs" {
+  description = "List of cidr blocks used to create several new vswitches when 'terway_vswitch_cidrs' is not specified."
+  type        = list(string)
+  default     = ["10.4.0.0/16", "10.5.0.0/16"]
+}
+
+variable "cluster_addons" {
+  type = list(object({
+    name   = string
+    config = string
+  }))
+
+  default = [
+    {
+      "name"   = "terway-eniip",
+      "config" = "",
+    },
+    {
+      "name"   = "csi-plugin",
+      "config" = "",
+    },
+    {
+      "name"   = "csi-provisioner",
+      "config" = "",
+    },
+    {
+      "name"   = "logtail-ds",
+      "config" = "{'IngressDashboardEnabled':'true'}",
+    },
+    {
+      "name"   = "nginx-ingress-controller",
+      "config" = "{'IngressSlbNetworkType':'internet'}",
+    },
+    {
+      "name"   = "arms-prometheus",
+      "config" = "",
+    },
+    {
+      "name"   = "ack-node-problem-detector",
+      "config" = "{'sls_project_name':''}",
+    }
+  ]
+}
+
+data "alicloud_enhanced_nat_available_zones" "enhanced" {}
+
+# If there is not specifying vpc_id, the module will launch a new vpc
+resource "alicloud_vpc" "vpc" {
+  count      = var.vpc_id == "" ? 1 : 0
+  cidr_block = var.vpc_cidr
+}
+
+# According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "vswitches" {
+  count      = length(var.vswitch_ids) > 0 ? 0 : length(var.vswitch_cidrs)
+  vpc_id     = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block = element(var.vswitch_cidrs, count.index)
+  zone_id    = data.alicloud_enhanced_nat_available_zones.enhanced.zones[count.index].zone_id
+}
+
+# According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "terway_vswitches" {
+  count      = length(var.terway_vswitch_ids) > 0 ? 0 : length(var.terway_vswitch_cidrs)
+  vpc_id     = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block = element(var.terway_vswitch_cidrs, count.index)
+  zone_id    = data.alicloud_enhanced_nat_available_zones.enhanced.zones[count.index].zone_id
+}
+
 resource "alicloud_cs_managed_kubernetes" "default" {
-  name                        = var.name
-  count                       = 1
-  cluster_spec                = "ack.pro.small"
-  worker_vswitch_ids          = [local.vswitch_id]
-  new_nat_gateway             = true
-  worker_instance_types       = ["${data.alicloud_instance_types.default.instance_types.0.id}"]
-  node_port_range             = "30000-32767"
-  password                    = "Hello1234"
-  pod_cidr                    = cidrsubnet("10.0.0.0/8", 8, 41)
-  service_cidr                = cidrsubnet("172.17.0.0/16", 4, 3)
-  install_cloud_monitor       = true
-  slb_internet_enabled        = true
-  worker_disk_category        = "cloud_efficiency"
-  worker_data_disk_category   = "cloud_ssd"
-  worker_data_disk_size       = 200
-  worker_disk_size            = 40
-  worker_instance_charge_type = "PostPaid"
-  depends_on                  = ["alicloud_ram_user_policy_attachment.attach"]
+  count=2
+  name         = join("-",[var.name,count.index])
+  cluster_spec = "ack.pro.small"
+  # version can not be defined in variables.tf.
+  version            = "1.26.3-aliyun.1"
+  worker_vswitch_ids = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)) : length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.vswitches.*.id))
+  pod_vswitch_ids    = length(var.terway_vswitch_ids) > 0 ? split(",", join(",", var.terway_vswitch_ids)) : length(var.terway_vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.terway_vswitches.*.id))
+  new_nat_gateway    = true
+  node_cidr_mask     = var.node_cidr_mask
+  proxy_mode         = var.proxy_mode
+  service_cidr       = var.service_cidr
+
+  dynamic "addons" {
+    for_each = var.cluster_addons
+    content {
+      name   = lookup(addons.value, "name", var.cluster_addons)
+      config = lookup(addons.value, "config", var.cluster_addons)
+    }
+  }
 }
 
 # Create a new RAM user.
@@ -135,6 +304,200 @@ resource "alicloud_ram_user" "user" {
   mobile       = "86-18688888888"
   email        = "hello.uuu@aaa.com"
   comments     = "yoyoyo"
+}
+
+# Create a new RAM Policy, .
+resource "alicloud_ram_policy" "policy" {
+  policy_name     = var.name
+  policy_document = <<EOF
+  {
+    "Statement":[
+      {
+        "Action":"cs:Get*",
+        "Effect":"Allow",
+        "Resource":[
+            "*"
+        ]
+      }
+    ],
+    "Version":"1"
+  }
+  EOF
+  description = "this is a policy test by tf"
+}
+
+# Authorize the RAM user
+resource "alicloud_ram_user_policy_attachment" "attach" {
+  policy_name = alicloud_ram_policy.policy.name
+  policy_type = alicloud_ram_policy.policy.type
+  user_name   = alicloud_ram_user.user.name
+}
+
+`, name)
+}
+
+func resourceCSUpdatePermissionsConfigDependence(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+	default = "%s"
+}
+
+# leave it to empty would create a new one
+variable "vpc_id" {
+  description = "Existing vpc id used to create several vswitches and other resources."
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  description = "The cidr block used to launch a new vpc when 'vpc_id' is not specified."
+  default     = "10.0.0.0/8"
+}
+
+# leave it to empty then terraform will create several vswitches
+variable "vswitch_ids" {
+  description = "List of existing vswitch id."
+  type        = list(string)
+  default     = []
+}
+
+
+variable "vswitch_cidrs" {
+  description = "List of cidr blocks used to create several new vswitches when 'vswitch_ids' is not specified."
+  type        = list(string)
+  default     = ["10.1.0.0/16", "10.2.0.0/16"]
+}
+
+# options: between 24-28
+variable "node_cidr_mask" {
+  description = "The node cidr block to specific how many pods can run on single node."
+  default     = 24
+}
+
+# options: ipvs|iptables
+variable "proxy_mode" {
+  description = "Proxy mode is option of kube-proxy."
+  default     = "ipvs"
+}
+
+variable "service_cidr" {
+  description = "The kubernetes service cidr block. It cannot be equals to vpc's or vswitch's or pod's and cannot be in them."
+  default     = "192.168.0.0/16"
+}
+
+variable "terway_vswitch_ids" {
+  description = "List of existing vswitch ids for terway."
+  type        = list(string)
+  default     = []
+}
+
+variable "terway_vswitch_cidrs" {
+  description = "List of cidr blocks used to create several new vswitches when 'terway_vswitch_cidrs' is not specified."
+  type        = list(string)
+  default     = ["10.4.0.0/16", "10.5.0.0/16"]
+}
+
+variable "cluster_addons" {
+  type = list(object({
+    name   = string
+    config = string
+  }))
+
+  default = [
+    {
+      "name"   = "terway-eniip",
+      "config" = "",
+    },
+    {
+      "name"   = "csi-plugin",
+      "config" = "",
+    },
+    {
+      "name"   = "csi-provisioner",
+      "config" = "",
+    },
+    {
+      "name"   = "logtail-ds",
+      "config" = "{'IngressDashboardEnabled':'true'}",
+    },
+    {
+      "name"   = "nginx-ingress-controller",
+      "config" = "{'IngressSlbNetworkType':'internet'}",
+    },
+    {
+      "name"   = "arms-prometheus",
+      "config" = "",
+    },
+    {
+      "name"   = "ack-node-problem-detector",
+      "config" = "{'sls_project_name':''}",
+    }
+  ]
+}
+
+data "alicloud_enhanced_nat_available_zones" "enhanced" {}
+
+# If there is not specifying vpc_id, the module will launch a new vpc
+resource "alicloud_vpc" "vpc" {
+  count      = var.vpc_id == "" ? 1 : 0
+  cidr_block = var.vpc_cidr
+}
+
+# According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "vswitches" {
+  count      = length(var.vswitch_ids) > 0 ? 0 : length(var.vswitch_cidrs)
+  vpc_id     = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block = element(var.vswitch_cidrs, count.index)
+  zone_id    = data.alicloud_enhanced_nat_available_zones.enhanced.zones[count.index].zone_id
+}
+
+# According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "terway_vswitches" {
+  count      = length(var.terway_vswitch_ids) > 0 ? 0 : length(var.terway_vswitch_cidrs)
+  vpc_id     = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block = element(var.terway_vswitch_cidrs, count.index)
+  zone_id    = data.alicloud_enhanced_nat_available_zones.enhanced.zones[count.index].zone_id
+}
+
+resource "alicloud_cs_managed_kubernetes" "default" {
+  count=2
+  name         = join("-",[var.name,count.index])
+  cluster_spec = "ack.pro.small"
+  # version can not be defined in variables.tf.
+  version            = "1.26.3-aliyun.1"
+  worker_vswitch_ids = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)) : length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.vswitches.*.id))
+  pod_vswitch_ids    = length(var.terway_vswitch_ids) > 0 ? split(",", join(",", var.terway_vswitch_ids)) : length(var.terway_vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.terway_vswitches.*.id))
+  new_nat_gateway    = true
+  node_cidr_mask     = var.node_cidr_mask
+  proxy_mode         = var.proxy_mode
+  service_cidr       = var.service_cidr
+
+  dynamic "addons" {
+    for_each = var.cluster_addons
+    content {
+      name   = lookup(addons.value, "name", var.cluster_addons)
+      config = lookup(addons.value, "config", var.cluster_addons)
+    }
+  }
+}
+# Create a new RAM user.
+resource "alicloud_ram_user" "user" {
+  name         = var.name
+  display_name = var.name
+  mobile       = "86-18688888888"
+  email        = "hello.uuu@aaa.com"
+  comments     = "yoyoyo"
+}
+
+resource "alicloud_cs_kubernetes_permissions" "test" {
+  uid = "${alicloud_ram_user.user.id}"
+  permissions {
+    cluster     = "${alicloud_cs_managed_kubernetes.default.0.id}"
+    role_type   = "cluster"
+    role_name   = "dev"
+    is_custom   = false
+    is_ram_role = false
+    namespace = ""
+  }
 }
 
 # Create a new RAM Policy, .
