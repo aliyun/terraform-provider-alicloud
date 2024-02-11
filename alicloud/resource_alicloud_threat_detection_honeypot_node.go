2c107bfaf3562ad8c3248aaf014dba64ba6e2f47
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-02-11 10:31:16 +0800

    resource/alicloud_threat_detection_honeypot_node: modify creating asynchronous timeout.

--- alicloud/resource_alicloud_threat_detection_honeypot_node.go
+++ alicloud/resource_alicloud_threat_detection_honeypot_node.go
@@ -22,9 +22,9 @@ func resourceAlicloudThreatDetectionHoneypotNode() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(5 * time.Minute),
-			Delete: schema.DefaultTimeout(5 * time.Minute),
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"allow_honeypot_access_internet": {
