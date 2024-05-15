4c3a0a3eba8b590ab3e295faa012aa728dcfba26
MrWolong <ly01269865@alibaba-inc.com>
2024-05-15 18:34:28 +0800

    resource/alicloud_kms_key: Added the field policy; Improved alicloud_kms_key testcase

--- alicloud/service_alicloud_kms.go
+++ alicloud/service_alicloud_kms.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
@@ -19,15 +20,17 @@ type KmsService struct {
 
 func (s *KmsService) DescribeKmsKey(id string) (object map[string]interface{}, err error) {
 	var response map[string]interface{}
+	action := "DescribeKey"
+
 	conn, err := s.client.NewKmsClient()
 	if err != nil {
 		return nil, WrapError(err)
 	}
-	action := "DescribeKey"
+
 	request := map[string]interface{}{
-		"RegionId": s.client.RegionId,
-		"KeyId":    id,
+		"KeyId": id,
 	}
+
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
@@ -43,21 +46,90 @@ func (s *KmsService) DescribeKmsKey(id string) (object map[string]interface{}, e
 		return nil
 	})
 	addDebug(action, response, request)
+
 	if err != nil {
 		if IsExpectedErrors(err, []string{"Forbidden.AliasNotFound", "Forbidden.KeyNotFound"}) {
-			return object, WrapErrorf(Error(GetNotFoundMessage("KMS:Key", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
+			return object, WrapErrorf(Error(GetNotFoundMessage("Kms:Key", id)), NotFoundMsg, ProviderERROR, fmt.Sprint(response["RequestId"]))
 		}
 		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
 	}
+
 	v, err := jsonpath.Get("$.KeyMetadata", response)
 	if err != nil {
 		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.KeyMetadata", response)
 	}
+
 	object = v.(map[string]interface{})
+
 	if object["KeyState"] == "PendingDeletion" {
-		log.Printf("[WARN] Removing KmsKey  %s because it's already gone", id)
-		return object, WrapErrorf(Error(GetNotFoundMessage("KmsKey", id)), NotFoundMsg, ProviderERROR)
+		log.Printf("[WARN] Removing Kms:Key %s because it's already gone", id)
+		return object, WrapErrorf(Error(GetNotFoundMessage("Kms:Key", id)), NotFoundMsg, ProviderERROR)
 	}
+
+	return object, nil
+}
+
+func (s *KmsService) KmsKeyStateRefreshFunc(id string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeKmsKey(id)
+		if err != nil {
+			if NotFoundError(err) {
+				// Set this to nil as if we didn't find anything.
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		for _, failState := range failStates {
+			if fmt.Sprint(object["KeyState"]) == failState {
+				return object, fmt.Sprint(object["KeyState"]), WrapError(Error(FailedToReachTargetStatus, fmt.Sprint(object["KeyState"])))
+			}
+		}
+
+		return object, fmt.Sprint(object["KeyState"]), nil
+	}
+}
+
+func (s *KmsService) DescribeKmsKeyPolicy(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	action := "GetKeyPolicy"
+
+	conn, err := s.client.NewKmsClient()
+	if err != nil {
+		return nil, WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"KeyId": id,
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+
+	object = v.(map[string]interface{})
+
 	return object, nil
 }
 
@@ -279,148 +351,181 @@ func (s *KmsService) DescribeKmsKeyVersion(id string) (object map[string]interfa
 	return object, nil
 }
 
-func (s *KmsService) SetResourceTags(d *schema.ResourceData, resourceType string) error {
-	oldItems, newItems := d.GetChange("tags")
-	added := make([]JsonTag, 0)
-	for key, value := range newItems.(map[string]interface{}) {
-		added = append(added, JsonTag{
-			TagKey:   key,
-			TagValue: value.(string),
-		})
-	}
-	removed := make([]string, 0)
-	for key := range oldItems.(map[string]interface{}) {
-		removed = append(removed, key)
-	}
-	// 对系统 Tag 进行过滤
-	removedTagKeys := make([]string, 0)
-	for _, v := range removed {
-		if !ignoredTags(v, "") {
-			removedTagKeys = append(removedTagKeys, v)
-		}
-	}
-	if len(removedTagKeys) > 0 {
-		var response map[string]interface{}
-		conn, err := s.client.NewKmsClient()
-		if err != nil {
-			return WrapError(err)
-		}
-		action := "UntagResource"
-		request := map[string]interface{}{
-			"RegionId": s.client.RegionId,
-		}
-		if resourceType == "key" {
-			request["KeyId"] = d.Id()
-		}
-		if resourceType == "secret" {
-			request["SecretName"] = d.Id()
-		}
-		remove, err := json.Marshal(removed)
-		if err != nil {
-			return WrapError(err)
-		}
-		request["TagKeys"] = string(remove)
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
-			if err != nil {
-				if NeedRetry(err) {
-					wait()
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-			return nil
-		})
-		addDebug(action, response, request)
-		if err != nil {
-			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
-		}
-	}
-	if len(added) > 0 {
-		var response map[string]interface{}
-		conn, err := s.client.NewKmsClient()
-		if err != nil {
-			return WrapError(err)
-		}
-		action := "TagResource"
-		request := map[string]interface{}{
-			"RegionId": s.client.RegionId,
-		}
-		if resourceType == "key" {
-			request["KeyId"] = d.Id()
-		}
-		if resourceType == "secret" {
-			request["SecretName"] = d.Id()
-		}
-		add, err := json.Marshal(added)
-		if err != nil {
-			return WrapError(err)
-		}
-		request["Tags"] = string(add)
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
-			if err != nil {
-				if NeedRetry(err) {
-					wait()
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-			return nil
-		})
-		addDebug(action, response, request)
-		if err != nil {
-			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
-		}
-	}
-	return nil
-}
-
-func (s *KmsService) ListTagResources(id string) (object interface{}, err error) {
+func (s *KmsService) ListTagResources(id string, resourceType string) (object interface{}, err error) {
 	conn, err := s.client.NewKmsClient()
 	if err != nil {
 		return nil, WrapError(err)
 	}
-	action := "ListResourceTags"
+	action := "ListTagResources"
 
 	request := map[string]interface{}{
-		"KeyId": id,
+		"RegionId":     s.client.RegionId,
+		"ResourceType": resourceType,
+	}
+
+	resourceIdNum := strings.Count(id, ":")
+
+	switch resourceIdNum {
+	case 0:
+		request["ResourceId.1"] = id
+	case 1:
+		parts, err := ParseResourceId(id, 2)
+		if err != nil {
+			return object, WrapError(err)
+		}
+		request["ResourceId.1"] = parts[resourceIdNum]
 	}
 
 	tags := make([]interface{}, 0)
 	var response map[string]interface{}
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
-		if err != nil {
-			if NeedRetry(err) {
-				wait()
-				return resource.RetryableError(err)
+	for {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
 			}
-			return resource.NonRetryableError(err)
-		}
+			addDebug(action, response, request)
+			v, err := jsonpath.Get("$.TagResources.TagResource", response)
+			if err != nil {
+				return resource.NonRetryableError(WrapErrorf(err, FailedGetAttributeMsg, id, "$.TagResources.TagResource", response))
+			}
 
-		addDebug(action, response, request)
+			if v != nil {
+				tags = append(tags, v.([]interface{})...)
+			}
 
-		v, err := jsonpath.Get("$.Tags.Tag", response)
+			return nil
+		})
 		if err != nil {
-			return resource.NonRetryableError(WrapErrorf(err, FailedGetAttributeMsg, id, "$.Tags.Tag", response))
+			err = WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+			return
 		}
 
-		if v != nil {
-			tags = append(tags, v.([]interface{})...)
+		if nextToken, ok := response["NextToken"].(string); ok && nextToken != "" {
+			request["NextToken"] = nextToken
+		} else {
+			break
 		}
-
-		return nil
-	})
-
-	if err != nil {
-		err = WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
-		return
 	}
 
 	return tags, nil
+}
+
+func (s *KmsService) SetResourceTags(d *schema.ResourceData, resourceType string) error {
+
+	resourceIdNum := strings.Count(d.Id(), ":")
+
+	if d.HasChange("tags") {
+		added, removed := parsingTags(d)
+		conn, err := s.client.NewKmsClient()
+		if err != nil {
+			return WrapError(err)
+		}
+
+		removedTagKeys := make([]string, 0)
+		for _, v := range removed {
+			if !ignoredTags(v, "") {
+				removedTagKeys = append(removedTagKeys, v)
+			}
+		}
+
+		if len(removedTagKeys) > 0 {
+			action := "UntagResources"
+			request := map[string]interface{}{
+				"RegionId":     s.client.RegionId,
+				"ResourceType": resourceType,
+			}
+
+			switch resourceIdNum {
+			case 0:
+				request["ResourceId.1"] = d.Id()
+			case 1:
+				parts, err := ParseResourceId(d.Id(), 2)
+				if err != nil {
+					return WrapError(err)
+				}
+				request["ResourceId.1"] = parts[resourceIdNum]
+			}
+
+			for i, key := range removedTagKeys {
+				request[fmt.Sprintf("TagKey.%d", i+1)] = key
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(2*time.Second, 1*time.Second)
+			err := resource.Retry(10*time.Minute, func() *resource.RetryError {
+				response, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+
+					}
+					return resource.NonRetryableError(err)
+				}
+				addDebug(action, response, request)
+				return nil
+			})
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+		}
+
+		if len(added) > 0 {
+			action := "TagResources"
+			request := map[string]interface{}{
+				"RegionId":     s.client.RegionId,
+				"ResourceType": resourceType,
+			}
+
+			switch resourceIdNum {
+			case 0:
+				request["ResourceId.1"] = d.Id()
+			case 1:
+				parts, err := ParseResourceId(d.Id(), 2)
+				if err != nil {
+					return WrapError(err)
+				}
+				request["ResourceId.1"] = parts[resourceIdNum]
+			}
+
+			count := 1
+			for key, value := range added {
+				request[fmt.Sprintf("Tag.%d.Key", count)] = key
+				request[fmt.Sprintf("Tag.%d.Value", count)] = value
+				count++
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(2*time.Second, 1*time.Second)
+			err := resource.Retry(10*time.Minute, func() *resource.RetryError {
+				response, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2016-01-20"), StringPointer("AK"), nil, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+
+					}
+					return resource.NonRetryableError(err)
+				}
+				addDebug(action, response, request)
+				return nil
+			})
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+		}
+		d.SetPartial("tags")
+	}
+	return nil
 }
