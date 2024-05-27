b2a267dc87ebdb8671d093c0ef26b59c218b2175
MrWolong <ly01269865@alibaba-inc.com>
2024-05-27 11:18:56 +0800

    data-source/alicloud_api_gateway_apis: Fixed the read bug; Added the field api_id

--- alicloud/data_source_alicloud_api_gateway_apis.go
+++ alicloud/data_source_alicloud_api_gateway_apis.go
@@ -1,6 +1,7 @@
 package alicloud
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
@@ -10,27 +11,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func dataSourceAlicloudApiGatewayApis() *schema.Resource {
+func dataSourceAliCloudApiGatewayApis() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceAlicloudApigatewayApisRead,
-
+		Read: dataSourceAliCloudApiGatewayApisRead,
 		Schema: map[string]*schema.Schema{
-			"group_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-			"api_id": {
-				Type:       schema.TypeString,
-				Optional:   true,
-				ForceNew:   true,
-				Deprecated: "Field 'api_id' has been deprecated from provider version 1.52.2. New field 'ids' replaces it.",
-			},
 			"ids": {
 				Type:     schema.TypeList,
 				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+				ForceNew: true,
 				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			"name_regex": {
 				Type:         schema.TypeString,
@@ -38,11 +28,20 @@ func dataSourceAlicloudApiGatewayApis() *schema.Resource {
 				ForceNew:     true,
 				ValidateFunc: validation.ValidateRegexp,
 			},
+			"group_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"api_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
 			"output_file": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			// Computed values
 			"names": {
 				Type:     schema.TypeList,
 				Computed: true,
@@ -57,15 +56,19 @@ func dataSourceAlicloudApiGatewayApis() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"group_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"api_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"name": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"region_id": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"group_id": {
+						"description": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -73,7 +76,7 @@ func dataSourceAlicloudApiGatewayApis() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"description": {
+						"region_id": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -83,49 +86,24 @@ func dataSourceAlicloudApiGatewayApis() *schema.Resource {
 		},
 	}
 }
-func dataSourceAlicloudApigatewayApisRead(d *schema.ResourceData, meta interface{}) error {
+
+func dataSourceAliCloudApiGatewayApisRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+
 	request := cloudapi.CreateDescribeApisRequest()
 	request.RegionId = client.RegionId
-
-	if groupId, ok := d.GetOk("group_id"); ok {
-		request.GroupId = groupId.(string)
-	}
-	if apiId, ok := d.GetOk("api_id"); ok {
-		request.ApiId = apiId.(string)
-	}
-
 	request.PageSize = requests.NewInteger(PageSizeLarge)
 	request.PageNumber = requests.NewInteger(1)
 
-	var allapis []cloudapi.ApiSummary
-
-	for {
-		raw, err := client.WithCloudApiClient(func(cloudApiClient *cloudapi.Client) (interface{}, error) {
-			return cloudApiClient.DescribeApis(request)
-		})
-		if err != nil {
-			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_api_gateway_apis", request.GetActionName(), AlibabaCloudSdkGoERROR)
-		}
-		addDebug(request.GetActionName(), raw, request.RpcRequest, request)
-		response, _ := raw.(*cloudapi.DescribeApisResponse)
-
-		allapis = append(allapis, response.ApiSummarys.ApiSummary...)
-
-		if len(allapis) < PageSizeLarge {
-			break
-		}
-
-		page, err := getNextpageNumber(request.PageNumber)
-		if err != nil {
-			return WrapError(err)
-		}
-		request.PageNumber = page
+	if v, ok := d.GetOk("group_id"); ok {
+		request.GroupId = v.(string)
 	}
 
-	var filteredApisTemp []cloudapi.ApiSummary
+	if v, ok := d.GetOk("api_id"); ok {
+		request.ApiId = v.(string)
+	}
 
-	// ids
+	var objects []cloudapi.ApiSummary
 	idsMap := make(map[string]string)
 	if v, ok := d.GetOk("ids"); ok {
 		for _, vv := range v.([]interface{}) {
@@ -135,57 +113,89 @@ func dataSourceAlicloudApigatewayApisRead(d *schema.ResourceData, meta interface
 			idsMap[vv.(string)] = vv.(string)
 		}
 	}
-	for _, api := range allapis {
-		if v, ok := d.GetOk("name_regex"); ok && v.(string) != "" {
-			r := regexp.MustCompile(v.(string))
-			if !r.MatchString(api.ApiName) {
-				continue
-			}
+
+	var apiNameRegex *regexp.Regexp
+	if v, ok := d.GetOk("name_regex"); ok {
+		r, err := regexp.Compile(v.(string))
+		if err != nil {
+			return WrapError(err)
 		}
-		if len(idsMap) > 0 {
-			if _, ok := idsMap[api.ApiId]; !ok {
-				continue
-			}
-		}
-		filteredApisTemp = append(filteredApisTemp, api)
+		apiNameRegex = r
 	}
 
-	return apiGatewayApisDescribeSummarys(d, filteredApisTemp, meta)
-}
+	for {
+		response, err := client.WithCloudApiClient(func(cloudApiClient *cloudapi.Client) (interface{}, error) {
+			return cloudApiClient.DescribeApis(request)
+		})
+		addDebug(request.GetActionName(), response, request.RpcRequest, request)
 
-func apiGatewayApisDescribeSummarys(d *schema.ResourceData, apis []cloudapi.ApiSummary, meta interface{}) error {
-	var ids []string
-	var names []string
-	var s []map[string]interface{}
-	for _, api := range apis {
-		mapping := map[string]interface{}{
-			"id":          api.ApiId,
-			"name":        api.ApiName,
-			"region_id":   api.RegionId,
-			"group_id":    api.GroupId,
-			"group_name":  api.GroupName,
-			"description": api.Description,
+		if err != nil {
+			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_api_gateway_apis", request.GetActionName(), AlibabaCloudSdkGoERROR)
 		}
-		ids = append(ids, api.ApiId)
-		s = append(s, mapping)
-		names = append(names, api.ApiName)
 
+		result := response.(*cloudapi.DescribeApisResponse).ApiSummarys.ApiSummary
+		for _, item := range result {
+			if len(idsMap) > 0 {
+				if _, ok := idsMap[fmt.Sprintf("%v:%v", item.GroupId, item.ApiId)]; !ok {
+					continue
+				}
+			}
+
+			if apiNameRegex != nil && !apiNameRegex.MatchString(fmt.Sprint(item.ApiName)) {
+				continue
+			}
+
+			objects = append(objects, item)
+		}
+
+		if len(result) < PageSizeLarge {
+			break
+		}
+
+		pageNum, err := getNextpageNumber(request.PageNumber)
+		if err != nil {
+			return WrapError(err)
+		}
+
+		request.PageNumber = pageNum
+	}
+
+	ids := make([]string, 0)
+	names := make([]interface{}, 0)
+	s := make([]map[string]interface{}, 0)
+	for _, object := range objects {
+		mapping := map[string]interface{}{
+			"id":          fmt.Sprintf("%v:%v", object.GroupId, object.ApiId),
+			"group_id":    fmt.Sprint(object.GroupId),
+			"api_id":      fmt.Sprint(object.ApiId),
+			"name":        object.ApiName,
+			"description": object.Description,
+			"group_name":  object.GroupName,
+			"region_id":   object.RegionId,
+		}
+
+		ids = append(ids, fmt.Sprint(mapping["id"]))
+		names = append(names, object.ApiName)
+		s = append(s, mapping)
 	}
 
 	d.SetId(dataResourceIdHash(ids))
-	if err := d.Set("apis", s); err != nil {
-		return WrapError(err)
-	}
+
 	if err := d.Set("ids", ids); err != nil {
 		return WrapError(err)
 	}
+
 	if err := d.Set("names", names); err != nil {
 		return WrapError(err)
 	}
 
-	// create a json file in current directory and write data source to it.
+	if err := d.Set("apis", s); err != nil {
+		return WrapError(err)
+	}
+
 	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
 		writeToFile(output.(string), s)
 	}
+
 	return nil
 }
