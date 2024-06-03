6d3930c3977b4d2a1d00a08e4343e94a49883292
MrWolong <ly01269865@alibaba-inc.com>
2024-06-03 18:34:41 +0800

    resource/alicloud_image_import: Added the field boot_mode; Improved alicloud_image_import testcase

--- alicloud/resource_alicloud_image_import.go
+++ alicloud/resource_alicloud_image_import.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 func resourceAliCloudImageImport() *schema.Resource {
@@ -25,33 +25,18 @@ func resourceAliCloudImageImport() *schema.Resource {
 		},
 		Schema: map[string]*schema.Schema{
 			"architecture": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-				Default:  "x86_64",
-				ValidateFunc: validation.StringInSlice([]string{
-					"x86_64",
-					"i386",
-				}, false),
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      "x86_64",
+				ValidateFunc: StringInSlice([]string{"x86_64", "i386"}, false),
 			},
-			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"image_name": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"license_type": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-				Default:  "Auto",
-				ValidateFunc: validation.StringInSlice([]string{
-					"Auto",
-					"Aliyun",
-					"BYOL",
-				}, false),
+			"os_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      "linux",
+				ValidateFunc: StringInSlice([]string{"windows", "linux"}, false),
 			},
 			"platform": {
 				Type:     schema.TypeString,
@@ -59,44 +44,40 @@ func resourceAliCloudImageImport() *schema.Resource {
 				ForceNew: true,
 				Computed: true,
 			},
-			"os_type": {
+			"boot_mode": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Computed:     true,
+				ValidateFunc: StringInSlice([]string{"BIOS", "UEFI"}, false),
+			},
+			"license_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "Auto",
+				ValidateFunc: StringInSlice([]string{"Auto", "Aliyun", "BYOL"}, false),
+			},
+			"image_name": {
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
-				Default:  "linux",
-				ValidateFunc: validation.StringInSlice([]string{
-					"windows",
-					"linux",
-				}, false),
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 			"disk_device_mapping": {
 				Type:     schema.TypeList,
-				ForceNew: true,
 				Required: true,
+				ForceNew: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"device": {
-							Type:     schema.TypeString,
-							Optional: true,
-							ForceNew: true,
-							Computed: true,
-						},
-						"disk_image_size": {
-							Type:     schema.TypeInt,
-							Optional: true,
-							Default:  5,
-							ForceNew: true,
-						},
 						"format": {
-							Type:     schema.TypeString,
-							Optional: true,
-							ForceNew: true,
-							Computed: true,
-							ValidateFunc: validation.StringInSlice([]string{
-								"RAW",
-								"VHD",
-								"qcow2",
-							}, false),
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							Computed:     true,
+							ValidateFunc: StringInSlice([]string{"RAW", "VHD", "qcow2"}, false),
 						},
 						"oss_bucket": {
 							Type:     schema.TypeString,
@@ -108,6 +89,18 @@ func resourceAliCloudImageImport() *schema.Resource {
 							Optional: true,
 							ForceNew: true,
 						},
+						"device": {
+							Type:     schema.TypeString,
+							Optional: true,
+							ForceNew: true,
+							Computed: true,
+						},
+						"disk_image_size": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							ForceNew: true,
+							Default:  5,
+						},
 					},
 				},
 			},
@@ -118,47 +111,99 @@ func resourceAliCloudImageImport() *schema.Resource {
 func resourceAliCloudImageImportCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	ecsService := EcsService{client: client}
-
 	request := ecs.CreateImportImageRequest()
 	request.RegionId = client.RegionId
-	request.Architecture = d.Get("architecture").(string)
-	request.Description = d.Get("description").(string)
-	request.ImageName = d.Get("image_name").(string)
-	request.LicenseType = d.Get("license_type").(string)
-	request.OSType = d.Get("os_type").(string)
-	request.Platform = d.Get("platform").(string)
 
-	diskDeviceMappings := d.Get("disk_device_mapping").([]interface{})
-	if diskDeviceMappings != nil && len(diskDeviceMappings) > 0 {
-		mappings := make([]ecs.ImportImageDiskDeviceMapping, 0, len(diskDeviceMappings))
-		for _, diskDeviceMapping := range diskDeviceMappings {
-			mapping := diskDeviceMapping.(map[string]interface{})
-			size := strconv.Itoa(mapping["disk_image_size"].(int))
-			diskmapping := ecs.ImportImageDiskDeviceMapping{
-				Device:        mapping["device"].(string),
-				DiskImageSize: size,
-				Format:        mapping["format"].(string),
-				OSSBucket:     mapping["oss_bucket"].(string),
-				OSSObject:     mapping["oss_object"].(string),
-			}
-			mappings = append(mappings, diskmapping)
+	if v, ok := d.GetOk("architecture"); ok {
+		request.Architecture = v.(string)
+	}
+
+	if v, ok := d.GetOk("os_type"); ok {
+		request.OSType = v.(string)
+	}
+
+	if v, ok := d.GetOk("platform"); ok {
+		request.Platform = v.(string)
+	}
+
+	if v, ok := d.GetOk("boot_mode"); ok {
+		request.BootMode = v.(string)
+	}
+
+	if v, ok := d.GetOk("license_type"); ok {
+		request.LicenseType = v.(string)
+	}
+
+	if v, ok := d.GetOk("image_name"); ok {
+		request.ImageName = v.(string)
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		request.Description = v.(string)
+	}
+
+	diskDeviceMappings := d.Get("disk_device_mapping")
+	diskDeviceMappingsMaps := make([]ecs.ImportImageDiskDeviceMapping, 0)
+	for _, diskDeviceMappingsList := range diskDeviceMappings.([]interface{}) {
+		var diskDeviceMappingsMap ecs.ImportImageDiskDeviceMapping
+		diskDeviceMappingsArg := diskDeviceMappingsList.(map[string]interface{})
+
+		if format, ok := diskDeviceMappingsArg["format"]; ok {
+			diskDeviceMappingsMap.Format = format.(string)
 		}
-		request.DiskDeviceMapping = &mappings
+
+		if ossBucket, ok := diskDeviceMappingsArg["oss_bucket"]; ok {
+			diskDeviceMappingsMap.OSSBucket = ossBucket.(string)
+		}
+
+		if ossObject, ok := diskDeviceMappingsArg["oss_object"]; ok {
+			diskDeviceMappingsMap.OSSObject = ossObject.(string)
+		}
+
+		if device, ok := diskDeviceMappingsArg["device"]; ok {
+			diskDeviceMappingsMap.Device = device.(string)
+		}
+
+		if diskImageSize, ok := diskDeviceMappingsArg["disk_image_size"]; ok {
+			diskImageSizeStr := strconv.Itoa(diskImageSize.(int))
+			diskDeviceMappingsMap.DiskImageSize = diskImageSizeStr
+		}
+
+		diskDeviceMappingsMaps = append(diskDeviceMappingsMaps, diskDeviceMappingsMap)
 	}
 
-	raw, err := client.WithEcsClient(func(ecsClient *ecs.Client) (interface{}, error) {
-		return ecsClient.ImportImage(request)
+	request.DiskDeviceMapping = &diskDeviceMappingsMaps
+
+	var raw interface{}
+	var err error
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
+		raw, err = client.WithEcsClient(func(ecsClient *ecs.Client) (interface{}, error) {
+			return ecsClient.ImportImage(request)
+		})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
 	})
-	if err != nil {
-		return WrapErrorf(err, DefaultErrorMsg, "alicloud_import_image", request.GetActionName(), AlibabaCloudSdkGoERROR)
-	}
 	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_image_import", request.GetActionName(), AlibabaCloudSdkGoERROR)
+	}
+
 	resp, _ := raw.(*ecs.ImportImageResponse)
 	d.SetId(resp.ImageId)
+
 	stateConf := BuildStateConf([]string{}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 1*time.Minute, ecsService.ImageStateRefreshFunc(d.Id(), []string{"CreateFailed", "UnAvailable"}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	return resourceAliCloudImageImportRead(d, meta)
 }
 
@@ -174,12 +219,20 @@ func resourceAliCloudImageImportRead(d *schema.ResourceData, meta interface{}) e
 		}
 		return WrapError(err)
 	}
-	d.Set("image_name", object.ImageName)
-	d.Set("description", object.Description)
+
 	d.Set("architecture", object.Architecture)
 	d.Set("os_type", object.OSType)
 	d.Set("platform", object.Platform)
-	d.Set("disk_device_mapping", FlattenImageImportDiskDeviceMappings(object.DiskDeviceMappings.DiskDeviceMapping))
+	d.Set("boot_mode", object.BootMode)
+	d.Set("image_name", object.ImageName)
+	d.Set("description", object.Description)
+
+	diskDeviceMappings, err := FlattenImageImportDiskDeviceMappings(object.DiskDeviceMappings.DiskDeviceMapping)
+	if err != nil {
+		return WrapError(err)
+	}
+
+	d.Set("disk_device_mapping", diskDeviceMappings)
 
 	return nil
 }
@@ -191,27 +244,35 @@ func resourceAliCloudImageImportUpdate(d *schema.ResourceData, meta interface{})
 	if err != nil {
 		return WrapError(err)
 	}
+
 	return resourceAliCloudImageRead(d, meta)
 }
 
 func resourceAliCloudImageImportDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
 	ecsService := EcsService{client}
+
 	return ecsService.deleteImage(d)
 }
 
-func FlattenImageImportDiskDeviceMappings(list []ecs.DiskDeviceMapping) []map[string]interface{} {
+func FlattenImageImportDiskDeviceMappings(list []ecs.DiskDeviceMapping) ([]map[string]interface{}, error) {
 	result := make([]map[string]interface{}, 0, len(list))
-	for _, i := range list {
-		size, _ := strconv.Atoi(i.Size)
-		l := map[string]interface{}{
-			"device":          i.Device,
-			"disk_image_size": size,
-			"format":          i.Format,
-			"oss_bucket":      i.ImportOSSBucket,
-			"oss_object":      i.ImportOSSObject,
+	for _, v := range list {
+		diskImageSize, err := strconv.Atoi(v.Size)
+		if err != nil {
+			return nil, WrapError(err)
 		}
-		result = append(result, l)
+
+		diskDeviceMappings := map[string]interface{}{
+			"format":          v.Format,
+			"oss_bucket":      v.ImportOSSBucket,
+			"oss_object":      v.ImportOSSObject,
+			"device":          v.Device,
+			"disk_image_size": diskImageSize,
+		}
+
+		result = append(result, diskDeviceMappings)
 	}
-	return result
+
+	return result, nil
 }
--- alicloud/resource_alicloud_image_import_test.go
+++ alicloud/resource_alicloud_image_import_test.go
@@ -2,32 +2,28 @@ package alicloud
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccAlicloudECSImageImport(t *testing.T) {
-	var v ecs.Image
+func TestAccAliCloudImageImport_basic0(t *testing.T) {
+	var v map[string]interface{}
 	resourceId := "alicloud_image_import.default"
-	ra := resourceAttrInit(resourceId, testAccImageImageCheckMap)
-	serviceFunc := func() interface{} {
+	ra := resourceAttrInit(resourceId, AliCloudImageImportMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
-	}
-	rand := acctest.RandIntRange(1000, 9999)
-	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, serviceFunc, "DescribeImageById")
+	}, "DescribeImageById")
 	rac := resourceAttrCheckInit(rc, ra)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
-	name := fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%d", rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceImageImageBasicConfigDependence)
+	rand := acctest.RandIntRange(1000, 9999)
+	name := fmt.Sprintf("tf-testacc%simageimport%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudImageImportBasicDependence0)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
-			testAccPreCheckOSSForImageImport(t)
 		},
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -35,62 +31,36 @@ func TestAccAlicloudECSImageImport(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"description":  fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescription", rand),
-					"image_name":   name,
-					"architecture": "x86_64",
-					"license_type": "Auto",
-					"platform":     "Ubuntu",
-					"os_type":      "linux",
 					"disk_device_mapping": []map[string]interface{}{
 						{
-							"oss_bucket": os.Getenv("ALICLOUD_OSS_BUCKET_FOR_IMAGE"),
-							"oss_object": os.Getenv("ALICLOUD_OSS_OBJECT_FOR_IMAGE"),
+							"oss_bucket": "${alicloud_oss_bucket.default.id}",
+							"oss_object": "${alicloud_oss_bucket_object.default.id}",
 						},
 					},
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"description":                      fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescription", rand),
-						"image_name":                       name,
-						"architecture":                     "x86_64",
-						"license_type":                     "Auto",
-						"platform":                         "Ubuntu",
-						"os_type":                          "linux",
-						"disk_device_mapping.#":            "1",
-						"disk_device_mapping.0.oss_bucket": CHECKSET,
-						"disk_device_mapping.0.oss_object": CHECKSET,
+						"disk_device_mapping.#": "1",
 					}),
 				),
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"description": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescriptionchange", rand),
+					"image_name": name,
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"description": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescriptionchange", rand),
+						"image_name": name,
 					}),
 				),
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"image_name": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%dchange", rand),
+					"description": name,
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"image_name": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%dchange", rand),
-					}),
-				),
-			},
-			{
-				Config: testAccConfig(map[string]interface{}{
-					"description": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescription", rand),
-					"image_name":  fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%d", rand),
-				}),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"description": fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%ddescription", rand),
-						"image_name":  fmt.Sprintf("tf-testAccEcsImageImportConfigBasic%d", rand),
+						"description": name,
 					}),
 				),
 			},
@@ -105,8 +75,93 @@ func TestAccAlicloudECSImageImport(t *testing.T) {
 
 }
 
-var testAccImageImageCheckMap = map[string]string{}
+func TestAccAliCloudImageImport_basic0_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_image_import.default"
+	ra := resourceAttrInit(resourceId, AliCloudImageImportMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeImageById")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000, 9999)
+	name := fmt.Sprintf("tf-testacc%simageimport%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudImageImportBasicDependence0)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"architecture": "i386",
+					"os_type":      "linux",
+					"platform":     "Aliyun",
+					"boot_mode":    "UEFI",
+					"license_type": "Auto",
+					"image_name":   name,
+					"description":  name,
+					"disk_device_mapping": []map[string]interface{}{
+						{
+							"format":          "RAW",
+							"oss_bucket":      "${alicloud_oss_bucket.default.id}",
+							"oss_object":      "${alicloud_oss_bucket_object.default.id}",
+							"device":          "/dev/xvda",
+							"disk_image_size": "6",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"architecture":          "i386",
+						"os_type":               "linux",
+						"platform":              "Aliyun",
+						"boot_mode":             "UEFI",
+						"image_name":            name,
+						"description":           name,
+						"disk_device_mapping.#": "1",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"license_type"},
+			},
+		},
+	})
 
-func resourceImageImageBasicConfigDependence(name string) string {
-	return ""
+}
+
+var AliCloudImageImportMap0 = map[string]string{
+	"platform":   CHECKSET,
+	"boot_mode":  CHECKSET,
+	"image_name": CHECKSET,
+}
+
+func AliCloudImageImportBasicDependence0(name string) string {
+	return fmt.Sprintf(`
+	variable "name" {
+  		default = "%s"
+	}
+
+	resource "alicloud_oss_bucket" "default" {
+  		bucket = var.name
+	}
+
+	resource "alicloud_oss_bucket_object" "default" {
+  		bucket  = alicloud_oss_bucket.default.id
+  		key     = "fc/hello.zip"
+  		content = <<EOF
+		# -*- coding: utf-8 -*-
+		def handler(event, context):
+		print "hello world"
+		return 'hello world'
+		EOF
+	}
+`, name)
 }
