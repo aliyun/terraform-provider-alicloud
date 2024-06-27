c7fa3293ff5a71c152bbaf6024d609b43c1dbacb
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-06-27 17:31:22 +0800

    New Resource: alicloud_cen_transit_router_ecr_attachment.

---
+++ alicloud/resource_alicloud_cen_transit_router_ecr_attachment.go
@@ -0,0 +1,288 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudCenTransitRouterEcrAttachment() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudCenTransitRouterEcrAttachmentCreate,
+		Read:   resourceAliCloudCenTransitRouterEcrAttachmentRead,
+		Update: resourceAliCloudCenTransitRouterEcrAttachmentUpdate,
+		Delete: resourceAliCloudCenTransitRouterEcrAttachmentDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"cen_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ecr_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"ecr_owner_id": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tags": tagsSchema(),
+			"transit_router_attachment_description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"transit_router_ecr_attachment_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"transit_router_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudCenTransitRouterEcrAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateTransitRouterEcrAttachment"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	conn, err := client.NewCenClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	request["RegionId"] = client.RegionId
+	request["ClientToken"] = buildClientToken(action)
+
+	request["EcrId"] = d.Get("ecr_id")
+	if v, ok := d.GetOk("ecr_owner_id"); ok {
+		request["EcrOwnerId"] = v
+	}
+	if v, ok := d.GetOk("cen_id"); ok {
+		request["CenId"] = v
+	}
+	if v, ok := d.GetOk("transit_router_id"); ok {
+		request["TransitRouterId"] = v
+	}
+	if v, ok := d.GetOk("transit_router_ecr_attachment_name"); ok {
+		request["TransitRouterAttachmentName"] = v
+	}
+	if v, ok := d.GetOk("transit_router_attachment_description"); ok {
+		request["TransitRouterAttachmentDescription"] = v
+	}
+	if v, ok := d.GetOk("tags"); ok {
+		tagsMap := ConvertTags(v.(map[string]interface{}))
+		request = expandTagsToMap(request, tagsMap)
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-12"), StringPointer("AK"), query, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_cen_transit_router_ecr_attachment", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprint(response["TransitRouterAttachmentId"]))
+
+	cenServiceV2 := CenServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{"Attached"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, cenServiceV2.CenTransitRouterEcrAttachmentStateRefreshFunc(d.Id(), "Status", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudCenTransitRouterEcrAttachmentUpdate(d, meta)
+}
+
+func resourceAliCloudCenTransitRouterEcrAttachmentRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	cenServiceV2 := CenServiceV2{client}
+
+	objectRaw, err := cenServiceV2.DescribeCenTransitRouterEcrAttachment(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_cen_transit_router_ecr_attachment DescribeCenTransitRouterEcrAttachment Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	if objectRaw["CenId"] != nil {
+		d.Set("cen_id", objectRaw["CenId"])
+	}
+	if objectRaw["CreationTime"] != nil {
+		d.Set("create_time", objectRaw["CreationTime"])
+	}
+	if objectRaw["EcrId"] != nil {
+		d.Set("ecr_id", objectRaw["EcrId"])
+	}
+	if objectRaw["EcrOwnerId"] != nil {
+		d.Set("ecr_owner_id", objectRaw["EcrOwnerId"])
+	}
+	if objectRaw["Status"] != nil {
+		d.Set("status", objectRaw["Status"])
+	}
+	if objectRaw["TransitRouterAttachmentDescription"] != nil {
+		d.Set("transit_router_attachment_description", objectRaw["TransitRouterAttachmentDescription"])
+	}
+	if objectRaw["TransitRouterAttachmentName"] != nil {
+		d.Set("transit_router_ecr_attachment_name", objectRaw["TransitRouterAttachmentName"])
+	}
+	if objectRaw["TransitRouterId"] != nil {
+		d.Set("transit_router_id", objectRaw["TransitRouterId"])
+	}
+
+	tagsMaps := objectRaw["Tags"]
+	d.Set("tags", tagsToMap(tagsMaps))
+
+	return nil
+}
+
+func resourceAliCloudCenTransitRouterEcrAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
+	action := "UpdateTransitRouterEcrAttachmentAttribute"
+	conn, err := client.NewCenClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["TransitRouterAttachmentId"] = d.Id()
+	request["ClientToken"] = buildClientToken(action)
+	if !d.IsNewResource() && d.HasChange("transit_router_ecr_attachment_name") {
+		update = true
+		request["TransitRouterAttachmentName"] = d.Get("transit_router_ecr_attachment_name")
+	}
+
+	if !d.IsNewResource() && d.HasChange("transit_router_attachment_description") {
+		update = true
+		request["TransitRouterAttachmentDescription"] = d.Get("transit_router_attachment_description")
+	}
+
+	if update {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-12"), StringPointer("AK"), query, request, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			addDebug(action, response, request)
+			return nil
+		})
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+		cenServiceV2 := CenServiceV2{client}
+		stateConf := BuildStateConf([]string{}, []string{"Attached"}, d.Timeout(schema.TimeoutUpdate), 5*time.Second, cenServiceV2.CenTransitRouterEcrAttachmentStateRefreshFunc(d.Id(), "Status", []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
+	}
+
+	if d.HasChange("tags") {
+		cenServiceV2 := CenServiceV2{client}
+		if err := cenServiceV2.SetResourceTags(d, "TRANSITROUTERECRATTACHMENT"); err != nil {
+			return WrapError(err)
+		}
+	}
+	return resourceAliCloudCenTransitRouterEcrAttachmentRead(d, meta)
+}
+
+func resourceAliCloudCenTransitRouterEcrAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	action := "DeleteTransitRouterEcrAttachment"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	conn, err := client.NewCenClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query["TransitRouterAttachmentId"] = d.Id()
+
+	request["ClientToken"] = buildClientToken(action)
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-09-12"), StringPointer("AK"), query, request, &runtime)
+		request["ClientToken"] = buildClientToken(action)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	cenServiceV2 := CenServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{""}, d.Timeout(schema.TimeoutDelete), 5*time.Second, cenServiceV2.CenTransitRouterEcrAttachmentStateRefreshFunc(d.Id(), "Status", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return nil
+}
