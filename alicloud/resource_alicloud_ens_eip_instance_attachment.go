f54ebb3a0acbe456a9e70257fb2aa838ce7f9eea
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-07-04 18:44:51 +0800

    New Resource: alicloud_ens_nat_gateway; New Resource: alicloud_ens_eip_instance_attachment.

---
+++ alicloud/resource_alicloud_ens_eip_instance_attachment.go
@@ -0,0 +1,190 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudEnsEipInstanceAttachment() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudEnsEipInstanceAttachmentCreate,
+		Read:   resourceAliCloudEnsEipInstanceAttachmentRead,
+		Delete: resourceAliCloudEnsEipInstanceAttachmentDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"allocation_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"instance_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ForceNew:     true,
+				ValidateFunc: StringInSlice([]string{"Nat", "SlbInstance", "NetworkInterface", "EnsInstance"}, false),
+			},
+			"standby": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudEnsEipInstanceAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "AssociateEnsEipAddress"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	conn, err := client.NewEnsClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query["AllocationId"] = d.Get("allocation_id")
+	query["InstanceId"] = d.Get("instance_id")
+	query["InstanceType"] = d.Get("instance_type")
+
+	if v, ok := d.GetOkExists("standby"); ok {
+		request["Standby"] = v
+	}
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-11-10"), StringPointer("AK"), query, request, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ens_eip_instance_attachment", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprintf("%v:%v:%v", query["AllocationId"], query["InstanceId"], query["InstanceType"]))
+
+	ensServiceV2 := EnsServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{"InUse"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, ensServiceV2.EnsEipInstanceAttachmentStateRefreshFunc(d.Id(), "Status", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudEnsEipInstanceAttachmentRead(d, meta)
+}
+
+func resourceAliCloudEnsEipInstanceAttachmentRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	ensServiceV2 := EnsServiceV2{client}
+
+	objectRaw, err := ensServiceV2.DescribeEnsEipInstanceAttachment(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_ens_eip_instance_attachment DescribeEnsEipInstanceAttachment Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	if objectRaw["Standby"] != nil {
+		d.Set("standby", objectRaw["Standby"])
+	}
+	if objectRaw["Status"] != nil {
+		d.Set("status", objectRaw["Status"])
+	}
+	if objectRaw["AllocationId"] != nil {
+		d.Set("allocation_id", objectRaw["AllocationId"])
+	}
+	if objectRaw["InstanceId"] != nil {
+		d.Set("instance_id", objectRaw["InstanceId"])
+	}
+	if objectRaw["InstanceType"] != nil {
+		d.Set("instance_type", objectRaw["InstanceType"])
+	}
+
+	parts := strings.Split(d.Id(), ":")
+	d.Set("allocation_id", parts[0])
+	d.Set("instance_id", parts[1])
+	d.Set("instance_type", parts[2])
+
+	return nil
+}
+
+func resourceAliCloudEnsEipInstanceAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "UnAssociateEnsEipAddress"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	conn, err := client.NewEnsClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query["AllocationId"] = parts[0]
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2017-11-10"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		addDebug(action, response, request)
+		return nil
+	})
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	ensServiceV2 := EnsServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, ensServiceV2.EnsEipInstanceAttachmentStateRefreshFunc(d.Id(), "InstanceId", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+	return nil
+}
---
+++ website/docs/r/ens_eip_instance_attachment.html.markdown
@@ -0,0 +1,106 @@
+---
+subcategory: "Ens"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_ens_eip_instance_attachment"
+description: |-
+  Provides a Alicloud Ens Eip Instance Attachment resource.
+---
+
+# alicloud_ens_eip_instance_attachment
+
+Provides a Ens Eip Instance Attachment resource.
+
+Bind an EIP to an instance.
+
+For information about Ens Eip Instance Attachment and how to use it, see [What is Eip Instance Attachment](https://www.alibabacloud.com/help/en/).
+
+-> **NOTE:** Available since v1.227.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-hangzhou"
+}
+
+variable "ens_region_id" {
+  default = "cn-chenzhou-telecom_unicom_cmcc"
+}
+
+resource "alicloud_ens_instance" "defaultXKjq1W" {
+  system_disk {
+    size     = "20"
+    category = "cloud_efficiency"
+  }
+  scheduling_strategy      = "Concentrate"
+  schedule_area_level      = "Region"
+  image_id                 = "centos_6_08_64_20G_alibase_20171208"
+  payment_type             = "Subscription"
+  instance_type            = "ens.sn1.stiny"
+  password                 = "12345678abcABC"
+  status                   = "Running"
+  amount                   = "1"
+  internet_charge_type     = "95BandwidthByMonth"
+  instance_name            = var.name
+  auto_use_coupon          = "true"
+  instance_charge_strategy = "PriceHighPriority"
+  ens_region_id            = var.ens_region_id
+  period_unit              = "Month"
+}
+
+resource "alicloud_ens_eip" "defaultsGsN4e" {
+  bandwidth            = "5"
+  eip_name             = var.name
+  ens_region_id        = var.ens_region_id
+  internet_charge_type = "95BandwidthByMonth"
+  payment_type         = "PayAsYouGo"
+}
+
+resource "alicloud_ens_eip_instance_attachment" "default" {
+  instance_id   = alicloud_ens_instance.defaultXKjq1W.id
+  allocation_id = alicloud_ens_eip.defaultsGsN4e.id
+  instance_type = "EnsInstance"
+  standby       = "false"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `allocation_id` - (Required, ForceNew) The first ID of the resource
+* `instance_id` - (Required, ForceNew) Instance ID
+* `instance_type` - (Optional, ForceNew, Computed) The type of the EIP instance. Value:
+  - `Nat`:NAT gateway.
+  - `SlbInstance`: Server Load Balancer (ELB).
+  - `NetworkInterface`: Secondary ENI.
+  - `EnsInstance` (default): The ENS instance.
+* `standby` - (Optional, ForceNew) Indicates whether the EIP is a backup EIP. Value:
+  - true: Spare.
+  - false: not standby.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<allocation_id>:<instance_id>:<instance_type>`.
+* `status` - The status of the EIP.
+
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Eip Instance Attachment.
+* `delete` - (Defaults to 5 mins) Used when delete the Eip Instance Attachment.
+
+## Import
+
+Ens Eip Instance Attachment can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_ens_eip_instance_attachment.example <allocation_id>:<instance_id>:<instance_type>
+```
