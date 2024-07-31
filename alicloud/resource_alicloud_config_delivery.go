55ebf85daebc4ac0844652285f178992219ac658
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-07-31 11:33:05 +0800

    resource/alicloud_config_delivery: Supports resource snapshots for SLS channel; resource/alicloud_config_aggregate_delivery: Supports resource snapshots for SLS channel.

--- alicloud/resource_alicloud_config_delivery.go
+++ alicloud/resource_alicloud_config_delivery.go
@@ -1,3 +1,4 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
@@ -9,49 +10,34 @@ import (
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudConfigDelivery() *schema.Resource {
+func resourceAliCloudConfigDelivery() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudConfigDeliveryCreate,
-		Read:   resourceAlicloudConfigDeliveryRead,
-		Update: resourceAlicloudConfigDeliveryUpdate,
-		Delete: resourceAlicloudConfigDeliveryDelete,
+		Create: resourceAliCloudConfigDeliveryCreate,
+		Read:   resourceAliCloudConfigDeliveryRead,
+		Update: resourceAliCloudConfigDeliveryUpdate,
+		Delete: resourceAliCloudConfigDeliveryDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(1 * time.Minute),
-			Update: schema.DefaultTimeout(1 * time.Minute),
-			Delete: schema.DefaultTimeout(1 * time.Minute),
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"configuration_item_change_notification": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Computed: true,
 			},
 			"configuration_snapshot": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Computed: true,
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					if v, ok := d.GetOk("delivery_channel_type"); ok && v.(string) == "OSS" {
-						return false
-					}
-					return true
-				},
 			},
 			"delivery_channel_condition": {
 				Type:     schema.TypeString,
 				Optional: true,
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					if v, ok := d.GetOk("delivery_channel_type"); ok && v.(string) == "MNS" {
-						return false
-					}
-					return true
-				},
 			},
 			"delivery_channel_name": {
 				Type:     schema.TypeString,
@@ -62,10 +48,9 @@ func resourceAlicloudConfigDelivery() *schema.Resource {
 				Required: true,
 			},
 			"delivery_channel_type": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"MNS", "OSS", "SLS"}, false),
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 			"description": {
 				Type:     schema.TypeString,
@@ -74,13 +59,6 @@ func resourceAlicloudConfigDelivery() *schema.Resource {
 			"non_compliant_notification": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Computed: true,
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					if v, ok := d.GetOk("delivery_channel_type"); ok && (v.(string) == "MNS" || v.(string) == "SLS") {
-						return false
-					}
-					return true
-				},
 			},
 			"oversized_data_oss_target_arn": {
 				Type:     schema.TypeString,
@@ -90,25 +68,32 @@ func resourceAlicloudConfigDelivery() *schema.Resource {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				Computed:     true,
-				ValidateFunc: validation.IntInSlice([]int{0, 1}),
+				ValidateFunc: IntInSlice([]int{1, 0}),
 			},
 		},
 	}
 }
 
-func resourceAlicloudConfigDeliveryCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudConfigDeliveryCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
+
 	action := "CreateConfigDeliveryChannel"
-	request := make(map[string]interface{})
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
 	conn, err := client.NewConfigClient()
 	if err != nil {
 		return WrapError(err)
 	}
-	if v, ok := d.GetOkExists("configuration_item_change_notification"); ok {
+	request = make(map[string]interface{})
+
+	request["ClientToken"] = buildClientToken(action)
+
+	if v, ok := d.GetOk("configuration_item_change_notification"); ok {
 		request["ConfigurationItemChangeNotification"] = v
 	}
-	if v, ok := d.GetOkExists("configuration_snapshot"); ok {
+	if v, ok := d.GetOk("configuration_snapshot"); ok {
 		request["ConfigurationSnapshot"] = v
 	}
 	if v, ok := d.GetOk("delivery_channel_condition"); ok {
@@ -122,18 +107,17 @@ func resourceAlicloudConfigDeliveryCreate(d *schema.ResourceData, meta interface
 	if v, ok := d.GetOk("description"); ok {
 		request["Description"] = v
 	}
-	if v, ok := d.GetOkExists("non_compliant_notification"); ok {
+	if v, ok := d.GetOk("non_compliant_notification"); ok {
 		request["NonCompliantNotification"] = v
 	}
 	if v, ok := d.GetOk("oversized_data_oss_target_arn"); ok {
 		request["OversizedDataOSSTargetArn"] = v
 	}
-	request["ClientToken"] = buildClientToken("CreateConfigDeliveryChannel")
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), nil, request, &runtime)
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), query, request, &runtime)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -141,112 +125,133 @@ func resourceAlicloudConfigDeliveryCreate(d *schema.ResourceData, meta interface
 			}
 			return resource.NonRetryableError(err)
 		}
+		addDebug(action, response, request)
 		return nil
 	})
-	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_config_delivery", action, AlibabaCloudSdkGoERROR)
 	}
 
 	d.SetId(fmt.Sprint(response["DeliveryChannelId"]))
 
-	return resourceAlicloudConfigDeliveryUpdate(d, meta)
+	return resourceAliCloudConfigDeliveryUpdate(d, meta)
 }
-func resourceAlicloudConfigDeliveryRead(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudConfigDeliveryRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	configService := ConfigService{client}
-	object, err := configService.DescribeConfigDelivery(d.Id())
+	configServiceV2 := ConfigServiceV2{client}
+
+	objectRaw, err := configServiceV2.DescribeConfigDelivery(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_config_delivery configService.DescribeConfigDelivery Failed!!! %s", err)
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_config_delivery DescribeConfigDelivery Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
-	d.Set("configuration_item_change_notification", object["ConfigurationItemChangeNotification"])
-	d.Set("configuration_snapshot", object["ConfigurationSnapshot"])
-	d.Set("delivery_channel_condition", object["DeliveryChannelCondition"])
-	d.Set("delivery_channel_name", object["DeliveryChannelName"])
-	d.Set("delivery_channel_target_arn", object["DeliveryChannelTargetArn"])
-	d.Set("delivery_channel_type", object["DeliveryChannelType"])
-	d.Set("description", object["Description"])
-	d.Set("non_compliant_notification", object["NonCompliantNotification"])
-	d.Set("oversized_data_oss_target_arn", object["OversizedDataOSSTargetArn"])
-	d.Set("status", formatInt(object["Status"]))
+
+	if objectRaw["ConfigurationItemChangeNotification"] != nil {
+		d.Set("configuration_item_change_notification", objectRaw["ConfigurationItemChangeNotification"])
+	}
+	if objectRaw["ConfigurationSnapshot"] != nil {
+		d.Set("configuration_snapshot", objectRaw["ConfigurationSnapshot"])
+	}
+	if objectRaw["DeliveryChannelCondition"] != nil {
+		d.Set("delivery_channel_condition", objectRaw["DeliveryChannelCondition"])
+	}
+	if objectRaw["DeliveryChannelName"] != nil {
+		d.Set("delivery_channel_name", objectRaw["DeliveryChannelName"])
+	}
+	if objectRaw["DeliveryChannelTargetArn"] != nil {
+		d.Set("delivery_channel_target_arn", objectRaw["DeliveryChannelTargetArn"])
+	}
+	if objectRaw["DeliveryChannelType"] != nil {
+		d.Set("delivery_channel_type", objectRaw["DeliveryChannelType"])
+	}
+	if objectRaw["Description"] != nil {
+		d.Set("description", objectRaw["Description"])
+	}
+	if objectRaw["NonCompliantNotification"] != nil {
+		d.Set("non_compliant_notification", objectRaw["NonCompliantNotification"])
+	}
+	if objectRaw["OversizedDataOSSTargetArn"] != nil {
+		d.Set("oversized_data_oss_target_arn", objectRaw["OversizedDataOSSTargetArn"])
+	}
+	if objectRaw["Status"] != nil {
+		d.Set("status", formatInt(objectRaw["Status"]))
+	}
+
 	return nil
 }
-func resourceAlicloudConfigDeliveryUpdate(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudConfigDeliveryUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
+	action := "UpdateConfigDeliveryChannel"
 	conn, err := client.NewConfigClient()
 	if err != nil {
 		return WrapError(err)
 	}
-	var response map[string]interface{}
-	update := false
-	request := map[string]interface{}{
-		"DeliveryChannelId": d.Id(),
-	}
-	if v, ok := d.GetOkExists("configuration_item_change_notification"); ok {
-		request["ConfigurationItemChangeNotification"] = v
-	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	query["DeliveryChannelId"] = d.Id()
+
+	request["ClientToken"] = buildClientToken(action)
 	if !d.IsNewResource() && d.HasChange("configuration_item_change_notification") {
 		update = true
+		request["ConfigurationItemChangeNotification"] = d.Get("configuration_item_change_notification")
 	}
-	if v, ok := d.GetOkExists("configuration_snapshot"); ok {
-		request["ConfigurationSnapshot"] = v
-	}
+
 	if !d.IsNewResource() && d.HasChange("configuration_snapshot") {
 		update = true
+		request["ConfigurationSnapshot"] = d.Get("configuration_snapshot")
 	}
-	if v, ok := d.GetOk("delivery_channel_condition"); ok {
-		request["DeliveryChannelCondition"] = v
-	}
+
 	if !d.IsNewResource() && d.HasChange("delivery_channel_condition") {
 		update = true
+		request["DeliveryChannelCondition"] = d.Get("delivery_channel_condition")
 	}
-	if v, ok := d.GetOk("delivery_channel_name"); ok {
-		request["DeliveryChannelName"] = v
-	}
+
 	if !d.IsNewResource() && d.HasChange("delivery_channel_name") {
 		update = true
+		request["DeliveryChannelName"] = d.Get("delivery_channel_name")
 	}
+
 	if !d.IsNewResource() && d.HasChange("delivery_channel_target_arn") {
 		update = true
 	}
 	request["DeliveryChannelTargetArn"] = d.Get("delivery_channel_target_arn")
-	if v, ok := d.GetOk("description"); ok {
-		request["Description"] = v
-	}
 	if !d.IsNewResource() && d.HasChange("description") {
 		update = true
+		request["Description"] = d.Get("description")
 	}
-	if v, ok := d.GetOkExists("non_compliant_notification"); ok {
-		request["NonCompliantNotification"] = v
-	}
+
 	if !d.IsNewResource() && d.HasChange("non_compliant_notification") {
 		update = true
+		request["NonCompliantNotification"] = d.Get("non_compliant_notification")
 	}
-	if v, ok := d.GetOk("oversized_data_oss_target_arn"); ok {
-		request["OversizedDataOSSTargetArn"] = v
-	}
+
 	if !d.IsNewResource() && d.HasChange("oversized_data_oss_target_arn") {
 		update = true
+		request["OversizedDataOSSTargetArn"] = d.Get("oversized_data_oss_target_arn")
 	}
-	if v, ok := d.GetOkExists("status"); ok {
-		request["Status"] = v
-	}
-	if d.HasChange("status") {
+
+	if d.HasChange("status") || d.IsNewResource() {
 		update = true
+		request["Status"] = d.Get("status")
 	}
+
 	if update {
-		action := "UpdateConfigDeliveryChannel"
-		request["ClientToken"] = buildClientToken("UpdateConfigDeliveryChannel")
 		runtime := util.RuntimeOptions{}
 		runtime.SetAutoretry(true)
-		wait := incrementalWait(3*time.Second, 3*time.Second)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
 		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), nil, request, &runtime)
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), query, request, &runtime)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -254,30 +259,37 @@ func resourceAlicloudConfigDeliveryUpdate(d *schema.ResourceData, meta interface
 				}
 				return resource.NonRetryableError(err)
 			}
+			addDebug(action, response, request)
 			return nil
 		})
-		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
 	}
-	return resourceAlicloudConfigDeliveryRead(d, meta)
+
+	return resourceAliCloudConfigDeliveryRead(d, meta)
 }
-func resourceAlicloudConfigDeliveryDelete(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudConfigDeliveryDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	action := "DeleteConfigDeliveryChannel"
+	var request map[string]interface{}
 	var response map[string]interface{}
+	query := make(map[string]interface{})
 	conn, err := client.NewConfigClient()
 	if err != nil {
 		return WrapError(err)
 	}
-	request := map[string]interface{}{
-		"DeliveryChannelId": d.Id(),
-	}
+	request = make(map[string]interface{})
+	query["DeliveryChannelId"] = d.Id()
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-09-07"), StringPointer("AK"), query, request, &runtime)
+
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -285,11 +297,13 @@ func resourceAlicloudConfigDeliveryDelete(d *schema.ResourceData, meta interface
 			}
 			return resource.NonRetryableError(err)
 		}
+		addDebug(action, response, request)
 		return nil
 	})
-	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	return nil
 }
