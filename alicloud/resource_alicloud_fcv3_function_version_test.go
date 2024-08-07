fbffcb4c5eceee7959f6629e595dcc7cdd634a2b
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-08-07 14:19:52 +0800

    New Resource: alicloud_fcv3_function_version.

---
+++ alicloud/resource_alicloud_fcv3_function_version_test.go
@@ -0,0 +1,127 @@
+package alicloud
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+// Test Fcv3 FunctionVersion. >>> Resource test cases, automatically generated.
+// Case TestFunctionVersion_Base 7234
+func TestAccAliCloudFcv3FunctionVersion_basic7234(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_fcv3_function_version.default"
+	ra := resourceAttrInit(resourceId, AlicloudFcv3FunctionVersionMap7234)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &Fcv3ServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeFcv3FunctionVersion")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%sfcv3functionversion%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudFcv3FunctionVersionBasicDependence7234)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"function_name": "${alicloud_fcv3_function.function.function_name}",
+					"description":   "version1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"function_name": CHECKSET,
+						"description":   "version1",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+var AlicloudFcv3FunctionVersionMap7234 = map[string]string{
+	"create_time": CHECKSET,
+}
+
+func AlicloudFcv3FunctionVersionBasicDependence7234(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+resource "alicloud_fcv3_function" "function" {
+  memory_size = "512"
+  cpu         = 0.5
+  handler     = "index.Handler"
+  code {
+    zip_file = "UEsDBBQACAAIAAAAAAAAAAAAAAAAAAAAAAAIAAAAaW5kZXgucHmEkEFKxEAQRfd9ig9ZTCJOooIwDMwNXLqXnnQlaalUhU5lRj2KZ/FOXkESGR114bJ/P/7jV4b1xRq1hijtFpM1682cuNgPmgysbRulPT0fRxXnMtwrSPyeCdYRokSLnuMLJTTkbUqEvDMbxm1VdcRD6Tk+T1LW2ldB66knsYdA5iNX17ebm6tN2VnPhcswMPmREPuBacb+CiapLarAj9gT6/H97dVlCNScY3mtYvRkxdZlwDKDEnanPWVLdrdkeXEGlFEazVdfPVHaVeHc3N15CUwppwOJXeK7HshAB8NuOU7J6sP4SRXuH/EvbUfMiqMmDqv5M5FNSfAj/wgAAP//UEsHCPl//NYAAQAArwEAAFBLAQIUABQACAAIAAAAAAD5f/zWAAEAAK8BAAAIAAAAAAAAAAAAAAAAAAAAAABpbmRleC5weVBLBQYAAAAAAQABADYAAAA2AQAAAAA="
+  }
+  function_name = var.name
+  runtime       = "python3.9"
+  disk_size     = "512"
+  log_config {
+    log_begin_rule = "None"
+  }
+}
+
+`, name)
+}
+
+// Case TestFunctionVersion_Base 7234  raw
+func TestAccAliCloudFcv3FunctionVersion_basic7234_raw(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_fcv3_function_version.default"
+	ra := resourceAttrInit(resourceId, AlicloudFcv3FunctionVersionMap7234)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &Fcv3ServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeFcv3FunctionVersion")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%sfcv3functionversion%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudFcv3FunctionVersionBasicDependence7234)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"function_name": "${alicloud_fcv3_function.function.function_name}",
+					"description":   "version1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"function_name": CHECKSET,
+						"description":   "version1",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+// Test Fcv3 FunctionVersion. <<< Resource test cases, automatically generated.
---
+++ website/docs/r/fcv3_function_version.html.markdown
@@ -0,0 +1,80 @@
+---
+subcategory: "Function Compute Service V3 (FCV3)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_fcv3_function_version"
+description: |-
+  Provides a Alicloud FCV3 Function Version resource.
+---
+
+# alicloud_fcv3_function_version
+
+Provides a FCV3 Function Version resource.
+
+Version of the function.
+
+For information about FCV3 Function Version and how to use it, see [What is Function Version](https://www.alibabacloud.com/help/en/functioncompute/api-fc-2023-03-30-listfunctionversions).
+
+-> **NOTE:** Available since v1.228.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-hangzhou"
+}
+
+resource "random_uuid" "default" {
+}
+
+resource "alicloud_fcv3_function" "function" {
+  memory_size = "512"
+  cpu         = 0.5
+  handler     = "index.Handler"
+  code {
+    zip_file = "UEsDBBQACAAIAAAAAAAAAAAAAAAAAAAAAAAIAAAAaW5kZXgucHmEkEFKxEAQRfd9ig9ZTCJOooIwDMwNXLqXnnQlaalUhU5lRj2KZ/FOXkESGR114bJ/P/7jV4b1xRq1hijtFpM1682cuNgPmgysbRulPT0fRxXnMtwrSPyeCdYRokSLnuMLJTTkbUqEvDMbxm1VdcRD6Tk+T1LW2ldB66knsYdA5iNX17ebm6tN2VnPhcswMPmREPuBacb+CiapLarAj9gT6/H97dVlCNScY3mtYvRkxdZlwDKDEnanPWVLdrdkeXEGlFEazVdfPVHaVeHc3N15CUwppwOJXeK7HshAB8NuOU7J6sP4SRXuH/EvbUfMiqMmDqv5M5FNSfAj/wgAAP//UEsHCPl//NYAAQAArwEAAFBLAQIUABQACAAIAAAAAAD5f/zWAAEAAK8BAAAIAAAAAAAAAAAAAAAAAAAAAABpbmRleC5weVBLBQYAAAAAAQABADYAAAA2AQAAAAA="
+  }
+  function_name = "${var.name}-${random_uuid.default.result}"
+  runtime       = "python3.9"
+  disk_size     = "512"
+  log_config {
+    log_begin_rule = "None"
+  }
+}
+
+resource "alicloud_fcv3_function_version" "default" {
+  function_name = alicloud_fcv3_function.function.function_name
+  description   = "version1"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `description` - (Optional, ForceNew) Description of the function version
+* `function_name` - (Required, ForceNew) Function Name
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<function_name>:<version_id>`.
+* `create_time` - The creation time of the resource
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Function Version.
+* `delete` - (Defaults to 5 mins) Used when delete the Function Version.
+
+## Import
+
+FCV3 Function Version can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_fcv3_function_version.example <function_name>:<version_id>
+```
