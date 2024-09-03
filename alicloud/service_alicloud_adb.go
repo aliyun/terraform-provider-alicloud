ca10b8816f197669e7c7a8d8d4e95eebe78930a6
MrWolong <ly01269865@alibaba-inc.com>
2024-09-03 11:53:32 +0800

    resource/alicloud_adb_db_cluster: Added the field enable_ssl

--- alicloud/service_alicloud_adb.go
+++ alicloud/service_alicloud_adb.go
@@ -1026,3 +1026,46 @@ func (s *AdbService) DescribeAdbResourceGroup(id string) (object map[string]inte
 
 	return object, nil
 }
+
+func (s *AdbService) DescribeAdbDbClusterSSL(id string) (object map[string]interface{}, err error) {
+	var response map[string]interface{}
+	action := "DescribeDBClusterSSL"
+
+	conn, err := s.client.NewAdsClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+
+	request := map[string]interface{}{
+		"DBClusterId": id,
+	}
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(10*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-03-15"), StringPointer("AK"), nil, request, &runtime)
+		if err != nil {
+			if IsExpectedErrors(err, []string{"ADBVersionNotSupport"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+
+	object = v.(map[string]interface{})
+
+	return object, nil
+}
