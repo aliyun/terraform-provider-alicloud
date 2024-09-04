0c68502b390ef6ed34637431a999eb3768f19490
MrWolong <ly01269865@alibaba-inc.com>
2024-09-04 15:42:58 +0800

    data-source/alicloud_ga_endpoint_group_ip_address_cidr_blocks: Added the field accelerator_id

--- alicloud/data_source_alicloud_ga_endpoint_group_ip_address_cidr_blocks_test.go
+++ alicloud/data_source_alicloud_ga_endpoint_group_ip_address_cidr_blocks_test.go
@@ -1,37 +1,77 @@
 package alicloud
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"strings"
 
 	"testing"
 )
 
 func TestAccAliCloudGaEndpointGroupIpAddressCidrBlocksDataSource(t *testing.T) {
-	resourceId := "data.alicloud_ga_endpoint_group_ip_address_cidr_blocks.default"
-	testAccCheck := resourceAttrInit(resourceId, map[string]string{}).resourceAttrMapUpdateSet()
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckAliCloudGaEndpointGroupIpAddressCidrBlocksDataSource,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"endpoint_group_ip_address_cidr_blocks.#":                          "1",
-						"endpoint_group_ip_address_cidr_blocks.0.endpoint_group_region":    CHECKSET,
-						"endpoint_group_ip_address_cidr_blocks.0.ip_address_cidr_blocks.#": CHECKSET,
-						"endpoint_group_ip_address_cidr_blocks.0.status":                   CHECKSET,
-					}),
-				),
-			},
-		},
-	})
+	rand := acctest.RandInt()
+
+	allConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceName(rand, map[string]string{
+			"accelerator_id": `"${data.alicloud_ga_accelerators.exist.accelerators.0.id}"`,
+		}),
+		fakeConfig: testAccCheckAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceName(rand, map[string]string{
+			"accelerator_id": `"${data.alicloud_ga_accelerators.fake.accelerators.0.id}"`,
+		}),
+	}
+
+	var existAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"endpoint_group_ip_address_cidr_blocks.#":                          "1",
+			"endpoint_group_ip_address_cidr_blocks.0.endpoint_group_region":    CHECKSET,
+			"endpoint_group_ip_address_cidr_blocks.0.ip_address_cidr_blocks.#": CHECKSET,
+			"endpoint_group_ip_address_cidr_blocks.0.status":                   CHECKSET,
+		}
+	}
+
+	var fakeAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceNameMapFunc = func(rand int) map[string]string {
+		return map[string]string{
+			"endpoint_group_ip_address_cidr_blocks.#":                          "1",
+			"endpoint_group_ip_address_cidr_blocks.0.endpoint_group_region":    CHECKSET,
+			"endpoint_group_ip_address_cidr_blocks.0.ip_address_cidr_blocks.#": "0",
+			"endpoint_group_ip_address_cidr_blocks.0.status":                   CHECKSET,
+		}
+	}
+
+	var aliCloudGaEndpointGroupIpAddressCidrBlocksCheckInfo = dataSourceAttr{
+		resourceId:   "data.alicloud_ga_endpoint_group_ip_address_cidr_blocks.default",
+		existMapFunc: existAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceNameMapFunc,
+		fakeMapFunc:  fakeAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceNameMapFunc,
+	}
+
+	preCheck := func() {
+		testAccPreCheck(t)
+	}
+
+	aliCloudGaEndpointGroupIpAddressCidrBlocksCheckInfo.dataSourceTestCheckWithPreCheck(t, rand, preCheck, allConf)
 }
 
-const testAccCheckAliCloudGaEndpointGroupIpAddressCidrBlocksDataSource = `
+func testAccCheckAliCloudGaEndpointGroupIpAddressCidrBlocksDataSourceName(rand int, attrMap map[string]string) string {
+	var pairs []string
+	for k, v := range attrMap {
+		pairs = append(pairs, k+" = "+v)
+	}
+
+	config := fmt.Sprintf(`
+	data "alicloud_ga_accelerators" "exist" {
+  		status                 = "active"
+  		bandwidth_billing_type = "BandwidthPackage"
+	}
+
+	data "alicloud_ga_accelerators" "fake" {
+  		status                 = "active"
+  		bandwidth_billing_type = "CDT"
+	}
+
 	data "alicloud_ga_endpoint_group_ip_address_cidr_blocks" "default" {
   		endpoint_group_region = "cn-hangzhou"
+		%s
 	}
-`
+`, strings.Join(pairs, " \n "))
+	return config
+}
