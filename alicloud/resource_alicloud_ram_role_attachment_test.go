bb336d97bb3728036ba32dd5e3b36c004a8b8b10
MrWolong <ly01269865@alibaba-inc.com>
2024-09-19 13:44:10 +0800

    testcase: Improved alicloud_ram_role_attachment testcase

--- alicloud/resource_alicloud_ram_role_attachment_test.go
+++ alicloud/resource_alicloud_ram_role_attachment_test.go
@@ -2,149 +2,232 @@ package alicloud
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestAccAlicloudRAMRoleAttachment_basic(t *testing.T) {
-	var instanceA ecs.Instance
-	var instanceB ecs.Instance
-	var v *ecs.DescribeInstanceRamRoleResponse
+func TestAccAliCloudRamRoleAttachment_basic0(t *testing.T) {
+	var v map[string]interface{}
 	resourceId := "alicloud_ram_role_attachment.default"
-	ra := resourceAttrInit(resourceId, ramRoleAttachmentMap)
-	serviceFuncRam := func() interface{} {
+	ra := resourceAttrInit(resourceId, AliCloudRamRoleAttachmentMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &RamService{testAccProvider.Meta().(*connectivity.AliyunClient)}
-	}
-	serviceFuncEcs := func() interface{} {
-		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
-	}
-	rc := resourceCheckInit(resourceId, &v, serviceFuncRam)
-	rcInstanceA := resourceCheckInit("alicloud_instance.default.0", &instanceA, serviceFuncEcs)
-	rcInstanceB := resourceCheckInit("alicloud_instance.default.1", &instanceB, serviceFuncEcs)
-
+	}, "DescribeRamRoleAttachment")
 	rac := resourceAttrCheckInit(rc, ra)
-
-	rand := acctest.RandIntRange(1000000, 9999999)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAcc%sRamRoleAttachment%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudRamRoleAttachmentBasicDependence0)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
-
-		// module name
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
-		CheckDestroy:  testAccCheckRamRoleAttachmentDestroy,
+		CheckDestroy:  rac.checkResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccRamRoleAttachmentConfig(EcsInstanceCommonTestCase, rand),
+				Config: testAccConfig(map[string]interface{}{
+					"role_name":    "${alicloud_ram_role.default.id}",
+					"instance_ids": []string{"${alicloud_instance.default.id}"},
+				}),
 				Check: resource.ComposeTestCheckFunc(
-					rcInstanceA.checkResourceExists(),
-					rcInstanceB.checkResourceExists(),
-					testAccCheck(nil),
+					testAccCheck(map[string]string{
+						"role_name":      CHECKSET,
+						"instance_ids.#": "1",
+					}),
 				),
 			},
 		},
 	})
 }
 
-var ramRoleAttachmentMap = map[string]string{
-	"role_name":      CHECKSET,
-	"instance_ids.#": "2",
+func TestAccAliCloudRamRoleAttachment_basic0_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_ram_role_attachment.default"
+	ra := resourceAttrInit(resourceId, AliCloudRamRoleAttachmentMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &RamService{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeRamRoleAttachment")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testAcc%sRamRoleAttachment%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudRamRoleAttachmentBasicDependence1)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"role_name":    "${alicloud_ram_role.default.id}",
+					"instance_ids": "${alicloud_instance.default.*.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"role_name":      CHECKSET,
+						"instance_ids.#": "5",
+					}),
+				),
+			},
+		},
+	})
 }
 
-func testAccRamRoleAttachmentConfig(common string, rand int) string {
+var AliCloudRamRoleAttachmentMap0 = map[string]string{}
+
+func AliCloudRamRoleAttachmentBasicDependence0(name string) string {
 	return fmt.Sprintf(`
-	%s
 	variable "name" {
-		default = "tf-testAcc%sRamRoleAttachmentConfig-%d"
+  		default = "%s"
 	}
 
-	resource "alicloud_instance" "default" {
-		vswitch_id = "${alicloud_vswitch.default.id}"
-		image_id = "${data.alicloud_images.default.images.0.id}"
+	data "alicloud_zones" "default" {
+  		available_disk_category     = "cloud_efficiency"
+  		available_resource_creation = "VSwitch"
+	}
 
-		# series III
-		instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
-		instance_name = "${var.name}"
-		system_disk_category = "cloud_efficiency"
-		count = 2
+	data "alicloud_images" "default" {
+  		most_recent = true
+  		owners      = "system"
+	}
 
-		internet_charge_type = "PayByTraffic"
-		internet_max_bandwidth_out = 5
-		security_groups = ["${alicloud_security_group.default.id}"]
+	data "alicloud_instance_types" "default" {
+  		availability_zone = data.alicloud_zones.default.zones.0.id
+  		image_id          = data.alicloud_images.default.images.0.id
 	}
 
 	resource "alicloud_ram_role" "default" {
-	  name = "${var.name}"
-	  document = <<EOF
+  		name     = var.name
+  		document = <<EOF
 		{
-		  "Statement": [
-			{
-			  "Action": "sts:AssumeRole",
-			  "Effect": "Allow",
-			  "Principal": {
-				"Service": [
-				  "ecs.aliyuncs.com"
-				]
-			  }
-			}
-		  ],
-		  "Version": "1"
-		}
-	  EOF
-	  description = "this is a test"
-	  force = true
-	}
-
-	resource "alicloud_ram_role_attachment" "default" {
-	  role_name = "${alicloud_ram_role.default.name}"
-	  instance_ids = "${alicloud_instance.default.*.id}"
-	}`, common, defaultRegionToTest, rand)
-}
-
-func testAccCheckRamRoleAttachmentDestroy(s *terraform.State) error {
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "alicloud_ram_role_attachment" {
-			continue
-		}
-
-		// Try to find the attachment
-		client := testAccProvider.Meta().(*connectivity.AliyunClient)
-
-		request := ecs.CreateDescribeInstanceRamRoleRequest()
-		request.InstanceIds = strings.Split(rs.Primary.ID, ":")[1]
-
-		for {
-			raw, err := client.WithEcsClient(func(ecsClient *ecs.Client) (interface{}, error) {
-				return ecsClient.DescribeInstanceRamRole(request)
-			})
-			if IsExpectedErrors(err, []string{"unexpected end of JSON input"}) {
-				continue
-			}
-			if IsExpectedErrors(err, []string{"InvalidInstanceId.NotFound"}) {
-				break
-			}
-			if err == nil {
-				response, _ := raw.(*ecs.DescribeInstanceRamRoleResponse)
-				if len(response.InstanceRamRoleSets.InstanceRamRoleSet) > 0 {
-					for _, v := range response.InstanceRamRoleSets.InstanceRamRoleSet {
-						if v.RamRoleName != "" {
-							return WrapError(fmt.Errorf("Attach %s still exists.", rs.Primary.ID))
-						}
+			"Statement": [
+				{
+					"Action": "sts:AssumeRole",
+					"Effect": "Allow",
+					"Principal": {
+					"Service": [
+					"ecs.aliyuncs.com"
+					]
 					}
 				}
-				break
-			}
-			return WrapError(err)
+		  	],
+			"Version": "1"
 		}
+	  	EOF
+		force    = true
 	}
-	return nil
+
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = var.name
+  		cidr_block = "192.168.0.0/16"
+	}
+
+	resource "alicloud_vswitch" "default" {
+  		vswitch_name = var.name
+  		vpc_id       = alicloud_vpc.default.id
+  		cidr_block   = "192.168.192.0/24"
+  		zone_id      = data.alicloud_zones.default.zones.0.id
+	}
+
+	resource "alicloud_security_group" "default" {
+  		name   = var.name
+  		vpc_id = alicloud_vpc.default.id
+	}
+
+	resource "alicloud_instance" "default" {
+  		image_id                   = data.alicloud_images.default.images.0.id
+  		instance_type              = data.alicloud_instance_types.default.instance_types.0.id
+  		security_groups            = alicloud_security_group.default.*.id
+  		internet_charge_type       = "PayByTraffic"
+  		internet_max_bandwidth_out = "10"
+  		availability_zone          = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
+  		instance_charge_type       = "PostPaid"
+  		system_disk_category       = "cloud_efficiency"
+  		vswitch_id                 = alicloud_vswitch.default.id
+  		instance_name              = var.name
+	}
+`, name)
+}
+
+func AliCloudRamRoleAttachmentBasicDependence1(name string) string {
+	return fmt.Sprintf(`
+	variable "name" {
+  		default = "%s"
+	}
+
+	data "alicloud_zones" "default" {
+  		available_disk_category     = "cloud_efficiency"
+  		available_resource_creation = "VSwitch"
+	}
+
+	data "alicloud_images" "default" {
+  		most_recent = true
+  		owners      = "system"
+	}
+
+	data "alicloud_instance_types" "default" {
+  		availability_zone = data.alicloud_zones.default.zones.0.id
+  		image_id          = data.alicloud_images.default.images.0.id
+	}
+
+	resource "alicloud_ram_role" "default" {
+  		name     = var.name
+  		document = <<EOF
+		{
+			"Statement": [
+				{
+					"Action": "sts:AssumeRole",
+					"Effect": "Allow",
+					"Principal": {
+					"Service": [
+					"ecs.aliyuncs.com"
+					]
+					}
+				}
+		  	],
+			"Version": "1"
+		}
+	  	EOF
+		force    = true
+	}
+
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = var.name
+  		cidr_block = "192.168.0.0/16"
+	}
+
+	resource "alicloud_vswitch" "default" {
+  		vswitch_name = var.name
+  		vpc_id       = alicloud_vpc.default.id
+  		cidr_block   = "192.168.192.0/24"
+  		zone_id      = data.alicloud_zones.default.zones.0.id
+	}
+
+	resource "alicloud_security_group" "default" {
+  		name   = var.name
+  		vpc_id = alicloud_vpc.default.id
+	}
+
+	resource "alicloud_instance" "default" {
+		count                      = 5
+  		image_id                   = data.alicloud_images.default.images.0.id
+  		instance_type              = data.alicloud_instance_types.default.instance_types.0.id
+  		security_groups            = alicloud_security_group.default.*.id
+  		internet_charge_type       = "PayByTraffic"
+  		internet_max_bandwidth_out = "10"
+  		availability_zone          = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
+  		instance_charge_type       = "PostPaid"
+  		system_disk_category       = "cloud_efficiency"
+  		vswitch_id                 = alicloud_vswitch.default.id
+  		instance_name              = var.name
+	}
+`, name)
 }
