125c8089666e061e955f4efe7db28aeef977d255
wb-zln530580 <wb-zln530580@alibaba-inc.com>
2024-09-23 15:20:26 +0800

    resource/alicloud_ess_alb_server_group_attachment: rename alicloud_ess_server_group_attachment and add attribute of type.

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -1058,7 +1058,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_ess_notification":                                     resourceAlicloudEssNotification(),
 			"alicloud_ess_alarm":                                            resourceAlicloudEssAlarm(),
 			"alicloud_ess_scalinggroup_vserver_groups":                      resourceAlicloudEssScalingGroupVserverGroups(),
-			"alicloud_ess_alb_server_group_attachment":                      resourceAlicloudEssAlbServerGroupAttachment(),
+			"alicloud_ess_server_group_attachment":                          resourceAlicloudEssServerGroupAttachment(),
 			"alicloud_vpc":                                                  resourceAliCloudVpcVpc(),
 			"alicloud_nat_gateway":                                          resourceAlicloudNatGateway(),
 			"alicloud_nas_file_system":                                      resourceAlicloudNasFileSystem(),
