907492afbcb0b918774a19f5713ed00a9ff3db19
MrWolong <ly01269865@alibaba-inc.com>
2024-09-24 11:26:51 +0800

    testcase: Improved alicloud_image_export testcase

--- alicloud/resource_alicloud_image_export_test.go
+++ alicloud/resource_alicloud_image_export_test.go
@@ -4,27 +4,23 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccAlicloudECSImageExport(t *testing.T) {
-	var v ecs.Image
+func TestAccAliCloudECSImageExport_basic0(t *testing.T) {
+	var v map[string]interface{}
 	resourceId := "alicloud_image_export.default"
-	ra := resourceAttrInit(resourceId, testAccExportImageCheckMap)
-	serviceFunc := func() interface{} {
+	ra := resourceAttrInit(resourceId, AliCloudECSImageExportMap0)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &EcsService{testAccProvider.Meta().(*connectivity.AliyunClient)}
-	}
-
-	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, serviceFunc, "DescribeImageById")
+	}, "DescribeImageById")
 	rac := resourceAttrCheckInit(rc, ra)
-
 	rand := acctest.RandIntRange(1000, 9999)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	name := fmt.Sprintf("tf-testaccecsimageexportconfigbasic%d", rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceImageExportBasicConfigDependence)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudECSImageExportBasicDependence0)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -41,7 +37,7 @@ func TestAccAlicloudECSImageExport(t *testing.T) {
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"oss_prefix": "ecsExport",
+						"image_id": CHECKSET,
 					}),
 				),
 			},
@@ -49,60 +45,71 @@ func TestAccAlicloudECSImageExport(t *testing.T) {
 	})
 }
 
-var testAccExportImageCheckMap = map[string]string{
-	"image_id":   CHECKSET,
-	"oss_bucket": CHECKSET,
-}
+var AliCloudECSImageExportMap0 = map[string]string{}
 
-func resourceImageExportBasicConfigDependence(name string) string {
+func AliCloudECSImageExportBasicDependence0(name string) string {
 	return fmt.Sprintf(`
-variable "name" {
-	default = "%s"
-}
+	variable "name" {
+  		default = "%s"
+	}
 
-data "alicloud_instance_types" "default" {
-  instance_type_family = "ecs.sn1ne"
-}
-data "alicloud_images" "default" {
- name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
- owners      = "system"
-}
+	data "alicloud_resource_manager_resource_groups" "default" {
+  		status = "OK"
+	}
 
-data "alicloud_vpcs" "default" {
-	name_regex = "^default-NODELETING$"
-}
-data "alicloud_vswitches" "default" {
-	vpc_id = data.alicloud_vpcs.default.ids.0
-	zone_id      = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
-}
-resource "alicloud_vswitch" "vswitch" {
-  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs[0].cidr_block, 8, 8)
-  zone_id           = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
-  vswitch_name      = var.name
-}
+	data "alicloud_zones" "default" {
+  		available_disk_category     = "cloud_efficiency"
+  		available_resource_creation = "VSwitch"
+	}
 
-locals {
-  vswitch_id = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids[0] : concat(alicloud_vswitch.vswitch.*.id, [""])[0]
-}
-resource "alicloud_security_group" "default" {
- name   = "${var.name}"
- vpc_id = data.alicloud_vpcs.default.ids.0
-}
-resource "alicloud_instance" "default" {
- image_id = "${data.alicloud_images.default.ids[0]}"
- instance_type = "${data.alicloud_instance_types.default.ids[0]}"
- security_groups = "${[alicloud_security_group.default.id]}"
- vswitch_id = local.vswitch_id
- instance_name = "${var.name}"
-}
-resource "alicloud_image" "default" {
- instance_id = "${alicloud_instance.default.id}"
- image_name        = "${var.name}"
-}
-resource "alicloud_oss_bucket" "default" {
-  bucket = "${var.name}"
-}
+	data "alicloud_images" "default" {
+  		name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
+  		most_recent = true
+  		owners      = "system"
+	}
+
+	data "alicloud_instance_types" "default" {
+  		availability_zone = data.alicloud_zones.default.zones.0.id
+  		image_id          = data.alicloud_images.default.images.0.id
+	}
+
+	resource "alicloud_vpc" "default" {
+  		vpc_name   = var.name
+  		cidr_block = "192.168.0.0/16"
+	}
+
+	resource "alicloud_vswitch" "default" {
+  		vswitch_name = var.name
+  		vpc_id       = alicloud_vpc.default.id
+  		cidr_block   = "192.168.192.0/24"
+  		zone_id      = data.alicloud_zones.default.zones.0.id
+	}
+
+	resource "alicloud_security_group" "default" {
+  		name   = var.name
+  		vpc_id = alicloud_vpc.default.id
+	}
+
+	resource "alicloud_instance" "default" {
+  		image_id                   = data.alicloud_images.default.images.0.id
+  		instance_type              = data.alicloud_instance_types.default.instance_types.0.id
+  		security_groups            = alicloud_security_group.default.*.id
+  		internet_charge_type       = "PayByTraffic"
+  		internet_max_bandwidth_out = "10"
+  		availability_zone          = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
+  		instance_charge_type       = "PostPaid"
+  		system_disk_category       = "cloud_efficiency"
+  		vswitch_id                 = alicloud_vswitch.default.id
+  		instance_name              = var.name
+	}
+
+	resource "alicloud_image" "default" {
+  		instance_id = alicloud_instance.default.id
+  		image_name  = var.name
+	}
+
+	resource "alicloud_oss_bucket" "default" {
+  		bucket = var.name
+	}
 `, name)
 }
