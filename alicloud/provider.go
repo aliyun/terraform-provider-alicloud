0ea4030c990c39860d0ed43dfc5615188b64c7ad
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-09-25 20:22:08 +0800

    New Data Source: alicloud_gpdb_databackups.

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -164,6 +164,7 @@ func Provider() terraform.ResourceProvider {
 			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
+"alicloud_gpdb_databackups":dataSourceAliCloudGpdbDatabackups(),
 			"alicloud_governance_baselines":   dataSourceAliCloudGovernanceBaselines(),
 			"alicloud_vpn_gateway_zones":      dataSourceAliCloudVPNGatewayZones(),
 			"alicloud_account":                dataSourceAlicloudAccount(),
