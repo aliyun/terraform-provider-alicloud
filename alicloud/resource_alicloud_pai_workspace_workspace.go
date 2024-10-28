14d0dc689bc4080ee523fad49db88791972b9d51
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-10-28 15:29:38 +0800

    New Resource: alicloud_pai_workspace_workspace.

---
+++ alicloud/resource_alicloud_pai_workspace_workspace.go
@@ -0,0 +1,259 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudPaiWorkspaceWorkspace() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudPaiWorkspaceWorkspaceCreate,
+		Read:   resourceAliCloudPaiWorkspaceWorkspaceRead,
+		Update: resourceAliCloudPaiWorkspaceWorkspaceUpdate,
+		Delete: resourceAliCloudPaiWorkspaceWorkspaceDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"env_types": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"workspace_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudPaiWorkspaceWorkspaceCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := fmt.Sprintf("/api/v1/workspaces")
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]*string)
+	body := make(map[string]interface{})
+	conn, err := client.NewPaiworkspaceClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+
+	request["WorkspaceName"] = d.Get("workspace_name")
+	request["Description"] = d.Get("description")
+	if v, ok := d.GetOk("display_name"); ok {
+		request["DisplayName"] = v
+	}
+	if v, ok := d.GetOk("env_types"); ok {
+		envTypesMaps := v.([]interface{})
+		request["EnvTypes"] = envTypesMaps
+	}
+
+	body = request
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer("2021-02-04"), nil, StringPointer("POST"), StringPointer("AK"), StringPointer(action), query, nil, body, &runtime)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_pai_workspace_workspace", action, AlibabaCloudSdkGoERROR)
+	}
+
+	id, _ := jsonpath.Get("$.body.WorkspaceId", response)
+	d.SetId(fmt.Sprint(id))
+
+	paiWorkspaceServiceV2 := PaiWorkspaceServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{"ENABLED"}, d.Timeout(schema.TimeoutCreate), 30*time.Second, paiWorkspaceServiceV2.PaiWorkspaceWorkspaceStateRefreshFunc(d.Id(), "Status", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return resourceAliCloudPaiWorkspaceWorkspaceRead(d, meta)
+}
+
+func resourceAliCloudPaiWorkspaceWorkspaceRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	paiWorkspaceServiceV2 := PaiWorkspaceServiceV2{client}
+
+	objectRaw, err := paiWorkspaceServiceV2.DescribePaiWorkspaceWorkspace(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_pai_workspace_workspace DescribePaiWorkspaceWorkspace Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	if objectRaw["GmtCreateTime"] != nil {
+		d.Set("create_time", objectRaw["GmtCreateTime"])
+	}
+	if objectRaw["Description"] != nil {
+		d.Set("description", objectRaw["Description"])
+	}
+	if objectRaw["DisplayName"] != nil {
+		d.Set("display_name", objectRaw["DisplayName"])
+	}
+	if objectRaw["Status"] != nil {
+		d.Set("status", objectRaw["Status"])
+	}
+	if objectRaw["WorkspaceName"] != nil {
+		d.Set("workspace_name", objectRaw["WorkspaceName"])
+	}
+
+	envTypes1Raw := make([]interface{}, 0)
+	if objectRaw["EnvTypes"] != nil {
+		envTypes1Raw = objectRaw["EnvTypes"].([]interface{})
+	}
+
+	d.Set("env_types", envTypes1Raw)
+
+	return nil
+}
+
+func resourceAliCloudPaiWorkspaceWorkspaceUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]*string
+	var body map[string]interface{}
+	update := false
+	WorkspaceId := d.Id()
+	action := fmt.Sprintf("/api/v1/workspaces/%s", WorkspaceId)
+	conn, err := client.NewPaiworkspaceClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]*string)
+	body = make(map[string]interface{})
+	request["WorkspaceId"] = d.Id()
+
+	if d.HasChange("display_name") {
+		update = true
+	}
+	if v, ok := d.GetOk("display_name"); ok || d.HasChange("display_name") {
+		request["DisplayName"] = v
+	}
+	if d.HasChange("description") {
+		update = true
+	}
+	request["Description"] = d.Get("description")
+	body = request
+	if update {
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer("2021-02-04"), nil, StringPointer("PUT"), StringPointer("AK"), StringPointer(action), query, nil, body, &runtime)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+	}
+
+	return resourceAliCloudPaiWorkspaceWorkspaceRead(d, meta)
+}
+
+func resourceAliCloudPaiWorkspaceWorkspaceDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	WorkspaceId := d.Id()
+	action := fmt.Sprintf("/api/v1/workspaces/%s", WorkspaceId)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]*string)
+	conn, err := client.NewPaiworkspaceClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	request["WorkspaceId"] = d.Id()
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer("2021-02-04"), nil, StringPointer("DELETE"), StringPointer("AK"), StringPointer(action), query, nil, nil, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	paiWorkspaceServiceV2 := PaiWorkspaceServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, paiWorkspaceServiceV2.PaiWorkspaceWorkspaceStateRefreshFunc(d.Id(), "Status", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return nil
+}
---
+++ alicloud/service_alicloud_pai_workspace_v2.go
@@ -0,0 +1,83 @@
+package alicloud
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+type PaiWorkspaceServiceV2 struct {
+	client *connectivity.AliyunClient
+}
+
+// DescribePaiWorkspaceWorkspace <<< Encapsulated get interface for PaiWorkspace Workspace.
+
+func (s *PaiWorkspaceServiceV2) DescribePaiWorkspaceWorkspace(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]*string
+	WorkspaceId := id
+	action := fmt.Sprintf("/api/v1/workspaces/%s", WorkspaceId)
+	conn, err := client.NewPaiworkspaceClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]*string)
+	request["WorkspaceId"] = id
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer("2021-02-04"), nil, StringPointer("GET"), StringPointer("AK"), StringPointer(action), query, nil, nil, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"100400008", "100700008", "100400027"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("Workspace", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	response = response["body"].(map[string]interface{})
+
+	return response, nil
+}
+
+func (s *PaiWorkspaceServiceV2) PaiWorkspaceWorkspaceStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribePaiWorkspaceWorkspace(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return nil, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribePaiWorkspaceWorkspace >>> Encapsulated.
