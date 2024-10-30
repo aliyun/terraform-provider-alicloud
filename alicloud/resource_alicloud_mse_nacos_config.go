54b5f275d6abdd62af6c52864dfbf2360fe11415
kkqqqqqq <kongkeqing.kkq@alibaba-inc.com>
2024-10-30 19:35:20 +0800

    New Resource: alicloud_mse_nacos_config;New Data Source: alicloud_mse_nacos_configs.

---
+++ alicloud/resource_alicloud_mse_nacos_config.go
@@ -0,0 +1,282 @@
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAlicloudMseNacosConfig() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAlicloudMseNacosConfigCreate,
+		Read:   resourceAlicloudMseNacosConfigRead,
+		Update: resourceAlicloudMseNacosConfigUpdate,
+		Delete: resourceAlicloudMseNacosConfigDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(1 * time.Minute),
+			Delete: schema.DefaultTimeout(1 * time.Minute),
+			Update: schema.DefaultTimeout(1 * time.Minute),
+		},
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"accept_language": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: StringInSlice([]string{"zh", "en"}, false),
+			},
+			"instance_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"namespace_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"data_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"group": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"app_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"desc": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"content": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"encrypted_data_key": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"beta_ips": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAlicloudMseNacosConfigCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var response map[string]interface{}
+	action := "CreateNacosConfig"
+	request := make(map[string]interface{})
+	conn, err := client.NewMseClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	if v, ok := d.GetOk("accept_language"); ok {
+		request["AcceptLanguage"] = v
+	}
+
+	request["InstanceId"] = d.Get("instance_id")
+	request["DataId"] = d.Get("data_id")
+	request["Group"] = d.Get("group")
+
+	if v, ok := d.GetOk("app_name"); ok {
+		request["AppName"] = v
+	}
+
+	if v, ok := d.GetOk("tags"); ok {
+		request["Tags"] = v
+	}
+	if v, ok := d.GetOk("desc"); ok {
+		request["Desc"] = v
+	}
+	if v, ok := d.GetOk("type"); ok {
+		request["Type"] = v
+	}
+	if v, ok := d.GetOk("content"); ok {
+		request["Content"] = v
+	}
+	if v, ok := d.GetOk("namespace_id"); ok {
+		request["NamespaceId"] = v
+	}
+	if v, ok := d.GetOk("beta_ips"); ok {
+		request["BetaIps"] = v
+	}
+
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-05-31"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_mse_nacos_config", action, AlibabaCloudSdkGoERROR)
+	}
+
+	if fmt.Sprint(response["Success"]) == "false" {
+		return WrapError(fmt.Errorf("%s failed, response: %v", action, response))
+	}
+	var namespaceId = request["NamespaceId"]
+	if namespaceId == nil {
+		namespaceId = ""
+	}
+
+	d.SetId(fmt.Sprint(request["InstanceId"], ":", namespaceId, ":", request["DataId"], ":", request["Group"]))
+
+	return resourceAlicloudMseNacosConfigRead(d, meta)
+}
+
+func resourceAlicloudMseNacosConfigRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	mseService := MseService{client}
+
+	object, err := mseService.DescribeMseNacosConfig(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_mse_nacos_config mseService.DescribeMseNacosConfig Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	parts, err := ParseResourceId(d.Id(), 4)
+
+	err = d.Set("instance_id", parts[0])
+	if err != nil {
+		return err
+	}
+	d.Set("namespace_id", parts[1])
+	d.Set("app_name", object["AppName"])
+	d.Set("data_id", object["DataId"])
+	d.Set("group", object["Group"])
+	d.Set("type", object["Type"])
+	d.Set("tags", object["Tags"])
+	d.Set("content", object["Content"])
+	d.Set("desc", object["Desc"])
+	d.Set("id", object["Id"])
+
+	return nil
+}
+func resourceAlicloudMseNacosConfigUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	conn, err := client.NewMseClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	var response map[string]interface{}
+	update := false
+	parts, err := ParseResourceId(d.Id(), 4)
+	if err != nil {
+		return WrapError(err)
+	}
+	request := map[string]interface{}{
+		"InstanceId":  parts[0],
+		"NamespaceId": parts[1],
+		"DataId":      parts[2],
+		"Group":       parts[3],
+	}
+
+	if d.HasChanges("content", "app_name", "tags", "desc", "type", "beta_ips") {
+		update = true
+		request["Content"] = d.Get("content")
+		request["AppName"] = d.Get("app_name")
+		request["Tags"] = d.Get("tags")
+		request["Desc"] = d.Get("desc")
+		request["Type"] = d.Get("type")
+		request["BetaIps"] = d.Get("beta_ips")
+
+	}
+
+	if update {
+		if v, ok := d.GetOk("accept_language"); ok {
+			request["AcceptLanguage"] = v
+		}
+		action := "UpdateNacosConfig"
+		wait := incrementalWait(3*time.Second, 3*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-05-31"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+	}
+	return resourceAlicloudMseNacosConfigRead(d, meta)
+}
+func resourceAlicloudMseNacosConfigDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	action := "DeleteNacosConfig"
+	var response map[string]interface{}
+	conn, err := client.NewMseClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	parts, err := ParseResourceId(d.Id(), 4)
+	if err != nil {
+		return WrapError(err)
+	}
+	request := map[string]interface{}{
+		"InstanceId":  parts[0],
+		"NamespaceId": parts[1],
+		"DataId":      parts[2],
+		"Group":       parts[3],
+	}
+	if v, ok := d.GetOk("accept_language"); ok {
+		request["AcceptLanguage"] = v
+	}
+	wait := incrementalWait(3*time.Second, 3*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2019-05-31"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+	return nil
+}
---
+++ website/docs/r/mse_nacos_config.html.markdown
@@ -0,0 +1,108 @@
+---
+subcategory: "Microservice Engine (MSE)"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_mse_nacos_config"
+sidebar_current: "docs-alicloud-resource-mse-nacos-config"
+description: |-
+  Provides a Alicloud Microservice Engine (MSE) Nacos Config resource.
+---
+
+# alicloud\_mse\_nacos\_config
+
+Provides a Microservice Engine (MSE) Nacos Config resource.
+
+For information about Microservice Engine (MSE) Nacos Config and how to use it, see [What is Nacos configuration](https://www.alibabacloud.com/help/en/mse/developer-reference/api-mse-2019-05-31-createnacosconfig)
+-> **NOTE:** Available since v1.233.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+data "alicloud_zones" "example" {
+  available_resource_creation = "VSwitch"
+}
+
+resource "alicloud_vpc" "example" {
+  vpc_name   = "terraform-example"
+  cidr_block = "172.17.3.0/24"
+}
+
+resource "alicloud_vswitch" "example" {
+  vswitch_name = "terraform-example"
+  cidr_block   = "172.17.3.0/24"
+  vpc_id       = alicloud_vpc.example.id
+  zone_id      = data.alicloud_zones.example.zones.0.id
+}
+
+resource "alicloud_mse_cluster" "example" {
+  connection_type       = "slb"
+  net_type              = "privatenet"
+  vswitch_id            = alicloud_vswitch.example.id
+  cluster_specification = "MSE_SC_1_2_60_c"
+  cluster_version       = "NACOS_2_0_0"
+  instance_count        = "3"
+  pub_network_flow      = "1"
+  cluster_alias_name    = "example"
+  mse_version           = "mse_pro"
+  cluster_type          = "Nacos-Ans"
+}
+
+resource "alicloud_mse_engine_namespace" "example" {
+  instance_id         = alicloud_mse_cluster.example.id
+  namespace_show_name = "example"
+  namespace_id        = "example"
+}
+
+resource "alicloud_mse_nacos_config" "example" {
+  instance_id  = alicloud_mse_cluster.example.id
+  data_id      = "example"
+  group        = "example"
+  namespace_id = alicloud_mse_engine_namespace.example.namespace_id
+  content      = "example"
+  type         = "text"
+  tags         = "example"
+  app_name     = "example"
+  desc         = "example"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `accept_language` - (Optional) The language type of the returned information. Valid values: `zh`, `en`.
+* `instance_id` - (Required, ForceNew) The ID of the instance.
+* `data_id` - (Required, ForceNew) The ID of the data.
+* `namespace_id` - (Optional, ForceNew) The id of Namespace. If you want to create a config under the `public` namespace, this parameter can be set to an empty string  *`""`* or just not set this parameter.
+* `group` - (Required, ForceNew) The ID of the group.
+* `app_name` - (Optional) The name of the application.
+* `tags` - (Optional) The tags of the configuration.
+* `type` - (Optional) The format of the configuration. Supported formats include TEXT, JSON, and XML.
+* `content` - (Required) The content of the configuration.
+* `desc` - (Optional) The description of the configuration.
+* `beta_ips`-(Optional, Computed) The list of IP addresses where the beta release of the configuration is performed.
+
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The resource ID in terraform of Nacos Config. It is formatted to `<instance_id>:<namespace_id>:<data_id>:<group>`.
+* `encrypted_data_key` - The encryption key.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+
+* `create` - (Defaults to 1 mins) Used when creating the Nacos Config.
+* `update` - (Defaults to 1 mins) Used when updating the Nacos Config.
+* `delete` - (Defaults to 1 mins) Used when deleting adb Nacos Config.
+
+## Import
+
+Microservice Engine (MSE) Nacos Config can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_mse_nacos_config.example <instance_id>:<namespace_id>:<data_id>:<group>
+```
