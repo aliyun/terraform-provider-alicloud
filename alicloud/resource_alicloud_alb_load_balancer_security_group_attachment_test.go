293130f0991155711703c77a148dcc398003e5b6
MrWolong <ly01269865@alibaba-inc.com>
2024-10-31 13:14:41 +0800

    testcase: Improved alicloud_alb_load_balancer_security_group_attachment testcase

--- alicloud/resource_alicloud_alb_load_balancer_security_group_attachment_test.go
+++ alicloud/resource_alicloud_alb_load_balancer_security_group_attachment_test.go
@@ -44,73 +44,67 @@ func TestAccAliCloudAlbLoadBalancerSecurityGroupAttachment_basic7120(t *testing.
 				),
 			},
 			{
-				ResourceName:            resourceId,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{},
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
 }
 
-var AlicloudAlbLoadBalancerSecurityGroupAttachmentMap7120 = map[string]string{
-	"security_group_id": CHECKSET,
-}
+var AlicloudAlbLoadBalancerSecurityGroupAttachmentMap7120 = map[string]string{}
 
 func AlicloudAlbLoadBalancerSecurityGroupAttachmentBasicDependence7120(name string) string {
 	return fmt.Sprintf(`
-variable "name" {
-    default = "%s"
-}
+	variable "name" {
+  		default = "%s"
+	}
 
-data "alicloud_zones" "default" {
-  available_resource_creation = "VSwitch"
-}
+	data "alicloud_alb_zones" "default" {
+	}
 
-resource "alicloud_vpc" "create_vpc" {
-  cidr_block = "192.168.0.0/16"
-  vpc_name   = var.name
-}
+	resource "alicloud_vpc" "create_vpc" {
+  		cidr_block = "192.168.0.0/16"
+  		vpc_name   = var.name
+	}
 
-resource "alicloud_vswitch" "create_vsw_1" {
-  vpc_id       = alicloud_vpc.create_vpc.id
-  zone_id      = data.alicloud_zones.default.zones.0.id
-  cidr_block   = "192.168.1.0/24"
-  vswitch_name = var.name
-}
+	resource "alicloud_vswitch" "create_vsw_1" {
+  		vpc_id       = alicloud_vpc.create_vpc.id
+  		zone_id      = data.alicloud_alb_zones.default.zones.0.id
+  		cidr_block   = "192.168.1.0/24"
+  		vswitch_name = var.name
+	}
 
-resource "alicloud_vswitch" "create_vsw_2" {
-  vpc_id       = alicloud_vpc.create_vpc.id
-  zone_id      = data.alicloud_zones.default.zones.1.id
-  cidr_block   = "192.168.2.0/24"
-  vswitch_name = var.name
-}
+	resource "alicloud_vswitch" "create_vsw_2" {
+  		vpc_id       = alicloud_vpc.create_vpc.id
+  		zone_id      = data.alicloud_alb_zones.default.zones.1.id
+  		cidr_block   = "192.168.2.0/24"
+  		vswitch_name = var.name
+	}
 
-resource "alicloud_security_group" "create_security_group" {
-  name   = var.name
-  vpc_id = alicloud_vpc.create_vpc.id
-}
+	resource "alicloud_security_group" "create_security_group" {
+  		name   = var.name
+  		vpc_id = alicloud_vpc.create_vpc.id
+	}
 
-resource "alicloud_alb_load_balancer" "create_alb" {
-  load_balancer_name    = var.name
-  load_balancer_edition = "Standard"
-  vpc_id                = alicloud_vpc.create_vpc.id
-  load_balancer_billing_config {
-    pay_type = "PayAsYouGo"
-  }
-  address_type           = "Intranet"
-  address_allocated_mode = "Fixed"
-  zone_mappings {
-    vswitch_id = alicloud_vswitch.create_vsw_2.id
-    zone_id    = alicloud_vswitch.create_vsw_2.zone_id
-  }
-  zone_mappings {
-    vswitch_id = alicloud_vswitch.create_vsw_1.id
-    zone_id    = alicloud_vswitch.create_vsw_1.zone_id
-  }
-}
-
-
+	resource "alicloud_alb_load_balancer" "create_alb" {
+  		load_balancer_name    = var.name
+  		load_balancer_edition = "Standard"
+  		vpc_id                = alicloud_vpc.create_vpc.id
+  		load_balancer_billing_config {
+    		pay_type = "PayAsYouGo"
+  		}
+  		address_type           = "Intranet"
+  		address_allocated_mode = "Fixed"
+  		zone_mappings {
+    		vswitch_id = alicloud_vswitch.create_vsw_2.id
+    		zone_id    = alicloud_vswitch.create_vsw_2.zone_id
+  		}
+  		zone_mappings {
+    		vswitch_id = alicloud_vswitch.create_vsw_1.id
+    		zone_id    = alicloud_vswitch.create_vsw_1.zone_id
+  		}
+	}
 `, name)
 }
 
