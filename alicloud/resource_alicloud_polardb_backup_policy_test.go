d3c213c719257ff03057657f26f9eb4a26555ccf
WB659441 <wb-lm659441@alibaba-inc.com>
2024-11-06 10:48:06 +0800

    testcase: fix testcase for resource/alicloud_polardb_backup_policy, resource/alicloud_polardb_account_priviledge.

--- alicloud/resource_alicloud_polardb_backup_policy_test.go
+++ alicloud/resource_alicloud_polardb_backup_policy_test.go
@@ -252,25 +252,33 @@ func TestAccAliCloudPolarDBNewBackupPolicy(t *testing.T) {
 
 func resourcePolarDBBackupPolicyConfigDependence(name string) string {
 	return fmt.Sprintf(`
-	%s
 	variable "name" {
 		default = "%s"
 	}
-	data "alicloud_polardb_node_classes" "this" {
-	  db_type    = "MySQL"
-	  db_version = "8.0"
-      pay_type   = "PostPaid"
-	  zone_id    = local.zone_id
-      category   = "Normal"
+	data "alicloud_vpcs" "default" {
+		name_regex = "^default-NODELETING$"
 	}
-
-	resource "alicloud_polardb_cluster" "default" {
-		db_type = "MySQL"
+	resource "alicloud_vpc" "default" {
+		vpc_name = var.name
+	}
+	resource "alicloud_vswitch" "default" {
+		zone_id = data.alicloud_polardb_node_classes.default.classes.0.zone_id
+		vpc_id = alicloud_vpc.default.id
+		cidr_block = cidrsubnet(alicloud_vpc.default.cidr_block, 8, 4)
+	}
+	data "alicloud_polardb_node_classes" "default" {
+		pay_type   = "PostPaid"
+		db_type    = "MySQL"
 		db_version = "8.0"
-		pay_type = "PostPaid"
-        db_node_class = data.alicloud_polardb_node_classes.this.classes.0.supported_engines.0.available_resources.0.db_node_class
-		vswitch_id = local.vswitch_id
-		description = "${var.name}"
+		category   = "Normal"
 	}
-`, PolarDBCommonTestCase, name)
+	resource "alicloud_polardb_cluster" "default" {
+		db_type       = "MySQL"
+		db_version    = "8.0"
+		pay_type      = "PostPaid"
+		db_node_class = data.alicloud_polardb_node_classes.default.classes.0.supported_engines.0.available_resources.2.db_node_class
+		vswitch_id    = alicloud_vswitch.default.id
+		description   = "${var.name}"
+	}
+	`, name)
 }
