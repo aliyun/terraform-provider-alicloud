5ea0bedd5893eccdb3169ff5b70fb41057fb0d83
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-11-28 17:20:02 +0800

    New Resource: alicloud_pai_workspace_experiment.

--- alicloud/service_alicloud_pai_workspace_v2.go
+++ alicloud/service_alicloud_pai_workspace_v2.go
@@ -156,3 +156,77 @@ func (s *PaiWorkspaceServiceV2) PaiWorkspaceDatasetStateRefreshFunc(id string, f
 }
 
 // DescribePaiWorkspaceDataset >>> Encapsulated.
+// DescribePaiWorkspaceExperiment <<< Encapsulated get interface for PaiWorkspace Experiment.
+
+func (s *PaiWorkspaceServiceV2) DescribePaiWorkspaceExperiment(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]*string
+	ExperimentId := id
+	action := fmt.Sprintf("/api/v1/experiments/%s", ExperimentId)
+	conn, err := client.NewPaiworkspaceClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]*string)
+	request["ExperimentId"] = id
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer("2021-02-04"), nil, StringPointer("GET"), StringPointer("AK"), StringPointer(action), query, nil, nil, &runtime)
+
+		if err != nil {
+			if IsExpectedErrors(err, []string{"NotFoundErrorProblem"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InternalServerErrorProblem"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("Experiment", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+	response = response["body"].(map[string]interface{})
+
+	return response, nil
+}
+
+func (s *PaiWorkspaceServiceV2) PaiWorkspaceExperimentStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribePaiWorkspaceExperiment(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribePaiWorkspaceExperiment >>> Encapsulated.
