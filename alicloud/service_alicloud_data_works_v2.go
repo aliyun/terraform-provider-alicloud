77a611798c1cbd307372b1efcdc406d9a86f0cb3
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2024-12-06 10:20:49 +0800

    resource/alicloud_data_works_project: add new attributes dev_environment_enabled, dev_role_disabled, pai_task_enabled and etc.

--- alicloud/service_alicloud_data_works_v2.go
+++ alicloud/service_alicloud_data_works_v2.go
@@ -2,12 +2,15 @@ package alicloud
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
+	rpc "github.com/alibabacloud-go/tea-rpc/client"
 	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 type DataWorksServiceV2 struct {
@@ -28,35 +31,33 @@ func (s *DataWorksServiceV2) DescribeDataWorksProject(id string) (object map[str
 	}
 	request = make(map[string]interface{})
 	query = make(map[string]interface{})
-	query["ProjectId"] = id
+	query["Id"] = id
+	query["RegionId"] = client.RegionId
 
 	runtime := util.RuntimeOptions{}
 	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 5*time.Second)
-	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-05-18"), StringPointer("AK"), query, request, &runtime)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("GET"), StringPointer("2024-05-18"), StringPointer("AK"), query, nil, &runtime)
 
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"9990020002", "9990040003"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
-	code, _ := jsonpath.Get("$.HttpStatusCode", response)
-	if InArray(fmt.Sprint(code), []string{"Invalid.Tenant.ProjectNotExists"}) {
-		return object, WrapErrorf(Error(GetNotFoundMessage("Project", id)), NotFoundMsg, response)
-	}
-
-	v, err := jsonpath.Get("$.Data", response)
+	addDebug(action, response, request)
 	if err != nil {
-		return object, WrapErrorf(Error(GetNotFoundMessage("Project", id)), NotFoundMsg, response)
+		if IsExpectedErrors(err, []string{"1101080008"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("Project", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
 	}
 
-	return v.(map[string]interface{}), nil
+	return response, nil
 }
 
 func (s *DataWorksServiceV2) DataWorksProjectStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
@@ -72,6 +73,13 @@ func (s *DataWorksServiceV2) DataWorksProjectStateRefreshFunc(id string, field s
 		v, err := jsonpath.Get(field, object)
 		currentStatus := fmt.Sprint(v)
 
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
 		for _, failState := range failStates {
 			if currentStatus == failState {
 				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
@@ -82,3 +90,102 @@ func (s *DataWorksServiceV2) DataWorksProjectStateRefreshFunc(id string, field s
 }
 
 // DescribeDataWorksProject >>> Encapsulated.
+
+// SetResourceTags <<< Encapsulated tag function for DataWorks.
+func (s *DataWorksServiceV2) SetResourceTags(d *schema.ResourceData, resourceType string) error {
+	if d.HasChange("tags") {
+		var err error
+		var action string
+		var conn *rpc.Client
+		client := s.client
+		var request map[string]interface{}
+		var response map[string]interface{}
+		query := make(map[string]interface{})
+
+		added, removed := parsingTags(d)
+		removedTagKeys := make([]string, 0)
+		for _, v := range removed {
+			if !ignoredTags(v, "") {
+				removedTagKeys = append(removedTagKeys, v)
+			}
+		}
+		if len(removedTagKeys) > 0 {
+			action = "UntagResources"
+			conn, err = client.NewDataworkspublicClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["ResourceId.1"] = d.Id()
+			request["RegionId"] = client.RegionId
+			request["ResourceType"] = resourceType
+			for i, key := range removedTagKeys {
+				request[fmt.Sprintf("TagKey.%d", i+1)] = key
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-05-18"), StringPointer("AK"), query, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+		if len(added) > 0 {
+			action = "TagResources"
+			conn, err = client.NewDataworkspublicClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["ResourceId.1"] = d.Id()
+			request["RegionId"] = client.RegionId
+			request["ResourceType"] = resourceType
+			count := 1
+			for key, value := range added {
+				request[fmt.Sprintf("Tag.%d.Key", count)] = key
+				request[fmt.Sprintf("Tag.%d.Value", count)] = value
+				count++
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-05-18"), StringPointer("AK"), query, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+	}
+
+	return nil
+}
+
+// SetResourceTags >>> tag function encapsulated.
--- website/docs/r/data_works_project.html.markdown
+++ website/docs/r/data_works_project.html.markdown
@@ -12,7 +12,7 @@ Provides a Data Works Project resource.
 
 
 
-For information about Data Works Project and how to use it, see [What is Project](https://www.alibabacloud.com/help/en/dataworks/developer-reference/api-dataworks-public-2020-05-18-createproject).
+For information about Data Works Project and how to use it, see [What is Project](https://www.alibabacloud.com/help/en/).
 
 -> **NOTE:** Available since v1.229.0.
 
@@ -20,56 +20,52 @@ For information about Data Works Project and how to use it, see [What is Project
 
 Basic Usage
 
-<div style="display: block;margin-bottom: 40px;"><div class="oics-button" style="float: right;position: absolute;margin-bottom: 10px;">
-  <a href="https://api.aliyun.com/terraform?resource=alicloud_data_works_project&exampleId=1477296e-1cb3-70c6-612d-94ff543c341c267b7202&activeTab=example&spm=docs.r.data_works_project.0.1477296e1c&intl_lang=EN_US" target="_blank">
-    <img alt="Open in AliCloud" src="https://img.alicdn.com/imgextra/i1/O1CN01hjjqXv1uYUlY56FyX_!!6000000006049-55-tps-254-36.svg" style="max-height: 44px; max-width: 100%;">
-  </a>
-</div></div>
-
 ```terraform
 variable "name" {
   default = "tf_example"
 }
 
-resource "random_integer" "default" {
-  min = 10000
-  max = 99999
+resource "random_integer" "randint" {
+  max = 999
+  min = 1
 }
 
-provider "alicloud" {
-  region = "cn-hangzhou"
-}
+data "alicloud_resource_manager_resource_groups" "default" {}
 
 resource "alicloud_data_works_project" "default" {
-  project_name = "${var.name}_${random_integer.default.result}"
-  project_mode = "2"
-  description  = "${var.name}_${random_integer.default.result}"
-  display_name = "${var.name}_${random_integer.default.result}"
-  status       = "0"
+  status                  = "Available"
+  description             = "tf_desc"
+  project_name            = "${var.name}${random_integer.randint.id}"
+  pai_task_enabled        = "false"
+  display_name            = "tf_new_api_display"
+  dev_role_disabled       = "true"
+  dev_environment_enabled = "false"
+  resource_group_id       = data.alicloud_resource_manager_resource_groups.default.ids.0
 }
 ```
 
 ## Argument Reference
 
 The following arguments are supported:
-* `description` - (Required) Description of the workspace
-* `display_name` - (Required) The display name of the workspace.
-* `project_mode` - (Optional, ForceNew) The mode of the workspace, with the following values:
-  - 2, indicates the simple workspace mode.
-  - 3, indicating the standard workspace mode.
-* `project_name` - (Required, ForceNew) Immutable Name of the workspace.
-* `status` - (Optional, Computed) The status of the resource
+* `description` - (Optional) Workspace Description
+* `dev_environment_enabled` - (Optional, Computed, Available since v1.237.0) Is Development Environment Enabled
+* `dev_role_disabled` - (Optional, Computed, Available since v1.237.0) Is Development Role Disabled
+* `display_name` - (Required) Workspace Display Name
+* `pai_task_enabled` - (Required, Available since v1.237.0) Create PAI Workspace Together
+* `project_name` - (Required, ForceNew) Workspace Name
+* `resource_group_id` - (Optional, Computed, Available since v1.237.0) Aliyun Resource Group Id
+* `status` - (Optional, Computed) Workspace Status
+* `tags` - (Optional, Map, Available since v1.237.0) Aliyun Resource Tag
 
 ## Attributes Reference
 
 The following attributes are exported:
 * `id` - The ID of the resource supplied above.
-* `create_time` - The creation time of the resource
 
 ## Timeouts
 
 The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
-* `create` - (Defaults to 5 mins) Used when create the Project.
+* `create` - (Defaults to 15 mins) Used when create the Project.
 * `delete` - (Defaults to 5 mins) Used when delete the Project.
 * `update` - (Defaults to 5 mins) Used when update the Project.
 
