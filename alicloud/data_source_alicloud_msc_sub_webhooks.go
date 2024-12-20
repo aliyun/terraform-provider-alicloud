c0c38de1b1cc39b4a8e4f32ce39322e190aa2ea8
guimin.hgm <guimin.hgm@alibaba-inc.com>
2024-12-21 00:35:42 +0800

    MscOpenSubscription: Improves the invoking MscOpenSubscription api method and supports refreshing credential automatically

--- alicloud/data_source_alicloud_msc_sub_webhooks.go
+++ alicloud/data_source_alicloud_msc_sub_webhooks.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
-	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -94,16 +93,11 @@ func dataSourceAlicloudMscSubWebhooksRead(d *schema.ResourceData, meta interface
 		}
 	}
 	var response map[string]interface{}
-	conn, err := client.NewMscopensubscriptionClient()
-	if err != nil {
-		return WrapError(err)
-	}
+	var err error
 	for {
-		runtime := util.RuntimeOptions{}
-		runtime.SetAutoretry(true)
 		wait := incrementalWait(3*time.Second, 3*time.Second)
 		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("GET"), StringPointer("2021-07-13"), StringPointer("AK"), request, nil, &runtime)
+			response, err = client.RpcGet("MscOpenSubscription", "2021-07-13", action, request, nil)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
