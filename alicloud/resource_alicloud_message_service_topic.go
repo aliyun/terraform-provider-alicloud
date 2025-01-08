aec1127722611a86981f486d6e6bc62550178ab4
MrWolong <ly01269865@alibaba-inc.com>
2025-01-08 09:10:33 +0800

    resource/alicloud_message_service_topic: Refactored the resource alicloud_message_service_topic; Added the field enable_logging, tags, create_time; Deprecated the field logging_enabled

--- alicloud/resource_alicloud_message_service_topic.go
+++ alicloud/resource_alicloud_message_service_topic.go
@@ -1,73 +1,97 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudMessageServiceTopic() *schema.Resource {
+func resourceAliCloudMessageServiceTopic() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudMessageServiceTopicCreate,
-		Read:   resourceAlicloudMessageServiceTopicRead,
-		Update: resourceAlicloudMessageServiceTopicUpdate,
-		Delete: resourceAlicloudMessageServiceTopicDelete,
+		Create: resourceAliCloudMessageServiceTopicCreate,
+		Read:   resourceAliCloudMessageServiceTopicRead,
+		Update: resourceAliCloudMessageServiceTopicUpdate,
+		Delete: resourceAliCloudMessageServiceTopicDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(3 * time.Minute),
-			Update: schema.DefaultTimeout(3 * time.Minute),
-			Delete: schema.DefaultTimeout(3 * time.Minute),
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
-			"topic_name": {
+			"create_time": {
 				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Computed: true,
+			},
+			"enable_logging": {
+				Type:          schema.TypeBool,
+				Optional:      true,
+				ConflictsWith: []string{"logging_enabled"},
+				Computed:      true,
 			},
 			"max_message_size": {
 				Type:         schema.TypeInt,
 				Optional:     true,
 				Computed:     true,
-				ValidateFunc: validation.IntBetween(1024, 65536),
+				ValidateFunc: IntBetween(1024, 65536),
+			},
+			"tags": tagsSchema(),
+			"topic_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 			"logging_enabled": {
-				Type:     schema.TypeBool,
-				Optional: true,
+				Type:       schema.TypeBool,
+				Optional:   true,
+				Computed:   true,
+				Deprecated: "Field `logging_enabled` has been deprecated from provider version 1.241.0. New field `enable_logging` instead.",
 			},
 		},
 	}
 }
 
-func resourceAlicloudMessageServiceTopicCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudMessageServiceTopicCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
+
 	action := "CreateTopic"
-	request := make(map[string]interface{})
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
 	conn, err := client.NewMnsClient()
 	if err != nil {
 		return WrapError(err)
 	}
-
+	request = make(map[string]interface{})
 	request["TopicName"] = d.Get("topic_name")
 
-	if v, ok := d.GetOk("max_message_size"); ok {
-		request["MaxMessageSize"] = v
-	}
-
-	if v, ok := d.GetOkExists("logging_enabled"); ok {
+	if v, ok := d.GetOkExists("enable_logging"); ok {
+		request["EnableLogging"] = v
+	} else if v, ok := d.GetOkExists("logging_enabled"); ok {
 		request["EnableLogging"] = v
 	}
+	if v, ok := d.GetOkExists("max_message_size"); ok {
+		request["MaxMessageSize"] = v
+	}
+	if v, ok := d.GetOk("tags"); ok {
+		tagsMap := ConvertTags(v.(map[string]interface{}))
+		request = expandTagsToMap(request, tagsMap)
+	}
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -85,61 +109,90 @@ func resourceAlicloudMessageServiceTopicCreate(d *schema.ResourceData, meta inte
 
 	d.SetId(fmt.Sprint(request["TopicName"]))
 
-	return resourceAlicloudMessageServiceTopicRead(d, meta)
+	return resourceAliCloudMessageServiceTopicRead(d, meta)
 }
 
-func resourceAlicloudMessageServiceTopicRead(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudMessageServiceTopicRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	mnsOpenService := MnsOpenService{client}
+	messageServiceServiceV2 := MessageServiceServiceV2{client}
 
-	object, err := mnsOpenService.DescribeMessageServiceTopic(d.Id())
+	objectRaw, err := messageServiceServiceV2.DescribeMessageServiceTopic(d.Id())
 	if err != nil {
 		if NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_message_service_topic DescribeMessageServiceTopic Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
 
-	d.Set("topic_name", object["TopicName"])
-	d.Set("max_message_size", object["MaxMessageSize"])
-	d.Set("logging_enabled", object["LoggingEnabled"])
+	if objectRaw["CreateTime"] != nil {
+		d.Set("create_time", objectRaw["CreateTime"])
+	}
+	if objectRaw["LoggingEnabled"] != nil {
+		d.Set("enable_logging", objectRaw["LoggingEnabled"])
+		d.Set("logging_enabled", objectRaw["LoggingEnabled"])
+	}
+	if objectRaw["MaxMessageSize"] != nil {
+		d.Set("max_message_size", objectRaw["MaxMessageSize"])
+	}
+	if objectRaw["TopicName"] != nil {
+		d.Set("topic_name", objectRaw["TopicName"])
+	}
+
+	if objectRaw["Tags"] != nil {
+		d.Set("tags", tagsToMap(objectRaw["Tags"]))
+	}
 
 	return nil
 }
 
-func resourceAlicloudMessageServiceTopicUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudMessageServiceTopicUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
 	var response map[string]interface{}
+	var query map[string]interface{}
 	update := false
-	request := map[string]interface{}{
-		"TopicName": d.Id(),
+
+	action := "SetTopicAttributes"
+	conn, err := client.NewMnsClient()
+	if err != nil {
+		return WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["TopicName"] = d.Id()
+
+	if d.HasChange("enable_logging") {
+		update = true
+
+		if v, ok := d.GetOkExists("enable_logging"); ok {
+			request["EnableLogging"] = v
+		}
 	}
 
-	if !d.IsNewResource() && d.HasChange("max_message_size") {
+	if d.HasChange("logging_enabled") {
 		update = true
-	}
-	if v, ok := d.GetOk("max_message_size"); ok {
-		request["MaxMessageSize"] = v
+
+		if v, ok := d.GetOkExists("logging_enabled"); ok {
+			request["EnableLogging"] = v
+		}
 	}
 
-	if !d.IsNewResource() && d.HasChange("logging_enabled") {
+	if d.HasChange("max_message_size") {
 		update = true
-	}
-	if v, ok := d.GetOkExists("logging_enabled"); ok {
-		request["EnableLogging"] = v
+
+		if v, ok := d.GetOkExists("max_message_size"); ok {
+			request["MaxMessageSize"] = v
+		}
 	}
 
 	if update {
-		action := "SetTopicAttributes"
-		conn, err := client.NewMnsClient()
-		if err != nil {
-			return WrapError(err)
-		}
-
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+		runtime := util.RuntimeOptions{}
+		runtime.SetAutoretry(true)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -150,30 +203,40 @@ func resourceAlicloudMessageServiceTopicUpdate(d *schema.ResourceData, meta inte
 			return nil
 		})
 		addDebug(action, response, request)
-
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
 	}
 
-	return resourceAlicloudMessageServiceTopicRead(d, meta)
+	if d.HasChange("tags") {
+		messageServiceServiceV2 := MessageServiceServiceV2{client}
+		if err := messageServiceServiceV2.SetResourceTags(d, "topic"); err != nil {
+			return WrapError(err)
+		}
+	}
+	return resourceAliCloudMessageServiceTopicRead(d, meta)
 }
 
-func resourceAlicloudMessageServiceTopicDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudMessageServiceTopicDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	action := "DeleteTopic"
+	var request map[string]interface{}
 	var response map[string]interface{}
+	query := make(map[string]interface{})
 	conn, err := client.NewMnsClient()
 	if err != nil {
 		return WrapError(err)
 	}
-	request := map[string]interface{}{
-		"TopicName": d.Id(),
-	}
+	request = make(map[string]interface{})
+	request["TopicName"] = d.Id()
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), nil, request, &util.RuntimeOptions{})
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
+
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
--- alicloud/service_alicloud_message_service_v2.go
+++ alicloud/service_alicloud_message_service_v2.go
@@ -1,13 +1,17 @@
 package alicloud
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
+	rpc "github.com/alibabacloud-go/tea-rpc/client"
 	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/tidwall/sjson"
 )
 
 type MessageServiceServiceV2 struct {
@@ -83,3 +87,163 @@ func (s *MessageServiceServiceV2) MessageServiceQueueStateRefreshFunc(id string,
 }
 
 // DescribeMessageServiceQueue >>> Encapsulated.
+
+// DescribeMessageServiceTopic <<< Encapsulated get interface for MessageService Topic.
+
+func (s *MessageServiceServiceV2) DescribeMessageServiceTopic(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	action := "GetTopicAttributes"
+	conn, err := client.NewMnsClient()
+	if err != nil {
+		return object, WrapError(err)
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["TopicName"] = id
+
+	runtime := util.RuntimeOptions{}
+	runtime.SetAutoretry(true)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"ResourceNotFound:TopicName", "TopicNotExist"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("Topic", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Data", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Data", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+// DescribeMessageServiceTopic >>> Encapsulated.
+
+// SetResourceTags <<< Encapsulated tag function for MessageService.
+func (s *MessageServiceServiceV2) SetResourceTags(d *schema.ResourceData, resourceType string) error {
+	if d.HasChange("tags") {
+		var err error
+		var action string
+		var conn *rpc.Client
+		client := s.client
+		var request map[string]interface{}
+		var response map[string]interface{}
+		query := make(map[string]interface{})
+
+		added, removed := parsingTags(d)
+		removedTagKeys := make([]string, 0)
+		for _, v := range removed {
+			if !ignoredTags(v, "") {
+				removedTagKeys = append(removedTagKeys, v)
+			}
+		}
+		if len(removedTagKeys) > 0 {
+			action = "UntagResources"
+			conn, err = client.NewMnsClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["RegionId"] = client.RegionId
+			request["ResourceType"] = resourceType
+			for i, key := range removedTagKeys {
+				request[fmt.Sprintf("TagKey.%d", i+1)] = key
+			}
+
+			jsonString := convertObjectToJsonString(request)
+			jsonString, _ = sjson.Set(jsonString, "ResourceId.0", d.Id())
+			err = json.Unmarshal([]byte(jsonString), &request)
+			if err != nil {
+				return WrapError(err)
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+		if len(added) > 0 {
+			action = "TagResources"
+			conn, err = client.NewMnsClient()
+			if err != nil {
+				return WrapError(err)
+			}
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["RegionId"] = client.RegionId
+			count := 1
+			for key, value := range added {
+				request[fmt.Sprintf("Tag.%d.Key", count)] = key
+				request[fmt.Sprintf("Tag.%d.Value", count)] = value
+				count++
+			}
+
+			request["ResourceType"] = resourceType
+			jsonString := convertObjectToJsonString(request)
+			jsonString, _ = sjson.Set(jsonString, "ResourceId.0", d.Id())
+			err = json.Unmarshal([]byte(jsonString), &request)
+			if err != nil {
+				return WrapError(err)
+			}
+
+			runtime := util.RuntimeOptions{}
+			runtime.SetAutoretry(true)
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2022-01-19"), StringPointer("AK"), query, request, &runtime)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+	}
+
+	return nil
+}
+
+// SetResourceTags >>> tag function encapsulated.
