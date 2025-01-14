9b9e810b7fbf1c639d476a3fab3ad42f3288a0d6
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-01-14 13:11:46 +0800

    resource/alicloud_cen_transit_router_multicast_domain: add new attribute options.

--- alicloud/resource_alicloud_cen_transit_router_multicast_domain_test.go
+++ alicloud/resource_alicloud_cen_transit_router_multicast_domain_test.go
@@ -155,3 +155,154 @@ func resourceAliCloudCenTransitRouterMulticastDomainBasicDependence(name string)
 	}
 `, name)
 }
+
+// Test Cen TransitRouterMulticastDomain. >>> Resource test cases, automatically generated.
+// Case TransitRouterMulticastDomain_20241108_线上 8809
+func TestAccAliCloudCenTransitRouterMulticastDomain_basic8809(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cen_transit_router_multicast_domain.default"
+	ra := resourceAttrInit(resourceId, AlicloudCenTransitRouterMulticastDomainMap8809)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CenServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCenTransitRouterMulticastDomain")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1, 999)
+	name := fmt.Sprintf("tf_testacc%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudCenTransitRouterMulticastDomainBasicDependence8809)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hongkong"})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_multicast_domain_name":        name,
+					"transit_router_multicast_domain_description": "description",
+					"transit_router_id":                           "${alicloud_cen_transit_router.defaultSwwLm7.transit_router_id}",
+					"options": []map[string]interface{}{
+						{
+							"igmpv2_support": "disable",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_multicast_domain_name":        name,
+						"transit_router_multicast_domain_description": "description",
+						"transit_router_id":                           CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"transit_router_multicast_domain_name":        name + "_update",
+					"transit_router_multicast_domain_description": "deeeee",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"transit_router_multicast_domain_name":        name + "_update",
+						"transit_router_multicast_domain_description": "deeeee",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"options": []map[string]interface{}{
+						{
+							"igmpv2_support": "enable",
+						},
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "Test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF",
+						"tags.For":     "Test",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF-update",
+						"For":     "Test-update",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF-update",
+						"tags.For":     "Test-update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": REMOVEKEY,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "0",
+						"tags.Created": REMOVEKEY,
+						"tags.For":     REMOVEKEY,
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+var AlicloudCenTransitRouterMulticastDomainMap8809 = map[string]string{
+	"status":    CHECKSET,
+	"region_id": CHECKSET,
+}
+
+func AlicloudCenTransitRouterMulticastDomainBasicDependence8809(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+resource "alicloud_cen_instance" "defaultPNnbnI" {
+  cen_instance_name = var.name
+}
+
+resource "alicloud_cen_transit_router" "defaultSwwLm7" {
+  support_multicast   = true
+  cen_id              = alicloud_cen_instance.defaultPNnbnI.id
+  transit_router_name = format("%%s1", var.name)
+}
+
+
+`, name)
+}
+
+// Test Cen TransitRouterMulticastDomain. <<< Resource test cases, automatically generated.
