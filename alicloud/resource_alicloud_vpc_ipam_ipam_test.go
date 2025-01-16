3b76ac76dafc5c50635e0d82978b95a8d8b60acb
笑忘书 <44337897+ChenHanZhang@users.noreply.github.com>
2025-01-16 13:56:18 +0800

    resource/alicloud_vpc_ipam_ipam: add new attribute tag.

--- alicloud/resource_alicloud_vpc_ipam_ipam_test.go
+++ alicloud/resource_alicloud_vpc_ipam_ipam_test.go
@@ -10,6 +10,146 @@ import (
 )
 
 // Test VpcIpam Ipam. >>> Resource test cases, automatically generated.
+// Case test_ipam_20250115 10035
+func TestAccAliCloudVpcIpamIpam_basic10035(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_vpc_ipam_ipam.default"
+	ra := resourceAttrInit(resourceId, AlicloudVpcIpamIpamMap10035)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &VpcIpamServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeVpcIpamIpam")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%svpcipamipam%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudVpcIpamIpamBasicDependence10035)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"ipam_description":  "This is my first Ipam.",
+					"ipam_name":         name,
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.0}",
+					"operating_region_list": []string{
+						"cn-hangzhou"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"ipam_description":        "This is my first Ipam.",
+						"ipam_name":               name,
+						"resource_group_id":       CHECKSET,
+						"operating_region_list.#": "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"ipam_description":  "This is my new ipam.",
+					"ipam_name":         name + "_update",
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.1}",
+					"operating_region_list": []string{
+						"cn-hangzhou", "cn-beijing", "cn-qingdao"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"ipam_description":        "This is my new ipam.",
+						"ipam_name":               name + "_update",
+						"resource_group_id":       CHECKSET,
+						"operating_region_list.#": "3",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.0}",
+					"operating_region_list": []string{
+						"cn-hangzhou"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"resource_group_id":       CHECKSET,
+						"operating_region_list.#": "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "Test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF",
+						"tags.For":     "Test",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF-update",
+						"For":     "Test-update",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF-update",
+						"tags.For":     "Test-update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": REMOVEKEY,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "0",
+						"tags.Created": REMOVEKEY,
+						"tags.For":     REMOVEKEY,
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+var AlicloudVpcIpamIpamMap10035 = map[string]string{
+	"status":                   CHECKSET,
+	"create_time":              CHECKSET,
+	"region_id":                CHECKSET,
+	"private_default_scope_id": CHECKSET,
+}
+
+func AlicloudVpcIpamIpamBasicDependence10035(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+data "alicloud_resource_manager_resource_groups" "default" {}
+
+
+`, name)
+}
+
 // Case test_ipam 7856
 func TestAccAliCloudVpcIpamIpam_basic7856(t *testing.T) {
 	var v map[string]interface{}
