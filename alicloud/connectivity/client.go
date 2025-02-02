6ef2ab7e16cb6d4a05594396bd46111c57007462
guimin.hgm <guimin.hgm@alibaba-inc.com>
2025-02-02 19:46:20 +0800

    Cloudauth: Improves the invoking api method and supports refreshing credential automatically

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -3609,30 +3609,6 @@ func (client *AliyunClient) NewGdsClient() (*rpc.Client, error) {
 	}
 	return conn, nil
 }
-func (client *AliyunClient) NewCloudauthClient() (*rpc.Client, error) {
-	productCode := "cloudauth"
-	endpoint := ""
-	if v, ok := client.config.Endpoints.Load(productCode); !ok || v.(string) == "" {
-		if err := client.loadEndpoint(productCode); err != nil {
-			endpoint = "cloudauth.aliyuncs.com"
-			client.config.Endpoints.Store(productCode, endpoint)
-			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the endpoint %s instead.", productCode, err, endpoint)
-		}
-	}
-	if v, ok := client.config.Endpoints.Load(productCode); ok && v.(string) != "" {
-		endpoint = v.(string)
-	}
-	if endpoint == "" {
-		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
-	}
-	sdkConfig := client.teaSdkConfig
-	sdkConfig.SetEndpoint(endpoint)
-	conn, err := rpc.NewClient(&sdkConfig)
-	if err != nil {
-		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
-	}
-	return conn, nil
-}
 
 func (client *AliyunClient) NewImpClient() (*rpc.Client, error) {
 	productCode := "imp"
