81e714d07db3291e6ec64351c640b98d711056ae
guimin.hgm <guimin.hgm@alibaba-inc.com>
2025-02-02 19:55:08 +0800

    IMP: Improves the invoking api method and supports refreshing credential automatically

--- alicloud/data_source_alicloud_imp_app_templates.go
+++ alicloud/data_source_alicloud_imp_app_templates.go
@@ -146,16 +146,13 @@ func dataSourceAlicloudImpAppTemplatesRead(d *schema.ResourceData, meta interfac
 	}
 	status, statusOk := d.GetOk("status")
 	var response map[string]interface{}
-	conn, err := client.NewImpClient()
-	if err != nil {
-		return WrapError(err)
-	}
+	var err error
 	for {
 		runtime := util.RuntimeOptions{}
 		runtime.SetAutoretry(true)
 		wait := incrementalWait(3*time.Second, 3*time.Second)
 		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2021-06-30"), StringPointer("AK"), nil, request, &runtime)
+			response, err = client.RpcPost("imp", "2021-06-30", action, nil, request, true)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
--- alicloud/service_alicloud_imp.go
+++ alicloud/service_alicloud_imp.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
-	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
@@ -15,22 +14,16 @@ type ImpService struct {
 }
 
 func (s *ImpService) DescribeImpAppTemplate(id string) (object map[string]interface{}, err error) {
-	conn, err := s.client.NewImpClient()
-	if err != nil {
-		return object, WrapError(err)
-	}
-
+	client := s.client
 	request := map[string]interface{}{
 		"AppTemplateId": id,
 	}
 
 	var response map[string]interface{}
 	action := "GetAppTemplate"
-	runtime := util.RuntimeOptions{}
-	runtime.SetAutoretry(true)
 	wait := incrementalWait(3*time.Second, 3*time.Second)
 	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		resp, err := conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2021-06-30"), StringPointer("AK"), nil, request, &runtime)
+		resp, err := client.RpcPost("imp", "2021-06-30", action, nil, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
