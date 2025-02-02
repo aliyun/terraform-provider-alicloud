794a37c89dfca4fc623434c4af1b1fc56a76e0f7
guimin.hgm <guimin.hgm@alibaba-inc.com>
2025-02-02 16:19:11 +0800

    Eais: Improves the invoking api method and supports refreshing credential automatically

--- alicloud/connectivity/client.go
+++ alicloud/connectivity/client.go
@@ -3660,31 +3660,6 @@ func (client *AliyunClient) NewDbfsClient() (*rpc.Client, error) {
 	return conn, nil
 }
 
-func (client *AliyunClient) NewEaisClient() (*rpc.Client, error) {
-	productCode := "eais"
-	endpoint := ""
-	if v, ok := client.config.Endpoints.Load(productCode); !ok || v.(string) == "" {
-		if err := client.loadEndpoint(productCode); err != nil {
-			endpoint = fmt.Sprintf("eais.%s.aliyuncs.com", client.config.RegionId)
-			client.config.Endpoints.Store(productCode, endpoint)
-			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the endpoint %s instead.", productCode, err, endpoint)
-		}
-	}
-	if v, ok := client.config.Endpoints.Load(productCode); ok && v.(string) != "" {
-		endpoint = v.(string)
-	}
-	if endpoint == "" {
-		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
-	}
-	sdkConfig := client.teaSdkConfig
-	sdkConfig.SetEndpoint(endpoint)
-	conn, err := rpc.NewClient(&sdkConfig)
-	if err != nil {
-		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
-	}
-	return conn, nil
-}
-
 func (client *AliyunClient) NewCloudauthClient() (*rpc.Client, error) {
 	productCode := "cloudauth"
 	endpoint := ""
