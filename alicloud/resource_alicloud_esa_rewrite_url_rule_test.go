679cf364e88fa24930afaeb73edb093189873697
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-02-05 19:23:20 +0800

    New Resource: alicloud_esa_rewrite_url_rule; New Resource: alicloud_esa_redirect_rule; New Resource: alicloud_esa_http_response_header_modification_rule.

---
+++ alicloud/resource_alicloud_esa_rewrite_url_rule_test.go
@@ -0,0 +1,168 @@
+package alicloud
+
+import (
+	"fmt"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+// Test ESA RewriteUrlRule. >>> Resource test cases, automatically generated.
+// Case resource_RewriteUrlRule_test
+func TestAccAliCloudESARewriteUrlRuleresource_RewriteUrlRule_test(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_esa_rewrite_url_rule.default"
+	ra := resourceAttrInit(resourceId, AliCloudESARewriteUrlRuleresource_RewriteUrlRule_testMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &EsaServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeEsaRewriteUrlRule")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%sESARewriteUrlRule%d", defaultRegionToTest, rand)
+
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudESARewriteUrlRuleresource_RewriteUrlRule_testBasicDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"site_id":                   "${alicloud_esa_site.resource_RewriteUrlRule_Site_test.id}",
+					"rewrite_uri_type":          "static",
+					"rule_enable":               "on",
+					"rewrite_query_string_type": "static",
+					"query_string":              "example=123",
+					"rule":                      "http.host eq \\\"video.example.com\\\"",
+					"site_version":              "0",
+					"uri":                       "/image/example.jpg",
+					"rule_name":                 "test",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rule": "http.host eq \\\"video.example.com\\\"",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rule_name": "重写URL规则名称示例",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rule_enable": "on",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rewrite_uri_type": "static",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"uri": "/rewritten/target-uri",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rewrite_query_string_type": "static",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"query_string": "重写后的查询字符串示例",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"rewrite_uri_type":          "static",
+					"rule_enable":               "off",
+					"rewrite_query_string_type": "static",
+					"query_string":              "新的查询字符串示例",
+					"rule":                      "http.host eq \\\"video.change.com\\\"",
+					"uri":                       "/rewritten/new-target-uri",
+					"rule_name":                 "新规则名称示例",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+var AliCloudESARewriteUrlRuleresource_RewriteUrlRule_testMap = map[string]string{
+	"id": CHECKSET,
+}
+
+func AliCloudESARewriteUrlRuleresource_RewriteUrlRule_testBasicDependence(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+
+resource "alicloud_esa_rate_plan_instance" "resource_RewriteUrlRule_RatePlanInstance_test" {
+  type         = "NS"
+  auto_renew   = "false"
+  period       = "1"
+  payment_type = "Subscription"
+  coverage     = "overseas"
+  auto_pay     = "true"
+  plan_name    = "high"
+}
+
+resource "alicloud_esa_site" "resource_RewriteUrlRule_Site_test" {
+  site_name   = "gositecdn.cn"
+  instance_id = alicloud_esa_rate_plan_instance.resource_RewriteUrlRule_RatePlanInstance_test.id
+  coverage    = "overseas"
+  access_type = "NS"
+}
+
+`, name)
+}
+
+// Test ESA RewriteUrlRule. <<< Resource test cases, automatically generated.
