3c75571e29f059f00d4fbaac2814c1e0dc9ae34d
guimin.hgm <guimin.hgm@alibaba-inc.com>
2025-02-06 19:19:01 +0800

    DevopsRdc: Improves the invoking api method and supports refreshing credential automatically

--- alicloud/data_source_alicloud_rdc_organizations.go
+++ alicloud/data_source_alicloud_rdc_organizations.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/PaesslerAG/jsonpath"
-	util "github.com/alibabacloud-go/tea-utils/service"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -96,15 +95,10 @@ func dataSourceAlicloudRdcOrganizationsRead(d *schema.ResourceData, meta interfa
 		}
 	}
 	var response map[string]interface{}
-	conn, err := client.NewDevopsrdcClient()
-	if err != nil {
-		return WrapError(err)
-	}
-	runtime := util.RuntimeOptions{}
-	runtime.SetAutoretry(true)
+	var err error
 	wait := incrementalWait(3*time.Second, 3*time.Second)
 	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		response, err = conn.DoRequest(StringPointer(action), nil, StringPointer("POST"), StringPointer("2020-03-03"), StringPointer("AK"), nil, request, &runtime)
+		response, err = client.RpcPost("devops-rdc", "2020-03-03", action, nil, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
