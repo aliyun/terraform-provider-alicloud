49b1ea5ba16ea4c8bc80f705e728d5ebb38b6d15
lijiuxing <ljx01887644@alibaba-inc.com>
2025-02-14 13:54:03 +0800

    data-source/alicloud_cs_managed_kubernetes_clusters: deprecated kubeconfig attribute; data-source/alicloud_cs_kubernetes_clusters: deprecated kubeconfig attribute; data-source/alicloud_cs_serverless_kubernetes_clusters: deprecated kubeconfig attribute; docs: deprecated kubeconfig attribute

--- alicloud/data_source_alicloud_cs_managed_kubernetes_clusters.go
+++ alicloud/data_source_alicloud_cs_managed_kubernetes_clusters.go
@@ -47,6 +47,7 @@ func dataSourceAlicloudCSManagerKubernetesClusters() *schema.Resource {
 			"kube_config_file_prefix": {
 				Type:     schema.TypeString,
 				Optional: true,
+				Deprecated: "Field 'kube_config_file_prefix' has been deprecated from provider version 1.243.0. From version 1.243.0, please use the attribute 'output_file' of new DataSource 'alicloud_cs_cluster_credential' to replace it.",
 			},
 			// Computed values
 			"names": {
