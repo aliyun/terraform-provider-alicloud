e18f63c0e24e7a8237acc6adf5b7bac0c82b05fd
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-02-27 11:21:57 +0800

    New Resource: alicloud_esa_waiting_room_rule.

---
+++ alicloud/resource_alicloud_esa_waiting_room_rule.go
@@ -0,0 +1,216 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudEsaWaitingRoomRule() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudEsaWaitingRoomRuleCreate,
+		Read:   resourceAliCloudEsaWaitingRoomRuleRead,
+		Update: resourceAliCloudEsaWaitingRoomRuleUpdate,
+		Delete: resourceAliCloudEsaWaitingRoomRuleDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"rule": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"rule_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"site_id": {
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"waiting_room_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"waiting_room_rule_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudEsaWaitingRoomRuleCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateWaitingRoomRule"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOk("site_id"); ok {
+		request["SiteId"] = v
+	}
+	if v, ok := d.GetOk("waiting_room_id"); ok {
+		request["WaitingRoomId"] = v
+	}
+	request["RegionId"] = client.RegionId
+
+	request["RuleEnable"] = d.Get("status")
+	request["RuleName"] = d.Get("rule_name")
+	request["Rule"] = d.Get("rule")
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_esa_waiting_room_rule", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprintf("%v:%v:%v", request["SiteId"], request["WaitingRoomId"], response["WaitingRoomRuleId"]))
+
+	return resourceAliCloudEsaWaitingRoomRuleRead(d, meta)
+}
+
+func resourceAliCloudEsaWaitingRoomRuleRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	esaServiceV2 := EsaServiceV2{client}
+
+	objectRaw, err := esaServiceV2.DescribeEsaWaitingRoomRule(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_esa_waiting_room_rule DescribeEsaWaitingRoomRule Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("rule", objectRaw["Rule"])
+	d.Set("rule_name", objectRaw["RuleName"])
+	d.Set("status", objectRaw["RuleEnable"])
+	d.Set("waiting_room_rule_id", objectRaw["WaitingRoomRuleId"])
+
+	parts := strings.Split(d.Id(), ":")
+	d.Set("site_id", formatInt(parts[0]))
+	d.Set("waiting_room_id", parts[1])
+
+	return nil
+}
+
+func resourceAliCloudEsaWaitingRoomRuleUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
+
+	var err error
+	parts := strings.Split(d.Id(), ":")
+	action := "UpdateWaitingRoomRule"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["SiteId"] = parts[0]
+	request["WaitingRoomRuleId"] = parts[2]
+	request["RegionId"] = client.RegionId
+	if d.HasChange("status") {
+		update = true
+	}
+	request["RuleEnable"] = d.Get("status")
+	if d.HasChange("rule_name") {
+		update = true
+	}
+	request["RuleName"] = d.Get("rule_name")
+	if d.HasChange("rule") {
+		update = true
+	}
+	request["Rule"] = d.Get("rule")
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+	}
+
+	return resourceAliCloudEsaWaitingRoomRuleRead(d, meta)
+}
+
+func resourceAliCloudEsaWaitingRoomRuleDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "DeleteWaitingRoomRule"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["SiteId"] = parts[0]
+	request["WaitingRoomRuleId"] = parts[2]
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	return nil
+}
---
+++ website/docs/r/esa_waiting_room_rule.html.markdown
@@ -0,0 +1,99 @@
+---
+subcategory: "ESA"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_esa_waiting_room_rule"
+description: |-
+  Provides a Alicloud ESA Waiting Room Rule resource.
+---
+
+# alicloud_esa_waiting_room_rule
+
+Provides a ESA Waiting Room Rule resource.
+
+
+
+For information about ESA Waiting Room Rule and how to use it, see [What is Waiting Room Rule](https://next.api.alibabacloud.com/document/ESA/2024-09-10/CreateWaitingRoomRule).
+
+-> **NOTE:** Available since v1.244.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+data "alicloud_esa_sites" "default" {
+  plan_subscribe_type = "enterpriseplan"
+}
+
+resource "alicloud_esa_site" "default" {
+  site_name   = "terraform.site"
+  instance_id = data.alicloud_esa_sites.default.sites.0.instance_id
+  coverage    = "overseas"
+  access_type = "NS"
+}
+
+resource "alicloud_esa_waiting_room" "default" {
+  status                         = "off"
+  site_id                        = alicloud_esa_site.default.id
+  json_response_enable           = "off"
+  description                    = "example"
+  waiting_room_type              = "default"
+  disable_session_renewal_enable = "off"
+  cookie_name                    = "__aliwaitingroom_example"
+  waiting_room_name              = "waitingroom_example"
+  queue_all_enable               = "off"
+  queuing_status_code            = "200"
+  custom_page_html               = ""
+  new_users_per_minute           = "200"
+  session_duration               = "5"
+  language                       = "zhcn"
+  total_active_users             = "300"
+  queuing_method                 = "fifo"
+  host_name_and_path {
+    domain    = "sub_domain.com"
+    path      = "/example"
+    subdomain = "example_sub_domain.com."
+  }
+}
+
+resource "alicloud_esa_waiting_room_rule" "default" {
+  rule            = "(http.host eq \"video.example.com\")"
+  waiting_room_id = alicloud_esa_waiting_room.default.waiting_room_id
+  rule_name       = "WaitingRoomRule_example1"
+  status          = "off"
+  site_id         = alicloud_esa_site.default.id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `rule` - (Required) Specifies whether to enable the rule. Valid values:
+
+  - on
+  - off
+* `rule_name` - (Required) Optional. The rule ID, which can be used to query a specific rule.
+* `site_id` - (Required, ForceNew, Int) The site ID, which can be obtained by calling the ListSites API.
+* `status` - (Required) The rule name.
+* `waiting_room_id` - (Required, ForceNew) The website ID, which can be obtained by calling the [ListSites](https://www.alibabacloud.com/help/en/doc-detail/2850189.html) operation.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<site_id>:<waiting_room_id>:<waiting_room_rule_id>`.
+* `waiting_room_rule_id` - WaitingRoomRuleId Id
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Waiting Room Rule.
+* `delete` - (Defaults to 5 mins) Used when delete the Waiting Room Rule.
+* `update` - (Defaults to 5 mins) Used when update the Waiting Room Rule.
+
+## Import
+
+ESA Waiting Room Rule can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_esa_waiting_room_rule.example <site_id>:<waiting_room_id>:<waiting_room_rule_id>
+```
