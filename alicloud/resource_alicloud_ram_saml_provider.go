ffb75f2926a8a835263c24ad7e09091fa5ecd0f1
笑忘书 <44337897+ChenHanZhang@users.noreply.github.com>
2025-03-10 14:48:26 +0800

    resource/alicloud_ram_saml_provider: Improves the invkoing api method. (#8473)

--- alicloud/resource_alicloud_ram_saml_provider.go
+++ alicloud/resource_alicloud_ram_saml_provider.go
@@ -1,3 +1,4 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
@@ -7,6 +8,7 @@ import (
 
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/blues/jsonata-go"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -20,11 +22,19 @@ func resourceAliCloudRamSamlProvider() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
 		Schema: map[string]*schema.Schema{
-			"saml_provider_name": {
+			"arn": {
 				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 			"encodedsaml_metadata_document": {
 				Type:     schema.TypeString,
@@ -33,13 +43,10 @@ func resourceAliCloudRamSamlProvider() *schema.Resource {
 					return ramSAMLProviderDiffSuppressFunc(old, new)
 				},
 			},
-			"description": {
+			"saml_provider_name": {
 				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"arn": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Required: true,
+				ForceNew: true,
 			},
 			"update_date": {
 				Type:     schema.TypeString,
@@ -50,22 +57,26 @@ func resourceAliCloudRamSamlProvider() *schema.Resource {
 }
 
 func resourceAliCloudRamSamlProviderCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
-	action := "CreateSAMLProvider"
-	request := make(map[string]interface{})
-	var err error
 
-	request["SAMLProviderName"] = d.Get("saml_provider_name")
-	request["EncodedSAMLMetadataDocument"] = d.Get("encodedsaml_metadata_document")
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateSAMLProvider"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOk("saml_provider_name"); ok {
+		request["SAMLProviderName"] = v
+	}
 
 	if v, ok := d.GetOk("description"); ok {
 		request["Description"] = v
 	}
-
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
-		response, err = client.RpcPost("Ims", "2019-08-15", action, nil, request, true)
+	request["EncodedSAMLMetadataDocument"] = d.Get("encodedsaml_metadata_document")
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("Ims", "2019-08-15", action, query, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -81,66 +92,66 @@ func resourceAliCloudRamSamlProviderCreate(d *schema.ResourceData, meta interfac
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ram_saml_provider", action, AlibabaCloudSdkGoERROR)
 	}
 
-	if resp, err := jsonpath.Get("$.SAMLProvider", response); err != nil || resp == nil {
-		return WrapErrorf(err, IdMsg, "alicloud_ram_saml_provider")
-	} else {
-		samlProviderName := resp.(map[string]interface{})["SAMLProviderName"]
-		d.SetId(fmt.Sprint(samlProviderName))
-	}
+	id, _ := jsonpath.Get("$.SAMLProvider.SAMLProviderName", response)
+	d.SetId(fmt.Sprint(id))
 
 	return resourceAliCloudRamSamlProviderRead(d, meta)
 }
 
 func resourceAliCloudRamSamlProviderRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	imsService := ImsService{client}
+	ramServiceV2 := RamServiceV2{client}
 
-	object, err := imsService.DescribeRamSamlProvider(d.Id())
+	objectRaw, err := ramServiceV2.DescribeRamSamlProvider(d.Id())
 	if err != nil {
 		if !d.IsNewResource() && NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_ram_saml_provider imsService.DescribeRamSamlProvider Failed!!! %s", err)
+			log.Printf("[DEBUG] Resource alicloud_ram_saml_provider DescribeRamSamlProvider Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
 
-	d.Set("saml_provider_name", object["SAMLProviderName"])
-	d.Set("encodedsaml_metadata_document", object["EncodedSAMLMetadataDocument"])
-	d.Set("description", object["Description"])
-	d.Set("arn", object["Arn"])
-	d.Set("update_date", object["UpdateDate"])
+	d.Set("arn", objectRaw["Arn"])
+	d.Set("description", objectRaw["Description"])
+	d.Set("update_date", objectRaw["UpdateDate"])
+	d.Set("saml_provider_name", objectRaw["SAMLProviderName"])
+
+	e := jsonata.MustCompile("$replace($.EncodedSAMLMetadataDocument, \"\n\", \"\")")
+	evaluation, _ := e.Eval(objectRaw)
+	d.Set("encodedsaml_metadata_document", evaluation)
+
+	d.Set("saml_provider_name", d.Id())
 
 	return nil
 }
 
 func resourceAliCloudRamSamlProviderUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
 	var response map[string]interface{}
-	var err error
+	var query map[string]interface{}
 	update := false
 
-	request := map[string]interface{}{
-		"SAMLProviderName": d.Id(),
+	var err error
+	action := "UpdateSAMLProvider"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["SAMLProviderName"] = d.Id()
+
+	if d.HasChange("description") {
+		update = true
+		request["NewDescription"] = d.Get("description")
 	}
 
 	if d.HasChange("encodedsaml_metadata_document") {
 		update = true
 	}
 	request["NewEncodedSAMLMetadataDocument"] = d.Get("encodedsaml_metadata_document")
-
-	if d.HasChange("description") {
-		update = true
-	}
-	if v, ok := d.GetOk("description"); ok {
-		request["NewDescription"] = v
-	}
-
 	if update {
-		action := "UpdateSAMLProvider"
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
-			response, err = client.RpcPost("Ims", "2019-08-15", action, nil, request, true)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("Ims", "2019-08-15", action, query, request, true)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -151,7 +162,6 @@ func resourceAliCloudRamSamlProviderUpdate(d *schema.ResourceData, meta interfac
 			return nil
 		})
 		addDebug(action, response, request)
-
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
@@ -161,18 +171,20 @@ func resourceAliCloudRamSamlProviderUpdate(d *schema.ResourceData, meta interfac
 }
 
 func resourceAliCloudRamSamlProviderDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	action := "DeleteSAMLProvider"
+	var request map[string]interface{}
 	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
+	request = make(map[string]interface{})
+	request["SAMLProviderName"] = d.Id()
 
-	request := map[string]interface{}{
-		"SAMLProviderName": d.Id(),
-	}
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("Ims", "2019-08-15", action, query, request, true)
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
-		response, err = client.RpcPost("Ims", "2019-08-15", action, nil, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -185,7 +197,7 @@ func resourceAliCloudRamSamlProviderDelete(d *schema.ResourceData, meta interfac
 	addDebug(action, response, request)
 
 	if err != nil {
-		if IsExpectedErrors(err, []string{"EntityNotExist.SAMLProviderError"}) || NotFoundError(err) {
+		if IsExpectedErrors(err, []string{"EntityNotExist.SAMLProvider"}) || NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
