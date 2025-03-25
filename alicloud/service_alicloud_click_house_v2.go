a98e94fc31c84b25559850a85d9f08b917e6350a
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-03-25 20:55:03 +0800

    New Resource: alicloud_click_house_enterprise_db_cluster_account.

--- alicloud/service_alicloud_click_house_v2.go
+++ alicloud/service_alicloud_click_house_v2.go
@@ -86,3 +86,132 @@ func (s *ClickHouseServiceV2) ClickHouseEnterpriseDBClusterStateRefreshFunc(id s
 }
 
 // DescribeClickHouseEnterpriseDBCluster >>> Encapsulated.
+// DescribeClickHouseEnterpriseDBClusterAccount <<< Encapsulated get interface for ClickHouse EnterpriseDBClusterAccount.
+
+func (s *ClickHouseServiceV2) DescribeClickHouseEnterpriseDBClusterAccount(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["Account"] = parts[1]
+	request["DBInstanceId"] = parts[0]
+	request["RegionId"] = client.RegionId
+	action := "DescribeAccountAuthority"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("clickhouse", "2023-05-22", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InvalidAccountName.NotFound", "InvalidDBInstanceId.NotFound"}) {
+			return object, WrapErrorf(Error(GetNotFoundMessage("EnterpriseDBClusterAccount", id)), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Data", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Data", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+func (s *ClickHouseServiceV2) DescribeEnterpriseDBClusterAccountDescribeAccounts(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DBInstanceId"] = parts[0]
+	request["RegionId"] = client.RegionId
+	action := "DescribeAccounts"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("clickhouse", "2023-05-22", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Data.Accounts[*]", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Data.Accounts[*]", response)
+	}
+
+	if len(v.([]interface{})) == 0 {
+		return object, WrapErrorf(Error(GetNotFoundMessage("EnterpriseDBClusterAccount", id)), NotFoundMsg, response)
+	}
+
+	result, _ := v.([]interface{})
+	for _, v := range result {
+		item := v.(map[string]interface{})
+		if fmt.Sprint(item["Account"]) != parts[1] {
+			continue
+		}
+		return item, nil
+	}
+	return object, WrapErrorf(Error(GetNotFoundMessage("EnterpriseDBClusterAccount", id)), NotFoundMsg, response)
+}
+
+func (s *ClickHouseServiceV2) ClickHouseEnterpriseDBClusterAccountStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeClickHouseEnterpriseDBClusterAccount(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeClickHouseEnterpriseDBClusterAccount >>> Encapsulated.
---
+++ website/docs/r/click_house_enterprise_db_cluster_account.html.markdown
@@ -0,0 +1,128 @@
+---
+subcategory: "Click House"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_click_house_enterprise_db_cluster_account"
+description: |-
+  Provides a Alicloud Click House Enterprise Db Cluster Account resource.
+---
+
+# alicloud_click_house_enterprise_db_cluster_account
+
+Provides a Click House Enterprise Db Cluster Account resource.
+
+Clickhouse enterprise instance account.
+
+For information about Click House Enterprise Db Cluster Account and how to use it, see [What is Enterprise Db Cluster Account](https://next.api.alibabacloud.com/document/clickhouse/2023-05-22/CreateAccount).
+
+-> **NOTE:** Available since v1.247.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-beijing"
+}
+
+variable "region_id" {
+  default = "cn-beijing"
+}
+
+variable "vsw_ip_range_i" {
+  default = "172.16.1.0/24"
+}
+
+variable "vpc_ip_range" {
+  default = "172.16.0.0/12"
+}
+
+variable "zone_id_i" {
+  default = "cn-beijing-i"
+}
+
+resource "alicloud_vpc" "defaultktKLuM" {
+  cidr_block = var.vpc_ip_range
+}
+
+resource "alicloud_vswitch" "defaultTQWN3k" {
+  vpc_id     = alicloud_vpc.defaultktKLuM.id
+  zone_id    = var.zone_id_i
+  cidr_block = var.vsw_ip_range_i
+}
+
+resource "alicloud_click_house_enterprise_db_cluster" "defaultWrovOd" {
+  zone_id    = var.zone_id_i
+  vpc_id     = alicloud_vpc.defaultktKLuM.id
+  scale_min  = "8"
+  scale_max  = "16"
+  vswitch_id = alicloud_vswitch.defaultTQWN3k.id
+}
+
+
+resource "alicloud_click_house_enterprise_db_cluster_account" "default" {
+  account        = "abc"
+  description    = "example_desc"
+  db_instance_id = alicloud_click_house_enterprise_db_cluster.defaultWrovOd.id
+  account_type   = "NormalAccount"
+  password       = "abc123456!"
+  dml_auth_setting {
+    dml_authority      = "0"
+    ddl_authority      = true
+    allow_dictionaries = ["*"]
+    allow_databases    = ["*"]
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `account` - (Required, ForceNew) The name of the database account.
+* `account_type` - (Required, ForceNew) The type of the database account. Valid values:
+  - `NormalAccount`: Normal account number.
+  - `SuperAccount`: The privileged account.
+* `db_instance_id` - (Required, ForceNew) The cluster ID.
+* `description` - (Optional) Note information.
+* `dml_auth_setting` - (Optional, List) Authorization information. See [`dml_auth_setting`](#dml_auth_setting) below.
+* `password` - (Required) Database account password. Set the following rules.
+  - Consists of at least three of uppercase letters, lowercase letters, numbers, and special characters.
+  - Oh-! @#$%^& *()_+-= is a special character.
+  - Length is 8~32 characters.
+
+### `dml_auth_setting`
+
+The dml_auth_setting supports the following:
+* `allow_databases` - (Optional, List) The list of databases that require authorization. If there are more than one, separate them with commas (,).
+* `allow_dictionaries` - (Optional, List) List of dictionaries that require authorization. If there are more than one, separate them with commas (,).
+* `ddl_authority` - (Required) Whether to grant the DDL permission to the database account. Value description:
+  - `true`: allows DDL.
+  - `false`: DDL is disabled.
+* `dml_authority` - (Required, Int) Whether to grant the DML permission to the database account. The values are as follows:
+  - `0`: Queries that allow reading, writing, and changing settings
+  - `1`: Only queries for reading data are allowed.
+  - `2`: allows queries to read data and change settings.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<db_instance_id>:<account>`.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://www.terraform.io/docs/configuration-0-11/resources.html#timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Enterprise Db Cluster Account.
+* `delete` - (Defaults to 5 mins) Used when delete the Enterprise Db Cluster Account.
+* `update` - (Defaults to 5 mins) Used when update the Enterprise Db Cluster Account.
+
+## Import
+
+Click House Enterprise Db Cluster Account can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_click_house_enterprise_db_cluster_account.example <db_instance_id>:<account>
+```
