8dac01927a6af987a60f020eab929606a44b4cc2
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-03-31 16:53:21 +0800

    New Resource: alicloud_esa_edge_container_app_record.

---
+++ alicloud/resource_alicloud_esa_edge_container_app_record.go
@@ -0,0 +1,156 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudEsaEdgeContainerAppRecord() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudEsaEdgeContainerAppRecordCreate,
+		Read:   resourceAliCloudEsaEdgeContainerAppRecordRead,
+		Delete: resourceAliCloudEsaEdgeContainerAppRecordDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"app_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"record_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"site_id": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudEsaEdgeContainerAppRecordCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateEdgeContainerAppRecord"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOk("app_id"); ok {
+		request["AppId"] = v
+	}
+	if v, ok := d.GetOk("site_id"); ok {
+		request["SiteId"] = v
+	}
+	if v, ok := d.GetOk("record_name"); ok {
+		request["RecordName"] = v
+	}
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_esa_edge_container_app_record", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprintf("%v:%v:%v", request["SiteId"], request["AppId"], request["RecordName"]))
+
+	return resourceAliCloudEsaEdgeContainerAppRecordRead(d, meta)
+}
+
+func resourceAliCloudEsaEdgeContainerAppRecordRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	esaServiceV2 := EsaServiceV2{client}
+
+	objectRaw, err := esaServiceV2.DescribeEsaEdgeContainerAppRecord(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_esa_edge_container_app_record DescribeEsaEdgeContainerAppRecord Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("create_time", objectRaw["CreateTime"])
+	d.Set("app_id", objectRaw["AppId"])
+	d.Set("record_name", objectRaw["RecordName"])
+	d.Set("site_id", objectRaw["SiteId"])
+
+	return nil
+}
+
+func resourceAliCloudEsaEdgeContainerAppRecordDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "DeleteEdgeContainerAppRecord"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["AppId"] = parts[1]
+	request["SiteId"] = parts[0]
+	request["RecordName"] = parts[2]
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	return nil
+}
---
+++ alicloud/resource_alicloud_esa_edge_container_app_record_test.go
@@ -0,0 +1,102 @@
+package alicloud
+
+import (
+	"fmt"
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+// Test ESA EdgeContainerAppRecord. >>> Resource test cases, automatically generated.
+// Case resource_EdgeContainerAppRecord_test
+func TestAccAliCloudESAEdgeContainerAppRecordresource_EdgeContainerAppRecord_test(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_esa_edge_container_app_record.default"
+	ra := resourceAttrInit(resourceId, AliCloudESAEdgeContainerAppRecordresource_EdgeContainerAppRecord_testMap)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &EsaServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeEsaEdgeContainerAppRecord")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("bcd%d.com", rand)
+
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudESAEdgeContainerAppRecordresource_EdgeContainerAppRecord_testBasicDependence)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"record_name": "tf." + name,
+					"site_id":     "${alicloud_esa_site.resource_Site_OriginPool_test.id}",
+					"app_id":      "${alicloud_esa_edge_container_app.default.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+var AliCloudESAEdgeContainerAppRecordresource_EdgeContainerAppRecord_testMap = map[string]string{
+	"id": CHECKSET,
+}
+
+func AliCloudESAEdgeContainerAppRecordresource_EdgeContainerAppRecord_testBasicDependence(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+data "alicloud_esa_sites" "default" {
+  plan_subscribe_type = "enterpriseplan"
+}
+
+resource "alicloud_esa_site" "resource_Site_OriginPool_test" {
+  site_name   = var.name
+  instance_id = data.alicloud_esa_sites.default.sites.0.instance_id
+  coverage    = "overseas"
+  access_type = "NS"
+}
+
+resource "alicloud_esa_edge_container_app" "default" {
+  health_check_host = "example.com"
+  health_check_type = "l7"
+  service_port = "80"
+  health_check_interval = "5"
+  edge_container_app_name = "terraform-app"
+  health_check_http_code = "http_2xx"
+  health_check_uri = "/"
+  health_check_timeout = "3"
+  health_check_succ_times = "2"
+  remarks = "无备注信息1"
+  health_check_method = "HEAD"
+  health_check_port = "80"
+  health_check_fail_times = "5"
+  target_port = "3000"
+}
+
+`, name)
+}
+
+// Test ESA EdgeContainerAppRecord. <<< Resource test cases, automatically generated.
