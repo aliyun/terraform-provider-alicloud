56890ff318a3f5d4503f3954463dde54a5ed0d52
MrWolong <ly01269865@alibaba-inc.com>
2025-04-21 17:55:37 +0800

    resource/alicloud_slb_listener: Added the field acl_ids; Deprecated the field acl_id

--- alicloud/resource_alicloud_slb_listener_test.go
+++ alicloud/resource_alicloud_slb_listener_test.go
@@ -309,7 +309,6 @@ func TestAccAliCloudSLBListener_Forward(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
-
 		// module name
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -322,6 +321,9 @@ func TestAccAliCloudSLBListener_Forward(t *testing.T) {
 					"protocol":         "http",
 					"listener_forward": "on",
 					"forward_port":     "${alicloud_slb_listener.default-1.frontend_port}",
+					"acl_status":       "on",
+					"acl_type":         "white",
+					"acl_ids":          "${alicloud_slb_acl.acl.*.id}",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
@@ -343,9 +345,9 @@ func TestAccAliCloudSLBListener_Forward(t *testing.T) {
 						"unhealthy_threshold":       "3",
 						"health_check_timeout":      "5",
 						"health_check_interval":     "2",
-						"acl_status":                string(OffFlag),
-						"acl_type":                  NOSET,
-						"acl_id":                    NOSET,
+						"acl_status":                "on",
+						"acl_type":                  string(AclTypeWhite),
+						"acl_ids.#":                 "3",
 						"gzip":                      NOSET,
 						"idle_timeout":              NOSET,
 						"request_timeout":           NOSET,
@@ -356,6 +358,7 @@ func TestAccAliCloudSLBListener_Forward(t *testing.T) {
 		},
 	})
 }
+
 func TestAccAliCloudSLBListener_same_port(t *testing.T) {
 	var v map[string]interface{}
 	resourceId := "alicloud_slb_listener.default"
@@ -399,6 +402,7 @@ func TestAccAliCloudSLBListener_same_port(t *testing.T) {
 		},
 	})
 }
+
 func TestAccAliCloudSLBListener_https_update(t *testing.T) {
 	var v map[string]interface{}
 	resourceId := "alicloud_slb_listener.default"
@@ -415,7 +419,6 @@ func TestAccAliCloudSLBListener_https_update(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
-
 		// module name
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -447,9 +450,6 @@ func TestAccAliCloudSLBListener_https_update(t *testing.T) {
 							"retrive_slb_id": "true",
 						},
 					},
-					"acl_status":            "on",
-					"acl_type":              "white",
-					"acl_id":                "${alicloud_slb_acl.default.id}",
 					"request_timeout":       "80",
 					"idle_timeout":          "30",
 					"enable_http2":          "on",
@@ -476,9 +476,6 @@ func TestAccAliCloudSLBListener_https_update(t *testing.T) {
 						"unhealthy_threshold":       "8",
 						"health_check_timeout":      "8",
 						"health_check_interval":     "5",
-						"acl_status":                "on",
-						"acl_type":                  string(AclTypeWhite),
-						"acl_id":                    CHECKSET,
 						"gzip":                      "true",
 						"idle_timeout":              "30",
 						"request_timeout":           "80",
@@ -645,6 +642,30 @@ func TestAccAliCloudSLBListener_https_update(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
 						"bandwidth": "15",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"acl_status": "on",
+					"acl_type":   "white",
+					"acl_id":     "${alicloud_slb_acl.default.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"acl_status": "on",
+						"acl_type":   string(AclTypeWhite),
+						"acl_id":     CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"acl_id": "${alicloud_slb_acl.acl.0.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"acl_id": CHECKSET,
 					}),
 				),
 			},
@@ -873,7 +894,6 @@ func TestAccAliCloudSLBListener_tcp_server_group(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheck(t)
 		},
-
 		// module name
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -1207,6 +1227,7 @@ func TestAccAliCloudSLBListener_http_healcheckmethod(t *testing.T) {
 		},
 	})
 }
+
 func TestAccAliCloudSLBListener_https_healcheckmethod(t *testing.T) {
 	var v map[string]interface{}
 	resourceId := "alicloud_slb_listener.default"
@@ -1224,7 +1245,6 @@ func TestAccAliCloudSLBListener_https_healcheckmethod(t *testing.T) {
 			testAccPreCheckWithRegions(t, true, connectivity.HttpHttpsHealthCheckMehtodSupportedRegions)
 			testAccPreCheck(t)
 		},
-
 		// module name
 		IDRefreshName: resourceId,
 		Providers:     testAccProviders,
@@ -1256,9 +1276,6 @@ func TestAccAliCloudSLBListener_https_healcheckmethod(t *testing.T) {
 							"retrive_slb_id": "true",
 						},
 					},
-					"acl_status":            "on",
-					"acl_type":              "white",
-					"acl_id":                "${alicloud_slb_acl.default.id}",
 					"request_timeout":       "80",
 					"idle_timeout":          "30",
 					"enable_http2":          "on",
@@ -1284,9 +1301,6 @@ func TestAccAliCloudSLBListener_https_healcheckmethod(t *testing.T) {
 						"unhealthy_threshold":       "8",
 						"health_check_timeout":      "8",
 						"health_check_interval":     "5",
-						"acl_status":                "on",
-						"acl_type":                  string(AclTypeWhite),
-						"acl_id":                    CHECKSET,
 						"gzip":                      "true",
 						"idle_timeout":              "30",
 						"request_timeout":           "80",
@@ -1312,6 +1326,40 @@ func TestAccAliCloudSLBListener_https_healcheckmethod(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
 						"health_check_method": "get",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"acl_status": "on",
+					"acl_type":   "white",
+					"acl_ids":    "${alicloud_slb_acl.acl.*.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"acl_status": "on",
+						"acl_type":   string(AclTypeWhite),
+						"acl_ids.#":  "3",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"acl_ids": []string{"${alicloud_slb_acl.acl.0.id}"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"acl_ids.#": "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"acl_ids": "${alicloud_slb_acl.acl.*.id}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"acl_ids.#": "3",
 					}),
 				),
 			},
@@ -1570,6 +1618,12 @@ func resourceSlbHTTPSListenerConfigDependence(name string) string {
   		server_certificate = "-----BEGIN CERTIFICATE-----\nMIIDRjCCAq+gAwIBAgIJAJn3ox4K13PoMA0GCSqGSIb3DQEBBQUAMHYxCzAJBgNV\nBAYTAkNOMQswCQYDVQQIEwJCSjELMAkGA1UEBxMCQkoxDDAKBgNVBAoTA0FMSTEP\nMA0GA1UECxMGQUxJWVVOMQ0wCwYDVQQDEwR0ZXN0MR8wHQYJKoZIhvcNAQkBFhB0\nZXN0QGhvdG1haWwuY29tMB4XDTE0MTEyNDA2MDQyNVoXDTI0MTEyMTA2MDQyNVow\ndjELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAkJKMQswCQYDVQQHEwJCSjEMMAoGA1UE\nChMDQUxJMQ8wDQYDVQQLEwZBTElZVU4xDTALBgNVBAMTBHRlc3QxHzAdBgkqhkiG\n9w0BCQEWEHRlc3RAaG90bWFpbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJ\nAoGBAM7SS3e9+Nj0HKAsRuIDNSsS3UK6b+62YQb2uuhKrp1HMrOx61WSDR2qkAnB\ncoG00Uz38EE+9DLYNUVQBK7aSgLP5M1Ak4wr4GqGyCgjejzzh3DshUzLCCy2rook\nKOyRTlPX+Q5l7rE1fcSNzgepcae5i2sE1XXXzLRIDIvQxcspAgMBAAGjgdswgdgw\nHQYDVR0OBBYEFBdy+OuMsvbkV7R14f0OyoLoh2z4MIGoBgNVHSMEgaAwgZ2AFBdy\n+OuMsvbkV7R14f0OyoLoh2z4oXqkeDB2MQswCQYDVQQGEwJDTjELMAkGA1UECBMC\nQkoxCzAJBgNVBAcTAkJKMQwwCgYDVQQKEwNBTEkxDzANBgNVBAsTBkFMSVlVTjEN\nMAsGA1UEAxMEdGVzdDEfMB0GCSqGSIb3DQEJARYQdGVzdEBob3RtYWlsLmNvbYIJ\nAJn3ox4K13PoMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAY7KOsnyT\ncQzfhiiG7ASjiPakw5wXoycHt5GCvLG5htp2TKVzgv9QTliA3gtfv6oV4zRZx7X1\nOfi6hVgErtHaXJheuPVeW6eAW8mHBoEfvDAfU3y9waYrtUevSl07643bzKL6v+Qd\nDUBTxOAvSYfXTtI90EAxEG/bJJyOm5LqoiA=\n-----END CERTIFICATE-----"
   		private_key = "-----BEGIN RSA PRIVATE KEY-----\nMIICXAIBAAKBgQDO0kt3vfjY9BygLEbiAzUrEt1Cum/utmEG9rroSq6dRzKzsetV\nkg0dqpAJwXKBtNFM9/BBPvQy2DVFUASu2koCz+TNQJOMK+BqhsgoI3o884dw7IVM\nywgstq6KJCjskU5T1/kOZe6xNX3Ejc4HqXGnuYtrBNV118y0SAyL0MXLKQIDAQAB\nAoGAfe3NxbsGKhN42o4bGsKZPQDfeCHMxayGp5bTd10BtQIE/ST4BcJH+ihAS7Bd\n6FwQlKzivNd4GP1MckemklCXfsVckdL94e8ZbJl23GdWul3v8V+KndJHqv5zVJmP\nhwWoKimwIBTb2s0ctVryr2f18N4hhyFw1yGp0VxclGHkjgECQQD9CvllsnOwHpP4\nMdrDHbdb29QrobKyKW8pPcDd+sth+kP6Y8MnCVuAKXCKj5FeIsgVtfluPOsZjPzz\n71QQWS1dAkEA0T0KXO8gaBQwJhIoo/w6hy5JGZnrNSpOPp5xvJuMAafs2eyvmhJm\nEv9SN/Pf2VYa1z6FEnBaLOVD6hf6YQIsPQJAX/CZPoW6dzwgvimo1/GcY6eleiWE\nqygqjWhsh71e/3bz7yuEAnj5yE3t7Zshcp+dXR3xxGo0eSuLfLFxHgGxwQJAAxf8\n9DzQ5NkPkTCJi0sqbl8/03IUKTgT6hcbpWdDXa7m8J3wRr3o5nUB+TPQ5nzAbthM\nzWX931YQeACcwhxvHQJBAN5mTzzJD4w4Ma6YTaNHyXakdYfyAWrOkPIWZxfhMfXe\nDrlNdiysTI4Dd1dLeErVpjsckAaOW/JDG5PCSwkaMxk=\n-----END RSA PRIVATE KEY-----"
 	}
+
+resource "alicloud_slb_acl" "acl" {
+  count      = 3
+  name       = "${var.name}-${count.index}"
+  ip_version = "ipv4"
+}
 	`, SlbListenerCommonTestCase, name)
 }
 
--- alicloud/service_alicloud_common_test.go
+++ alicloud/service_alicloud_common_test.go
@@ -1627,76 +1627,78 @@ resource "alicloud_slb_acl" "default" {
 `
 const SlbListenerVserverCommonTestCase = `
 data "alicloud_zones" "default" {
-  available_disk_category = "cloud_efficiency"
-  available_resource_creation= "VSwitch"
-}
-
-data "alicloud_instance_types" "default" {
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
+  available_disk_category     = "cloud_efficiency"
+  available_resource_creation = "VSwitch"
 }
 
 data "alicloud_images" "default" {
-  name_regex = "^ubuntu"
+  name_regex  = "^ubuntu_[0-9]+_[0-9]+_x64*"
   most_recent = true
-  owners = "system"
+  owners      = "system"
+}
+
+data "alicloud_instance_types" "default" {
+  availability_zone    = data.alicloud_zones.default.zones.0.id
+  image_id             = data.alicloud_images.default.images.0.id
+  instance_type_family = "ecs.g6"
 }
 
 resource "alicloud_vpc" "default" {
-  vpc_name = "${var.name}"
-  cidr_block = "172.16.0.0/16"
+  vpc_name   = var.name
+  cidr_block = "192.168.0.0/16"
 }
 
 resource "alicloud_vswitch" "default" {
-  vpc_id = "${alicloud_vpc.default.id}"
-  cidr_block = "172.16.0.0/16"
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-  name = "${var.name}"
+  vswitch_name = var.name
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "192.168.192.0/24"
+  zone_id      = data.alicloud_zones.default.zones.0.id
 }
 
 resource "alicloud_security_group" "default" {
-  name = "${var.name}"
-  vpc_id = "${alicloud_vpc.default.id}"
+  name   = var.name
+  vpc_id = alicloud_vpc.default.id
 }
 
 resource "alicloud_instance" "default" {
-  image_id = "${data.alicloud_images.default.images.0.id}"
-  instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
-  instance_name = "${var.name}"
-  count = "2"
-  security_groups = "${alicloud_security_group.default.*.id}"
-  internet_charge_type = "PayByTraffic"
+  count                      = "2"
+  image_id                   = data.alicloud_images.default.images.0.id
+  instance_type              = data.alicloud_instance_types.default.instance_types.0.id
+  security_groups            = alicloud_security_group.default.*.id
+  internet_charge_type       = "PayByTraffic"
   internet_max_bandwidth_out = "10"
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-  instance_charge_type = "PostPaid"
-  system_disk_category = "cloud_efficiency"
-  vswitch_id = "${alicloud_vswitch.default.id}"
+  availability_zone          = data.alicloud_instance_types.default.instance_types.0.availability_zones.0
+  instance_charge_type       = "PostPaid"
+  system_disk_category       = "cloud_efficiency"
+  vswitch_id                 = alicloud_vswitch.default.id
+  instance_name              = var.name
 }
 
 resource "alicloud_slb_load_balancer" "default" {
-  load_balancer_name = "${var.name}"
-  vswitch_id = "${alicloud_vswitch.default.id}"
+  load_balancer_name = var.name
+  vswitch_id         = alicloud_vswitch.default.id
   load_balancer_spec = "slb.s1.small"
 }
 
 resource "alicloud_slb_server_group" "default" {
-  load_balancer_id = "${alicloud_slb_load_balancer.default.id}"
-  name = "${var.name}"
+  load_balancer_id = alicloud_slb_load_balancer.default.id
+  name             = var.name
 }
 
 resource "alicloud_slb_master_slave_server_group" "default" {
-  load_balancer_id = "${alicloud_slb_load_balancer.default.id}"
-  name = "${var.name}"
+  load_balancer_id = alicloud_slb_load_balancer.default.id
+  name             = var.name
   servers {
-      server_id = "${alicloud_instance.default.0.id}"
-      port = 80
-      weight = 100
-      server_type = "Master"
+    server_id   = alicloud_instance.default.0.id
+    port        = 80
+    weight      = 100
+    server_type = "Master"
   }
   servers {
-      server_id = "${alicloud_instance.default.1.id}"
-      port = 80
-      weight = 100
-      server_type = "Slave"
+    server_id   = alicloud_instance.default.1.id
+    port        = 80
+    weight      = 100
+    server_type = "Slave"
   }
 }
 `
