651e960c549e398cf26e60b2ad61fcc10d53ace0
MrWolong <ly01269865@alibaba-inc.com>
2025-05-21 11:30:25 +0800

    data-source/alicloud_oos_parameters: Improved alicloud_oos_parameters testcase and document

--- alicloud/data_source_alicloud_oos_parameters.go
+++ alicloud/data_source_alicloud_oos_parameters.go
@@ -12,86 +12,71 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func dataSourceAlicloudOosParameters() *schema.Resource {
+func dataSourceAliCloudOosParameters() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceAlicloudOosParametersRead,
+		Read: dataSourceAliCloudOosParametersRead,
 		Schema: map[string]*schema.Schema{
+			"ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
 			"name_regex": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.ValidateRegexp,
 				ForceNew:     true,
-			},
-			"names": {
-				Type:     schema.TypeList,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Computed: true,
+				ValidateFunc: validation.ValidateRegexp,
 			},
 			"parameter_name": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
-			"ids": {
-				Type:     schema.TypeList,
-				Optional: true,
-				ForceNew: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Computed: true,
+			"type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: StringInSlice([]string{"String", "StringList"}, false),
 			},
 			"resource_group_id": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 			},
+			"tags": tagsSchemaForceNew(),
 			"sort_field": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"Name", "CreatedDate"}, false),
+				ValidateFunc: StringInSlice([]string{"Name", "CreatedDate"}, false),
 			},
 			"sort_order": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"Ascending", "Descending"}, false),
+				ValidateFunc: StringInSlice([]string{"Ascending", "Descending"}, false),
 			},
-			"tags": tagsSchema(),
-			"type": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"String", "StringList"}, false),
+			"enable_details": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
 			},
 			"output_file": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"names": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
 			},
 			"parameters": {
 				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"constraints": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"create_time": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"created_by": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"description": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"parameter_id": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
 						"id": {
 							Type:     schema.TypeString,
 							Computed: true,
@@ -100,15 +85,27 @@ func dataSourceAlicloudOosParameters() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"parameter_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"parameter_version": {
 							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"share_type": {
+							Type:     schema.TypeString,
 							Computed: true,
 						},
 						"resource_group_id": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"share_type": {
+						"description": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -116,7 +113,19 @@ func dataSourceAlicloudOosParameters() *schema.Resource {
 							Type:     schema.TypeMap,
 							Computed: true,
 						},
-						"type": {
+						"constraints": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"created_by": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"create_time": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -128,58 +137,51 @@ func dataSourceAlicloudOosParameters() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"value": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
 					},
 				},
-			},
-			"enable_details": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
 			},
 		},
 	}
 }
 
-func dataSourceAlicloudOosParametersRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceAliCloudOosParametersRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+
 	action := "ListParameters"
 	request := make(map[string]interface{})
+	request["RegionId"] = client.RegionId
+	request["MaxResults"] = PageSizeLarge
+
 	if v, ok := d.GetOk("parameter_name"); ok {
 		request["Name"] = v
 	}
-	if v, ok := d.GetOk("resource_group_id"); ok {
-		request["ResourceGroupId"] = v
-	}
-	if v, ok := d.GetOk("sort_field"); ok {
-		request["SortField"] = v
-	}
-	if v, ok := d.GetOk("sort_order"); ok {
-		request["SortOrder"] = v
-	}
+
 	if v, ok := d.GetOk("type"); ok {
 		request["Type"] = v
 	}
+
+	if v, ok := d.GetOk("resource_group_id"); ok {
+		request["ResourceGroupId"] = v
+	}
+
+	if v, ok := d.GetOk("sort_field"); ok {
+		request["SortField"] = v
+	}
+
+	if v, ok := d.GetOk("sort_order"); ok {
+		request["SortOrder"] = v
+	}
+
 	if v, ok := d.GetOk("tags"); ok {
 		respJson, err := convertMaptoJsonString(v.(map[string]interface{}))
 		if err != nil {
 			return WrapError(err)
 		}
+
 		request["Tags"] = respJson
 	}
-	request["MaxResults"] = PageSizeLarge
+
 	var objects []map[string]interface{}
-	var parameterNameRegex *regexp.Regexp
-	if v, ok := d.GetOk("name_regex"); ok {
-		r, err := regexp.Compile(v.(string))
-		if err != nil {
-			return WrapError(err)
-		}
-		parameterNameRegex = r
-	}
 	idsMap := make(map[string]string)
 	if v, ok := d.GetOk("ids"); ok {
 		for _, vv := range v.([]interface{}) {
@@ -189,8 +191,19 @@ func dataSourceAlicloudOosParametersRead(d *schema.ResourceData, meta interface{
 			idsMap[vv.(string)] = vv.(string)
 		}
 	}
+
+	var parameterNameRegex *regexp.Regexp
+	if v, ok := d.GetOk("name_regex"); ok {
+		r, err := regexp.Compile(v.(string))
+		if err != nil {
+			return WrapError(err)
+		}
+		parameterNameRegex = r
+	}
+
 	var response map[string]interface{}
 	var err error
+
 	for {
 		wait := incrementalWait(3*time.Second, 3*time.Second)
 		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
@@ -205,79 +218,95 @@ func dataSourceAlicloudOosParametersRead(d *schema.ResourceData, meta interface{
 			return nil
 		})
 		addDebug(action, response, request)
+
 		if err != nil {
 			return WrapErrorf(err, DataDefaultErrorMsg, "alicloud_oos_parameters", action, AlibabaCloudSdkGoERROR)
 		}
+
 		resp, err := jsonpath.Get("$.Parameters", response)
 		if err != nil {
 			return WrapErrorf(err, FailedGetAttributeMsg, action, "$.Parameters", response)
 		}
+
 		result, _ := resp.([]interface{})
 		for _, v := range result {
 			item := v.(map[string]interface{})
-			if parameterNameRegex != nil && !parameterNameRegex.MatchString(fmt.Sprint(item["Name"])) {
-				continue
-			}
 			if len(idsMap) > 0 {
 				if _, ok := idsMap[fmt.Sprint(item["Name"])]; !ok {
 					continue
 				}
 			}
+
+			if parameterNameRegex != nil && !parameterNameRegex.MatchString(fmt.Sprint(item["Name"])) {
+				continue
+			}
+
 			objects = append(objects, item)
 		}
+
 		if nextToken, ok := response["NextToken"].(string); ok && nextToken != "" {
 			request["NextToken"] = nextToken
 		} else {
 			break
 		}
 	}
+
 	ids := make([]string, 0)
 	names := make([]interface{}, 0)
 	s := make([]map[string]interface{}, 0)
 	for _, object := range objects {
 		mapping := map[string]interface{}{
-			"create_time":       object["CreatedDate"],
-			"created_by":        object["CreatedBy"],
-			"description":       object["Description"],
-			"parameter_id":      object["Id"],
 			"id":                fmt.Sprint(object["Name"]),
 			"parameter_name":    fmt.Sprint(object["Name"]),
-			"parameter_version": formatInt(object["ParameterVersion"]),
-			"resource_group_id": object["ResourceGroupId"],
-			"share_type":        object["ShareType"],
+			"parameter_id":      object["Id"],
 			"type":              object["Type"],
+			"parameter_version": formatInt(object["ParameterVersion"]),
+			"share_type":        object["ShareType"],
+			"resource_group_id": object["ResourceGroupId"],
+			"description":       object["Description"],
+			"tags":              tagsToMap(object["Tags"]),
+			"created_by":        object["CreatedBy"],
+			"create_time":       object["CreatedDate"],
 			"updated_by":        object["UpdatedBy"],
 			"updated_date":      object["UpdatedDate"],
 		}
-		mapping["tags"] = tagsToMap(object["Tags"])
 
 		ids = append(ids, fmt.Sprint(mapping["id"]))
 		names = append(names, object["Name"])
+
 		if detailedEnabled := d.Get("enable_details"); !detailedEnabled.(bool) {
 			s = append(s, mapping)
 			continue
 		}
+
 		id := fmt.Sprint(object["Name"])
 		oosService := OosService{client}
-		getResp, err := oosService.DescribeOosParameter(id)
+
+		oosParameterDetail, err := oosService.DescribeOosParameter(id)
 		if err != nil {
 			return WrapError(err)
 		}
-		mapping["constraints"] = getResp["Constraints"]
-		mapping["value"] = getResp["Value"]
+
+		mapping["constraints"] = oosParameterDetail["Constraints"]
+		mapping["value"] = oosParameterDetail["Value"]
+
 		s = append(s, mapping)
 	}
 
 	d.SetId(dataResourceIdHash(ids))
+
 	if err := d.Set("ids", ids); err != nil {
 		return WrapError(err)
 	}
+
 	if err := d.Set("names", names); err != nil {
 		return WrapError(err)
 	}
+
 	if err := d.Set("parameters", s); err != nil {
 		return WrapError(err)
 	}
+
 	if output, ok := d.GetOk("output_file"); ok && output.(string) != "" {
 		writeToFile(output.(string), s)
 	}
--- website/docs/d/oos_parameters.html.markdown
+++ website/docs/d/oos_parameters.html.markdown
@@ -7,83 +7,84 @@ description: |-
   Provides a list of Oos Parameters to the user.
 ---
 
-# alicloud\_oos\_parameters
+# alicloud_oos_parameters
 
 This data source provides the Oos Parameters of the current Alibaba Cloud user.
 
--> **NOTE:** Available in v1.147.0+.
+-> **NOTE:** Available since v1.147.0.
 
 ## Example Usage
 
 Basic Usage
 
 ```terraform
-data "alicloud_oos_parameters" "ids" {
-  ids = ["my-Parameter"]
-}
-output "oos_parameter_id_1" {
-  value = data.alicloud_oos_parameters.ids.parameters.0.id
+variable "name" {
+  default = "terraform-example"
 }
 
-data "alicloud_oos_parameters" "nameRegex" {
-  name_regex = "^my-Parameter"
-}
-output "oos_parameter_id_2" {
-  value = data.alicloud_oos_parameters.nameRegex.parameters.0.id
-}
-
-data "alicloud_oos_parameters" "resourceGroupId" {
-  ids               = ["my-Parameter"]
-  resource_group_id = "example_value"
-}
-output "oos_parameter_id_3" {
-  value = data.alicloud_oos_parameters.resourceGroupId.parameters.0.id
-}
-
-data "alicloud_oos_parameters" "tags" {
-  ids = ["my-Parameter"]
+resource "alicloud_oos_parameter" "default" {
+  parameter_name = var.name
+  value          = "tf-testacc-oos_parameter"
+  type           = "String"
+  description    = var.name
+  constraints    = <<EOF
+  {
+    "AllowedValues": [
+        "tf-testacc-oos_parameter"
+    ],
+    "AllowedPattern": "tf-testacc-oos_parameter",
+    "MinLength": 1,
+    "MaxLength": 100
+  }
+  EOF
   tags = {
     Created = "TF"
-    For     = "OosParameter"
+    For     = "Parameter"
   }
 }
-output "oos_parameter_id_4" {
-  value = data.alicloud_oos_parameters.tags.parameters.0.id
+
+data "alicloud_oos_parameters" "ids" {
+  ids = [alicloud_oos_parameter.default.id]
 }
 
+output "oos_secret_parameter_id_0" {
+  value = data.alicloud_oos_parameters.ids.parameters.0.id
+}
 ```
 
 ## Argument Reference
 
 The following arguments are supported:
 
-* `enable_details` - (Optional) Default to `false`. Set it to `true` can output more details about resource attributes.
-* `ids` - (Optional, ForceNew, Computed)  A list of Parameter IDs. Its element value is same as Parameter Name.
-* `parameter_name` - (Optional, ForceNew) The name of the common parameter. You can enter a keyword to query parameter names in fuzzy match mode.
+* `ids` - (Optional, ForceNew, List) A list of Parameter IDs.
 * `name_regex` - (Optional, ForceNew) A regex string to filter results by Parameter name.
-* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
+* `parameter_name` - (Optional, ForceNew) The name of the common parameter. You can enter a keyword to query parameter names in fuzzy match mode.
+* `type` - (Optional, ForceNew) The data type of the common parameter. Valid values: `String`, `StringList`.
 * `resource_group_id` - (Optional, ForceNew) The ID of the Resource Group.
-* `type` - (Optional, ForceNew) The data type of the common parameter. Valid values: `String` and `StringList`.
 * `tags` - (Optional, ForceNew) A mapping of tags to assign to the resource.
+* `sort_field` - (Optional, ForceNew) The field used to sort the query results. Valid values: `Name`, `CreatedDate`.
+* `sort_order` - (Optional, ForceNew) The order in which the entries are sorted. Default value: `Descending`. Valid values: `Ascending`, `Descending`.
+* `enable_details` - (Optional, Bool) Whether to query the detailed list of resource attributes. Default value: `false`.
+* `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
 
-## Argument Reference
+## Attributes Reference
 
 The following attributes are exported in addition to the arguments listed above:
 
 * `names` - A list of Parameter names.
 * `parameters` - A list of Oos Parameters. Each element contains the following attributes:
-	* `constraints` - The constraints of the common parameter.
-	* `create_time` - The time when the common parameter was created.
-	* `created_by` - The user who created the common parameter.
-	* `description` - The description of the common parameter.
-	* `id` - The ID of the Parameter. Its value is same as `parameter_name`.
-	* `parameter_id` - The ID of the common parameter.
-	* `parameter_name` - The name of the common parameter.
-	* `parameter_version` - The version number of the common parameter.
-	* `resource_group_id` - The ID of the Resource Group.
-	* `share_type` - The share type of the common parameter.
-	* `tags` - The tag of the resource.
-	* `type` - The data type of the common parameter.
-	* `updated_by` - The user who updated the common parameter.
-	* `updated_date` - The time when the common parameter was updated.
-	* `value` - The value of the common parameter.
+  * `id` - The ID of the Parameter. Its value is same as `parameter_name`.
+  * `parameter_name` - The name of the common parameter.
+  * `parameter_id` - The ID of the common parameter.
+  * `type` - The data type of the common parameter.
+  * `parameter_version` - The version number of the common parameter.
+  * `share_type` - The share type of the common parameter.
+  * `resource_group_id` - The ID of the Resource Group.
+  * `description` - The description of the common parameter.
+  * `tags` - The tags added to the common parameter.
+  * `constraints` - The constraints of the common parameter. **Note:** `constraints` takes effect only if `enable_details` is set to `true`.
+  * `value` - (Available since v1.231.0) The value of the common parameter. **Note:** `value` takes effect only if `enable_details` is set to `true`.
+  * `created_by` - The user who created the common parameter.
+  * `create_time` - The time when the common parameter was created.
+  * `updated_by` - The user who updated the common parameter.
+  * `updated_date` - The time when the common parameter was updated.
