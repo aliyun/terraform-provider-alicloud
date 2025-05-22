547d47ef4f84aff869407cc8ead961850ecc6047
MrWolong <ly01269865@alibaba-inc.com>
2025-05-22 18:38:43 +0800

    New Resource: alicloud_ecs_ram_role_attachment

--- alicloud/service_alicloud_ecs_v2.go
+++ alicloud/service_alicloud_ecs_v2.go
@@ -653,3 +653,93 @@ func (s *EcsServiceV2) EcsSnapshotStateRefreshFunc(id string, field string, fail
 }
 
 // DescribeEcsSnapshot >>> Encapsulated.
+
+// DescribeEcsRamRoleAttachment <<< Encapsulated get interface for Ecs RamRoleAttachment.
+
+func (s *EcsServiceV2) DescribeEcsRamRoleAttachment(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["InstanceIds"] = fmt.Sprintf("[\"%s\"]", parts[0])
+	request["RamRoleName"] = parts[1]
+	request["RegionId"] = client.RegionId
+	action := "DescribeInstanceRamRole"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Ecs", "2014-05-26", action, query, request, true)
+
+		if err != nil {
+			if IsExpectedErrors(err, []string{"unexpected end of JSON input"}) || NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InvalidRamRole.NotFound"}) {
+			return object, WrapErrorf(NotFoundErr("RamRoleAttachment", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.InstanceRamRoleSets.InstanceRamRoleSet[*]", response)
+	if err != nil {
+		return object, WrapErrorf(NotFoundErr("RamRoleAttachment", id), NotFoundMsg, response)
+	}
+
+	if len(v.([]interface{})) == 0 {
+		return object, WrapErrorf(NotFoundErr("RamRoleAttachment", id), NotFoundMsg, response)
+	}
+
+	currentStatus := v.([]interface{})[0].(map[string]interface{})["RamRoleName"]
+	if currentStatus == nil {
+		return object, WrapErrorf(NotFoundErr("RamRoleAttachment", id), NotFoundMsg, response)
+	}
+
+	object = v.([]interface{})[0].(map[string]interface{})
+	object["RegionId"] = response["RegionId"]
+
+	return object, nil
+}
+
+func (s *EcsServiceV2) EcsRamRoleAttachmentStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeEcsRamRoleAttachment(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeEcsRamRoleAttachment >>> Encapsulated.
