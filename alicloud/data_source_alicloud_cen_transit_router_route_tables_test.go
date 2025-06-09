d10e680bc0a7e553b3cbc181145806545497543b
MrWolong <ly01269865@alibaba-inc.com>
2025-06-09 13:24:36 +0800

    data-source/alicloud_cen_transit_router_route_tables: Fixed transit_router_route_table_ids, transit_router_route_table_names invalid error

--- alicloud/data_source_alicloud_cen_transit_router_route_tables_test.go
+++ alicloud/data_source_alicloud_cen_transit_router_route_tables_test.go
@@ -10,133 +10,162 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 )
 
-func TestAccAlicloudCenTransitRouterRouteTablesDataSource(t *testing.T) {
+func TestAccAliCloudCenTransitRouterRouteTablesDataSource_basic0(t *testing.T) {
 	rand := acctest.RandInt()
+
 	idsConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"ids": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
 		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"ids": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}_fake"]`,
 		}),
 	}
-	transitRouterRouteTableStatusConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":                               `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
-			"transit_router_route_table_status": `"Active"`,
-		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":                               `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
-			"transit_router_route_table_status": `"Creating"`,
-		}),
-	}
+
 	nameRegexConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":        `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"name_regex": `"${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}"`,
 		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":        `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"name_regex": `"${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}_fake"`,
 		}),
 	}
+
+	transitRouterRouteTableTypeConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_type": `"Custom"`,
+		}),
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"ids":                             `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}_fake"]`,
+			"transit_router_route_table_type": `"Custom"`,
+		}),
+	}
+
+	transitRouterRouteTableStatusConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"ids":                               `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
+			"transit_router_route_table_status": `"Active"`,
+		}),
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_status": `"Creating"`,
+		}),
+	}
+
 	statusConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"ids":    `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
 			"status": `"Active"`,
 		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":    `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"status": `"Creating"`,
 		}),
 	}
-	typeConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"transit_router_route_table_type": `"System"`,
+
+	transitRouterRouteTableIdsConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_ids": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
 		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
-			"ids":                             `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
-			"transit_router_route_table_type": `"System"`,
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_ids": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}_fake"]`,
 		}),
 	}
+
+	transitRouterRouteTableNamesConf := dataSourceTestAccConfig{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_names": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}"]`,
+		}),
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+			"transit_router_route_table_names": `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}_fake"]`,
+		}),
+	}
+
 	allConf := dataSourceTestAccConfig{
-		existConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+		existConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"ids":                               `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
 			"name_regex":                        `"${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}"`,
+			"transit_router_route_table_type":   `"Custom"`,
+			"transit_router_route_table_status": `"Active"`,
 			"status":                            `"Active"`,
 			"transit_router_route_table_ids":    `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}"]`,
 			"transit_router_route_table_names":  `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}"]`,
-			"transit_router_route_table_status": `"Active"`,
-			"transit_router_route_table_type":   `"Custom"`,
 		}),
-		fakeConfig: testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
+		fakeConfig: testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand, map[string]string{
 			"ids":                               `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}_fake"]`,
 			"name_regex":                        `"${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}_fake"`,
+			"transit_router_route_table_type":   `"System"`,
+			"transit_router_route_table_status": `"Creating"`,
 			"status":                            `"Creating"`,
 			"transit_router_route_table_ids":    `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_id}_fake"]`,
 			"transit_router_route_table_names":  `["${alicloud_cen_transit_router_route_table.default.transit_router_route_table_name}_fake"]`,
-			"transit_router_route_table_status": `"Creating"`,
-			"transit_router_route_table_type":   `"System"`,
 		}),
 	}
-	var existAlicloudCenTransitRouterRouteTablesDataSourceNameMapFunc = func(rand int) map[string]string {
+
+	var existAliCloudCenTransitRouterRouteTablesDataSourceNameMapFunc = func(rand int) map[string]string {
 		return map[string]string{
 			"ids.#":                                  "1",
 			"names.#":                                "1",
 			"tables.#":                               "1",
+			"tables.0.id":                            CHECKSET,
 			"tables.0.transit_router_route_table_id": CHECKSET,
-			"tables.0.transit_router_route_table_type": CHECKSET,
+			"tables.0.transit_router_route_table_type":        CHECKSET,
+			"tables.0.transit_router_route_table_name":        CHECKSET,
+			"tables.0.transit_router_route_table_description": CHECKSET,
+			"tables.0.status": CHECKSET,
 		}
 	}
-	var fakeAlicloudCenTransitRouterRouteTablesDataSourceNameMapFunc = func(rand int) map[string]string {
+
+	var fakeAliCloudCenTransitRouterRouteTablesDataSourceNameMapFunc = func(rand int) map[string]string {
 		return map[string]string{
-			"ids.#":   "0",
-			"names.#": "0",
+			"ids.#":    "0",
+			"names.#":  "0",
+			"tables.#": "0",
 		}
 	}
-	var alicloudCenTransitRouterRouteTablesCheckInfo = dataSourceAttr{
+
+	var aliCloudCenTransitRouterRouteTablesCheckInfo = dataSourceAttr{
 		resourceId:   "data.alicloud_cen_transit_router_route_tables.default",
-		existMapFunc: existAlicloudCenTransitRouterRouteTablesDataSourceNameMapFunc,
-		fakeMapFunc:  fakeAlicloudCenTransitRouterRouteTablesDataSourceNameMapFunc,
+		existMapFunc: existAliCloudCenTransitRouterRouteTablesDataSourceNameMapFunc,
+		fakeMapFunc:  fakeAliCloudCenTransitRouterRouteTablesDataSourceNameMapFunc,
 	}
+
 	preCheck := func() {
 		testAccPreCheckWithRegions(t, true, connectivity.CenTRSupportRegions)
 	}
-	alicloudCenTransitRouterRouteTablesCheckInfo.dataSourceTestCheckWithPreCheck(t, rand, preCheck, idsConf, transitRouterRouteTableStatusConf, nameRegexConf, statusConf, typeConf, allConf)
+
+	aliCloudCenTransitRouterRouteTablesCheckInfo.dataSourceTestCheckWithPreCheck(t, rand, preCheck, idsConf, nameRegexConf, transitRouterRouteTableTypeConf, transitRouterRouteTableStatusConf, statusConf, transitRouterRouteTableIdsConf, transitRouterRouteTableNamesConf, allConf)
 }
-func testAccCheckAlicloudCenTransitRouterRouteTablesDataSourceName(rand int, attrMap map[string]string) string {
+
+func testAccCheckAliCloudCenTransitRouterRouteTablesDataSourceName(rand int, attrMap map[string]string) string {
 	var pairs []string
 	for k, v := range attrMap {
 		pairs = append(pairs, k+" = "+v)
 	}
 
 	config := fmt.Sprintf(`
+	variable "name" {
+  		default = "tf-testAccDataTransitRouterRouteTable-%d"
+	}
 
-variable "name" {	
-	default = "tf-testAccDataTransitRouterRouteTable-%d"
-}
+	resource "alicloud_cen_instance" "default" {
+  		cen_instance_name = var.name
+  		protection_level  = "REDUCED"
+	}
 
-resource "alicloud_cen_instance" "default" {
-  cen_instance_name = var.name
-  protection_level = "REDUCED"
-}
+	resource "alicloud_cen_transit_router" "default" {
+  		cen_id              = alicloud_cen_instance.default.id
+  		transit_router_name = var.name
+	}
 
-resource "alicloud_cen_transit_router" "default" {
-cen_id= alicloud_cen_instance.default.id
-transit_router_name = var.name
-}
+	resource "alicloud_cen_transit_router_route_table" "default" {
+  		transit_router_id                      = alicloud_cen_transit_router.default.transit_router_id
+  		transit_router_route_table_name        = var.name
+  		transit_router_route_table_description = var.name
+	}
 
-resource "alicloud_cen_transit_router_route_table" "default" {
-transit_router_id = alicloud_cen_transit_router.default.transit_router_id
-transit_router_route_table_description = "desp"
-transit_router_route_table_name = var.name
-depends_on = [alicloud_cen_transit_router.default]
-}
-
-data "alicloud_cen_transit_router_route_tables" "default" {	
-transit_router_id = alicloud_cen_transit_router.default.transit_router_id
-	%s
-}
+	data "alicloud_cen_transit_router_route_tables" "default" {
+  		transit_router_id = alicloud_cen_transit_router.default.transit_router_id
+		%s
+	}
 `, rand, strings.Join(pairs, " \n "))
 	return config
 }
--- website/docs/d/cen_transit_router_route_tables.html.markdown
+++ website/docs/d/cen_transit_router_route_tables.html.markdown
@@ -4,21 +4,24 @@ layout: "alicloud"
 page_title: "Alicloud: alicloud_cen_transit_router_route_tables"
 sidebar_current: "docs-alicloud-datasource-cen-transit-router-route-tables"
 description: |-
-  Provides a list of CEN Transit Router Route Tables owned by an Alibaba Cloud account.
+  Provides a list of CEN Transit Router Route Tables to the user.
 ---
 
 # alicloud_cen_transit_router_route_tables
 
-This data source provides CEN Transit Router Route Tables available to the user.[What is Cen Transit Router Route Tables](https://www.alibabacloud.com/help/en/cen/developer-reference/api-cbn-2017-09-12-listtransitrouterroutetables)
+This data source provides the CEN Transit Router Route Tables of the current Alibaba Cloud user.
 
 -> **NOTE:** Available since v1.126.0.
 
 ## Example Usage
 
+Basic Usage
+
 ```terraform
 variable "name" {
-  default = "tf-example"
+  default = "terraform-example"
 }
+
 resource "alicloud_cen_instance" "default" {
   cen_instance_name = var.name
   protection_level  = "REDUCED"
@@ -31,16 +34,17 @@ resource "alicloud_cen_transit_router" "default" {
 
 resource "alicloud_cen_transit_router_route_table" "default" {
   transit_router_id                      = alicloud_cen_transit_router.default.transit_router_id
-  transit_router_route_table_description = "desp"
+  transit_router_route_table_description = var.name
   transit_router_route_table_name        = var.name
 }
 
-data "alicloud_cen_transit_router_route_tables" "default" {
-  transit_router_id = alicloud_cen_transit_router.default.transit_router_id
+data "alicloud_cen_transit_router_route_tables" "ids" {
+  transit_router_id = alicloud_cen_transit_router_route_table.default.transit_router_id
+  ids               = [alicloud_cen_transit_router_route_table.default.transit_router_route_table_id]
 }
 
-output "first_transit_router_route_table_type" {
-  value = data.alicloud_cen_transit_router_route_tables.default.tables.0.transit_router_route_table_type
+output "cen_transit_router_route_table_id_0" {
+  value = data.alicloud_cen_transit_router_route_tables.ids.tables.0.id
 }
 ```
 
@@ -48,26 +52,25 @@ output "first_transit_router_route_table_type" {
 
 The following arguments are supported:
 
-* `transit_router_id` - (Required, ForceNew) ID of the CEN Transit Router Route Table.
-* `transit_router_route_table_names` - (Optional, ForceNew) A list of name of the CEN Transit Router Route Table.  
-* `transit_router_route_table_ids` - (Optional, ForceNew) A list of ID of the CEN Transit Router Route Table.
-* `transit_router_route_table_type` - (Optional, ForceNew, Available since v1.229.1.) The type of the transit router route table to query. Valid values `System` and `Custom`.
-* `transit_router_route_table_status` - (Optional, ForceNew) The status of the transit router route table to query. Valid values `Creating`, `Active` and `Deleting`..
+* `ids` - (Optional, ForceNew, List) A list of Transit Router Route Table IDs.
+* `name_regex` - (Optional, ForceNew) A regex string to filter results by Transit Router Route Table name.
+* `transit_router_id` - (Required, ForceNew) The ID of the Enterprise Edition transit router.
+* `transit_router_route_table_type` - (Optional, ForceNew, Available since v1.229.1.) The type of the route table. Valid values: `System`, `Custom`.
+* `transit_router_route_table_status` - (Optional, ForceNew) The status of the route table. Valid values: `Creating`, `Active`, `Deleting`.
+* `status` - (Optional, ForceNew) The status of the route table. Valid values: `Creating`, `Active`, `Deleting`.
+* `transit_router_route_table_ids` - (Optional, ForceNew, List) A list of ID of the CEN Transit Router Route Table.
+* `transit_router_route_table_names` - (Optional, ForceNew, List) A list of name of the CEN Transit Router Route Table.
 * `output_file` - (Optional) File name where to save data source results (after running `terraform plan`).
-* `name_regex` - (Optional, ForceNew) A regex string to filter CEN Transit Router Route Table by name.
-* `ids` - (Optional, ForceNew) A list of CEN Transit Router Route Table IDs.
-* `status` - (Optional, ForceNew) The status of the transit router route table to query. Valid values `Creating`, `Active` and `Deleting`..
 
 ## Attributes Reference
 
 The following attributes are exported in addition to the arguments listed above:
 
-
-* `names` - A list of name of CEN Transit Router Route Tables.
-* `tables` - A list of CEN Route Entries. Each element contains the following attributes:
-    * `status` - The status of the route table.
-    * `transit_router_route_table_description` - The description of the transit router route table.
-    * `id` - ID of resource.
-    * `transit_router_route_table_id` - ID of the trabsit router route table.
-    * `transit_router_route_table_name` - Name of the transit router route table.  
-    * `transit_router_route_table_type` - Type of the transit router route table.
+* `names` - A list of Transit Router Route Table names.
+* `tables` - A list of Transit Router Route Tables. Each element contains the following attributes:
+  * `id` - The ID of the Transit Router Route Table.
+  * `transit_router_route_table_id` - The ID of the Transit Router Route Table.
+  * `transit_router_route_table_type` - The type of the route table.
+  * `transit_router_route_table_name` - The name of the route table.
+  * `transit_router_route_table_description` - The description of the route table.
+  * `status` - The status of the route table.
