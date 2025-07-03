c3ea4c5b71aa48abaa11a7a615d19bbd818870c0
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-07-03 10:59:17 +0800

    New Resource: alicloud_esa_version.

---
+++ alicloud/resource_alicloud_esa_version.go
@@ -0,0 +1,204 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
+package alicloud
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAliCloudEsaVersion() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAliCloudEsaVersionCreate,
+		Read:   resourceAliCloudEsaVersionRead,
+		Update: resourceAliCloudEsaVersionUpdate,
+		Delete: resourceAliCloudEsaVersionDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"site_id": {
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"origin_version": {
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"site_version": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAliCloudEsaVersionCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CloneVersion"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOkExists("origin_version"); ok {
+		request["SiteVersion"] = v
+	}
+	if v, ok := d.GetOk("site_id"); ok {
+		request["SiteId"] = v
+	}
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_esa_version", action, AlibabaCloudSdkGoERROR)
+	}
+
+	d.SetId(fmt.Sprintf("%v:%v", request["SiteId"], response["CloneVersion"]))
+
+	return resourceAliCloudEsaVersionUpdate(d, meta)
+}
+
+func resourceAliCloudEsaVersionRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	esaServiceV2 := EsaServiceV2{client}
+
+	objectRaw, err := esaServiceV2.DescribeEsaVersion(d.Id())
+	if err != nil {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_esa_version DescribeEsaVersion Failed!!! %s", err)
+			d.SetId("")
+			return nil
+		}
+		return WrapError(err)
+	}
+
+	d.Set("create_time", objectRaw["CreateTime"])
+	d.Set("description", objectRaw["Description"])
+	d.Set("status", objectRaw["Status"])
+	d.Set("site_version", objectRaw["SiteVersion"])
+
+	parts := strings.Split(d.Id(), ":")
+	d.Set("site_id", formatInt(parts[0]))
+
+	return nil
+}
+
+func resourceAliCloudEsaVersionUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
+
+	var err error
+	parts := strings.Split(d.Id(), ":")
+	action := "UpdateVersionDesc"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["SiteVersion"] = parts[1]
+	request["SiteId"] = parts[0]
+	request["RegionId"] = client.RegionId
+	if d.HasChange("description") {
+		update = true
+	}
+	request["Description"] = d.Get("description")
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+	}
+
+	return resourceAliCloudEsaVersionRead(d, meta)
+}
+
+func resourceAliCloudEsaVersionDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "DeleteVersion"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["SiteVersion"] = parts[1]
+	request["SiteId"] = parts[0]
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("ESA", "2024-09-10", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if NotFoundError(err) {
+			return nil
+		}
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+	}
+
+	return nil
+}
---
+++ website/docs/r/esa_version.html.markdown
@@ -0,0 +1,75 @@
+---
+subcategory: "ESA"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_esa_version"
+description: |-
+  Provides a Alicloud ESA Site Version resource.
+---
+
+# alicloud_esa_version
+
+Provides a ESA Site Version resource.
+
+
+
+For information about ESA Site Version and how to use it, see [What is Site Version](https://next.api.alibabacloud.com/document/ESA/2024-09-10/CloneVersion).
+
+-> **NOTE:** Available since v1.251.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "bcd72239.com"
+}
+
+data "alicloud_esa_sites" "default" {
+  plan_subscribe_type = "enterpriseplan"
+}
+
+resource "alicloud_esa_site" "default" {
+  site_name          = var.name
+  instance_id        = data.alicloud_esa_sites.default.sites.0.instance_id
+  coverage           = "overseas"
+  access_type        = "NS"
+  version_management = true
+}
+
+resource "alicloud_esa_version" "default" {
+  site_id        = alicloud_esa_site.default.id
+  description    = "example"
+  origin_version = "0"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `description` - (Optional) The Site version's description.
+* `site_id` - (Required, ForceNew, Int) The site ID, which can be obtained by calling the ListSites API.
+* `origin_version` - (Required, ForceNew, Int) The version number of the site configuration. For sites that have enabled configuration version management, this parameter can be used to specify the effective version of the configuration site, which defaults to version 0.
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<site_id>:<site_version>`.
+* `site_version` - The version number of the site configuration.
+* `create_time` - The creation time. The date format follows ISO8601 notation and uses UTC time. The format is yyyy-MM-ddTHH:mm:ssZ.
+* `status` - Site version status:：`online`.：`configuring`._faild`：`configure_faild`.
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the Site Version.
+* `delete` - (Defaults to 5 mins) Used when delete the Site Version.
+* `update` - (Defaults to 5 mins) Used when update the Site Version.
+
+## Import
+
+ESA Site Version can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_esa_version.example <site_id>:<site_version>
+```
