6705f6e60210a3e2454bd6f28ee4b8d06146dd31
MrWolong <ly01269865@alibaba-inc.com>
2025-07-08 17:26:08 +0800

    client: Supported proxy for old clients

--- alicloud/connectivity/client_test.go
+++ alicloud/connectivity/client_test.go
@@ -1,12 +1,16 @@
 package connectivity
 
 import (
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
-	"github.com/aliyun/credentials-go/credentials"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"sync"
 	"testing"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ram"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/vpc"
+	"github.com/aliyun/credentials-go/credentials"
+	"github.com/stretchr/testify/assert"
 )
 
 var endpointMap sync.Map
@@ -50,26 +54,174 @@ func NewTestClient(t *testing.T) *AliyunClient {
 }
 
 func TestUnitCommonWithEcsClient_UsingHttpMock(t *testing.T) {
-	var accessKey, secretKey string
-	accessKey = os.Getenv("ALICLOUD_ACCESS_KEY")
-	secretKey = os.Getenv("ALICLOUD_SECRET_KEY")
-	config := &Config{
-		Region:      Beijing,
-		RegionId:    "cn-beijing",
-		AccessKey:   accessKey,
-		SecretKey:   secretKey,
-		Protocol:    "HTTPS",
-		Endpoints:   &endpointMap,
-		SignVersion: &signVersion,
-	}
-	credentialConfig := new(credentials.Config).SetType("access_key").SetAccessKeyId(accessKey).SetAccessKeySecret(secretKey)
-	credential, _ := credentials.NewCredential(credentialConfig)
-	config.Credential = credential
 	client := NewTestClient(t)
 
 	res, _ := client.WithEcsClient(func(c *ecs.Client) (interface{}, error) {
 		req := ecs.CreateDescribeInstancesRequest()
 		return c.DescribeInstances(req)
 	})
+
 	assert.NotNil(t, res)
+}
+
+func TestUnitCommonWithEcsClient_Proxy(t *testing.T) {
+	client := NewTestClient(t)
+
+	testCases := []struct {
+		name          string
+		proxyURL      string
+		skipProxy     bool
+		expectedHTTP  string
+		expectedHTTPS string
+	}{
+		{
+			name:         "HTTP协议代理设置",
+			proxyURL:     "http://proxy.example.com:8080",
+			expectedHTTP: "http://proxy.example.com:8080",
+		},
+		{
+			name:          "HTTPS协议代理设置",
+			proxyURL:      "https://proxy.example.com:8443",
+			expectedHTTPS: "https://proxy.example.com:8443",
+		},
+		{
+			name: "无代理设置",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ecsconn, err := ecs.NewClientWithOptions(client.config.RegionId, client.getSdkConfig(time.Duration(60)*time.Second), client.config.getAuthCredential(true))
+			assert.NoError(t, err)
+
+			if tc.expectedHTTP != "" {
+				client.config.Protocol = "HTTP"
+				t.Setenv("HTTP_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				ecsconn.SetHttpProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTP, ecsconn.GetHttpProxy())
+				assert.Empty(t, ecsconn.GetHttpsProxy())
+			} else if tc.expectedHTTPS != "" {
+				client.config.Protocol = "HTTPS"
+				t.Setenv("HTTPS_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				ecsconn.SetHttpsProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTPS, ecsconn.GetHttpsProxy())
+				assert.Empty(t, ecsconn.GetHttpProxy())
+			} else {
+				assert.Empty(t, ecsconn.GetHttpProxy())
+				assert.Empty(t, ecsconn.GetHttpsProxy())
+			}
+		})
+	}
+}
+
+func TestUnitCommonWithVpcClient_Proxy(t *testing.T) {
+	client := NewTestClient(t)
+
+	testCases := []struct {
+		name          string
+		proxyURL      string
+		skipProxy     bool
+		expectedHTTP  string
+		expectedHTTPS string
+	}{
+		{
+			name:         "HTTP协议代理设置",
+			proxyURL:     "http://proxy.example.com:8080",
+			expectedHTTP: "http://proxy.example.com:8080",
+		},
+		{
+			name:          "HTTPS协议代理设置",
+			proxyURL:      "https://proxy.example.com:8443",
+			expectedHTTPS: "https://proxy.example.com:8443",
+		},
+		{
+			name: "无代理设置",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			vpcconn, err := vpc.NewClientWithOptions(client.config.RegionId, client.getSdkConfig(time.Duration(60)*time.Second), client.config.getAuthCredential(true))
+			assert.NoError(t, err)
+
+			if tc.expectedHTTP != "" {
+				client.config.Protocol = "HTTP"
+				t.Setenv("HTTP_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				vpcconn.SetHttpProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTP, vpcconn.GetHttpProxy())
+				assert.Empty(t, vpcconn.GetHttpsProxy())
+			} else if tc.expectedHTTPS != "" {
+				client.config.Protocol = "HTTPS"
+				t.Setenv("HTTPS_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				vpcconn.SetHttpsProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTPS, vpcconn.GetHttpsProxy())
+				assert.Empty(t, vpcconn.GetHttpProxy())
+			} else {
+				assert.Empty(t, vpcconn.GetHttpProxy())
+				assert.Empty(t, vpcconn.GetHttpsProxy())
+			}
+		})
+	}
+}
+
+func TestUnitCommonWithRamClient_Proxy(t *testing.T) {
+	client := NewTestClient(t)
+
+	testCases := []struct {
+		name          string
+		proxyURL      string
+		skipProxy     bool
+		expectedHTTP  string
+		expectedHTTPS string
+	}{
+		{
+			name:         "HTTP协议代理设置",
+			proxyURL:     "http://proxy.example.com:8080",
+			expectedHTTP: "http://proxy.example.com:8080",
+		},
+		{
+			name:          "HTTPS协议代理设置",
+			proxyURL:      "https://proxy.example.com:8443",
+			expectedHTTPS: "https://proxy.example.com:8443",
+		},
+		{
+			name: "无代理设置",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ramconn, err := ram.NewClientWithOptions(client.config.RegionId, client.getSdkConfig(0), client.config.getAuthCredential(true))
+			assert.NoError(t, err)
+
+			if tc.expectedHTTP != "" {
+				client.config.Protocol = "HTTP"
+				t.Setenv("HTTP_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				ramconn.SetHttpProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTP, ramconn.GetHttpProxy())
+				assert.Empty(t, ramconn.GetHttpsProxy())
+			} else if tc.expectedHTTPS != "" {
+				client.config.Protocol = "HTTPS"
+				t.Setenv("HTTPS_PROXY", tc.proxyURL)
+				proxy, err := client.getHttpProxy()
+				ramconn.SetHttpsProxy(proxy.String())
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedHTTPS, ramconn.GetHttpsProxy())
+				assert.Empty(t, ramconn.GetHttpProxy())
+			} else {
+				assert.Empty(t, ramconn.GetHttpProxy())
+				assert.Empty(t, ramconn.GetHttpsProxy())
+			}
+		})
+	}
 }
