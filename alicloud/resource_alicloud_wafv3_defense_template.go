334acf90fc13a691ea3117ef18c1b07f876fbe39
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-08-05 17:03:01 +0800

    resource/alicloud_wafv3_defense_template: Add new attribute resources.

--- alicloud/resource_alicloud_wafv3_defense_template.go
+++ alicloud/resource_alicloud_wafv3_defense_template.go
@@ -53,6 +53,11 @@ func resourceAliCloudWafv3DefenseTemplate() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"resources": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
 			"status": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -63,9 +68,10 @@ func resourceAliCloudWafv3DefenseTemplate() *schema.Resource {
 				ForceNew: true,
 			},
 			"template_type": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: StringInSlice([]string{"user_default", "user_custom"}, false),
 			},
 		},
 	}
@@ -81,24 +87,22 @@ func resourceAliCloudWafv3DefenseTemplateCreate(d *schema.ResourceData, meta int
 	query := make(map[string]interface{})
 	var err error
 	request = make(map[string]interface{})
-	query["InstanceId"] = d.Get("instance_id")
+	if v, ok := d.GetOk("instance_id"); ok {
+		request["InstanceId"] = v
+	}
 	request["RegionId"] = client.RegionId
 
-	request["DefenseScene"] = d.Get("defense_scene")
-	request["TemplateOrigin"] = d.Get("template_origin")
+	request["TemplateName"] = d.Get("defense_template_name")
 	request["TemplateType"] = d.Get("template_type")
 	if v, ok := d.GetOk("description"); ok {
 		request["Description"] = v
 	}
-	request["TemplateName"] = d.Get("defense_template_name")
-	if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
-		request["ResourceManagerResourceGroupId"] = v
-	}
+	request["DefenseScene"] = d.Get("defense_scene")
 	request["TemplateStatus"] = d.Get("status")
+	request["TemplateOrigin"] = d.Get("template_origin")
 	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, false)
-
+		response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -106,17 +110,17 @@ func resourceAliCloudWafv3DefenseTemplateCreate(d *schema.ResourceData, meta int
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
 
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_wafv3_defense_template", action, AlibabaCloudSdkGoERROR)
 	}
 
-	d.SetId(fmt.Sprintf("%v:%v", query["InstanceId"], response["TemplateId"]))
+	d.SetId(fmt.Sprintf("%v:%v", request["InstanceId"], response["TemplateId"]))
 
-	return resourceAliCloudWafv3DefenseTemplateRead(d, meta)
+	return resourceAliCloudWafv3DefenseTemplateUpdate(d, meta)
 }
 
 func resourceAliCloudWafv3DefenseTemplateRead(d *schema.ResourceData, meta interface{}) error {
@@ -141,9 +145,22 @@ func resourceAliCloudWafv3DefenseTemplateRead(d *schema.ResourceData, meta inter
 	d.Set("template_type", objectRaw["TemplateType"])
 	d.Set("defense_template_id", objectRaw["TemplateId"])
 
+	objectRaw, err = wafv3ServiceV2.DescribeDefenseTemplateDescribeTemplateResources(d.Id())
+	if err != nil && !NotFoundError(err) {
+		return WrapError(err)
+	}
+
+	d.Set("defense_template_id", objectRaw["TemplateId"])
+
+	resourcesRaw := make([]interface{}, 0)
+	if objectRaw["Resources"] != nil {
+		resourcesRaw = objectRaw["Resources"].([]interface{})
+	}
+
+	d.Set("resources", resourcesRaw)
+
 	parts := strings.Split(d.Id(), ":")
 	d.Set("instance_id", parts[0])
-	d.Set("defense_template_id", parts[1])
 
 	return nil
 }
@@ -155,19 +172,20 @@ func resourceAliCloudWafv3DefenseTemplateUpdate(d *schema.ResourceData, meta int
 	var query map[string]interface{}
 	update := false
 	d.Partial(true)
+
+	var err error
 	parts := strings.Split(d.Id(), ":")
 	action := "ModifyDefenseTemplate"
-	var err error
 	request = make(map[string]interface{})
 	query = make(map[string]interface{})
-	query["TemplateId"] = parts[1]
-	query["InstanceId"] = parts[0]
+	request["InstanceId"] = parts[0]
+	request["TemplateId"] = parts[1]
 	request["RegionId"] = client.RegionId
-	if d.HasChange("defense_template_name") {
+	if !d.IsNewResource() && d.HasChange("defense_template_name") {
 		update = true
 	}
 	request["TemplateName"] = d.Get("defense_template_name")
-	if d.HasChange("description") {
+	if !d.IsNewResource() && d.HasChange("description") {
 		update = true
 		request["Description"] = d.Get("description")
 	}
@@ -178,8 +196,7 @@ func resourceAliCloudWafv3DefenseTemplateUpdate(d *schema.ResourceData, meta int
 	if update {
 		wait := incrementalWait(3*time.Second, 5*time.Second)
 		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
-			response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, false)
-
+			response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -187,35 +204,32 @@ func resourceAliCloudWafv3DefenseTemplateUpdate(d *schema.ResourceData, meta int
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, response, request)
 			return nil
 		})
+		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
-		d.SetPartial("defense_template_name")
-		d.SetPartial("description")
 	}
 	update = false
 	parts = strings.Split(d.Id(), ":")
 	action = "ModifyDefenseTemplateStatus"
 	request = make(map[string]interface{})
 	query = make(map[string]interface{})
-	query["InstanceId"] = parts[0]
-	query["TemplateId"] = parts[1]
+	request["InstanceId"] = parts[0]
+	request["TemplateId"] = parts[1]
 	request["RegionId"] = client.RegionId
-	if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
-		request["ResourceManagerResourceGroupId"] = v
-	}
-	if d.HasChange("status") {
+	if !d.IsNewResource() && d.HasChange("status") {
 		update = true
 	}
 	request["TemplateStatus"] = d.Get("status")
+	if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
+		request["ResourceManagerResourceGroupId"] = v
+	}
 	if update {
 		wait := incrementalWait(3*time.Second, 5*time.Second)
 		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
-			response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, false)
-
+			response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -223,15 +237,166 @@ func resourceAliCloudWafv3DefenseTemplateUpdate(d *schema.ResourceData, meta int
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, response, request)
 			return nil
 		})
+		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
-		d.SetPartial("status")
 	}
 
+	if d.HasChange("resource_groups") {
+		oldEntry, newEntry := d.GetChange("resource_groups")
+		oldEntrySet := oldEntry.(*schema.Set)
+		newEntrySet := newEntry.(*schema.Set)
+		removed := oldEntrySet.Difference(newEntrySet)
+		added := newEntrySet.Difference(oldEntrySet)
+
+		if removed.Len() > 0 {
+			parts := strings.Split(d.Id(), ":")
+			action := "ModifyTemplateResources"
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["InstanceId"] = parts[0]
+			request["TemplateId"] = parts[1]
+			request["RegionId"] = client.RegionId
+			localData := removed.List()
+			unbindResourceGroupsMapsArray := localData
+			request["UnbindResourceGroups"] = unbindResourceGroupsMapsArray
+
+			if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
+				request["ResourceManagerResourceGroupId"] = v
+			}
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+		if added.Len() > 0 {
+			parts := strings.Split(d.Id(), ":")
+			action := "ModifyTemplateResources"
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["InstanceId"] = parts[0]
+			request["TemplateId"] = parts[1]
+			request["RegionId"] = client.RegionId
+			localData := added.List()
+			bindResourceGroupsMapsArray := localData
+			request["BindResourceGroups"] = bindResourceGroupsMapsArray
+
+			if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
+				request["ResourceManagerResourceGroupId"] = v
+			}
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+	}
+	if d.HasChange("resources") {
+		oldEntry, newEntry := d.GetChange("resources")
+		oldEntrySet := oldEntry.(*schema.Set)
+		newEntrySet := newEntry.(*schema.Set)
+		removed := oldEntrySet.Difference(newEntrySet)
+		added := newEntrySet.Difference(oldEntrySet)
+
+		if removed.Len() > 0 {
+			parts := strings.Split(d.Id(), ":")
+			action := "ModifyTemplateResources"
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["InstanceId"] = parts[0]
+			request["TemplateId"] = parts[1]
+			request["RegionId"] = client.RegionId
+			localData := removed.List()
+			unbindResourcesMapsArray := localData
+			request["UnbindResources"] = unbindResourcesMapsArray
+
+			if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
+				request["ResourceManagerResourceGroupId"] = v
+			}
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+		if added.Len() > 0 {
+			parts := strings.Split(d.Id(), ":")
+			action := "ModifyTemplateResources"
+			request = make(map[string]interface{})
+			query = make(map[string]interface{})
+			request["InstanceId"] = parts[0]
+			request["TemplateId"] = parts[1]
+			request["RegionId"] = client.RegionId
+			localData := added.List()
+			bindResourcesMapsArray := localData
+			request["BindResources"] = bindResourcesMapsArray
+
+			if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
+				request["ResourceManagerResourceGroupId"] = v
+			}
+			wait := incrementalWait(3*time.Second, 5*time.Second)
+			err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+				response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
+				if err != nil {
+					if NeedRetry(err) {
+						wait()
+						return resource.RetryableError(err)
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			addDebug(action, response, request)
+			if err != nil {
+				return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+			}
+
+		}
+
+	}
 	d.Partial(false)
 	return resourceAliCloudWafv3DefenseTemplateRead(d, meta)
 }
@@ -246,8 +411,8 @@ func resourceAliCloudWafv3DefenseTemplateDelete(d *schema.ResourceData, meta int
 	query := make(map[string]interface{})
 	var err error
 	request = make(map[string]interface{})
-	query["InstanceId"] = parts[0]
-	query["TemplateId"] = parts[1]
+	request["InstanceId"] = parts[0]
+	request["TemplateId"] = parts[1]
 	request["RegionId"] = client.RegionId
 
 	if v, ok := d.GetOk("resource_manager_resource_group_id"); ok {
@@ -255,7 +420,7 @@ func resourceAliCloudWafv3DefenseTemplateDelete(d *schema.ResourceData, meta int
 	}
 	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
-		response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, false)
+		response, err = client.RpcPost("waf-openapi", "2021-10-01", action, query, request, true)
 
 		if err != nil {
 			if NeedRetry(err) {
@@ -264,11 +429,14 @@ func resourceAliCloudWafv3DefenseTemplateDelete(d *schema.ResourceData, meta int
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
 
 	if err != nil {
+		if IsExpectedErrors(err, []string{"Defense.Control.DefenseTemplateNotExist"}) || NotFoundError(err) {
+			return nil
+		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
 
