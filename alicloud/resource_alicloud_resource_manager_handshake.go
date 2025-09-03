f2b42dcf6925124e6e96db93463678d23a0421ff
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2025-09-03 16:40:33 +0800

    New Resource: alicloud_resource_manager_handshake;

--- alicloud/resource_alicloud_resource_manager_handshake.go
+++ alicloud/resource_alicloud_resource_manager_handshake.go
@@ -1,3 +1,4 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
@@ -5,21 +6,29 @@ import (
 	"log"
 	"time"
 
+	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudResourceManagerHandshake() *schema.Resource {
+func resourceAliCloudResourceManagerHandshake() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudResourceManagerHandshakeCreate,
-		Read:   resourceAlicloudResourceManagerHandshakeRead,
-		Delete: resourceAlicloudResourceManagerHandshakeDelete,
+		Create: resourceAliCloudResourceManagerHandshakeCreate,
+		Read:   resourceAliCloudResourceManagerHandshakeRead,
+		Delete: resourceAliCloudResourceManagerHandshakeDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
 		Schema: map[string]*schema.Schema{
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"expire_time": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -55,30 +64,33 @@ func resourceAlicloudResourceManagerHandshake() *schema.Resource {
 				ForceNew: true,
 			},
 			"target_type": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"Account", "Email"}, false),
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 		},
 	}
 }
 
-func resourceAlicloudResourceManagerHandshakeCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudResourceManagerHandshakeCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
+
 	action := "InviteAccountToResourceDirectory"
-	request := make(map[string]interface{})
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
+	request = make(map[string]interface{})
+
+	request["TargetType"] = d.Get("target_type")
 	if v, ok := d.GetOk("note"); ok {
 		request["Note"] = v
 	}
-
 	request["TargetEntity"] = d.Get("target_entity")
-	request["TargetType"] = d.Get("target_type")
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, nil, request, false)
+		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, query, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -86,52 +98,63 @@ func resourceAlicloudResourceManagerHandshakeCreate(d *schema.ResourceData, meta
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_resource_manager_handshake", action, AlibabaCloudSdkGoERROR)
 	}
-	responseHandshake := response["Handshake"].(map[string]interface{})
-	d.SetId(fmt.Sprint(responseHandshake["HandshakeId"]))
 
-	return resourceAlicloudResourceManagerHandshakeRead(d, meta)
+	id, _ := jsonpath.Get("$.Handshake.HandshakeId", response)
+	d.SetId(fmt.Sprint(id))
+
+	return resourceAliCloudResourceManagerHandshakeRead(d, meta)
 }
-func resourceAlicloudResourceManagerHandshakeRead(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudResourceManagerHandshakeRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	resourcemanagerService := ResourcemanagerService{client}
-	object, err := resourcemanagerService.DescribeResourceManagerHandshake(d.Id())
+	resourceManagerServiceV2 := ResourceManagerServiceV2{client}
+
+	objectRaw, err := resourceManagerServiceV2.DescribeResourceManagerHandshake(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_resource_manager_handshake resourcemanagerService.DescribeResourceManagerHandshake Failed!!! %s", err)
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_resource_manager_handshake DescribeResourceManagerHandshake Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
-	d.Set("expire_time", object["ExpireTime"])
-	d.Set("master_account_id", object["MasterAccountId"])
-	d.Set("master_account_name", object["MasterAccountName"])
-	d.Set("modify_time", object["ModifyTime"])
-	d.Set("note", object["Note"])
-	d.Set("resource_directory_id", object["ResourceDirectoryId"])
-	d.Set("status", object["Status"])
-	d.Set("target_entity", object["TargetEntity"])
-	d.Set("target_type", object["TargetType"])
+
+	d.Set("create_time", objectRaw["CreateTime"])
+	d.Set("expire_time", objectRaw["ExpireTime"])
+	d.Set("master_account_id", objectRaw["MasterAccountId"])
+	d.Set("master_account_name", objectRaw["MasterAccountName"])
+	d.Set("modify_time", objectRaw["ModifyTime"])
+	d.Set("note", objectRaw["Note"])
+	d.Set("resource_directory_id", objectRaw["ResourceDirectoryId"])
+	d.Set("status", objectRaw["Status"])
+	d.Set("target_entity", objectRaw["TargetEntity"])
+	d.Set("target_type", objectRaw["TargetType"])
+
 	return nil
 }
-func resourceAlicloudResourceManagerHandshakeDelete(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudResourceManagerHandshakeDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	action := "CancelHandshake"
+	var request map[string]interface{}
 	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
-	request := map[string]interface{}{
-		"HandshakeId": d.Id(),
-	}
+	request = make(map[string]interface{})
+	request["HandshakeId"] = d.Id()
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
-		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, nil, request, false)
+		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, query, request, true)
+
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -139,14 +162,16 @@ func resourceAlicloudResourceManagerHandshakeDelete(d *schema.ResourceData, meta
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
-		if IsExpectedErrors(err, []string{"EntityNotExists.Handshake", "HandshakeStatusMismatch"}) {
+		if IsExpectedErrors(err, []string{"HandshakeStatusMismatch", "EntityNotExists.Handshake"}) || NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	return nil
 }
