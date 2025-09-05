3a7bcc87c1a2541e36cea0e1a0f0c02247015d54
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2025-09-05 17:16:29 +0800

    New Resource: alicloud_resource_manager_control_policy;

--- alicloud/resource_alicloud_resource_manager_control_policy.go
+++ alicloud/resource_alicloud_resource_manager_control_policy.go
@@ -1,3 +1,4 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
@@ -5,35 +6,44 @@ import (
 	"log"
 	"time"
 
+	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudResourceManagerControlPolicy() *schema.Resource {
+func resourceAliCloudResourceManagerControlPolicy() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudResourceManagerControlPolicyCreate,
-		Read:   resourceAlicloudResourceManagerControlPolicyRead,
-		Update: resourceAlicloudResourceManagerControlPolicyUpdate,
-		Delete: resourceAlicloudResourceManagerControlPolicyDelete,
+		Create: resourceAliCloudResourceManagerControlPolicyCreate,
+		Read:   resourceAliCloudResourceManagerControlPolicyRead,
+		Update: resourceAliCloudResourceManagerControlPolicyUpdate,
+		Delete: resourceAliCloudResourceManagerControlPolicyDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"control_policy_name": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"effect_scope": {
-				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.StringInSlice([]string{"RAM"}, false),
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
 			},
 			"policy_document": {
 				Type:         schema.TypeString,
@@ -44,29 +54,33 @@ func resourceAlicloudResourceManagerControlPolicy() *schema.Resource {
 					return equal
 				},
 			},
+			"tags": tagsSchema(),
 		},
 	}
 }
 
-func resourceAlicloudResourceManagerControlPolicyCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudResourceManagerControlPolicyCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
+
 	action := "CreateControlPolicy"
-	request := make(map[string]interface{})
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
-	request["PolicyName"] = d.Get("control_policy_name")
+	request = make(map[string]interface{})
+
+	request["PolicyDocument"] = d.Get("policy_document")
 	if v, ok := d.GetOk("description"); ok {
 		request["Description"] = v
 	}
-
+	request["PolicyName"] = d.Get("control_policy_name")
 	request["EffectScope"] = d.Get("effect_scope")
-	request["PolicyDocument"] = d.Get("policy_document")
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	request["ClientToken"] = buildClientToken("CreateControlPolicy")
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, nil, request, true)
+		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, query, request, true)
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"ConcurrentCallNotSupported"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
@@ -74,101 +88,135 @@ func resourceAlicloudResourceManagerControlPolicyCreate(d *schema.ResourceData, 
 		}
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_resource_manager_control_policy", action, AlibabaCloudSdkGoERROR)
 	}
-	addDebug(action, response, request)
-	responseControlPolicy := response["ControlPolicy"].(map[string]interface{})
-	d.SetId(fmt.Sprint(responseControlPolicy["PolicyId"]))
 
-	return resourceAlicloudResourceManagerControlPolicyRead(d, meta)
+	id, _ := jsonpath.Get("$.ControlPolicy.PolicyId", response)
+	d.SetId(fmt.Sprint(id))
+
+	return resourceAliCloudResourceManagerControlPolicyUpdate(d, meta)
 }
-func resourceAlicloudResourceManagerControlPolicyRead(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudResourceManagerControlPolicyRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	resourcemanagerService := ResourcemanagerService{client}
-	object, err := resourcemanagerService.DescribeResourceManagerControlPolicy(d.Id())
+	resourceManagerServiceV2 := ResourceManagerServiceV2{client}
+
+	objectRaw, err := resourceManagerServiceV2.DescribeResourceManagerControlPolicy(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_resource_manager_control_policy resourcemanagerService.DescribeResourceManagerControlPolicy Failed!!! %s", err)
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_resource_manager_control_policy DescribeResourceManagerControlPolicy Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
-	d.Set("control_policy_name", object["PolicyName"])
-	d.Set("description", object["Description"])
-	d.Set("effect_scope", object["EffectScope"])
-	d.Set("policy_document", object["PolicyDocument"])
+
+	d.Set("control_policy_name", objectRaw["PolicyName"])
+	d.Set("create_time", objectRaw["CreateDate"])
+	d.Set("description", objectRaw["Description"])
+	d.Set("effect_scope", objectRaw["EffectScope"])
+	d.Set("policy_document", objectRaw["PolicyDocument"])
+
+	objectRaw, err = resourceManagerServiceV2.DescribeControlPolicyListTagResources(d.Id())
+	if err != nil && !NotFoundError(err) {
+		return WrapError(err)
+	}
+
+	tagsMaps := objectRaw["TagResources"]
+	d.Set("tags", tagsToMap(tagsMaps))
+
 	return nil
 }
-func resourceAlicloudResourceManagerControlPolicyUpdate(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudResourceManagerControlPolicyUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	var err error
+	var request map[string]interface{}
 	var response map[string]interface{}
+	var query map[string]interface{}
 	update := false
-	request := map[string]interface{}{
-		"PolicyId": d.Id(),
-	}
-	if d.HasChange("control_policy_name") {
+
+	var err error
+	action := "UpdateControlPolicy"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["PolicyId"] = d.Id()
+
+	if !d.IsNewResource() && d.HasChange("policy_document") {
 		update = true
-		request["NewPolicyName"] = d.Get("control_policy_name")
 	}
-	if d.HasChange("description") {
+	request["NewPolicyDocument"] = d.Get("policy_document")
+	if !d.IsNewResource() && d.HasChange("description") {
 		update = true
 		request["NewDescription"] = d.Get("description")
 	}
-	if d.HasChange("policy_document") {
+
+	if !d.IsNewResource() && d.HasChange("control_policy_name") {
 		update = true
-		request["NewPolicyDocument"] = d.Get("policy_document")
 	}
+	request["NewPolicyName"] = d.Get("control_policy_name")
 	if update {
-		action := "UpdateControlPolicy"
-		wait := incrementalWait(3*time.Second, 3*time.Second)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
 		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
-			response, err = client.RpcPost("ResourceManager", "2020-03-31", action, nil, request, false)
+			response, err = client.RpcPost("ResourceManager", "2020-03-31", action, query, request, true)
 			if err != nil {
-				if NeedRetry(err) {
+				if IsExpectedErrors(err, []string{"ConcurrentCallNotSupported"}) || NeedRetry(err) {
 					wait()
 					return resource.RetryableError(err)
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, response, request)
 			return nil
 		})
+		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
 	}
-	return resourceAlicloudResourceManagerControlPolicyRead(d, meta)
+
+	if d.HasChange("tags") {
+		resourceManagerServiceV2 := ResourceManagerServiceV2{client}
+		if err := resourceManagerServiceV2.SetResourceTags(d, "ControlPolicy"); err != nil {
+			return WrapError(err)
+		}
+	}
+	return resourceAliCloudResourceManagerControlPolicyRead(d, meta)
 }
-func resourceAlicloudResourceManagerControlPolicyDelete(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudResourceManagerControlPolicyDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	action := "DeleteControlPolicy"
+	var request map[string]interface{}
 	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
-	request := map[string]interface{}{
-		"PolicyId": d.Id(),
-	}
+	request = make(map[string]interface{})
+	request["PolicyId"] = d.Id()
 
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
-		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, nil, request, false)
+		response, err = client.RpcPost("ResourceManager", "2020-03-31", action, query, request, true)
+
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"ConcurrentCallNotSupported"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
-		if IsExpectedErrors(err, []string{"EntityNotExists.ControlPolicy"}) {
+		if IsExpectedErrors(err, []string{"EntityNotExists.ControlPolicy"}) || NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	return nil
 }
