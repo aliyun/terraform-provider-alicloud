a50cab03f9df48edb4a882542b8f594781cb69b0
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-09-12 10:24:27 +0800

    New Resource: alicloud_cloud_firewall_threat_intelligence_switch.

--- alicloud/service_alicloud_cloud_firewall_v2.go
+++ alicloud/service_alicloud_cloud_firewall_v2.go
@@ -436,3 +436,83 @@ func (s *CloudFirewallServiceV2) CloudFirewallPolicyAdvancedConfigStateRefreshFu
 }
 
 // DescribeCloudFirewallPolicyAdvancedConfig >>> Encapsulated.
+// DescribeCloudFirewallThreatIntelligenceSwitch <<< Encapsulated get interface for CloudFirewall ThreatIntelligenceSwitch.
+
+func (s *CloudFirewallServiceV2) DescribeCloudFirewallThreatIntelligenceSwitch(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+
+	action := "DescribeThreatIntelligenceSwitch"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Cloudfw", "2017-12-07", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.CategoryList[*]", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.CategoryList[*]", response)
+	}
+
+	if len(v.([]interface{})) == 0 {
+		return object, WrapErrorf(NotFoundErr("ThreatIntelligenceSwitch", id), NotFoundMsg, response)
+	}
+
+	result, _ := v.([]interface{})
+	for _, v := range result {
+		item := v.(map[string]interface{})
+		if fmt.Sprint(item["CategoryId"]) != id {
+			continue
+		}
+		return item, nil
+	}
+	return object, WrapErrorf(NotFoundErr("ThreatIntelligenceSwitch", id), NotFoundMsg, response)
+}
+
+func (s *CloudFirewallServiceV2) CloudFirewallThreatIntelligenceSwitchStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeCloudFirewallThreatIntelligenceSwitch(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeCloudFirewallThreatIntelligenceSwitch >>> Encapsulated.
