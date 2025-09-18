ec3dfd54affd12abf541ba2266a0b4a19607b221
MrWolong <ly01269865@alibaba-inc.com>
2025-09-18 10:45:30 +0800

    New Resource: alicloud_cloud_sso_user_provisioning

--- alicloud/service_alicloud_cloud_sso_v2.go
+++ alicloud/service_alicloud_cloud_sso_v2.go
@@ -758,3 +758,126 @@ func (s *CloudSSOServiceV2) CloudSSODelegateAccountStateRefreshFunc(id string, f
 }
 
 // DescribeCloudSSODelegateAccount >>> Encapsulated.
+
+// DescribeCloudSSOUserProvisioning <<< Encapsulated get interface for CloudSSO UserProvisioning.
+
+func (s *CloudSSOServiceV2) DescribeCloudSSOUserProvisioning(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DirectoryId"] = parts[0]
+	request["UserProvisioningId"] = parts[1]
+
+	action := "GetUserProvisioning"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("cloudsso", "2021-05-15", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EntityNotExists.UserProvisioning"}) {
+			return object, WrapErrorf(NotFoundErr("UserProvisioning", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.UserProvisioning", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.UserProvisioning", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *CloudSSOServiceV2) DescribeUserProvisioningGetUserProvisioningStatistics(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DirectoryId"] = parts[0]
+	request["UserProvisioningId"] = parts[1]
+
+	action := "GetUserProvisioningStatistics"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("cloudsso", "2021-05-15", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"EntityNotExists.UserProvisioning"}) {
+			return object, WrapErrorf(NotFoundErr("UserProvisioning", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.UserProvisioningStatistics", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.UserProvisioningStatistics", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *CloudSSOServiceV2) CloudSSOUserProvisioningStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := s.DescribeCloudSSOUserProvisioning(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeCloudSSOUserProvisioning >>> Encapsulated.
---
+++ website/docs/r/cloud_sso_user_provisioning.html.markdown
@@ -0,0 +1,111 @@
+---
+subcategory: "Cloud SSO"
+layout: "alicloud"
+page_title: "Alicloud: alicloud_cloud_sso_user_provisioning"
+description: |-
+  Provides a Alicloud Cloud SSO User Provisioning resource.
+---
+
+# alicloud_cloud_sso_user_provisioning
+
+Provides a Cloud SSO User Provisioning resource.
+
+RAM user synchronization.
+
+For information about Cloud SSO User Provisioning and how to use it, see [What is User Provisioning](https://next.api.alibabacloud.com/document/cloudsso/2021-05-15/CreateUserProvisioning).
+
+-> **NOTE:** Available since v1.260.0.
+
+## Example Usage
+
+Basic Usage
+
+```terraform
+variable "name" {
+  default = "terraform-example"
+}
+
+provider "alicloud" {
+  region = "cn-shanghai"
+}
+
+data "alicloud_account" "default" {
+}
+
+data "alicloud_cloud_sso_directories" "default" {
+}
+
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
+resource "alicloud_cloud_sso_directory" "default" {
+  count          = length(data.alicloud_cloud_sso_directories.default.ids) > 0 ? 0 : 1
+  directory_name = var.name
+}
+
+resource "alicloud_cloud_sso_user" "default" {
+  directory_id = local.directory_id
+  user_name    = "${var.name}-${random_integer.default.result}"
+}
+
+resource "alicloud_cloud_sso_group" "default" {
+  directory_id = local.directory_id
+  group_name   = var.name
+  description  = var.name
+}
+
+resource "alicloud_cloud_sso_user_provisioning" "default" {
+  description          = "description"
+  principal_id         = alicloud_cloud_sso_user.default.user_id
+  target_type          = "RD-Account"
+  deletion_strategy    = "Keep"
+  duplication_strategy = "KeepBoth"
+  principal_type       = "User"
+  target_id            = data.alicloud_account.default.id
+  directory_id         = alicloud_cloud_sso_user.default.directory_id
+}
+
+locals {
+  directory_id = length(data.alicloud_cloud_sso_directories.default.ids) > 0 ? data.alicloud_cloud_sso_directories.default.ids[0] : concat(alicloud_cloud_sso_directory.default.*.id, [""])[0]
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+* `deletion_strategy` - (Required) The processing policy for users who have been synchronized when deleting synchronization
+* `description` - (Optional) Description of User Synchronization
+* `directory_id` - (Required, ForceNew) The ID of the directory to which the synchronization belongs
+* `duplication_strategy` - (Required) Processing Policy for Synchronization Conflicts
+* `principal_id` - (Required, ForceNew) The ID of the CloudSSO user/group associated with the synchronization.
+* `principal_type` - (Required, ForceNew) The ID of the CloudSSO user/group associated with the synchronization.
+* `target_id` - (Required, ForceNew) The ID of the destination associated with the synchronization.
+* `target_type` - (Required, ForceNew) The target type associated with the synchronization
+
+## Attributes Reference
+
+The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<directory_id>:<user_provisioning_id>`.
+* `create_time` - The creation time of the synchronization
+* `status` - The status of the resource
+* `user_provisioning_id` - The first ID of the resource
+* `user_provisioning_statistics` - User Provisioning statistics
+  * `failed_event_count` - Number of failed events
+  * `gmt_latest_sync` - Last Provisioning time
+
+## Timeouts
+
+The `timeouts` block allows you to specify [timeouts](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts) for certain actions:
+* `create` - (Defaults to 5 mins) Used when create the User Provisioning.
+* `delete` - (Defaults to 5 mins) Used when delete the User Provisioning.
+* `update` - (Defaults to 5 mins) Used when update the User Provisioning.
+
+## Import
+
+Cloud SSO User Provisioning can be imported using the id, e.g.
+
+```shell
+$ terraform import alicloud_cloud_sso_user_provisioning.example <directory_id>:<user_provisioning_id>
+```
