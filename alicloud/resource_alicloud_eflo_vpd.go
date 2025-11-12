0a622fd2edb56b3176a2579e076a50be29bd9047
MrWolong <ly01269865@alibaba-inc.com>
2025-11-12 17:00:22 +0800

    resource/alicloud_eflo_vpd: Refactored the resource alicloud_eflo_vpd; Added the field secondary_cidr_blocks, tags, region_id; Removed the ForceNew for field resource_group_id

--- alicloud/resource_alicloud_eflo_vpd.go
+++ alicloud/resource_alicloud_eflo_vpd.go
@@ -11,12 +11,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func resourceAlicloudEfloVpd() *schema.Resource {
+func resourceAliCloudEfloVpd() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudEfloVpdCreate,
-		Read:   resourceAlicloudEfloVpdRead,
-		Update: resourceAlicloudEfloVpdUpdate,
-		Delete: resourceAlicloudEfloVpdDelete,
+		Create: resourceAliCloudEfloVpdCreate,
+		Read:   resourceAliCloudEfloVpdRead,
+		Update: resourceAliCloudEfloVpdUpdate,
+		Delete: resourceAliCloudEfloVpdDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -27,52 +27,70 @@ func resourceAlicloudEfloVpd() *schema.Resource {
 		},
 		Schema: map[string]*schema.Schema{
 			"cidr": {
+				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-				Type:     schema.TypeString,
 			},
 			"create_time": {
-				Computed: true,
 				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"gmt_modified": {
-				Computed: true,
 				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"region_id": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"resource_group_id": {
-				Optional: true,
-				ForceNew: true,
 				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"secondary_cidr_blocks": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			"status": {
+				Type:     schema.TypeString,
 				Computed: true,
-				Type:     schema.TypeString,
 			},
+			"tags": tagsSchema(),
 			"vpd_name": {
-				Required: true,
 				Type:     schema.TypeString,
+				Required: true,
 			},
 		},
 	}
 }
 
-func resourceAlicloudEfloVpdCreate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	efloService := EfloService{client}
-	request := make(map[string]interface{})
-	var err error
+func resourceAliCloudEfloVpdCreate(d *schema.ResourceData, meta interface{}) error {
 
-	request["ClientToken"] = buildClientToken("CreateVpd")
-	request["VpdName"] = d.Get("vpd_name")
-	request["Cidr"] = d.Get("cidr")
+	client := meta.(*connectivity.AliyunClient)
+
+	action := "CreateVpd"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["RegionId"] = client.RegionId
+
 	if v, ok := d.GetOk("resource_group_id"); ok {
 		request["ResourceGroupId"] = v
 	}
-	var response map[string]interface{}
-	action := "CreateVpd"
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutCreate)), func() *resource.RetryError {
-		resp, err := client.RpcPost("eflo", "2022-05-30", action, nil, request, true)
+	if v, ok := d.GetOk("tags"); ok {
+		tagsMap := ConvertTags(v.(map[string]interface{}))
+		request = expandTagsToMap(request, tagsMap)
+	}
+
+	request["Cidr"] = d.Get("cidr")
+	request["VpdName"] = d.Get("vpd_name")
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("eflo", "2022-05-30", action, query, request, true)
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -80,72 +98,82 @@ func resourceAlicloudEfloVpdCreate(d *schema.ResourceData, meta interface{}) err
 			}
 			return resource.NonRetryableError(err)
 		}
-		response = resp
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_eflo_vpd", action, AlibabaCloudSdkGoERROR)
 	}
 
-	if v, err := jsonpath.Get("$.Content.VpdId", response); err != nil || v == nil {
-		return WrapErrorf(err, IdMsg, "alicloud_eflo_vpd")
-	} else {
-		d.SetId(fmt.Sprint(v))
-	}
-	stateConf := BuildStateConf([]string{}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, efloService.EfloVpdStateRefreshFunc(d.Id(), []string{}))
+	id, _ := jsonpath.Get("$.Content.VpdId", response)
+	d.SetId(fmt.Sprint(id))
+
+	efloServiceV2 := EfloServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, efloServiceV2.EfloVpdStateRefreshFunc(d.Id(), "Status", []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
-	return resourceAlicloudEfloVpdRead(d, meta)
+
+	return resourceAliCloudEfloVpdUpdate(d, meta)
 }
 
-func resourceAlicloudEfloVpdRead(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudEfloVpdRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	efloService := EfloService{client}
+	efloServiceV2 := EfloServiceV2{client}
 
-	object, err := efloService.DescribeEfloVpd(d.Id())
+	objectRaw, err := efloServiceV2.DescribeEfloVpd(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_eflo_vpd efloService.DescribeEfloVpd Failed!!! %s", err)
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_eflo_vpd DescribeEfloVpd Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
 
-	d.Set("cidr", object["Cidr"])
-	d.Set("create_time", object["CreateTime"])
-	d.Set("gmt_modified", object["GmtModified"])
-	d.Set("resource_group_id", object["ResourceGroupId"])
-	d.Set("status", object["Status"])
-	d.Set("vpd_name", object["VpdName"])
+	d.Set("cidr", objectRaw["Cidr"])
+	d.Set("create_time", objectRaw["CreateTime"])
+	d.Set("gmt_modified", objectRaw["GmtModified"])
+	d.Set("region_id", objectRaw["RegionId"])
+	d.Set("resource_group_id", objectRaw["ResourceGroupId"])
+	d.Set("status", objectRaw["Status"])
+	d.Set("vpd_name", objectRaw["VpdName"])
+
+	secondaryCidrBlocksRaw := make([]interface{}, 0)
+	if objectRaw["SecondaryCidrBlocks"] != nil {
+		secondaryCidrBlocksRaw = objectRaw["SecondaryCidrBlocks"].([]interface{})
+	}
+
+	d.Set("secondary_cidr_blocks", secondaryCidrBlocksRaw)
+	tagsMaps := objectRaw["Tags"]
+	d.Set("tags", tagsToMap(tagsMaps))
 
 	return nil
 }
 
-func resourceAlicloudEfloVpdUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudEfloVpdUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
+	d.Partial(true)
 
 	var err error
-	update := false
-	request := map[string]interface{}{
-		"VpdId":    d.Id(),
-		"RegionId": client.RegionId,
-	}
-
-	if d.HasChange("vpd_name") {
+	action := "UpdateVpd"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["VpdId"] = d.Id()
+	request["RegionId"] = client.RegionId
+	if !d.IsNewResource() && d.HasChange("vpd_name") {
 		update = true
-		if v, ok := d.GetOk("vpd_name"); ok {
-			request["VpdName"] = v
-		}
 	}
-
+	request["VpdName"] = d.Get("vpd_name")
 	if update {
-		action := "UpdateVpd"
-		wait := incrementalWait(3*time.Second, 3*time.Second)
-		err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutUpdate)), func() *resource.RetryError {
-			resp, err := client.RpcPost("eflo", "2022-05-30", action, nil, request, true)
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("eflo", "2022-05-30", action, query, request, true)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
@@ -153,32 +181,151 @@ func resourceAlicloudEfloVpdUpdate(d *schema.ResourceData, meta interface{}) err
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(action, resp, request)
 			return nil
 		})
+		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
 	}
-
-	return resourceAlicloudEfloVpdRead(d, meta)
-}
-
-func resourceAlicloudEfloVpdDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	efloService := EfloService{client}
-	var response map[string]interface{}
-	var err error
-
-	request := map[string]interface{}{
-
-		"VpdId": d.Id(),
+	update = false
+	action = "ChangeResourceGroup"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["ResourceId"] = d.Id()
+	request["ResourceRegionId"] = client.RegionId
+	if _, ok := d.GetOk("resource_group_id"); ok && !d.IsNewResource() && d.HasChange("resource_group_id") {
+		update = true
+	}
+	request["ResourceGroupId"] = d.Get("resource_group_id")
+	request["ResourceType"] = "Vpd"
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("eflo-controller", "2022-12-15", action, query, request, true)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
+		efloServiceV2 := EfloServiceV2{client}
+		stateConf := BuildStateConf([]string{}, []string{fmt.Sprint(d.Get("resource_group_id"))}, d.Timeout(schema.TimeoutUpdate), 10*time.Second, efloServiceV2.EfloVpdStateRefreshFunc(d.Id(), "ResourceGroupId", []string{}))
+		if _, err := stateConf.WaitForState(); err != nil {
+			return WrapErrorf(err, IdMsg, d.Id())
+		}
 	}
 
+	if d.HasChange("secondary_cidr_blocks") {
+		oldEntry, newEntry := d.GetChange("secondary_cidr_blocks")
+		removed := oldEntry
+		added := newEntry
+
+		if len(removed.([]interface{})) > 0 {
+			secondaryCidrBlocks := removed.([]interface{})
+
+			for _, item := range secondaryCidrBlocks {
+				action := "UnAssociateVpdCidrBlock"
+				request = make(map[string]interface{})
+				query = make(map[string]interface{})
+				request["VpdId"] = d.Id()
+				request["RegionId"] = client.RegionId
+				if v, ok := item.(string); ok {
+					secondaryCidrBlocksJsonPath, err := jsonpath.Get("$", v)
+					if err != nil {
+						return WrapError(err)
+					}
+					request["SecondaryCidrBlock"] = secondaryCidrBlocksJsonPath
+				}
+				wait := incrementalWait(3*time.Second, 5*time.Second)
+				err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+					response, err = client.RpcPost("eflo", "2022-05-30", action, query, request, true)
+					if err != nil {
+						if NeedRetry(err) {
+							wait()
+							return resource.RetryableError(err)
+						}
+						return resource.NonRetryableError(err)
+					}
+					return nil
+				})
+				addDebug(action, response, request)
+				if err != nil {
+					return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+				}
+
+			}
+		}
+
+		if len(added.([]interface{})) > 0 {
+			secondaryCidrBlocks := added.([]interface{})
+
+			for _, item := range secondaryCidrBlocks {
+				action := "AssociateVpdCidrBlock"
+				request = make(map[string]interface{})
+				query = make(map[string]interface{})
+				request["VpdId"] = d.Id()
+				request["RegionId"] = client.RegionId
+				if v, ok := item.(string); ok {
+					secondaryCidrBlocksJsonPath, err := jsonpath.Get("$", v)
+					if err != nil {
+						return WrapError(err)
+					}
+					request["SecondaryCidrBlock"] = secondaryCidrBlocksJsonPath
+				}
+				wait := incrementalWait(3*time.Second, 5*time.Second)
+				err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+					response, err = client.RpcPost("eflo", "2022-05-30", action, query, request, true)
+					if err != nil {
+						if NeedRetry(err) {
+							wait()
+							return resource.RetryableError(err)
+						}
+						return resource.NonRetryableError(err)
+					}
+					return nil
+				})
+				addDebug(action, response, request)
+				if err != nil {
+					return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+				}
+
+			}
+		}
+	}
+	if !d.IsNewResource() && d.HasChange("tags") {
+		efloServiceV2 := EfloServiceV2{client}
+		if err := efloServiceV2.SetResourceTags(d, "Vpd"); err != nil {
+			return WrapError(err)
+		}
+	}
+	d.Partial(false)
+	return resourceAliCloudEfloVpdRead(d, meta)
+}
+
+func resourceAliCloudEfloVpdDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
 	action := "DeleteVpd"
-	wait := incrementalWait(3*time.Second, 3*time.Second)
-	err = resource.Retry(client.GetRetryTimeout(d.Timeout(schema.TimeoutDelete)), func() *resource.RetryError {
-		response, err = client.RpcPost("eflo", "2022-05-30", action, nil, request, false)
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["VpdId"] = d.Id()
+	request["RegionId"] = client.RegionId
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("eflo", "2022-05-30", action, query, request, true)
+
 		if err != nil {
 			if NeedRetry(err) {
 				wait()
@@ -186,18 +333,22 @@ func resourceAlicloudEfloVpdDelete(d *schema.ResourceData, meta interface{}) err
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
-		if NotFoundError(err) || IsExpectedErrors(err, []string{"1003"}) {
+		if IsExpectedErrors(err, []string{"1003"}) || NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
-	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 5*time.Second, efloService.EfloVpdStateRefreshFunc(d.Id(), []string{}))
+
+	efloServiceV2 := EfloServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{}, d.Timeout(schema.TimeoutDelete), 10*time.Second, efloServiceV2.EfloVpdStateRefreshFunc(d.Id(), "VpdName", []string{}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
+
 	return nil
 }
--- website/docs/r/eflo_vpd.html.markdown
+++ website/docs/r/eflo_vpd.html.markdown
@@ -2,7 +2,6 @@
 subcategory: "Eflo"
 layout: "alicloud"
 page_title: "Alicloud: alicloud_eflo_vpd"
-sidebar_current: "docs-alicloud-resource-eflo-vpd"
 description: |-
   Provides a Alicloud Eflo Vpd resource.
 ---
@@ -11,7 +10,9 @@ description: |-
 
 Provides a Eflo Vpd resource.
 
-For information about Eflo Vpd and how to use it, see [What is Vpd](https://www.alibabacloud.com/help/en/pai/user-guide/overview-of-intelligent-computing-lingjun).
+Lingjun Network Segment.
+
+For information about Eflo Vpd and how to use it, see [What is Vpd](https://next.api.alibabacloud.com/document/eflo/2022-05-30/CreateVpd).
 
 -> **NOTE:** Available since v1.201.0.
 
@@ -27,13 +28,11 @@ Basic Usage
 
 ```terraform
 variable "name" {
-  default = "tf-example"
+  default = "terraform-example"
 }
 
-provider "alicloud" {
-  region = "cn-wulanchabu"
+data "alicloud_resource_manager_resource_groups" "default" {
 }
-data "alicloud_resource_manager_resource_groups" "default" {}
 
 resource "alicloud_eflo_vpd" "default" {
   cidr              = "10.0.0.0/8"
@@ -45,17 +44,20 @@ resource "alicloud_eflo_vpd" "default" {
 ## Argument Reference
 
 The following arguments are supported:
-* `cidr` - (Required, ForceNew) CIDR network segment.
-* `resource_group_id` - (Optional, ForceNew) The Resource group id.
-* `vpd_name` - (Required) The Name of the VPD.
+* `cidr` - (Required, ForceNew) The CIDR block of the VPD.
+* `resource_group_id` - (Optional) The Resource group ID. **NOTE:** From version 1.263.0, `resource_group_id` can be modified.
+* `secondary_cidr_blocks` - (Optional, List, Available since v1.263.0) The additional CIDR block.
+* `tags` - (Optional, Map, Available since v1.263.0) The tag of the resource.
+* `vpd_name` - (Required) The name of the VPD instance.
 
 ## Attributes Reference
 
 The following attributes are exported:
-* `id` - The `key` of the resource supplied above.
-* `create_time` - The creation time of the resource
-* `gmt_modified` - Modification time
-* `status` - The Vpd status.
+* `id` - The ID of the resource supplied above.
+* `create_time` - The time when the activation code was created.
+* `gmt_modified` - The time when the O&M task was modified.
+* `region_id` - (Available since v1.263.0) The region ID.
+* `status` - The current state of the instance.
 
 ## Timeouts
 
