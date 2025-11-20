416c26f41c9417de09738bbde856973d98a2c0fd
zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>
2025-11-20 10:28:39 +0800

    sls logstore index support new attributes
    Change-Id: I2cfab2114276910d258a33e5929aa04e82a6c808
    Co-developed-by: Cursor <noreply@cursor.com>

--- alicloud/resource_alicloud_log_store_index_test.go
+++ alicloud/resource_alicloud_log_store_index_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
+func TestAccAliCloudLogStoreIndex_basic(t *testing.T) {
 	var v *sls.Index
 	resourceId := "alicloud_log_store_index.default"
 	ra := resourceAttrInit(resourceId, logStoreIndexMap)
@@ -34,20 +34,31 @@ func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"project":  "${alicloud_log_project.default.name}",
-					"logstore": "${alicloud_log_store.default.name}",
+					"project":               "${alicloud_log_project.default.name}",
+					"logstore":              "${alicloud_log_store.default.name}",
+					"log_reduce":            true,
+					"log_reduce_black_list": []interface{}{"test"},
+					"log_reduce_white_list": []interface{}{"name"},
+					"max_text_len":          2048,
 					"full_text": []map[string]interface{}{
 						{
-							"case_sensitive": "true",
-							"token":          ` #$^*\r\n\t`,
+							"case_sensitive":  true,
+							"token":           ` #$^*\r\n\t`,
+							"include_chinese": true,
 						},
 					},
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"project":     name,
-						"logstore":    name,
-						"full_text.#": "1",
+						"project":                 name,
+						"logstore":                name,
+						"log_reduce":              "true",
+						"log_reduce_black_list.#": "1",
+						"log_reduce_black_list.0": "test",
+						"log_reduce_white_list.#": "1",
+						"log_reduce_white_list.0": "name",
+						"max_text_len":            "2048",
+						"full_text.#":             "1",
 					}),
 				),
 			},
@@ -58,11 +69,15 @@ func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"full_text": REMOVEKEY,
+					"log_reduce":            false,
+					"log_reduce_black_list": []interface{}{},
+					"log_reduce_white_list": []interface{}{},
+					"max_text_len":          1024,
+					"full_text":             REMOVEKEY,
 					"field_search": []map[string]interface{}{
 						{
 							"name":             "${var.name}",
-							"enable_analytics": "true",
+							"enable_analytics": true,
 							"token":            ` #$^*\r\n\t`,
 							"type":             "text",
 						},
@@ -70,18 +85,28 @@ func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"full_text.#":    REMOVEKEY,
-						"field_search.#": "1",
+						"log_reduce":              "false",
+						"log_reduce_black_list.#": "0",
+						"log_reduce_black_list.0": REMOVEKEY,
+						"log_reduce_white_list.#": "0",
+						"log_reduce_white_list.0": REMOVEKEY,
+						"max_text_len":            "1024",
+						"full_text.#":             REMOVEKEY,
+						"field_search.#":          "1",
 					}),
 				),
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-
+					"log_reduce":            REMOVEKEY,
+					"log_reduce_black_list": REMOVEKEY,
+					"log_reduce_white_list": REMOVEKEY,
+					"max_text_len":          REMOVEKEY,
 					"full_text": []map[string]interface{}{
 						{
-							"case_sensitive": "true",
-							"token":          ` #$^*\r\n\t`,
+							"case_sensitive":  "true",
+							"token":           `,`,
+							"include_chinese": "false",
 						},
 					},
 					"field_search": []map[string]interface{}{
@@ -91,14 +116,17 @@ func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
 							"token":            ` #$^*\r\n\t`,
 							"type":             "json",
 							"json_keys": []map[string]interface{}{
-								{"name": "key2222", "alias": "alisa22222"},
-								{"name": "key1111", "alias": "alisa1111"},
+								{"name": "key2222", "alias": "alisa22222", "doc_value": true, "type": "long"},
+								{"name": "key1111", "alias": "alisa1111", "doc_value": false, "type": "text"},
 							},
 						},
 						{
-							"name":  "${var.name}-2",
-							"token": ` #$^*\r\n\t`,
-							"type":  "json",
+							"name":            "${var.name}-2",
+							"token":           ` #$^*\r\n\t`,
+							"type":            "json",
+							"case_sensitive":  true,
+							"include_chinese": true,
+							"alias":           "json_alias",
 							"json_keys": []map[string]interface{}{
 								{"name": "key3333", "alias": "alisa3333"},
 								{"name": "key4444", "alias": "alisa4444"},
@@ -108,10 +136,17 @@ func TestAccAlicloudLogStoreIndex_basic(t *testing.T) {
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"full_text.#":    "1",
-						"field_search.#": "2",
+						"log_reduce":              REMOVEKEY,
+						"log_reduce_black_list.#": REMOVEKEY,
+						"log_reduce_white_list.#": REMOVEKEY,
+						"max_text_len":            REMOVEKEY,
+						"full_text.#":             "1",
+						"field_search.#":          "2",
 					}),
 				),
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
--- website/docs/r/log_store_index.html.markdown
+++ website/docs/r/log_store_index.html.markdown
@@ -7,10 +7,12 @@ description: |-
   Provides a Alicloud log store index resource.
 ---
 
-# alicloud\_log\_store\_index
+# alicloud_log_store_index
 
 Log Service provides the LogSearch/Analytics function to query and analyze large amounts of logs in real time.
 You can use this function by enabling the index and field statistics. [Refer to details](https://www.alibabacloud.com/help/doc-detail/43772.htm)
+
+-> **NOTE:** Available since v1.0.0.
 
 ## Example Usage
 
@@ -69,26 +71,44 @@ The following arguments are supported:
 
 * `project` - (Required, ForceNew) The project name to the log store belongs.
 * `logstore` - (Required, ForceNew) The log store name to the query index belongs.
-* `full_text` - The configuration of full text index. Valid item as follows:
+* `log_reduce` - (Optional, Available since v1.263.0) Whether to enable log reduce. Default to false.
+* `log_reduce_black_list` - (Optional, Available since v1.263.0) The black list of log reduce.
+* `log_reduce_white_list` - (Optional, Available since v1.263.0) The white list of log reduce.
+* `max_text_len` - (Optional, Available since v1.263.0) The max text length.
+* `full_text` - (Optional) The configuration of full text index. See [`full_text`](#full_text) below.
+* `field_search` - (Optional) List configurations of field search index. See [`field_search`](#field_search) below.
 
-    * `case_sensitive` - (Optional) Whether the case sensitive. Default to false.
-    * `include_chinese` - (Optional) Whether includes the chinese. Default to false.
-    * `token` - (Optional) The string of several split words, like "\r", "#"
+-> **Note:** At least one of the "full_text" and "field_search" should be specified.
 
-* `field_search` - List configurations of field search index. Valid item as follows:
+### `full_text`
 
-    * `name` - (Required) The field name, which is unique in the same log store.
-    * `type` - (Optional) The type of one field. Valid values: ["long", "text", "double", "json"]. Default to "long".
-    * `alias` - (Optional) The alias of one field
-    * `case_sensitive` - (Optional) Whether the case sensitive for the field. Default to false. It is valid when "type" is "text" or "json".
-    * `include_chinese` - (Optional) Whether includes the chinese for the field. Default to false. It is valid when "type" is "text" or "json".
-    * `token` - (Optional) The string of several split words, like "\r", "#". It is valid when "type" is "text" or "json".
-    * `enable_analytics` - (Optional) Whether to enable field analytics. Default to true.
-    * `json_keys` - (Optional, Available in 1.66.0+) Use nested index when type is json
-        * `name` - (Required) When using the json_keys field, this field is required.
-        * `type` - (Optional) The type of one field. Valid values: ["long", "text", "double"]. Default to "long"
-        * `alias` - (Optional) The alias of one field.
-        * `doc_value` - (Optional) Whether to enable statistics. default to true.
+The full_text supports the following:
+
+* `case_sensitive` - (Optional) Whether the case sensitive. Default to false.
+* `include_chinese` - (Optional) Whether includes the chinese. Default to false.
+* `token` - (Optional) The string of several split words, like "\r", "#"
+
+### `field_search`
+
+The field_search supports the following:
+
+* `name` - (Required) The field name, which is unique in the same log store.
+* `type` - (Optional) The type of one field. Valid values: ["long", "text", "double", "json"]. Default to "long".
+* `alias` - (Optional) The alias of one field
+* `case_sensitive` - (Optional) Whether the case sensitive for the field. Default to false. It is valid when "type" is "text" or "json".
+* `include_chinese` - (Optional) Whether includes the chinese for the field. Default to false. It is valid when "type" is "text" or "json".
+* `token` - (Optional) The string of several split words, like "\r", "#". It is valid when "type" is "text" or "json".
+* `enable_analytics` - (Optional) Whether to enable field analytics. Default to true.
+* `json_keys` - (Optional, Available in 1.66.0+) Use nested index when type is json. See [`json_keys`](#field_search-json_keys) below.
+
+### `field_search-json_keys`
+
+The field_search-json_keys supports the following:
+
+* `name` - (Required) When using the json_keys field, this field is required.
+* `type` - (Optional) The type of one field. Valid values: ["long", "text", "double"]. Default to "long"
+* `alias` - (Optional) The alias of one field.
+* `doc_value` - (Optional) Whether to enable statistics. default to true.
 
 -> **Note:** At least one of the "full_text" and "field_search" should be specified.
 
