9cbb424733b25ddf1a0f51f7ae9383cab332492e
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-11-28 13:25:50 +0800

    resource/alicloud_esa_waf_rule: Refactored the resource and improve the docs.

--- alicloud/service_alicloud_esa_v2.go
+++ alicloud/service_alicloud_esa_v2.go
@@ -3931,6 +3931,7 @@ func (s *EsaServiceV2) DescribeEsaWafRule(id string) (object map[string]interfac
 	parts := strings.Split(id, ":")
 	if len(parts) != 2 {
 		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
 	}
 	request = make(map[string]interface{})
 	query = make(map[string]interface{})
@@ -3978,7 +3979,6 @@ func (s *EsaServiceV2) EsaWafRuleStateRefreshFuncWithApi(id string, field string
 			}
 			return nil, "", WrapError(err)
 		}
-
 		v, err := jsonpath.Get(field, object)
 		currentStatus := fmt.Sprint(v)
 
