daff0fd6838bded91523a29e188b928ab7b73de5
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-12-02 23:45:16 +0800

    resource/alicloud_esa_https_application_configuration: Refactored the resource and improve the docs.

--- alicloud/service_alicloud_esa_v2.go
+++ alicloud/service_alicloud_esa_v2.go
@@ -1448,6 +1448,7 @@ func (s *EsaServiceV2) DescribeEsaHttpsApplicationConfiguration(id string) (obje
 	parts := strings.Split(id, ":")
 	if len(parts) != 2 {
 		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
 	}
 	request = make(map[string]interface{})
 	query = make(map[string]interface{})
