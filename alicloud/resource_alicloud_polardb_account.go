604866b73fb9d56f965081e7569ac1e77f225818
MrWolong <ly01269865@alibaba-inc.com>
2025-12-02 11:08:51 +0800

    resource/alicloud_polardb_account: Refactored the resource alicloud_polardb_account; Added the field account_lock_state, account_password_valid_time, status

--- alicloud/resource_alicloud_polardb_account.go
+++ alicloud/resource_alicloud_polardb_account.go
@@ -1,53 +1,78 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
 	"fmt"
+	"log"
 	"strings"
 	"time"
-
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/polardb"
 
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudPolarDBAccount() *schema.Resource {
+func resourceAliCloudPolarDbAccount() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudPolarDBAccountCreate,
-		Read:   resourceAlicloudPolarDBAccountRead,
-		Update: resourceAlicloudPolarDBAccountUpdate,
-		Delete: resourceAlicloudPolarDBAccountDelete,
+		Create: resourceAliCloudPolarDbAccountCreate,
+		Read:   resourceAliCloudPolarDbAccountRead,
+		Update: resourceAliCloudPolarDbAccountUpdate,
+		Delete: resourceAliCloudPolarDbAccountDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
-
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(8 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(8 * time.Minute),
+		},
 		Schema: map[string]*schema.Schema{
-			"db_cluster_id": {
+			"account_description": {
 				Type:     schema.TypeString,
-				ForceNew: true,
-				Required: true,
+				Optional: true,
 			},
-
+			"account_lock_state": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: StringInSlice([]string{"UnLock", "Lock"}, false),
+			},
 			"account_name": {
 				Type:     schema.TypeString,
-				ForceNew: true,
 				Required: true,
+				ForceNew: true,
 			},
-
 			"account_password": {
 				Type:      schema.TypeString,
-				Required:  true,
+				Optional:  true,
 				Sensitive: true,
 			},
-
+			"account_password_valid_time": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"account_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Computed:     true,
+				ValidateFunc: StringInSlice([]string{"Normal", "Super"}, false),
+			},
+			"db_cluster_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"kms_encrypted_password": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				DiffSuppressFunc: kmsDiffSuppressFunc,
 			},
-
 			"kms_encryption_context": {
 				Type:     schema.TypeMap,
 				Optional: true,
@@ -56,30 +81,32 @@ func resourceAlicloudPolarDBAccount() *schema.Resource {
 				},
 				Elem: schema.TypeString,
 			},
-
-			"account_type": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: validation.StringInSlice([]string{string("Normal"), string("Super")}, false),
-				Default:      "Normal",
-				ForceNew:     true,
-			},
-
-			"account_description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
 		},
 	}
 }
 
-func resourceAlicloudPolarDBAccountCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudPolarDbAccountCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	polarDBService := PolarDBService{client}
-	request := polardb.CreateCreateAccountRequest()
-	request.RegionId = client.RegionId
-	request.DBClusterId = d.Get("db_cluster_id").(string)
-	request.AccountName = d.Get("account_name").(string)
+
+	action := "CreateAccount"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOk("account_name"); ok {
+		request["AccountName"] = v
+	}
+	if v, ok := d.GetOk("db_cluster_id"); ok {
+		request["DBClusterId"] = v
+	}
+
+	request["ClientToken"] = buildClientToken(action)
+
+	if v, ok := d.GetOk("account_description"); ok {
+		request["AccountDescription"] = v
+	}
 
 	password := d.Get("account_password").(string)
 	kmsPassword := d.Get("kms_encrypted_password").(string)
@@ -88,198 +115,235 @@ func resourceAlicloudPolarDBAccountCreate(d *schema.ResourceData, meta interface
 		return WrapError(Error("One of the 'password' and 'kms_encrypted_password' should be set."))
 	}
 	if password != "" {
-		request.AccountPassword = password
+		request["AccountPassword"] = password
 	} else {
 		kmsService := KmsService{client}
 		decryptResp, err := kmsService.Decrypt(kmsPassword, d.Get("kms_encryption_context").(map[string]interface{}))
 		if err != nil {
 			return WrapError(err)
 		}
-		request.AccountPassword = decryptResp
+		request["AccountPassword"] = decryptResp
 	}
-
-	request.AccountType = d.Get("account_type").(string)
-
-	// Description will not be set when account type is normal and it is a API bug
-	if v, ok := d.GetOk("account_description"); ok && v.(string) != "" {
-		request.AccountDescription = v.(string)
+	if v, ok := d.GetOk("account_type"); ok {
+		request["AccountType"] = v
+	} else {
+		request["AccountType"] = "Normal"
 	}
-
-	err := resource.Retry(5*time.Minute, func() *resource.RetryError {
-		raw, err := client.WithPolarDBClient(func(polarDBClient *polardb.Client) (interface{}, error) {
-			return polarDBClient.CreateAccount(request)
-		})
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("polardb", "2017-08-01", action, query, request, true)
 		if err != nil {
-			if IsExpectedErrors(err, OperationDeniedDBStatus) {
-				time.Sleep(5 * time.Second)
+			if IsExpectedErrors(err, []string{"IncorrectDBInstanceState", "OperationDenied.OutofUsage", "InstanceConnectTimeoutFault", "OperationDenied.DBInstanceStatus", "ConcurrentTaskExceeded", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "Database.ConnectError", "LockTimeout"}) || NeedRetry(err) {
+				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 		return nil
 	})
+	addDebug(action, response, request)
 
 	if err != nil {
-		return WrapErrorf(err, DefaultErrorMsg, "alicloud_polardb_account", request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_polardb_account", action, AlibabaCloudSdkGoERROR)
 	}
 
-	d.SetId(fmt.Sprintf("%s%s%s", request.DBClusterId, COLON_SEPARATED, request.AccountName))
+	d.SetId(fmt.Sprintf("%v:%v", request["DBClusterId"], request["AccountName"]))
 
-	if err := polarDBService.WaitForPolarDBAccount(d.Id(), Available, DefaultTimeoutMedium); err != nil {
-		return WrapError(err)
+	polarDbServiceV2 := PolarDbServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 5*time.Second, polarDbServiceV2.PolarDbAccountStateRefreshFunc(d.Id(), "AccountStatus", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
 	}
 
-	return resourceAlicloudPolarDBAccountRead(d, meta)
+	return resourceAliCloudPolarDbAccountUpdate(d, meta)
 }
 
-func resourceAlicloudPolarDBAccountRead(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudPolarDbAccountRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	polarDBService := PolarDBService{client}
-	object, err := polarDBService.DescribePolarDBAccount(d.Id())
+	polarDbServiceV2 := PolarDbServiceV2{client}
+
+	objectRaw, err := polarDbServiceV2.DescribePolarDbAccount(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_polardb_account DescribePolarDbAccount Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
 
-	parts, err := ParseResourceId(d.Id(), 2)
-	if err != nil {
-		return WrapError(err)
-	}
+	d.Set("account_description", objectRaw["AccountDescription"])
+	d.Set("account_lock_state", objectRaw["AccountLockState"])
+	d.Set("account_password_valid_time", objectRaw["AccountPasswordValidTime"])
+	d.Set("account_type", objectRaw["AccountType"])
+	d.Set("status", objectRaw["AccountStatus"])
+	d.Set("account_name", objectRaw["AccountName"])
 
+	parts := strings.Split(d.Id(), ":")
 	d.Set("db_cluster_id", parts[0])
-	d.Set("account_name", object.AccountName)
-	d.Set("account_type", object.AccountType)
-	d.Set("account_description", object.AccountDescription)
 
 	return nil
 }
 
-func resourceAlicloudPolarDBAccountUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudPolarDbAccountUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	polarDBService := PolarDBService{client}
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
 	d.Partial(true)
-	parts := strings.Split(d.Id(), COLON_SEPARATED)
-	instanceId := parts[0]
-	accountName := parts[1]
 
-	if d.HasChange("account_description") {
-		if err := polarDBService.WaitForPolarDBAccount(d.Id(), Available, DefaultTimeoutMedium); err != nil {
-			return WrapError(err)
-		}
-		request := polardb.CreateModifyAccountDescriptionRequest()
-		request.RegionId = client.RegionId
-		request.DBClusterId = instanceId
-		request.AccountName = accountName
-		request.AccountDescription = d.Get("account_description").(string)
+	var err error
+	parts := strings.Split(d.Id(), ":")
+	action := "ModifyAccountDescription"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["AccountName"] = parts[1]
+	request["DBClusterId"] = parts[0]
 
-		if err := resource.Retry(5*time.Minute, func() *resource.RetryError {
-			raw, err := client.WithPolarDBClient(func(polarDBClient *polardb.Client) (interface{}, error) {
-				return polarDBClient.ModifyAccountDescription(request)
-			})
+	if !d.IsNewResource() && d.HasChange("account_description") {
+		update = true
+	}
+	request["AccountDescription"] = d.Get("account_description")
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("polardb", "2017-08-01", action, query, request, true)
 			if err != nil {
-				if IsExpectedErrors(err, OperationDeniedDBStatus) {
-					time.Sleep(DefaultIntervalShort * time.Second)
+				if IsExpectedErrors(err, []string{"IncorrectDBInstanceState", "OperationDenied.OutofUsage", "InstanceConnectTimeoutFault", "OperationDenied.DBInstanceStatus", "ConcurrentTaskExceeded", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "Database.ConnectError", "LockTimeout"}) || NeedRetry(err) {
+					wait()
 					return resource.RetryableError(err)
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 			return nil
-		}); err != nil {
-			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
+	}
+	update = false
+	parts = strings.Split(d.Id(), ":")
+	action = "ModifyAccountLockState"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["AccountName"] = parts[1]
+	request["DBClusterId"] = parts[0]
 
-		d.SetPartial("account_description")
+	if d.HasChange("account_password_valid_time") {
+		update = true
+		request["AccountPasswordValidTime"] = d.Get("account_password_valid_time")
 	}
 
-	if d.HasChange("account_password") || d.HasChange("kms_encrypted_password") {
-		if err := polarDBService.WaitForPolarDBAccount(d.Id(), Available, DefaultTimeoutMedium); err != nil {
-			return WrapError(err)
-		}
-		request := polardb.CreateModifyAccountPasswordRequest()
-		request.RegionId = client.RegionId
-		request.DBClusterId = instanceId
-		request.AccountName = accountName
-
-		password := d.Get("account_password").(string)
-		kmsPassword := d.Get("kms_encrypted_password").(string)
-		if password == "" && kmsPassword == "" {
-			return WrapError(Error("One of the 'password' and 'kms_encrypted_password' should be set."))
-		}
-
-		if password != "" {
-			d.SetPartial("account_password")
-			request.NewAccountPassword = password
-		} else {
-			kmsService := KmsService{meta.(*connectivity.AliyunClient)}
-			decryptResp, err := kmsService.Decrypt(kmsPassword, d.Get("kms_encryption_context").(map[string]interface{}))
+	if d.HasChange("account_lock_state") {
+		update = true
+	}
+	request["AccountLockState"] = d.Get("account_lock_state")
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("polardb", "2017-08-01", action, query, request, true)
 			if err != nil {
-				return WrapError(err)
-			}
-			request.NewAccountPassword = decryptResp
-			d.SetPartial("kms_encrypted_password")
-			d.SetPartial("kms_encryption_context")
-		}
-
-		if err := resource.Retry(5*time.Minute, func() *resource.RetryError {
-			raw, err := client.WithPolarDBClient(func(polarDBClient *polardb.Client) (interface{}, error) {
-				return polarDBClient.ModifyAccountPassword(request)
-			})
-			if err != nil {
-				if IsExpectedErrors(err, OperationDeniedDBStatus) {
-					time.Sleep(DefaultIntervalShort * time.Second)
+				if IsExpectedErrors(err, []string{"IncorrectDBInstanceState", "OperationDenied.OutofUsage", "InstanceConnectTimeoutFault", "OperationDenied.DBInstanceStatus", "ConcurrentTaskExceeded", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "Database.ConnectError", "LockTimeout"}) || NeedRetry(err) {
+					wait()
 					return resource.RetryableError(err)
 				}
 				return resource.NonRetryableError(err)
 			}
-			addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 			return nil
-		}); err != nil {
-			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
+	}
+	update = false
+	parts = strings.Split(d.Id(), ":")
+	action = "ModifyAccountPassword"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["AccountName"] = parts[1]
+	request["DBClusterId"] = parts[0]
 
-		d.SetPartial("account_password")
+	if !d.IsNewResource() && d.HasChange("account_password") {
+		update = true
+
+		request["NewAccountPassword"] = d.Get("account_password")
+	}
+
+	if !d.IsNewResource() && d.HasChange("kms_encrypted_password") {
+		update = true
+
+		kmsService := KmsService{meta.(*connectivity.AliyunClient)}
+		decryptResp, err := kmsService.Decrypt(d.Get("kms_encrypted_password").(string), d.Get("kms_encryption_context").(map[string]interface{}))
+		if err != nil {
+			return WrapError(err)
+		}
+		request["NewAccountPassword"] = decryptResp
+	}
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("polardb", "2017-08-01", action, query, request, true)
+			if err != nil {
+				if IsExpectedErrors(err, []string{"IncorrectDBInstanceState", "OperationDenied.OutofUsage", "InstanceConnectTimeoutFault", "OperationDenied.DBInstanceStatus", "ConcurrentTaskExceeded", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "Database.ConnectError", "LockTimeout"}) || NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
+		if err != nil {
+			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		}
 	}
 
 	d.Partial(false)
-	return resourceAlicloudPolarDBAccountRead(d, meta)
+	return resourceAliCloudPolarDbAccountRead(d, meta)
 }
 
-func resourceAlicloudPolarDBAccountDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	polarDBService := PolarDBService{client}
-	parts, err := ParseResourceId(d.Id(), 2)
-	if err != nil {
-		return WrapError(err)
-	}
-	request := polardb.CreateDeleteAccountRequest()
-	request.RegionId = client.RegionId
-	request.DBClusterId = parts[0]
-	request.AccountName = parts[1]
+func resourceAliCloudPolarDbAccountDelete(d *schema.ResourceData, meta interface{}) error {
 
-	if err := resource.Retry(5*time.Minute, func() *resource.RetryError {
-		raw, err := client.WithPolarDBClient(func(polarDBClient *polardb.Client) (interface{}, error) {
-			return polarDBClient.DeleteAccount(request)
-		})
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "DeleteAccount"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["AccountName"] = parts[1]
+	request["DBClusterId"] = parts[0]
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
+		response, err = client.RpcPost("polardb", "2017-08-01", action, query, request, true)
 		if err != nil {
-			if IsExpectedErrors(err, OperationDeniedDBStatus) {
-				time.Sleep(DefaultIntervalShort * time.Second)
+			if IsExpectedErrors(err, []string{"IncorrectDBInstanceState", "OperationDenied.OutofUsage", "InstanceConnectTimeoutFault", "OperationDenied.DBInstanceStatus", "ConcurrentTaskExceeded", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "Database.ConnectError", "LockTimeout"}) || NeedRetry(err) {
+				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 		return nil
-	}); err != nil {
-		if IsExpectedErrors(err, []string{"InvalidAccountName.NotFound"}) {
+	})
+	addDebug(action, response, request)
+
+	if err != nil {
+		if IsExpectedErrors(err, []string{"InvalidAccountName.NotFound", "InvalidDBClusterId.NotFound"}) || NotFoundError(err) {
 			return nil
 		}
-		return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
+		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
 
-	return polarDBService.WaitForPolarDBAccount(d.Id(), Deleted, DefaultTimeoutMedium)
+	polarDbServiceV2 := PolarDbServiceV2{client}
+	stateConf := BuildStateConf([]string{}, []string{""}, d.Timeout(schema.TimeoutDelete), 5*time.Second, polarDbServiceV2.PolarDbAccountStateRefreshFunc(d.Id(), "AccountStatus", []string{}))
+	if _, err := stateConf.WaitForState(); err != nil {
+		return WrapErrorf(err, IdMsg, d.Id())
+	}
+
+	return nil
 }
--- website/docs/r/polardb_account.html.markdown
+++ website/docs/r/polardb_account.html.markdown
@@ -2,16 +2,19 @@
 subcategory: "PolarDB"
 layout: "alicloud"
 page_title: "Alicloud: alicloud_polardb_account"
-sidebar_current: "docs-alicloud-resource-polardb-account"
 description: |-
-  Provides a RDS account resource.
+  Provides a Alicloud Polar Db Account resource.
 ---
 
-# alicloud\_polardb\_account
+# alicloud_polardb_account
 
-Provides a PolarDB account resource and used to manage databases.
+Provides a Polar Db Account resource.
 
--> **NOTE:** Available since v1.67.0. 
+Database account information.
+
+For information about Polar Db Account and how to use it, see [What is Account](https://next.api.alibabacloud.com/document/polardb/2017-08-01/CreateAccount).
+
+-> **NOTE:** Available since v1.67.0.
 
 ## Example Usage
 
@@ -61,25 +64,44 @@ resource "alicloud_polardb_account" "default" {
 ## Argument Reference
 
 The following arguments are supported:
-
-* `db_cluster_id` - (Required, ForceNew) The Id of cluster in which account belongs.
-* `account_name` - (Required, ForceNew) Operation account requiring a uniqueness check. It may consist of lower case letters, numbers, and underlines, and must start with a letter and have no more than 16 characters.
-* `account_password` - (Required) Operation password. It may consist of letters, digits, or underlines, with a length of 6 to 32 characters.
+* `account_description` - (Optional) The description of the database account.
+* `account_lock_state` - (Optional, Available since v1.265.0) The lock status of the account. Valid values:
+  - `UnLock`: The account is not locked.
+  - `Lock`: The account is locked.
+* `account_name` - (Required, ForceNew) The account name. Must meet the following requirements:
+  - Start with a lowercase letter and end with a letter or number.
+  - Consists of lowercase letters, numbers, or underscores.
+  - The length is 2 to 16 characters.
+  - You cannot use some reserved usernames, such as root and admin.
+* `account_password` - (Optional) The account password. You have to specify one of `account_password` and `kms_encrypted_password` fields. Must  meet the following requirements:
+  - Contain at least three of the following character types: uppercase letters, lowercase letters, digits, and special characters.
+  - Be 8 to 32 characters in length.
+  - Special characters include !@#$%^&*()_+-=.
+* `account_password_valid_time` - (Optional, Available since v1.265.0) The time when the password for the database account expires.
+* `account_type` - (Optional, ForceNew) The account type. Default value:`Normal`. Valid values: `Normal`, `Super`.
+* `db_cluster_id` - (Required, ForceNew) The cluster ID.
 * `kms_encrypted_password` - (Optional) An KMS encrypts password used to a db account. If the `account_password` is filled in, this field will be ignored.
 * `kms_encryption_context` - (Optional) An KMS encryption context used to decrypt `kms_encrypted_password` before creating or updating a db account with `kms_encrypted_password`. See [Encryption Context](https://www.alibabacloud.com/help/doc-detail/42975.htm). It is valid when `kms_encrypted_password` is set.
-* `account_description` - (Optional) Account description. It cannot begin with https://. It must start with a Chinese character or English letter. It can include Chinese and English characters, underlines (_), hyphens (-), and numbers. The length may be 2-256 characters.
-* `account_type` - (Optional, ForceNew) Account type, Valid values are `Normal`, `Super`, Default to `Normal`.
 
 ## Attributes Reference
 
 The following attributes are exported:
+* `id` - The ID of the resource supplied above.The value is formulated as `<db_cluster_id>:<account_name>`.
+* `status` - (Available since v1.265.0) The status of the database account.
 
-* `id` - The current account resource ID. Composed of instance ID and account name with format `<instance_id>:<name>`.
+## Timeouts
+
+-> **NOTE:** Available since v1.265.0.
+
+The `timeouts` block allows you to specify [timeouts](https://developer.hashicorp.com/terraform/language/resources/syntax#operation-timeouts) for certain actions:
+* `create` - (Defaults to 8 mins) Used when create the Account.
+* `delete` - (Defaults to 8 mins) Used when delete the Account.
+* `update` - (Defaults to 5 mins) Used when update the Account.
 
 ## Import
 
-PolarDB account can be imported using the id, e.g.
+Polar Db Account can be imported using the id, e.g.
 
 ```shell
-$ terraform import alicloud_polardb_account.example "pc-12345:tf_account"
+$ terraform import alicloud_polardb_account.example <db_cluster_id>:<account_name>
 ```
