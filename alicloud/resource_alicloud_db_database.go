41d278ed175b21c0f67df92586fe0a155b0fe37c
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2025-12-16 12:36:48 +0800

    resource/alicloud_db_database: Fixed the field and test.

--- alicloud/resource_alicloud_db_database.go
+++ alicloud/resource_alicloud_db_database.go
@@ -1,42 +1,60 @@
+// Package alicloud. This file is generated automatically. Please do not modify it manually, thank you!
 package alicloud
 
 import (
 	"fmt"
+	"log"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
-func resourceAlicloudDBDatabase() *schema.Resource {
+func resourceAliCloudRdsDatabase() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudDBDatabaseCreate,
-		Read:   resourceAlicloudDBDatabaseRead,
-		Update: resourceAlicloudDBDatabaseUpdate,
-		Delete: resourceAlicloudDBDatabaseDelete,
+		Create: resourceAliCloudRdsDatabaseCreate,
+		Read:   resourceAliCloudRdsDatabaseRead,
+		Update: resourceAliCloudRdsDatabaseUpdate,
+		Delete: resourceAliCloudRdsDatabaseDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
-
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
 		Schema: map[string]*schema.Schema{
 			"instance_id": {
 				Type:     schema.TypeString,
-				ForceNew: true,
 				Required: true,
+				ForceNew: true,
 			},
-
+			"data_base_name": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				Computed:      true,
+				ValidateFunc:  validation.StringMatch(regexp.MustCompile(`^[a-z][a-z0-9_-]*[a-z0-9]$`), "The name can consist of lowercase letters, numbers, underscores, and middle lines, and must begin with letters and end with letters or numbers"),
+				ConflictsWith: []string{"name"},
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"name": {
-				Type:         schema.TypeString,
-				ForceNew:     true,
-				Required:     true,
-				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-z][a-z0-9_-]*[a-z0-9]$`), "The name can consist of lowercase letters, numbers, underscores, and middle lines, and must begin with letters and end with letters or numbers"),
+				Type:          schema.TypeString,
+				ForceNew:      true,
+				Optional:      true,
+				Computed:      true,
+				ValidateFunc:  validation.StringMatch(regexp.MustCompile(`^[a-z][a-z0-9_-]*[a-z0-9]$`), "The name can consist of lowercase letters, numbers, underscores, and middle lines, and must begin with letters and end with letters or numbers"),
+				Deprecated:    "Field 'name' has been deprecated from provider version 1.266.0. New field 'data_base_name' instead.",
+				ConflictsWith: []string{"data_base_name"},
 			},
-
 			"character_set": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -60,7 +78,6 @@ func resourceAlicloudDBDatabase() *schema.Resource {
 					return false
 				},
 			},
-
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -69,94 +86,134 @@ func resourceAlicloudDBDatabase() *schema.Resource {
 	}
 }
 
-func resourceAlicloudDBDatabaseCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudRdsDatabaseCreate(d *schema.ResourceData, meta interface{}) error {
 
 	client := meta.(*connectivity.AliyunClient)
+
 	action := "CreateDatabase"
-	request := map[string]interface{}{
-		"RegionId":         client.RegionId,
-		"DBInstanceId":     d.Get("instance_id"),
-		"DBName":           d.Get("name"),
-		"CharacterSetName": d.Get("character_set"),
-		"SourceIp":         client.SourceIp,
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var err error
+	request = make(map[string]interface{})
+	if v, ok := d.GetOk("instance_id"); ok {
+		request["DBInstanceId"] = v
 	}
-	if v, ok := d.GetOk("description"); ok && v.(string) != "" {
+
+	if v, ok := d.GetOk("data_base_name"); ok {
+		request["DBName"] = v
+	} else if v, ok := d.GetOk("name"); ok {
+		request["DBName"] = v
+	} else {
+		return WrapError(Error(`[ERROR] Argument "name" or "data_base_name" must be set one!`))
+	}
+	request["RegionId"] = client.RegionId
+
+	if v, ok := d.GetOk("character_set"); ok {
+		request["CharacterSetName"] = v
+	}
+	if v, ok := d.GetOk("description"); ok {
 		request["DBDescription"] = v
 	}
-	var err error
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		response, err := client.RpcPost("Rds", "2014-08-15", action, nil, request, false)
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		response, err = client.RpcPost("Rds", "2014-08-15", action, nil, request, true)
 		if err != nil {
-			if IsExpectedErrors(err, OperationDeniedDBStatus) || NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"OperationDenied.OutofUsage", "OperationDenied.DBInstanceStatus", "OperationDenied.DBClusterStatus", "OperationDenied.DBStatus", "InternalError"}) || NeedRetry(err) {
+				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
 
 	if err != nil {
-		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
+		return WrapErrorf(err, DefaultErrorMsg, "alicloud_db_database", action, AlibabaCloudSdkGoERROR)
 	}
 
-	d.SetId(fmt.Sprintf("%v%s%v", request["DBInstanceId"], COLON_SEPARATED, request["DBName"]))
+	d.SetId(fmt.Sprintf("%v:%v", request["DBInstanceId"], request["DBName"]))
 
-	return resourceAlicloudDBDatabaseRead(d, meta)
+	return resourceAliCloudRdsDatabaseRead(d, meta)
 }
 
-func resourceAlicloudDBDatabaseRead(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudRdsDatabaseRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	rsdService := RdsService{client}
-	object, err := rsdService.DescribeDBDatabase(d.Id())
+	rdsServiceV2 := RdsServiceV2{client}
+
+	objectRaw, err := rdsServiceV2.DescribeRdsDatabase(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_db_database DescribeRdsDatabase Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
 
-	d.Set("instance_id", object["DBInstanceId"])
-	d.Set("name", object["DBName"])
-	if string(PostgreSQL) == object["Engine"] {
-		var strArray = []string{object["CharacterSetName"].(string), object["Collate"].(string), object["Ctype"].(string)}
+	d.Set("description", objectRaw["DBDescription"])
+	d.Set("status", objectRaw["DBStatus"])
+	d.Set("data_base_name", objectRaw["DBName"])
+	d.Set("name", objectRaw["DBName"])
+	d.Set("instance_id", objectRaw["DBInstanceId"])
+
+	if string(PostgreSQL) == objectRaw["Engine"] {
+		var strArray = []string{objectRaw["CharacterSetName"].(string), objectRaw["Collate"].(string), objectRaw["Ctype"].(string)}
 		postgreSQLCharacterSet := strings.Join(strArray, ",")
 		d.Set("character_set", postgreSQLCharacterSet)
 	} else {
-		d.Set("character_set", object["CharacterSetName"])
+		d.Set("character_set", objectRaw["CharacterSetName"])
 	}
-	d.Set("description", object["DBDescription"])
-
 	return nil
 }
 
-func resourceAlicloudDBDatabaseUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudRdsDatabaseUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	if d.HasChange("description") && !d.IsNewResource() {
-		parts, err := ParseResourceId(d.Id(), 2)
-		if err != nil {
-			return WrapError(err)
-		}
-		action := "ModifyDBDescription"
-		request := map[string]interface{}{
-			"RegionId":      client.RegionId,
-			"DBInstanceId":  parts[0],
-			"DBName":        parts[1],
-			"DBDescription": d.Get("description"),
-			"SourceIp":      client.SourceIp,
-		}
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	update := false
 
-		response, err := client.RpcPost("Rds", "2014-08-15", action, nil, request, false)
+	var err error
+	parts := strings.Split(d.Id(), ":")
+	action := "ModifyDBDescription"
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DBInstanceId"] = parts[0]
+	request["DBName"] = parts[1]
+	request["RegionId"] = client.RegionId
+	request["SourceIp"] = client.SourceIp
+
+	if d.HasChange("description") {
+		update = true
+		request["DBDescription"] = d.Get("description")
+	}
+
+	if update {
+		wait := incrementalWait(3*time.Second, 5*time.Second)
+		err = resource.Retry(d.Timeout(schema.TimeoutUpdate), func() *resource.RetryError {
+			response, err = client.RpcPost("Rds", "2014-08-15", action, query, request, true)
+			if err != nil {
+				if NeedRetry(err) {
+					wait()
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+		addDebug(action, response, request)
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 		}
-		addDebug(action, response, request)
 	}
-	return resourceAlicloudDBDatabaseRead(d, meta)
+
+	return resourceAliCloudRdsDatabaseRead(d, meta)
 }
 
-func resourceAlicloudDBDatabaseDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudRdsDatabaseDelete(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
 	rdsService := RdsService{client}
 	parts, err := ParseResourceId(d.Id(), 2)
--- alicloud/service_alicloud_rds_v2.go
+++ alicloud/service_alicloud_rds_v2.go
@@ -352,7 +352,89 @@ func (s *RdsServiceV2) RdsCustomDiskStateRefreshFunc(id string, field string, fa
 	}
 }
 
-// DescribeRdsCustomDisk >>> Encapsulated.
+// DescribeRdsDatabase >>> Encapsulated.
+// DescribeRdsDatabase <<< Encapsulated get interface for Rds Database.
+
+func (s *RdsServiceV2) DescribeRdsDatabase(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DBName"] = parts[1]
+	request["DBInstanceId"] = parts[0]
+	request["RegionId"] = client.RegionId
+	action := "DescribeDatabases"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Rds", "2014-08-15", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Databases.Database[*]", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Databases.Database[*]", response)
+	}
+
+	if len(v.([]interface{})) == 0 {
+		return object, WrapErrorf(NotFoundErr("Database", id), NotFoundMsg, response)
+	}
+
+	return v.([]interface{})[0].(map[string]interface{}), nil
+}
+
+func (s *RdsServiceV2) RdsDatabaseStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.RdsDatabaseStateRefreshFuncWithApi(id, field, failStates, s.DescribeRdsDatabase)
+}
+
+func (s *RdsServiceV2) RdsDatabaseStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeRdsDatabase >>> Encapsulated.
 // DescribeRdsAccount <<< Encapsulated get interface for Rds Account.
 
 func (s *RdsServiceV2) DescribeRdsAccount(id string) (object map[string]interface{}, err error) {
