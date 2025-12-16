15ec58bed655e2709d88382de53c35859d970540
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-12-16 21:13:22 +0800

    New Resource: alicloud_mongodb_node.

--- alicloud/service_alicloud_mongodb_v2.go
+++ alicloud/service_alicloud_mongodb_v2.go
@@ -406,3 +406,80 @@ func (s *MongodbServiceV2) DescribeAsyncDescribeDBInstanceAttribute(d *schema.Re
 }
 
 // DescribeAsyncDescribeDBInstanceAttribute >>> Encapsulated.
+
+// DescribeMongodbNode <<< Encapsulated get interface for Mongodb Node.
+
+func (s *MongodbServiceV2) DescribeMongodbNode(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["DBInstanceId"] = parts[0]
+	request["NodeId"] = parts[1]
+	request["RegionId"] = client.RegionId
+	action := "DescribeShardingNodeAttribute"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Dds", "2015-12-01", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"Node.NotFound"}) {
+			return object, WrapErrorf(NotFoundErr("Node", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	return response, nil
+}
+
+func (s *MongodbServiceV2) MongodbNodeStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.MongodbNodeStateRefreshFuncWithApi(id, field, failStates, s.DescribeMongodbNode)
+}
+
+func (s *MongodbServiceV2) MongodbNodeStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeMongodbNode >>> Encapsulated.
