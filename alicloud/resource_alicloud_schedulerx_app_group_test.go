547d8f956e1475681f2c4dda9fb0b0c9a49ab7b6
MrWolong <ly01269865@alibaba-inc.com>
2025-12-18 11:56:26 +0800

    resource/alicloud_schedulerx_app_group: Fixed the read invalid error caused by enable_log

--- alicloud/resource_alicloud_schedulerx_app_group_test.go
+++ alicloud/resource_alicloud_schedulerx_app_group_test.go
@@ -14,7 +14,7 @@ import (
 func TestAccAliCloudSchedulerxAppGroup_basic9957(t *testing.T) {
 	var v map[string]interface{}
 	resourceId := "alicloud_schedulerx_app_group.default"
-	ra := resourceAttrInit(resourceId, AlicloudSchedulerxAppGroupMap9957)
+	ra := resourceAttrInit(resourceId, AliCloudSchedulerxAppGroupMap9957)
 	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &SchedulerxServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
 	}, "DescribeSchedulerxAppGroup")
@@ -22,7 +22,102 @@ func TestAccAliCloudSchedulerxAppGroup_basic9957(t *testing.T) {
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := acctest.RandIntRange(10000, 99999)
 	name := fmt.Sprintf("tf-testacc%sschedulerxappgroup%d", defaultRegionToTest, rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudSchedulerxAppGroupBasicDependence9957)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudSchedulerxAppGroupBasicDependence9957)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"app_name":       name,
+					"group_id":       name,
+					"namespace":      "${alicloud_schedulerx_namespace.CreateNameSpace.namespace_uid}",
+					"namespace_name": "default",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"app_name":  name,
+						"group_id":  name,
+						"namespace": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"app_version": "1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"app_version": "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": name,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"enable_log": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"enable_log": "true",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"monitor_config_json": "{\\\"sendChannel\\\":\\\"sms,ding\\\",\\\"alarmType\\\": \\\"Contacts\\\",\\\"webhookIsAtAll\\\": \\\"false\\\"}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"monitor_config_json": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"monitor_contacts_json": "[{\\\"name\\\":\\\"用户-手机\\\"},{\\\"name\\\":\\\"用户-钉钉\\\"}]",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"monitor_contacts_json": CHECKSET,
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"app_type", "delete_jobs", "max_concurrency", "namespace_name", "namespace_source", "schedule_busy_workers"},
+			},
+		},
+	})
+}
+
+func TestAccAliCloudSchedulerxAppGroup_basic9957_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_schedulerx_app_group.default"
+	ra := resourceAttrInit(resourceId, AliCloudSchedulerxAppGroupMap9957)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &SchedulerxServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeSchedulerxAppGroup")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf-testacc%sschedulerxappgroup%d", defaultRegionToTest, rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudSchedulerxAppGroupBasicDependence9957)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -34,56 +129,32 @@ func TestAccAliCloudSchedulerxAppGroup_basic9957(t *testing.T) {
 			{
 				Config: testAccConfig(map[string]interface{}{
 					"namespace":             "${alicloud_schedulerx_namespace.CreateNameSpace.namespace_uid}",
-					"group_id":              "test-appgroup-pop-autotest",
-					"description":           "appgroup 资源用例自动生成",
+					"group_id":              name,
+					"description":           name,
 					"monitor_contacts_json": "[{\\\"name\\\":\\\"用户-手机\\\"},{\\\"name\\\":\\\"用户-钉钉\\\"}]",
-					"enable_log":            "false",
-					"app_name":              "test-appgroup-pop-autotest",
+					"enable_log":            "true",
+					"app_name":              name,
 					"app_version":           "1",
 					"namespace_name":        "default",
 					"monitor_config_json":   "{\\\"sendChannel\\\":\\\"sms,ding\\\",\\\"alarmType\\\": \\\"Contacts\\\",\\\"webhookIsAtAll\\\": \\\"false\\\"}",
 					"app_type":              "1",
+					"max_concurrency":       "500",
 					"max_jobs":              "100",
 					"namespace_source":      "schedulerx",
-					"schedule_busy_workers": "false",
-					"delete_jobs":           "false",
-				}),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheck(map[string]string{
-						"namespace":             CHECKSET,
-						"group_id":              "test-appgroup-pop-autotest",
-						"description":           "appgroup 资源用例自动生成",
-						"monitor_contacts_json": CHECKSET,
-						"enable_log":            "false",
-						"app_name":              "test-appgroup-pop-autotest",
-						"app_version":           CHECKSET,
-						"namespace_name":        "default",
-						"monitor_config_json":   CHECKSET,
-						"app_type":              "1",
-						"max_jobs":              "100",
-						"namespace_source":      "schedulerx",
-						"schedule_busy_workers": "false",
-						"delete_jobs":           "false",
-					}),
-				),
-			},
-			{
-				Config: testAccConfig(map[string]interface{}{
-					"description":           "appgroup 资源用例自动生成_update",
-					"monitor_contacts_json": "[{\\\"name\\\":\\\"用户-飞书\\\"},{\\\"name\\\":\\\"用户-钉钉\\\"}]",
-					"app_version":           "2",
-					"monitor_config_json":   "{\\\"sendChannel\\\":\\\"sms,ding\\\",\\\"alarmType\\\": \\\"Contacts\\\",\\\"webhookIsAtAll\\\": \\\"true\\\"}",
-					"max_concurrency":       "500",
+					"schedule_busy_workers": "true",
 					"delete_jobs":           "true",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"description":           "appgroup 资源用例自动生成_update",
+						"namespace":             CHECKSET,
+						"group_id":              CHECKSET,
+						"description":           name,
 						"monitor_contacts_json": CHECKSET,
-						"app_version":           CHECKSET,
+						"enable_log":            "true",
+						"app_name":              name,
+						"app_version":           "1",
+						"max_jobs":              "100",
 						"monitor_config_json":   CHECKSET,
-						"max_concurrency":       "500",
-						"delete_jobs":           "true",
 					}),
 				),
 			},
@@ -91,15 +162,18 @@ func TestAccAliCloudSchedulerxAppGroup_basic9957(t *testing.T) {
 				ResourceName:            resourceId,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"app_type", "enable_log", "max_concurrency", "namespace_name", "namespace_source", "schedule_busy_workers", "delete_jobs"},
+				ImportStateVerifyIgnore: []string{"app_type", "delete_jobs", "max_concurrency", "namespace_name", "namespace_source", "schedule_busy_workers"},
 			},
 		},
 	})
 }
 
-var AlicloudSchedulerxAppGroupMap9957 = map[string]string{}
+var AliCloudSchedulerxAppGroupMap9957 = map[string]string{
+	"app_version": CHECKSET,
+	"max_jobs":    CHECKSET,
+}
 
-func AlicloudSchedulerxAppGroupBasicDependence9957(name string) string {
+func AliCloudSchedulerxAppGroupBasicDependence9957(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
     default = "%s"
