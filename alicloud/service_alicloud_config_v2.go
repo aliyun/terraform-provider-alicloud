9424e1ff562c2edf52516e7469a7ce7248442361
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2025-12-18 17:16:20 +0800

    New Resource: alicloud_config_report_template.

--- alicloud/service_alicloud_config_v2.go
+++ alicloud/service_alicloud_config_v2.go
@@ -2,9 +2,10 @@ package alicloud
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"strings"
 	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
@@ -425,3 +426,78 @@ func (s *ConfigServiceV2) DescribeAsyncGetAggregator(d *schema.ResourceData, res
 }
 
 // DescribeAsyncGetAggregator >>> Encapsulated.
+// DescribeConfigReportTemplate <<< Encapsulated get interface for Config ReportTemplate.
+
+func (s *ConfigServiceV2) DescribeConfigReportTemplate(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["ReportTemplateId"] = id
+
+	action := "GetReportTemplate"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Config", "2020-09-07", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"ReportTemplateNotExist"}) {
+			return object, WrapErrorf(NotFoundErr("ReportTemplate", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.ReportTemplate", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.ReportTemplate", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *ConfigServiceV2) ConfigReportTemplateStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.ConfigReportTemplateStateRefreshFuncWithApi(id, field, failStates, s.DescribeConfigReportTemplate)
+}
+
+func (s *ConfigServiceV2) ConfigReportTemplateStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeConfigReportTemplate >>> Encapsulated.
