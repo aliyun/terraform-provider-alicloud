01d428d2f9d5e3b34a640cf119145b484e407aae
chenhanzhang <chenhanzhang.chz@alibaba-inc.com>
2025-12-19 11:12:09 +0800

    New Resource: alicloud_ehpc_queue.

--- alicloud/service_alicloud_ehpc_v2.go
+++ alicloud/service_alicloud_ehpc_v2.go
@@ -124,3 +124,85 @@ func (s *EhpcServiceV2) EhpcClusterV2StateRefreshFuncWithApi(id string, field st
 }
 
 // DescribeEhpcClusterV2 >>> Encapsulated.
+
+// DescribeEhpcQueue <<< Encapsulated get interface for Ehpc Queue.
+
+func (s *EhpcServiceV2) DescribeEhpcQueue(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
+	}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+	request["ClusterId"] = parts[0]
+	request["QueueName"] = parts[1]
+
+	action := "GetQueue"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("EHPC", "2024-07-30", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"ClusterNotFound", "InvalidParams"}) {
+			return object, WrapErrorf(NotFoundErr("Queue", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.Queue", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.Queue", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *EhpcServiceV2) EhpcQueueStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.EhpcQueueStateRefreshFuncWithApi(id, field, failStates, s.DescribeEhpcQueue)
+}
+
+func (s *EhpcServiceV2) EhpcQueueStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeEhpcQueue >>> Encapsulated.
