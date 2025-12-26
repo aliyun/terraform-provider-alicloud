b98e4f61b6a05d714d4fe6e7bc7da494754db530
wb-ssc771735 <wb-ssc771735@alibaba-inc.com>
2025-12-26 19:27:34 +0800

    New Resource: alicloud_threat_detection_check_config.

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -918,6 +918,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_vpc_ipam_ipams":                                   dataSourceAliCloudVpcIpamIpams(),
 		},
 		ResourcesMap: map[string]*schema.Resource{
+			"alicloud_threat_detection_check_config":                        resourceAliCloudThreatDetectionCheckConfig(),
 			"alicloud_config_aggregate_remediation":                         resourceAliCloudConfigAggregateRemediation(),
 			"alicloud_kms_value_added_service":                              resourceAliCloudKmsValueAddedService(),
 			"alicloud_nas_protocol_service":                                 resourceAliCloudNasProtocolService(),
--- alicloud/service_alicloud_threat_detection_v2.go
+++ alicloud/service_alicloud_threat_detection_v2.go
@@ -1255,3 +1255,69 @@ func (s *ThreatDetectionServiceV2) ThreatDetectionCheckStructureStateRefreshFunc
 }
 
 // DescribeThreatDetectionCheckStructure >>> Encapsulated.
+// DescribeThreatDetectionCheckConfig <<< Encapsulated get interface for ThreatDetection CheckConfig.
+
+func (s *ThreatDetectionServiceV2) DescribeThreatDetectionCheckConfig(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+
+	action := "GetCheckConfig"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Sas", "2018-12-03", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	return response, nil
+}
+
+func (s *ThreatDetectionServiceV2) ThreatDetectionCheckConfigStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.ThreatDetectionCheckConfigStateRefreshFuncWithApi(id, field, failStates, s.DescribeThreatDetectionCheckConfig)
+}
+
+func (s *ThreatDetectionServiceV2) ThreatDetectionCheckConfigStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeThreatDetectionCheckConfig >>> Encapsulated.
