28dc0793460877fbde7f31e26b7b69b9fd6d63b1
MrWolong <ly01269865@alibaba-inc.com>
2026-01-21 18:06:41 +0800

    testcase: Improved alicloud_cloud_firewall_instance_v2 International testcase

--- alicloud/resource_alicloud_cloud_firewall_instance_v2_test.go
+++ alicloud/resource_alicloud_cloud_firewall_instance_v2_test.go
@@ -150,6 +150,128 @@ variable "name" {
 `, name)
 }
 
+// Case 国际版按量付费2.0 11710
+func TestAccAliCloudCloudFirewallInstanceV2_basic11710(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cloud_firewall_instance_v2.default"
+	ra := resourceAttrInit(resourceId, AliCloudCloudFirewallInstanceV2Map11709)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CloudFirewallServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCloudFirewallInstance")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfacccloudfirewall%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudCloudFirewallInstanceV2BasicDependence11709)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithAccountSiteType(t, IntlSite)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"payment_type": "PayAsYouGo",
+					"product_code": "cfw",
+					"product_type": "cfw_elasticity_public_intl",
+					"spec":         "payg_version",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"payment_type": "PayAsYouGo",
+						"product_code": "cfw",
+						"product_type": "cfw_elasticity_public_intl",
+						"spec":         "payg_version",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"sdl": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"sdl": "true",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"cfw_log":     "false",
+					"modify_type": "Upgrade",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"cfw_log": "false",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"modify_type", "period"},
+			},
+		},
+	})
+}
+
+func TestAccAliCloudCloudFirewallInstanceV2_basic11710_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cloud_firewall_instance_v2.default"
+	ra := resourceAttrInit(resourceId, AliCloudCloudFirewallInstanceV2Map11709)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CloudFirewallServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCloudFirewallInstance")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfacccloudfirewall%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudCloudFirewallInstanceV2BasicDependence11709)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithAccountSiteType(t, IntlSite)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"payment_type": "PayAsYouGo",
+					"product_code": "cfw",
+					"product_type": "cfw_elasticity_public_intl",
+					"spec":         "payg_version",
+					"sdl":          "true",
+					"cfw_log":      "false",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"payment_type": "PayAsYouGo",
+						"product_code": "cfw",
+						"product_type": "cfw_elasticity_public_intl",
+						"spec":         "payg_version",
+						"sdl":          "true",
+						"cfw_log":      "false",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"modify_type", "period"},
+			},
+		},
+	})
+}
+
 // Case 国内版预付费2.0 11711
 func TestAccAliCloudCloudFirewallInstanceV2_basic11711(t *testing.T) {
 	var v map[string]interface{}
@@ -167,6 +289,7 @@ func TestAccAliCloudCloudFirewallInstanceV2_basic11711(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheckWithAccountSiteType(t, DomesticSite)
 			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheckWithTime(t, []int{1})
 			testAccPreCheck(t)
 		},
 		IDRefreshName: resourceId,
@@ -231,6 +354,7 @@ func TestAccAliCloudCloudFirewallInstanceV2_basic11711_twin(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheckWithAccountSiteType(t, DomesticSite)
 			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheckWithTime(t, []int{1})
 			testAccPreCheck(t)
 		},
 		IDRefreshName: resourceId,
@@ -282,6 +406,132 @@ var AliCloudCloudFirewallInstanceV2Map11711 = map[string]string{
 	"end_time":              CHECKSET,
 	"user_status":           CHECKSET,
 	"status":                CHECKSET,
+}
+
+// Case 国际版预付费2.0 11712
+func TestAccAliCloudCloudFirewallInstanceV2_basic11712(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cloud_firewall_instance_v2.default"
+	ra := resourceAttrInit(resourceId, AliCloudCloudFirewallInstanceV2Map11711)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CloudFirewallServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCloudFirewallInstance")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfacccloudfirewall%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudCloudFirewallInstanceV2BasicDependence11709)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithAccountSiteType(t, IntlSite)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheckWithTime(t, []int{1})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"payment_type": "Subscription",
+					"product_code": "cfw",
+					"product_type": "cfw_sub_public_intl",
+					"spec":         "premium_version",
+					"period":       "1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"payment_type": "Subscription",
+						"product_code": "cfw",
+						"product_type": "cfw_sub_public_intl",
+						"spec":         "premium_version",
+						"period":       "1",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"renewal_duration":      "1",
+					"renewal_duration_unit": "Y",
+					"renewal_status":        "AutoRenewal",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"renewal_duration":      "1",
+						"renewal_duration_unit": "Y",
+						"renewal_status":        "AutoRenewal",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"modify_type", "period"},
+			},
+		},
+	})
+}
+
+func TestAccAliCloudCloudFirewallInstanceV2_basic11712_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_cloud_firewall_instance_v2.default"
+	ra := resourceAttrInit(resourceId, AliCloudCloudFirewallInstanceV2Map11711)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &CloudFirewallServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeCloudFirewallInstance")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfacccloudfirewall%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudCloudFirewallInstanceV2BasicDependence11709)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithAccountSiteType(t, IntlSite)
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheckWithTime(t, []int{1})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  nil,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"payment_type":          "Subscription",
+					"product_code":          "cfw",
+					"product_type":          "cfw_sub_public_intl",
+					"spec":                  "ultimate_version",
+					"sdl":                   "true",
+					"cfw_log":               "false",
+					"renewal_duration":      "1",
+					"renewal_duration_unit": "Y",
+					"renewal_status":        "AutoRenewal",
+					"period":                "1",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"payment_type":          "Subscription",
+						"product_code":          "cfw",
+						"product_type":          "cfw_sub_public_intl",
+						"spec":                  "ultimate_version",
+						"sdl":                   "true",
+						"cfw_log":               "false",
+						"renewal_duration":      "1",
+						"renewal_duration_unit": "Y",
+						"renewal_status":        "AutoRenewal",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"modify_type", "period"},
+			},
+		},
+	})
 }
 
 // Test CloudFirewall Instance. <<< Resource test cases, automatically generated.
