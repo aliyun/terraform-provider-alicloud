03d64c4e9c4c6025303ad717b38cb0eaf10a13fd
MrWolong <ly01269865@alibaba-inc.com>
2026-02-08 17:46:09 +0800

    resource/alicloud_ecs_auto_snapshot_policy_attachment: Refactored the resource alicloud_ecs_auto_snapshot_policy_attachment; Added the field region_id; Added retry strategy for error code InvalidOperation.Conflict

--- alicloud/resource_alicloud_ecs_auto_snapshot_policy_attachment.go
+++ alicloud/resource_alicloud_ecs_auto_snapshot_policy_attachment.go
@@ -3,6 +3,7 @@ package alicloud
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
@@ -10,13 +11,17 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func resourceAlicloudEcsAutoSnapshotPolicyAttachment() *schema.Resource {
+func resourceAliCloudEcsAutoSnapshotPolicyAttachment() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAlicloudEcsAutoSnapshotPolicyAttachmentCreate,
-		Read:   resourceAlicloudEcsAutoSnapshotPolicyAttachmentRead,
-		Delete: resourceAlicloudEcsAutoSnapshotPolicyAttachmentDelete,
+		Create: resourceAliCloudEcsAutoSnapshotPolicyAttachmentCreate,
+		Read:   resourceAliCloudEcsAutoSnapshotPolicyAttachmentRead,
+		Delete: resourceAliCloudEcsAutoSnapshotPolicyAttachmentDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
 			"auto_snapshot_policy_id": {
@@ -29,88 +34,107 @@ func resourceAlicloudEcsAutoSnapshotPolicyAttachment() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
+			"region_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
-func resourceAlicloudEcsAutoSnapshotPolicyAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceAliCloudEcsAutoSnapshotPolicyAttachmentCreate(d *schema.ResourceData, meta interface{}) error {
+
 	client := meta.(*connectivity.AliyunClient)
-	var response map[string]interface{}
+
 	action := "ApplyAutoSnapshotPolicy"
-	request := make(map[string]interface{})
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
 	var err error
-	request["autoSnapshotPolicyId"] = d.Get("auto_snapshot_policy_id")
+	request = make(map[string]interface{})
 	request["diskIds"] = convertListToJsonString([]interface{}{d.Get("disk_id")})
+	if v, ok := d.GetOk("auto_snapshot_policy_id"); ok {
+		request["autoSnapshotPolicyId"] = v
+	}
 	request["regionId"] = client.RegionId
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		response, err = client.RpcPost("Ecs", "2014-05-26", action, nil, request, false)
+		response, err = client.RpcPost("Ecs", "2014-05-26", action, query, request, true)
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"InvalidOperation.Conflict"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
 		return WrapErrorf(err, DefaultErrorMsg, "alicloud_ecs_auto_snapshot_policy_attachment", action, AlibabaCloudSdkGoERROR)
 	}
-	d.SetId(fmt.Sprint(request["autoSnapshotPolicyId"], ":", d.Get("disk_id")))
-	return resourceAlicloudEcsAutoSnapshotPolicyAttachmentRead(d, meta)
+
+	d.SetId(fmt.Sprintf("%v:%v", request["autoSnapshotPolicyId"], d.Get("disk_id")))
+
+	return resourceAliCloudEcsAutoSnapshotPolicyAttachmentRead(d, meta)
 }
-func resourceAlicloudEcsAutoSnapshotPolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
+
+func resourceAliCloudEcsAutoSnapshotPolicyAttachmentRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*connectivity.AliyunClient)
-	ecsService := EcsService{client}
-	_, err := ecsService.DescribeEcsAutoSnapshotPolicyAttachment(d.Id())
+	ecsServiceV2 := EcsServiceV2{client}
+
+	objectRaw, err := ecsServiceV2.DescribeEcsAutoSnapshotPolicyAttachment(d.Id())
 	if err != nil {
-		if NotFoundError(err) {
-			log.Printf("[DEBUG] Resource alicloud_ecs_auto_snapshot_policy_attachment ecsService.DescribeEcsAutoSnapshotPolicyAttachment Failed!!! %s", err)
+		if !d.IsNewResource() && NotFoundError(err) {
+			log.Printf("[DEBUG] Resource alicloud_ecs_auto_snapshot_policy_attachment DescribeEcsAutoSnapshotPolicyAttachment Failed!!! %s", err)
 			d.SetId("")
 			return nil
 		}
 		return WrapError(err)
 	}
-	parts, err := ParseResourceId(d.Id(), 2)
 
-	d.Set("auto_snapshot_policy_id", parts[0])
-	d.Set("disk_id", parts[1])
+	d.Set("region_id", objectRaw["RegionId"])
+	d.Set("auto_snapshot_policy_id", objectRaw["AutoSnapshotPolicyId"])
+	d.Set("disk_id", objectRaw["DiskId"])
 
 	return nil
 }
-func resourceAlicloudEcsAutoSnapshotPolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*connectivity.AliyunClient)
-	parts, err := ParseResourceId(d.Id(), 2)
-	if err != nil {
-		return WrapError(err)
-	}
-	action := "CancelAutoSnapshotPolicy"
-	var response map[string]interface{}
-	request := map[string]interface{}{
-		"diskIds": convertListToJsonString([]interface{}{parts[1]}),
-	}
 
+func resourceAliCloudEcsAutoSnapshotPolicyAttachmentDelete(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*connectivity.AliyunClient)
+	parts := strings.Split(d.Id(), ":")
+	action := "CancelAutoSnapshotPolicy"
+	var request map[string]interface{}
+	var response map[string]interface{}
+	query := make(map[string]interface{})
+	var err error
+	request = make(map[string]interface{})
+	request["diskIds"] = convertListToJsonString([]interface{}{parts[1]})
 	request["regionId"] = client.RegionId
-	wait := incrementalWait(3*time.Second, 3*time.Second)
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
 	err = resource.Retry(d.Timeout(schema.TimeoutDelete), func() *resource.RetryError {
-		response, err = client.RpcPost("Ecs", "2014-05-26", action, nil, request, false)
+		response, err = client.RpcPost("Ecs", "2014-05-26", action, query, request, true)
 		if err != nil {
-			if NeedRetry(err) {
+			if IsExpectedErrors(err, []string{"InvalidOperation.Conflict"}) || NeedRetry(err) {
 				wait()
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
-		addDebug(action, response, request)
 		return nil
 	})
+	addDebug(action, response, request)
+
 	if err != nil {
-		if IsExpectedErrors(err, []string{"InvalidDiskId.NotFound"}) {
+		if NotFoundError(err) {
 			return nil
 		}
 		return WrapErrorf(err, DefaultErrorMsg, d.Id(), action, AlibabaCloudSdkGoERROR)
 	}
+
 	return nil
 }
