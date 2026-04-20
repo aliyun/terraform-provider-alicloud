d3f400ce07022ede49ca3236dc78489356b02c45
MrWolong <ly01269865@alibaba-inc.com>
2026-04-20 18:33:21 +0800

    resource/alicloud_pai_workspace_workspace: Added the field resource_group_id; Improved alicloud_pai_workspace_workspace testcase

--- alicloud/resource_alicloud_pai_workspace_workspace_test.go
+++ alicloud/resource_alicloud_pai_workspace_workspace_test.go
@@ -14,7 +14,7 @@ import (
 func TestAccAliCloudPaiWorkspaceWorkspace_basic7875(t *testing.T) {
 	var v map[string]interface{}
 	resourceId := "alicloud_pai_workspace_workspace.default"
-	ra := resourceAttrInit(resourceId, AlicloudPaiWorkspaceWorkspaceMap7875)
+	ra := resourceAttrInit(resourceId, AliCloudPaiWorkspaceWorkspaceMap7875)
 	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
 		return &PaiWorkspaceServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
 	}, "DescribePaiWorkspaceWorkspace")
@@ -22,7 +22,7 @@ func TestAccAliCloudPaiWorkspaceWorkspace_basic7875(t *testing.T) {
 	testAccCheck := rac.resourceAttrMapUpdateSet()
 	rand := acctest.RandIntRange(10000, 99999)
 	name := fmt.Sprintf("tf_testaccworkspace%d", rand)
-	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudPaiWorkspaceWorkspaceBasicDependence7875)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudPaiWorkspaceWorkspaceBasicDependence7875)
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
@@ -36,7 +36,6 @@ func TestAccAliCloudPaiWorkspaceWorkspace_basic7875(t *testing.T) {
 				Config: testAccConfig(map[string]interface{}{
 					"description":    "452",
 					"workspace_name": name,
-					"display_name":   "test_pop_584",
 					"env_types": []string{
 						"prod"},
 				}),
@@ -44,32 +43,37 @@ func TestAccAliCloudPaiWorkspaceWorkspace_basic7875(t *testing.T) {
 					testAccCheck(map[string]string{
 						"description":    CHECKSET,
 						"workspace_name": name,
-						"display_name":   CHECKSET,
 						"env_types.#":    "1",
 					}),
 				),
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"description":  "184",
 					"display_name": "new_test_pop_559",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"description":  CHECKSET,
 						"display_name": CHECKSET,
 					}),
 				),
 			},
 			{
 				Config: testAccConfig(map[string]interface{}{
-					"description":  "986",
-					"display_name": "test_pop_403",
+					"description": "986",
 				}),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(map[string]string{
-						"description":  CHECKSET,
 						"display_name": CHECKSET,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.1}",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"resource_group_id": CHECKSET,
 					}),
 				),
 			},
@@ -83,17 +87,70 @@ func TestAccAliCloudPaiWorkspaceWorkspace_basic7875(t *testing.T) {
 	})
 }
 
-var AlicloudPaiWorkspaceWorkspaceMap7875 = map[string]string{
-	"status":      CHECKSET,
-	"create_time": CHECKSET,
+func TestAccAliCloudPaiWorkspaceWorkspace_basic7875_twin(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_pai_workspace_workspace.default"
+	ra := resourceAttrInit(resourceId, AliCloudPaiWorkspaceWorkspaceMap7875)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &PaiWorkspaceServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribePaiWorkspaceWorkspace")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tf_testaccworkspace%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AliCloudPaiWorkspaceWorkspaceBasicDependence7875)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{"cn-hangzhou"})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description":       "452",
+					"workspace_name":    name,
+					"display_name":      "new_test_pop_559",
+					"resource_group_id": "${data.alicloud_resource_manager_resource_groups.default.ids.1}",
+					"env_types": []string{
+						"prod"},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description":       CHECKSET,
+						"workspace_name":    name,
+						"display_name":      CHECKSET,
+						"resource_group_id": CHECKSET,
+						"env_types.#":       "1",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
 }
 
-func AlicloudPaiWorkspaceWorkspaceBasicDependence7875(name string) string {
+var AliCloudPaiWorkspaceWorkspaceMap7875 = map[string]string{
+	"status":            CHECKSET,
+	"create_time":       CHECKSET,
+	"resource_group_id": CHECKSET,
+	"display_name":      CHECKSET,
+}
+
+func AliCloudPaiWorkspaceWorkspaceBasicDependence7875(name string) string {
 	return fmt.Sprintf(`
 variable "name" {
     default = "%s"
 }
-
+data "alicloud_resource_manager_resource_groups" "default" {
+}
 
 `, name)
 }
