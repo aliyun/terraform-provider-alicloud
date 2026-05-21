e1008eb76b0e1222c8865d6dc3044823f92857f5
lichaolin <lichaolin.lcl@alibaba-inc.com>
2026-05-21 09:44:49 +0800

    resource/alicloud_rds_custom: update generated implementation

--- alicloud/resource_alicloud_rds_custom_test.go
+++ alicloud/resource_alicloud_rds_custom_test.go
@@ -35,7 +35,7 @@ func TestAccAliCloudRdsCustom_basic10836(t *testing.T) {
 			{
 				Config: testAccConfig(map[string]interface{}{
 					"amount":        "1",
-					"vswitch_id":    "${alicloud_vswitch.vSwitchId.id}",
+					"vswitch_id":    "${data.alicloud_vswitches.default.ids.0}",
 					"auto_renew":    "false",
 					"period":        "1",
 					"auto_pay":      "true",
@@ -49,7 +49,7 @@ func TestAccAliCloudRdsCustom_basic10836(t *testing.T) {
 					},
 					"status": "Running",
 					"security_group_ids": []string{
-						"${alicloud_security_group.securityGroupId.id}"},
+						"${data.alicloud_security_groups.default.ids.0}"},
 					"io_optimized":                  "optimized",
 					"description":                   "ran_test_ram_code",
 					"key_pair_name":                 "${alicloud_ecs_key_pair.KeyPairName.id}",
@@ -193,7 +193,7 @@ func TestAccAliCloudRdsCustom_basic10836(t *testing.T) {
 				ResourceName:            resourceId,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"amount", "auto_pay", "auto_renew", "create_extra_param", "create_mode", "direction", "dry_run", "force_stop", "host_name", "image_id", "instance_charge_type", "internet_charge_type", "internet_max_bandwidth_out", "io_optimized", "key_pair_name", "password", "period", "period_unit", "security_enhancement_strategy", "spot_strategy", "support_case", "system_disk"},
+				ImportStateVerifyIgnore: []string{"amount", "auto_pay", "auto_renew", "create_extra_param", "create_mode", "direction", "dry_run", "force_stop", "host_name", "image_id", "instance_charge_type", "internet_charge_type", "internet_max_bandwidth_out", "io_optimized", "key_pair_name", "password", "period", "period_unit", "security_enhancement_strategy", "spot_strategy", "support_case"},
 			},
 		},
 	})
@@ -219,26 +219,21 @@ variable "test_zone_id" {
 
 data "alicloud_resource_manager_resource_groups" "default" {}
 
-resource "alicloud_vpc" "vpcId" {
-  cidr_block = "172.16.0.0/12"
+data "alicloud_vpcs" "default" {
+  ids = [data.alicloud_vswitches.default.vswitches.0.vpc_id]
 }
 
-resource "alicloud_vswitch" "vSwitchId" {
-  vpc_id       = alicloud_vpc.vpcId.id
-  cidr_block   = "172.16.5.0/24"
-  zone_id      = var.test_zone_id
-  vswitch_name = "test_vswitch"
+data "alicloud_vswitches" "default" {
+  status  = "Available"
+  zone_id = var.test_zone_id
 }
 
-resource "alicloud_security_group" "securityGroupId" {
-  vpc_id = alicloud_vpc.vpcId.id
-}
-
-resource "alicloud_ecs_deployment_set" "deploymentSet" {
+data "alicloud_security_groups" "default" {
+  vpc_id = data.alicloud_vpcs.default.ids.0
 }
 
 resource "alicloud_ecs_key_pair" "KeyPairName" {
-  key_pair_name = alicloud_vswitch.vSwitchId.id
+  key_pair_name = var.name
 }
 
 
@@ -246,3 +241,403 @@ resource "alicloud_ecs_key_pair" "KeyPairName" {
 }
 
 // Test Rds Custom. <<< Resource test cases, automatically generated.
+
+// Case resourceCase_20260415_01_clone_1_clone_0 12788
+func TestAccAliCloudRdsCustom_basic12788(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_rds_custom.default"
+	ra := resourceAttrInit(resourceId, AlicloudRdsCustomMap12788)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &RdsServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeRdsCustom")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfaccrds%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudRdsCustomBasicDependence12788)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description":          "ran资源用名称050801",
+					"instance_charge_type": "PostPaid",
+					"auto_renew":           "false",
+					"amount":               "1",
+					"vswitch_id":           "${data.alicloud_vswitches.default.ids.0}",
+					"dry_run":              "false",
+					"deployment_set_id":    "${alicloud_rds_custom_deployment_set.deploymentSet.id}",
+					"auto_pay":             "true",
+					"force":                "false",
+					"support_case":         "eni",
+					"security_group_ids": []string{
+						"${data.alicloud_security_groups.default.ids.0}"},
+					"system_disk": []map[string]interface{}{
+						{
+							"category": "cloud_essd",
+							"size":     "40",
+						},
+					},
+					"instance_name": "namecreate",
+					"data_disk": []map[string]interface{}{
+						{
+							"category":          "cloud_essd",
+							"performance_level": "PL0",
+							"size":              "40",
+						},
+					},
+					"create_mode":   "0",
+					"instance_type": "mysql.x2.large.9cm",
+					"spot_strategy": "NoSpot",
+					"host_name":     "testhostNameran",
+					"period_unit":   "Month",
+					"password":      "@MY7yxqc9YXQC",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description":          "ran资源用名称050801",
+						"instance_charge_type": "PostPaid",
+						"auto_renew":           "false",
+						"amount":               "1",
+						"vswitch_id":           CHECKSET,
+						"dry_run":              "false",
+						"deployment_set_id":    CHECKSET,
+						"auto_pay":             "true",
+						"force":                "false",
+						"support_case":         "eni",
+						"security_group_ids.#": "1",
+						"instance_name":        "namecreate",
+						"data_disk.#":          "1",
+						"create_mode":          CHECKSET,
+						"instance_type":        "mysql.x2.large.9cm",
+						"spot_strategy":        "NoSpot",
+						"host_name":            "testhostNameran",
+						"period_unit":          "Month",
+						"password":             "@MY7yxqc9YXQC",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": "修改描述",
+					"force_stop":  "false",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": "修改描述",
+						"force_stop":  "false",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"instance_name": "update名称",
+					"host_name":     "hostnameupdate",
+					"password":      "@MY7yxqc9YXQCUPDATE",
+					"force":         "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"instance_name": "update名称",
+						"host_name":     "hostnameupdate",
+						"password":      "@MY7yxqc9YXQCUPDATE",
+						"force":         "true",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "Test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF",
+						"tags.For":     "Test",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF-update",
+						"For":     "Test-update",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF-update",
+						"tags.For":     "Test-update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": REMOVEKEY,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "0",
+						"tags.Created": REMOVEKEY,
+						"tags.For":     REMOVEKEY,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"status":     "Stopped",
+					"force_stop": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"status":     "Stopped",
+						"force_stop": "true",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"amount", "auto_pay", "auto_renew", "create_mode", "direction", "dry_run", "force", "force_stop", "host_name", "image_id", "instance_charge_type", "internet_charge_type", "internet_max_bandwidth_out", "io_optimized", "key_pair_name", "password", "period", "period_unit", "security_enhancement_strategy", "spot_strategy", "support_case"},
+			},
+		},
+	})
+}
+
+var AlicloudRdsCustomMap12788 = map[string]string{
+	"system_disk_id": CHECKSET,
+	"region_id":      CHECKSET,
+}
+
+func AlicloudRdsCustomBasicDependence12788(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+data "alicloud_vpcs" "default" {
+  ids = [data.alicloud_vswitches.default.vswitches.0.vpc_id]
+}
+
+data "alicloud_vswitches" "default" {
+  status  = "Available"
+  zone_id = "cn-beijing-i"
+}
+
+data "alicloud_security_groups" "default" {
+  vpc_id = data.alicloud_vpcs.default.ids.0
+}
+
+resource "alicloud_rds_custom_deployment_set" "deploymentSet" {
+  custom_deployment_set_name            = var.name
+  description                           = var.name
+  group_count                           = 3
+  on_unable_to_redeploy_failed_instance = "CancelMembershipAndStart"
+  strategy                              = "Availability"
+}
+
+
+`, name)
+}
+
+// Case resourceCase_20260415_01 12772
+func TestAccAliCloudRdsCustom_basic12772(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_rds_custom.default"
+	ra := resourceAttrInit(resourceId, AlicloudRdsCustomMap12772)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &RdsServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeRdsCustom")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfaccrds%d", rand)
+	privateIpAddress := fmt.Sprintf("172.16.1.%d", acctest.RandIntRange(10, 240))
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudRdsCustomBasicDependence12772)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description":          "ran资源用例描述042001",
+					"private_ip_address":   privateIpAddress,
+					"instance_charge_type": "PostPaid",
+					"auto_renew":           "false",
+					"amount":               "1",
+					"vswitch_id":           "${data.alicloud_vswitches.default.ids.0}",
+					"dry_run":              "false",
+					"auto_pay":             "true",
+					"security_group_ids": []string{
+						"${data.alicloud_security_groups.default.ids.0}"},
+					"system_disk": []map[string]interface{}{
+						{
+							"category": "cloud_essd",
+							"size":     "40",
+						},
+					},
+					"data_disk": []map[string]interface{}{
+						{
+							"category":          "cloud_essd",
+							"performance_level": "PL0",
+							"size":              "40",
+						},
+					},
+					"create_mode":   "0",
+					"instance_type": "mysql.x2.large.9cm",
+					"spot_strategy": "NoSpot",
+					"host_name":     "testhostNameran",
+					"period_unit":   "Month",
+					"password":      "@MY7yxqc9YXQC",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description":          "ran资源用例描述042001",
+						"private_ip_address":   privateIpAddress,
+						"instance_charge_type": "PostPaid",
+						"auto_renew":           "false",
+						"amount":               "1",
+						"vswitch_id":           CHECKSET,
+						"dry_run":              "false",
+						"auto_pay":             "true",
+						"security_group_ids.#": "1",
+						"data_disk.#":          "1",
+						"create_mode":          CHECKSET,
+						"instance_type":        "mysql.x2.large.9cm",
+						"spot_strategy":        "NoSpot",
+						"host_name":            "testhostNameran",
+						"period_unit":          "Month",
+						"password":             "@MY7yxqc9YXQC",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"description": "修改描述",
+					"force_stop":  "false",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"description": "修改描述",
+						"force_stop":  "false",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"password":      "@MY7yxqc9YXQCUPDATE",
+					"instance_name": "update名称",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"password":      "@MY7yxqc9YXQCUPDATE",
+						"instance_name": "update名称",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF",
+						"For":     "Test",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF",
+						"tags.For":     "Test",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": map[string]string{
+						"Created": "TF-update",
+						"For":     "Test-update",
+					},
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "2",
+						"tags.Created": "TF-update",
+						"tags.For":     "Test-update",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"tags": REMOVEKEY,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"tags.%":       "0",
+						"tags.Created": REMOVEKEY,
+						"tags.For":     REMOVEKEY,
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"status":     "Stopped",
+					"force_stop": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"status":     "Stopped",
+						"force_stop": "true",
+					}),
+				),
+			},
+			{
+				ResourceName:            resourceId,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"amount", "auto_pay", "auto_renew", "create_mode", "direction", "dry_run", "force_stop", "host_name", "image_id", "instance_charge_type", "internet_charge_type", "internet_max_bandwidth_out", "io_optimized", "key_pair_name", "password", "period", "period_unit", "security_enhancement_strategy", "spot_strategy", "support_case"},
+			},
+		},
+	})
+}
+
+var AlicloudRdsCustomMap12772 = map[string]string{
+	"system_disk_id": CHECKSET,
+	"region_id":      CHECKSET,
+}
+
+func AlicloudRdsCustomBasicDependence12772(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
+}
+
+data "alicloud_vpcs" "default" {
+  ids = [data.alicloud_vswitches.default.vswitches.0.vpc_id]
+}
+
+data "alicloud_vswitches" "default" {
+  cidr_block = "172.16.0.0/23"
+  status     = "Available"
+  zone_id    = "cn-beijing-i"
+}
+
+data "alicloud_security_groups" "default" {
+  vpc_id = data.alicloud_vpcs.default.ids.0
+}
+
+
+`, name)
+}
