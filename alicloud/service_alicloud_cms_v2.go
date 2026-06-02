a64f8cbdace7b7bf095533218bd22f9d1a95b248
MrWolong <ly01269865@alibaba-inc.com>
2026-06-02 13:23:30 +0800

    New Resource: alicloud_cms_agg_task_group

--- alicloud/service_alicloud_cms_v2.go
+++ alicloud/service_alicloud_cms_v2.go
@@ -390,3 +390,85 @@ func (s *CmsServiceV2) CmsAddonReleaseStateRefreshFuncWithApi(id string, field s
 }
 
 // DescribeCmsAddonRelease >>> Encapsulated.
+
+// DescribeCmsAggTaskGroup <<< Encapsulated get interface for Cms AggTaskGroup.
+
+func (s *CmsServiceV2) DescribeCmsAggTaskGroup(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]*string
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 {
+		err = WrapError(fmt.Errorf("invalid Resource Id %s. Expected parts' length %d, got %d", id, 2, len(parts)))
+		return nil, err
+	}
+	groupId := parts[1]
+	instanceId := parts[0]
+	request = make(map[string]interface{})
+	query = make(map[string]*string)
+
+	action := fmt.Sprintf("/prometheus-instances/%s/agg-task-groups/%s", instanceId, groupId)
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RoaGet("Cms", "2024-03-30", action, query, nil, nil)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		if IsExpectedErrors(err, []string{"404"}) {
+			return object, WrapErrorf(NotFoundErr("AggTaskGroup", id), NotFoundMsg, response)
+		}
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$.aggTaskGroup", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$.aggTaskGroup", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *CmsServiceV2) CmsAggTaskGroupStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.CmsAggTaskGroupStateRefreshFuncWithApi(id, field, failStates, s.DescribeCmsAggTaskGroup)
+}
+
+func (s *CmsServiceV2) CmsAggTaskGroupStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeCmsAggTaskGroup >>> Encapsulated.
