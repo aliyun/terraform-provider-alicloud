ff47db8b32871e9c68387c2c8c4849bbc87cb08d
xuzhang3 <zhang.zx@alibaba-inc.com>
2026-06-09 19:33:53 +0800

    fix import error

--- alicloud/resource_alicloud_polardb_cluster_test.go
+++ alicloud/resource_alicloud_polardb_cluster_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/polardb"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
