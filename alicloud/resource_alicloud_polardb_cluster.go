79d8168254eed7918a40cf84543b5b0cbb8a01ea
米汐 <mizu.lm@alibaba-inc.com>
2026-08-19 19:43:17 +0800

    fix error code

--- alicloud/resource_alicloud_polardb_cluster.go
+++ alicloud/resource_alicloud_polardb_cluster.go
@@ -753,7 +753,7 @@ func resourceAlicloudPolarDBClusterUpdate(d *schema.ResourceData, meta interface
 				return polardbClient.ModifyAutoRenewAttribute(request)
 			})
 			if err != nil {
-				if IsExpectedErrors(err, []string{"-999"}) || NeedRetry(err) {
+				if IsExpectedErrors(err, []string{"-999", "-1002"}) || NeedRetry(err) {
 					wait()
 					return resource.RetryableError(err)
 				}
@@ -762,7 +762,6 @@ func resourceAlicloudPolarDBClusterUpdate(d *schema.ResourceData, meta interface
 			addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 			return nil
 		})
-
 		if err != nil {
 			return WrapErrorf(err, DefaultErrorMsg, d.Id(), request.GetActionName(), AlibabaCloudSdkGoERROR)
 		}
