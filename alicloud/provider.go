282e801f15bf3ff9278d7af07d5d4f4b828580c6
米汐 <mizu.lm@alibaba-inc.com>
2026-08-21 14:33:23 +0800

    resource/alicloud_polardb_gateway: add new resource

--- alicloud/provider.go
+++ alicloud/provider.go
@@ -1612,6 +1612,7 @@ func Provider() terraform.ResourceProvider {
 			"alicloud_polardb_application_endpoint":                          resourceAlicloudPolarDBApplicationEndpoint(),
 			"alicloud_polardb_batch_task":                                    resourceAlicloudPolarDBBatchTask(),
 			"alicloud_polardb_aicluster":                                     resourceAlicloudPolarDBAICluster(),
+			"alicloud_polardb_gateway":                                       resourceAlicloudPolarDBGateway(),
 			"alicloud_hbase_instance":                                        resourceAlicloudHBaseInstance(),
 			"alicloud_market_order":                                          resourceAlicloudMarketOrder(),
 			"alicloud_adb_cluster":                                           resourceAliCloudAdbDbCluster(),
