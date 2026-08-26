c513be7bae90583cc45c226d2f0126dc4883fa32
MrWolong <ly01269865@alibaba-inc.com>
2026-08-26 18:12:30 +0800

    resource/alicloud_fcv3_async_invoke_config: Fixed the create error caused by field destination_config

--- alicloud/service_alicloud_fcv3_v2.go
+++ alicloud/service_alicloud_fcv3_v2.go
@@ -329,17 +329,27 @@ func (s *Fcv3ServiceV2) DescribeFcv3AsyncInvokeConfig(id string) (object map[str
 }
 
 func (s *Fcv3ServiceV2) Fcv3AsyncInvokeConfigStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.Fcv3AsyncInvokeConfigStateRefreshFuncWithApi(id, field, failStates, s.DescribeFcv3AsyncInvokeConfig)
+}
+
+func (s *Fcv3ServiceV2) Fcv3AsyncInvokeConfigStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		object, err := s.DescribeFcv3AsyncInvokeConfig(id)
+		object, err := call(id)
 		if err != nil {
 			if NotFoundError(err) {
 				return object, "", nil
 			}
 			return nil, "", WrapError(err)
 		}
-
 		v, err := jsonpath.Get(field, object)
 		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
 
 		for _, failState := range failStates {
 			if currentStatus == failState {
