d4d0f6ea3b5f3f729171ddb77649bef728686439
zhang.zx@alibaba-inc.com <zhang.zx@alibaba-inc.com>
2026-08-28 14:36:53 +0800

    resource/alicloud_gwlb_server_group: add draining_servers attribute and skip draining/removing servers during removal

--- alicloud/resource_alicloud_gwlb_server_group_test.go
+++ alicloud/resource_alicloud_gwlb_server_group_test.go
@@ -198,6 +198,22 @@ variable "region_id" {
 
 data "alicloud_resource_manager_resource_groups" "default" {}
 
+data "alicloud_zones" "default" {
+  available_disk_category     = "cloud_efficiency"
+  available_resource_creation = "VSwitch"
+}
+
+data "alicloud_instance_types" "default" {
+  availability_zone    = data.alicloud_zones.default.zones.0.id
+  instance_type_family = "ecs.sn1ne"
+}
+
+data "alicloud_images" "default" {
+  name_regex  = "^aliyun_2_1903_x64_20G_alibase"
+  most_recent = true
+  owners      = "system"
+}
+
 resource "alicloud_vpc" "defaultEaxcvb" {
   cidr_block = "10.0.0.0/8"
   vpc_name   = "tf-gwlb-vpc"
@@ -205,7 +221,7 @@ resource "alicloud_vpc" "defaultEaxcvb" {
 
 resource "alicloud_vswitch" "defaultc3uVID" {
   vpc_id       = alicloud_vpc.defaultEaxcvb.id
-  zone_id      = "cn-hangzhou-h"
+  zone_id      = data.alicloud_zones.default.zones.0.id
   cidr_block   = "10.0.0.0/24"
   vswitch_name = "tf-test-vsw1"
 }
@@ -219,9 +235,9 @@ resource "alicloud_security_group" "default7NNxRl" {
 
 resource "alicloud_instance" "defaultH6McvC" {
 	vswitch_id = alicloud_vswitch.defaultc3uVID.id
-	image_id = "aliyun_2_1903_x64_20G_alibase_20231221.vhd"
+	image_id = data.alicloud_images.default.images.0.id
 	
-	instance_type = "ecs.g6.large"
+	instance_type = data.alicloud_instance_types.default.instance_types.0.id
 	system_disk_category = "cloud_efficiency"
 	
 	internet_charge_type = "PayByTraffic"
@@ -701,6 +717,222 @@ variable "name" {
 
 variable "region_id" {
   default = "cn-wulanchabu"
+}
+
+data "alicloud_resource_manager_resource_groups" "default" {}
+
+resource "alicloud_vpc" "defaultEaxcvb" {
+  cidr_block = "10.0.0.0/8"
+  vpc_name   = "tf-gwlb-vpc"
+}
+
+
+`, name)
+}
+
+// Case ServerGroup Test_draining 8570
+func TestAccAliCloudGwlbServerGroupDraining8570(t *testing.T) {
+	var v map[string]interface{}
+	resourceId := "alicloud_gwlb_server_group.default"
+	ra := resourceAttrInit(resourceId, AlicloudGwlbServerGroupMap8570)
+	rc := resourceCheckInitWithDescribeMethod(resourceId, &v, func() interface{} {
+		return &GwlbServiceV2{testAccProvider.Meta().(*connectivity.AliyunClient)}
+	}, "DescribeGwlbServerGroup")
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(10000, 99999)
+	name := fmt.Sprintf("tfaccgwlb%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, AlicloudGwlbServerGroupBasicDependence8570)
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckWithRegions(t, true, []connectivity.Region{connectivity.Hangzhou})
+			testAccPreCheck(t)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				// create two servers with connection drain enabled (1h timeout keeps the
+				// Draining status stable across all the following steps)
+				Config: testAccConfig(map[string]interface{}{
+					"protocol":          "GENEVE",
+					"server_group_type": "Ip",
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "true",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id": "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers": []map[string]interface{}{
+						{
+							"server_id":   "10.0.0.1",
+							"server_ip":   "10.0.0.1",
+							"server_type": "Ip",
+						},
+						{
+							"server_id":   "10.0.0.2",
+							"server_ip":   "10.0.0.2",
+							"server_type": "Ip",
+						},
+					},
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"protocol":           "GENEVE",
+						"server_group_type":  "Ip",
+						"vpc_id":             CHECKSET,
+						"servers.#":          "2",
+						"draining_servers.#": "0",
+						"server_group_name":  name,
+					}),
+				),
+			},
+			{
+				// remove 10.0.0.1: with drain enabled it enters Draining and is no longer
+				// kept in servers (only Available servers are persisted); it is exposed
+				// through the draining_servers field instead
+				Config: testAccConfig(map[string]interface{}{
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "true",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id": "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers": []map[string]interface{}{
+						{
+							"server_id":   "10.0.0.2",
+							"server_ip":   "10.0.0.2",
+							"server_type": "Ip",
+						},
+					},
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"servers.#":          "1",
+						"draining_servers.#": "1",
+					}),
+				),
+			},
+			{
+				// remove 10.0.0.2 while 10.0.0.1 is still Draining: the provider must skip the
+				// Draining server in RemoveServersFromServerGroup, otherwise the API rejects the
+				// request with an IncorrectStatus error. Both servers are now out of state.
+				// REMOVEKEY drops the servers key from the generated config; an empty map
+				// (CLEARMAP) or list renders invalid HCL for a block-typed attribute.
+				Config: testAccConfig(map[string]interface{}{
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "true",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id":            "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers":           REMOVEKEY,
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"servers.#":          "0",
+						"draining_servers.#": "2",
+					}),
+				),
+			},
+			{
+				// re-add the Draining server 10.0.0.1: it must be rescued via AddServers
+				Config: testAccConfig(map[string]interface{}{
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "true",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id": "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers": []map[string]interface{}{
+						{
+							"server_id":   "10.0.0.1",
+							"server_ip":   "10.0.0.1",
+							"server_type": "Ip",
+						},
+					},
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"servers.#":          "1",
+						"draining_servers.#": "1",
+					}),
+				),
+			},
+			{
+				// re-add 10.0.0.2 as well: both servers back to Available
+				Config: testAccConfig(map[string]interface{}{
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "true",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id": "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers": []map[string]interface{}{
+						{
+							"server_id":   "10.0.0.1",
+							"server_ip":   "10.0.0.1",
+							"server_type": "Ip",
+						},
+						{
+							"server_id":   "10.0.0.2",
+							"server_ip":   "10.0.0.2",
+							"server_type": "Ip",
+						},
+					},
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"servers.#":          "2",
+						"draining_servers.#": "0",
+					}),
+				),
+			},
+			{
+				// disable connection drain and clear the servers so the destroy is clean;
+				// servers dropped from the config via REMOVEKEY (empty map/list renders invalid HCL)
+				Config: testAccConfig(map[string]interface{}{
+					"connection_drain_config": []map[string]interface{}{
+						{
+							"connection_drain_enabled": "false",
+							"connection_drain_timeout": "3600",
+						},
+					},
+					"vpc_id":            "${alicloud_vpc.defaultEaxcvb.id}",
+					"servers":           REMOVEKEY,
+					"server_group_name": name,
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"servers.#":          "0",
+						"draining_servers.#": CHECKSET,
+					}),
+				),
+			},
+		},
+	})
+}
+
+var AlicloudGwlbServerGroupMap8570 = map[string]string{
+	"status":      CHECKSET,
+	"create_time": CHECKSET,
+}
+
+func AlicloudGwlbServerGroupBasicDependence8570(name string) string {
+	return fmt.Sprintf(`
+variable "name" {
+    default = "%s"
 }
 
 data "alicloud_resource_manager_resource_groups" "default" {}
