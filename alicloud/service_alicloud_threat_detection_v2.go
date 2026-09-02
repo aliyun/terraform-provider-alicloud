e1df7da15e8b1a3b72f13cacf35a545fb7fd0a9a
api-tool-agent <cloudspec_bot@alibaba-inc.com>
2026-09-02 21:42:48 +0800

    New Resource: alicloud_threat_detection_monitor_account

--- alicloud/service_alicloud_threat_detection_v2.go
+++ alicloud/service_alicloud_threat_detection_v2.go
@@ -1509,3 +1509,74 @@ func (s *ThreatDetectionServiceV2) DescribeAsyncDescribeServiceLinkedRoleStatus(
 }
 
 // DescribeAsyncDescribeServiceLinkedRoleStatus >>> Encapsulated.
+// DescribeThreatDetectionMonitorAccount <<< Encapsulated get interface for ThreatDetection MonitorAccount.
+
+func (s *ThreatDetectionServiceV2) DescribeThreatDetectionMonitorAccount(id string) (object map[string]interface{}, err error) {
+	client := s.client
+	var request map[string]interface{}
+	var response map[string]interface{}
+	var query map[string]interface{}
+	request = make(map[string]interface{})
+	query = make(map[string]interface{})
+
+	action := "DescribeMonitorAccounts"
+
+	wait := incrementalWait(3*time.Second, 5*time.Second)
+	err = resource.Retry(1*time.Minute, func() *resource.RetryError {
+		response, err = client.RpcPost("Sas", "2018-12-03", action, query, request, true)
+
+		if err != nil {
+			if NeedRetry(err) {
+				wait()
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	addDebug(action, response, request)
+	if err != nil {
+		return object, WrapErrorf(err, DefaultErrorMsg, id, action, AlibabaCloudSdkGoERROR)
+	}
+
+	v, err := jsonpath.Get("$", response)
+	if err != nil {
+		return object, WrapErrorf(err, FailedGetAttributeMsg, id, "$", response)
+	}
+
+	return v.(map[string]interface{}), nil
+}
+
+func (s *ThreatDetectionServiceV2) ThreatDetectionMonitorAccountStateRefreshFunc(id string, field string, failStates []string) resource.StateRefreshFunc {
+	return s.ThreatDetectionMonitorAccountStateRefreshFuncWithApi(id, field, failStates, s.DescribeThreatDetectionMonitorAccount)
+}
+
+func (s *ThreatDetectionServiceV2) ThreatDetectionMonitorAccountStateRefreshFuncWithApi(id string, field string, failStates []string, call func(id string) (map[string]interface{}, error)) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		object, err := call(id)
+		if err != nil {
+			if NotFoundError(err) {
+				return object, "", nil
+			}
+			return nil, "", WrapError(err)
+		}
+		v, err := jsonpath.Get(field, object)
+		currentStatus := fmt.Sprint(v)
+
+		if strings.HasPrefix(field, "#") {
+			v, _ := jsonpath.Get(strings.TrimPrefix(field, "#"), object)
+			if v != nil {
+				currentStatus = "#CHECKSET"
+			}
+		}
+
+		for _, failState := range failStates {
+			if currentStatus == failState {
+				return object, currentStatus, WrapError(Error(FailedToReachTargetStatus, currentStatus))
+			}
+		}
+		return object, currentStatus, nil
+	}
+}
+
+// DescribeThreatDetectionMonitorAccount >>> Encapsulated.
