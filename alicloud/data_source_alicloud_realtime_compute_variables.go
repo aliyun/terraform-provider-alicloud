ffeb08a76fea91c0ec36d84883d4fdf7f2d38534
zhang.zx@alibaba-inc.com <zhang.zx@alibaba-inc.com>
2026-09-24 21:19:27 +0800

    chore: migrate merged upstream files from helper/resource to helper/retry

--- alicloud/data_source_alicloud_realtime_compute_variables.go
+++ alicloud/data_source_alicloud_realtime_compute_variables.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -125,14 +125,14 @@ func dataSourceAliCloudRealtimeComputeVariableRead(d *schema.ResourceData, meta 
 
 	for {
 		wait := incrementalWait(3*time.Second, 5*time.Second)
-		err = resource.Retry(d.Timeout(schema.TimeoutRead), func() *resource.RetryError {
+		err = retry.Retry(d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
 			response, err = client.RoaGet("ververica", "2022-07-18", action, request, header, nil)
 			if err != nil {
 				if NeedRetry(err) {
 					wait()
-					return resource.RetryableError(err)
+					return retry.RetryableError(err)
 				}
-				return resource.NonRetryableError(err)
+				return retry.NonRetryableError(err)
 			}
 			return nil
 		})
